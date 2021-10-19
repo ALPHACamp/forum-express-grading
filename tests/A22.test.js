c7a336da58970fb98f516deb9739c49ee8635de2
Stanney <freehourse2sh@gmail.com>
2021-10-19 12:22:31 +0800

    add comments for A22

--- tests/A22.test.js
+++ tests/A22.test.js
@@ -22,7 +22,7 @@ const mockResponse = () => {
     render: sinon.spy(),
   }
 };
-
+// 模擬的餐廳資料
 let mockRestaurantData = [{
   id: 1,
   name: 'Restaurant1',
@@ -48,6 +48,7 @@ describe('# A22: TOP 10 人氣餐廳 ', function () {
 
   context('# [網址正確、畫面正常執行]', () => {
     before(async () => {
+      // 模擬驗證資料
       this.ensureAuthenticated = sinon.stub(
         helpers, 'ensureAuthenticated'
       ).returns(true);
@@ -60,11 +61,14 @@ describe('# A22: TOP 10 人氣餐廳 ', function () {
       request(app)
         .get('/restaurants/top')
         .end(function (err, res) {
+          // 檢查回傳的資料中，是否有 Top 10 人氣餐廳資訊
+          res.text.should.include('Top 10 人氣餐廳')  // 可以正確執行，沒有 error 即可通過
           done()
         });
     });
 
     after(async () => {
+      // 清除模擬驗證資料
       this.ensureAuthenticated.restore();
       this.getUser.restore();
     })
@@ -73,6 +77,7 @@ describe('# A22: TOP 10 人氣餐廳 ', function () {
 
   context('# [當你點擊畫面上的「加入最愛 / 移除最愛」按鈕時，會重新計算「收藏數」的數字]', () => {
     before(async () => {
+      // 模擬驗證資料
       this.ensureAuthenticated = sinon.stub(
         helpers, 'ensureAuthenticated'
       ).returns(true);
@@ -80,27 +85,33 @@ describe('# A22: TOP 10 人氣餐廳 ', function () {
         helpers, 'getUser'
       ).returns({ id: 1, Followings: [], FavoritedRestaurants: [] });
 
+      // 模擬 Restaurant db 資料
       this.restaurantMock = dbMock.define('Restaurant');
       this.restaurantMock.$queryInterface.$useHandler((query, queryOptions,done) => {
         if (query === 'findAll') {
           return Promise.resolve(mockRestaurantData.map(d => this.restaurantMock.build(d)));
         }
       });
+
+      // 將 restController 中的 Restaurant db 取代成 Restaurant mock db
       this.restController = proxyquire('../controllers/restController', {
         '../models': {
           Restaurant: this.restaurantMock,
         }
       });
 
+      // 模擬 Favorite db 資料
       this.favoriteMock = dbMock.define('Favorite');
       this.favoriteMock.create = this.favoriteMock.upsert;
       this.favoriteMock.$queryInterface.$useHandler((query, queryOptions,done) => {
         if (query === 'upsert') {
+          // 新增 favortie 資料到模擬資料
           const {UserId, RestaurantId} = queryOptions[0];
           const restaurant = mockRestaurantData.find(d => d.id === RestaurantId);
           restaurant.FavoritedUsers.push({UserId: UserId});
           return Promise.resolve(mockRestaurantData.map(d => this.restaurantMock.build(d)));
         } else if (query === 'destroy') {
+          // 刪除模擬資料中的某一筆 favortie 資料
           const {UserId, RestaurantId} = queryOptions[0].where;
           const restaurant = mockRestaurantData.find(d => d.id === RestaurantId);
           restaurant.FavoritedUsers = restaurant.FavoritedUsers.filter(d => !(d.UserId === UserId))
@@ -108,32 +119,42 @@ describe('# A22: TOP 10 人氣餐廳 ', function () {
         }
       });
 
+      // 將 userController 中的 Favorite db 取代成 Favorite mock db
       this.userController = proxyquire('../controllers/userController', {
         '../models': {Favorite: this.favoriteMock}
       })
     })
 
     it(" POST favorite ", async () => {
+      // 模擬 request 新增 user id = 1, 喜愛 restaurant id = 2
       const req = mockRequest({user: { FavoritedRestaurants: []}, params: {restaurantId: 2}});
       const res = mockResponse();
 
+      // call addFavorite
       await this.userController.addFavorite(req, res);
+
+      // 確認 getTopRestaurant 取得餐廳 2 的 favorite 數量會不同 
       await this.restController.getTopRestaurant(req, res);
 
       res.render.getCall(0).args[1].restaurants[1].FavoritedCount.should.equal(1);
     })
 
     it(" DELETE favorite ", async() => {
+      // 模擬 request 新增 user id = 1, 不喜愛 restaurant id = 1
       const req = mockRequest({user: { FavoritedRestaurants: []}, params: {restaurantId: 1}});
       const res = mockResponse();
 
+      // call removeFavorite
       await this.userController.removeFavorite(req, res);
+      // 取得餐廳排序資料
       await this.restController.getTopRestaurant(req, res);
 
+      // 確認餐廳排序有變，第一個餐廳資料 id = 2
       res.render.getCall(0).args[1].restaurants[0].id.should.equal(2);
     })
 
     after(async () => {
+      // 清除模擬驗證資料
       this.ensureAuthenticated.restore();
       this.getUser.restore();
     })
