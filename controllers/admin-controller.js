ef28a000b8ba3cb93f4d2bcc8c4671e9506f7771
Carrot7712 <carrot7712@gmail.com>
2022-01-07 19:10:24 +0800

    feat: create restaurant

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -7,6 +7,26 @@ const adminController = {
     })
       .then(restaurants => res.render('admin/restaurants', { restaurants }))
       .catch(err => next(err))
+  },
+  createRestaurant: (req, res) => {
+    return res.render('admin/create-restaurant')
+  },
+  postRestaurant: (req, res, next) => {
+    const { name, tel, address, openingHours, description } = req.body
+    if (!name) throw new Error('Restaurant name is required!')
+
+    Restaurant.create({
+      name,
+      tel,
+      address,
+      openingHours,
+      description
+    })
+      .then(() => {
+        req.flash('success_messages', 'restaurant was successfully created')
+        res.redirect('/admin/restaurants')
+      })
+      .catch(err => next(err))
   }
 }
 
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -3,9 +3,10 @@ const router = express.Router()
 
 const adminController = require('../../controllers/admin-controller')
 
-const { authenticatedAdmin } = require('../../middleware/auth')
+router.get('/restaurants/create', adminController.createRestaurant)
+router.get('/restaurants', adminController.getRestaurants)
+router.post('/restaurants', adminController.postRestaurant)
 
-router.get('/restaurants', authenticatedAdmin, adminController.getRestaurants)
 router.get('/', (req, res) => res.redirect('/admin/restaurants'))
 
 module.exports = router
