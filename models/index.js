a49f0c1f50fd3fb70331965fa3fd1e970ee4fe3f
Carrot7712 <carrot7712@gmail.com>
2022-01-10 18:58:08 +0800

    test 用舊版的 models/index.js

--- models/index.js
+++ models/index.js
@@ -1,13 +1,11 @@
 'use strict'
-
 const fs = require('fs')
 const path = require('path')
 const Sequelize = require('sequelize')
 const basename = path.basename(__filename)
 const env = process.env.NODE_ENV || 'development'
-const config = require(path.resolve(__dirname, '../config/config.json'))[env]
+const config = require(__dirname + '/../config/config.json')[env]
 const db = {}
-
 // 資料庫連線
 let sequelize
 if (config.use_env_variable) {
@@ -15,7 +13,6 @@ if (config.use_env_variable) {
 } else {
   sequelize = new Sequelize(config.database, config.username, config.password, config)
 }
-
 // 動態引入其他 models
 fs
   .readdirSync(__dirname)
@@ -23,19 +20,16 @@ fs
     return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js')
   })
   .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes)
+    const model = sequelize.import(path.join(__dirname, file))
     db[model.name] = model
   })
-
 // 設定 Models 之間的關聯
 Object.keys(db).forEach(modelName => {
   if (db[modelName].associate) {
     db[modelName].associate(db)
   }
 })
-
 // 匯出需要的物件
 db.sequelize = sequelize
 db.Sequelize = Sequelize
-
 module.exports = db
