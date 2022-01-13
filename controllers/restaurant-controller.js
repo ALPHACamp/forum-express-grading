c34dac81da3963649bb2485bf148a54a5615c154
Carrot7712 <carrot7712@gmail.com>
2022-01-13 19:13:26 +0800

    finish R03

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -59,14 +59,15 @@ const restaurantController = {
   },
   getDashboard: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
-      include: Category,
-      nest: true,
-      raw: true
+      include: [
+        Category,
+        { model: Comment, include: User }
+      ]
     })
       .then(restaurant => {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
 
-        res.render('dashboard', { restaurant })
+        res.render('dashboard', { restaurant: restaurant.toJSON() })
       })
       .catch(err => next(err))
   }
