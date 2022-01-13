12f26a44bd64a1ba8662d99ad71547921ae0db27
Carrot7712 <carrot7712@gmail.com>
2022-01-13 16:48:07 +0800

    feat: add R01.test.js and unit-test-helpers.js

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -2,14 +2,13 @@ const router = require('express').Router()
 const adminController = require('../../controllers/admin-controller')
 const upload = require('../../middleware/multer')
 
-
 router.get(
   '/restaurants/create',
   adminController.createRestaurant
 )
 router.get('/restaurants/:id/edit', adminController.editRestaurant)
 router.get('/restaurants/:id', adminController.getRestaurant)
-router.put('/restaurants/:id',upload.single('image'), adminController.putRestaurant)
+router.put('/restaurants/:id', upload.single('image'), adminController.putRestaurant)
 router.delete('/restaurants/:id', adminController.deleteRestaurant)
 router.get('/restaurants', adminController.getRestaurants)
 router.post('/restaurants', upload.single('image'), adminController.postRestaurant)
