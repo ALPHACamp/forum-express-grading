16608eade5715453411179b9bda041f9e8dc2c78
phiphi0912 <610733005@gms.ndhu.edu.tw>
2022-01-18 11:22:52 +0800

    feat: Create a website admin entry

---
+++ routes/modules/admin.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+
+const adminController = require('../../controllers/admin-controller')
+
+router.get('/restaurants', adminController.getRestaurants)
+router.get('', (req, res) => res.redirect('/admin/restaurants'))
+
+module.exports = router
