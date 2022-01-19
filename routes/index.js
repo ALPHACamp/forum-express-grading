86ea5b67a88025e5a9204069ce110f0c1d9acbf3
lavender <id0421942@gmail.com>
2022-01-20 05:10:59 +0800

    passport init & signin

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,18 @@
 const express = require('express')
 const router = express.Router()
+const passport = require('../config/passport')
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
 const { generalErrorHandler } = require('../middleware/error-handler')
 
 const admin = require('./modules/admin')
 
+router.get('/signin', userController.signInPage)
+router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
+router.get('/logout', userController.logout)
 router.use('/admin', admin)
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
 router.get('/restaurants', restController.getRestaurants)
-router.use('/', (req, res) => res.redirect('/restaurants'))
-router.use('/', generalErrorHandler)
 
 module.exports = router
