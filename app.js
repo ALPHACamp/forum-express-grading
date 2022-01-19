d8e3b39384eef28ccd76a28d16b6f0a3a1d0fd84
phiphi0912 <610733005@gms.ndhu.edu.tw>
2022-01-19 19:48:21 +0800

    feat: add edit function

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const routes = require('./routes')
 const session = require('express-session')
 const flash = require('connect-flash')
 const passport = require('./config/passport')
+const methodOverride = require('method-override')
 
 const { getUser } = require('./helpers/auth-helpers')
 
@@ -25,6 +26,7 @@ app.use(session({
 app.use(passport.initialize())
 app.use(passport.session())
 app.use(flash())
+app.use(methodOverride('_method'))
 
 app.use((req, res, next) => {
   res.locals.success_msg = req.flash('success_msg')
