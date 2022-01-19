e707bb8e28a3b006e1836b4758ffbc6666b6d06e
Richard Widjaya <ricwidjaya@gmail.com>
2022-01-19 19:14:57 +0800

    feat: create restaurant

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -2,9 +2,32 @@ const { Restaurant } = require('../models')
 
 const adminController = {
   getRestaurants: (req, res, next) => {
-    Restaurant.findAll({ raw: true }).then(restaurants => {
-      return res.render('admin/restaurants', { restaurants })
-    }).catch(err => next(err))
+    Restaurant.findAll({ raw: true })
+      .then(restaurants => {
+        return res.render('admin/restaurants', { restaurants })
+      })
+      .catch(err => next(err))
+  },
+
+  createRestaurant: (req, res) => {
+    return res.render('admin/create-restaurant')
+  },
+
+  postRestaurant: (req, res, next) => {
+    const { name, tel, address, openingHours, description } = req.body
+    if (!name) throw new Error('Restaurant name is required!')
+    Restaurant.create({
+      name,
+      tel,
+      address,
+      openingHours,
+      description
+    })
+      .then(() => {
+        req.flash('success_messages', 'restaurant was successfully created')
+        return res.redirect('/admin/restaurants')
+      })
+      .catch(err => next(err))
   }
 }
 
