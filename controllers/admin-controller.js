9e002923f0f9d899ed6fd40a392e32c902c3e2c0
Richie-Yang <richieyang8813@gmail.com>
2022-01-21 10:06:06 +0800

    feat: create restaurant

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -5,6 +5,27 @@ const adminController = {
     return Restaurant.findAll({ raw: true })
       .then(restaurants => res.render('admin/restaurants', { restaurants }))
       .catch(err => next(err))
+  },
+
+  createRestaurant: (req, res) => {
+    return res.render('admin/create-restaurant.hbs')
+  },
+
+  postRestaurant: (req, res, next) => {
+    const {
+      name, tel, address, openingHours, description
+    } = req.body
+
+    if (!name) throw new Error('Restaurant name is required!')
+
+    return Restaurant.create({
+      name, tel, address, openingHours, description
+    })
+      .then(() => {
+        req.flash('success_messages', 'Restaurant was successfully created')
+        res.redirect('/admin/restaurants')
+      })
+      .catch(err => next(err))
   }
 }
 
