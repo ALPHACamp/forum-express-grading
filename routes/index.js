7734b1ba31dbd92cad98dc1a04ed8ea9d712d736
songerard <songerard@gmail.com>
2022-01-22 15:38:39 +0800

    feat: user singup

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,9 @@ const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
 const admin = require('./modules/admin')
 
-router.use('/admin', admin)
+router.get('/admin', admin)
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
 router.get('/restaurants', restController.getRestaurants)
-router.use('/', (req, res) => res.redirect('/restaurants'))
+router.get('/', (req, res) => res.redirect('/restaurants'))
 module.exports = router
