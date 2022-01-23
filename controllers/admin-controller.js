8a2687b6f067b085b4cb717721e48db210843503
shibao <dream0184@yahoo.com.tw>
2022-01-24 06:44:24 +0800

    feat: modify admin restaurants page

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,5 +1,11 @@
+const { Restaurant } = require('../models')
 const adminController = {
-  getRestaurants: (req, res) => {
+  getRestaurants: (req, res, next) => {
+    Restaurant.findAll({
+      raw: true
+    })
+      .then(restaurants => res.render('admin/restaurants', { restaurants }))
+      .catch(err => next(err))
     return res.render('admin/restaurants')
   }
 }
