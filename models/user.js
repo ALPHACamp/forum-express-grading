fd13a45eaeffdb1fbb933a196405cf3522330a72
shibao <dream0184@yahoo.com.tw>
2022-01-24 05:30:24 +0800

    feat: user model add is_admin

--- models/user.js
+++ models/user.js
@@ -4,11 +4,6 @@ const {
 } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate (models) {
       // define association here
     }
@@ -16,7 +11,8 @@ module.exports = (sequelize, DataTypes) => {
   User.init({
     name: DataTypes.STRING,
     email: DataTypes.STRING,
-    password: DataTypes.STRING
+    password: DataTypes.STRING,
+    isAdmin: DataTypes.BOOLEAN
   }, {
     sequelize,
     modelName: 'User',
