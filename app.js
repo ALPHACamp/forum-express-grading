fb987dbd4982dec38c00192ae616049be7237608
shibao <dream0184@yahoo.com.tw>
2022-01-23 19:03:32 +0800

    feat: passport init & signin

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const handlebars = require('express-handlebars')
 const flash = require('connect-flash')
 const session = require('express-session')
+const passport = require('./config/passport')
 const routes = require('./routes')
 const app = express()
 const port = process.env.PORT || 3000
@@ -15,6 +16,8 @@ app.use(session({
   resave: false,
   saveUninitialized: false
 }))
+app.use(passport.initialize())
+app.use(passport.session())
 app.use(flash())
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
