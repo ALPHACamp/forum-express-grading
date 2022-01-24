28dcbcffd40c3c3fb4296402f9eb2fa4df855ccb
HY <homtele@gmail.com>
2022-01-25 00:03:03 +0800

    feat: user singup

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,11 @@ const express = require('express')
 const router = express.Router()
 const admin = require('./modules/admin')
 const restController = require('../controllers/restaurant-controller')
+const userController = require('../controllers/user-controller')
 
 router.use('/admin', admin)
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
 router.get('/restaurants', restController.getRestaurants)
 router.use('/', (req, res) => res.redirect('/restaurants'))
 module.exports = router
