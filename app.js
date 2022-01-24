b25467bfabee6e6d2cf374215d6b0e3c668b6e2f
ronnychiang <ronnychiang1164@gmail.com>
2022-01-24 17:11:53 +0800

    feat: add restaurant image

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const passport = require('./config/passport')
 const { getUser } = require('./helpers/auth-helpers')
 const handlebarsHelpers = require('./helpers/handlebars-helpers')
 const methodOverride = require('method-override')
+const path = require('path')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -19,6 +20,7 @@ app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
 app.use(methodOverride('_method'))
+app.use('/upload', express.static(path.join(__dirname, 'upload')))
 app.use(passport.initialize()) // 增加這行，初始化 Passport
 app.use(passport.session()) // 增加這行，啟動 session 功能
 app.use(flash()) // 掛載套件
--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,5 +1,6 @@
-const { Restaurant } = require('../models') // 新增這裡
-const adminController = { // 修改這裡
+const { Restaurant } = require('../models')
+const { localFileHandler } = require('../helpers/file-helpers')
+const adminController = {
   getRestaurants: (req, res, next) => {
     Restaurant.findAll({
       raw: true
@@ -13,13 +14,16 @@ const adminController = { // 修改這裡
   postRestaurant: (req, res, next) => {
     const { name, tel, address, openingHours, description } = req.body  // 從 req.body 拿出表單裡的資料
     if (!name) throw new Error('Restaurant name is required!') // name 是必填，若發先是空值就會終止程式碼，並在畫面顯示錯誤提示
-    Restaurant.create({ //產生一個新的 Restaurant 物件實例，並存入資料庫
-      name,
-      tel,
-      address,
-      openingHours,
-      description
-    })
+    const { file } = req
+    localFileHandler(file)
+      .then(filePath => Restaurant.create({ //產生一個新的 Restaurant 物件實例，並存入資料庫
+        name,
+        tel,
+        address,
+        openingHours,
+        description,
+        image: filePath || null
+      }))
       .then(() => {
         req.flash('success_messages', 'restaurant was successfully created') // 在畫面顯示成功提示
         res.redirect('/admin/restaurants') //新增完成後導回後台首頁
@@ -49,15 +53,20 @@ const adminController = { // 修改這裡
   putRestaurant: (req, res, next) => {
     const { name, tel, address, openingHours, description } = req.body
     if (!name) throw new Error('Restaurant name is required!')
-    Restaurant.findByPk(req.params.id)
-      .then(restaurant => {
+    const { file } = req
+    Promise.all([
+      Restaurant.findByPk(req.params.id),
+      localFileHandler(file)
+    ])
+      .then(([restaurant, filePath]) => {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
         return restaurant.update({
           name,
           tel,
           address,
           openingHours,
-          description
+          description,
+          image: filePath || restaurant.image
         })
       })
       .then(() => {
