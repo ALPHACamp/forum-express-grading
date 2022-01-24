42785f05233f14af268b69bc964929705ae04a01
Richie-Yang <richieyang8813@gmail.com>
2022-01-24 13:21:32 +0800

    feat: add user permission management

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,4 +1,4 @@
-const { Restaurant } = require('../models')
+const { Restaurant, User } = require('../models')
 const { imgurFileHandler } = require('../helpers/file-helpers')
 
 const adminController = {
@@ -103,6 +103,31 @@ const adminController = {
         res.redirect('/admin/restaurants')
       })
       .catch(err => next(err))
+  },
+
+  getUsers: (req, res, next) => {
+    return User.findAll({ raw: true, nest: true })
+      .then(users => res.render('admin/users', { users }))
+      .catch(err => next(err))
+  },
+
+  patchUser: async (req, res, next) => {
+    try {
+      const { id } = req.params
+
+      const user = await User.findByPk(id)
+      if (!user) throw new Error("User didn't exists")
+
+      const { email, isAdmin } = user.dataValues
+      if (email === 'root@example.com') {
+        req.flash('error_messages', '禁止變更 root 權限')
+        return res.redirect('back')
+      }
+
+      await user.update({ isAdmin: !isAdmin })
+      req.flash('success_messages', '使用者權限變更成功')
+      return res.redirect('/admin/users')
+    } catch (err) { next(err) }
   }
 }
 
