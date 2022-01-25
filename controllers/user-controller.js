a44454e2ad0fe624ee390334c17a55dc4ae4eccf
diosleonor <tccoolovl@gmail.com>
2022-01-25 15:44:10 +0800

    feat: flash msg & signup verification

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -5,16 +5,23 @@ const userController = {
   signUpPage: (req, res) => {
     res.render('signup')
   },
-  signUp: (req, res) => {
-    bcrypt.hash(req.body.password, 10)
+  signUp: (req, res, next) => {
+    if (req.body.password !== req.body.passwordCheck) throw new Error('Passwords do not match!')
+    User.findOne({ where: { email: req.body.email } })
+      .then(user => {
+        if (user) throw new Error('Email already exists!')
+        return bcrypt.hash(req.body.password, 10)
+      })
       .then(hash => User.create({
         name: req.body.name,
         email: req.body.email,
         password: hash
       }))
       .then(() => {
+        req.flash('success_messages', 'Account successfully created.')
         res.redirect('signin')
       })
+      .catch(err => next(err))
   }
 }
 module.exports = userController
--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,12 @@ const router = express.Router()
 const restaurantController = require('../controllers/restaurant-controller.js')
 const userController = require('../controllers/user-controller.js')
 const admin = require('./modules/admin')
-
+const { generalErrorHandler } = require('../middleware/error-handler')
 router.use('/admin', admin)
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
 router.get('/restaurants', restaurantController.getRestaurants)
-
+router.use('/', generalErrorHandler)
 router.get('/', (req, res) => res.redirect('/restaurants'))
 
 module.exports = router
