abe8684b5e8b703c1a155fcbe24ffff69a25add0
Dandy <lavenderflyyaya@gmail.com>
2022-01-25 22:21:33 +0800

    feat:add admin index page

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const router = express.Router()
-// 新增，載入 controller
+
 const restController = require('../controllers/restaurant-controller')
-// 新增
+const admin = require('./modules/admin')
+
+router.use('/admin', admin)
+
 router.get('/restaurants', restController.getRestaurants)
-router.use('/', (req, res) => res.redirect('/restaurants'))
+router.get('/', (req, res) => res.redirect('/restaurants'))
+
 module.exports = router
