c2da941a8c44d3f46fb268c057c95e3c7c32cf29
deansyue <c1318131801@gmail.com>
2022-01-25 23:31:42 +0800

    feat: modify admin restaurants page

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,7 +1,17 @@
+// 引入model
+const { Restaurant } = require('../models')
+
 const adminController = {
   // 瀏覽後台網頁
-  getRestaurants: (req, res) => {
-    return res.render('admin/restaurants')
+  getRestaurants: (req, res, next) => {
+    // 查詢Restaurant所有資料
+    Restaurant.findAll({
+      // 去除sequelize物件實例
+      raw: true
+    })
+      // 渲染admin/restaurants畫面
+      .then(restaurants => res.render('admin/restaurants', { restaurants }))
+      .catch(err => next(err))
   }
 }
 
