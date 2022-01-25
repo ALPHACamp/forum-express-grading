4bb7385456991c8710498476dc951cdf10822b9d
hmrvc <hmrvc@yahoo.com.tw>
2022-01-25 17:12:28 +0800

    feat: passport init & signin

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,22 @@ const router = express.Router()
 const restaurantController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
 const { generalErrorHandler } = require('../middleware/error-handler')
+const passport = require('../config/passport')
 const admin = require('./modules/admin')
 // router.get('/', (req, res) => {
 //   res.send('Hello World!')
 // })
 router.use('/admin', admin)
+// 註冊路由
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
+// 登入登出路由
+router.get('/signin', userController.signInPage)
+router.post('/signin', passport.authenticate('local', {
+  failureRedirect: '/signin',
+  failureFlash: true
+}), userController.signIn)
+router.get('/logout', userController.logout)
 router.get('/restaurants', restaurantController.getRestaurants)
 router.use('/', (req, res) => res.redirect('/restaurants'))
 router.use('/', generalErrorHandler)
