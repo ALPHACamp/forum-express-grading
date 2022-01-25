a3102ca717b17c4b8b2c6878b6c6616b65070ab5
diosleonor <tccoolovl@gmail.com>
2022-01-25 14:47:30 +0800

    feat: user signup

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const app = express()
 const port = process.env.PORT || 3000
 app.engine('hbs', exphbs({ extname: '.hbs' }))
 app.set('view engine', 'hbs')
+app.use(express.urlencoded({ extended: true }))
 app.use(routes)
 
 app.listen(port, () => {
