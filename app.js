459c0daa8ad11058c213dfa37aa97a1fd5cb5df1
hmrvc <hmrvc@yahoo.com.tw>
2022-01-25 10:13:57 +0800

    feat: user signup

--- app.js
+++ app.js
@@ -5,11 +5,9 @@ const routes = require('./routes')
 const app = express()
 const port = process.env.PORT || 3000
 
-const db = require('./models')
-
 app.engine('hbs', handlebars({ extname: '.hbs' }))
 app.set('view engine', 'hbs')
-
+app.use(express.urlencoded({ extended: true }))
 app.use(routes)
 
 app.listen(port, () => {
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const router = express.Router()
 const restaurantController = require('../controllers/restaurant-controller')
+const userController = require('../controllers/user-controller')
 const admin = require('./modules/admin')
 // router.get('/', (req, res) => {
 //   res.send('Hello World!')
 // })
 router.use('/admin', admin)
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
 router.get('/restaurants', restaurantController.getRestaurants)
 router.use('/', (req, res) => res.redirect('/restaurants'))
 
