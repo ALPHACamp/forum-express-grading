789f877a1c45a8575e0fd9feee37e80cb2444dfa
lavender <id0421942@gmail.com>
2022-01-26 12:32:17 +0800

    add user permission management

--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -1,5 +1,8 @@
 const dayjs = require('dayjs') // 載入 dayjs 套件
 
+const currentYear = () => dayjs().year()
+// 取得當年年份作為 currentYear 的屬性值，並導出
+
 module.exports = {
-  currentYear: () => dayjs().year() // 取得當年年份作為 currentYear 的屬性值，並導出
+  currentYear
 }
