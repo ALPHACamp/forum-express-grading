bd472c7c234cc8064ad682db482caa98912a64e6
xnubber <aabbccdd10902@gmail.com>
2022-01-26 15:30:23 +0800

    feat: add user signup

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,13 @@ const app = require('../app')
 const router = express.Router()
 
 const restaurantController = require('../controllers/restaurant-controller')
+const userController = require('../controllers/user-controller')
 const admin = require('./modules/admin')
 
 router.use('/admin', admin)
-router.use('/restaurants', restaurantController.getRestaurants)
-router.use('/', (req, res) => { res.redirect('/restaurants') })
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
+router.get('/restaurants', restaurantController.getRestaurants)
+router.get('/', (req, res) => { res.redirect('/restaurants') })
 
 module.exports = router
