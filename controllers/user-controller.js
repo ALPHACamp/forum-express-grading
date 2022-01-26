d1cc06772513c0fac002e156047570f418b2cee4
DAI <senseye935@gmail.com>
2022-01-26 22:17:30 +0800

    feat: passport init & signin

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -19,10 +19,22 @@ const userController = {
         password: hash
       }))
       .then(() => {
-        req.flash('success_messages', '成功註冊帳號!')
+        req.flash('success_messages', '成功註冊帳號！')
         res.redirect('/signin')
       })
       .catch(err => next(err))
+  },
+  signInPage: (req, res) => {
+    res.render('signin')
+  },
+  signIn: (req, res) => {
+    req.flash('success_messages', '成功登入！')
+    res.redirect('/restaurants')
+  },
+  logout: (req, res) => {
+    req.flash('success_messages', '登出成功！')
+    req.logout()
+    res.redirect('/signin')
   }
 }
 module.exports = userController
