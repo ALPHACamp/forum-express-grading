425c4df3e21dbbe3474b93f4dfcf0c8e2f54475e
Richie-Yang <richieyang8813@gmail.com>
2022-01-26 17:02:40 +0800

    feat: add restaurants index page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,6 +1,23 @@
+const { Restaurant, Category } = require('../models')
+
 const restaurantController = {
-  getRestaurants: (req, res) => {
-    return res.render('restaurants')
+  getRestaurants: (req, res, next) => {
+    return Restaurant.findAll({
+      raw: true,
+      nest: true,
+      include: [Category]
+    })
+      .then(restaurants => {
+        const data = restaurants.map(r => ({
+          ...r,
+          description: r.description.substring(0, 50) + '...'
+        }))
+
+        return res.render('restaurants', {
+          restaurants: data
+        })
+      })
+      .catch(err => next(err))
   }
 }
 
