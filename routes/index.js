533475d326cded0273ef4b3557538728856daf06
Kemal-Wuzhi <camelwuzhi@gmail.com>
2022-01-26 21:08:25 +0800

    feat:user model add is_admin

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,9 @@ const router = express.Router()
 const passport = require('../config/passport') // 引入 Passport，需要他幫忙做驗證
 const admin = require('./modules/admin')
 const userController = require('../controllers/user-controller')
+const {
+  authenticated
+} = require('../middleware/auth')
 const restController = require('../controllers/restaurant-controller')
 const {
   generalErrorHandler
@@ -17,7 +20,7 @@ router.post('/signin', passport.authenticate('local', {
   failureFlash: true
 }), userController.signIn) // 注意是 Post
 router.get('/logout', userController.logout)
-router.get('/restaurants', restController.getRestaurants)
+router.get('/restaurants', authenticated, restController.getRestaurants)
 router.use('/', (req, res) => res.redirect('/restaurants'))
 router.use('/', generalErrorHandler)
 
