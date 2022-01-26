56ebfd7603f5db6fad49bcfd07a8af87e8638517
Kemal-Wuzhi <camelwuzhi@gmail.com>
2022-01-26 19:30:39 +0800

    feat:passport init & signin

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const passport = require('../config/passport') // 引入 Passport，需要他幫忙做驗證
 const admin = require('./modules/admin')
 const userController = require('../controllers/user-controller')
 const restController = require('../controllers/restaurant-controller')
@@ -10,6 +11,12 @@ const {
 router.use('/admin', admin)
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
+router.get('/signin', userController.signInPage)
+router.post('/signin', passport.authenticate('local', {
+  failureRedirect: '/signin',
+  failureFlash: true
+}), userController.signIn) // 注意是 Post
+router.get('/logout', userController.logout)
 router.get('/restaurants', restController.getRestaurants)
 router.use('/', (req, res) => res.redirect('/restaurants'))
 router.use('/', generalErrorHandler)
