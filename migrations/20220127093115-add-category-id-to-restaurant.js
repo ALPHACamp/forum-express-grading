18587b792c3b66b83c70a2fee1fb861407c8b1ef
lavender <id0421942@gmail.com>
2022-01-27 17:33:44 +0800

    add category model

---
+++ migrations/20220127093115-add-category-id-to-restaurant.js
@@ -0,0 +1,23 @@
+'use strict'
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('Restaurants', 'category_id', {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Categories',
+        key: 'id'
+      }
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+}
