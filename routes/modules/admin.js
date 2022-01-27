3b27d5e3efbe3e27efb177228c6cca1c8080745b
martinchiu <martin87713@gmail.com>
2022-01-27 21:51:27 +0800

    delete restaurant

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -5,10 +5,16 @@ const adminController = require('../../controllers/admin-controller')
 
 router.get('/restaurants/create', adminController.createRestaurant)
 router.post('/restaurants', adminController.postRestaurant)
+
 router.get('/restaurants/:id/edit', adminController.editRestaurant)
 router.put('/restaurants/:id', adminController.putRestaurant)
+
+router.delete('/restaurants/:id', adminController.deleteRestaurant)
+
 router.get('/restaurants/:id', adminController.getRestaurant)
+
 router.get('/restaurants', adminController.getRestaurants)
+
 router.get('/', (req, res) => res.redirect('/admin/restaurants'))
 
 module.exports = router
