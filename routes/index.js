c76139f8004ab5de37c7f2cf5d1efef54cd38b1d
xnubber <aabbccdd10902@gmail.com>
2022-01-27 11:18:00 +0800

    feat: flash msg & signup verification

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const router = express.Router()
 
 const restaurantController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
+const { generalErrorHandler } = require('../middleware/error-handler')
 const admin = require('./modules/admin')
 
 router.use('/admin', admin)
@@ -11,5 +12,6 @@ router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
 router.get('/restaurants', restaurantController.getRestaurants)
 router.get('/', (req, res) => { res.redirect('/restaurants') })
+router.use('/', generalErrorHandler)
 
 module.exports = router
