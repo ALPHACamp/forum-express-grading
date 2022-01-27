b587b1d8935b4c1df078370ab9f70cca6a0044cd
diosleonor <tccoolovl@gmail.com>
2022-01-27 15:01:43 +0800

    feat: add restaurant image

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const flash = require('connect-flash')
 const methodOverride = require('method-override')
@@ -18,6 +19,7 @@ app.use(passport.initialize())
 app.use(passport.session())
 app.use(flash())
 app.use(methodOverride('_method'))
+app.use('/upload', express.static(path.join(__dirname, 'upload')))
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
   res.locals.error_messages = req.flash('error_messages')
--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,4 +1,5 @@
 const { Restaurant } = require('../models')
+const { localFileHandler } = require('../helpers/file-helpers')
 const adminController = {
   getRestaurants: (req, res, next) => {
     Restaurant.findAll({
@@ -15,7 +16,11 @@ const adminController = {
   postRestaurant: (req, res, next) => {
     const { name, tel, address, openingHours, description } = req.body
     if (!name) throw new Error('Restaurant name is required!')
-    Restaurant.create({ name, tel, address, openingHours, description })
+    const { file } = req
+    localFileHandler(file)
+      .then(filePath => {
+        Restaurant.create({ name, tel, address, openingHours, description, image: filePath || null })
+      })
       .then(() => {
         req.flash('success_messages', 'Restaurant was successfully created!')
         res.redirect('/admin/restaurants')
@@ -41,16 +46,19 @@ const adminController = {
   putRestaurant: (req, res, next) => {
     const { name, tel, address, openingHours, description } = req.body
     if (!name) throw new Error('Restaurant name is required!')
-    Restaurant.findByPk(req.params.id)
-      .then(restaurant => {
+    const { file } = req
+    Promise.all([
+      Restaurant.findByPk(req.params.id), localFileHandler(file)
+    ])
+      .then(([restaurant, filePath]) => {
         if (!restaurant) throw new Error('Restaurant did not exist!')
-        return restaurant.update({ name, tel, address, openingHours, description })
-          .then(() => {
-            req.flash('success_messages', 'Restaurant was successfully updated!')
-            res.redirect('/admin/restaurants')
-          })
-          .catch(err => next(err))
+        return restaurant.update({ name, tel, address, openingHours, description, image: filePath || restaurant.image })
       })
+      .then(() => {
+        req.flash('success_messages', 'Restaurant was successfully updated!')
+        res.redirect('/admin/restaurants')
+      })
+      .catch(err => next(err))
   },
   deleteRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id)
