518eb6fc0ea47969223cb99ad068e262018592ca
Kemal-Wuzhi <camelwuzhi@gmail.com>
2022-01-27 22:36:44 +0800

    feat: admin restaurant page

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,10 +1,55 @@
 const {
   Restaurant
 } = require('../models')
-
 const adminController = {
-  getRestaurants: (req, res) => {
-    Restaurant.findAll({ raw: true })
+  getRestaurants: (req, res, next) => {
+    Restaurant.findAll({
+      raw: true
+    })
+      .then(restaurants => res.render('admin/restaurants', {
+        restaurants
+      }))
+      .catch(err => next(err))
+  },
+  createRestaurant: (req, res) => {
+    return res.render('admin/create-restaurant')
+  },
+  postRestaurant: (req, res, next) => {
+    const {
+      name,
+      tel,
+      address,
+      openingHours,
+      description
+    } = req.body
+    if (!name) throw new Error('Restaurant name is required!')
+
+    Restaurant.create({
+      name,
+      tel,
+      address,
+      openingHours,
+      description
+    })
+      .then(() => {
+        req.flash('success_messages', 'restaurant was successfully created')
+        res.redirect('/admin/restaurants')
+      })
+      .catch(err => next(err))
+  },
+  getRestaurant: (req, res, next) => {
+    Restaurant.findByPk(req.params.id, { // 去資料庫用 id 找一筆資料
+      raw: true // 找到以後整理格式再回傳
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        //  如果找不到，回傳錯誤訊息，後面不執行
+        res.render('admin/restaurant', {
+          restaurant
+        })
+      })
+      .catch(err => next(err))
   }
 }
+
 module.exports = adminController
--- routes/index.js
+++ routes/index.js
@@ -4,14 +4,16 @@ const passport = require('../config/passport') // 引入 Passport，需要他幫
 const admin = require('./modules/admin')
 const userController = require('../controllers/user-controller')
 const {
-  authenticated
+  authenticated,
+  authenticatedAdmin
 } = require('../middleware/auth')
 const restController = require('../controllers/restaurant-controller')
 const {
   generalErrorHandler
 } = require('../middleware/error-handler')
+const adminController = require('../controllers/admin-controller')
 
-router.use('/admin', admin)
+router.use('/admin', authenticatedAdmin, admin)
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
 router.get('/signin', userController.signInPage)
@@ -20,6 +22,7 @@ router.post('/signin', passport.authenticate('local', {
   failureFlash: true
 }), userController.signIn) // 注意是 Post
 router.get('/logout', userController.logout)
+router.get('/restaurants/create', adminController.createRestaurant)
 router.get('/restaurants', authenticated, restController.getRestaurants)
 router.use('/', (req, res) => res.redirect('/restaurants'))
 router.use('/', generalErrorHandler)
