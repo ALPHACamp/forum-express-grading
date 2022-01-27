37fd75b0ebc8486f2f83c8fdae5a04108e821b94
songerard <songerard@gmail.com>
2022-01-27 12:37:55 +0800

    feat: add imgur api

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const passport = require('./config/passport')
 const { getUser } = require('./helpers/auth-helpers')
 const handlebarsHelpers = require('./helpers/handlebars-helpers')
 const routes = require('./routes')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const app = express()
 const port = process.env.PORT || 3000
 const SESSION_SECRET = 'secret'
