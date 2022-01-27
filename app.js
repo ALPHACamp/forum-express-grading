e937f1bbf87f2cf988071ac0289eed8d8c1100b8
z88243310 <z88243310@gmail.com>
2022-01-27 13:47:57 +0800

    feat: add header & footer

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const handlebars = require('express-handlebars')
 const flash = require('connect-flash')
 const session = require('express-session')
 const passport = require('./config/passport')
+const handlebarsHelpers = require('./helpers/handlebars-helpers')
+const { getUser } = require('./helpers/auth-helpers')
 
 const routes = require('./routes')
 
@@ -11,7 +13,7 @@ const port = process.env.PORT || 3000
 const SESSION_SECRET = 'secret'
 
 // 註冊 handlebars 樣板引擎，指定副檔名為 .hbs，接著設定使用 handlebars 作為樣板引擎
-app.engine('hbs', handlebars({ extname: '.hbs' }))
+app.engine('hbs', handlebars({ extname: '.hbs', helpers: handlebarsHelpers }))
 app.set('view engine', 'hbs')
 
 // body-parser & session & passport & flash-msg
@@ -25,6 +27,7 @@ app.use(flash())
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages') // 設定 success_msg 訊息
   res.locals.error_messages = req.flash('error_messages') // 設定 warning_msg 訊息
+  res.locals.user = getUser(req)
   next()
 })
 
