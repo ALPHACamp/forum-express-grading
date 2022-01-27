bf000c1f8b27de13c871aec96ba0e9dbd356a847
LAPTOP-VLRN913M\twang <asakura4@gmail.com>
2022-01-27 00:02:59 -0800

    add signup page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const restaurantController = require('../controllers/restaurant-controllers')
+const userController = require('../controllers/user-controller')
 const admin = require('./modules/admin')
 
 router.use('/admin', admin)
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
 router.get('/restaurants', restaurantController.getRestaurants)
 router.get('/', (req, res) => res.redirect('/restaurants'))
 
