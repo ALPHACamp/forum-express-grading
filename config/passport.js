24b222f9026c9d1b34840e8b128f12037078a0a1
LAPTOP-VLRN913M\twang <asakura4@gmail.com>
2022-01-28 00:20:38 -0800

    passport init & signin

---
+++ config/passport.js
@@ -0,0 +1,36 @@
+const passport = require('passport')
+const LocalStrategy = require('passport-local')
+const bcrpt = require('bcryptjs')
+const db = require('../models')
+const User = db.User
+
+passport.use(new LocalStrategy({
+  usernameField: 'email',
+  passwordField: 'password',
+  passReqToCallback: true
+}, (req, email, password, cb) => {
+  User.findOne({ where: { email } })
+    .then(user => {
+      if (!user) {
+        return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤！'))
+      }
+      bcrpt.compare(password, user.password)
+        .then(res => {
+          if (!res) {
+            return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤！'))
+          }
+          return cb(null, user)
+        })
+    })
+}))
+passport.serializeUser((user, cb) => {
+  return cb(null, user.id)
+})
+
+passport.deserializeUser((id, cb) => {
+  User.findByPk(id).then(user => {
+    return cb(null, user)
+  })
+})
+
+module.exports = passport
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,19 @@ const router = express.Router()
 const restaurantController = require('../controllers/restaurant-controllers')
 const userController = require('../controllers/user-controller')
 const { generalErrorHandler } = require('../middleware/error-handler')
+const passport = require('../config/passport')
 const admin = require('./modules/admin')
 
 router.use('/admin', admin)
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
+router.get('/signin', userController.signInPage)
+router.post('/signin', passport.authenticate('local', {
+  failureRedirect: '/signin',
+  failureFlash: true
+}), userController.signIn)
+router.get('/logout', userController.logout)
+
 router.get('/restaurants', restaurantController.getRestaurants)
 router.get('/', (req, res) => res.redirect('/restaurants'))
 router.use('/', generalErrorHandler)
