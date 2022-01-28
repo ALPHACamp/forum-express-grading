37f31c860e65a90008f8aead281af19f5d6929ba
eklipsorz <eklipsorz@protonmail.ch>
2022-01-28 16:37:11 +0800

    feat: create restaurant

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -3,6 +3,8 @@ const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
 
 router.get('/restaurants', adminController.getRestaurants)
+router.get('/restaurants/create', adminController.createRestaurant)
+router.post('/restaurants', adminController.postRestaurant)
 router.get('/', (req, res) => res.redirect('/admin/restaurants'))
 
 exports = module.exports = router
