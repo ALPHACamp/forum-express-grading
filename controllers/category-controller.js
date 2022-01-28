109bbcbb065276b93813f21e0e31efd4bea52a1e
ronnychiang <ronnychiang1164@gmail.com>
2022-01-28 15:20:27 +0800

    feat: category update

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -1,10 +1,14 @@
 const { Category } = require('../models')
 const categoryController = {
   getCategories: (req, res, next) => {
-    return Category.findAll({
-      raw: true
-    })
-      .then(categories => res.render('admin/categories', { categories }))
+    return Promise.all([
+      Category.findAll({ raw: true }),
+      req.params.id ? Category.findByPk(req.params.id, { raw: true }) : null
+    ])
+      .then(([categories, category]) => res.render('admin/categories', {
+        categories,
+        category
+      }))
       .catch(err => next(err))
   },
   postCategory: async (req, res, next) => {
@@ -18,20 +22,33 @@ const categoryController = {
       req.flash('success_messages', `Category ${name}  was successfully created`) // 在畫面顯示成功提示
       res.redirect('/admin/categories')
     } catch (err) { next(err) }
-
-
     // const { name } = req.body
-    // if (!name) throw new Error('Category name is required!') // name 是必填，若發先是空值就會終止程式碼，並在畫面顯示錯誤提示
+    // if (!name) throw new Error('Category name is required!') 
     // Category.findOne({ where: { name } })
     //   .then(category => {
     //     if (category) throw new Error('Email already exists!')
     //     return Category.create({ name })
     //       .then(() => {
-    //         req.flash('success_messages', `Category ${name}  was successfully created`) // 在畫面顯示成功提示
-    //         res.redirect('/admin/categories') // 新增完成後導回後台首頁
+    //         req.flash('success_messages', `Category ${name}  was successfully created`) 
+    //         res.redirect('/admin/categories') 
     //       })
     //       .catch(err => next(err))
     //   })
+  },
+  putCategory: (req, res, next) => {
+    return Promise.all([
+      Category.findOne({ where: { name: req.body.name } }),
+      Category.findByPk(req.params.id)
+    ])
+      .then(([category, nowCategory]) => {
+        const { name } = req.body
+        if (!name) throw new Error('Category name is required!')
+        if (category) throw new Error(`Category ${name} already exists!`)
+        if (!nowCategory) throw new Error("Category doesn't exist!")
+        return nowCategory.update({ name })
+      })
+      .then(() => res.redirect('/admin/categories'))
+      .catch(err => next(err))
   }
 }
 module.exports = categoryController
