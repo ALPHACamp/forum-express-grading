2484ee237a984f41e5dc80a453a65a8acfad7f59
Kemal-Wuzhi <camelwuzhi@gmail.com>
2022-01-28 12:18:03 +0800

    feat:admin update restaurant

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
 const flash = require('connect-flash')
+const methodOverride = require('method-override')
 const session = require('express-session')
 const passport = require('./config/passport')
 const handlebarsHelpers = require('./helpers/handlebars-helpers')
@@ -33,6 +34,7 @@ app.use(passport.initialize())
 app.use(passport.session())
 
 app.use(flash()) // 掛載套件
+app.use(methodOverride('_method'))
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages') // 設定 success_msg 訊息
   res.locals.error_messages = req.flash('error_messages') // 設定 warning_msg 訊息
