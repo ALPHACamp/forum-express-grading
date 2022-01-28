f764ea0f9de6a7ace0ee1faea6890fbbdc38fe0f
z88243310 <z88243310@gmail.com>
2022-01-28 16:28:45 +0800

    feat: adjust middleware/auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,17 +1,17 @@
-const { ensureAuthenticated, getUser } = require('../helpers/auth-helpers')
+const helpers = require('../helpers/auth-helpers')
 
 const authenticated = (req, res, next) => {
   // 已登入，繼續
-  if (ensureAuthenticated(req)) return next()
+  if (helpers.ensureAuthenticated(req)) return next()
   // 未登入，返回登入頁面
   res.redirect('/signin')
 }
 
 const authenticatedAdmin = (req, res, next) => {
   // 已登入
-  if (ensureAuthenticated(req)) {
+  if (helpers.ensureAuthenticated(req)) {
     // 管理員，繼續
-    if (getUser(req).isAdmin) return next()
+    if (helpers.getUser(req).isAdmin) return next()
     // 一般用戶，返回登入頁面
     return res.redirect('/')
   }
