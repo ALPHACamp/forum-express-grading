4aabd70eb140f9466587ef01ed47e07807fc96a9
Kemal-Wuzhi <camelwuzhi@gmail.com>
2022-01-28 20:52:30 +0800

    feat: add restaurant image

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const handlebars = require('express-handlebars')
 const flash = require('connect-flash')
@@ -35,6 +36,7 @@ app.use(passport.session())
 
 app.use(flash()) // 掛載套件
 app.use(methodOverride('_method'))
+app.use('/upload', express.static(path.join(__dirname, 'upload')))
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages') // 設定 success_msg 訊息
   res.locals.error_messages = req.flash('error_messages') // 設定 warning_msg 訊息
--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,6 +1,9 @@
 const {
   Restaurant
 } = require('../models')
+const {
+  localFileHandler
+} = require('../helpers/file-helpers')
 const adminController = {
   getRestaurants: (req, res, next) => {
     Restaurant.findAll({
@@ -23,14 +26,18 @@ const adminController = {
       description
     } = req.body
     if (!name) throw new Error('Restaurant name is required!')
-
-    Restaurant.create({
-      name,
-      tel,
-      address,
-      openingHours,
-      description
-    })
+    const {
+      file
+    } = req
+    localFileHandler(file)
+      .then(filePath => Restaurant.create({
+        name,
+        tel,
+        address,
+        openingHours,
+        description,
+        image: filePath || null
+      }))
       .then(() => {
         req.flash('success_messages', 'restaurant was successfully created')
         res.redirect('/admin/restaurants')
@@ -72,17 +79,22 @@ const adminController = {
       description
     } = req.body
     if (!name) throw new Error('Restaurant name is required!')
-
-    Restaurant.findByPk(req.params.id)
-      .then(restaurant => {
+    const {
+      file
+    } = req // 把檔案取出來
+    Promise.all([ // 非同步處理
+      Restaurant.findByPk(req.params.id), // 去資料庫查有沒有這間餐廳
+      localFileHandler(file) // 把檔案傳到 file-helper 處理
+    ])
+      .then(([restaurant, filePath]) => { // 以上兩樣事都做完以後
         if (!restaurant) throw new Error("Restaurant didn't exist!")
-
-        return restaurant.update({
+        return restaurant.update({ // 修改這筆資料
           name,
           tel,
           address,
           openingHours,
-          description
+          description,
+          image: filePath || restaurant.image // 如果 filePath 是 Truthy (使用者有上傳新照片) 就用 filePath，是 Falsy (使用者沒有上傳新照片) 就沿用原本資料庫內的值
         })
       })
       .then(() => {
