a71c0818baca40a85f56e39577e1e44671a92cef
songerard <songerard@gmail.com>
2022-01-29 14:47:45 +0800

    feat: category create

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -6,6 +6,13 @@ const categoryController = {
     })
       .then(categories => res.render('admin/categories', { categories }))
       .catch(err => next(err))
+  },
+  postCategory: (req, res, next) => {
+    const { name } = req.body
+    if (!name) throw new Error('Category name is required!')
+    return Category.create({ name })
+      .then(() => res.redirect('/admin/categories'))
+      .catch(err => next(err))
   }
 }
 module.exports = categoryController
