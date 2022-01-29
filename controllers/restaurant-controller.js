e64f916dd41135642a1a6a130219c91003dba13b
lavender <id0421942@gmail.com>
2022-01-29 12:55:29 +0800

    add restaurant dashboard

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -20,13 +20,28 @@ const restaurantController = {
 
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
-      include: Category, // 拿出關聯的 Category model
+      include: Category // 拿出關聯的 Category model
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        return restaurant.increment('viewCounts')
+      })
+      .then(restaurant => {
+        restaurant = restaurant.toJSON()
+        res.render('restaurant', { restaurant })
+      })
+      .catch(err => next(err))
+  },
+
+  getDashboard: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, {
+      include: Category,
       nest: true,
       raw: true
     })
       .then(restaurant => {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
-        res.render('restaurant', {
+        res.render('dashboard', {
           restaurant
         })
       })
