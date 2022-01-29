eba230fd290c20d7a40c69036513e4f5d5c275ce
Dandy <lavenderflyyaya@gmail.com>
2022-01-29 15:06:09 +0800

    push heroku

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,4 +1,4 @@
-const { Restaurant } = require('../models')
+const { Restaurant, User } = require('../models')
 const { imgurFileHandler } = require('../helpers/file-helpers')
 const adminController = {
   getRestaurants: (req, res, next) => {
@@ -86,6 +86,27 @@ const adminController = {
       })
       .then(() => res.redirect('/admin/restaurants'))
       .catch(err => next(err))
+  },
+  getUsers: (req, res, next) => {
+    return User.findAll({ raw: true })
+      .then(users => {
+        if (!users) throw new Error("User didn't exist!")
+        return res.render('admin/users', { users })
+      })
+  },
+  patchUser: async (req, res, next) => {
+    try {
+      const user = await User.findByPk(req.params.id)
+      const { email, isAdmin } = user
+      if (email !== 'root@example.com') {
+        await user.update({ isAdmin: !isAdmin })
+        req.flash('success_messages', '使用者權限變更成功')
+        return res.redirect('/admin/users')
+      } else {
+        req.flash('error_messages', '禁止變更 root 權限')
+        return res.redirect('back')
+      }
+    } catch (err) { next(err) }
   }
 }
 
