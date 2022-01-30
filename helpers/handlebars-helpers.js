e9282a95b102ea6621315d23a96e69c5ec18361e
z88243310 <z88243310@gmail.com>
2022-01-31 01:50:16 +0800

    feat: add self auth warning

--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -1,4 +1,14 @@
 const dayjs = require('dayjs') // 載入 dayjs 套件
 module.exports = {
-  currentYear: () => dayjs().year() // 取得當年年份作為 currentYear 的屬性值，並導出
+  // 取得當年年份作為 currentYear 的屬性值，並導出
+  currentYear: () => dayjs().year(),
+
+  // 比較兩輸入值是否一致
+  ifCond: function (a, b, options) {
+    console.log(a, b)
+    if (String(a) === String(b)) {
+      return options.fn()
+    }
+    return options.inverse()
+  }
 }
