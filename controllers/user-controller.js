e3c96f9307830ed006e8df0d10de2d46cfe5370a
phiphi0912 <610733005@gms.ndhu.edu.tw>
2022-01-31 14:33:33 +0800

    feat: add User Profile(R03)

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcryptjs')
-const db = require('../models')
-const { User } = db
+const { User, Comment, Restaurant } = require('../models')
+const { imgurFileHandler } = require('../helpers/file-helper')
 
 const userController = {
   signUpPage: (req, res) => {
@@ -39,6 +39,54 @@ const userController = {
     req.flash('success_messages', '成功登出！')
     req.logout()
     res.redirect('/signin')
+  },
+  getUser: (req, res, next) => {
+    const userSession = req.user
+    const userId = req.params.id
+
+    return User.findByPk(userId, {
+      include: [{ model: Comment, include: [Restaurant] }],
+      group: 'Comments->Restaurant.id'
+    })
+      .then(user => {
+        if (!user) throw new Error('User do not exist')
+        user = user.toJSON()
+
+        res.render('users/profile', { user, userSession })
+      })
+      .catch(err => next(err))
+  },
+  editUser: (req, res, next) => {
+    const userId = Number(req.params.id)
+    // if (Number(userId) !== req.user.id) throw new Error('You can only edit yourself profile')
+
+    return User.findByPk(userId)
+      .then(user => {
+        if (!user) throw new Error('User do not exist')
+
+        res.render('users/edit', { user: user.toJSON() })
+      })
+      .catch(err => next(err))
+  },
+  putUser: (req, res, next) => {
+    const { name } = req.body
+    if (!name) throw new Error("User's name is required")
+
+    const { file } = req
+    return Promise.all([
+      User.findByPk(req.params.id),
+      imgurFileHandler(file)
+    ])
+      .then(([user, filePath]) => {
+        if (!user) throw new Error("User don't exist")
+
+        return user.update({ name, image: filePath || user.image })
+      })
+      .then(() => {
+        req.flash('success_messages', '使用者資料編輯成功')
+        res.redirect(`/users/${req.params.id}`)
+      })
+      .catch(err => next(err))
   }
 }
 
