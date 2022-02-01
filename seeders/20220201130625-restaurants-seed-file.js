41ec4e936c140e6226307e80716dee22a1031a87
Kemal-Wuzhi <camelwuzhi@gmail.com>
2022-02-01 12:33:18 +0800

    feat:update seed files

--- seeders/20220201130625-restaurants-seed-file.js
+++ seeders/20220201130625-restaurants-seed-file.js
@@ -3,6 +3,11 @@ const faker = require('faker')
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    // 新增以下三行，先去查詢現在 Categories 的 id有哪些
+    const categories = await queryInterface.sequelize.query(
+      'SELECT id FROM Categories',
+      { type: queryInterface.sequelize.QueryTypes.SELECT }
+    )
     await queryInterface.bulkInsert('Restaurants',
       Array.from({
         length: 50
@@ -14,7 +19,8 @@ module.exports = {
         image: `https://loremflickr.com/320/240/restaurant,food/?random=${Math.random() * 100}`,
         description: faker.lorem.text(),
         created_at: new Date(),
-        updated_at: new Date()
+        updated_at: new Date(),
+        category_id: categories[Math.floor(Math.random() * categories.length)].id
       }))
     )
   },
