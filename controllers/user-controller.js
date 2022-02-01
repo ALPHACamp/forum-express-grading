801112a20c4176673bb5a1952b7d7f48c07a8b42
Prysline <mayhu1993@gmail.com>
2022-02-01 21:40:42 +0800

    feat: user signup

---
+++ controllers/user-controller.js
@@ -0,0 +1,22 @@
+const bcrypt = require('bcryptjs')
+const db = require('../models')
+const { User } = db
+const userController = {
+  signUpPage: (req, res) => {
+    res.render('signup')
+  },
+  signUp: async (req, res, next) => {
+    try {
+      const hash = await bcrypt.hash(req.body.password, 10)
+      await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: hash
+      })
+      res.redirect('/signin')
+    } catch (error) {
+      next(error)
+    }
+  }
+}
+module.exports = userController
