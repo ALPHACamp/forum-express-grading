f0d88c81c758791bc2d705fa0aa699e788ce95aa
Chen ChiaChi <chenchiachi.ccc@gmail.com>
2022-02-02 23:46:17 +0100

    feat: create restaurant

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
-const { authenticatedAdmin } = require('../../middleware/auth')
-
-router.get('/restaurants', authenticatedAdmin, adminController.getRestaurants)
+router.get('/restaurants/create', adminController.createRestaurant)
+router.get('/restaurants', adminController.getRestaurants)
+router.post('/restaurants', adminController.postRestaurant)
 router.get('/', (req, res) =>
-  res.redirect('admin/restaurants'))
+  res.redirect('/admin/restaurants'))
 
 module.exports = router
