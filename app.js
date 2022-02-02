3437d7fde4a0ab525ba38631cbe53890de841959
Prysline <mayhu1993@gmail.com>
2022-02-02 21:04:25 +0800

    feat: passport init & signin

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const handlebars = require('express-handlebars')
 const flash = require('connect-flash')
 const session = require('express-session')
 const routes = require('./routes')
+const passport = require('./config/passport')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -13,6 +14,8 @@ app.engine('hbs', handlebars({ extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
+app.use(passport.initialize())
+app.use(passport.session())
 app.use(flash())
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const passport = require('../config/passport')
 const admin = require('./modules/admin')
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
@@ -7,6 +8,12 @@ const { generalErrorHandler } = require('../middleware/error-handlers')
 
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
+router.get('/signin', userController.signInPage)
+router.post('/signin', passport.authenticate('local', {
+  failureRedirect: '/sign',
+  failureFlash: true
+}), userController.signIn)
+router.get('/logout', userController.logout)
 router.use('/admin', admin)
 router.get('/restaurants', restController.getRestaurant)
 router.get('/', (req, res) => res.redirect('/restaurants'))
