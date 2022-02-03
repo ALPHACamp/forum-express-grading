1e0134a2ccb4fccef20976672caf603ac0778057
diosleonor <tccoolovl@gmail.com>
2022-02-03 19:08:41 +0800

    feat: category update

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -1,9 +1,15 @@
 const { Category } = require('../models')
 const categoryController = {
   getCategories: (req, res, next) => {
-    Category.findAll({ raw: true })
-      .then(categories => {
-        res.render('admin/restaurants', { categories })
+    return Promise.all([
+      Category.findAll({ raw: true }),
+      req.params.id ? Category.findByPk(req.params.id, { raw: true }) : null
+    ])
+      .then(([categories, category]) => {
+        res.render('admin/restaurants', {
+          categories,
+          category
+        })
       })
       .catch(err => next(err))
   },
@@ -15,6 +21,15 @@ const categoryController = {
         res.redirect('/admin/categories')
       })
       .catch(err => next(err))
+  },
+  putCategory: (req, res, next) => {
+    const { name } = req.body
+    if (!name) throw new Error('Restaurant name is required!')
+    return Category.findByPk(req.params.id)
+      .then(category => { return category.update({ name }) })
+      .then(() => { res.redirect('/admin/categories') })
+      .catch(err => next(err))
   }
+
 }
 module.exports = categoryController
