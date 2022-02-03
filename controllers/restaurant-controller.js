2fa518bfbbb671deb78b4e796150b9357d5c2749
eklipsorz <eklipsorz@protonmail.ch>
2022-02-03 23:00:34 +0800

    feat: add restaurants index page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,6 +1,15 @@
+const { Restaurant, Category, User } = require('../models')
+
 const restaurantController = {
-  getRestaurants: (req, res) => {
-    res.render('restaurants')
+  getRestaurants: (req, res, next) => {
+    // res.render('restaurants')
+    return Restaurant.findAll({ raw: true, nest: true, include: [Category] })
+      .then(restaurants => restaurants.map(r => ({
+        ...r,
+        description: r.description.substring(0, 50)
+      })))
+      .then(restaurants => res.render('restaurants', { restaurants }))
+      .catch(error => next(error))
   }
 }
 
