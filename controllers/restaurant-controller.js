06eb88a69681a2d817f4df166fd19747fc71b40e
diosleonor <tccoolovl@gmail.com>
2022-02-05 22:06:01 +0800

    A18: 餐廳資訊整理

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,5 +1,16 @@
 const { Restaurant, Category } = require('../models')
-const restaurantController = {
+const restController = {
+  getDashboard: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, {
+      raw: true,
+      nest: true,
+      include: Category
+    }).then(restaurant => {
+      if (!restaurant) throw new Error('Restaurant did not exist!')
+      res.render('dashboard', { restaurant })
+    })
+      .catch(err => next(err))
+  },
   getRestaurants: (req, res) => {
     return Restaurant.findAll({
       include: Category,
@@ -10,25 +21,18 @@ const restaurantController = {
       restaurants.map(r => ({
         ...r, description: r.description.substring(0, 50)
       }))
-      return res.render('restaurants', { restaurants: data})
+      return res.render('restaurants', { restaurants: data })
     })
   },
-  /*使用者可以依分類篩選餐廳
-  使用者可以切換分頁，一次瀏覽 10 筆餐廳*/
   getRestaurant: (req, res, next) => {
     Restaurant.findByPk(req.params.id, {
-      raw: true,
-      nest: true,
       include: Category
     }).then(restaurant => {
+      const viewCounts = restaurant.viewCounts
       if (!restaurant) throw new Error('Restaurant did not exist!')
-      res.render('restaurant', { restaurant })
-    })
+      return restaurant.update({ viewCounts: viewCounts + 1 })
+    }).then(restaurant => res.render('restaurant', { restaurant: restaurant.toJSON() }))
       .catch(err => next(err))
   }
-
-  /*使用者可以新增評論
-    使用者可以瀏覽該餐廳的所有評論
-    管理員可以刪除餐廳*/
 }
-module.exports = restaurantController
+module.exports = restController
---
+++ migrations/20220205125704-add-view-counts-to-restaurant.js
@@ -0,0 +1,15 @@
+'use strict'
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.addColumn('Restaurants', 'view_counts', {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      defaultValue: 0
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.removeColumn('Restaurants', 'view_counts')
+  }
+}
