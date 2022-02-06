da576e4dcb5f470ac3074524dedb1fcafae2a82a
HY <homtele@gmail.com>
2022-02-06 22:10:00 +0800

    feat: add user profile page

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,7 +1,44 @@
 const bcrypt = require('bcryptjs')
-const db = require('../models')
-const { User } = db
+const { User } = require('../models')
+const { imgurFileHandler } = require('../helpers/file-helpers')
+
 const userController = {
+  getUser: (req, res, next) => {
+    return User
+      .findByPk(req.params.id, { raw: true })
+      .then(user => {
+        if (!user) throw new Error("User didn't exist!")
+        return res.render('users/profile', { user })
+      })
+      .catch(err => next(err))
+  },
+  editUser: (req, res, next) => {
+    return User
+      .findByPk(req.params.id, { raw: true })
+      .then(user => {
+        if (!user) throw new Error("User didn't exist!")
+        return res.render('users/edit', { user })
+      })
+      .catch(err => next(err))
+  },
+  putUser: (req, res, next) => {
+    if (Number(req.params.id) !== req.user.id) throw new Error("User didn't match!")
+    const { name } = req.body
+    const { file } = req
+    return Promise.all([
+      User.findByPk(req.params.id),
+      imgurFileHandler(file)
+    ])
+      .then(([user, filePath]) => {
+        if (!user) throw new Error("User did'nt exit")
+        return user.update({ name, image: filePath || user.image })
+      })
+      .then(() => {
+        req.flash('success_messages', '使用者資料編輯成功')
+        res.redirect(`/users/${req.params.id}`)
+      })
+      .catch(err => next(err))
+  },
   signUpPage: (req, res) => {
     res.render('signup')
   },
--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -3,11 +3,13 @@ const relativeTime = require('dayjs/plugin/relativeTime')
 dayjs.extend(relativeTime)
 const currentYear = () => dayjs().year()
 const relativeTimeFromNow = a => dayjs(a).fromNow()
+const noImage = a => a || 'http://via.placeholder.com/300x300?text=No+Image'
 const ifCond = function (a, b, options) {
   return a === b ? options.fn(this) : options.inverse(this)
 }
 module.exports = {
   currentYear,
   relativeTimeFromNow,
+  noImage,
   ifCond
 }
