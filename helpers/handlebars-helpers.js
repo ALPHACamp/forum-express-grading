f95e9073ba008eafedd31459763561437baebfde
deansyue <c1318131801@gmail.com>
2022-02-07 22:03:20 +0800

    feat: add ifCond helper & update category selector

--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -3,5 +3,10 @@ const dayjs = require('dayjs')
 
 // 匯出模組
 module.exports = {
-  currentYear: () => dayjs().year() // 取得當年年份作為currentYear的屬性值，並導出
+  currentYear: () => dayjs().year(), // 取得當年年份作為currentYear的屬性值，並導出
+
+  ifCond: function (a, b, options) {
+    // a === b時回傳fn(this)， 不相等則回傳inverse(this)
+    return a === b ? options.fn(this) : options.inverse(this)
+  }
 }
