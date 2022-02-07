ae60656c19fc55056276bd0e4c0379239b27240d
hsinyi8997 <hsinyi8997@gmail.com>
2022-02-07 21:49:30 +0800

    feat: add restaurant page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -14,6 +14,18 @@ const restaurantController = {
         }))
         return res.render('restaurants', { restaurants: data })
       })
+  },
+  getRestaurant: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, {
+      include: Category,
+      nest: true,
+      raw: true
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error('Restaurant is not exist')
+        return res.render('restaurant', { restaurant })
+      })
+      .catch(err => next(err))
   }
 }
 module.exports = restaurantController
