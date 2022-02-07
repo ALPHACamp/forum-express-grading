edfad4934fd8e67578e4a9f740f2c10938b4da62
wenrou77 <wendyog@gmail.com>
2022-02-07 20:12:09 +0800

    feat: add admin categories page

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -10,7 +10,7 @@ const adminController = {
       .then(restaurants => res.render('admin/restaurants', { restaurants }))
       .catch(err => next(err))
   },
-  createRestaurant: (req, res) => {
+  createRestaurant: (req, res, next) => {
     return Category.findAll({
       raw: true
     })
