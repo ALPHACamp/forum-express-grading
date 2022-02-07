a2c464375fc74f61d3d878dbcf9d8d4804750122
Chen ChiaChi <chenchiachi.ccc@gmail.com>
2022-02-07 22:20:46 +0100

    feat:admin user page

--- tests/R01.test.js
+++ tests/R01.test.js
@@ -7,10 +7,10 @@ const app = require('../app')
 const { createModelMock, createControllerProxy, mockRequest, mockResponse, mockNext } = require('../helpers/unit-test-helper');
 
 describe('# R01', () => {
-  describe('登入測試: POST /signin', function(){
+  describe('登入測試: POST /signin', function () {
     // 以下測試會發出請求，測試資料庫內是否有作業指定的使用者資料
     // 測試資料的來源是真實的資料庫
-    it('#1 密碼錯誤', function(done){
+    it('#1 密碼錯誤', function (done) {
       request(app)
         // 對 POST /signin 發出請求，參數是錯誤的密碼
         .post('/signin')
@@ -21,7 +21,7 @@ describe('# R01', () => {
         .expect(302, done)
     })
 
-    it('#2 帳號錯誤', function(done){
+    it('#2 帳號錯誤', function (done) {
       request(app)
         // 對 POST /signin 發出請求，參數是錯誤的帳號
         .post('/signin')
@@ -32,7 +32,7 @@ describe('# R01', () => {
         .expect(302, done)
     })
 
-    it('#3 成功登入', function(done){
+    it('#3 成功登入', function (done) {
       request(app)
         // 對 POST /signin 發出請求，參數是作業指定的使用者帳號密碼
         .post('/signin')
@@ -59,7 +59,7 @@ describe('# R01', () => {
       // 修改 adminController 中的資料庫連線設定，由連向真實的資料庫 -> 改為連向模擬的 User table
       this.adminController = createControllerProxy('../controllers/admin-controller', { User: this.UserMock })
     })
-    
+
     // 開始測試
     context('# [顯示使用者清單]', () => {
       it(' GET /admin/users ', async () => {
@@ -80,10 +80,10 @@ describe('# R01', () => {
 
     context('# [修改使用者權限] for root', () => {
       before(() => {
-      // 製作假資料
-      // 本 context 會用這筆資料進行測試
+        // 製作假資料
+        // 本 context 會用這筆資料進行測試
         this.UserMock = createModelMock(
-          'User', 
+          'User',
           [{
             id: 1,
             email: 'root@example.com',
@@ -91,7 +91,7 @@ describe('# R01', () => {
             isAdmin: true, // 是管理者
           }]
         )
-        
+
         // 將 adminController 中的 User db 取代成 User mock db
         this.adminController = createControllerProxy('../controllers/admin-controller', { User: this.UserMock })
       })
@@ -102,12 +102,12 @@ describe('# R01', () => {
         const res = mockResponse()
         const next = mockNext
 
-       // 測試作業指定的 adminController.patchUser 函式
+        // 測試作業指定的 adminController.patchUser 函式
         await this.adminController.patchUser(req, res, next)
 
         // patchUser 正確執行的話，應呼叫 req.flash
         // req.flash 的參數應該要與下列字串一致
-        req.flash.calledWith('error_messages','禁止變更 root 權限').should.be.true
+        req.flash.calledWith('error_messages', '禁止變更 root 權限').should.be.true
 
         // patchUser 執行完畢，應呼叫 res.redirect 並重新導向上一頁 
         res.redirect.calledWith('back').should.be.true
@@ -142,7 +142,7 @@ describe('# R01', () => {
 
         // patchUser 正確執行的話，應呼叫 req.flash 
         // req.flash 的參數應與下列字串一致
-        req.flash.calledWith('success_messages','使用者權限變更成功').should.be.true
+        req.flash.calledWith('success_messages', '使用者權限變更成功').should.be.true
         // patchUser 執行完畢，應呼叫 res.redirect 並重新導向 /admin/users
         res.redirect.calledWith('/admin/users').should.be.true
 
@@ -181,7 +181,7 @@ describe('# R01', () => {
 
         // patchUser 正確執行的話，應呼叫 req.flash 
         // req.flash 的參數應與下列字串一致
-        req.flash.calledWith('success_messages','使用者權限變更成功').should.be.true
+        req.flash.calledWith('success_messages', '使用者權限變更成功').should.be.true
         // patchUser 執行完畢，應呼叫 res.redirect 並重新導向 /admin/users
         res.redirect.calledWith('/admin/users').should.be.true
 
