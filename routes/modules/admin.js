1bed72c7af42318997db893c61c55544098b7aba
wenrou77 <wendyog@gmail.com>
2022-02-07 20:14:03 +0800

    feat: category create

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -14,6 +14,9 @@ router.post('/restaurants', upload.single('image'), adminController.postRestaura
 
 router.get('/users', adminController.getUsers)
 router.patch('/users/:id', adminController.patchUser)
+
 router.get('/categories', categoryController.getCategories)
+router.post('/categories', categoryController.postCategory)
+
 router.get('/', (req, res) => res.redirect('/admin/restaurants'))
 module.exports = router
