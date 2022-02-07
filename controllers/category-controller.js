a0694795b3b320dbf7b05b6796cb75915a5efc39
martinchiu <martin87713@gmail.com>
2022-02-07 14:18:49 +0800

    feat: category update

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -1,10 +1,16 @@
 const { Category } = require('../models')
 const categoryController = {
   getCategories: (req, res, next) => {
-    return Category.findAll({
-      raw: true
-    })
-      .then(categories => res.render('admin/categories', { categories }))
+    return Promise.all([
+      Category.findAll({ raw: true }),
+      req.params.id ? Category.findByPk(req.params.id, { raw: true }) : null
+    ])
+      .then(([categories, category]) => {
+        res.render('admin/categories', {
+          categories,
+          category
+        })
+      })
       .catch(err => next(err))
   },
   postCategory: (req, res, next) => {
@@ -13,6 +19,20 @@ const categoryController = {
     Category.create({ name })
       .then(() => {
         req.flash('success_messages', 'category was successfully created')
+        res.redirect('/admin/categories')
+      })
+      .catch(err => next(err))
+  },
+  putCategory: (req, res, next) => {
+    const { name } = req.body
+    if (!name) throw new Error('Category name is required!')
+    return Category.findByPk(req.params.id)
+      .then(category => {
+        if (!category) throw new Error("Category doesn't exist!")
+        return category.update({ name })
+      })
+      .then(() => {
+        req.flash('success_messages', 'restaurant was successfully to update')
         res.redirect('/admin/categories')
       })
       .catch(err => next(err))
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -20,6 +20,9 @@ router.get('/restaurants', adminController.getRestaurants)
 router.get('/users', adminController.getUsers)
 router.patch('/users/:id', adminController.patchUser)
 
+router.get('/categories/:id', categoryController.getCategories)
+router.put('/categories/:id', categoryController.putCategory)
+
 router.get('/categories', categoryController.getCategories)
 router.post('/categories', categoryController.postCategory)
 
