3ba906b4a278c07366a96072722104795954fbd3
deansyue <c1318131801@gmail.com>
2022-02-08 00:08:00 +0800

    feat: add restaurants index page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,7 +1,24 @@
+// 引入model
+const { Restaurant, Category } = require('../models')
+
 const restaurantController = {
   // 瀏覽餐廳頁面
   getRestaurants: (req, res) => {
-    return res.render('restaurants')
+    // 尋找全部restaurant資料，並關連category
+    Restaurant.findAll({
+      include: Category,
+      nest: true,
+      raw: true
+    })
+      .then(restaurants => {
+        // 新增變數data，取得map回傳陣列
+        const data = restaurants.map(r => ({
+          ...r, // 展開r的物件
+          description: r.description.substring(0, 50) // 將餐廳描述截為50字
+        }))
+        // 渲染restaurants
+        return res.render('restaurants', { restaurants: data })
+      })
   }
 }
 
