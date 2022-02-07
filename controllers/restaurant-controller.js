20ae2ea8f5df952fdb53ed798d21cf467e984b8c
wenrou77 <wendyog@gmail.com>
2022-02-07 22:53:35 +0800

    start R02

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -28,6 +28,20 @@ const restaurantController = {
         })
       })
       .catch(err => next(err))
+  },
+  getDashboard: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, {
+      include: Category,
+      nest: true,
+      raw: true
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        res.render('dashboard', {
+          restaurant
+        })
+      })
+      .catch(err => next(err))
   }
 }
 module.exports = restaurantController
