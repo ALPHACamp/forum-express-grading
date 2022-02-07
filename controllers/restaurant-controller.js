75b03cbff0d632ef057b34e22997fcb61b2c67fd
HY <homtele@gmail.com>
2022-02-07 09:56:03 +0800

    feat: add feeds page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -35,6 +35,26 @@ const restaurantController = {
       })
       .catch(err => next(err))
   },
+  getFeeds: (req, res, next) => {
+    return Promise.all([
+      Restaurant.findAll({
+        limit: 10,
+        order: [['createdAt', 'DESC']],
+        include: [Category],
+        raw: true,
+        nest: true
+      }),
+      Comment.findAll({
+        limit: 10,
+        order: [['createdAt', 'DESC']],
+        include: [User, Restaurant],
+        raw: true,
+        nest: true
+      })
+    ])
+      .then(([restaurants, comments]) => res.render('feeds', { restaurants, comments }))
+      .catch(err => next(err))
+  },
   getRestaurant: (req, res, next) => {
     return Restaurant
       .findByPk(req.params.id, {
