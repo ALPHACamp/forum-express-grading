b4a121048d90266e9b7240a138912054084b9094
hsinyi8997 <hsinyi8997@gmail.com>
2022-02-07 15:58:16 +0800

    feat: category update

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -15,8 +15,10 @@ router.post('/restaurants', upload.single('image'), adminController.postRestaura
 router.patch('/users/:id', adminController.patchUser)
 router.get('/users', adminController.getUsers)
 
+router.put('/categories/:id', categoryController.putCategory)
+router.get('/categories/:id', categoryController.getCategories)
+router.get('/categories/', categoryController.getCategories)
 router.post('/categories', categoryController.postCategory)
-router.get('/categories', categoryController.getCategories)
 
 router.get('/', (req, res) => res.redirect('/admin/restaurants'))
 
