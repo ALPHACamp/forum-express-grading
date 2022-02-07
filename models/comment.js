bdd46febb7163fb6e826e4662ae070c826cc4428
LAPTOP-VLRN913M\twang <asakura4@gmail.com>
2022-02-06 23:25:20 -0800

    add post and show comment in restaurant page

--- models/comment.js
+++ models/comment.js
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate (models) {
       // define association here
-      Comment.belongsTo(models.Resaturant, { foreignKey: 'restaurantId' })
+      Comment.belongsTo(models.Restaurant, { foreignKey: 'restaurantId' })
       Comment.belongsTo(models.User, { foreignKey: 'userId' })
     }
   };
