faf575623eeea7e9b7705b1e2bb9262fe1fffce9
martinchiu <martin87713@gmail.com>
2022-02-08 11:15:57 +0800

    feat: add pagination and categories navbar on admin restaurants index page

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,13 +1,33 @@
 const { Restaurant, User, Category } = require('../models')
 const { imgurFileHandler } = require('../helpers/file-helpers')
+const { getOffset, getPagination } = require('../helpers/pagination-helper')
 const adminController = {
   getRestaurants: (req, res, next) => {
-    Restaurant.findAll({
-      raw: true,
-      nest: true,
-      include: [Category]
-    })
-      .then(restaurants => res.render('admin/restaurants', { restaurants }))
+    const DEFAULT_LIMIT = 10
+    const categoryId = Number(req.query.categoryId) || ''
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || DEFAULT_LIMIT
+    const offset = getOffset(limit, page)
+
+    return Promise.all([
+      Restaurant.findAndCountAll({
+        include: Category,
+        where: {
+          ...categoryId ? { categoryId } : {}
+        },
+        limit,
+        offset,
+        nest: true,
+        raw: true
+      }),
+      Category.findAll({ raw: true })
+    ])
+      .then(([restaurants, categories]) => res.render('admin/restaurants', {
+        restaurants: restaurants.rows,
+        categories,
+        categoryId,
+        pagination: getPagination(limit, page, restaurants.count)
+      }))
       .catch(err => next(err))
   },
   createRestaurant: (req, res, next) => {
