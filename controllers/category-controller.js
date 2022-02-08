f8e516a3a42eb30060e7a9f1626fc0b2a780965b
xnubber <aabbccdd10902@gmail.com>
2022-02-08 17:48:13 +0800

    feat:category update

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -2,9 +2,15 @@ const { Category } = require('../models')
 
 const categoryController = {
   getCategories: (req, res, next) => {
-    return Category.findAll({ raw: true })
-      .then(categories => {
-        res.render('admin/categories', { categories })
+    return Promise.all([
+      Category.findAll({ raw: true }),
+      req.params.id ? Category.findByPk(req.params.id, { raw: true }) : null
+    ])
+      .then(([categories, category]) => {
+        res.render('admin/categories', {
+          categories,
+          category
+        })
       })
       .catch(err => next(err))
   },
@@ -23,6 +29,18 @@ const categoryController = {
         res.redirect('/admin/categories')
       })
       .catch(err => next(err))
+  },
+  putCategory: (req, res, next) => {
+    const { name } = req.body
+    if (!name) throw new Error('Category name is required!')
+    return Category.findByPk(req.params.id)
+      .then(category => {
+        if (!category) throw new Error("Category doesn't exist")
+        return category.update({ name })
+      })
+      .then(() => res.redirect('/admin/categories'))
+      .catch(err => next(err))
   }
 }
+
 module.exports = categoryController
