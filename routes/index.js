35dfbafb55104714a8b5a89f30255763892e7f67
HY <homtele@gmail.com>
2022-02-08 17:13:53 +0800

    feat: addFollowing & removeFollowing function

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,7 @@ const { generalErrorHandler } = require('../middleware/error-handler')
 const upload = require('../middleware/multer')
 
 router.use('/admin', authenticatedAdmin, admin)
+router.get('/users/top', authenticated, userController.getTopUsers)
 router.get('/users/:id/edit', authenticated, userController.editUser)
 router.get('/users/:id', authenticated, userController.getUser)
 router.put('/users/:id', authenticated, upload.single('image'), userController.putUser)
