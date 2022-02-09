b5263e967c6a01b70b64f505e010c98c2e2c9a55
LAPTOP-VLRN913M\twang <asakura4@gmail.com>
2022-02-08 23:32:38 -0800

    add user page and edit user page

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcryptjs')
-const db = require('../models')
-const { User } = db
+const { User } = require('../models')
+const { imgurFileHandler } = require('../helpers/file-helpers')
 const userController = {
   signUpPage: (req, res) => {
     res.render('signup')
@@ -38,7 +38,60 @@ const userController = {
     req.flash('success_messages', '登出成功！')
     req.logout()
     res.redirect('/signin')
+  },
+  getUser: (req, res, next) => {
+    return User.findByPk(req.params.id, {
+      nest: true,
+      raw: true
+    })
+      .then(user => {
+        if (!user) {
+          throw new Error("User didn't exist!")
+        }
+        const defaultProfileIcon = `/upload/${process.env.DEFAULT_PROFILE}`
+        return res.render('users/profile', { user, defaultProfileIcon })
+      })
+      .catch(err => next(err))
+  },
+  editUser: (req, res, next) => {
+    return User.findByPk(req.params.id, {
+      nest: true,
+      raw: true
+    })
+      .then(user => {
+        if (!user) {
+          throw new Error("User didn't exist!")
+        }
+        return res.render('users/edit', { user })
+      })
+      .catch(err => next(err))
+  },
+  putUser: (req, res, next) => {
+    const { name } = req.body
+    if (!name) {
+      throw new Error('User name is required')
+    }
+    const { file } = req
+    return Promise.all([
+      User.findByPk(req.params.id),
+      imgurFileHandler(file)
+    ])
+      .then(([user, filePath]) => {
+        if (!user) {
+          throw new Error("User didn't exist")
+        }
+        return user.update({
+          name,
+          image: filePath || user.image
+        })
+      })
+      .then(() => {
+        req.flash('success_messages', '使用者資料編輯成功')
+        return res.redirect(`/users/${req.params.id}`)
+      })
+      .catch(err => next(err))
   }
+
 }
 
 module.exports = userController
