9aea476f1349eec5cafb7922f14fc1a84dc1c332
z88243310 <z88243310@gmail.com>
2022-02-11 01:41:45 +0800

    feat: category update

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -2,8 +2,10 @@ const { Category } = require('../models')
 
 const categoryController = {
   getCategories: (req, res, next) => {
-    return Category.findAll({ raw: true })
-      .then(categories => res.render('admin/categories', { categories }))
+    return Promise.all([Category.findAll({ raw: true }), req.params.id ? Category.findByPk(req.params.id, { raw: true }) : null])
+      .then(([categories, category]) => {
+        res.render('admin/categories', { categories, category })
+      })
       .catch(err => next(err))
   },
 
@@ -15,6 +17,25 @@ const categoryController = {
       .then(category => {
         if (category) throw new Error('Category name exists')
         return Category.create({ name })
+      })
+      .then(() => res.redirect('/admin/categories'))
+      .catch(err => next(err))
+  },
+
+  putCategory: (req, res, next) => {
+    const { name } = req.body
+    if (!name) throw new Error('Category name is required!')
+
+    return Promise.all([Category.findAll({ where: { name }, raw: true }), req.params.id ? Category.findByPk(req.params.id) : null])
+      .then(([categories, category]) => {
+        // 搜尋錯誤，此 id 不存在
+        if (!category) throw new Error("Category doesn't exist!")
+        // 沒改變，不更新
+        if (category.name === name) return
+        // 有改變，檢查是不是已經存在
+        if (categories.length > 0) throw new Error('Category name exists!')
+        // 有改變，沒有重複
+        return category.update({ name })
       })
       .then(() => res.redirect('/admin/categories'))
       .catch(err => next(err))
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -9,28 +9,28 @@ const categoryController = require('../../controllers/category-controller')
 // 新增餐廳
 router.get('/restaurants/create', adminController.createRestaurant)
 router.post('/restaurants', upload.single('image'), adminController.postRestaurant)
-
 // 顯示單一餐廳
 router.get('/restaurants/:id', adminController.getRestaurant)
-
 // 編輯單一餐廳
 router.get('/restaurants/:id/edit', adminController.editRestaurant)
 router.put('/restaurants/:id', upload.single('image'), adminController.putRestaurant)
-
 // 刪除單一餐聽
 router.delete('/restaurants/:id', adminController.deleteRestaurant)
-
 // 顯示所有餐廳
 router.get('/restaurants', adminController.getRestaurants)
 
 // 顯示所有使用者
 router.get('/users', adminController.getUsers)
-
 // 更新使用者權限
 router.patch('/users/:id', adminController.patchUser)
 
-// 類別
+// 顯示類別於編輯欄
+router.get('/categories/:id', categoryController.getCategories)
+// 更新類別
+router.put('/categories/:id', categoryController.putCategory)
+// 顯示所有類別
 router.get('/categories', categoryController.getCategories)
+// 新增類別
 router.post('/categories', categoryController.postCategory)
 
 // 首頁路由
