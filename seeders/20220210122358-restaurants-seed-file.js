23873a4b7578a8781cdcfabd2c3d9f47589122c7
z88243310 <z88243310@gmail.com>
2022-02-10 20:33:41 +0800

    feat: update seed files

--- seeders/20220210122358-restaurants-seed-file.js
+++ seeders/20220210122358-restaurants-seed-file.js
@@ -4,6 +4,9 @@ const faker = require('faker')
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    const categories = await queryInterface.sequelize.query('SELECT id FROM Categories;', {
+      type: queryInterface.sequelize.QueryTypes.SELECT
+    })
     await queryInterface.bulkInsert(
       'Restaurants',
       Array.from({ length: 50 }, () => ({
@@ -14,7 +17,8 @@ module.exports = {
         image: `https://loremflickr.com/320/240/restaurant,food/?random=${Math.random() * 100}`,
         description: faker.lorem.text(),
         created_at: new Date(),
-        updated_at: new Date()
+        updated_at: new Date(),
+        category_id: categories[Math.floor(Math.random() * categories.length)].id
       }))
     )
   },
