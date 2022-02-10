9e4ae9d8900b711c7ee86510932aaaa345a4f4a8
hsinyi8997 <hsinyi8997@gmail.com>
2022-02-10 14:39:42 +0800

    feat: add comment record to user profile

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use('/upload', express.static(path.join(__dirname, 'upload')))
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
   res.locals.error_messages = req.flash('error_messages')
-  res.locals.user = getUser(req)
+  res.locals.currentUser = getUser(req)
   next()
 })
 
