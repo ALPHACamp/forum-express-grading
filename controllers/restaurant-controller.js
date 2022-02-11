0c086e7cccb24a28ce5622398874d0d0c8fc9230
z88243310 <z88243310@gmail.com>
2022-02-11 18:44:47 +0800

    add dashboard page, merge R02 to main

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -17,11 +17,22 @@ const restaurantController = {
       .catch(err => next(err))
   },
   getRestaurant: (req, res, next) => {
-    console.log('hi')
+    return Restaurant.findByPk(req.params.id, { include: Category, nest: true })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+
+        // 當按下頁面重新整理時，Cache-Control 就會出現，以此作為判斷避免觀看數+1
+        const cacheControl = req.rawHeaders.includes('Cache-Control')
+        return !cacheControl ? restaurant.update({ viewCounts: restaurant.viewCounts + 1 }) : restaurant
+      })
+      .then(restaurant => res.render('restaurant', { restaurant: restaurant.toJSON() }))
+      .catch(err => next(err))
+  },
+  getDashboard: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, { include: Category, raw: true, nest: true })
       .then(restaurant => {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
-        res.render('restaurant', { restaurant })
+        res.render('dashboard', { restaurant })
       })
       .catch(err => next(err))
   }
