d9020c7de79d1570ee9f001f9e1a83ddcf500983
Chen ChiaChi <chenchiachi.ccc@gmail.com>
2022-02-11 14:33:42 +0100

    feat: category create

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -17,7 +17,7 @@ router.get('/users', adminController.getUsers)
 router.patch('/users/:id', adminController.patchUser)
 
 router.get('/categories', categoryController.getCategories)
-
+router.post('/categories', categoryController.postCategory)
 router.get('/', (req, res) =>
   res.redirect('/admin/restaurants'))
 
