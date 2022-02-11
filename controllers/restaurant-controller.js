041772c2fcad63ac40e04dd3eedff62e86860802
Chen ChiaChi <chenchiachi.ccc@gmail.com>
2022-02-12 00:10:55 +0100

    feat: add dashboard page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -17,13 +17,24 @@ const restaurantController = {
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
+      include: Category
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        return restaurant.increment('viewCounts')
+      })
+      .then(restaurant => res.render('restaurant', { restaurant: restaurant.toJSON() }))
+      .catch(err => next(err))
+  },
+  getDashboard: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, {
       include: Category,
       nest: true,
       raw: true
     })
       .then(restaurant => {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
-        res.render('restaurant', {
+        res.render('dashboard', {
           restaurant
         })
       })
