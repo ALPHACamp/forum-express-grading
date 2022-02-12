50eb477dbd10ef6779ad9d7e2093b55364b2d4e3
Prysline <mayhu1993@gmail.com>
2022-02-12 12:47:54 +0800

    feat: add restaurants index page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,6 +1,21 @@
+const { Restaurant, Category } = require('../models')
+
 const restaurantController = {
-  getRestaurant: (req, res) => {
-    return res.render('restaurants')
+  getRestaurants: async (req, res, next) => {
+    try {
+      const restaurants = await Restaurant.findAll({
+        raw: true,
+        nest: true,
+        include: Category
+      })
+      const data = restaurants.map(item => ({
+        ...item,
+        description: item.description.substring(0, 50)
+      }))
+      res.render('restaurants', { restaurants: data })
+    } catch (error) {
+      next(error)
+    }
   }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,7 @@ router.post('/signin', passport.authenticate('local', {
 }), userController.signIn)
 router.get('/logout', userController.logout)
 router.use('/admin', authenticatedAdmin, admin)
-router.get('/restaurants', authenticated, restController.getRestaurant)
+router.get('/restaurants', authenticated, restController.getRestaurants)
 router.get('/', (req, res) => res.redirect('/restaurants'))
 router.use('/', generalErrorHandler)
 module.exports = router
