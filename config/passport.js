9861380c942fd0d74390864e72fdbc82bf3dbf5b
xnubber <aabbccdd10902@gmail.com>
2022-02-13 19:20:10 +0800

    feat:create followship model

--- config/passport.js
+++ config/passport.js
@@ -27,7 +27,9 @@ passport.deserializeUser((id, cb) => {
   User.findByPk(id, {
     include: [
       { model: Restaurant, as: 'FavoritedRestaurants' },
-      { model: Restaurant, as: 'LikedRestaurants' }
+      { model: Restaurant, as: 'LikedRestaurants' },
+      { model: User, as: 'Followers' },
+      { model: User, as: 'Followings' }
     ]
   })
     .then(user => cb(null, user.toJSON()))
