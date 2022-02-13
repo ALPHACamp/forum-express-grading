ddaf764425b4b337e3d0ee6773fd57d44e5918f1
xnubber <aabbccdd10902@gmail.com>
2022-02-13 16:46:45 +0800

    feat: add like/unlike function

--- routes/index.js
+++ routes/index.js
@@ -28,6 +28,8 @@ router.post('/comments', authenticated, commentController.postComment)
 
 router.post('/favorite/:restaurantId', authenticated, userController.addFavorite)
 router.delete('/favorite/:restaurantId', authenticated, userController.removeFavorite)
+router.post('/like/:restaurantId', authenticated, userController.addLike)
+router.delete('/like/:restaurantId', authenticated, userController.removeLike)
 
 router.put('/users/:id', authenticated, upload.single('image'), userController.putUser)
 router.get('/users/:id/edit', authenticated, userController.editUser)
