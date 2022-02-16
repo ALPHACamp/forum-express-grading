c424931afcec9fd2e489e985bde8fb88c1ca5e97
Young <phanton0117@gmail.com>
2022-02-17 03:39:30 +0800

    feat: add user administration

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,4 +1,4 @@
-const { Restaurant } = require('../models')
+const { Restaurant, User } = require('../models')
 const { imgurFileHandler } = require('../helpers/file-helpers')
 const adminController = {
   getRestaurants: (req, res, next) => {
@@ -83,6 +83,31 @@ const adminController = {
       })
       .then(() => res.redirect('/admin/restaurants'))
       .catch(err => next(err))
+  },
+  getUsers: (req, res, next) => {
+    return User.findAll({
+      raw: true
+    })
+      .then(users => res.render('admin/users', { users }))
+      .catch(err => next(err))
+  },
+  patchUser: async (req, res, next) => {
+    try {
+      const { id } = req.params
+
+      const user = await User.findByPk(id)
+      if (!user) throw new Error("User didn't exists")
+
+      const { email, isAdmin } = user.dataValues
+      if (email === process.env.SUPERUSER_EMAIL) {
+        req.flash('error_messages', '禁止變更 root 權限')
+        return res.redirect('back')
+      }
+
+      await user.update({ isAdmin: !isAdmin })
+      req.flash('success_messages', '使用者權限變更成功')
+      return res.redirect('/admin/users')
+    } catch (err) { next(err) }
   }
 }
 module.exports = adminController
