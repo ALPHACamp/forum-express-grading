d722098c9bb180d7ab7d16d9747f756359bc513b
snmo2546 <snmo2546@gmail.com>
2022-02-24 17:53:28 +0800

    feat: create restaurant

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -7,6 +7,27 @@ const adminController = {
     })
       .then(restaurants => res.render('admin/restaurants', { restaurants }))
       .catch(err => next(err))
+  },
+  createRestaurant: (req, res) => {
+    return res.render('admin/create-restaurant')
+  },
+  postRestaurant: (req, res, next) => {
+    const { name, tel, address, openingHours, description } = req.body
+
+    if (!name) throw new Error('Restaurant name is required!')
+
+    Restaurant.create({
+      name,
+      tel,
+      address,
+      openingHours,
+      description
+    })
+      .then(() => {
+        req.flash('success_messages', 'Restaurant was successfully created') // 在畫面顯示成功提示
+        res.redirect('/admin/restaurants')
+      })
+      .catch(err => next(err))
   }
 }
 
