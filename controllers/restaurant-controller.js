51960b90dcd16422c466a59dff2e6c74056fbd85
samhuang1992 <bob60117@gmail.com>
2022-02-26 11:33:48 +0800

    feat: add restaurant page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -12,11 +12,22 @@ const restaurantController = {
           ...r,
           description: r.description.substring(0, 50)
         }))
-        console.log(data)
         return res.render('restaurants', {
           restaurants: data
         })
       })
+  },
+  getRestaurant: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, {
+      include: Category,
+      nest: true,
+      raw: true
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        res.render('restaurant', { restaurant })
+      })
+      .catch(err => next(err))
   }
 }
 
