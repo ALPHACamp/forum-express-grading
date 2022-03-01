31185391bc37dba2fd2dcc8c1a7f052123b52f18
snmo2546 <snmo2546@gmail.com>
2022-03-01 17:04:24 +0800

    feat: admin update restaurant

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -13,9 +13,7 @@ const adminController = {
   },
   postRestaurant: (req, res, next) => {
     const { name, tel, address, openingHours, description } = req.body
-
     if (!name) throw new Error('Restaurant name is required!')
-
     Restaurant.create({
       name,
       tel,
@@ -24,7 +22,7 @@ const adminController = {
       description
     })
       .then(() => {
-        req.flash('success_messages', 'Restaurant was successfully created') // 在畫面顯示成功提示
+        req.flash('success_messages', 'Restaurant was successfully created')
         res.redirect('/admin/restaurants')
       })
       .catch(err => next(err))
@@ -37,6 +35,37 @@ const adminController = {
         if (!restaurant) throw new Error('Restaurant doesn\'t exist!')
 
         res.render('admin/restaurant', { restaurant })
+      })
+      .catch(err => next(err))
+  },
+  editRestaurant: (req, res, next) => {
+    Restaurant.findByPk(req.params.id, {
+      raw: true
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        res.render('admin/edit-restaurant', { restaurant })
+      })
+      .catch(err => next(err))
+  },
+  putRestaurant: (req, res, next) => {
+    const { name, tel, address, openingHours, description } = req.body
+    if (!name) throw new Error('Restaurant name is required!')
+
+    Restaurant.findByPk(req.params.id)
+      .then(restaurant => {
+        if (!restaurant) throw new Error('Restaurant does not exist!')
+        return restaurant.update({
+          name,
+          tel,
+          address,
+          openingHours,
+          description
+        })
+      })
+      .then(() => {
+        req.flash('success_messages', 'Restaurant was successfully updated')
+        res.redirect('/admin/restaurants')
       })
       .catch(err => next(err))
   }
