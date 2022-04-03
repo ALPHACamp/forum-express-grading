7b35231b6ebe1256b831ffc3749c7f944f7c6065
Sin0001 <bboyliljie@gmail.com>
2022-04-04 00:23:05 +0800

    feat: add ifCond hbs helper & update category selector

--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -2,11 +2,14 @@ const dayjs = require('dayjs')
 
 module.exports = {
   currentYear: () => dayjs().year(),
-  formateRole: (number) => {
+  formateRole: function (number) {
     if (number === 1) {
       return 'Admin'
     } else {
       return 'User'
     }
+  },
+  ifCond: function (a, b, options) {
+    return a === b ? options.fn(this) : options.inverse(this)
   }
 }
