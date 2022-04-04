08e5bede551b3a519306a52880a58c5eae4d4514
Sin0001 <bboyliljie@gmail.com>
2022-04-04 13:03:47 +0800

    feat: add restaurant page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -14,6 +14,21 @@ const restaurantController = {
         restaurants: data
       })
     })
+  },
+
+  getRestaurant: async (req, res, next) => {
+    const id = req.params.id
+    try {
+      const restaurant = await Restaurant.findByPk(id, {
+        include: Category,
+        nest: true,
+        raw: true
+      })
+      if (!restaurant) throw new Error('Restaurant didn\'t exist!')
+      res.render('restaurant', { restaurant })
+    } catch (err) {
+      next(err)
+    }
   }
 }
 module.exports = restaurantController
