1f3e39a2d26f4b56c9841a8022092e1361aa20ed
Sin0001 <bboyliljie@gmail.com>
2022-04-04 12:13:54 +0800

    feat: category create

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -6,6 +6,19 @@ const categoryController = {
     })
       .then(categories => res.render('admin/categories', { categories }))
       .catch(err => next(err))
+  },
+
+  postCategory: async (req, res, next) => {
+    try {
+      const { name } = req.body
+      if (!name) throw new Error('未輸入分類名稱')
+      const category = await Category.findOne({ where: { name } })
+      if (category) throw new Error(`已經有 ${name} 這項分類`)
+      await Category.create({ name })
+      res.redirect('back')
+    } catch (err) {
+      next(err)
+    }
   }
 }
 module.exports = categoryController
