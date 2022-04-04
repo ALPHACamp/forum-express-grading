8c2e8481fafcfd336b196eea8fe33214950509d8
Sin0001 <bboyliljie@gmail.com>
2022-04-04 14:08:53 +0800

    Merge R02

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -20,12 +20,30 @@ const restaurantController = {
     const id = req.params.id
     try {
       const restaurant = await Restaurant.findByPk(id, {
-        include: Category,
-        nest: true,
-        raw: true
+        include: Category
       })
       if (!restaurant) throw new Error('Restaurant didn\'t exist!')
-      res.render('restaurant', { restaurant })
+      let viewCounts = restaurant.viewCounts
+      viewCounts += 1
+      await restaurant.update({
+        viewCounts
+      })
+      res.render('restaurant', { restaurant: restaurant.toJSON() })
+    } catch (err) {
+      next(err)
+    }
+  },
+
+  getDashboard: async (req, res, next) => {
+    const id = req.params.id
+    try {
+      const restaurant = await Restaurant.findByPk(id, {
+        include: Category,
+        raw: true,
+        nest: true
+      })
+      if (!restaurant) throw new Error('Restaurant didn\'t exist!')
+      res.render('dashboard', { restaurant })
     } catch (err) {
       next(err)
     }
---
+++ migrations/20220404054014-add-viewCounts-to-restaurant.js
@@ -0,0 +1,13 @@
+'use strict'
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('Restaurants', 'view_counts', {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      default: 0
+    })
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('Restaurants', 'view_counts')
+  }
+}
