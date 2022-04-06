c1f89543022e0e9aa2a8124d17bc0d1a832df8d0
Sin0001 <bboyliljie@gmail.com>
2022-04-06 23:47:56 +0800

    feat: get user's favorited restaurants & switch button

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -23,9 +23,11 @@ const restaurantController = {
       Category.findAll({ raw: true })
     ])
       .then(([restaurants, categories]) => {
+        const favoritedRestaurantsId = req.user && req.user.FavoritedRestaurants.map(fr => fr.id)
         const data = restaurants.rows.map(r => ({
           ...r,
-          description: r.description.substring(0, 50)
+          description: r.description.substring(0, 50),
+          isFavorited: favoritedRestaurantsId.includes(r.id)
         }))
         return res.render('restaurants', {
           restaurants: data,
@@ -43,16 +45,21 @@ const restaurantController = {
       const restaurant = await Restaurant.findByPk(id, {
         include: [
           Category,
-          { model: Comment, include: User }
+          { model: Comment, include: User },
+          { model: User, as: 'FavoritedUsers' }
         ]
       })
       if (!restaurant) throw new Error('Restaurant didn\'t exist!')
+      const isFavorited = restaurant.FavoritedUsers.some(f => f.id === req.user.id)
       let viewCounts = restaurant.viewCounts
       viewCounts += 1
       await restaurant.update({
         viewCounts
       })
-      res.render('restaurant', { restaurant: restaurant.toJSON() })
+      res.render('restaurant', {
+        restaurant: restaurant.toJSON(),
+        isFavorited
+      })
     } catch (err) {
       next(err)
     }
