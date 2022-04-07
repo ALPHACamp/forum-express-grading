0293a357f151f50da09dc37ee4ef4e90bb8e6ec6
Sin0001 <bboyliljie@gmail.com>
2022-04-07 13:05:12 +0800

    feat: create followship model

--- models/user.js
+++ models/user.js
@@ -22,6 +22,18 @@ module.exports = (sequelize, DataTypes) => {
         foreignKey: 'userId',
         as: 'LikedRestaurants'
       })
+      // 找我的追蹤者
+      User.belongsToMany(User, {
+        through: models.Followship,
+        foreignKey: 'followingId',
+        as: 'Followers'
+      })
+      // 找我正在追蹤的人
+      User.belongsToMany(User, {
+        through: models.Followship,
+        foreignKey: 'followerId',
+        as: 'Followings'
+      })
     }
   };
   User.init({
