adfa358e6404999ac68a98c2fd6fad82aea149fa
jameshoo123 <hoosinyong@hotmail.com>
2022-04-13 17:20:39 +0800

    feat: modify models user.js and user-controller.js

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -10,7 +10,7 @@ const userController = {
     if (req.body.password !== req.body.passwordCheck) {
       throw new Error('Passwords do not match!')
     }
-    User.findOne({ where: { email: req.body.email } })
+    return User.findOne({ where: { email: req.body.email } })
       .then(user => {
         if (user) throw new Error('Email already exists!')
         return bcrypt.hash(req.body.password, 10)
