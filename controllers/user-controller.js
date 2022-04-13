c383d3b9d42cab006dd2b29ba25daf1b8d66c7d6
JINGWEN LIN <yaysa903@gmail.com>
2022-04-13 23:00:38 +0800

    feat: flash msg & signup verification

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -5,20 +5,25 @@ const userController = {
   signUpPage: (req, res) => {
     return res.render('signup')
   },
-  signUP: async (req, res) => {
+  signUP: async (req, res, next) => {
     try {
-      const { name, email, password } = req.body
-      const hash = bcrypt.hashSync(password, 10)
+      const { name, email, password, passwordCheck } = req.body
+      if (password !== passwordCheck) throw new Error('兩次密碼不相符！')
 
+      const user = await User.findOne({ where: { email } })
+      if (user) throw new Error('該電子郵件已經註冊過！')
+
+      const hash = bcrypt.hashSync(password, 10)
       await User.create({
         name,
         email,
         password: hash
       })
 
+      req.flash('success_messages', '帳號成功註冊！')
       return res.redirect('/signin')
     } catch (err) {
-      console.error(err)
+      next(err)
     }
   }
 }
--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,8 @@ const admin = require('./module/admin')
 const restaurantController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
 
+const { generalErrorHandler } = require('../middleware/error-handler')
+
 router.use('/admin', admin)
 
 router.get('/signup', userController.signUpPage)
@@ -13,5 +15,6 @@ router.post('/signup', userController.signUP)
 
 router.get('/restaurants', restaurantController.getRestaurants)
 router.get('/', (req, res) => res.redirect('/restaurants'))
+router.use('/', generalErrorHandler)
 
 module.exports = router
