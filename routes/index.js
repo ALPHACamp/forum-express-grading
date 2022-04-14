6b7d676f194636f7f9d78fffa16d4eb00931a52f
aziz0916 <aziz0916@yahoo.com.tw>
2022-04-14 21:56:07 +0800

    feat: user signup

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const router = express.Router()
-
-const restaurantController = require('../controllers/restaurant-controller')
 const admin = require('./modules/admin')
+const restaurantController = require('../controllers/restaurant-controller')
+const userController = require('../controllers/user-controller')
 
 router.use('/admin', admin)
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
 router.get('/restaurants', restaurantController.getRestaurants)
 router.use('/', (req, res) => res.redirect('/restaurants'))
 
