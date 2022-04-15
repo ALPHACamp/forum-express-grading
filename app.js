91d61c05f1c82990a7b5d63cddae7068cda12243
Jyun-Fu-Chen <than3698741@gmail.com>
2022-04-15 20:30:56 +0800

    feat: add index page

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const routes = require('./routes')
-const exphbs = require('express-handlebars')
+const handlebars = require('express-handlebars')
 
 const app = express()
 const port = process.env.PORT || 3000
 
-app.engine('hbs', exphbs.engine({ extname: '.hbs' }))
+app.engine('.hbs', handlebars({ extname: '.hbs' }))
 
 app.set('view engine', '.hbs')
 app.use(routes)
