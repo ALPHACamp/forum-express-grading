f1a68644c3e4e61384ed7840f211c4d84d7f5997
Rubylago <rubybibi6@gmail.com>
2022-04-16 21:55:44 +0800

    feat: add index page

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.set('view engine', 'hbs')
 app.use(routes)
 
 app.listen(port, () => {
-  console.info(`Example app listening on port ${port}!`)
+  console.info(`Example app listening on port ${port}! http://localhost:${port}`)
 })
 
 module.exports = app
