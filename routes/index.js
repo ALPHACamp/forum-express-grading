2e33569977e02ca90741727bdd917d34ed5b7ae6
Rubylago <rubybibi6@gmail.com>
2022-04-17 22:46:12 +0800

    feat: passport init & sigin

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,23 @@ const router = express.Router()
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
 const { generalErrorHandler } = require('../middleware/error-handler')
+const passport = require('../config/passport')
 const admin = require('./modules/admin')
 
 router.use('/admin', admin)
+
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
+
+router.get('/signin', userController.signInPage)
+router.post('/signin', passport.authenticate('local', {
+  failureRedirect: '/signin',
+  failureFlash: true
+}), userController.signIn)
+
 router.get('/restaurants', restController.getRestaurants)
+
 router.get('/', (req, res) => res.redirect('/restaurants'))
 router.use('/', generalErrorHandler)
+
 module.exports = router
