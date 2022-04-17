c767212e531e480980c14c7b1d6880324eabe467
zxc58 <abc01234567889@gmail.com>
2022-04-18 00:03:06 +0800

    u107表單驗證與錯誤訊息

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const router = express.Router()
 const restController = require('../controllers/restaurant-controller')
 const admin = require('./modules/admin')
 const userController = require('../controllers/user-controller')
+const { generalErrorHandler } = require('../middleware/error-handler')
 //
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
@@ -12,5 +13,5 @@ router.use('/admin', admin)
 router.get('/restaurants', restController.getRestaurants)
 
 router.use('/', (req, res) => res.redirect('/restaurants'))
-
+router.use('/', generalErrorHandler)
 module.exports = router
