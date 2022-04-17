469cf8e27a2b093e47101670d69bf07eac37ca20
zxc58 <abc01234567889@gmail.com>
2022-04-17 19:01:40 +0800

    新增admin頁面路由

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,6 +1,6 @@
 const restaurantController = {
-    getRestaurants: (req, res) => {
-      return res.render('restaurants')
-    }
+  getRestaurants: (req, res) => {
+    return res.render('restaurants')
   }
-  module.exports = restaurantController
+}
+module.exports = restaurantController
--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,10 @@ const express = require('express')
 
 const router = express.Router()
 
-
-
-const restController = require('../controllers/restaurant-controller') 
-
-
-
-
-router.get('/restaurants', restController.getRestaurants) 
+const restController = require('../controllers/restaurant-controller')
+const admin = require('./modules/admin')
+router.use('/admin', admin)
+router.get('/restaurants', restController.getRestaurants)
 
 router.use('/', (req, res) => res.redirect('/restaurants'))
 
