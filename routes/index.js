b7be27997458b8957e3b3d051a891bd81848b488
zxc58 <abc01234567889@gmail.com>
2022-04-17 23:32:40 +0800

    u106 建立使用者註冊流程

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,10 @@ const router = express.Router()
 
 const restController = require('../controllers/restaurant-controller')
 const admin = require('./modules/admin')
+const userController = require('../controllers/user-controller')
+//
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
 router.use('/admin', admin)
 router.get('/restaurants', restController.getRestaurants)
 
