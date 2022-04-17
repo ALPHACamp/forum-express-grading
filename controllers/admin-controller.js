92bffcfe6bd7839474a4632cb5a212d7c26497ed
JINGWEN LIN <yaysa903@gmail.com>
2022-04-17 22:05:43 +0800

    feat: modify admin restaurants page

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,6 +1,17 @@
+const { Restaurant } = require('../models')
+
 const adminController = {
-  getRestaurants: (req, res) => {
-    res.render('admin/restaurants')
+  getRestaurants: async (req, res, next) => {
+    try {
+      const restaurants = await Restaurant.findAll({
+        raw: true,
+        nest: true
+      })
+
+      return res.render('admin/restaurants', { restaurants })
+    } catch (err) {
+      next(err)
+    }
   }
 }
 
