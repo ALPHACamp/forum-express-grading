3096f458c1a50d7767c6c5e1beb13f2e3c930f43
zxc58 <abc01234567889@gmail.com>
2022-04-18 20:09:39 +0800

    u114 新增一筆餐廳資料

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -4,7 +4,29 @@ const adminController = {
     Restaurant.findAll({
       raw: true
     })
-      .then(restaurants => res.render('admin/restaurants', { restaurants }))
+      .then(restaurants => {
+        res.render('admin/restaurants', { restaurants })
+        console.log({ restaurants })
+      })
+      .catch(err => next(err))
+  },
+  createRestaurant: (req, res) => {
+    return res.render('admin/create-restaurant')
+  },
+  postRestaurant: (req, res, next) => {
+    const { name, tel, address, openingHours, description } = req.body
+    if (!name) throw new Error('Restaurant name is required!')
+    Restaurant.create({
+      name,
+      tel,
+      address,
+      openingHours,
+      description
+    })
+      .then(() => {
+        req.flash('success_messages', 'restaurant was successfully created')
+        res.redirect('/admin/restaurants')
+      })
       .catch(err => next(err))
   }
 }
