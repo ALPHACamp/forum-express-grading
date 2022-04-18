3940cb9dc4c5eb7d75918ed9acac69acf74b9730
GallonShih <gallonshih0526@gmail.com>
2022-04-19 07:15:33 +0800

    feet: add user model

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const restController = require('../controllers/restaurant-controller')
 const admin = require('./modules/admin')
 router.use('/admin', admin)
-router.get('/restaurants', restController.getRestaurants) 
+router.get('/restaurants', restController.getRestaurants)
 router.use('/', (req, res) => res.redirect('/restaurants'))
 
 module.exports = router
