325c984d3b79abd0de61332cc06f36ceae6f1cbe
Marcus Liu <me@mail.lmarcus.onl>
2022-04-19 02:38:42 +0800

    feat: 新增首頁

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// 載入 controller
+const restController = require('../controllers/restaurant-controller')
+
+// 條件較多的 route 盡量往前面
+
+router.get('/restaurants', restController.getRestaurants)
+router.use('/', (req, res) => res.redirect('/restaurants'))
 
 module.exports = router
