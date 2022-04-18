79391c949ca787b07feeca56b9ee32ed1616445c
naivelove0822 <naivelove0822@gmail.com>
2022-04-18 15:39:15 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
+// 重要 每次打開都要設定 nvm use 14.16.0 node版本
+
 const express = require('express')
 const routes = require('./routes')
+const handlebars = require('express-handlebars')
 
 const app = express()
 const port = process.env.PORT || 3000
 
+app.engine('hbs', handlebars({ extname: '.hbs' }))
+app.set('view engine', 'hbs')
 app.use(routes)
 
 app.listen(port, () => {
