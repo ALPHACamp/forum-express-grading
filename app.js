68bf94c8086c28e8725924cceaa608ee9d6b8718
LouisChen-TW <yue841206@gmail.com>
2022-04-18 11:32:52 +0800

    feat: user signup

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ const db = require('./models')
 app.engine('hbs', handlebars({ extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(express.urlencoded({ extended: true }))
 app.use(routes)
 
 app.listen(port, () => {
