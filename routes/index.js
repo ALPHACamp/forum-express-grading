5f8d769494b7dc55187beb6d83ed0d5bc4a4923f
eruc <eruc101010@gmail.com>
2022-04-18 16:54:00 +0800

    feat: user signup

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,13 @@
 const express = require('express')
 
 const router = express.Router()
-
+const userController = require('../controllers/user-controller') // 新增user controller
 const admin = require('./modules/admin') // 新增這行，載入 admin.js
 const restController = require('../controllers/restaurant-controller')// 新增，載入 controller
 
 router.use('/admin', admin) // 新增後台
-// 新增
-
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
 router.get('/restaurants', restController.getRestaurants)
 
 router.use('/', (req, res) => res.redirect('/restaurants'))
