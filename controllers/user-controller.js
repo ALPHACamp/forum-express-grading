ea8e254ef0393b8b4ff77a090345de2fe966b58a
PichaHuang <ychuang33@gmail.com>
2022-04-18 17:09:02 +0800

    feat: flash msg & signup verification

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -6,16 +6,25 @@ const userController = {
   signUpPage: (req, res) => {
     res.render('signup')
   },
-  signUp: (req, res) => {
-    bcrypt.hash(req.body.password, 10)
+  signUp: (req, res, next) => {
+    // 如果兩次輸入的密碼不同，就建立一個 Error 物件並拋出
+    if (req.body.password !== req.body.passwordCheck) throw new Error('Passwords do not match!')
+    // 確認資料裡面沒有一樣的 email，若有，就建立一個 Error 物件並拋出
+    User.findOne({ where: { email: req.body.email } })
+      .then(user => {
+        if (user) throw new Error('Email already exists!')
+        return bcrypt.hash(req.body.password, 10) // 前面加 return
+      })
       .then(hash => User.create({
         name: req.body.name,
         email: req.body.email,
         password: hash
       }))
       .then(() => {
+        req.flash('success_messages', '成功註冊帳號！')
         res.redirect('/signin')
       })
+      .catch(err => next(err)) //接住前面拋出的錯誤，呼叫專門做錯誤處理的 middleware
   }
 }
 
