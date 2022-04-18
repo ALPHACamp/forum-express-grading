694099892bf3f07a15f97177cab93a6c6b867de3
zxc58 <abc01234567889@gmail.com>
2022-04-18 12:12:44 +0800

    u108 建立使用者登入機制

--- app.js
+++ app.js
@@ -6,14 +6,18 @@ const session = require('express-session')
 const app = express()
 const port = process.env.PORT || 3000
 const SESSION_SECRET = 'secret'
+const passport = require('./config/passport')
 //
 app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
+app.use(passport.initialize())
+app.use(passport.session())
 app.use(flash())
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
   res.locals.error_messages = req.flash('error_messages')
   next()
 })
+
 app.engine('hbs', handlebars({ extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
