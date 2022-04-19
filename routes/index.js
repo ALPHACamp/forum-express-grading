d89d8ba952c2a5d4029fa9b2f006ce8b7ad8eb37
GallonShih <gallonshih0526@gmail.com>
2022-04-19 20:23:14 +0800

    feet: add user signup

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const router = express.Router()
-const restController = require('../controllers/restaurant-controller')
+
 const admin = require('./modules/admin')
+const restController = require('../controllers/restaurant-controller')
+const userController = require('../controllers/user-controller')
+
 router.use('/admin', admin)
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
 router.get('/restaurants', restController.getRestaurants)
 router.use('/', (req, res) => res.redirect('/restaurants'))
 
