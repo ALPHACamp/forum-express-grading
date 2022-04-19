61f859bdd168b201be58ef13d7ccf47caf256d81
JINGWEN LIN <yaysa903@gmail.com>
2022-04-20 00:07:33 +0800

    feat: add category update

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -3,8 +3,12 @@ const { Category } = require('../models')
 const categoryController = {
   getCategories: async (req, res, next) => {
     try {
-      const categories = await Category.findAll({ raw: true })
-      return res.render('admin/categories', { categories })
+      const { id } = req.params
+      const [categories, category] = await Promise.all([
+        Category.findAll({ raw: true }),
+        id ? Category.findByPk(id, { raw: true }) : null
+      ])
+      return res.render('admin/categories', { categories, category })
     } catch (err) {
       next(err)
     }
@@ -12,12 +16,26 @@ const categoryController = {
   postCategory: async (req, res, next) => {
     try {
       const { name } = req.body
-      if (!name) throw new Error('名字欄位不可為空。')
+      if (!name) throw new Error('名字欄位不可為空！')
 
       const existedName = await Category.findOne({ where: { name } })
-      if (existedName) throw new Error('不可新增已存在的名稱。')
+      if (existedName) throw new Error('不可新增已存在的名稱！')
 
       await Category.create({ name })
+      return res.redirect('/admin/categories')
+    } catch (err) {
+      next(err)
+    }
+  },
+  putCategory: async (req, res, next) => {
+    try {
+      const { id } = req.params
+      const { name } = req.body
+
+      const category = await Category.findByPk(id)
+      if (!category) throw new Error('該分類不存在！')
+
+      await category.update({ name })
       return res.redirect('/admin/categories')
     } catch (err) {
       next(err)
--- routes/module/admin.js
+++ routes/module/admin.js
@@ -9,6 +9,8 @@ const upload = require('../../middleware/multer')
 router.get('/users', adminController.getUsers)
 router.patch('/users/:id', adminController.patchUser)
 
+router.get('/categories/:id', categoryController.getCategories)
+router.put('/categories/:id', categoryController.putCategory)
 router.get('/categories', categoryController.getCategories)
 router.post('/categories', categoryController.postCategory)
 
