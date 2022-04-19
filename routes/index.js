95fa8fe15f18764a2eddbe8f6235a5503ca4fa84
eruc <eruc101010@gmail.com>
2022-04-19 14:27:52 +0800

    feat: create restaurant

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,19 @@
 const express = require('express')
-
 const router = express.Router()
-const userController = require('../controllers/user-controller') // 新增user controller
 const passport = require('../config/passport')
-const admin = require('./modules/admin') // 新增這行，載入 admin.js
-const restController = require('../controllers/restaurant-controller')// 新增，載入 controller
+const admin = require('./modules/admin')
+const restController = require('../controllers/restaurant-controller')
+const userController = require('../controllers/user-controller')
+const { authenticated, authenticatedAdmin } = require('../middleware/auth')
 const { generalErrorHandler } = require('../middleware/error-handler')
-const { authenticated } = require('../middleware/auth')
 
-router.use('/admin', admin) // 新增後台
+router.use('/admin', authenticatedAdmin, admin)
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
 router.get('/signin', userController.signInPage)
-router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn) // 注意是 Post
+router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
 router.get('/logout', userController.logout)
 router.get('/restaurants', authenticated, restController.getRestaurants)
 router.use('/', (req, res) => res.redirect('/restaurants'))
-router.use('/', generalErrorHandler) // 加入這行
-
+router.use('/', generalErrorHandler)
 module.exports = router
