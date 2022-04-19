ae8c71713fba71b9947fceaa52aed49dcd3c39b6
GallonShih <gallonshih0526@gmail.com>
2022-04-19 20:53:55 +0800

    feat: flash msg & signup verification

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,20 +1,28 @@
-const bcrypt = require('bcryptjs') //載入 bcrypt
+const bcrypt = require('bcryptjs')
 const db = require('../models')
 const { User } = db
 const userController = {
   signUpPage: (req, res) => {
     res.render('signup')
   },
-  signUp: (req, res) => {
-    bcrypt.hash(req.body.password, 10)
+  signUp: (req, res, next) => {
+    if (req.body.password !== req.body.passwordCheck) throw new Error('Passwords do not match!')
+    
+    User.findOne({ where: { email: req.body.email } })
+      .then(user => {
+        if (user) throw new Error('Email already exists!')
+        return bcrypt.hash(req.body.password, 10)
+      })
       .then(hash => User.create({
         name: req.body.name,
         email: req.body.email,
         password: hash
       }))
       .then(() => {
+        req.flash('success_messages', '成功註冊帳號！')
         res.redirect('/signin')
       })
+      .catch(err => next(err))
   }
 }
 module.exports = userController
