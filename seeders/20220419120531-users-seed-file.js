e29017fb5246e637da4d35ff40a0cbb85928b9d4
maximeri <t.s.eliot1996@gmail.com>
2022-04-19 20:28:39 +0800

    feat: add seed data

---
+++ seeders/20220419120531-users-seed-file.js
@@ -0,0 +1,31 @@
+'use strict'
+const bcrypt = require('bcryptjs')
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('Users', [{ // 一次新增三筆資料
+      email: 'root@example.com',
+      password: await bcrypt.hash('12345678', 10),
+      is_admin: true,
+      name: 'root',
+      created_at: new Date(),
+      updated_at: new Date()
+    }, {
+      email: 'user1@example.com',
+      password: await bcrypt.hash('12345678', 10),
+      is_admin: false,
+      name: 'user1',
+      created_at: new Date(),
+      updated_at: new Date()
+    }, {
+      email: 'user2@example.com',
+      password: await bcrypt.hash('12345678', 10),
+      is_admin: false,
+      name: 'user2',
+      created_at: new Date(),
+      updated_at: new Date()
+    }], {})
+  },
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Users', null, {}) // 第三個參數可以指定 where 條件，但這裡因為全部刪除，所以只傳入空物件。
+  }
+}
