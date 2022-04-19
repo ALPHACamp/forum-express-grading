1e90a8278fbaa3e29896f32212955a89bffa299d
jameshoo123 <hoosinyong@hotmail.com>
2022-04-19 13:00:57 +0800

    feat: category delete

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -26,6 +26,7 @@ router.get('/categories', categoryController.getCategories)
 router.post('/categories', categoryController.postCategory)
 router.get('/categories/:id', categoryController.getCategories)
 router.put('/categories/:id', categoryController.putCategory)
+router.delete('/categories/:id', categoryController.deleteCategory)
 router.get('/', (req, res) => res.redirect('/admin/restaurants'))
 
 module.exports = router
