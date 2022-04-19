0be4d89ef735cf3bcc9c2007ef8ba75bee4e4314
PichaHuang <ychuang33@gmail.com>
2022-04-19 09:49:59 +0800

    feat: user model add is_admin

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
 app.use(passport.initialize())
 app.use(passport.session())
-app.use(flash()) // 掛載套件
+app.use(flash())
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
   res.locals.error_messages = req.flash('error_messages')
