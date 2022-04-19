ac34caa2dbd46ba7f74b9677e19afa13908e76b1
naivelove0822 <naivelove0822@gmail.com>
2022-04-19 14:58:25 +0800

    feat: add error_message flash and session

--- app.js
+++ app.js
@@ -1,14 +1,27 @@
 // 重要 每次打開都要設定 nvm use 14.16.0 node版本
-
 const express = require('express')
 const routes = require('./routes')
 const handlebars = require('express-handlebars')
+const flash = require('connect-flash')
+const session = require('express-session')
 
 const app = express()
 const port = process.env.PORT || 3000
+const SESSION_SECRET = 'secret'
+
+app.use(session({ secret: SESSION_SECRET, resave: false, saveUnintialized: false }))
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.success_messageg = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 
 app.engine('hbs', handlebars({ extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(express.urlencoded({ extended: true }))
+
 app.use(routes)
 
 app.listen(port, () => {
--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,16 @@ const express = require('express')
 const router = express.Router()
 const restController = require('../controllers/restaurant-controller')
 const admin = require('./modules/admin')
-
+const userController = require('../controllers/user-controller')
+const { generalErrorHandler } = require('../middleware/error-handler')
 router.use('/admin', admin)
+
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
+
 router.get('/restaurants', restController.getRestaurants)
 router.get('/', (req, res) => res.redirect('/restaurants'))
+
+router.use('/', generalErrorHandler)
 
 module.exports = router
