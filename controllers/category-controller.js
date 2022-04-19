ff8778e2b0e1367c0a8a824c3918005b685740ba
JINGWEN LIN <yaysa903@gmail.com>
2022-04-19 23:54:10 +0800

    feat: category create

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -8,6 +8,20 @@ const categoryController = {
     } catch (err) {
       next(err)
     }
+  },
+  postCategory: async (req, res, next) => {
+    try {
+      const { name } = req.body
+      if (!name) throw new Error('名字欄位不可為空。')
+
+      const existedName = await Category.findOne({ where: { name } })
+      if (existedName) throw new Error('不可新增已存在的名稱。')
+
+      await Category.create({ name })
+      return res.redirect('/admin/categories')
+    } catch (err) {
+      next(err)
+    }
   }
 }
 
