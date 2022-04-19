2ab2f6b37cb1de96d4f501cf2efc3ffda2252b3f
AngelaC123 <angelarare@gmail.com>
2022-04-19 17:48:37 +0800

    feat: user signup

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const port = process.env.PORT || 3000
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(express.urlencoded({ extended: true }))
+
 app.use(routes)
 
 app.listen(port, () => {
