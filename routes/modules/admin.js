4b445e68c780ce4d7abc9a9ae779a55418102426
softtoday72 <demonhunter720525@hotmail.com>
2022-04-20 15:04:44 +0800

    feat add admin index page fix error

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
 
 // 實際路由: '/admin/restaurant', render到 admin/restaurants.hbs
-router.get('/restaurants', adminController.gerRestaurants)
+router.get('/restaurants', adminController.getRestaurants)
 router.get('/', (req, res) => res.redirect('/admin/restaurants'))
 
 module.exports = router
