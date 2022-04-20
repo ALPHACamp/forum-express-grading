df4c60c6144931453cce6ce6e1b481472ea36341
Charles <charles0527@hotmail.com>
2022-04-21 03:03:26 +0800

    feat: modify admin restaurants page

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,6 +1,11 @@
+const { Restaurant } = require('../models') // 新增這裡
 const adminController = {
-  getRestaurants: (req, res) => {
-    return res.render('admin/restaurants')
+  getRestaurants: (req, res, next) => {
+    Restaurant.findAll({
+      raw: true
+    })
+      .then(restaurants => res.render('admin/restaurants', { restaurants }))
+      .catch(err => next(err))
   }
 }
 module.exports = adminController
