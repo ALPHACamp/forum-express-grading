c75b498b4eeaa1a239b4ddd0e49a4aa9f2b0a81f
JINGWEN LIN <yaysa903@gmail.com>
2022-04-20 17:59:03 +0800

    feat: add restaurants index page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,6 +1,21 @@
+const { Restaurant, Category } = require('../models')
+
 const restaurantController = {
-  getRestaurants: (req, res) => {
-    return res.render('restaurants')
+  getRestaurants: async (req, res, next) => {
+    try {
+      const restaurants = await Restaurant.findAll({
+        raw: true,
+        nest: true,
+        include: [Category]
+      })
+      const revisedRestaurants = restaurants.map(res => ({
+        ...res,
+        description: res.description.substring(0, 50)
+      }))
+      return res.render('restaurants', { restaurants: revisedRestaurants })
+    } catch (err) {
+      next(err)
+    }
   }
 }
 
