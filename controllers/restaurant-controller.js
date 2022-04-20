626a89b7c954ce3f60aa9a7c6d6ddc7441df26fc
jameshoo123 <hoosinyong@hotmail.com>
2022-04-20 15:28:47 +0800

    feat: add comment model

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -3,23 +3,22 @@ const { getOffset, getPagination } = require('../helpers/pagination-helper') // 
 
 const restaurantController = {
   getRestaurants: (req, res, next) => {
-    const DEFAULT_LIMIT = 9 // 加入這行
+    const DEFAULT_LIMIT = 9
     const page = Number(req.query.page) || 1
     const limit = Number(req.query.limit) || DEFAULT_LIMIT
-    const offset = getOffset(limit, page) // 增加這裡
-    const categoryId = Number(req.query.categoryId) || '' // 新增這裡，從網址上拿下來的參數是字串，先轉成 Number 再操作
+    const offset = getOffset(limit, page)
+    const categoryId = Number(req.query.categoryId) || ''
     return Promise.all([
       // Restaurant.findAll({
       Restaurant.findAndCountAll({
         include: Category,
         where: {
-          // 新增查詢條件
-          ...(categoryId ? { categoryId } : {}) // 檢查 categoryId 是否為空值
+          ...(categoryId ? { categoryId } : {})
         },
         nest: true,
         raw: true,
-        limit, // 增加這裡
-        offset // 增加這裡
+        limit,
+        offset
       }),
       Category.findAll({ raw: true })
     ])
@@ -32,11 +31,11 @@ const restaurantController = {
         return res.render('restaurants', {
           restaurants: data,
           categories,
-          categoryId, // 新增這裡
+          categoryId,
           pagination: getPagination(limit, page, restaurants.count)
         })
       })
-      .catch(err => next(err)) // 補上
+      .catch(err => next(err))
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
