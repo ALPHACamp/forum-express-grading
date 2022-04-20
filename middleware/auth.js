7af7b0b44d7d45253846e131436061fe3ee5b884
maximeri <t.s.eliot1996@gmail.com>
2022-04-20 21:18:15 +0800

    feat: add admin user feature

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,9 @@
-const { ensureAuthenticated, getUser } = require('../helpers/auth-helpers')
+const helpers = require('../helpers/auth-helpers')
 
 // 登入驗證
 const authenticated = (req, res, next) => {
   // if (req.isAuthenticated)
-  if (ensureAuthenticated(req)) {
+  if (helpers.ensureAuthenticated(req)) {
     return next()
   }
   res.redirect('/signin')
@@ -12,8 +12,8 @@ const authenticated = (req, res, next) => {
 // admin 身份驗證
 const authenticatedAdmin = (req, res, next) => {
   // if (req.isAuthenticated)
-  if (ensureAuthenticated(req)) { // 是否登入
-    if (getUser(req).isAdmin) return next() // 是否為 admin，是就往下走
+  if (helpers.ensureAuthenticated(req)) { // 是否登入
+    if (helpers.getUser(req).isAdmin) return next() // 是否為 admin，是就往下走
     res.redirect('/') // 否就導回首頁
   } else {
     res.redirect('/signin') // 沒有登入就回 signin page
