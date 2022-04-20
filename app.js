2185c394e10e231b5a84e7541e0ed1b1a83d51a9
KateChu <chu.shihchien@gmail.com>
2022-04-20 09:32:08 +0800

    AC tutorial followed

--- app.js
+++ app.js
@@ -1,17 +1,38 @@
 const express = require('express')
+const path = require('path')
 const handlebars = require('express-handlebars')
 const routes = require('./routes')
+const flash = require('connect-flash')
+const session = require('express-session')
+const methodOverride = require('method-override')
+const SESSION_SECRET = 'secret'
+const passport = require('./config/passport')
+const { getUser } = require('./helpers/auth-helpers')
+const handlebarsHelpers = require('./helpers/handlebars-helpers')
 
 const app = express()
 const port = process.env.PORT || 3000
 
-app.engine('handlebars', handlebars.engine({ defaultLayout: 'main' }))
+app.engine('handlebars', handlebars.engine({ defaultLayout: 'main', helpers: handlebarsHelpers }))
 app.set('view engine', 'handlebars')
+app.use(express.urlencoded({ extended: true }))
+app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
+app.use(methodOverride('_method'))
+app.use('/upload', express.static(path.join(__dirname, 'upload')))
 
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  res.locals.user = getUser(req)
+  next()
+})
 app.use(routes)
 
 app.listen(port, () => {
-  console.info(`Example app listening on port ${port}!`)
+  console.info(`Example app listening on port http://localhost:${port}`)
 })
 
 module.exports = app
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,24 @@
 const express = require('express')
 const router = express.Router()
+const restController = require('../controllers/restaurant-controller')
+const userController = require('../controllers/user-controller')
+const { authenticated, authenticatedAdmin } = require('../middleware/auth')
+const { generalErrorHandler } = require('../middleware/error-handler')
+const admin = require('./modules/admin')
+const passport = require('../config/passport')
 
-router.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+router.use('/admin', authenticatedAdmin, admin)
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
+router.get('/signin', userController.signInPage)
+router.post('/signin', passport.authenticate('local', {
+  failureRedirect: '/signin', failureFlash: true
+}), userController.signIn)
+
+router.get('/logout', userController.logout)
+router.get('/restaurants', authenticated, restController.getRestaurants)
+
+router.use('/', (req, res) => res.redirect('/restaurants'))
+router.use('/', generalErrorHandler)
 
 module.exports = router
