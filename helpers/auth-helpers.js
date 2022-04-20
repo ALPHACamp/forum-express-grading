aec213b9636ba35f963939cdf228f47ce6618ff1
AngelaC123 <angelarare@gmail.com>
2022-04-20 12:05:55 +0800

    feat: user model add is_admin and add authentication to user and admin

--- helpers/auth-helpers.js
+++ helpers/auth-helpers.js
@@ -1,7 +1,15 @@
 const getUser = req => {
   return req.user || null
 }
-module.exports = { getUser }
+
+const ensureAuthenticated = req => {
+  return req.isAuthenticated()
+}
+
+module.exports = {
+  getUser,
+  ensureAuthenticated
+}
 
 // same as above
 // module.exports = {
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const passport = require('../config/passport')
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
 const { generalErrorHandler } = require('../middleware/error-handler')
+const { authenticated } = require('../middleware/auth')
 
 const admin = require('./modules/admin')
 
@@ -16,7 +17,7 @@ router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
 router.get('/logout', userController.logout)
 
-router.get('/restaurants', restController.getRestaurants)
+router.get('/restaurants', authenticated, restController.getRestaurants)
 router.get('/', (req, res) => {
   res.redirect('/restaurants')
 })
