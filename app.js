535f40d1ec94877e0603bc20dd1b1af1afb88cac
KateChu <chu.shihchien@gmail.com>
2022-04-20 10:44:52 +0800

    feat: add imgur api

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ const passport = require('./config/passport')
 const { getUser } = require('./helpers/auth-helpers')
 const handlebarsHelpers = require('./helpers/handlebars-helpers')
 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const app = express()
 const port = process.env.PORT || 3000
 
