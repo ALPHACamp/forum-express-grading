3d1f7c39aa7ac0c61acb984a1dbf89c15dbd45b8
Tommy1023 <j.h.chen1023@gmail.com>
2022-04-20 23:38:22 +0800

    feat: user signup

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -3,6 +3,6 @@ const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
 
 router.get('/restaurants', adminController.getRestaurants)
-router.use('/', (req, res) => res.redirect('/admin/restaurants'))
+router.get('/', (req, res) => res.redirect('/admin/restaurants'))
 
 module.exports = router
