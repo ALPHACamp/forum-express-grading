b9a139eb3f4b5c73889af7431634cea13811de3e
Jyun-Fu-Chen <than3698741@gmail.com>
2022-04-20 22:25:21 +0800

    feat: add imgur api

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const path = require('path')
 const routes = require('./routes')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const handlebars = require('express-handlebars')
 const session = require('express-session')
 const flash = require('connect-flash')
