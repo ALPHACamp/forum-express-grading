869379ea4239e3d54f54598573bb214c51c4831c
Charles <charles0527@hotmail.com>
2022-04-21 03:31:00 +0800

    feat: create restaurant

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,10 +1,31 @@
 const { Restaurant } = require('../models') // 新增這裡
 const adminController = {
   getRestaurants: (req, res, next) => {
-    Restaurant.findAll({
+    return Restaurant.findAll({
       raw: true
     })
       .then(restaurants => res.render('admin/restaurants', { restaurants }))
+      .catch(err => next(err))
+  },
+  createRestaurant: (req, res, next) => {
+    return res.render('admin/create-restaurant')
+  },
+  postRestaurant: (req, res, next) => {
+    const { name, tel, address, openingHours, description } =
+      req.body
+    if (!name) throw new Error('Restaurant name is required!')
+    Restaurant.create({
+      // 產生一個新的 Restaurant 物件實例，並存入資料庫
+      name,
+      tel,
+      address,
+      openingHours,
+      description
+    })
+      .then(() => {
+        req.flash('success_messages', 'Restaurant was successfully created!') // 在畫面顯示成功提示
+        res.redirect('/admin/restaurants') // 新增完成後導回後台首頁
+      })
       .catch(err => next(err))
   }
 }
