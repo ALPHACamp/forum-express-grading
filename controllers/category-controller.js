e9ee09a6c8ed680a35d5f1c5b28d2af76a62dcca
aziz0916 <aziz0916@yahoo.com.tw>
2022-04-20 13:49:35 +0800

    feat: category create

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -6,6 +6,14 @@ const categoryController = {
     })
       .then(categories => res.render('admin/categories', { categories }))
       .catch(err => next(err))
+  },
+  postCategory: (req, res, next) => {
+    const { name } = req.body
+
+    if (!name) throw new Error('Category name is required!')
+    return Category.create({ name })
+      .then(() => res.redirect('/admin/categories'))
+      .catch(err => next(err))
   }
 }
 
