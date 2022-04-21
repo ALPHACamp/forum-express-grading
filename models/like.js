2be381800c674b8cc25bde99f3aea4534ee76172
jameshoo123 <hoosinyong@hotmail.com>
2022-04-21 11:44:35 +0800

    feat: create favorite model

--- models/like.js
+++ models/like.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
-  class Comment extends Model {
+  class Like extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -11,22 +11,19 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate (models) {
       // define association here
-      Comment.belongsTo(models.Restaurant, { foreignKey: 'restaurantId' })
-      Comment.belongsTo(models.User, { foreignKey: 'userId' })
     }
   };
-  Comment.init(
+  Like.init(
     {
-      text: DataTypes.STRING,
-      userId: DataTypes.INTEGER,
-      restaurantId: DataTypes.INTEGER
+      user_id: DataTypes.INTEGER,
+      restaurant_id: DataTypes.INTEGER
     },
     {
       sequelize,
-      modelName: 'Comment',
-      tableName: 'Comments',
+      modelName: 'Like',
+      tableName: 'Likes',
       underscored: true
     }
   )
-  return Comment
+  return Like
 }
