e0c5c4fa71c609859fc31036ef2b858f36df6cce
LouisChen-TW <yue841206@gmail.com>
2022-04-21 11:31:19 +0800

    feat: admin/user page & switch admin/user

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,4 +1,5 @@
 const { Restaurant } = require('../models')
+const { User } = require('../models')
 const { imgurFileHandler } = require('../helpers/file-helpers')
 const adminController = {
   getRestaurants: async (req, res, next) => {
@@ -80,6 +81,33 @@ const adminController = {
       if (!restaurant) throw new Error("Restaurant didn't exist!")
       await restaurant.destroy()
       res.redirect('/admin/restaurants')
+    } catch (err) {
+      next(err)
+    }
+  },
+  getUsers: async (req, res, next) => {
+    try {
+      const users = await User.findAll({
+        raw: true
+      })
+      res.render('admin/users', { users })
+    } catch (err) {
+      next(err)
+    }
+  },
+  patchUser: async (req, res, next) => {
+    try {
+      const user = await User.findByPk(req.params.id)
+      if (user.email === 'root@example.com') {
+        req.flash('error_messages', '禁止變更 root 權限')
+        return res.redirect('back')
+      }
+      user.isAdmin === false ? (user.isAdmin = true) : (user.isAdmin = false)
+      await user.update({
+        isAdmin: user.isAdmin
+      })
+      req.flash('success_messages', '使用者權限變更成功')
+      res.redirect('/admin/users')
     } catch (err) {
       next(err)
     }
