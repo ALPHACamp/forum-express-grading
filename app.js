86d4646c6abafe138af4872917e1357a44292aa4
Marcus Liu <me@mail.lmarcus.onl>
2022-04-22 03:00:02 +0800

    feat: passport init & signin

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const handlebars = require('express-handlebars')
 const flash = require('connect-flash')
 const session = require('express-session')
+const passport = require('./config/passport')
 const routes = require('./routes')
 
 const app = express()
@@ -17,6 +18,10 @@ app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 
 app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
+// 初始化 passport 功能
+app.use(passport.initialize())
+// 增加 session 功能
+app.use(passport.session())
 app.use(flash())
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,19 @@ const userController = require('../controllers/user-controller')
 
 // 錯誤處理
 const { generalErrorHandler } = require('../middleware/error-handler')
+const passport = require('../config/passport')
 const admin = require('./modules/admin')
 // 條件較多的 route 盡量往前面
 
 router.use('/admin', admin)
+// 註冊
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
+//
+router.get('/signin', userController.signInPage)
+router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn) // 注意是 Post
+router.get('/logout', userController.logout)
+
 router.get('/restaurants', restController.getRestaurants)
 
 router.use('/', (req, res) => res.redirect('/restaurants'))
