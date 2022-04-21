7fd13a4b1c1a2d931183a1f231ad34c568a513c7
KateChu <chu.shihchien@gmail.com>
2022-04-21 14:54:16 +0800

    feat: user managemanet

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,4 +1,4 @@
-const { Restaurant } = require('../models')
+const { Restaurant, User } = require('../models')
 const { imgurFileHandler } = require('../helpers/file-helpers')
 
 const adminController = {
@@ -85,6 +85,34 @@ const adminController = {
         return restaurant.destroy()
       })
       .then(() => res.redirect('/admin/restaurants'))
+      .catch(err => next(err))
+  },
+  getUsers: (req, res) => {
+    return User.findAll({
+      raw: true
+    }).then(users => {
+      return res.render('admin/manage-users', {
+        users: users
+      })
+    })
+  },
+  patchUser: (req, res, next) => {
+    return User.findByPk(req.params.id)
+      .then(user => {
+        if (!user) throw new Error("沒有找到該名使用者")
+        if (user.email === 'root@example.com') {
+          req.flash('error_messages', '禁止變更 root 權限')
+          return res.redirect('back')
+        }
+        user.isAdmin = !user.isAdmin
+        return user.update({ isAdmin: user.isAdmin })
+      })
+      .then(
+        () => {
+          req.flash('success_messages', '使用者權限變更成功')
+          return res.redirect('/admin/users')
+        }
+      )
       .catch(err => next(err))
   }
 }
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -3,6 +3,9 @@ const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
 const upload = require('../../middleware/multer')
 
+router.patch('/users/:id', adminController.patchUser)
+router.get('/users', adminController.getUsers)
+
 router.get('/restaurants/create', adminController.createRestaurant)
 router.get('/restaurants/:id/edit', adminController.editRestaurant)
 router.get('/restaurants/:id', adminController.getRestaurant)
@@ -11,5 +14,4 @@ router.delete('/restaurants/:id', adminController.deleteRestaurant)
 router.get('/restaurants', adminController.getRestaurants)
 router.post('/restaurants', upload.single('image'), adminController.postRestaurant)
 router.get('', (req, res) => res.redirect('/admin/restaurants'))
-
 module.exports = router
