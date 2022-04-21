83de5338132c52ec30990ff9009a309829071501
Marcus Liu <me@mail.lmarcus.onl>
2022-04-21 17:44:25 +0800

    feat: user signup 注意 model/user.js 有改變

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ app.engine('hbs', handlebars({ extname: '.hbs' }))
 // 設定使用 Handlebars 做為樣板引擎
 app.set('view engine', 'hbs')
 
+// body-parser
+app.use(express.urlencoded({ extended: true }))
+
 app.use(routes)
 
 app.listen(port, () => {
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,14 @@ const router = express.Router()
 
 // 載入 controller
 const restController = require('../controllers/restaurant-controller')
+const userController = require('../controllers/user-controller')
 
 const admin = require('./modules/admin')
 // 條件較多的 route 盡量往前面
 
 router.use('/admin', admin)
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
 router.get('/restaurants', restController.getRestaurants)
 router.use('/', (req, res) => res.redirect('/restaurants'))
 
