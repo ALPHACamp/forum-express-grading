d9ef053a195108d39cba1b1eff0e4483bd78351e
jameshoo123 <hoosinyong@hotmail.com>
2022-04-21 16:32:09 +0800

    feat: addFollowing & removeFollowing function

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,4 +1,11 @@
-const { User, Comment, Restaurant, Favorite, Like } = require('../models')
+const {
+  User,
+  Comment,
+  Restaurant,
+  Favorite,
+  Like,
+  Followship
+} = require('../models')
 const bcrypt = require('bcryptjs')
 const { imgurFileHandler } = require('../helpers/file-helpers')
 
@@ -176,21 +183,69 @@ const userController = {
   },
   getTopUsers: (req, res, next) => {
     // 撈出所有 User 與 followers 資料
-    return User.findAll({
-      include: [{ model: User, as: 'Followers' }]
-    })
-      .then(users => {
-        // 整理 users 資料，把每個 user 項目都拿出來處理一次，並把新陣列儲存在 users 裡
-        users = users.map(user => ({
-          // 整理格式
-          ...user.toJSON(),
-          // 計算追蹤者人數
-          followerCount: user.Followers.length,
-          // 判斷目前登入使用者是否已追蹤該 user 物件
-          isFollowed: req.user.Followings.some(f => f.id === user.id)
-        }))
-        res.render('top-users', { users: users })
+    return (
+      User.findAll({
+        include: [{ model: User, as: 'Followers' }]
       })
+        // .then(users => {
+        //   // 整理 users 資料，把每個 user 項目都拿出來處理一次，並把新陣列儲存在 users 裡
+        //   users = users.map(user => ({
+        //     // 整理格式
+        //     ...user.toJSON(),
+        //     // 計算追蹤者人數
+        //     followerCount: user.Followers.length,
+        //     // 判斷目前登入使用者是否已追蹤該 user 物件
+        //     isFollowed: req.user.Followings.some(f => f.id === user.id)
+        //   }))
+        //   users = users.sort((a, b) => b.followerCount - a.followerCount)
+        // res.render('top-users', { users: users })
+        .then(users => {
+          const result = users
+            .map(user => ({
+              ...user.toJSON(),
+              followerCount: user.Followers.length,
+              isFollowed: req.user.Followings.some(f => f.id === user.id)
+            }))
+            .sort((a, b) => b.followerCount - a.followerCount)
+          res.render('top-users', { users: result })
+        })
+        .catch(err => next(err))
+    )
+  },
+  addFollowing: (req, res, next) => {
+    const { userId } = req.params
+    Promise.all([
+      User.findByPk(userId),
+      Followship.findOne({
+        where: {
+          followerId: req.user.id,
+          followingId: req.params.userId
+        }
+      })
+    ])
+      .then(([user, followship]) => {
+        if (!user) throw new Error("User didn't exist!")
+        if (followship) throw new Error('You are already following this user!')
+        return Followship.create({
+          followerId: req.user.id,
+          followingId: userId
+        })
+      })
+      .then(() => res.redirect('back'))
+      .catch(err => next(err))
+  },
+  removeFollowing: (req, res, next) => {
+    Followship.findOne({
+      where: {
+        followerId: req.user.id,
+        followingId: req.params.userId
+      }
+    })
+      .then(followship => {
+        if (!followship) throw new Error("You haven't followed this user!")
+        return followship.destroy()
+      })
+      .then(() => res.redirect('back'))
       .catch(err => next(err))
   }
 }
