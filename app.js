078d713426166b9eb6605f2ed8fb122f27710072
Marcus Liu <me@mail.lmarcus.onl>
2022-04-22 01:12:30 +0800

    feat: add error flash message

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
+const flash = require('connect-flash')
+const session = require('express-session')
 const routes = require('./routes')
 
 const app = express()
 const port = process.env.PORT || 3000
-
-const db = require('./models')
+const SESSION_SECRET = 'eldenring'
 
 // 註冊 Handlebars 樣板引擎，並指定副檔名為 .hbs
 app.engine('hbs', handlebars({ extname: '.hbs' }))
@@ -14,6 +15,14 @@ app.set('view engine', 'hbs')
 
 // body-parser
 app.use(express.urlencoded({ extended: true }))
+
+app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.success_massages = req.flash('success_massages')
+  res.locals.error_massages = req.flash('error_massages')
+  next()
+})
 
 app.use(routes)
 
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -5,16 +5,26 @@ const userController = {
   signUpPage: (req, res) => {
     res.render('signup')
   },
-  signUp: (req, res) => {
-    bcrypt.hash(req.body.password, 10)
+  signUp: (req, res, next) => {
+    // 輸入密碼不同，就建立一個 Error 物件並拋出
+    if (req.body.password !== req.body.passwordCheck) throw new Error('Password do not match!')
+
+    // 驗證資料裡是否有一樣的 email，有就建立一個 error 物件並拋出
+    User.findOne({ where: { email: req.body.email } })
+      .then(user => {
+        if (user) throw new Error('email already exists!')
+        return bcrypt.hash(req.body.password, 10)
+      })
       .then(hash => User.create({
         name: req.body.name,
         email: req.body.email,
         password: hash
       }))
       .then(() => {
+        req.flash('success_messages', '成功註冊帳號！')
         res.redirect('/signin')
       })
+      .catch(err => next(err))
   }
 }
 
