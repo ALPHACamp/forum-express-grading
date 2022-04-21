f25d8ad2f808c687f8fb7dbc2cecefaaab620048
softtoday72 <demonhunter720525@hotmail.com>
2022-04-22 00:54:57 +0800

    $ git commit -m

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
+const { authenticatedAdmin } = require('../../middleware/auth')
 
 // 實際路由: '/admin/restaurant', render到 admin/restaurants.hbs
-router.get('/restaurants', adminController.getRestaurants)
+router.get('/restaurants', authenticatedAdmin, adminController.getRestaurants)
 router.get('/', (req, res) => res.redirect('/admin/restaurants'))
 
 module.exports = router
