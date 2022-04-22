7a82d4b0cd6f8d5634250e6b42caf83bc3ab9e6b
Marcus Liu <me@mail.lmarcus.onl>
2022-04-22 14:49:51 +0800

    feat: add header & footer

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express')
 const handlebars = require('express-handlebars')
 const flash = require('connect-flash')
 const session = require('express-session')
+const handlebarsHelpers = require('./helpers/handlebars-helpers')
 const passport = require('./config/passport')
+const { getUser } = require('./helpers/auth-helpers')
 const routes = require('./routes')
 
 const app = express()
@@ -10,7 +12,7 @@ const port = process.env.PORT || 3000
 const SESSION_SECRET = 'eldenring'
 
 // 註冊 Handlebars 樣板引擎，並指定副檔名為 .hbs
-app.engine('hbs', handlebars({ extname: '.hbs' }))
+app.engine('hbs', handlebars({ extname: '.hbs', helpers: handlebarsHelpers }))
 // 設定使用 Handlebars 做為樣板引擎
 app.set('view engine', 'hbs')
 
@@ -26,6 +28,7 @@ app.use(flash())
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
   res.locals.error_messages = req.flash('error_messages')
+  res.locals.user = getUser(req)
   next()
 })
 
