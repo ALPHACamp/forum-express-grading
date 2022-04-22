1c8f5f143aa5c9302e23f47d8b4e8b39f5a5462b
Rubylago <rubybibi6@gmail.com>
2022-04-22 17:25:06 +0800

    feat: category delete

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -19,6 +19,7 @@ router.get('/categories/:id', categoryController.getCategories)
 router.put('/categories/:id', categoryController.putCategory)
 router.get('/categories', categoryController.getCategories)
 router.post('/categories', categoryController.postCategory)
+router.delete('/categories/:id', categoryController.deleteCategory)
 
 router.get('/', (req, res) => res.redirect('/admin/restaurants'))
 
