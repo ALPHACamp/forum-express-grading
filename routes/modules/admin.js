d33f263cb85478b2a42c209bf817c8f3c1699ab4
softtoday72 <demonhunter720525@hotmail.com>
2022-04-22 16:49:57 +0800

    feat creat restaurant

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
-const { authenticatedAdmin } = require('../../middleware/auth')
+// const { authenticatedAdmin } = require('../../middleware/auth')
 
 // 實際路由: '/admin/restaurant', render到 admin/restaurants.hbs
-router.get('/restaurants', authenticatedAdmin, adminController.getRestaurants)
+router.get('/restaurants/create', adminController.createRestaurant)
+router.get('/restaurants', adminController.getRestaurants)
+router.post('/restaurants', adminController.postRestaurant)
 router.get('/', (req, res) => res.redirect('/admin/restaurants'))
 
 module.exports = router
