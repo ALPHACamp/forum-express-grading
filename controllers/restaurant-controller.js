22b9125ce5cc637cba1956dd8e646a1af3115f98
Rubylago <rubybibi6@gmail.com>
2022-04-23 13:15:00 +0800

    feat: add restaurants index page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,6 +1,19 @@
+const { Restaurant, Category } = require('../models')
 const restaurantController = {
   getRestaurants: (req, res) => {
-    return res.render('restaurants')
+    return Restaurant.findAll({
+      raw: true,
+      include: Category,
+      nest: true
+    }).then(restaurants => {
+      const data = restaurants.map(r => {
+        return { ...r, description: r.description.substring(0, 50) }
+      })
+      // console.log(data) // [{...},{...}]
+      return res.render('restaurants', {
+        restaurants: data
+      })
+    })
   }
 }
 module.exports = restaurantController
