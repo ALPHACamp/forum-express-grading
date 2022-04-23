6c329c815aa75d718f8a393403f3eec7c063bad8
Tommy1023 <j.h.chen1023@gmail.com>
2022-04-24 04:03:27 +0800

    feat:add user management rights

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,4 +1,4 @@
-const { Restaurant } = require('../models')
+const { Restaurant, User } = require('../models')
 const { imgurFileHandler } = require('../helpers/file-helpers')
 
 const adminController = {
@@ -83,6 +83,31 @@ const adminController = {
         return restaurant.destroy()
       })
       .then(() => res.redirect('/admin/restaurants'))
+      .catch(err => next(err))
+  },
+  getUsers: (req, res, next) => {
+    return User.findAll({
+      raw: true
+    })
+      .then(users => {
+        res.render('admin/users', { users })
+      })
+      .catch(err => next(err))
+  },
+  patchUser: (req, res, next) => {
+    return User.findByPk(req.params.id)
+      .then(user => {
+        if (!user) throw new Error("User didn't exist!")
+        if (user.email === 'root@example.com') {
+          req.flash('error_messages', '禁止變更 root 權限')
+          return res.redirect('back')
+        }
+        user.update({ isAdmin: !user.isAdmin }, { where: { id: user.id } })
+          .then(() => {
+            req.flash('success_messages', '使用者權限變更成功')
+            return res.redirect('/admin/users')
+          })
+      })
       .catch(err => next(err))
   }
 }
