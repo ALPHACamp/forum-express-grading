5f2c577a657298b133071940014e7ddceccb94c6
softtoday72 <demonhunter720525@hotmail.com>
2022-04-23 21:14:46 +0800

    add restaurant image

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,16 +1,16 @@
 const express = require('express')
 const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
-// const { authenticatedAdmin } = require('../../middleware/auth')
+const upload = require('../../middleware/multer')
 
 // 實際路由: '/admin/restaurant', render到 admin/restaurants.hbs
 router.get('/restaurants/create', adminController.createRestaurant)
 router.get('/restaurants/:id/edit', adminController.editRestaurant)
 router.get('/restaurants/:id', adminController.getRestaurant)
-router.put('/restaurants/:id', adminController.putRestaurant)
+router.put('/restaurants/:id', upload.single('image'), adminController.putRestaurant)
 router.delete('/restaurants/:id', adminController.deleteRestaurant)
 router.get('/restaurants', adminController.getRestaurants)
-router.post('/restaurants', adminController.postRestaurant)
+router.post('/restaurants', upload.single('image'), adminController.postRestaurant)
 router.get('/', (req, res) => res.redirect('/admin/restaurants'))
 
 module.exports = router
