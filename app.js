9731defa57f1bf34b327a4f5f9f278a06d5481f1
tkooooo123 <tkooooo123@gmail.com>
2022-04-23 22:12:48 +0800

    feat: admin update restaurant

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const routes = require('./routes')
 const handlebars = require('express-handlebars')
 const flash = require('connect-flash')
+const methodOverride = require('method-override')
 const session = require('express-session')
 const passport = require('./config/passport')
 const { getUser } = require('./helpers/auth-helpers')
@@ -18,6 +19,7 @@ app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: fals
 app.use(passport.initialize()) // 增加這行，初始化 Passport
 app.use(passport.session())
 app.use(flash()) // 掛載套件
+app.use(methodOverride('_method'))
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages') // 設定 success_msg 訊息
   res.locals.error_messages = req.flash('error_messages') // 設定 warning_msg 訊息
