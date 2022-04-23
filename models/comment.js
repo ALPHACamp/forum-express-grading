63be8635657e0326075d4e5d9518ae5bc010407d
eruc <eruc101010@gmail.com>
2022-04-24 01:45:27 +0800

    fix: add tableName

--- models/comment.js
+++ models/comment.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate (models) {
+    static associate(models) {
       Comment.belongsTo(models.Restaurant, { foreignKey: 'restaurantId' })
       Comment.belongsTo(models.User, { foreignKey: 'userId' })
     }
@@ -21,6 +21,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'Comment',
+    tableName: 'Comments',
     underscored: true
   })
   return Comment
