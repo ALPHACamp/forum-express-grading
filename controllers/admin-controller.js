0518528433e4bf5c9241c5e3c16c0889fc1885d9
hitpop2216 <hitpop2216@gmail.com>
2022-04-23 14:05:18 +0800

    feat: init project

---
+++ controllers/admin-controller.js
@@ -0,0 +1,88 @@
+const { Restaurant } = require('../models')
+const { imgurFileHandler } = require('../helpers/file-helpers')
+
+const adminController = {
+  getRestaurants: (req, res, next) => {
+    Restaurant.findAll({ raw: true })
+      .then(restaurants => res.render('admin/restaurants', { restaurants }))
+      .catch(err => next(err))
+  },
+  createRestaurant: (req, res) => {
+    res.render('admin/create-restaurant')
+  },
+  postRestaurant: (req, res, next) => {
+    const { name, tel, address, openingHours, description } = req.body
+    if (!name) throw new Error('Restaurant name is required!')
+
+    imgurFileHandler(req.file)
+      .then(filePath => {
+        return Restaurant.create({
+          name,
+          tel,
+          address,
+          openingHours,
+          description,
+          image: filePath || null
+        })
+      })
+      .then(() => {
+        req.flash('success_messages', 'restaurant was successfully created')
+        res.redirect('/admin/create-restaurant')
+      })
+      .catch(err => next(err))
+  },
+  getRestaurant: (req, res, next) => {
+    Restaurant.findByPk(req.params.id, { // 去資料庫用 id 找一筆資料
+      raw: true // 找到以後整理格式再回傳
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!") //  如果找不到，回傳錯誤訊息，後面不執行
+        res.render('admin/restaurant', { restaurant })
+      })
+      .catch(err => next(err))
+  },
+  editRestaurant: (req, res, next) => { // 新增這段
+    Restaurant.findByPk(req.params.id, {
+      raw: true
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        res.render('admin/edit-restaurant', { restaurant })
+      })
+      .catch(err => next(err))
+  },
+  putRestaurant: (req, res, next) => {
+    const { name, tel, address, openingHours, description } = req.body
+    if (!name) throw new Error('Restaurant name is required!')
+    Promise.all([
+      Restaurant.findByPk(req.params.id),
+      imgurFileHandler(req.file)
+    ])
+      .then(([restaurant, filePath]) => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        return restaurant.update({
+          name,
+          tel,
+          address,
+          openingHours,
+          description,
+          image: filePath || restaurant.image
+        })
+      })
+      .then(() => {
+        req.flash('success_messages', 'restaurant was successfully to update')
+        res.redirect('/admin/restaurants')
+      })
+      .catch(err => next(err))
+  },
+  deleteRestaurant: (req, res, next) => { // 新增以下
+    return Restaurant.findByPk(req.params.id)
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        return restaurant.destroy()
+      })
+      .then(() => res.redirect('/admin/restaurants'))
+      .catch(err => next(err))
+  }
+}
+module.exports = adminController
---
+++ routes/modules/admin.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router()
+const adminController = require('../../controllers/admin-controller')
+const upload = require('../../middleware/multer')
+
+router.get('/restaurants/create', adminController.createRestaurant)
+router.get('/restaurants/:id/edit', adminController.editRestaurant)
+router.put('/restaurants/:id', upload.single('image'), adminController.putRestaurant)
+router.get('/restaurants/:id', adminController.getRestaurant)
+router.delete('/restaurants/:id', adminController.deleteRestaurant)
+router.post('/restaurants', upload.single('image'), adminController.postRestaurant)
+router.get('/restaurants', adminController.getRestaurants)
+router.get('', (req, res) => res.redirect('/admin/restaurants'))
+module.exports = router
