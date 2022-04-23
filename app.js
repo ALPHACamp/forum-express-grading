6b92e8dda513e212ae2a26a43246cfc1a2928a78
tkooooo123 <tkooooo123@gmail.com>
2022-04-23 23:20:13 +0800

    feat: add restaurant image

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const routes = require('./routes')
 const handlebars = require('express-handlebars')
@@ -20,6 +21,7 @@ app.use(passport.initialize()) // 增加這行，初始化 Passport
 app.use(passport.session())
 app.use(flash()) // 掛載套件
 app.use(methodOverride('_method'))
+app.use('/upload', express.static(path.join(__dirname, 'upload')))
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages') // 設定 success_msg 訊息
   res.locals.error_messages = req.flash('error_messages') // 設定 warning_msg 訊息
