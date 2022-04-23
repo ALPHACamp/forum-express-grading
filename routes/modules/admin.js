9341db633896c10c796dd4693d4284627c4b58ec
Marcus Liu <me@mail.lmarcus.onl>
2022-04-24 02:13:25 +0800

    fix: 修改 controller 命名

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
-const adminController = require('../../controllers/admin-controllers')
+const adminController = require('../../controllers/admin-controller')
 const upload = require('../../middleware/multer')
 
 router.get('/restaurants/create', adminController.createRestaurant)
