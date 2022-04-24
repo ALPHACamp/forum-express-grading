b656c2011fb6a933d9fcb263b348320ccebf6609
Charles <charles0527@hotmail.com>
2022-04-24 13:56:42 +0800

    feat: add admin categories page

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
+const categoryController = require('../../controllers/category-controller')
 const upload = require('../../middleware/multer')
 
 // 顯示使用者清單
 router.get('/users', adminController.getUsers)
 // 修改使用者權限
 router.patch('/users/:id', adminController.patchUser)
+
+// 瀏覽所有分類
+router.get('/categories', categoryController.getCategories)
+// 瀏覽編輯分類的表單
 
 router.get('/restaurants/create', adminController.createRestaurant)
 router.get('/restaurants/:id/edit', adminController.editRestaurant)
