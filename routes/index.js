9148a6d5cf3d00b2dfb9ed9a63e1a1cdd9011f60
tkooooo123 <tkooooo123@gmail.com>
2022-04-24 23:21:10 +0800

    feat:merge R01 into main

--- routes/index.js
+++ routes/index.js
@@ -11,8 +11,10 @@ router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
 router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
+router.get('/logout', userController.logout)
 router.use('/admin', authenticatedAdmin, admin)
 router.get('/restaurants', authenticated, restController.getRestaurants)
+
 router.use('/', (req, res) => res.redirect('/restaurants'))
 router.use('/', generalErrorHandler)
 
