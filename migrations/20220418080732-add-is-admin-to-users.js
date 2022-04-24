f47931056c684e7a86677517f3b78ccf462e6f18
maximeri <t.s.eliot1996@gmail.com>
2022-04-24 13:45:05 +0800

    fix: Users table name

--- migrations/20220418080732-add-is-admin-to-users.js
+++ migrations/20220418080732-add-is-admin-to-users.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   async up (queryInterface, Sequelize) {
-    await queryInterface.addColumn('users', 'is_admin', {
+    await queryInterface.addColumn('Users', 'is_admin', {
       type: Sequelize.BOOLEAN,
       defaultValue: false
     })
