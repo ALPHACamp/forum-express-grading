0a99f545faad6caa7ca9051b0f6fdf3db7490581
susanh <susanh@susanhdeMacBook-Pro.local>
2022-04-24 10:46:53 +0800

    feat: add admin restaurant page

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -6,6 +6,7 @@ const adminController = require('../../controllers/admin-controllers')
 // const { authenticatedAdmin } = require('../../middleware/auth')
 
 router.get('/restaurants/create', adminController.createRestaurant)
+router.get('/restaurants/:id', adminController.getRestaurant)
 router.get('/restaurants', adminController.getRestaurants)
 router.post('/restaurants', adminController.postRestaurants)
 router.use('', (req, res) => res.redirect('/admin/restaurants'))
