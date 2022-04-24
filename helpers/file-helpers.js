ddf0e25012b9a47556ae2b3424a0621b3b8fcfa0
LouisChen-TW <yue841206@gmail.com>
2022-04-24 22:20:57 +0800

    refactor: refactor with async/awiat in file-helpers.js

--- helpers/file-helpers.js
+++ helpers/file-helpers.js
@@ -3,29 +3,52 @@ const imgur = require('imgur')
 const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
 imgur.setClientId(IMGUR_CLIENT_ID)
 
-const localFileHandler = (file) => {
-  // file 是 multer 處理完的檔案
-  return new Promise((resolve, reject) => {
-    if (!file) return resolve(null)
+const localFileHandler = async file => {
+  try {
+    if (!file) return null
     const fileName = `upload/${file.originalname}`
-    return fs.promises
-      .readFile(file.path)
-      .then((data) => fs.promises.writeFile(fileName, data))
-      .then(() => resolve(`/${fileName}`))
-      .catch((err) => reject(err))
-  })
+    const data = await fs.promises.readFile(file.path)
+    await fs.promises.writeFile(fileName, data)
+    return `/${fileName}`
+  } catch (err) {
+    throw new Error(err)
+  }
 }
-const imgurFileHandler = (file) => {
-  return new Promise((resolve, reject) => {
-    if (!file) return resolve(null)
-    return imgur
-      .uploadFile(file.path)
-      .then((img) => {
-        resolve(img?.link || null) // 檢查 img 是否存在
-      })
-      .catch((err) => reject(err))
-  })
+
+// const localFileHandler = file => {
+//   // file 是 multer 處理完的檔案
+//   return new Promise((resolve, reject) => {
+//     if (!file) return resolve(null)
+//     const fileName = `upload/${file.originalname}`
+//     return fs.promises
+//       .readFile(file.path)
+//       .then(data => fs.promises.writeFile(fileName, data))
+//       .then(() => resolve(`/${fileName}`))
+//       .catch(err => reject(err))
+//   })
+// }
+// const imgurFileHandler = file => {
+//   return new Promise((resolve, reject) => {
+//     if (!file) return resolve(null)
+//     return imgur
+//       .uploadFile(file.path)
+//       .then(img => {
+//         resolve(img?.link || null) // 檢查 img 是否存在
+//       })
+//       .catch(err => reject(err))
+//   })
+// }
+
+const imgurFileHandler = async file => {
+  try {
+    if (!file) return null
+    const img = await imgur.uploadFile(file.path)
+    return img?.link || null
+  } catch (err) {
+    throw new Error(err)
+  }
 }
+
 module.exports = {
   localFileHandler,
   imgurFileHandler
