ebae88a14825cbfaabc68725d2f88fa8a2bd1f10
softtoday72 <demonhunter720525@hotmail.com>
2022-04-24 22:37:18 +0800

    push heroku

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,14 +1,14 @@
-const { ensureAuthenticated, getUser } = require('../helpers/auth-helpers')
+const helper = require('../helpers/auth-helpers')
 const authenticated = (req, res, next) => {
-  if (ensureAuthenticated(req)) {
+  if (helper.ensureAuthenticated(req)) {
     return next()
   }
   res.redirect('/signin')
 }
 
 const authenticatedAdmin = (req, res, next) => {
-  if (ensureAuthenticated(req)) {
-    if (getUser(req).isAdmin) return next()
+  if (helper.ensureAuthenticated(req)) {
+    if (helper.getUser(req).isAdmin) return next()
     res.redirect('/')
   } else {
     res.redirect('/signin')
