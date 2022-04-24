0074102e8216109e40f3256b2a18ae064e8b86c1
tkooooo123 <tkooooo123@gmail.com>
2022-04-24 20:52:40 +0800

    feat: add alter isAdmin value feature

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -91,6 +91,23 @@ const adminController = {
     })
       .then(users => res.render('admin/users', { users }))
       .catch(err => next(err))
+  },
+  patchUser: (req, res, next) => {
+    return User.findByPk(req.params.id)
+      .then(user => {
+        console.log(user.isAdmin)
+        if (!user) throw new Error("User didn't exist!")
+        if (user.email === 'root@example.com') {
+          req.flash('error_messages', '禁止變更 root 權限')
+          return res.redirect('back')
+        }
+        return user.update({ isAdmin: !user.isAdmin })
+          .then(() => {
+            req.flash('success_messages', '使用者權限變更成功')
+            res.redirect('/admin/users')
+          })
+      })
+      .catch(err => next(err))
   }
 }
 module.exports = adminController
