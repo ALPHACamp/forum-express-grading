12abc3215d5e09180d15b94469a0cd836d53e795
cdae6021 <cdae6021@yahoo.com.tw>
2022-04-25 12:41:18 +0800

    feat: user model add is_admin

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,21 @@
 const express = require('express')
 const router = express.Router()
 
+const passport = require('../config/passport')
 const restController = require('../controllers/restaurant-controller')
+const userController = require('../controllers/user-controller')
+const { authenticated } = require('../middleware/auth')
+const { generalErrorHandler } = require('../middleware/error-handler')
+const admin = require('./modules/admin')
 
-router.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-router.get('/restaurants', restController.getRestaurants)
+router.use('/admin', admin)
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
 
-router.use('/', (req, res) => res.redirect('/restaurants'))
+router.get('/signin', userController.signInPage)
+router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
+router.get('/restaurants', authenticated, restController.getRestaurants)
+router.get('/', (req, res) => res.redirect('/restaurants'))
+router.use('/', generalErrorHandler)
 
 module.exports = router
---
+++ routes/modules/admin.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const router = express.Router()
+const adminController = require('../../controllers/admin-controller')
+const { authenticatedAdmin } = require('../../middleware/auth')
+router.get('/restaurants', authenticatedAdmin, adminController.getRestaurants)
+router.get('/', (req, res) => res.redirect('/admin/restaurants'))
+module.exports = router
