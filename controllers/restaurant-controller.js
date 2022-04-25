6dedcd0986f8f9bf60f8dc3f8a2814c3af230dab
Rubylago <rubybibi6@gmail.com>
2022-04-25 21:15:49 +0800

    feat: add pagination on restaurants index page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,30 +1,39 @@
 const { Restaurant, Category } = require('../models')
+const { getOffset, getPagination } = require('../helpers/pagination-helper')
+
 const restaurantController = {
   getRestaurants: (req, res) => {
+    const DEFAULT_LIMIT = 9 // 代表想要一頁有 9 筆餐廳資料
     const categoryId = Number(req.query.categoryId) || '' // 從網址上拿下來的參數是字串
+    const page = Number(req.query.page) || 1 // 如果 query string 沒有攜帶特定數字的話page預設為 1
+    const limit = Number(req.query.limit) || DEFAULT_LIMIT // 預留加上讓使用者選擇「每頁顯示 N 筆」的功能
+    const offset = getOffset(limit, page)
     const where = {}
     if (categoryId) where.categoryId = categoryId
     return Promise.all([
-      Restaurant.findAll({
+      Restaurant.findAndCountAll({
         include: Category,
         where: where,
         // where: { // 新增查詢條件
         //   ...categoryId ? { categoryId } : {} // 檢查 categoryId 是否為空值
         // },
+        limit,
+        offset,
         nest: true,
         raw: true
       }),
       Category.findAll({ raw: true })
     ])
       .then(([restaurants, categories]) => {
-        const data = restaurants.map(r => ({
+        const data = restaurants.rows.map(r => ({
           ...r,
           description: r.description.substring(0, 50)
         }))
         return res.render('restaurants', {
           restaurants: data,
           categories,
-          categoryId
+          categoryId,
+          pagination: getPagination(limit, page, restaurants.count)
         })
       })
   },
