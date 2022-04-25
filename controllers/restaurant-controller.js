d1ccae4804a2fbac2d00a3bb6c7ed0bb038acc0e
LouisChen-TW <yue841206@gmail.com>
2022-04-25 17:25:25 +0800

    feat: add dashboard and viewCounts

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -20,12 +20,24 @@ const restaurantController = {
   getRestaurant: async (req, res, next) => {
     try {
       const restaurant = await Restaurant.findByPk(req.params.id, {
+        include: Category
+      })
+      if (!restaurant) throw new Error("Restaurant didn't exist!")
+      await restaurant.increment('viewCounts')
+      res.render('restaurant', { restaurant: restaurant.toJSON() })
+    } catch (err) {
+      next(err)
+    }
+  },
+  getDashboard: async (req, res, next) => {
+    try {
+      const restaurant = await Restaurant.findByPk(req.params.id, {
         include: Category,
         raw: true,
         nest: true
       })
-      if (!restaurant) throw new Error("Restaurant didn't exist!")
-      res.render('restaurant', { restaurant })
+      if (!restaurant) throw new Error("Restaurant didn't exist")
+      res.render('dashboard', { restaurant })
     } catch (err) {
       next(err)
     }
