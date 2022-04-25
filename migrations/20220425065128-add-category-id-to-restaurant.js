f197a41cd69932b24b181262d315e68c546be7d7
Leo <a131234568@gmail.com>
2022-04-25 15:03:43 +0800

    feat: add category model

---
+++ migrations/20220425065128-add-category-id-to-restaurant.js
@@ -0,0 +1,16 @@
+'use strict'
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('Restaurants', 'category_id', {
+      type: Sequelize.INTEGER,
+      allowNull: true,
+      references: {
+        model: 'Categories',
+        key: 'id'
+      }
+    })
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('Restaurants', 'category_id')
+  }
+}
