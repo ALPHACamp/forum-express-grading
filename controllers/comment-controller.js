2f134cb915ab562f8c67a09426d79779e919daa4
zxc58 <abc01234567889@gmail.com>
2022-04-25 13:41:20 +0800

    admin delete comment

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -1,4 +1,5 @@
 const { Comment, User, Restaurant } = require('../models')
+//
 const commentController = {
   postComment: (req, res, next) => {
     const { restaurantId, text } = req.body
@@ -20,6 +21,15 @@ const commentController = {
       .then(() => {
         res.redirect(`/restaurants/${restaurantId}`)
       })
+      .catch(err => next(err))
+  },
+  deleteComment: (req, res, next) => {
+    return Comment.findByPk(req.params.id)
+      .then(comment => {
+        if (!comment) throw new Error("Comment didn't exist!'")
+        return comment.destroy()
+      })
+      .then(deletedComment => res.redirect(`/restaurants/${deletedComment.restaurantId}`))
       .catch(err => next(err))
   }
 }
