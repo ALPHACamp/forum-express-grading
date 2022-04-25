4b3c9cc708962940689d827ce0345f3ec9864947
Tommy1023 <j.h.chen1023@gmail.com>
2022-04-26 01:55:44 +0800

    feat: category update

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -2,11 +2,16 @@ const { Category } = require('../models')
 
 const categoryController = {
   getCategories: (req, res, next) => {
-    Category.findAll({
-      raw: true,
-      nest: true
-    })
-      .then(categories => res.render('admin/categories', { categories }))
+    return Promise.all([
+      Category.findAll({ raw: true }),
+      req.params.id ? Category.findByPk(req.params.id, { raw: true }) : null
+    ])
+      .then(([categories, category]) => {
+        res.render('admin/categories', {
+          categories,
+          category
+        })
+      })
       .catch(err => next(err))
   },
   postCategory: (req, res, next) => {
@@ -17,6 +22,25 @@ const categoryController = {
         req.flash('success_messages', 'category was successfully created')
         res.redirect('/admin/categories')
       })
+      .catch(err => next(err))
+  },
+  editCategory: (req, res, next) => {
+    return Category.findByPk(req.params.id, { raw: true })
+      .then(category => {
+        if (!category) throw new Error("Category didn't exist!")
+        res.render('admin/categories', { category })
+      })
+      .catch(err => next(err))
+  },
+  putCategory: (req, res, next) => {
+    const { name } = req.body
+    if (!name) throw new Error('Category name is required!')
+    return Category.findByPk(req.params.id)
+      .then(category => {
+        if (!category) throw new Error("Category doesn't exist!")
+        return category.update({ name })
+      })
+      .then(() => res.redirect('/admin/categories'))
       .catch(err => next(err))
   }
 }
