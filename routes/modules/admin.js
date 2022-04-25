eeaddea8f25e29168ea82bde08d27d9526941653
Leo <a131234568@gmail.com>
2022-04-25 18:16:01 +0800

    feat: category create

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -4,8 +4,6 @@ const adminController = require('../../controllers/admin-controller')
 const categoryController = require('../../controllers/category-controller')
 const upload = require('../../middleware/multer')
 
-router.get('/users', adminController.getUsers)
-router.patch('/users/:id', adminController.patchUser)
 router.get('/restaurants/create', adminController.createRestaurant)
 router.get('/restaurants/:id/edit', adminController.editRestaurant)
 router.get('/restaurants/:id', adminController.getRestaurant)
@@ -13,7 +11,13 @@ router.put('/restaurants/:id', upload.single('image'), adminController.putRestau
 router.delete('/restaurants/:id', adminController.deleteRestaurant)
 router.get('/restaurants', adminController.getRestaurants)
 router.post('/restaurants', upload.single('image'), adminController.postRestaurant)
+
+router.patch('/users/:id', adminController.patchUser)
+router.get('/users', adminController.getUsers)
+
 router.get('/categories', categoryController.getCategories)
+router.post('/categories', categoryController.postCategory)
+
 router.get('/', (req, res) => res.redirect('/admin/restaurants'))
 
 module.exports = router
