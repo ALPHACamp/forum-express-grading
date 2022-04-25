c5d8066ce98b1de48d6ec556c6f48259d3125c9d
AngelaC123 <angelarare@gmail.com>
2022-04-25 15:31:00 +0800

    feat: add restaurant page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -14,6 +14,18 @@ const restaurantController = {
         }))
         return res.render('restaurants', { restaurants: data })
       })
+  },
+  getRestaurant: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, {
+      raw: true,
+      nest: true,
+      include: [Category]
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("restaurant didn't exist!")
+        res.render('restaurant', { restaurant })
+      })
+      .catch(err => next(err))
   }
 }
 
