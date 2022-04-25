d98c1304b82ce9eb3a8375b7e2490422b0757bc1
cdae6021 <cdae6021@yahoo.com.tw>
2022-04-25 11:44:43 +0800

    feat: add handlebars

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const router = express.Router()
 
+const restController = require('../controllers/restaurant-controller')
+
 router.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
 })
+router.get('/restaurants', restController.getRestaurants)
+
+router.use('/', (req, res) => res.redirect('/restaurants'))
 
 module.exports = router
