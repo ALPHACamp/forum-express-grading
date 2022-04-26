f33eac0584cd658291a66a3776fc82a961292d50
hitpop2216 <hitpop2216@gmail.com>
2022-04-27 01:07:22 +0800

    A19 Q5

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcryptjs')
 const db = require('../models')
-const { User } = db
+const { imgurFileHandler } = require('../helpers/file-helpers')
+const { User, Comment, Restaurant } = db
 const userController = {
   signUpPage: (req, res) => {
     res.render('signup')
@@ -35,6 +36,45 @@ const userController = {
     req.flash('success_messages', '登出成功！')
     req.logout()
     res.redirect('/signin')
+  },
+  getUser: (req, res, next) => {
+    return User.findByPk(req.params.id, {
+      include: [
+        Comment,
+        { model: Comment, include: Restaurant }
+      ]
+    })
+      .then(user => {
+        res.render('users/profile', {
+          user: user.toJSON(),
+          commentCounts: user.toJSON().Comments.length
+        })
+      })
+      .catch(err => next(err))
+  },
+  editUser: (req, res, next) => {
+    return User.findByPk(req.params.id, { raw: true })
+      .then(user => res.render('users/edit', { user }))
+      .catch(err => next(err))
+  },
+  putUser: (req, res, next) => {
+    const { name } = req.body
+    if (!req.body.name) throw new Error('Name is required!')
+    return Promise.all([
+      User.findByPk(req.params.id),
+      imgurFileHandler(req.file)
+    ])
+      .then(([user, filePath]) => {
+        return user.update({
+          name,
+          image: filePath || user.image
+        })
+      })
+      .then(() => {
+        req.flash('success_messages', '使用者資料編輯成功')
+        res.redirect(`/users/${req.params.id}`)
+      })
+      .catch(err => next(err))
   }
 }
 module.exports = userController
--- seeders/20220422015657-users-seed-file.js
+++ seeders/20220422015657-users-seed-file.js
@@ -8,14 +8,16 @@ module.exports = {
       is_admin: true,
       name: 'root',
       created_at: new Date(),
-      updated_at: new Date()
+      updated_at: new Date(),
+      image: `https://loremflickr.com/320/240/cat/?random=${Math.random() * 100}`
     }, {
       email: 'user1@example.com',
       password: await bcrypt.hash('12345678', 10),
       is_admin: false,
       name: 'user1',
       created_at: new Date(),
-      updated_at: new Date()
+      updated_at: new Date(),
+      image: `https://loremflickr.com/320/240/cat/?random=${Math.random() * 100}`
     }], {})
   },
   down: async (queryInterface, Sequelize) => { // 清空資料表中所有資料
