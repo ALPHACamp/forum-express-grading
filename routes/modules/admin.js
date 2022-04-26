a5281b79c708e2a4cf1fcdc29ec0e3b58f351a63
naivelove0822 <naivelove0822@gmail.com>
2022-04-26 13:34:23 +0800

    feat: category delete

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -11,10 +11,13 @@ router.put('/restaurants/:id', upload.single('image'), adminController.putRestau
 router.delete('/restaurants/:id', adminController.deleteRestaurant)
 router.get('/restaurants', adminController.getRestaurants)
 router.post('/restaurants', upload.single('image'), adminController.postRestaurant)
+
 router.patch('/users/:id', adminController.patchUser)
 router.get('/users', adminController.getUsers)
+
 router.get('/categories/:id', categoryController.getCategories)
 router.put('/categories/:id', categoryController.putCategory)
+router.delete('/categories/:id', categoryController.deleteCategory)
 router.get('/categories', categoryController.getCategories)
 router.post('/categories', categoryController.postCategory)
 router.get('/', (req, res) => res.redirect('/admin/restaurants'))
