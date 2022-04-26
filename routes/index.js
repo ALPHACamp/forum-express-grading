d8f9b4b36c5daabeeca02dbd6afc4408e38e226d
happygod119 <happ0983493655@gmail.com>
2022-04-26 23:19:30 +0800

    feat: Merge R03 to main

--- routes/index.js
+++ routes/index.js
@@ -2,26 +2,22 @@ const express = require('express')
 const router = express.Router()
 
 const passport = require('../config/passport')
-
 const admin = require('./modules/admin')
+const upload = require('../middleware/multer')
 
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
 const commentController = require('../controllers/​​comment-controller')
 
 const { authenticated, authenticatedAdmin } = require('../middleware/auth')
-
 const { generalErrorHandler } = require('../middleware/error-handler')
-const upload = require('../middleware/multer')
 
 router.use('/admin', authenticatedAdmin, admin)
 
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
-
 router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
-
 router.get('/logout', userController.logout)
 
 router.get('/users/:id/edit', authenticated, userController.editUser)
@@ -41,6 +37,7 @@ router.delete('/favorite/:restaurantId', authenticated, userController.removeFav
 
 router.use('/', (req, res) => res.redirect('/restaurants'))
 
+router.use('/', (req, res) => res.redirect('/restaurants'))
 router.use('/', generalErrorHandler)
 
 module.exports = router
