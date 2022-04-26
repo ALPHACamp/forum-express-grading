7c16c35a66b915a125cbc32700bc74a9a1ab6902
happygod119 <happ0983493655@gmail.com>
2022-04-26 17:26:33 +0800

    feat: create favorite model

--- models/restaurant.js
+++ models/restaurant.js
@@ -12,6 +12,11 @@ module.exports = (sequelize, DataTypes) => {
     static associate (models) {
       Restaurant.belongsTo(models.Category, { foreignKey: 'categoryId' })
       Restaurant.hasMany(models.Comment, { foreignKey: 'restaurantId' })
+      Restaurant.belongsToMany(models.User, {
+        through: models.Favorite,
+        foreignKey: 'restaurantId',
+        as: 'FavoritedUsers'
+      })
     }
   }
   Restaurant.init({
@@ -25,6 +30,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'Restaurant',
+    tableName: 'Restaurants',
     underscored: true
   })
   return Restaurant
