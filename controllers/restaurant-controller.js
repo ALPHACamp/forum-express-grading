409f12a5128e52f906d7ee3dfd77dbe8b0a1d81f
hitpop2216 <hitpop2216@gmail.com>
2022-04-26 17:02:18 +0800

    feat: fix conflict

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,6 +1,45 @@
+const { Restaurant, Category } = require('../models')
 const restaurantController = {
   getRestaurants: (req, res) => {
-    return res.render('restaurants')
+    return Restaurant.findAll({
+      include: Category,
+      nest: true,
+      raw: true
+    })
+      .then(restaurants => {
+        const data = restaurants.map(r => ({
+          ...r,
+          description: r.description.substring(0, 50)
+        }))
+        return res.render('restaurants', {
+          restaurants: data
+        })
+      })
+  },
+  getRestaurant: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, { include: Category })
+      .then(restaurant => {
+        return restaurant.increment('view_counts')
+      })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        res.render('restaurant', {
+          restaurant: restaurant.toJSON()
+        })
+      })
+      .catch(err => next(err))
+  },
+  getDashboard: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, {
+      include: Category
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        res.render('dashboard', {
+          restaurant: restaurant.toJSON()
+        })
+      })
+      .catch(err => next(err))
   }
 }
 module.exports = restaurantController
--- routes/index.js
+++ routes/index.js
@@ -17,6 +17,8 @@ router.post('/signin', passport.authenticate('local', { failureRedirect: '/signi
 // logout
 router.get('/logout', userController.logout)
 
+router.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard)
+router.get('/restaurants/:id', authenticated, restController.getRestaurant)
 router.get('/restaurants', authenticated, restController.getRestaurants)
 router.use('/', (req, res) => res.redirect('/restaurants'))
 router.use('/', generalErrorHandler)
