710d00269ae3e483db96b3fbabbfa5dd16bd6c8b
cdae6021 <cdae6021@yahoo.com.tw>
2022-04-26 08:52:35 +0800

    feat: add category model

--- models/category.js
+++ models/category.js
@@ -3,28 +3,23 @@ const {
   Model
 } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
-  class Restaurant extends Model {
+  class Category extends Model {
     /**
          * Helper method for defining associations.
          * This method is not a part of Sequelize lifecycle.
          * The `models/index` file will call this method automatically.
          */
     static associate (models) {
-      Restaurant.belongsTo(models.Category, { foreignKey: 'CategoryId' })
+      Category.hasMany(models.Restaurant, { foreignKey: 'CategoryId' })
     }
   };
-  Restaurant.init({
-    name: DataTypes.STRING,
-    tel: DataTypes.STRING,
-    address: DataTypes.STRING,
-    openingHours: DataTypes.STRING,
-    description: DataTypes.TEXT,
-    image: DataTypes.STRING
+  Category.init({
+    name: DataTypes.STRING
   }, {
     sequelize,
-    modelName: 'Restaurant',
-    tableName: 'Restaurants',
+    modelName: 'Category',
+    tableName: 'Categories',
     underscored: true
   })
-  return Restaurant
+  return Category
 }
