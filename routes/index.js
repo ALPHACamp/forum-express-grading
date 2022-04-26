0644ba2fa9d140b8b922d9534fff52477ef7319d
cdae6021 <cdae6021@yahoo.com.tw>
2022-04-26 20:56:14 +0800

    feat: add restaurant page

--- routes/index.js
+++ routes/index.js
@@ -17,6 +17,8 @@ router.post('/signup', userController.signUp)
 router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
 
+router.get('/logout', userController.logout)
+router.get('/restaurants:id', authenticated, restController.getRestaurant)
 router.get('/restaurants', authenticated, restController.getRestaurants)
 router.get('/', (req, res) => res.redirect('/restaurants'))
 router.use('/', generalErrorHandler)
