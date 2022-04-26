752cd199351e98886867690562b4d560dd18aa9e
Rubylago <rubybibi6@gmail.com>
2022-04-26 23:56:46 +0800

    squash R03

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcryptjs')
-const db = require('../models')
-const { User } = db
+const { imgurFileHandler } = require('../helpers/file-helpers')
+const { Restaurant, Comment, User } = require('../models')
+
 const userController = {
   signUpPage: (req, res) => {
     res.render('signup')
@@ -34,6 +35,57 @@ const userController = {
     req.flash('success_messages', '登出成功！')
     req.logout()
     res.redirect('/signin')
+  },
+  getUser: async (req, res) => {
+    try {
+      const user = await User.findByPk(req.params.id, {
+        include: [
+          { model: Comment, include: Restaurant }
+        ]
+      })
+      if (!user) throw new Error("user didn't exist!")
+      const amount = await Comment.findAndCountAll({
+        where: {
+          user_id: user.dataValues.id
+        }
+      })
+      return res.render('users/profile', {
+        user: user.toJSON(),
+        amount: amount.count
+      })
+    } catch (err) {
+      console.log(err)
+    }
+  },
+  editUser: (req, res, next) => {
+    return User.findByPk(req.params.id, { raw: true })
+      .then(user => {
+        if (!user) throw new Error("user doesn't exist!")
+        console.log(user)
+        return res.render('users/edit', { user })
+      })
+      .catch(err => next(err))
+  },
+  putUser: (req, res, next) => {
+    const { name } = req.body
+    if (!name) throw new Error('User name is required!')
+    const { file } = req
+    return Promise.all([
+      User.findByPk(req.params.id),
+      imgurFileHandler(file)
+    ])
+      .then(([user, filePath]) => {
+        if (!user) throw new Error("User didn't exist!")
+        return user.update({
+          name,
+          image: filePath || user.image
+        })
+      })
+      .then(() => {
+        req.flash('success_messages', '使用者資料編輯成功')
+        res.redirect(`/users/${req.params.id}`)
+      })
+      .catch(err => next(err))
   }
 }
 module.exports = userController
