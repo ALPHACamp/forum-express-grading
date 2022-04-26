3a4ddc0ca9ee5bdf58c9a8b3664c0cd8e3c2d0af
hitpop2216 <hitpop2216@gmail.com>
2022-04-26 14:43:15 +0800

    feat: restaurant views

---
+++ config/passport.js
@@ -0,0 +1,36 @@
+const passport = require('passport')
+const LocalStrategy = require('passport-local')
+const bcrypt = require('bcryptjs')
+const db = require('../models')
+const User = db.User
+
+passport.use(new LocalStrategy(
+  {
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true
+  },
+  (req, email, password, cb) => {
+    User.findOne({ where: { email } })
+      .then(user => {
+        if (!user) return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤！'))
+        bcrypt.compare(password, user.password).then(res => {
+          if (!res) return cb(null, false, req.flash('error_messages', '帳號或密碼錯誤！'))
+          return cb(null, user)
+        })
+      })
+  }
+))
+// 將使用者序列化為 id 便於節省記憶體空間
+passport.serializeUser((user, cb) => {
+  cb(null, user.id)
+})
+// 用使用者的序列化 id 去資料庫找對應的 id 的資料
+passport.deserializeUser((id, cb) => {
+  User.findByPk(id).then(user => {
+    user = user.toJSON()
+    return cb(null, user)
+  })
+})
+
+module.exports = passport
---
+++ controllers/restaurant-controller.js
@@ -0,0 +1,34 @@
+const { Restaurant, Category } = require('../models')
+const restaurantController = {
+  getRestaurants: (req, res) => {
+    return Restaurant.findAll({
+      include: Category,
+      nest: true,
+      raw: true
+    })
+      .then(restaurants => {
+        const data = restaurants.map(r => ({
+          ...r,
+          description: r.description.substring(0, 50)
+        }))
+        return res.render('restaurants', {
+          restaurants: data
+        })
+      })
+  },
+  getRestaurant: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, {
+      include: Category, // 拿出關聯的 Category model
+      nest: true,
+      raw: true
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        res.render('restaurant', {
+          restaurant
+        })
+      })
+      .catch(err => next(err))
+  }
+}
+module.exports = restaurantController
