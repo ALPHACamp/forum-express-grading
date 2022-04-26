8f4fa6f0abce188618883d96b9bb27233555230c
naivelove0822 <naivelove0822@gmail.com>
2022-04-26 12:59:43 +0800

    feat: category create

---
+++ controllers/category-controller.js
@@ -0,0 +1,16 @@
+const { Category } = require('../models')
+const categoryController = {
+  getCategories: (req, res, next) => {
+    return Category.findAll({ raw: true })
+      .then(categories => res.render('admin/categories', { categories }))
+      .catch(err => next(err))
+  },
+  postCategory: (req, res, next) => {
+    const { name } = req.body
+    if (!name) throw new Error('Category name is required!')
+    return Category.create({ name: req.body.name })
+      .then(() => res.redirect('/admin/categories'))
+      .catch(err => next(err))
+  }
+}
+module.exports = categoryController
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,6 +1,5 @@
 const bcrypt = require('bcryptjs')
-const db = require('../models')
-const { User } = db
+const { User } = require('../models')
 const userController = {
   signUpPage: (req, res) => {
     res.render('signup')
