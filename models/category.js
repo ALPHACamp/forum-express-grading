8bb08e8b65345744b92358675247707ac367be5c
cdae6021 <cdae6021@yahoo.com.tw>
2022-04-26 12:29:14 +0800

    feat: add category model

--- models/category.js
+++ models/category.js
@@ -1,25 +1,25 @@
 'use strict'
 const {
-  Model
+    Model
 } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
-  class Category extends Model {
-    /**
+    class Category extends Model {
+        /**
          * Helper method for defining associations.
          * This method is not a part of Sequelize lifecycle.
          * The `models/index` file will call this method automatically.
          */
-    static associate (models) {
-      Category.hasMany(models.Restaurant, { foreignKey: 'CategoryId' })
-    }
-  };
-  Category.init({
-    name: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Category',
-    tableName: 'Categories',
-    underscored: true
-  })
-  return Category
+        static associate(models) {
+            Category.hasMany(models.Restaurant, { foreignKey: 'categoryId' })
+        }
+    };
+    Category.init({
+        name: DataTypes.STRING
+    }, {
+        sequelize,
+        modelName: 'Category',
+        tableName: 'Categories',
+        underscored: true
+    })
+    return Category
 }
