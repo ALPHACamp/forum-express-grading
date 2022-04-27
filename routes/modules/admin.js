81e7ff90f579da387073ac8906f4bcb0a90f4d71
HO Chia Cung <john778300@gmail.com>
2022-04-27 12:35:28 +0800

    category update

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -4,12 +4,6 @@ const adminController = require('../../controllers/admin-controller')
 const categoryController = require('../../controllers/category-controller')
 const upload = require('../../middleware/multer')
 
-router.get('/users', adminController.getUsers)
-router.patch('/users/:id', adminController.patchUser)
-
-router.get('/categories', categoryController.getCategories)
-router.post('/categories', categoryController.postCategory)
-
 router.get('/restaurants/create', adminController.createRestaurant)
 router.get('/restaurants/:id/edit', adminController.editRestaurant)
 router.get('/restaurants/:id', adminController.getRestaurant)
@@ -17,6 +11,14 @@ router.put('/restaurants/:id', upload.single('image'), adminController.putRestau
 router.delete('/restaurants/:id', adminController.deleteRestaurant)
 router.get('/restaurants', adminController.getRestaurants)
 router.post('/restaurants', upload.single('image'), adminController.postRestaurant)
+
+router.get('/users', adminController.getUsers)
+router.patch('/users/:id', adminController.patchUser)
+router.get('/categories/:id', categoryController.getCategories)
+router.put('/categories/:id', categoryController.putCategory)
+router.get('/categories', categoryController.getCategories)
+router.post('/categories', categoryController.postCategory)
+
 router.get('/', (req, res) => res.redirect('/admin/restaurants'))
 
 module.exports = router
