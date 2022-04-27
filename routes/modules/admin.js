2678df3485fed49400841c256583c9ce8c145061
softtoday72 <demonhunter720525@hotmail.com>
2022-04-28 01:43:56 +0800

    add category create

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -15,6 +15,7 @@ router.post('/restaurants', upload.single('image'), adminController.postRestaura
 router.get('/users', adminController.getUsers)
 router.patch('/users/:id', adminController.patchUser)
 router.get('/categories', categoryController.getCategories)
+router.post('/categories', categoryController.postCategory)
 router.get('/', (req, res) => res.redirect('/admin/restaurants'))
 
 module.exports = router
