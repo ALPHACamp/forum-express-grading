d6d3990f24e3c03916ae634fb32a6dec74a61f66
Tommy1023 <j.h.chen1023@gmail.com>
2022-04-28 01:45:15 +0800

    feat: add dashboard viewCounts

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -19,6 +19,15 @@ const restaurantController = {
       .catch(err => next(err))
   },
   getRestaurant: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, { include: Category })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        res.render('restaurant', { restaurant: restaurant.toJSON() })
+        return restaurant.increment('view_counts')
+      })
+      .catch(err => next(err))
+  },
+  getDashboard: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
       raw: true,
       nest: true,
@@ -26,7 +35,7 @@ const restaurantController = {
     })
       .then(restaurant => {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
-        res.render('restaurant', { restaurant })
+        res.render('dashboard', { restaurant })
       })
       .catch(err => next(err))
   }
---
+++ migrations/20220427153716-add-view-counts-to-restaurant.js
@@ -0,0 +1,11 @@
+'use strict'
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('Restaurants', 'view_counts', {
+      type: Sequelize.INTEGER
+    })
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('Restaurants', 'view_counts')
+  }
+}
