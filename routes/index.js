60286845f06b448bdde581e0ce945b249e442954
happygod119 <happ0983493655@gmail.com>
2022-04-27 17:26:51 +0800

    feat: addFollowing & removeFollowing function

--- routes/index.js
+++ routes/index.js
@@ -2,24 +2,29 @@ const express = require('express')
 const router = express.Router()
 
 const passport = require('../config/passport')
+
 const admin = require('./modules/admin')
-const upload = require('../middleware/multer')
 
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
 const commentController = require('../controllers/​​comment-controller')
 
 const { authenticated, authenticatedAdmin } = require('../middleware/auth')
+
 const { generalErrorHandler } = require('../middleware/error-handler')
+const upload = require('../middleware/multer')
 
 router.use('/admin', authenticatedAdmin, admin)
 
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
+
 router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
+
 router.get('/logout', userController.logout)
 
+router.get('/users/top', authenticated, userController.getTopUsers)
 router.get('/users/:id/edit', authenticated, userController.editUser)
 router.get('/users/:id', authenticated, userController.getUser)
 router.put('/users/:id', authenticated, upload.single('image'), userController.putUser)
@@ -40,7 +45,6 @@ router.delete('/like/:restaurantId', authenticated, userController.removeLike)
 
 router.use('/', (req, res) => res.redirect('/restaurants'))
 
-router.use('/', (req, res) => res.redirect('/restaurants'))
 router.use('/', generalErrorHandler)
 
 module.exports = router
