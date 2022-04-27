86cbaebc891d6a611d195cdc2c5377d212a43a0c
GallonShih <gallonshih0526@gmail.com>
2022-04-27 23:31:59 +0800

    feet: add column view-counts into restaurants & dashboard page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -18,13 +18,26 @@ const restaurantController = {
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
       include: Category, // 拿出關聯的 Category model
-      nest: true,
-      raw: true
+      nest: true
     })
       .then(restaurant => {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
+        restaurant.increment('viewCounts', { by: 1 })
         res.render('restaurant', {
-          restaurant
+          restaurant: restaurant.toJSON()
+        })
+      })
+      .catch(err => next(err))
+  },
+  getDashboard: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, {
+      include: Category, // 拿出關聯的 Category model
+      nest: true
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        res.render('dashboard', {
+          restaurant: restaurant.toJSON()
         })
       })
       .catch(err => next(err))
