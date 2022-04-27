67575dabfd44d7f29f6af71340ad05e7e5e3c7b0
HO Chia Cung <john778300@gmail.com>
2022-04-27 22:38:29 +0800

    feat:add restaurant page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,5 +1,5 @@
 const { Restaurant, Category } = require('../models')
-const restaurantController = {
+const restController = {
   getRestaurants: (req, res) => {
     return Restaurant.findAll({
       include: Category,
@@ -12,6 +12,19 @@ const restaurantController = {
       }))
       return res.render('restaurants', { restaurants: data })
     })
+  },
+  getRestaurant: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, {
+      raw: true,
+      nest: true,
+      include: Category
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        res.render('restaurant', { restaurant })
+      })
+      .catch(err => next(err))
   }
+
 }
-module.exports = restaurantController
+module.exports = restController
