b5235c446f20a7bbeb6cc401fc6a107b297ea977
KateChu <chu.shihchien@gmail.com>
2022-04-27 18:31:41 +0800

    debug

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -3,17 +3,18 @@ const { Restaurant, Category } = require('../models')
 const restaurantController = {
   getRestaurants: (req, res) => {
     return Restaurant.findAll({
-      raw: true,
-      nest: true,
-      include: [Category]
-    })
-      .then(restaurants => {
-        const data = restaurants.map(r => ({
-          ...r,
-          description: r.description.substring(0, 50)
-        }))
-        return res.render('restaurants', { restaurants: data })
+      include: Category,
+      nest: true
+      // raw: true
+    }).then(restaurants => {
+      const data = restaurants.map(r => ({
+        ...r,
+        description: r.description.substring(0, 50)
+      }))
+      return res.render('restaurants', {
+        restaurants: data
       })
+    })
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
