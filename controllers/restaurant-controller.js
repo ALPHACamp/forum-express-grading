dde6d511dc19a017ebb6f006a08729181e185793
Leo <a131234568@gmail.com>
2022-04-27 20:46:21 +0800

    feat: show comments on restaurant page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,4 +1,4 @@
-const { Restaurant, Category } = require('../models')
+const { Restaurant, Category, Comment, User } = require('../models')
 const { getOffset, getPagination } = require('../helpers/pagination-helper')
 const restaurantController = {
   getRestaurants: (req, res, next) => {
@@ -37,7 +37,10 @@ const restaurantController = {
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
-      include: Category,
+      include: [
+        Category,
+        { model: Comment, include: User }
+      ],
       nest: true
     })
       .then(restaurant => {
