3fc07db9b07027cafec1c10e7de1c833706f82af
cdae6021 <cdae6021@yahoo.com.tw>
2022-04-27 21:55:58 +0800

    feat: add comment model

--- models/restaurant.js
+++ models/restaurant.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
          */
     static associate (models) {
       Restaurant.belongsTo(models.Category, { foreignKey: 'CategoryId' })
+      Restaurant.hasMany(models.Comment, { foreignKey: 'restaurantId' })
     }
   };
   Restaurant.init({
