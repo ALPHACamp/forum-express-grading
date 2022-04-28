756e733c05f3640e61770ddbaa6fbc8051a55c6e
KencodePM <88673690+KencodePM@users.noreply.github.com>
2022-04-28 23:00:07 +0800

    feat: add admin index page

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,11 @@ const router = express.Router()
 // 新增，載入 controller
 
 const restController = require('../controllers/restaurant-controller')
-
-// 新增
-
+const admin = require('./modules/admin') // 新增這行，載入 admin.js
+router.use('/admin', admin) // 新增這行
 router.get('/restaurants', restController.getRestaurants)
+router.get('/', (req, res) => res.redirect('/restaurants'))
+// 新增
 
 router.use('/', (req, res) => res.redirect('/restaurants'))
 
