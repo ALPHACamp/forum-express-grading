9e949e634740bcc4426f87ab3765197468d6c0fb
naivelove0822 <naivelove0822@gmail.com>
2022-04-28 17:26:47 +0800

    feat: add user profile
    feat: add profile imgur
    
    "feat: pass R03 test"
    
    rebase
    
    rebase fail
    
    shock...
    
    remake profile

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,5 +1,7 @@
 const bcrypt = require('bcryptjs')
 const { User } = require('../models')
+const { getUser } = require('../helpers/auth-helpers')
+const { imgurFileHandler } = require('../helpers/file-helpers')
 const userController = {
   signUpPage: (req, res) => {
     res.render('signup')
@@ -33,6 +35,46 @@ const userController = {
     req.flash('success_messages', '登出成功！')
     req.logout()
     res.redirect('/signin')
+  },
+  getUser: (req, res, next) => {
+    return User.findByPk(req.params.id, { raw: true, nest: true })
+      .then(user => res.render('users/profile', { user }))
+      .catch(err => next(err))
+  },
+  editUser: (req, res, next) => {
+    const userId = getUser(req).id
+    const paramsId = Number(req.params.id)
+    if (paramsId !== userId) {
+      req.flash('error_messages', '不能編輯他人的資料')
+      return res.redirect(`/users/${paramsId}`)
+    }
+    return User.findByPk(paramsId, { raw: true, nest: true })
+      .then(user => {
+        if (!user) throw new Error("User didn't exist!")
+        res.render('users/edit', { user })
+      })
+      .catch(err => next(err))
+  },
+  putUser: (req, res, next) => {
+    const { name } = req.body
+    if (!name) throw new Error('User name is required!')
+    const { file } = req
+    return Promise.all([
+      User.findByPk(req.params.id),
+      imgurFileHandler(file)
+    ])
+      .then(([user, filePath]) => {
+        if (!user) throw new Error("User didn't exist")
+        return user.update({
+          name,
+          image: filePath || user.image
+        })
+      })
+      .then(() => {
+        req.flash('success_messages', '使用者資料編輯成功')
+        res.redirect(`/users/${req.params.id}`)
+      })
+      .catch(err => next(err))
   }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const restController = require('../controllers/restaurant-controller')
 const admin = require('./modules/admin')
 const userController = require('../controllers/user-controller')
 const commentController = require('../controllers/comment-controller')
+const upload = require('../middleware/multer')
 const { authenticated, authenticatedAdmin } = require('../middleware/auth')
 const { generalErrorHandler } = require('../middleware/error-handler')
 
@@ -24,6 +25,10 @@ router.get('/', (req, res) => res.redirect('/restaurants'))
 
 router.delete('/comments/:id', authenticatedAdmin, commentController.deleteComment)
 router.post('/comments', authenticated, commentController.postComment)
+
+router.get('/users/:id/edit', authenticated, userController.editUser)
+router.put('/users/:id', upload.single('image'), authenticated, userController.putUser)
+router.get('/users/:id', authenticated, userController.getUser)
 
 router.use('/', generalErrorHandler)
 
