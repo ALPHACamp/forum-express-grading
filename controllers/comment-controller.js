d3fd011f00b4414edc641831a16460ee79de0cff
HitomiHuang <black011602@gmail.com>
2022-04-28 17:16:59 +0800

    feat: add delete comment for admin

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -1,9 +1,9 @@
-const {Comment, User, Restaurant} = require('../models')
+const { Comment, User, Restaurant } = require('../models')
 const commentController = {
   postComment: (req, res, next) => {
-    const {restaurantId, text} = req.body
+    const { restaurantId, text } = req.body
     const userId = req.user.id
-    if(!text) throw new Error('Comment text is required!')
+    if (!text) throw new Error('Comment text is required!')
     return Promise.all([
       User.findByPk(userId),
       Restaurant.findByPk(restaurantId)
@@ -20,6 +20,15 @@ const commentController = {
       .then(() => {
         res.redirect(`/restaurants/${restaurantId}`)
       })
+      .catch(err => next(err))
+  },
+  deleteComment: (req, res, next) => {
+    Comment.findByPk(req.params.id)
+      .then(comment => {
+        if (!comment) throw new Error("Comment didn't exist!")
+        return comment.destroy()
+      })
+      .then(deletedComment => res.redirect(`/restaurants/${deletedComment.restaurantId}`))
       .catch(err => next(err))
   }
 }
