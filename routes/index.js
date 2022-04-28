ef2d53a7394d100cdcdfa3fa23c3ffab06e24f2b
KencodePM <88673690+KencodePM@users.noreply.github.com>
2022-04-28 23:20:16 +0800

    feat: user signup

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,11 @@ const router = express.Router()
 // 新增，載入 controller
 
 const restController = require('../controllers/restaurant-controller')
+const userController = require('../controllers/user-controller')
 const admin = require('./modules/admin') // 新增這行，載入 admin.js
 router.use('/admin', admin) // 新增這行
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
 router.get('/restaurants', restController.getRestaurants)
 router.get('/', (req, res) => res.redirect('/restaurants'))
 // 新增
