20fa46a76489b138fc0f869b5603de941e804d89
KencodePM <88673690+KencodePM@users.noreply.github.com>
2022-04-29 00:32:59 +0800

    feat: user model add is_admin

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ const passport = require('../config/passport')
 
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
+const { authenticated } = require('../middleware/auth')
 const { generalErrorHandler } = require('../middleware/error-handler')
 const admin = require('./modules/admin') // 新增這行，載入 admin.js
 router.use('/admin', admin) // 新增這行
@@ -15,7 +16,7 @@ router.post('/signup', userController.signUp)
 router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
 router.get('/logout', userController.logout)
-router.get('/restaurants', restController.getRestaurants)
+router.get('/restaurants', authenticated, restController.getRestaurants)
 router.get('/', (req, res) => res.redirect('/restaurants'))
 // 新增
 
