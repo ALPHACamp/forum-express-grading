a42cafdd7e163118eb9e1278c067ca8f58312dc2
Jyun-Fu-Chen <than3698741@gmail.com>
2022-04-28 20:33:21 +0800

    feat:add feeds page

--- routes/index.js
+++ routes/index.js
@@ -21,6 +21,7 @@ router.get('/users/:id/edit', authenticated, userController.editUser)
 router.put('/users/:id', authenticated, upload.single('image'), userController.putUser)
 
 router.get('/logout', userController.logout)
+router.get('/restaurants/feeds', authenticated, restController.getFeeds)
 router.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard)
 router.get('/restaurants/:id', authenticated, restController.getRestaurant)
 router.get('/restaurants', authenticated, restController.getRestaurants)
