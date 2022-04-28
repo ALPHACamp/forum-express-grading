7b139d137d4f5f3abaf5511881e1d4d5a2b734db
zxc58 <abc01234567889@gmail.com>
2022-04-28 15:33:35 +0800

    a19q4

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,6 +1,9 @@
 const bcrypt = require('bcryptjs')
 const db = require('../models')
-const { User } = db
+const { User, Comment, Restaurant } = db
+const { imgurFileHandler } = require('../helpers/file-helpers')
+const {getUser} = require('../helpers/auth-helpers')
+//
 const userController = {
   signUpPage: (req, res) => {
     res.render('signup')
@@ -34,6 +37,46 @@ const userController = {
     req.flash('success_messages', '登出成功！')
     req.logout()
     res.redirect('/signin')
+  },
+  getUser: (req, res, next) => {
+    const paramsId = parseInt(req.params.id)
+    if (getUser(req).id === paramsId) {
+      return User.findByPk(paramsId)
+        .then(user => {
+          res.render('users/profile', { user: user.toJSON() })
+        })
+        .catch(err => next(err))
+    }
+    return next(new Error('發生錯誤，無權限的操作'))
+  },
+  editUser: (req, res, next) => {
+    const paramsId = parseInt(req.params.id)
+    if (getUser(req).id === paramsId) {
+      return User.findByPk(paramsId)
+        .then(user => {
+          res.render('users/edit', { user: user.toJSON() })
+        })
+        .catch(err => next(err))
+    }
+    return next(new Error('發生錯誤，無權限的操作'))
+  },
+  putUser: (req, res, next) => {
+    const paramsId = parseInt(req.params.id)
+    const { name } = req.body
+    const { file } = req
+    if (getUser(req).id === paramsId) {
+      return Promise.all([User.findByPk(paramsId), imgurFileHandler(file)])
+        .then(([user, filePath]) => {
+          return user.update({ name, image: filePath })
+        })
+        .then(() => {
+          req.flash('success_messages', '使用者資料編輯成功')
+          res.redirect(`/users/${req.user.id}`)
+        })
+        .catch(err => next(err))
+    }
+    return next(new Error('發生錯誤，無權限的操作'))
   }
 }
+//
 module.exports = userController
