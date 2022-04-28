b6328a691c7d6b9e9a91fb9d071557c16ad22ab6
softtoday72 <demonhunter720525@hotmail.com>
2022-04-28 19:42:49 +0800

    R02 push heroku

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -14,12 +14,20 @@ const restaurantController = {
     })
   },
   getRestaurant: (req, res, next) => {
-    return Restaurant.findByPk(req.params.id, { include: Category, raw: true, nest: true })
+    return Restaurant.findByPk(req.params.id, { include: Category, nest: true })
       .then(restaurant => {
         if (!restaurant) throw new Error("Restaurant doesn't exist!")
-        res.render('restaurant', { restaurant })
+        restaurant.increment('viewCount')
+        res.render('restaurant', { restaurant: restaurant.toJSON() })
       })
       .catch(err => next(err))
+  },
+  getDashboard: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, { raw: true, nest: true, include: Category })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant doesn't exist!")
+        res.render('dashboard', { restaurant })
+      })
   }
 }
 module.exports = restaurantController
