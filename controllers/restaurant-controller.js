2c3d818b900b100706b427dbc68b2576b414c2c0
Rubylago <rubybibi6@gmail.com>
2022-04-28 19:40:26 +0800

    feat: add feeds page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -68,6 +68,31 @@ const restaurantController = {
     res.render('dashboard', {
       restaurant
     })
+  },
+  getFeeds: (req, res, next) => {
+    return Promise.all([
+      Restaurant.findAll({
+        limit: 10,
+        order: [['createdAt', 'DESC']],
+        include: [Category],
+        raw: true,
+        nest: true
+      }),
+      Comment.findAll({
+        limit: 10,
+        order: [['createdAt', 'DESC']],
+        include: [User, Restaurant],
+        raw: true,
+        nest: true
+      })
+    ])
+      .then(([restaurants, comments]) => {
+        res.render('feeds', {
+          restaurants,
+          comments
+        })
+      })
+      .catch(err => next(err))
   }
 }
 module.exports = restaurantController
