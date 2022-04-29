802b2ee7382d0ae066ac0b979c3fa8990bb45031
naivelove0822 <naivelove0822@gmail.com>
2022-04-29 17:44:23 +0800

    feat: add favorite/unfavorite button at index page

--- routes/index.js
+++ routes/index.js
@@ -28,6 +28,9 @@ router.get('/logout', userController.logout)
 router.delete('/comments/:id', authenticatedAdmin, commentController.deleteComment)
 router.post('/comments', authenticated, commentController.postComment)
 
+router.post('/favorite/:restaurantId', authenticated, userController.addFavorite)
+router.delete('/favorite/:restaurantId', authenticated, userController.removeFavorite)
+
 router.get('/users/:id/edit', authenticated, userController.editUser)
 router.put('/users/:id', upload.single('image'), authenticated, userController.putUser)
 router.get('/users/:id', authenticated, userController.getUser)
