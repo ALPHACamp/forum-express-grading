15d27e6c731873126ff98dc94ce255cc0b4811ba
HitomiHuang <black011602@gmail.com>
2022-04-30 04:54:32 +0800

    feat: add top 10 restaurants

--- models/restaurant.js
+++ models/restaurant.js
@@ -35,7 +35,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'Restaurant',
-    tablename: 'Restaurants',
+    tableName: 'Restaurants',
     underscored: true
   })
   return Restaurant
