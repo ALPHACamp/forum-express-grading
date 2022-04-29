0f7b74e625989529f0a7877ae820023c6a6dd641
Jyun-Fu-Chen <than3698741@gmail.com>
2022-04-30 00:04:18 +0800

    feat: addFollowing & removeFollowing function

--- routes/index.js
+++ routes/index.js
@@ -16,8 +16,10 @@ router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
 router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
-router.get('/users/:id', authenticated, userController.getUser)
+
+router.get('/users/top', authenticated, userController.getTopUsers)
 router.get('/users/:id/edit', authenticated, userController.editUser)
+router.get('/users/:id', authenticated, userController.getUser)
 router.put('/users/:id', authenticated, upload.single('image'), userController.putUser)
 
 router.get('/logout', userController.logout)
@@ -34,7 +36,6 @@ router.delete('/favorite/:restaurantId', authenticated, userController.removeFav
 
 router.post('/like/:restaurantId', authenticated, userController.addLike)
 router.delete('/like/:restaurantId', authenticated, userController.removeLike)
-
 router.get('/', (req, res) => res.redirect('/restaurants'))
 
 router.use('/', generalErrorHandler)
