910e04f011176cdf2b7750280be0a4b5a86635c2
Lydia-09 <pasky0901@gmail.com>
2022-04-30 09:50:54 +0800

    feat: add header & footer

--- app.js
+++ app.js
@@ -8,9 +8,11 @@ const routes = require('./routes')
 const app = express()
 const port = process.env.PORT || 3000
 const SESSION_SECRET = 'secret'
+const handlebarsHelpers = require('./helpers/handlebars-helpers') // 引入 handlebars-helpers
+const { getUser } = require('./helpers/auth-helpers') // 引入自定義的 auth-helpers
 
 // 註冊 Handlebars 樣板引擎，並指定副檔名為 .hbs
-app.engine('hbs', handlebars({ extname: '.hbs' }))
+app.engine('hbs', handlebars({ extname: '.hbs', helpers: handlebarsHelpers }))
 // 設定使用 Handlebars 做為樣板引擎
 app.set('view engine', 'hbs')
 
@@ -18,10 +20,12 @@ app.use(express.urlencoded({ extended: true }))
 app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
 app.use(passport.initialize()) // 初始化 Passport
 app.use(passport.session()) // 啟動 session 功能
+
 app.use(flash()) // 掛載套件
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages') // 設定 success_msg 訊息
   res.locals.error_messages = req.flash('error_messages') // 設定 warning_msg 訊息
+  res.locals.user = getUser(req)
   next()
 })
 
