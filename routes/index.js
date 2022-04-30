2aa29378d5f3433506074e9fed8dbcb1b7f4af34
Jyun-Fu-Chen <than3698741@gmail.com>
2022-05-01 00:58:16 +0800

    feat: add top 10 restaurants

--- routes/index.js
+++ routes/index.js
@@ -24,6 +24,7 @@ router.put('/users/:id', authenticated, upload.single('image'), userController.p
 
 router.get('/logout', userController.logout)
 router.get('/restaurants/feeds', authenticated, restController.getFeeds)
+router.get('/restaurants/top', authenticated, restController.getTopRestaurants)
 router.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard)
 router.get('/restaurants/:id', authenticated, restController.getRestaurant)
 router.get('/restaurants', authenticated, restController.getRestaurants)
