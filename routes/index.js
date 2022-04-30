3df434088610385dffc81f0e154c62a539c52e40
maximeri <t.s.eliot1996@gmail.com>
2022-04-30 15:12:23 +0800

    feat: add feeds page

--- routes/index.js
+++ routes/index.js
@@ -19,6 +19,7 @@ router.delete('/comments/:id', authenticatedAdmin, commentController.deleteComme
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn) // 注意是 Post, 請 Passport 直接做身份驗證
 router.get('/logout', userController.logout)
 router.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard)
+router.get('/restaurants/feeds', authenticated, restController.getFeeds)
 router.get('/restaurants/:id', authenticated, restController.getRestaurant)
 router.get('/restaurants', authenticated, restController.getRestaurants) // 如果接受到的請求路徑是 /restaurants，那就交給 controller 的 getRestaurants 函式來處理。這行路由和請求如果匹配成功，以下的 router.get 就不會執行。
 router.get('/users/:id', authenticated, userController.getUser)
