0e6cee21fb8b725caf8bd04c0ef0971c401006ec
KateChu <chu.shihchien@gmail.com>
2022-05-01 00:20:43 +0800

    debug for R05 test

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,4 +1,4 @@
-const { Restaurant, Category, Comment, User, Favorite } = require('../models')
+const { Restaurant, Category, Comment, User } = require('../models')
 const { getOffset, getPagination } = require('../helpers/pagination-helper')
 
 const restaurantController = {
@@ -102,20 +102,12 @@ const restaurantController = {
       .catch(err => next(err))
   },
   getTopRestaurants: (req, res, next) => {
-    return Promise.all([
-      Restaurant.findAll({
-        include: [{ model: User, as: 'FavoritedUsers' }]
-      }),
-      Favorite.findAll({
-        where: { userId: req.user.id }
-      })
-    ])
-      .then(([restaurants, favorites]) => {
-        favorites = favorites.map(favorite => favorite.toJSON())
+    return Restaurant.findAll({ include: [{ model: User, as: 'FavoritedUsers' }] })
+      .then(restaurants => {
         restaurants = restaurants.map(restaurant => ({
           ...restaurant.toJSON(),
           favoritedCount: restaurant.FavoritedUsers.length,
-          isFavorited: favorites.some(f => f.restaurantId === restaurant.id)
+          isFavorited: req.user?.FavoritedRestaurants?.some(f => f.id === restaurant.id) || []
         }))
         restaurants = restaurants.sort((a, b) => b.favoritedCount - a.favoritedCount).slice(0, 10)
         return res.render('top-restaurants', { restaurants: restaurants })
