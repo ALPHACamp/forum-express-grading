8efa8fe0f28b578f531922153ad97e5498369e26
Lydia-09 <pasky0901@gmail.com>
2022-04-30 23:59:55 +0800

    add users list and admin permission

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -10,6 +10,8 @@ router.put('/restaurants/:id', upload.single('image'), adminController.putRestau
 router.delete('/restaurants/:id', adminController.deleteRestaurant)
 router.get('/restaurants', adminController.getRestaurants)
 router.post('/restaurants', upload.single('image'), adminController.postRestaurant)
+router.patch('/users/:id', adminController.patchUser) // 修改使用者權限
+router.get('/users', adminController.getUsers) // 顯示使用者清單
 router.use('/', (req, res) => res.redirect('/admin/restaurants'))
 
 module.exports = router
