ddb9b3e8e23f9ac27434ffce0720076999692e59
Marcus Liu <me@mail.lmarcus.onl>
2022-04-30 15:02:19 +0800

    feat: add feeds page

--- routes/index.js
+++ routes/index.js
@@ -25,6 +25,7 @@ router.post('/signin', passport.authenticate('local', { failureRedirect: '/signi
 router.get('/logout', userController.logout)
 
 // 餐廳瀏覽
+router.get('/restaurants/feeds', authenticated, restController.getFeeds)
 router.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard)
 router.get('/restaurants/:id', authenticated, restController.getRestaurant)
 router.get('/restaurants', authenticated, restController.getRestaurants)
