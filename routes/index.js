1a1ac3e2a0cde15bbbfe618743f21f748c76cff8
Lydia-09 <pasky0901@gmail.com>
2022-05-01 22:02:02 +0800

    feat: add User Profile feature

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ const userController = require('../controllers/user-controller')
 const commentController = require('../controllers/​​comment-controller')
 const { authenticated, authenticatedAdmin } = require('../middleware/auth')
 const { generalErrorHandler } = require('../middleware/error-handler')
+const upload = require('../middleware/multer')
 
 router.use('/admin', authenticatedAdmin, admin)
 
@@ -20,7 +21,10 @@ router.get('/restaurants/:id', authenticated, restController.getRestaurant)
 router.get('/restaurants', authenticated, restController.getRestaurants)
 router.delete('/comments/:id', authenticatedAdmin, commentController.deleteComment)
 router.post('/comments', authenticated, commentController.postComment)
-router.use('/', (req, res) => res.redirect('/restaurants'))
+router.get('/users/:id', authenticated, userController.getUser)
+router.get('/users/:id/edit', authenticated, userController.editUser)
+router.put('/users/:id', authenticated, upload.single('image'), userController.putUser)
+router.get('/', (req, res) => res.redirect('/restaurants'))
 router.use('/', generalErrorHandler)
 
 module.exports = router
