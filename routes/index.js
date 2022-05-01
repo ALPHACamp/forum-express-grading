27c8d25f7f536da4ee2febe0da8b27da92cd1aec
tkooooo123 <tkooooo123@gmail.com>
2022-05-01 23:57:07 +0800

    add top 10 restaurants page

--- routes/index.js
+++ routes/index.js
@@ -22,6 +22,7 @@ router.get('/users/:id', authenticated, userController.getUser)
 
 router.use('/admin', authenticatedAdmin, admin)
 
+router.get('/restaurants/top', authenticated, restController.getTopRestaurants)
 router.get('/restaurants/feeds', authenticated, restController.getFeeds)
 router.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard)
 router.get('/restaurants/:id', authenticated, restController.getRestaurant)
