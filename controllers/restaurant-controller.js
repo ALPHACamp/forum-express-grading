2b1de213c2ddbb6c77e026e9036b8b445b1aed6a
shuyuhuang23 <susan50337@gmail.com>
2022-05-01 10:36:28 +0800

    feat: get user's favorited restaurants & switch button

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -24,9 +24,11 @@ const restaurantController = {
       Category.findAll({ raw: true })
     ])
       .then(([restaurants, categories]) => {
+        const FavoritedRestaurantsId = req.user && req.user.FavoritedRestaurants.map(fr => fr.id)
         const data = restaurants.rows.map(r => ({
           ...r,
-          description: r.description.substring(0, 50)
+          description: r.description.substring(0, 50),
+          isFavorited: FavoritedRestaurantsId.includes(r.id)
         }))
         return res.render('restaurants', {
           restaurants: data,
@@ -42,14 +44,21 @@ const restaurantController = {
       nest: true,
       include: [
         Category,
-        { model: Comment, include: User }
+        { model: Comment, include: User },
+        { model: User, as: 'FavoritedUsers' }
       ]
     })
       .then(restaurant => {
         if (!restaurant) throw new Error('The restaurant does not exist.')
         return restaurant.increment('viewCounts')
       })
-      .then(restaurant => res.render('restaurant', { restaurant: restaurant.toJSON() }))
+      .then(restaurant => {
+        const isFavorited = restaurant.FavoritedUsers.some(f => f.id === req.user.id)
+        return res.render('restaurant', {
+          restaurant: restaurant.toJSON(),
+          isFavorited
+        })
+      })
       .catch(err => next(err))
   },
   getDashboard: (req, res, next) => {
--- controllers/​​comment-controller.js
+++ controllers/​​comment-controller.js
@@ -4,7 +4,7 @@ const commentController = {
   postComment: (req, res, next) => {
     const { restaurantId, text } = req.body
     const userId = req.user.id
-    if (!text) throw new Error('Comment test is required!')
+    if (!text) throw new Error('Comment text is required!')
     return Promise.all([
       User.findByPk(userId),
       Restaurant.findByPk(restaurantId)
