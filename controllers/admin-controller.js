7bccdc7e342e109b1a4bc1edf46a6e4dd39f1fc1
pusheng <sdf753741789@gmail.com>
2022-05-08 11:48:20 +0800

    fix select option

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -58,8 +58,7 @@ const adminController = {
       .catch(err => next(err))
   },
   putRestaurant: (req, res, next) => {
-    const { name, tel, address, openingHours, description, categoryId
-    } = req.body
+    const { name, tel, address, openingHours, description, categoryId } = req.body
     if (!name) throw new Error('Restaurant name is required!')
     const { file } = req
     Promise.all([Restaurant.findByPk(req.params.id), imgurFileHandler(file)]).then(([restaurant, filePath]) => {
