4f7e24495fa98676f4a8cae02f1abc23701e56da
pusheng <sdf753741789@gmail.com>
2022-05-10 23:36:42 +0800

    feat:add dashboard
    edit getDashboard

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -17,17 +17,31 @@ const restaurantController = {
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
-      include: Category, 
-      nest: true,
-      raw: true
+      include: Category,
+      nest: true
     })
       .then(restaurant => {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
-        res.render('restaurant', {
-          restaurant
+        return restaurant.increment(
+          'viewCounts', { by: 1 }
+        ).then(restaurant => {
+          res.render('restaurant', {
+            restaurant: restaurant.toJSON()
+          })
         })
       })
       .catch(err => next(err))
+  },
+  getDashboard: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, {
+      include: Category,
+      nest: true,
+      raw: true
+    }).then(restaurant => {
+      res.render('dashboard', {
+        restaurant
+      })
+    }).catch(err => next(err))
   }
 }
 module.exports = restaurantController
