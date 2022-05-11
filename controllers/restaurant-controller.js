d85633447c3012830d4cc3fd99882a8ce723fde1
pusheng <sdf753741789@gmail.com>
2022-05-11 21:23:45 +0800

    edit getDashboard

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -18,28 +18,28 @@ const restaurantController = {
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
       include: Category,
-      nest: true,
-      raw: true
+      nest: true
     })
       .then(restaurant => {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
-        res.render('restaurant', {
-          restaurant
+        return restaurant.increment(
+          'viewCounts', { by: 1 }
+        ).then(restaurant => {
+          res.render('restaurant', {
+            restaurant: restaurant.toJSON()
+          })
         })
       })
       .catch(err => next(err))
   },
   getDashboard: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
-      include: Category
-    }).then(restaurant => {
-      if (!restaurant) throw new Error("Restaurant didn't exist!")
-      return restaurant.increment(
-        'viewCounts', { by: 1 }
-      )
+      include: Category,
+      nest: true,
+      raw: true
     }).then(restaurant => {
       res.render('dashboard', {
-        restaurant: restaurant.toJSON()
+        restaurant
       })
     }).catch(err => next(err))
   }
