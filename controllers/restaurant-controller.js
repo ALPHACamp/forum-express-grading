071b3ae17cb115a9059bb5d81057184fd61090c4
pusheng <sdf753741789@gmail.com>
2022-05-12 21:35:51 +0800

    feat:add categories tabs

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,19 +1,30 @@
 const { Restaurant, Category } = require('../models')
 const restaurantController = {
-  getRestaurants: (req, res) => {
-    return Restaurant.findAll({
+  getRestaurants: (req, res, next) => {
+    const categoryId = Number(req.query.categoryId) || ''
+    Promise.all([Restaurant.findAll({
       include: Category,
+      where: {
+        ...categoryId
+          ? {
+              categoryId
+            }
+          : {}
+      },
       nest: true,
       raw: true
-    }).then(restaurants => {
+    }),
+    Category.findAll({ raw: true })]).then(([restaurants, categories]) => {
       const data = restaurants.map(r => ({
         ...r,
         description: r.description.substring(0, 50)
       }))
       return res.render('restaurants', {
-        restaurants: data
+        restaurants: data,
+        categories,
+        categoryId
       })
-    })
+    }).catch(err => next(err))
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
