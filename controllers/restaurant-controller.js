78ff26c7040e6ade1701ecc275816ceb398b24d4
pusheng <sdf753741789@gmail.com>
2022-05-14 16:48:52 +0800

    show comments on restaurant page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,4 +1,4 @@
-const { Restaurant, Category } = require('../models')
+const { Restaurant, Category, Comment, User } = require('../models')
 const { getOffset, getPagination } = require('../helpers/pagination-helper')
 
 const restaurantController = {
@@ -13,11 +13,7 @@ const restaurantController = {
     Promise.all([Restaurant.findAndCountAll({
       include: Category,
       where: {
-        ...categoryId
-          ? {
-              categoryId
-            }
-          : {}
+        ...categoryId ? { categoryId } : {}
       },
       limit,
       offset,
@@ -39,10 +35,13 @@ const restaurantController = {
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
-      include: Category
+      include: [Category, {
+        model: Comment, include: User
+      }]
     })
       .then(restaurant => {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
+        console.log(restaurant)
         return restaurant.increment(
           'viewCounts', { by: 1 }
         ).then(restaurant => {
