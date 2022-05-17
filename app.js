7facd51c0f304f8067b7861ba9bfbadcc3113d6d
smile19439 <smile19439@gmail.com>
2022-05-17 16:59:31 +0800

    add signup page and session

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
 const routes = require('./routes')
+const session = require('express-session')
+const flash = require('connect-flash')
 
 const app = express()
 const port = process.env.PORT || 3000
 
+const SESSION_SECRET = 'secret'
+
 app.engine('hbs', handlebars({ extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(express.urlencoded({ extended: true }))
+app.use(session({
+  secret: SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
+
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 
 app.use(routes)
 
---
+++ controllers/user-controller.js
@@ -0,0 +1,36 @@
+const bcrypt = require('bcryptjs')
+const db = require('../models')
+const { User } = db
+
+const userController = {
+  signUpPage: (req, res) => {
+    res.render('signup')
+  },
+
+  signUp: (req, res, next) => {
+    const { name, email, password, passwordCheck } = req.body
+
+    if (password !== passwordCheck) {
+      throw new Error('Passwords do not match!')
+    }
+    User.findOne({ where: { email } })
+      .then(user => {
+        if (user) {
+          throw new Error('Email already exists!')
+        }
+        return bcrypt.hash(password, 10)
+      })
+      .then(hash => User.create({
+        name,
+        email,
+        password: hash
+      }))
+      .then(() => {
+        req.flash('success_messages', '成功註冊帳號!')
+        res.redirect('/signin')
+      })
+      .catch(err => next(err))
+  }
+}
+
+module.exports = userController
