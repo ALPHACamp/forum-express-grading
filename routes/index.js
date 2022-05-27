6234b134e83fef4649887ff699666b9515ad6cd3
bft7658 <bft7658@gmail.com>
2022-05-27 17:16:35 +0800

    feat: add index page

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
+// 載入 controller
+const restController = require('../controllers/restaurant-controller')
 
-router.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+router.get('/restaurants', restController.getRestaurants)
+
+router.use('/', (req, res) => res.redirect('/restaurants'))
 
 module.exports = router
