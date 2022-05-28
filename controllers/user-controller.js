6dc8418d75d597d63756292551a29acf331e7a02
Pin Hsu Chen <paul791111@gmail.com>
2022-05-28 17:45:31 +0800

    add signup function

---
+++ controllers/user-controller.js
@@ -0,0 +1,20 @@
+const bcrypt = require('bcryptjs')
+const db = require('../models')
+const { User } = db
+
+const userController = {
+  signUpPage: (req, res) => {
+    res.render('signup')
+  },
+  signUp: async (req, res) => {
+    const hash = await bcrypt.hash(req.body.password, 10)
+    await User.create({
+      name: req.body.name,
+      email: req.body.email,
+      password: hash
+    })
+    return res.redirect('/signin')
+  }
+}
+
+module.exports = userController
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,11 @@ const express = require('express')
 const router = express.Router()
 const admin = require('./modules/admin')
 const restaurantController = require('../controllers/restaurant-controller')
+const userController = require('../controllers/user-controller')
 
 router.use('/admin', admin)
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
 router.get('/restaurants', restaurantController.getRestaurants)
 router.use('/', (req, res) => res.redirect('/restaurants'))
 
