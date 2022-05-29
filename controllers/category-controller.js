c7b081d38e3ec5a2d274ff362c1b23fb63e5fb0e
Pin Hsu Chen <paul791111@gmail.com>
2022-05-29 17:24:53 +0800

    add category update

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -2,8 +2,10 @@ const { Category } = require('../models')
 const categoryController = {
   getCategories: async (req, res, next) => {
     try {
+      const categoryId = req.params.id
       const categories = await Category.findAll({ raw: true })
-      return res.render('admin/categories', { categories })
+      const category = categoryId ? await Category.findByPk(categoryId, { raw: true }) : null
+      return res.render('admin/categories', { categories, category })
     } catch (error) {
       next(error)
     }
@@ -14,6 +16,20 @@ const categoryController = {
       if (!name) throw new Error('Category name is required!')
       await Category.create({ name })
       req.flash('success_messages', 'Category was successfully created')
+      return res.redirect('/admin/categories')
+    } catch (error) {
+      next(error)
+    }
+  },
+  putCategory: async (req, res, next) => {
+    try {
+      const { name } = req.body
+      const categoryId = req.params.id
+      if (!name) throw new Error('Category name is required!')
+      const category = await Category.findByPk(categoryId)
+      if (!category) throw new Error("Category didn't exist!")
+      await category.update({ name })
+      req.flash('success_messages', 'Category was successfully updated')
       return res.redirect('/admin/categories')
     } catch (error) {
       next(error)
