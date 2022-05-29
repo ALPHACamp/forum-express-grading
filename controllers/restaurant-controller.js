8c3f2eefb1c98c21f6b771b25457d4412c0b8e27
Pin Hsu Chen <paul791111@gmail.com>
2022-05-29 20:09:02 +0800

    add categories navbar on restaurants page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -3,11 +3,18 @@ const { Restaurant, Category } = require('../models')
 const restaurantController = {
   getRestaurants: async (req, res, next) => {
     try {
-      const restaurants = await Restaurant.findAll({ raw: true, nest: true, include: [Category] })
+      const categoryId = req.query.categoryId || ''
+      const restaurants = await Restaurant.findAll({
+        raw: true,
+        nest: true,
+        include: [Category],
+        where: { ...categoryId ? { categoryId } : {} }
+      })
+      const categories = await Category.findAll({ raw: true })
       const data = await restaurants.map(restaurant => ({
         ...restaurant, description: restaurant.description.substring(0, 50) + '...'
       }))
-      return res.render('restaurants', { restaurants: data })
+      return res.render('restaurants', { restaurants: data, categories, categoryId })
     } catch (error) {
       next(error)
     }
