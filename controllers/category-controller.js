8160ac260f2d95a41396954f9a65eeaf5158759a
Pin Hsu Chen <paul791111@gmail.com>
2022-05-29 17:13:15 +0800

    add category create

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -7,6 +7,17 @@ const categoryController = {
     } catch (error) {
       next(error)
     }
+  },
+  postCategory: async (req, res, next) => {
+    try {
+      const { name } = req.body
+      if (!name) throw new Error('Category name is required!')
+      await Category.create({ name })
+      req.flash('success_messages', 'Category was successfully created')
+      return res.redirect('/admin/categories')
+    } catch (error) {
+      next(error)
+    }
   }
 }
 module.exports = categoryController
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -16,6 +16,7 @@ router.patch('/users/:id', adminController.patchUser)
 router.get('/users', adminController.getUsers)
 
 router.get('/categories', categoryController.getCategories)
+router.post('/categories', categoryController.postCategory)
 
 router.get('/', (req, res) => res.redirect('/admin/restaurants'))
 
