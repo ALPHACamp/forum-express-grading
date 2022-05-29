63e951bd1c90b985c2d93fd54f9731905951a68d
Pin Hsu Chen <paul791111@gmail.com>
2022-05-29 10:38:59 +0800

    add admin create rest page & function

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -8,6 +8,20 @@ const adminController = {
     } catch (error) {
       next(error)
     }
+  },
+  createRestaurant: (req, res) => {
+    return res.render('admin/create-restaurant')
+  },
+  postRestaurant: async (req, res, next) => {
+    try {
+      const { name, tel, address, openingHours, description } = req.body
+      if (!name) throw new Error('Restaurant name is required!')
+      await Restaurant.create({ name, tel, address, openingHours, description })
+      req.flash('success_messages', 'restaurant was successfully created')
+      res.redirect('/admin/restaurants')
+    } catch (error) {
+      next(error)
+    }
   }
 }
 
