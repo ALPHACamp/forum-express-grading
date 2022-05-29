6851d6baef0076303d8df62e49b8fd3fa76de677
Pin Hsu Chen <paul791111@gmail.com>
2022-05-29 20:23:38 +0800

    add pagination

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,20 +1,27 @@
 const { Restaurant, Category } = require('../models')
+const { getOffset, getPagination } = require('../helpers/pagination-helper')
 
 const restaurantController = {
   getRestaurants: async (req, res, next) => {
     try {
+      const DEFAULT_LIMIT = 9
+      const page = Number(req.query.page) || 1
+      const limit = Number(req.query.limit) || DEFAULT_LIMIT
+      const offset = getOffset(limit, page)
       const categoryId = req.query.categoryId || ''
-      const restaurants = await Restaurant.findAll({
+      const restaurants = await Restaurant.findAndCountAll({
         raw: true,
         nest: true,
         include: [Category],
-        where: { ...categoryId ? { categoryId } : {} }
+        where: { ...categoryId ? { categoryId } : {} },
+        limit,
+        offset
       })
       const categories = await Category.findAll({ raw: true })
-      const data = await restaurants.map(restaurant => ({
+      const data = await restaurants.rows.map(restaurant => ({
         ...restaurant, description: restaurant.description.substring(0, 50) + '...'
       }))
-      return res.render('restaurants', { restaurants: data, categories, categoryId })
+      return res.render('restaurants', { restaurants: data, categories, categoryId, pagination: getPagination(limit, page, restaurants.count) })
     } catch (error) {
       next(error)
     }
