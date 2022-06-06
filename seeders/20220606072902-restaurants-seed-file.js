9600bbe156983e24c5b5538d208fab62c7dac908
bft7658 <bft7658@gmail.com>
2022-06-06 15:33:32 +0800

    feat: update seed files

--- seeders/20220606072902-restaurants-seed-file.js
+++ seeders/20220606072902-restaurants-seed-file.js
@@ -3,6 +3,11 @@ const faker = require('faker')
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    // 先去查詢現在 Categories 的 id 有哪些
+    const categories = await queryInterface.sequelize.query(
+      'SELECT id FROM Categories;',
+      { type: queryInterface.sequelize.QueryTypes.SELECT }
+    )
     await queryInterface.bulkInsert('Restaurants',
       Array.from({ length: 50 }, () => ({
         name: faker.name.findName(),
@@ -12,7 +17,8 @@ module.exports = {
         image: `https://loremflickr.com/320/240/restaurant,food/?random=${Math.random() * 100}`,
         description: faker.lorem.text(),
         created_at: new Date(),
-        updated_at: new Date()
+        updated_at: new Date(),
+        category_id: categories[Math.floor(Math.random() * categories.length)].id
       }))
     )
   },
