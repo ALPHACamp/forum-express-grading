19e1213dafd5db300d104edcab3cbc6b5283c9df
bft7658 <bft7658@gmail.com>
2022-06-07 14:20:59 +0800

    feat: category delete

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -15,6 +15,7 @@ router.post('/restaurants', upload.single('image'), adminController.postRestaura
 router.get('/users', adminController.getUsers)
 router.get('/categories/:id', categoryController.getCategories)
 router.put('/categories/:id', categoryController.putCategory)
+router.delete('/categories/:id', categoryController.deleteCategory)
 router.get('/categories', categoryController.getCategories)
 router.post('/categories', categoryController.postCategories)
 router.get('/', (req, res) => res.redirect('/admin/restaurants'))
