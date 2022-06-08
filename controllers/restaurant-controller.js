ce919d41339a3bfebf44dfbdf7180c91d45ca564
bft7658 <bft7658@gmail.com>
2022-06-08 11:33:42 +0800

    feat: R02 finished

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -18,15 +18,26 @@ const restaurantController = {
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
-      include: Category,
-      nest: true,
-      raw: true
+      include: Category
     })
       .then(restaurant => {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
-        res.render('restaurant', {
-          restaurant
-        })
+        return restaurant.increment('view_counts')
+      })
+      .then(restaurant => res.render('restaurant', {
+        restaurant: restaurant.toJSON()
+      }))
+      .catch(err => next(err))
+  },
+  getDashboard: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, {
+      include: Category,
+      raw: true,
+      nest: true
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Dashboard didn't exist!")
+        res.render('dashboard', { restaurant })
       })
       .catch(err => next(err))
   }
