c7b68f0a45369f28bee5c9ca3281903af922f3d8
bft7658 <bft7658@gmail.com>
2022-06-09 23:53:06 +0800

    feat: add profile page

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,5 +1,7 @@
 const bcrypt = require('bcryptjs')
 const { User } = require('../models')
+const { imgurFileHandler } = require('../helpers/file-helpers')
+const { getUser } = require('../helpers/auth-helpers')
 
 const userController = {
   signUpPage: (req, res) => {
@@ -38,6 +40,53 @@ const userController = {
     req.flash('success_messages', '登出成功！')
     req.logout()
     res.redirect('/signin')
+  },
+  getUser: (req, res, next) => {
+    const currentUser = getUser(req)
+    return User.findByPk(req.params.id, {
+      raw: true
+    })
+      .then(targetUser => {
+        if (!targetUser) throw new Error("User didn't exist!")
+        res.render('users/profile', {
+          user: currentUser, // 現在登入的使用者
+          targetUser // 查看其他使用者
+        })
+      })
+      .catch(err => next(err))
+  },
+  editUser: (req, res, next) => {
+    return User.findByPk(req.params.id, {
+      raw: true
+    })
+      .then(user => {
+        if (!user) throw new Error("User didn't exist!")
+        res.render('users/edit', { user })
+      })
+      .catch(err => next(err))
+  },
+  putUser: (req, res, next) => {
+    const id = req.params.id
+    const { name } = req.body
+    const { file } = req // 把檔案取出來
+    if (!name.trim()) throw new Error('User name is required!')
+
+    return Promise.all([
+      User.findByPk(id),
+      imgurFileHandler(file)
+    ])
+      .then(([user, filePath]) => {
+        if (!user) throw new Error("User didn't exist!")
+        return user.update({
+          name: name.trim(),
+          image: filePath || user.image
+        })
+      })
+      .then(() => {
+        req.flash('success_messages', '使用者資料編輯成功')
+        res.redirect(`/users/${id}`)
+      })
+      .catch(err => next(err))
   }
 }
 module.exports = userController
