5f9abce97ec572302b5f10ce9a5a36ab2a695e80
Rennll <dt616717@gmail.com>
2022-06-29 00:31:26 +0800

    feat: add show a reataurant

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -2,8 +2,9 @@ const express = require('express')
 const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
 
-router.get('/restaurants', adminController.getRestaurants)
 router.get('/restaurants/create', adminController.createRestaurant)
+router.get('/restaurants/:id', adminController.getRestaurant)
+router.get('/restaurants', adminController.getRestaurants)
 router.post('/restaurants', adminController.postRestaurant)
 router.use('/', (req, res) => res.redirect('admin/restaurants'))
 
