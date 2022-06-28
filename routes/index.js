b4f9a16a00efa5a2f05c8eb41f14478fda9f401a
AndyChienn <andy2002chien@hotmail.com.tw>
2022-06-28 12:21:29 +0800

    feat: user signup

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const restController = require('../controllers/restaurant-controller')
+const userController = require('../controllers/user-controller')
 const admin = require('./modules/admin') // 新增這行，載入 admin.js
 
 router.use('/admin', admin) // 新增這行
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
 router.get('/restaurants', restController.getRestaurants)
 router.use('/', (req, res) => res.redirect('/restaurants'))
 
