e7f6fcbd8fda61d461f40a185c2555abbd060b17
Pin Hsu Chen <paul791111@gmail.com>
2022-06-28 11:00:29 +0800

    feat: add cascade to FK of restaurant

--- models/category.js
+++ models/category.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate (models) {
-      Category.hasMany(models.Restaurant, { foreignKey: 'categoryId', onDelete: 'SET NULL' })
+      Category.hasMany(models.Restaurant, { foreignKey: 'categoryId', onDelete: 'SET NULL', onUpdate: 'CASCADE' })
     }
   };
   Category.init({
