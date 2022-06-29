c0b8f0ffa92c902da2d4d315f5fcbd895ad2df8e
AndyChienn <andy2002chien@hotmail.com.tw>
2022-06-29 15:30:59 +0800

    feat: admin update restaurant

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -3,7 +3,9 @@ const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
 
 router.get('/restaurants/create', adminController.createRestaurant) // 新增這行
+router.get('/restaurants/:id/edit', adminController.editRestaurant)
 router.get('/restaurants/:id', adminController.getRestaurant)
+router.put('/restaurants/:id', adminController.putRestaurant)
 router.get('/restaurants', adminController.getRestaurants)
 router.post('/restaurants', adminController.postRestaurant)
 router.get('/', (req, res) => res.redirect('/admin/restaurants'))
