7e26796a9a85b34fe0845285a270ccf3626da2c6
Rennll <dt616717@gmail.com>
2022-06-29 16:23:16 +0800

    fix: repleace login to sign in2

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,13 +1,17 @@
 const { ensureAuthenticated, getUser } = require('../helpers/auth-helper')
 
 const authenticated = (req, res, next) => {
-  if (ensureAuthenticated(req)) return next()
+  if (ensureAuthenticated(req)) {
+    return next()
+  }
   res.redirect('/signin')
 }
 
 const authenticatedAdmin = (req, res, next) => {
   if (ensureAuthenticated(req)) {
-    if (getUser(req).isAdmin) return next()
+    if (getUser(req).isAdmin) {
+      return next()
+    }
     res.redirect('/')
   }
   res.redirect('/signin')
