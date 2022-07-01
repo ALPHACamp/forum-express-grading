cc591e1ac4d76cf9f748c47eb9a3d0927e4deeb8
ChillDiver <yumingchang1991@gmail.com>
2022-07-01 20:02:20 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const handlebars = require('express-handlebars')
 const routes = require('./routes')
 
 const app = express()
 const port = process.env.PORT || 3000
+
+app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
 
 app.use(routes)
 
@@ -10,4 +14,5 @@ app.listen(port, () => {
   console.info(`Example app listening on port ${port}!`)
 })
 
+// this is for automatic test
 module.exports = app
