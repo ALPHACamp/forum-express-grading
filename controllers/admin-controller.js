043c47774d7659bc849a45ceb5ba9c2dc4568e21
litangchen <chen.li.tang@gmail.com>
2022-07-03 16:34:27 +0800

    feat: add upload image

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,4 +1,6 @@
 const { Restaurant } = require('../models')
+const { localFileHandler } = require('../helpers/file-helpers')
+
 const adminController = {
   getRestaurants: (req, res, next) => {
     return Restaurant.findAll({
@@ -17,13 +19,17 @@ const adminController = {
     if (!name) {
       throw new Error('Restaurant name is required!')
     }
-    Restaurant.create({
-      name,
-      tel,
-      address,
-      openingHours,
-      description
-    })
+
+    const { file } = req
+    localFileHandler(file)
+      .then(filePath => Restaurant.create({
+        name,
+        tel,
+        address,
+        openingHours,
+        description,
+        image: filePath || null
+      }))
       .then(() => {
         req.flash('success_messages', 'restaurant was successfully created')
         res.redirect('/admin/restaurants')
@@ -52,16 +58,27 @@ const adminController = {
   },
   putRestaurant: (req, res, next) => {
     const { name, tel, address, openingHours, description } = req.body
-    if (!name) throw new Error('Restaurant name is required!')
-    Restaurant.findByPk(req.params.id)
-      .then(restaurant => {
-        if (!restaurant) throw new Error("Restaurant didn't exist!")
+    if (!name) {
+      throw new Error('Restaurant name is required!')
+    }
+
+    const { file } = req
+    Promise.all([
+      Restaurant.findByPk(req.params.id),
+      localFileHandler(file)
+    ])
+      .then(([restaurant, filePath]) => {
+        if (!restaurant) {
+          throw new Error("Restaurant didn't exist!")
+        }
+
         return restaurant.update({
           name,
           tel,
           address,
           openingHours,
-          description
+          description,
+          image: filePath || restaurant.image
         })
       })
       .then(() => {
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -3,13 +3,14 @@ const router = express.Router()
 
 const adminController = require('../../controllers/admin-controller')
 const { authenticatedAdmin } = require('../../middleware/auth')
+const upload = require('../../middleware/multer')
 
 router.get('/restaurants/create', authenticatedAdmin, adminController.createRestaurant)
 router.get('/restaurants/:id/edit', adminController.editRestaurant)
 router.get('/restaurants/:id', adminController.getRestaurant)
-router.put('/restaurants/:id', adminController.putRestaurant)
+router.put('/restaurants/:id', upload.single('image'), adminController.putRestaurant)
 router.delete('/restaurants/:id', adminController.deleteRestaurant)
 router.get('/restaurants', authenticatedAdmin, adminController.getRestaurants)
-router.post('/restaurants', adminController.postRestaurant)
+router.post('/restaurants', upload.single('image'), adminController.postRestaurant)
 router.get('/', (req, res) => res.redirect('/admin/restaurants'))
 module.exports = router
