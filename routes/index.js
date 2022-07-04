cf4e937c307d17fe41ffcc3c02856290ccf7c718
AdrieneTZ <tznali17@gmail.com>
2022-07-04 16:00:43 +0800

    feat: add controllers file and index page

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,18 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// import controller modules
+const restaurantController = require('../controllers/restaurant-controller')
+
+/**
+ * as server gets request '/restaurants'
+ * this request will pass to the function 'getRestaurants' which is in object restaurantController
+ */
+router.get('/restaurants', restaurantController.getRestaurants)
+/**
+ * set fallback router
+ * if all routers above are not allow to get into, this fallback router is the only one to enter
+ */
+router.use('/', (req, res) => res.redirect('/restaurants'))
 
 module.exports = router
