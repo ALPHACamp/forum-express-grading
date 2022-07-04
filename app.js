9078acb0da4984de20cb58bf4fea70cb3b70d33d
InzooC <bluefreaks@gmail.com>
2022-07-04 15:22:54 +0800

    feat: user signup

--- app.js
+++ app.js
@@ -5,10 +5,12 @@ const routes = require('./routes')
 const app = express()
 const port = process.env.PORT || 3000
 
+const bcrypt = require('bcryptjs')
 const db = require('./models')
 
 app.engine('hbs', handlebars({ extname: '.hbs' }))
 app.set('view engine', 'hbs')
+app.use(express.urlencoded({ extended: true }))
 app.use(routes)
 
 app.listen(port, () => {
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const router = express.Router()
 const restController = require('../controllers/restaurant-controller')
+const userController = require('../controllers/user-controller')
 const admin = require('./modules/admin')
 
 //! 匹配條件多的路由寫在前面，讓程式先判斷。
 router.use('/admin', admin)
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
 router.get('/restaurants', restController.getRestaurants)
-router.use('/', (req, res) => res.redirect('/restaurants'))
+router.get('/', (req, res) => res.redirect('/restaurants'))
 
 module.exports = router
