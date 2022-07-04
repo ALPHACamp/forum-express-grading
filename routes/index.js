2f0997b4a575997129d4a6f76f16fec40ec92a89
cincinfish <96610786+cincinfish@users.noreply.github.com>
2022-07-04 14:52:52 +0800

    feat add index page

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
+const restController = require('../controllers/restaurant-controller')
+
+router.get('/restaurants', restController.getRestaurant)
+
 router.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.redirect('/restaurants')
 })
 
 module.exports = router
