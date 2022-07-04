bbfddaf61af3908e83d4b66939e436b219e79825
ChillDiver <yumingchang1991@gmail.com>
2022-07-04 19:24:02 +0800

    feat: add user signup

--- app.js
+++ app.js
@@ -5,9 +5,12 @@ const routes = require('./routes')
 const app = express()
 const port = process.env.PORT || 3000
 
+require('./models/index')
+
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
+app.use(express.urlencoded({ extended: true }))
 app.use(routes)
 
 app.listen(port, () => {
--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,11 @@ const router = express.Router()
 const admin = require('./modules/admin')
 
 const restController = require('../controllers/restaurant-controller')
+const userController = require('../controllers/user-controller')
 
 router.use('/admin', admin)
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
 router.get('/restaurants', restController.getRestaurants)
 router.use('/', (req, res) => res.redirect('/restaurants'))
 
