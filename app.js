b646836a90e97feea6e98b3e04d1fecd9bc4a48a
ChillDiver <yumingchang1991@gmail.com>
2022-07-04 22:15:34 +0800

    feat: add flash messages to main layout and signup path

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
+const flash = require('connect-flash')
+const session = require('express-session')
 const routes = require('./routes')
+
+const SESSION_SECRET = 'secret'
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -11,6 +15,13 @@ app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 
 app.use(express.urlencoded({ extended: true }))
+app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 app.use(routes)
 
 app.listen(port, () => {
