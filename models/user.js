f2d8648690db18621c7be6eb459066d54cb5df16
Rita Chien <ritachien929@gmail.com>
2022-07-04 11:56:40 +0800

    feat: add user model

---
+++ models/user.js
@@ -0,0 +1,22 @@
+'use strict'
+const {
+  Model
+} = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    static associate(models) {
+      // define association here
+    }
+  };
+  User.init({
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'User',
+    tablenName: 'Users',
+    underscored: true
+  })
+  return User
+}
