dfec48728605058a76dcf8daf931fc76612dd913
Roger <winter30009@yahoo.com.tw>
2022-07-04 18:10:05 +0800

    feat: user signup

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express()
 const port = process.env.PORT || 3000
 app.engine('hbs', handlebars({ extname: '.hbs' }))
 app.set('view engine', 'hbs')
+app.use(express.urlencoded({ extended: true }))
 app.use(routes)
 
 app.listen(port, () => {
