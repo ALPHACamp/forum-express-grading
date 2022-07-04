61ee9fd4cfb9c65b8afdc0f1519a274f79bb4929
Rennll <dt616717@gmail.com>
2022-07-04 19:04:59 +0800

    feat: add forestage  restaurant page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -15,6 +15,19 @@ const restaurantController = {
         return res.render('restaurants', { restaurants: data })
       })
       .catch(err => next(err))
+  },
+  getRestaurant: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, {
+      raw: true,
+      nest: true,
+      include: Category
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!") //  如果找不到，回傳錯誤訊息，後面不執行
+        
+        return res.render('restaurant', restaurant)
+      })
+      .catch(err => next(err))
   }
 }
 
