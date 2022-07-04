6800d373010b4bd7c791f13a34df4d2612adeb52
Rita Chien <ritachien929@gmail.com>
2022-07-04 21:08:17 +0800

    feat: modify admin restaurants page

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,7 +1,14 @@
 // FilePath: controllers/admin-controllers.js
+// Include modules
+const { Restaurant } = require('../models')
+
+// Controller
 const adminController = {
-  getRestaurants: (req, res) => {
-    return res.render('admin/restaurants')
+  getRestaurants: async (req, res, next) => {
+    try {
+      const restaurants = await Restaurant.findAll({ raw: true })
+      res.render('admin/restaurants', { restaurants })
+    } catch (err) { next(err) }
   }
 }
 
