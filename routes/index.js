926253337834a2e49d3325a241db3d4e2773ebcc
InzooC <bluefreaks@gmail.com>
2022-07-04 11:25:09 +0800

    feat: add index page

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
+const restController = require('../controllers/restaurant-controller')
 
-router.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+//! 匹配條件多的路由寫在前面，讓程式先判斷。
+router.get('/restaurants', restController.getRestaurants)
+router.use('/', (req, res) => res.redirect('/restaurants'))
 
 module.exports = router
