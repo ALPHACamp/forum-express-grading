5b4cb274b60b3641407dd4c6e33ecca919c666df
Rennll <dt616717@gmail.com>
2022-07-05 23:30:54 +0800

    feat: get user's favorited restaurants & switch button

--- config/passport.js
+++ config/passport.js
@@ -1,8 +1,7 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local').Strategy
 const bcrypt = require('bcryptjs')
-const db = require('../models')
-const User = db.User
+const { User, Restaurant } = require('../models')
 
 // set up Passport strategy
 // customize user field
@@ -27,11 +26,12 @@ passport.serializeUser((user, cb) => {
   cb(null, user.id)
 })
 passport.deserializeUser((id, cb) => {
-  User.findByPk(id)
-    .then(user => {
-      user = user.toJSON()
-      return cb(null, user)
-    })
+  User.findByPk(id, {
+    include: [
+      { model: Restaurant, as: 'FavoritedRestaurants' }
+    ]
+  })
+    .then(user => cb(null, user.toJSON()))
     .catch(err => cb(err, false))
 })
 
