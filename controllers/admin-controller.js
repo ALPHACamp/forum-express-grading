26666ce7f3bbbe7f0aaaabecd81dcc5817eabf9d
gene.31985906@gmail.com <gene.31985906@gmail.com>
2022-07-05 21:03:21 +0800

    feat: modify admin restaurants page

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,6 +1,11 @@
+const { Restaurant } = require('../models')
 const adminController = {
-  getRestaurants: (req, res) => {
-    return res.render('admin/restaurants')
+  getRestaurants: (req, res, next) => {
+    Restaurant.findAll({
+      raw: true
+    })
+      .then(restaurants => res.render('admin/restaurants', { restaurants }))
+      .catch(error => next(error))
   }
 }
 module.exports = adminController
