03a330a25407d6d19b919594cca7f1d2db8f19a7
yongsin0129 <yongsin0129@gmail.com>
2022-07-05 15:56:47 +0800

    feat: passport init & signin

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -2,9 +2,11 @@ const bcrypt = require('bcryptjs')
 const db = require('../models')
 const { User } = db
 const userController = {
+
   signUpPage: (req, res) => {
     res.render('signup')
   },
+
   signUp: (req, res, next) => {
     if (req.body.password !== req.body.passwordCheck) {
       throw new Error('Passwords do not match!')
@@ -27,6 +29,21 @@ const userController = {
         res.redirect('/signin')
       })
       .catch(err => next(err))
+  },
+
+  signInPage: (req, res) => {
+    res.render('signin')
+  },
+
+  signIn: (req, res) => {
+    req.flash('success_messages', '成功登入！')
+    res.redirect('/restaurants')
+  },
+
+  logout: (req, res) => {
+    req.flash('success_messages', '登出成功！')
+    req.logout()
+    res.redirect('/signin')
   }
 }
 module.exports = userController
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,20 @@ const router = express.Router()
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
 const { generalErrorHandler } = require('../middleware/error-handler')
+const passport = require('../config/passport')
 const admin = require('./modules/admin')
 
 router.use('/admin', admin)
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp) // 注意用 post
+router.get('/signin', userController.signInPage)
+router.post('/signin',
+  passport.authenticate('local', {
+    failureRedirect: '/signin',
+    failureFlash: true
+  }),
+  userController.signIn
+) // 注意是 post
 router.get('/restaurants', restController.getRestaurants)
 router.use('/', (req, res) => res.redirect('/restaurants'))
 router.use('/', generalErrorHandler)
