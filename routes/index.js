6d477f02bda9680a2f6501e37b2635833429a5fd
InzooC <bluefreaks@gmail.com>
2022-07-05 11:26:34 +0800

    feat: create restaurant

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,11 @@ const passport = require('../config/passport')
 const admin = require('./modules/admin')
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
-const { authenticated } = require('../middleware/auth')
+const { authenticated, authenticatedAdmin } = require('../middleware/auth')
 const { generalErrorHandler } = require('../middleware/error-handler')
 
 //! 匹配條件多的路由寫在前面，讓程式先判斷。
-router.use('/admin', admin)
+router.use('/admin', authenticatedAdmin, admin)
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
 router.get('/signin', userController.signInPage)
