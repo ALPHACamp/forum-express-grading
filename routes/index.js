133fb60d8003ec1f61db902dff4213dfded5d1d7
june850713@gmail.com <june850713@gmail.com>
2022-07-05 14:38:09 +0800

    feat: add signup page and route

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const restController = require('../controllers/restaurant-controller')
+const userController = require('../controllers/user-controller')
 const admin = require('./modules/admin')
 
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
 router.use('/admin', admin)
 router.get('/restaurants', restController.getRestaurants)
 router.use('/', (req, res) => res.redirect('/restaurants'))
