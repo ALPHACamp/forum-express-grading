ba20d97707013fa63e8f62f1dad21bd444022442
yaahsin <brillanteoboe@gmail.com>
2022-07-05 23:01:23 +0800

    feat: create restaurant

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,12 +1,33 @@
 const { Restaurant } = require('../models')
 
 const adminController = {
-  getRestaurants: (req, res, next) => {
-    Restaurant.findAll({
-      raw: true
+  getRestaurants: (req, res) => {
+    return Restaurant.findAll({ raw: true })
+      .then(restaurants => {
+        return res.render('admin/restaurants', { restaurants: restaurants })
+      })
+  },
+  // 新增畫面
+  createRestaurant: (req, res) => {
+    return res.render('admin/create-restaurant')
+  },
+  // 寫入資料
+  postRestaurant: (req, res, next) => {
+    const { name, tel, address, openingHours, description } = req.body
+    if (!name) throw new Error('Restaurant name is required!')
+    Restaurant.create({
+      name,
+      tel,
+      address,
+      openingHours,
+      description
     })
-      .then(restaurants => res.render('admin/restaurants', { restaurants }))
+      .then(() => {
+        req.flash('success_messages', 'restaurant was successfully created')
+        res.redirect('/admin/restaurants')
+      })
       .catch(err => next(err))
   }
 }
+
 module.exports = adminController
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -2,7 +2,10 @@ const express = require('express')
 const router = express.Router()
 
 const adminController = require('../../controllers/admin-controller')
-const { authenticatedAdmin } = require('../../middleware/auth') // 引入 auth.js
-router.get('/restaurants', authenticatedAdmin, adminController.getRestaurants) // 修改這行，新增 authenticatedAdmin 參數
+
+router.get('/restaurants/create', adminController.createRestaurant)
+router.get('/restaurants', adminController.getRestaurants)
+router.post('/restaurants', adminController.postRestaurant)
 router.get('', (req, res) => res.redirect('/admin/restaurants'))
+
 module.exports = router
