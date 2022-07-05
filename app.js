9a1c2c5479cd5792cc47296db0acabb1b8c23c90
Roger <winter30009@yahoo.com.tw>
2022-07-05 14:32:51 +0800

    feat: add header & footer

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
+const { getUser } = require('./helpers/auth-helpers')
 const routes = require('./routes')
 const flash = require('connect-flash')
 const session = require('express-session')
 const passport = require('./config/passport')
+const handlebarsHelpers = require('./helpers/handlebars-helpers')
 
 const app = express()
 const port = process.env.PORT || 3000
 const SESSION_SECRET = 'secret'
-app.engine('hbs', handlebars({ extname: '.hbs' }))
+app.engine('hbs', handlebars({ extname: '.hbs', helpers: handlebarsHelpers }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
@@ -18,6 +20,7 @@ app.use(flash())
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
   res.locals.error_messages = req.flash('error_messages')
+  res.locals.user = getUser(req)
   next()
 })
 app.use(routes)
