fdd8abe9464e4d23fd6946bc40401feac0bb4040
yaahsin <brillanteoboe@gmail.com>
2022-07-05 22:43:15 +0800

    feat: user model add is_admin

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const router = express.Router()
+
 const adminController = require('../../controllers/admin-controller')
-router.get('/restaurants', adminController.getRestaurants)
-router.use('/', (req, res) => res.redirect('/admin/restaurants'))
+const { authenticatedAdmin } = require('../../middleware/auth') // 引入 auth.js
+router.get('/restaurants', authenticatedAdmin, adminController.getRestaurants) // 修改這行，新增 authenticatedAdmin 參數
+router.get('', (req, res) => res.redirect('/admin/restaurants'))
 module.exports = router
