1825e36dfc8d2ff3545c3b520497c60a5de2103b
coldfish5630 <layan.schicksal@gmail.com>
2022-07-05 12:48:09 +0900

    feat: add admin page

---
+++ routes/modules/admin.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const router = express.Router()
+const adminController = require('../../controllers/admin-controllers')
+
+router.get('/restaurants', adminController.getRestaurants)
+router.get('/', (req, res) => res.redirect('/admin/restaurants'))
+
+module.exports = router
