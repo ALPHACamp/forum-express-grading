596c8d2776c9bdcb9231d69ad493cf85d559ae6d
AC-Galen <lottaie7359882@gmail.com>
2022-07-05 14:19:50 +0800

    feat: passport init & signin

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -7,25 +7,35 @@ const userController = {
     res.render('signup')
   },
   signUp: (req, res, next) => {
-    // 若兩次輸入密碼不同，建立一個Error 物件並拋出
     if (req.body.password !== req.body.passwordCheck) throw new Error('Passwords do no match!')
 
-    // 確認資料內有沒有一樣的email，若有就建立一個Error 物件並拋出
     User.findOne({ where: { email: req.body.email } })
       .then(user => {
         if (user) throw new Error('Email already exists!')
-        return bcrypt.hash(req.body.password, 10) // 前面加return，可讓這個promise resolve的值可傳到下一個.then 裡面，下一個.then內參數hash就會是加密後的密碼，此寫法可減少巢狀層級，讓程式碼比較好讀
+        return bcrypt.hash(req.body.password, 10)
       })
-      .then(hash => User.create({ // 上面錯誤都沒發生, 使用者資料寫入資料庫
+      .then(hash => User.create({
         name: req.body.name,
         email: req.body.email,
         password: hash
       }))
       .then(() => {
-        req.flash('success_message', '成功註冊帳號!') // 顯示成功訊息
+        req.flash('success_message', '成功註冊帳號!')
         res.redirect('/signin')
       })
-      .catch(err => next(err)) // 接住前面拋出的錯誤，呼叫專門處理錯誤的 middleware
+      .catch(err => next(err))
+  },
+  signInPage: (req, res) => {
+    res.render('signin')
+  },
+  signIn: (req, res) => {
+    req.flash('success_messages', '成功登入！')
+    res.redirect('/restaurants')
+  },
+  logout: (req, res) => {
+    req.flash('success_messages', '登出成功！')
+    req.logout()
+    res.redirect('/signin')
   }
 }
 module.exports = userController
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const { generalErrorHandler } = require('../middleware/error-handler')
 
 const admin = require('./modules/admin')
+const passport = require('../config/passport')
 
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
@@ -11,6 +12,11 @@ router.use('/admin', admin)
 
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
+
+router.get('/signin', userController.signInPage)
+router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn) // 注意是 post
+router.get('/logout', userController.logout)
+
 router.get('/restaurants', restController.getRestaurants)
 router.get('/', (req, res) => res.redirect('/restaurants'))
 
