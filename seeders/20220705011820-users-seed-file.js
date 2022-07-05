f443c7c4462738c0dc3d7aa69100e85576b0017d
dodouuu <joe30566@gmail.com>
2022-07-05 09:46:05 +0800

    feat: add seed data

---
+++ seeders/20220705011820-users-seed-file.js
@@ -0,0 +1,36 @@
+'use strict'
+const bcrypt = require('bcryptjs')
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('Users', [
+      { // 一次新增三筆資料
+        email: 'root@example.com',
+        password: await bcrypt.hash('12345678', 10),
+        is_admin: true,
+        name: 'root',
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        email: 'user1@example.com',
+        password: await bcrypt.hash('12345678', 10),
+        is_admin: false,
+        name: 'user1',
+        created_at: new Date(),
+        updated_at: new Date()
+      },
+      {
+        email: 'user2@example.com',
+        password: await bcrypt.hash('12345678', 10),
+        is_admin: false,
+        name: 'user2',
+        created_at: new Date(),
+        updated_at: new Date()
+      }
+    ], {})
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Users', null, {})
+  }
+}
