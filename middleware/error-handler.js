3902e1d77fa441c72dfa7c3ab728b22a4c4906f2
AC-Galen <lottaie7359882@gmail.com>
2022-07-05 22:34:58 +0800

    feat:modify admin restaurants page

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,9 +1,8 @@
 module.exports = {
   generalErrorHandler (err, req, res, next) {
-    // 先判斷err 是不是一個Error 物件
-    if (err instanceof Error) { // 是, Error物件會有的屬性name, message 用快閃訊息印出來給使用者看
+    if (err instanceof Error) {
       req.flash('error_messages', `${err.name}: ${err.message}`)
-    } else { // 否, 將Error 物件傳給下一個error handler
+    } else {
       req.flash('error_messages', `${err}`)
     }
     res.redirect('back')
