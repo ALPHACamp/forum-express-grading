e5469c0ea04f51d7380bb6223e16ddd726e4d7e7
Roger <winter30009@yahoo.com.tw>
2022-07-05 15:25:19 +0800

    feat: user model add is_admin

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
 const passport = require('../config/passport')
 const admin = require('./modules/admin')
+const { authenticated } = require('../middleware/auth')
 const { generalErrorHandler } = require('../middleware/error-handler')
 
 router.use('/admin', admin)
@@ -11,7 +12,7 @@ router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
 router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn) // 注意是 post
-router.get('/restaurants', restController.getRestaurants)
+router.get('/restaurants', authenticated, restController.getRestaurants)
 router.get('/', (req, res) => res.redirect('/restaurants'))
 router.use('/', generalErrorHandler)
 
