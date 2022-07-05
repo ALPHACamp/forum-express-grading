f2c4f459c1273b4c8a9d3c28e4d83a7523389b30
coldfish5630 <layan.schicksal@gmail.com>
2022-07-05 15:39:41 +0900

    feat: flash msg & signup verification

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -6,9 +6,15 @@ const userController = {
   signUpPage: (req, res) => {
     res.render('signup')
   },
-  signUp: (req, res) => {
-    bcrypt
-      .hash(req.body.password, 10)
+  signUp: (req, res, next) => {
+    if (req.body.password !== req.body.passwordCheck) {
+      throw new Error('Passwords do not match!')
+    }
+    User.findOne({ where: { email: req.body.email } })
+      .then(user => {
+        if (user) throw new Error('Email already exist')
+        return bcrypt.hash(req.body.password, 10)
+      })
       .then(hash =>
         User.create({
           name: req.body.name,
@@ -16,7 +22,11 @@ const userController = {
           password: hash
         })
       )
-      .then(() => res.redirect('/signin'))
+      .then(() => {
+        req.flash('success_messages', '成功註冊帳號')
+        res.redirect('/signin')
+      })
+      .catch(err => next(err))
   }
 }
 
