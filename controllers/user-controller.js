24544121abebafe596b8ee083f953327c4f44df3
Yunya Hsu <yunyahsu110@gmail.com>
2022-07-05 14:14:26 +0800

    feat: add register validation and show error messages

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -7,16 +7,25 @@ const userController = {
   signUpPage: (req, res) => { // 負責 render 註冊的頁面
     res.render('signup')
   },
-  signUp: (req, res) => { // 處理實際註冊行為
-    bcrypt.hash(req.body.password, 10)
+  signUp: (req, res, next) => { // 處理實際註冊行為
+    // 如果兩次輸入的密碼不同，就建立一個 Error 物件並拋出
+    if (req.body.password !== req.body.passwordCheck) throw new Error('Passwords do not match!') // 當此錯誤發生時，會跳出這個function
+
+    User.findOne({ where: { email: req.body.email } })
+      .then(user => {
+        if (user) throw new Error('Email already exists!') // 當此錯誤發生時，會跳出這個function
+        return bcrypt.hash(req.body.password, 10) // 這裡用return是為了讓 .then可以保持在同一層
+      })
       .then(hash => User.create({
         name: req.body.name,
         email: req.body.email,
         password: hash
       }))
       .then(() => {
-        res.render('/signin')
+        req.flash('success_message', '已成功註冊帳號，請登入後使用！')
+        res.redirect('/signin')
       })
+      .catch(err => next(err)) // next是指呼叫下一個middleware
   }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,8 @@ const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
 const admin = require('./modules/admin')
 
+const { generalErrorHandler } = require('../middleware/error-handler')
+
 router.use('/admin', admin)
 
 router.get('/signup', userController.signUpPage)
@@ -15,5 +17,7 @@ router.get('/restaurants', restController.getRestaurants) // 收到請求路徑�
 router.use('/', (req, res) => {
   res.redirect('/restaurants')
 })
+
+router.use('/', generalErrorHandler)
 
 module.exports = router
