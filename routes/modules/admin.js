a7b09a3a48e940fa6fc81e9b7e79361b052b2ea5
litangchen <chen.li.tang@gmail.com>
2022-07-05 09:14:02 +0800

    feat: add category CRUD page

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 const adminController = require('../../controllers/admin-controller')
+const categoryController = require('../../controllers/category-controller')
 const { authenticatedAdmin } = require('../../middleware/auth')
 const upload = require('../../middleware/multer')
 
@@ -14,5 +15,10 @@ router.get('/restaurants', authenticatedAdmin, adminController.getRestaurants)
 router.post('/restaurants', upload.single('image'), adminController.postRestaurant)
 router.patch('/users/:id', authenticatedAdmin, adminController.patchUser)
 router.get('/users', authenticatedAdmin, adminController.getUsers)
+router.get('/categories/:id', categoryController.getCategories)
+router.put('/categories/:id', categoryController.putCategory)
+router.delete('/categories/:id', categoryController.deleteCategory)
+router.get('/categories', categoryController.getCategories)
+router.post('/categories', categoryController.postCategory)
 router.get('/', (req, res) => res.redirect('/admin/restaurants'))
 module.exports = router
