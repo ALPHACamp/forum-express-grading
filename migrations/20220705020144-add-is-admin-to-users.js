abe4cfbd8436773dee0fb3834df4d89af2b5ce10
ChillDiver <yumingchang1991@gmail.com>
2022-07-05 10:21:03 +0800

    feat: add user/admin authentication middleware to route

---
+++ migrations/20220705020144-add-is-admin-to-users.js
@@ -0,0 +1,19 @@
+'use strict'
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('Users', 'is_admin', {
+      type: Sequelize.BOOLEAN,
+      defaultValue: false
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+  }
+}
