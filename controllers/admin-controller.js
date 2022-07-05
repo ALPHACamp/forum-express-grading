de6e0fbebec89291a9bf98c34f350142ceafc222
AlvinLee66 <ewqaz001@gmail.com>
2022-07-05 20:57:51 +0800

    feat: create restaurant

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -6,6 +6,26 @@ const adminController = {
     })
       .then(restaurants => res.render('admin/restaurants', { restaurants }))
       .catch(err => next(err))
+  },
+  createRestaurant: (req, res) => {
+    return res.render('admin/create-restaurant')
+  },
+  postRestaurant: (req, res, next) => {
+    const { name, tel, address, openingHours, description } = req.body
+    if (!name) throw new Error('餐廳名稱為必填欄位！')
+
+    Restaurant.create({
+      name,
+      tel,
+      address,
+      openingHours,
+      description
+    })
+      .then(() => {
+        req.flash('success_messages', '餐廳資訊建立成功！')
+        res.redirect('/admin/restaurants')
+      })
+      .catch(err => next(err))
   }
 }
 module.exports = adminController
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,10 +1,11 @@
 const express = require('express')
-const { authenticatedAdmin } = require('../../middleware/auth')
 const router = express.Router()
 
 const adminController = require('../../controllers/admin-controller')
 
-router.get('/restaurants', authenticatedAdmin, adminController.getRestaurants)
+router.get('/restaurants', adminController.getRestaurants)
+router.get('/restaurants/create', adminController.createRestaurant)
+router.post('/restaurants', adminController.postRestaurant)
 router.get('/', (req, res) => res.redirect('/admin/restaurants'))
 
 module.exports = router
