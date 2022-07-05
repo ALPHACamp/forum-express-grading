a588604ccd3f8342a47c7e5d6e4efcbe8456efca
yaahsin <brillanteoboe@gmail.com>
2022-07-05 23:03:23 +0800

    feat: admin restaurant page

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -27,6 +27,17 @@ const adminController = {
         res.redirect('/admin/restaurants')
       })
       .catch(err => next(err))
+  },
+  // 瀏覽單一餐廳
+  getRestaurant: (req, res, next) => {
+    Restaurant.findByPk(req.params.id, { // 去資料庫用 id 找一筆資料
+      raw: true // 找到以後整理格式再回傳
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!") //  如果找不到，回傳錯誤訊息，後面不執行
+        res.render('admin/restaurant', { restaurant })
+      })
+      .catch(err => next(err))
   }
 }
 
