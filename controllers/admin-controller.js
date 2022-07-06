0c0013f3b256a8776f188d445eda88d9863ed4ab
smile19439 <smile19439@gmail.com>
2022-07-06 21:57:32 +0800

    merge R01

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,4 +1,4 @@
-const { Restaurant } = require('../models')
+const { Restaurant, User } = require('../models')
 const { imgurFileHelper } = require('../helpers/file-helpers')
 
 const adminController = {
@@ -88,6 +88,41 @@ const adminController = {
         return restaurant.destroy()
       })
       .then(() => res.redirect('/admin/restaurants'))
+      .catch(err => next(err))
+  },
+  getUsers: (req, res, next) => {
+    return User.findAll({ raw: true })
+      .then(users => {
+        users.forEach(user => {
+          if (user.email === 'root@example.com') {
+            user.superuser = true
+          }
+          if (!!user.isAdmin === true) {
+            user.role = 'admin'
+          } else {
+            user.role = 'user'
+          }
+        })
+        res.render('admin/users', { users })
+      })
+      .catch(err => next(err))
+  },
+  patchUser: (req, res, next) => {
+    const id = req.params.id
+    return User.findByPk(id)
+      .then(user => {
+        if (user.email === 'root@example.com') {
+          req.flash('error_messages', '禁止變更 root 權限')
+          return res.redirect('back')
+        }
+        return user.update({
+          isAdmin: !user.isAdmin
+        })
+      })
+      .then(() => {
+        req.flash('success_messages', '使用者權限變更成功')
+        res.redirect('/admin/users')
+      })
       .catch(err => next(err))
   }
 }
