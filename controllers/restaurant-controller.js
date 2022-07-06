3fa623fa8b4b6badcf8b5d7618fcae2d49c24584
AndyChienn <andy2002chien@hotmail.com.tw>
2022-07-07 00:31:10 +0800

    feat: add restaurant page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,21 +1,37 @@
 const { Restaurant, Category } = require('../models')
 const restaurantController = {
-  getRestaurants: (req, res) => {
+  getRestaurants: (req, res, next) => {
     return Restaurant.findAll({
       include: Category,
       nest: true,
       raw: true
-    }).then(restaurants => {
-      const data = restaurants.map(restaurant => {
-        return {
-          ...restaurant,
-          description: restaurant.description.substring(0, 50)
-        }
-      })
-      return res.render('restaurants', {
-        restaurants: data
-      })
     })
+      .then(restaurants => {
+        const data = restaurants.map(restaurant => {
+          return {
+            ...restaurant,
+            description: restaurant.description.substring(0, 50)
+          }
+        })
+        return res.render('restaurants', {
+          restaurants: data
+        })
+      })
+      .catch(err => next(err))
+  },
+  getRestaurant: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, {
+      include: Category,
+      nest: true,
+      raw: true
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        return res.render('restaurant', {
+          restaurant
+        })
+      })
+      .catch(err => next(err))
   }
 }
 module.exports = restaurantController
