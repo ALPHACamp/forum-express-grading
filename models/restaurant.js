7e2a0544801b3f82abfa64737e0dfe47f2952795
june850713@gmail.com <june850713@gmail.com>
2022-07-06 15:22:44 +0800

    feat: modify admin restaurants page

--- models/restaurant.js
+++ models/restaurant.js
@@ -22,7 +22,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'Restaurant',
-    tableName: 'Restaurant',
+    tableName: 'Restaurants',
     underscored: true
   })
   return Restaurant
