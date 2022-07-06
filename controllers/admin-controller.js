7cefad138d0dafefff7f8a88b8126f8b2a66d91b
coldfish5630 <layan.schicksal@gmail.com>
2022-07-06 11:39:07 +0900

    feat: create restaurant

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,6 +1,27 @@
+const db = require('../models')
+const Restaurant = db.Restaurant
 const adminController = {
-  getRestaurants: (req, res) => {
-    return res.render('admin/restaurants')
+  getRestaurants: (req, res, next) => {
+    return Restaurant.findAll({ raw: true })
+      .then(restaurants => {
+        return res.render('admin/restaurants', { restaurants })
+      })
+      .catch(err => next(err))
+  },
+  createRestaurant: (req, res, next) => {
+    return res.render('admin/create-restaurant')
+  },
+  postRestaurant: (req, res, next) => {
+    const { name, tel, address, openingHours, description } = req.body
+    if (!name) {
+      throw new Error('Restaurant name is required')
+    }
+    Restaurant.create({ name, tel, address, openingHours, description })
+      .then(() => {
+        req.flash('success_messages', 'restaurant was successfully created')
+        res.redirect('/admin/restaurants')
+      })
+      .catch(err => next(err))
   }
 }
 
