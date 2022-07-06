0ac509835ace17260788ee4287a87f8de61d7bcd
JasonChou <swchou524@gmail.com>
2022-07-06 09:55:08 +0800

    feat: add signup

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,21 @@
 const express = require('express')
-
 const router = express.Router()
+const admin = require('./modules/admin')
 
 const restController = require('../controllers/restaurant-controller')
-const admin = require('./modules/admin')
+const userController = require('../controllers/user-controller')
+
+const { generalErrorHandler } = require('../middleware/error-handler')
 
 router.use('/admin', admin)
 
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
+
 router.get('/restaurants', restController.getRestaurants)
 
-router.use('/', (req, res) => res.redirect('/restaurants'))
+router.get('/', (req, res) => res.redirect('/restaurants'))
+
+router.use('/', generalErrorHandler)
 
 module.exports = router
