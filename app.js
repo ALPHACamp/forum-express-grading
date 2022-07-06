6ad1f187a932f6d67f515b8a9c8958602e5e8169
JasonChou <swchou524@gmail.com>
2022-07-06 10:45:54 +0800

    feat: passport init & signin

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const handlebars = require('express-handlebars')
 const flash = require('connect-flash')
 const session = require('express-session')
+const passport = require('./config/passport')
 const routes = require('./routes')
 
 const app = express()
@@ -16,7 +17,10 @@ app.use(express.urlencoded({ extended: true }))
 app.use(
   session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false })
 )
-app.use(flash()) // 掛載套件
+app.use(passport.initialize())
+app.use(passport.session())
+
+app.use(flash())
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages') // 設定 success_msg 訊息
   res.locals.error_messages = req.flash('error_messages') // 設定 warning_msg 訊息
