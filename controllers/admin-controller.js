e67e83d00d8f1d752f7ca36c8e8d69bb5ce16113
Yunya Hsu <yunyahsu110@gmail.com>
2022-07-06 13:45:49 +0800

    feat: add delete feature

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -69,6 +69,15 @@ const adminController = {
         res.redirect('/admin/restaurants')
       })
       .catch(err => next(err))
+  },
+  deleteRestaurant: (req, res, next) => {
+    Restaurant.findByPk(req.params.id)
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        return restaurant.destroy()
+      })
+      .then(() => res.redirect('/admin/restaurants'))
+      .catch(err => next(err))
   }
 }
 
