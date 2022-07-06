1bc794b270bc0bb680cc70059d7b0a5e62ed01e8
ChillDiver <yumingchang1991@gmail.com>
2022-07-06 15:18:13 +0800

    feat: add user authorization (R01)

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,4 +1,4 @@
-const { Restaurant } = require('../models')
+const { Restaurant, User } = require('../models')
 const { imgurFileHandler } = require('../helpers/file-helpers')
 
 const adminController = {
@@ -87,6 +87,34 @@ const adminController = {
       })
       .then(() => res.redirect('/admin/restaurants'))
       .catch(err => next(err))
+  },
+  getUsers: (req, res, next) => {
+    return User
+      .findAll({
+        raw: true
+      })
+      .then(users => {
+        return res.render('admin/users', { users })
+      })
+      .catch(err => next(err))
+  },
+  patchUser: (req, res) => {
+    return User
+      .findByPk(req.params.id)
+      .then(user => {
+        if (user.email === 'root@example.com') {
+          req.flash('error_messages', '禁止變更 root 權限')
+          return res.redirect('back')
+        }
+        user
+          .update({
+            isAdmin: !user.isAdmin
+          })
+          .then(() => {
+            req.flash('success_messages', '使用者權限變更成功')
+            return res.redirect('/admin/users')
+          })
+      })
   }
 }
 
