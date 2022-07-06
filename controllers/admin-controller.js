8808ccbd328b514674fad40cdb388ea6e3d7bba9
AC-Galen <lottaie7359882@gmail.com>
2022-07-06 16:21:55 +0800

    feat: delete restaurant

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,4 +1,3 @@
-const { restart } = require('nodemon')
 const { Restaurant } = require('../models')
 
 const adminController = {
@@ -67,6 +66,15 @@ const adminController = {
         req.flash('success_messages', 'restaurant was successfully to update')
         res.redirect('/admin/restaurants')
       })
+      .catch(err => next(err))
+  },
+  deleteRestaurant: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id)
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist ")
+        return restaurant.destroy()
+      })
+      .then(() => res.redirect('/admin/restaurants'))
       .catch(err => next(err))
   }
 }
