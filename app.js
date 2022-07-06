f8ee26c4ffb1a9cbc434d632ba9323071ba2190b
JasonChou <swchou524@gmail.com>
2022-07-06 23:10:30 +0800

    feat: admin update restaurant

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
 const flash = require('connect-flash')
+const methodOverride = require('method-override')
 const session = require('express-session')
+
 const passport = require('./config/passport')
 const handlebarsHelpers = require('./helpers/handlebars-helpers')
 const { getUser } = require('./helpers/auth-helpers')
@@ -23,6 +25,8 @@ app.use(passport.initialize())
 app.use(passport.session())
 
 app.use(flash())
+app.use(methodOverride('_method'))
+
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages') // 設定 success_msg 訊息
   res.locals.error_messages = req.flash('error_messages') // 設定 warning_msg 訊息
