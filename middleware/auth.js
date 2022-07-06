c6de51474eba179162564b92e9498ccee0c371f8
Rita Chien <ritachien929@gmail.com>
2022-07-06 09:35:37 +0800

    refactor: adjust middleware/auth.js for R01 test

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,16 +1,16 @@
 // Include modules
-const { ensureAuthenticated, getUser } = require('../helpers/auth-helpers')
+const helpers = require('../helpers/auth-helpers')
 
 // Auth for normal user router
 const authenticated = (req, res, next) => {
-  if (ensureAuthenticated(req)) return next()
+  if (helpers.ensureAuthenticated(req)) return next()
   res.redirect('/signin')
 }
 
 // Auth for admin router
 const authenticatedAdmin = (req, res, next) => {
-  if (ensureAuthenticated(req)) {
-    if (getUser(req).isAdmin) return next()
+  if (helpers.ensureAuthenticated(req)) {
+    if (helpers.getUser(req).isAdmin) return next()
     res.redirect('/')
   } else {
     res.redirect('/signin')
