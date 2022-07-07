e2a41b2db5abf53e3d8e13c175ff9de2b59d3120
chh817 <chh817@gmail.com>
2022-07-07 21:53:56 +0800

    feat: add seed data

---
+++ seeders/20220707135055-restaurants-seed-file.js
@@ -0,0 +1,23 @@
+'use strict'
+const faker = require('faker')
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('Restaurants',
+      Array.from({ length: 50 }, () => ({
+        name: faker.name.findName(),
+        tel: faker.phone.phoneNumber(),
+        address: faker.address.streetAddress(),
+        opening_hours: '08:00',
+        image: `https://loremflickr.com/320/240/restaurant,food/?random=${Math.random() * 100}`,
+        description: faker.lorem.text(),
+        created_at: new Date(),
+        updated_at: new Date()
+      }))
+    )
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Restaurants', null, {})
+  }
+}
