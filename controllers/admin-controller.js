3b301c1f75ce1a001525bc9ba48bd09cb8157d9a
cincinfish <96610786+cincinfish@users.noreply.github.com>
2022-07-07 09:30:10 +0800

    feat: delete restaurant

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -68,6 +68,18 @@ const adminController = {
         res.redirect('/admin/restaurants')
       })
       .catch(err => next(err))
+  },
+  deleteRestaurant: (req, res, next) => {
+    Restaurant.findByPk(req.params.id)
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        return restaurant.destroy()
+      })
+      .then(() => {
+        req.flash('success_messages', 'restaurant was successfully to delete')
+        res.redirect('/admin/restaurants')
+      })
+      .catch(err => next(err))
   }
 }
 
