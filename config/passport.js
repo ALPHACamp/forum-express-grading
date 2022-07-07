840065ff88cf404bf6d4decae9a357ca20380d8f
cincinfish <96610786+cincinfish@users.noreply.github.com>
2022-07-07 08:37:14 +0800

    fix: password error can login

--- config/passport.js
+++ config/passport.js
@@ -17,9 +17,11 @@ passport.use(new LocalStrategy(
       .then(user => {
         if (!user) return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤！'))
         bcrypt.compare(password, user.password).then(res => {
-          if (!res) return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤！'))
+          if (!res) {
+            return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤！'))
+          }
+          return cb(null, user)
         })
-        return cb(null, user)
       })
   }
 ))
