b4b8038cef87775a9a0eb30c777c2d2f20989803
AdrieneTZ <tznali17@gmail.com>
2022-07-07 16:46:56 +0800

    feat: add imgur api

--- helpers/file-helpers.js
+++ helpers/file-helpers.js
@@ -1,4 +1,9 @@
 const fs = require('fs')
+const imgur = require('imgur')
+const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
+imgur.setClientId(IMGUR_CLIENT_ID)
+
+const { resolve } = require('path')
 
 const localFileHandler = (file) => {
   return new Promise((resolve, reject) => {
@@ -13,6 +18,23 @@ const localFileHandler = (file) => {
   })
 }
 
+const imgurFileHandler = (file) => {
+  return new Promise((resolve, reject) => {
+    if (!file) return resolve(null)
+
+    return imgur
+      .uploadFile(file.path)
+      .then((img) => {
+        console.log(img)
+        // check if Object img exists?
+        // Yes >> take out property link
+        resolve(img?.link || null)
+      })
+      .catch((error) => reject(error))
+  })
+}
+
 module.exports = {
   localFileHandler,
+  imgurFileHandler,
 }
