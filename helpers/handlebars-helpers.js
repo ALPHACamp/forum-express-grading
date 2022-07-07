f3acf1f1d2dbc539b3be8c7c8e58a1d0792a8c70
gene.31985906@gmail.com <gene.31985906@gmail.com>
2022-07-07 23:13:08 +0800

    feat:update to Heroku

--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -1,5 +1,8 @@
 const dayjs = require('dayjs') // 載入 dayjs 套件
 module.exports = {
   // 取得當年年份作為 currentYear 的屬性值，並導出
-  currentYear: () => dayjs().year()
+  currentYear: () => dayjs().year(),
+  checkAdmin: function (isAdmin) {
+    return isAdmin === 1 ? 'set as user' : 'set ad admin'
+  }
 }
