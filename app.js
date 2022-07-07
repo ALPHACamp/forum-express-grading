5a75c219c2118246057fe24a636072ecbb361050
gene.31985906@gmail.com <gene.31985906@gmail.com>
2022-07-07 14:42:06 +0800

    feat: add imgur api

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const session = require('express-session')
 const passport = require('./config/passport')
 const handlebarsHelpers = require('./helpers/handlebars-helpers')
 const { getUser } = require('./helpers/auth-helpers')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const app = express()
 const port = process.env.PORT || 3000
 const SESSION_SECRET = 'secret'
--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,5 +1,6 @@
 const { Restaurant } = require('../models')
 const { localFileHandler } = require('../helpers/file-helpers')
+const { imgurFileHandler } = require('../helpers/file-helpers')
 const adminController = {
   getRestaurants: (req, res, next) => {
     Restaurant.findAll({
@@ -15,7 +16,7 @@ const adminController = {
     const { name, tel, address, openingHours, description } = req.body
     if (!name) throw new Error('Restaurant name is required!')
     const { file } = req
-    localFileHandler(file)
+    imgurFileHandler(file)
       .then(filePath =>
         Restaurant.create({
           name,
@@ -57,7 +58,7 @@ const adminController = {
     const { file } = req
     Promise.all([
       Restaurant.findByPk(req.params.id),
-      localFileHandler(file)
+      imgurFileHandler(file)
     ])
       .then(([restaurant, filePath]) => {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
