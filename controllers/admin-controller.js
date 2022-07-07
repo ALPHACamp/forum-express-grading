7da1306fed0a15f18bfb709de0d12af9b43205cd
yongsin0129 <yongsin0129@gmail.com>
2022-07-07 20:20:25 +0800

    feat: add restaurant image (by multer)

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,6 +1,7 @@
 const { Restaurant } = require('../models')
-const adminController = {
+const { localFileHandler } = require('../helpers/file-helpers')
 
+const adminController = {
   getRestaurants: (req, res, next) => {
     Restaurant.findAll({
       raw: true
@@ -9,27 +10,37 @@ const adminController = {
       .catch(err => next(err))
   },
 
-  createRestaurant: (req, res) => { return res.render('admin/create-restaurant') },
+  createRestaurant: (req, res) => {
+    return res.render('admin/create-restaurant')
+  },
 
   postRestaurant: (req, res, next) => {
-    const { name, tel, address, openingHours, description } = req.body // 從 req.body 拿出表單裡的資料
-    if (!name) throw new Error('Restaurant name is required!') // name 是必填，若發先是空值就會終止程式碼，並在畫面顯示錯誤提示
-    Restaurant.create({ // 產生一個新的 Restaurant 物件實例，並存入資料庫
-      name,
-      tel,
-      address,
-      openingHours,
-      description
-    })
+    const { name, tel, address, openingHours, description } = req.body
+    if (!name) throw new Error('Restaurant name is required!')
+    //  修改以下
+    const { file } = req // 把檔案取出來，也可以寫成 const file = req.file
+    localFileHandler(file) // 把取出的檔案傳給 file-helper 處理後
+      .then(filePath =>
+        Restaurant.create({
+          // 再 create 這筆餐廳資料
+          name,
+          tel,
+          address,
+          openingHours,
+          description,
+          image: filePath || null
+        })
+      )
       .then(() => {
-        req.flash('success_messages', 'restaurant was successfully created') // 在畫面顯示成功提示
-        res.redirect('/admin/restaurants') // 新增完成後導回後台首頁
+        req.flash('success_messages', 'restaurant was successfully created')
+        res.redirect('/admin/restaurants')
       })
       .catch(err => next(err))
   },
 
   getRestaurant: (req, res, next) => {
-    Restaurant.findByPk(req.params.id, { // 去資料庫用 id 找一筆資料
+    Restaurant.findByPk(req.params.id, {
+      // 去資料庫用 id 找一筆資料
       raw: true // 找到以後整理格式再回傳
     })
       .then(restaurant => {
@@ -39,7 +50,8 @@ const adminController = {
       .catch(err => next(err))
   },
 
-  editRestaurant: (req, res, next) => { // 新增這段
+  editRestaurant: (req, res, next) => {
+    // 新增這段
     Restaurant.findByPk(req.params.id, {
       raw: true
     })
@@ -53,15 +65,23 @@ const adminController = {
   putRestaurant: (req, res, next) => {
     const { name, tel, address, openingHours, description } = req.body
     if (!name) throw new Error('Restaurant name is required!')
-    Restaurant.findByPk(req.params.id)
-      .then(restaurant => {
+    const { file } = req // 把檔案取出來
+    Promise.all([
+      // 非同步處理
+      Restaurant.findByPk(req.params.id), // 去資料庫查有沒有這間餐廳
+      localFileHandler(file) // 把檔案傳到 file-helper 處理
+    ])
+      .then(([restaurant, filePath]) => {
+        // 以上兩樣事都做完以後
         if (!restaurant) throw new Error("Restaurant didn't exist!")
         return restaurant.update({
+          // 修改這筆資料
           name,
           tel,
           address,
           openingHours,
-          description
+          description,
+          image: filePath || restaurant.image // 如果 filePath 是 Truthy (使用者有上傳新照片) 就用 filePath，是 Falsy (使用者沒有上傳新照片) 就沿用原本資料庫內的值
         })
       })
       .then(() => {
@@ -71,7 +91,8 @@ const adminController = {
       .catch(err => next(err))
   },
 
-  deleteRestaurant: (req, res, next) => { // 新增以下
+  deleteRestaurant: (req, res, next) => {
+    // 新增以下
     return Restaurant.findByPk(req.params.id)
       .then(restaurant => {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
@@ -80,6 +101,5 @@ const adminController = {
       .then(() => res.redirect('/admin/restaurants'))
       .catch(err => next(err))
   }
-
 }
 module.exports = adminController
--- models/restaurant.js
+++ models/restaurant.js
@@ -1,7 +1,5 @@
 'use strict'
-const {
-  Model
-} = require('sequelize')
+const { Model } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
   class Restaurant extends Model {
     /**
@@ -12,18 +10,22 @@ module.exports = (sequelize, DataTypes) => {
     static associate (models) {
       // define association here
     }
-  };
-  Restaurant.init({
-    name: DataTypes.STRING,
-    tel: DataTypes.STRING,
-    address: DataTypes.STRING,
-    openingHours: DataTypes.STRING,
-    description: DataTypes.TEXT
-  }, {
-    sequelize,
-    modelName: 'Restaurant',
-    tableName: 'Restaurants',
-    underscored: true
-  })
+  }
+  Restaurant.init(
+    {
+      name: DataTypes.STRING,
+      tel: DataTypes.STRING,
+      address: DataTypes.STRING,
+      openingHours: DataTypes.STRING,
+      description: DataTypes.TEXT,
+      image: DataTypes.STRING
+    },
+    {
+      sequelize,
+      modelName: 'Restaurant',
+      tableName: 'Restaurants',
+      underscored: true
+    }
+  )
   return Restaurant
 }
