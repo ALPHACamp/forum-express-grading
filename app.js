ec50365b6e6e00147b09142475962aeb8626fdb4
AdrieneTZ <tznali17@gmail.com>
2022-07-07 09:31:26 +0800

    feat: add restaurant image

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path')
 const express = require('express')
 const handlebars = require('express-handlebars')
 const flash = require('connect-flash')
@@ -25,6 +26,7 @@ app.use(passport.initialize()) // init passport
 app.use(passport.session()) // turn on session function
 app.use(flash())
 app.use(methodOverride('_method'))
+app.use('/upload', express.static(path.join(__dirname, 'upload')))
 
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
