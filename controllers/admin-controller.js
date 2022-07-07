0792f4d400283d1b22d56d2c132965a2ef53aa0d
Jason Chan <jasonchan19981017@hotmail.com>
2022-07-07 22:31:56 +0800

    feat: create restaurant

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -5,6 +5,24 @@ const adminController = {
     Restaurant.findAll({ raw: true })
       .then(restaurants => res.render('admin/restaurants', { restaurants }))
       .catch(err => next(err))
+  },
+  createRestaurant: (req, res) => {
+    return res.render('admin/create-restaurant')
+  },
+  postRestaurant: (req, res, next) => {
+    const { name, tel, address, openingHours, description } = req.body
+    if (!name) throw new Error('Restaurant name is required!')
+
+    Restaurant.create({
+      name,
+      tel,
+      address,
+      openingHours,
+      description
+    }).then(() => {
+      req.flash('success_messages', 'You have created a restaurant!')
+      res.render('/admin/restaurants')
+    }).catch(err => { throw new Error(err) })
   }
 }
 
--- middleware/auth.js
+++ middleware/auth.js
@@ -9,8 +9,7 @@ const authenticated = (req, res, next) => {
 
 const authenticatedAdmin = (req, res, next) => {
   if (ensureAuthenticated(req)) {
-    if (getUser.isAdmin) return next()
-
+    if (getUser(req).isAdmin) return next()
     res.redirect('/')
   } else {
     res.redirect('/signin')
