e0d23e377e04a33063dbd9f8cc3838f14cab4a17
Yunya Hsu <yunyahsu110@gmail.com>
2022-07-07 18:15:33 +0800

    feat: merge R01

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,15 +1,15 @@
-const { getUser, ensureAuthenticated } = require('../helpers/auth-helpers')
+const helpers = require('../helpers/auth-helpers')
 
 const authenticated = (req, res, next) => {
-  if (ensureAuthenticated(req)) { // 如果有登入，繼續
+  if (helpers.ensureAuthenticated(req)) { // 如果有登入，繼續
     return next()
   }
   res.redirect('/signin') // 如果沒登入，丟回登入頁
 }
 
 const authenticatedAdmin = (req, res, next) => {
-  if (ensureAuthenticated(req)) { // 如果有登入，繼續
-    if (getUser(req).isAdmin) { // 如果有登入且為admin，繼續
+  if (helpers.ensureAuthenticated(req)) { // 如果有登入，繼續
+    if (helpers.getUser(req).isAdmin) { // 如果有登入且為admin，繼續
       return next()
     }
     res.redirect('/') // 如果有登入但不是admin，丟回首頁
