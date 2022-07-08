2e65e4a33a15c9c4fccdb5a15588dfda3a69e342
chh817 <chh817@gmail.com>
2022-07-08 14:07:40 +0800

    feat: admin users page
    fix: set as admin bug for root
    
    fix: error & success message in admin-controller
    
    fix: forum.users doesn't exist
    
    feat: admin users page

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -12,7 +12,6 @@ const userController = {
     User.findOne({ where: { email: req.body.email } })
       .then(user => {
         if (user) throw new Error('Email already exists!')
-
         return bcrypt.hash(req.body.password, 10)
       })
       .then(hash => User.create({
