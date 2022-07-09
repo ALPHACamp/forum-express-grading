fe3cefaf2c666c18c2be555f47b8a055a4c3d524
wuwac <wuwachon44@gmail.com>
2022-07-09 13:13:26 +0800

    feat: add admin/users page and routes

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,4 +1,4 @@
-const { Restaurant } = require('../models')
+const { Restaurant, User } = require('../models')
 const { imgurFileHandler } = require('../helpers/file-helpers')
 
 const adminController = {
@@ -79,6 +79,58 @@ const adminController = {
       if (!restaurant) throw new Error('Restaurant not exist!')
       await restaurant.destroy()
       res.redirect('/admin/restaurants')
+    } catch (err) {
+      next(err)
+    }
+  },
+  getUsers: async (req, res, next) => {
+    try {
+      const users = await User.findAll({ raw: true })
+      users.map(user => {
+        if (user.isAdmin) {
+          user.role = 'admin'
+          user.roleSwitch = 'set as user'
+          return user
+        }
+        user.role = 'user'
+        user.roleSwitch = 'set as admin'
+        return user
+      })
+      res.render('admin/users', { users })
+    } catch (err) {
+      next(err)
+    }
+  },
+  createUser: (req, res) => {
+    res.render('admin/create-user')
+  },
+  postUser: async (req, res, next) => {
+    try {
+      const { name, email, password } = req.body
+      if (!name || !email || !password) throw new Error('All fields are required!')
+      const isAdmin = !!req.body.isAdmin
+      await User.create({
+        name,
+        email,
+        password,
+        isAdmin
+      })
+      res.redirect('/admin/users')
+    } catch (err) {
+      next(err)
+    }
+  },
+  patchUser: async (req, res, next) => {
+    try {
+      const user = await User.findByPk(req.params.id)
+      if (!user) throw new Error('User not exist!')
+      if (user.email === 'root@example.com' && user.isAdmin) {
+        req.flash('error_messages', '禁止變更 root 權限')
+        return res.redirect('back')
+      }
+      await user.update({ isAdmin: !user.isAdmin })
+      req.flash('success_messages', '使用者權限變更成功')
+      return res.redirect('/admin/users')
     } catch (err) {
       next(err)
     }
