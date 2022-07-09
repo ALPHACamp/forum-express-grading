0b5d29a23391a8b94d915d4ac15f31ade36c17fb
wuwac <wuwachon44@gmail.com>
2022-07-09 17:37:52 +0800

    feat: merge R01 branch into main

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,12 +1,12 @@
-const { ensureAuthenticated, getUser } = require('../helpers/auth-helpers')
+const helpers = require('../helpers/auth-helpers')
 
 const authenticated = (req, res, next) => {
-  if (ensureAuthenticated(req)) return next()
+  if (helpers.ensureAuthenticated(req)) return next()
   res.redirect('/signin')
 }
 const authenticateAdmin = (req, res, next) => {
-  if (ensureAuthenticated(req)) {
-    if (getUser(req).isAdmin) return next()
+  if (helpers.ensureAuthenticated(req)) {
+    if (helpers.getUser(req).isAdmin) return next()
     return res.redirect('/')
   }
   res.redirect('/signin')
