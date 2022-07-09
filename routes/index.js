fdc5cd45bb0f536ead8867da5cbb4aadb1f3976e
SnowWuChiKuo <x9x9x8x8x7x7x6x6x@gmail.com>
2022-07-09 14:29:48 +0800

    feat: create restaurant

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,21 @@ const express = require('express')
 const router = express.Router()
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
-const { authenticated } = require('../middleware/auth')
+const { authenticated, authenticatedAdmin } = require('../middleware/auth')
 const { generalErrorHandler } = require('../middleware/error-handler')
 const passport = require('../config/passport')
 const admin = require('./modules/admin')
 
-router.use('/admin', admin)
+router.use('/admin', authenticatedAdmin, admin)
+
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
+
 router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
+
 router.get('/logout', userController.logout)
+
 router.get('/restaurants', authenticated, restController.getRestaurants)
 
 router.get('/', (req, res) => res.redirect('/restaurants'))
