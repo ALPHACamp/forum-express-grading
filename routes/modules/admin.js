97a03ed70d955e562ca88ee0bae6605b56b1045b
SnowWuChiKuo <x9x9x8x8x7x7x6x6x@gmail.com>
2022-07-10 21:25:30 +0800

    git rebase squash

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -8,9 +8,10 @@ router.get('/restaurants/:id/edit', adminController.editRestaurant)
 router.get('/restaurants/:id', adminController.getRestaurant)
 router.put('/restaurants/:id', upload.single('image'), adminController.putRestaurant)
 router.delete('/restaurants/:id', adminController.deleteRestaurant)
-router.get('/restaurants', adminController.getRestaurants)
 router.post('/restaurants', upload.single('image'), adminController.postRestaurant)
-
+router.get('/restaurants', adminController.getRestaurants)
+router.patch('/users/:id', adminController.patchUser)
+router.get('/users', adminController.getUsers)
 router.use('/', (req, res) => res.redirect('/admin/restaurants'))
 
 module.exports = router
