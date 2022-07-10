1c45b42af30756bdd5af283e25f6262ae42c4eae
Roger <winter30009@yahoo.com.tw>
2022-07-10 10:51:06 +0800

    feat: category update

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -14,6 +14,8 @@ router.get('/restaurants', adminController.getRestaurants)
 router.post('/restaurants', upload.single('image'), adminController.postRestaurant)
 router.get('/users', adminController.getUsers)
 
+router.get('/categories/:id', categoryController.getCategories)
+router.put('/categories/:id', categoryController.putCategory)
 router.get('/categories', categoryController.getCategories)
 router.post('/categories', categoryController.postCategory)
 router.get('/', (req, res) => res.redirect('/admin/restaurants'))
