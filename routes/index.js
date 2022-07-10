9393cc24f43b8630b37a6120028a384224260e61
AndyChienn <andy2002chien@hotmail.com.tw>
2022-07-11 02:10:40 +0800

    feat: passport init & signin

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,17 @@
 const express = require('express')
 const router = express.Router()
+const passport = require('../config/passport')
+const admin = require('./modules/admin') // 新增這行，載入 admin.js
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
 const { generalErrorHandler } = require('../middleware/error-handler')
-const admin = require('./modules/admin') // 新增這行，載入 admin.js
+
 router.use('/admin', admin) // 新增這行
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp) // 注意用 post
-
+router.get('/signin', userController.signInPage)
+router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn) // 注意是 post
+router.get('/logout', userController.logout)
 router.get('/restaurants', restController.getRestaurants)
 
 router.use('/', (req, res) => res.redirect('/restaurants'))
