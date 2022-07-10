9d9ea54ed27735839bf74dadd8738f163ea67329
singingw <op0408io1998@gmail.com>
2022-07-10 23:08:57 +0800

    R02

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -17,15 +17,26 @@ const restaurantController = {
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
-      include: Category, // 拿出關聯的 Category model
+      include: Category
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        return restaurant.increment('viewCount')
+      })
+      .then(restaurant => {
+        res.render('restaurant', { restaurant: restaurant.toJSON() })
+      })
+      .catch(err => next(err))
+  },
+  getDashboard: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, {
+      include: Category,
       nest: true,
       raw: true
     })
       .then(restaurant => {
-        if (!restaurant) throw new Error("Restaurant didn't exist!")
-        res.render('restaurant', {
-          restaurant
-        })
+        if (!restaurant) throw new Error("Restaurant doesn't exist!")
+        res.render('dashboard', { restaurant })
       })
       .catch(err => next(err))
   }
