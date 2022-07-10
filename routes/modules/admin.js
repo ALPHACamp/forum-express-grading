0d9e26cde908c108493a843d28866d319c08ea6d
SnowWuChiKuo <x9x9x8x8x7x7x6x6x@gmail.com>
2022-07-10 19:41:08 +0800

    feat : add update admin

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -10,6 +10,7 @@ router.put('/restaurants/:id', upload.single('image'), adminController.putRestau
 router.delete('/restaurants/:id', adminController.deleteRestaurant)
 router.post('/restaurants', upload.single('image'), adminController.postRestaurant)
 router.get('/restaurants', adminController.getRestaurants)
+router.patch('/users/:id', adminController.patchUser)
 router.get('/users', adminController.getUsers)
 router.use('/', (req, res) => res.redirect('/admin/restaurants'))
 
