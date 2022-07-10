0eda80b814b28e2d165d1938c542874063868e31
rubylo718 <rubylo718@gmail.com>
2022-07-10 15:20:55 +0800

    merge branch R01 into main

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,4 +1,4 @@
-const { Restaurant } = require('../models')
+const { Restaurant, User } = require('../models')
 // const { localFileHandler } = require('../helpers/file-helpers')
 const { imgurFileHandler } = require('../helpers/file-helpers')
 
@@ -85,6 +85,39 @@ const adminController = {
       })
       .then(() => res.redirect('/admin/restaurants'))
       .catch(err => next(err))
+  },
+  getUsers: async (req, res, next) => {
+    try {
+      const users = await User.findAll({ raw: true })
+      users.map(user => {
+        if (user.isAdmin) {
+          user.role = 'Admin'
+          user.roleSwitch = 'Set as user'
+        } else {
+          user.role = 'User'
+          user.roleSwitch = 'Set as admin'
+        }
+        return user
+      })
+      res.render('admin/users', { users })
+    } catch (error) {
+      next(error)
+    }
+  },
+  patchUser: async (req, res, next) => {
+    try {
+      const user = await User.findByPk(req.params.id)
+      if (user.email === 'root@example.com') {
+        req.flash('error_messages', '禁止變更 root 權限')
+        return res.redirect('back')
+      } else {
+        await user.update({ isAdmin: !user.isAdmin })
+        req.flash('success_messages', '使用者權限變更成功')
+        return res.redirect('/admin/users')
+      }
+    } catch (error) {
+      next(error)
+    }
   }
 }
 module.exports = adminController
