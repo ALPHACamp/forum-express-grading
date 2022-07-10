d18f21049d7f8ea1d90b3cf0c3e138fd0f5137d0
Roger <winter30009@yahoo.com.tw>
2022-07-10 15:47:32 +0800

    R02 done

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -19,12 +19,27 @@ const restaurantController = {
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
       include: Category, // 拿出關聯的 Category model
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        return restaurant.increment('view_counts')
+      })
+      .then(restaurant => {
+        res.render('restaurant', {
+          restaurant: restaurant.toJSON()
+        })
+      })
+      .catch(err => next(err))
+  },
+  getDashboard: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, {
+      include: Category, // 拿出關聯的 Category model
       nest: true,
       raw: true
     })
       .then(restaurant => {
-        if (!restaurant) throw new Error("Restaurant didn't exist!")
-        res.render('restaurant', {
+        if (!restaurant) throw new Error("Restaurant doesn't exist!")
+        res.render('dashboard', {
           restaurant
         })
       })
