73685ebd7a07faafd3a3f056a9af200859311b36
cincinfish <96610786+cincinfish@users.noreply.github.com>
2022-07-10 10:24:33 +0800

    feat: add user page & super admin function

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,5 +1,6 @@
 const { imgurFileHandler } = require('../helpers/file-helpers')
 const { Restaurant } = require('../models')
+const { User } = require('../models')
 
 const adminController = {
   getRestaurants: (req, res, next) => {
@@ -89,7 +90,34 @@ const adminController = {
         res.redirect('/admin/restaurants')
       })
       .catch(err => next(err))
+  },
+  getUsers: (req, res, next) => {
+    return User.findAll({
+      raw: true
+    })
+      .then(users => {
+        res.render('admin/users', { users })
+      })
+      .catch(err => next(err))
+  },
+  patchUser: async (req, res, next) => {
+    return User.findByPk(req.params.id)
+      .then(user => {
+        if (!user) throw new Error("User didn't exist!")
+
+        if (user.get('email') === process.env.SUDO_EMAIL) {
+          req.flash('error_messages', '禁止變更 root 權限')
+          return res.redirect('back')
+        }
+        return user.update({ isAdmin: !user.isAdmin })
+      })
+      .then(() => {
+        req.flash('success_messages', '使用者權限變更成功')
+        return res.redirect('/admin/users')
+      })
+      .catch(err => next(err))
   }
+
 }
 
 module.exports = adminController
