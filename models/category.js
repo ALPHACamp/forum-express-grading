e8c1559302156c81385af9878246480749bb03d1
JasonChou <swchou524@gmail.com>
2022-07-11 21:09:02 +0800

    feat: show category on admin restaurant pages

--- models/category.js
+++ models/category.js
@@ -18,7 +18,7 @@ module.exports = (sequelize, DataTypes) => {
     {
       sequelize,
       modelName: 'Category',
-      tableName: 'Categorise',
+      tableName: 'Categories',
       underscored: true
     }
   )
