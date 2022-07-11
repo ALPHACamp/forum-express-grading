dbdbc22ec1a3d7b682124902c111ee61d6bd517a
yaahsin <brillanteoboe@gmail.com>
2022-07-11 17:14:55 +0800

    feat: category delete_v1

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -1,4 +1,4 @@
-const { Category } = require('../models')
+const { Category, Restaurant } = require('../models')
 
 const categoryController = {
   getCategories: (req, res, next) => {
@@ -37,33 +37,28 @@ const categoryController = {
         res.redirect('/admin/categories')
       })
       .catch(err => next(err))
+  },
+  deleteCategory: (req, res, next) => {
+    // 檢查該分類是否已被使用
+    return Promise.all([
+      Restaurant.findOne({
+        attributes: ['category_id'],
+        where: { category_id: req.params.id },
+        raw: true
+      }),
+      Category.findByPk(req.params.id)
+    ])
+      .then(([restaurants, category]) => {
+        if (!category) throw new Error("Category didn't exist!")
+        // 若為是，則不刪除，將名稱改為未分類
+        if (restaurants) {
+          return category.update({ name: '(未分類)' })
+        } else {
+          return category.destroy()
+        }
+      })
+      .then(() => res.redirect('/admin/categories'))
+      .catch(err => next(err))
   }
 }
 module.exports = categoryController
-
-// 寫入資料
-
-//   putCategory: (req, res, next) => {
-//     const { name } = req.body
-//     if (!name) throw new Error('Category name is required!')
-//     Category.findByPk(req.params.id)
-//       .then(category => {
-//         if (!category) throw new Error("Category didn't exist!")
-//         return category.update({ name })
-//       })
-//       .then(() => {
-//         req.flash('success_messages', 'Category was successfully to update')
-//         res.redirect('/admin/categories/:id')
-//       })
-//       .catch(err => next(err))
-//   },
-//   deleteCategory: (req, res, next) => { // 新增以下
-//     return Category.findByPk(req.params.id)
-//       .then(category => {
-//         if (!category) throw new Error("Category didn't exist!")
-//         return category.destroy()
-//       })
-//       .then(() => res.redirect('/admin/categories'))
-//       .catch(err => next(err))
-//   }
-// }
