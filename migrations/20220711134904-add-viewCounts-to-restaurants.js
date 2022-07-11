9fe616967fef10402867c94df88cb1d02409bafb
ChillDiver <yumingchang1991@gmail.com>
2022-07-11 23:26:26 +0800

    fix: add default value for viewCounts

--- migrations/20220711134904-add-viewCounts-to-restaurants.js
+++ migrations/20220711134904-add-viewCounts-to-restaurants.js
@@ -3,7 +3,8 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.addColumn('Restaurants', 'view_counts', {
-      type: Sequelize.INTEGER
+      type: Sequelize.INTEGER,
+      defaultValue: 0
     })
   },
 
