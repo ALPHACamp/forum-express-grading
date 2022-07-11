ec7ffb619cfc063adb6cede4218859b43c7c18d6
Yunya Hsu <yunyahsu110@gmail.com>
2022-07-11 17:28:11 +0800

    feat: category update

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -2,8 +2,14 @@ const { Category } = require('../models')
 
 const categoryController = {
   getCategories: (req, res, next) => {
-    return Category.findAll({ raw: true })
-      .then(categories => res.render('admin/categories', { categories }))
+    return Promise.all([
+      Category.findAll({ raw: true }),
+      req.params.id ? Category.findByPk(req.params.id, { raw: true }) : null // 檢查 req.params.id 這個變數是否存在。如果存在，在資料庫查詢階段，除了全部類別資料外，還需要撈出這個 id 對應的那一筆資料。如果不存在，直接存成空值即可。
+    ])
+      .then(([categories, category]) => res.render('admin/categories', {
+        categories,
+        category
+      }))
       .catch(err => next(err))
   },
   postCategories: (req, res, next) => {
@@ -15,6 +21,18 @@ const categoryController = {
         req.flash('success_messages', 'category was successfully created')
         return res.redirect('/admin/categories')
       })
+      .catch(err => next(err))
+  },
+  putCategory: (req, res, next) => {
+    const { name } = req.body
+
+    if (!name) throw new Error('Category name is required!')
+    return Category.findByPk(req.params.id)
+      .then(category => {
+        if (!category) throw new Error("Category doesn't exist!")
+        return category.update({ name })
+      })
+      .then(() => res.redirect('/admin/categories'))
       .catch(err => next(err))
   }
 }
