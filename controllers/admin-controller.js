6642bf1bb1aeb2fb0d54aa37f2a5b542bb20ca5d
chh817 <chh817@gmail.com>
2022-07-11 18:31:16 +0800

    feat: add category model

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -6,7 +6,7 @@ const adminController = {
       raw: true
     })
       .then(restaurants => res.render('admin/restaurants', { restaurants }))
-      .catch(err => next(err))
+      .catch(next)
   },
   createRestaurant: (req, res) => {
     return res.render('admin/create-restaurant')
@@ -28,7 +28,7 @@ const adminController = {
         req.flash('success_messages', 'restaurant was successfully created')
         res.redirect('/admin/restaurants')
       })
-      .catch(err => next(err))
+      .catch(next)
   },
   getRestaurant: (req, res, next) => {
     const id = req.params.id
@@ -39,7 +39,7 @@ const adminController = {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
         res.render('admin/restaurant', { restaurant })
       })
-      .catch(err => next(err))
+      .catch(next)
   },
   editRestaurant: (req, res, next) => {
     const id = req.params.id
@@ -50,7 +50,7 @@ const adminController = {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
         res.render('admin/edit-restaurant', { restaurant })
       })
-      .catch(err => next(err))
+      .catch(next)
   },
   putRestaurant: (req, res, next) => {
     const id = req.params.id
@@ -76,7 +76,7 @@ const adminController = {
         req.flash('success_messages', 'Restaurant is successfully updated!')
         res.redirect('/admin/restaurants')
       })
-      .catch(err => next(err))
+      .catch(next)
   },
   deleteRestaurant: (req, res, next) => {
     const id = req.params.id
@@ -86,33 +86,43 @@ const adminController = {
         return restaurant.destroy()
       })
       .then(() => res.redirect('/admin/restaurants'))
-      .catch(err => next(err))
+      .catch(next)
   },
   getUsers: (req, res, next) => {
     return User.findAll({
       raw: true
     })
       .then(users => res.render('admin/users', { users }))
-      .catch(err => next(err))
+      .catch(next)
   },
   patchUser: (req, res, next) => {
     const id = req.params.id
+    const email = 'root@example.com'
+    class SetRootRoleError extends Error {
+      constructor(message) {
+        super(message)
+        this.name = SetRootRoleError
+      }
+    }
     return User.findByPk(id)
       .then(user => {
         if (!user) throw new Error("this user didn't exist!")
-        if (user.email === 'root@example.com') {
-          req.flash('error_messages', '禁止變更 root 權限')
-          return res.redirect('back')
-        }
-        user.update({
+        if (user.email === email) throw new SetRootRoleError('禁止變更 root 權限')
+        return user.update({
           isAdmin: !user.isAdmin
         })
       })
-      .then(() => {
+      .then(user => {
         req.flash('success_messages', '使用者權限變更成功')
         return res.redirect('/admin/users')
       })
-      .catch(err => next(err))
+      .catch(err => {
+        if (err.name === SetRootRoleError) {
+          req.flash('error_messages', '禁止變更 root 權限')
+          return res.redirect('back')
+        }
+        next(err)
+      })
   }
 }
 module.exports = adminController
