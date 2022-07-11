a1be69d4f63fdc5e5d2e6f85bb9d0511693f237f
yaahsin <brillanteoboe@gmail.com>
2022-07-11 15:44:10 +0800

    feat: add admin categories page

---
+++ controllers/category-controller.js
@@ -0,0 +1,50 @@
+const { Category } = require('../models')
+
+const categoryController = {
+  getCategories: (req, res, next) => {
+    return Category.findAll({
+      raw: true
+    })
+      .then(categories => res.render('admin/categories', { categories }))
+      .catch(err => next(err))
+  }
+}
+module.exports = categoryController
+
+// 寫入資料
+//   postCategory: (req, res, next) => {
+//     const { name } = req.body
+//     if (!name) {
+//       throw new Error('Category name is required!')
+//         .then(Category.create({ name }))
+//         .then(() => {
+//           req.flash('success_messages', 'Category was successfully created')
+//           res.redirect('/admin/categories')
+//         })
+//         .catch(err => next(err))
+//     }
+//   },
+//   putCategory: (req, res, next) => {
+//     const { name } = req.body
+//     if (!name) throw new Error('Category name is required!')
+//     Category.findByPk(req.params.id)
+//       .then(category => {
+//         if (!category) throw new Error("Category didn't exist!")
+//         return category.update({ name })
+//       })
+//       .then(() => {
+//         req.flash('success_messages', 'Category was successfully to update')
+//         res.redirect('/admin/categories/:id')
+//       })
+//       .catch(err => next(err))
+//   },
+//   deleteCategory: (req, res, next) => { // 新增以下
+//     return Category.findByPk(req.params.id)
+//       .then(category => {
+//         if (!category) throw new Error("Category didn't exist!")
+//         return category.destroy()
+//       })
+//       .then(() => res.redirect('/admin/categories'))
+//       .catch(err => next(err))
+//   }
+// }
