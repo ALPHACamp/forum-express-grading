c1a9bf7699b58a034ebf92a4783fb6a835d64b90
gene.31985906@gmail.com <gene.31985906@gmail.com>
2022-07-12 11:33:27 +0800

    feat: show comments on restaurant page

--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -1,7 +1,10 @@
 const dayjs = require('dayjs') // 載入 dayjs 套件
+const relativeTime = require('dayjs/plugin/relativeTime')
+dayjs.extend(relativeTime)
 module.exports = {
   // 取得當年年份作為 currentYear 的屬性值，並導出
   currentYear: () => dayjs().year(),
+  relativeTimeFromNow: a => dayjs(a).fromNow(),
   checkAdmin: function (isAdmin) {
     return isAdmin === 1 ? 'user' : 'admin'
   },
