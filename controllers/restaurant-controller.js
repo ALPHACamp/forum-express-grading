a2d7fb6fc23230eb9856d7b31806e807e7b56005
AndyChienn <andy2002chien@hotmail.com.tw>
2022-07-12 14:27:59 +0800

    feat: add feeds page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -59,7 +59,31 @@ const restaurantController = {
         return res.render('dashboard', { restaurant })
       })
       .catch(err => next(err))
+  },
+  getFeeds: (req, res, next) => {
+    return Promise.all([
+      Restaurant.findAll({
+        limit: 10,
+        include: [Category],
+        order: [['createdAt', 'DESC']],
+        nest: true,
+        raw: true
+      }),
+      Comment.findAll({
+        limit: 10,
+        order: [['createdAt', 'DESC']],
+        include: [User, Restaurant],
+        nest: true,
+        raw: true
+      })
+    ])
+      .then(([restaurants, comments]) => {
+        res.render('feeds', {
+          restaurants,
+          comments
+        })
+      })
+      .catch(err => next(err))
   }
-
 }
 module.exports = restaurantController
