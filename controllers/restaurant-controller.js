c64ee193a17bda50e806beff09c0ca257d9a0e9c
JasonChou <swchou524@gmail.com>
2022-07-12 21:20:30 +0800

    feat: add restaurant page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -12,6 +12,18 @@ const restaurantController = {
       description: r.description.substring(0, 50)
     }))
     res.render('restaurants', { restaurants: data })
+  },
+  getRestaurant: (req, res, next) => {
+    Restaurant.findByPk(req.params.id, {
+      raw: true,
+      nest: true,
+      include: [Category]
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        res.render('admin/restaurant', { restaurant })
+      })
+      .catch(err => next(err))
   }
 }
 module.exports = restaurantController
