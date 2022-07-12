a9b2910be4bdde722a94137a6033598f001ed6bf
ChillDiver <yumingchang1991@gmail.com>
2022-07-12 10:45:24 +0800

    feat: add comment model

--- migrations/20220711134904-add-viewCounts-to-restaurants.js
+++ migrations/20220711134904-add-viewCounts-to-restaurants.js
@@ -4,11 +4,8 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.addColumn('Restaurants', 'view_counts', {
       type: Sequelize.INTEGER,
-<<<<<<< HEAD
-      defaultValue: 0
-=======
+      defaultValue: 0,
       allowNull: false
->>>>>>> R02
     })
   },
 
