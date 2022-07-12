740d60b92709ca0738c9e3b1293723daf52e6f3c
Rita Chien <ritachien929@gmail.com>
2022-07-12 11:58:03 +0800

    feat: add category delete feature
    For those restaurants categorized to deleted category, re-categorize
    them to category-(未分類).

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -1,4 +1,4 @@
-const { Category } = require('../models')
+const { Category, Restaurant } = require('../models')
 
 module.exports = {
   getCategories: async (req, res, next) => {
@@ -27,6 +27,35 @@ module.exports = {
       if (!category) throw new Error("Category doesn't exist!")
       await category.update({ name })
 
+      res.redirect('/admin/categories')
+    } catch (err) { next(err) }
+  },
+  deleteCategory: async (req, res, next) => {
+    try {
+      // Find delete target
+      const categoryDelete = await Category.findByPk(req.params.id)
+      if (!categoryDelete) {
+        req.flash('error_messages', "Category doesn't exist!")
+        return res.redirect('back')
+      }
+
+      // Find replace target
+      const categoryReplace = await Category.findOne({
+        where: { name: '(未分類)' },
+        raw: true
+      })
+      if (!categoryReplace) {
+        req.flash('error_messages', "Category '(未分類)' doesn't exist!")
+        return res.redirect('back')
+      }
+
+      // Replace category to 'Uncategorizes'
+      await Restaurant.update({ categoryId: categoryReplace.id }, {
+        where: { categoryId: categoryDelete.id }
+      })
+      // Delete target category
+      await categoryDelete.destroy()
+      req.flash('success_messages', 'Category deleted. Related restaurants are updated to category-(未分類)!')
       res.redirect('/admin/categories')
     } catch (err) { next(err) }
   }
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -25,6 +25,7 @@ router.get('/', (req, res) => res.redirect('/admin/restaurants'))
 // Category routers
 router.get('/categories/:id', categoryController.getCategories)
 router.put('/categories/:id', categoryController.putCategory)
+router.delete('/categories/:id', categoryController.deleteCategory)
 router.get('/categories', categoryController.getCategories)
 router.post('/categories', categoryController.postCategory)
 
