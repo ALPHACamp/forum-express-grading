9de174d507845124b01d3c8564356f9ff1191ddc
smile19439 <smile19439@gmail.com>
2022-07-12 21:25:49 +0800

    feat: category create

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -7,5 +7,12 @@ module.exports = {
         res.render('admin/categories', { categories })
       })
       .catch(err => next(err))
+  },
+  postCategory: (req, res, next) => {
+    const { name } = req.body
+    if (!name) throw new Error('Category name is required!')
+    return Category.create({ name })
+      .then(() => res.redirect('/admin/categories'))
+      .catch(err => next(err))
   }
 }
