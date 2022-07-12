0144734e69466eb09f4be5a0d4de52c06ba239b0
ChillDiver <yumingchang1991@gmail.com>
2022-07-12 11:02:40 +0800

    feat: add render comments feature

--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -1,10 +1,13 @@
 const dayjs = require('dayjs')
+const relativeTime = require('dayjs/plugin/relativeTime')
+dayjs.extend(relativeTime)
 // the reason to use dayjs is because original format of JavaScript Date object is a bit mess
 // if using original JavaScript data object, we need to do some data formating
 // dayjs helps us do the cleansing, it is why we use this package here
 
 module.exports = {
   currentYear: () => dayjs().year(),
+  relativeTimeFromNow: a => dayjs(a).fromNow(),
   ifCond: function (a, b, options) {
     return a === b ? options.fn(this) : options.inverse(this)
   }
