b5e307191de89c3bb5dbcfc990fba95d7bb308b7
yaahsin <brillanteoboe@gmail.com>
2022-07-12 20:58:04 +0800

    feat: add comment model

--- models/restaurant.js
+++ models/restaurant.js
@@ -12,6 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     // 新增關聯性，restaurants --M---1--> category
     static associate (models) {
       Restaurant.belongsTo(models.Category, { foreignKey: 'categoryId' })
+      Restaurant.hasMany(models.Comment, { foreignKey: 'restaurantId' })
     }
   };
   Restaurant.init({
