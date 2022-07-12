0c14d9d7bb067307c9ef64f1505b9d4db2700ca8
忍者哈特利 <ks0dcongra@gmail.com>
2022-07-12 18:14:37 +0800

    feat: admin update restaurant

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const SESSION_SECRET = 'secret'
 const passport = require('./config/passport')
 const handlebarsHelpers = require('./helpers/handlebars-helpers')
 const { getUser } = require('./helpers/auth-helpers')
+const methodOverride = require('method-override')
 
 app.engine('hbs', handlebars({ extname: '.hbs', helpers: handlebarsHelpers }))
 app.set('view engine', 'hbs')
@@ -17,6 +18,7 @@ app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: fals
 app.use(passport.initialize()) // 增加這行，初始化 Passport
 app.use(passport.session()) // 增加這行，啟動 session 功能
 app.use(flash())
+app.use(methodOverride('_method'))
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages') // 設定 success_msg 訊息
   res.locals.error_messages = req.flash('error_messages') // 設定 warning_msg 訊息
