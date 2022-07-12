19665dc4f19f6fb8dc981a7a5206054cb3add694
AdrieneTZ <tznali17@gmail.com>
2022-07-12 14:06:48 +0800

    feat: add update category function

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -3,8 +3,12 @@ const { Category } = require('../models')
 const categoryController = {
   getCategories: async (req, res, next) => {
     try {
-      const categories = await Category.findAll({ raw: true })
-      return res.render('admin/categories', { categories })
+      // if req.params.id exists, get the category data set with this id
+      const [categories, category] = await Promise.all([
+        Category.findAll({ raw: true }),
+        req.params.id ? Category.findByPk(req.params.id, { raw: true }) : null,
+      ])
+      return res.render('admin/categories', { categories, category })
     } catch (error) {
       next(error)
     }
@@ -16,6 +20,21 @@ const categoryController = {
 
       await Category.create({ name })
       req.flash('success_messages', 'You have created a new category successfully!')
+      return res.redirect('/admin/categories')
+    } catch (error) {
+      next(error)
+    }
+  },
+  putCategory: async (req, res, next) => {
+    try {
+      const { name } = req.body
+      if (!name) throw new Error('Category name is required field!')
+
+      const category = await Category.findByPk(req.params.id)
+      if (!category) throw new Error('This category does not exist! Please create it.')
+      await category.update({ name })
+
+      req.flash('success_messages', 'You have updated the category successfully!')
       return res.redirect('/admin/categories')
     } catch (error) {
       next(error)
