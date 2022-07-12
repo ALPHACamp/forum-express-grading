a20bb8408b33d4f5daef6d0fa0e24714b3cd429c
Yunya Hsu <yunyahsu110@gmail.com>
2022-07-12 11:04:03 +0800

    feat: add restaurants index page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,9 +1,23 @@
 // 建立一個名為 restaurantController 的object
 // restaurantController object 中有一個方法叫做 getRestaurants，負責「瀏覽餐廳頁面」，也就是去 render 名為 restaurants 的hbs
 
+const { Restaurant, Category } = require('../models')
+
 const restaurantController = {
-  getRestaurants: (req, res) => {
-    return res.render('restaurants')
+  getRestaurants: (req, res, next) => {
+    return Restaurant.findAll({
+      include: [Category],
+      raw: true,
+      nest: true
+    })
+      .then(restaurants => {
+        const data = restaurants.map(r => ({
+          ...r,
+          description: r.description.substring(0, 50)
+        }))
+        return res.render('restaurants', { restaurants: data })
+      })
+      .catch(err => next(err))
   }
 }
 module.exports = restaurantController
