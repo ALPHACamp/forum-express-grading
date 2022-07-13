378442cbc174d1a1d672299e37f7dc1931b74518
MioRain <ericzero1593@hotmail.com>
2022-07-13 16:03:48 +0800

    feat: category create

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -15,6 +15,7 @@ router.delete('/restaurants/:id', adminController.deleteRestaurant)
 router.get('/restaurants', adminController.getRestaurants)
 router.post('/restaurants', upload.single('image'), adminController.postRestaurant)
 router.get('/categories', categoryController.getCategories)
+router.post('/categories', categoryController.postCategory)
 router.get('/', (req, res) => res.redirect('/admin/restaurants'))
 
 module.exports = router
