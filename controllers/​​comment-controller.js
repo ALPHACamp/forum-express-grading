15fd6c93fb0f6980001cd1fffe5784375d8b5c4b
AlvinLee66 <ewqaz001@gmail.com>
2022-07-13 16:40:44 +0800

    feat: add post comment on restaurant page

---
+++ controllers/​​comment-controller.js
@@ -0,0 +1,29 @@
+const { Comment, User, Restaurant } = require('../models')
+
+const commentController = {
+  postComment: (req, res, next) => {
+    const { restaurantId, text } = req.body
+    const userId = req.user.id
+
+    if (!text) throw new Error('留言內容不能為空！')
+    return Promise.all([
+      User.findByPk(userId),
+      Restaurant.findByPk(restaurantId)
+    ])
+      .then(([user, restaurant]) => {
+        if (!user) throw new Error('使用者資料不存在！')
+        if (!restaurant) throw new Error('餐廳資料不存在！')
+
+        return Comment.create({
+          text,
+          restaurantId,
+          userId
+        })
+      })
+      .then(() => {
+        res.redirect(`/restaurants/${restaurantId}`)
+      })
+      .catch(err => next(err))
+  }
+}
+module.exports = commentController
