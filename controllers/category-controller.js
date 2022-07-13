38930e021585427d623e451d1725b96ff1cf2ef9
smile19439 <smile19439@gmail.com>
2022-07-13 16:15:45 +0800

    feat: category update

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -2,9 +2,14 @@ const { Category } = require('../models')
 
 module.exports = {
   getCategories: (req, res, next) => {
+    const id = req.params.id
     return Category.findAll({ raw: true })
       .then(categories => {
-        res.render('admin/categories', { categories })
+        const category = categories.find(category => {
+          return category.id === Number(id)
+        })
+        console.log(category)
+        res.render('admin/categories', { categories, category })
       })
       .catch(err => next(err))
   },
@@ -12,6 +17,18 @@ module.exports = {
     const { name } = req.body
     if (!name) throw new Error('Category name is required!')
     return Category.create({ name })
+      .then(() => res.redirect('/admin/categories'))
+      .catch(err => next(err))
+  },
+  putCategory: (req, res, next) => {
+    const id = req.params.id
+    const { name } = req.body
+    if (!name) throw new Error('Category name is required!')
+    return Category.findByPk(id)
+      .then(category => {
+        if (!category) throw new Error("Category doesn't exist!")
+        return category.update({ name })
+      })
       .then(() => res.redirect('/admin/categories'))
       .catch(err => next(err))
   }
