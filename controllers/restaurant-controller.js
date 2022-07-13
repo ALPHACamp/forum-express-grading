fc2b867ed3d14844fc345c6a7876f5969d3589a4
AlvinLee66 <ewqaz001@gmail.com>
2022-07-13 22:01:04 +0800

    feat: show comments on restaurant page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,4 +1,4 @@
-const { Restaurant, Category } = require('../models')
+const { Restaurant, Category, Comment, User } = require('../models')
 const { getOffset, getPagination } = require('../helpers/pagination-helper')
 
 const restaurantController = {
@@ -38,9 +38,16 @@ const restaurantController = {
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
-      include: [Category]
+      include: [
+        Category,
+        { model: Comment, include: User }
+      ],
+      order: [
+        [Comment, 'createdAt', 'DESC']
+      ]
     })
       .then(restaurant => {
+        console.log(restaurant.Comments)
         if (!restaurant) throw new Error('找不到此餐廳資訊！')
         return restaurant.increment('viewCounts')
       })
