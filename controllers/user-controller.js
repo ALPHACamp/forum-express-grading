625094574771feabab2dfae00b7d1bae137e66a2
ysRedhouse <yongsin0129@gmail.com>
2022-07-13 12:09:04 +0800

    feat : add User Profile

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,8 +1,8 @@
 const bcrypt = require('bcryptjs')
 const db = require('../models')
+const { imgurFileHandler } = require('../helpers/file-helpers')
 const { User } = db
 const userController = {
-
   signUpPage: (req, res) => {
     res.render('signup')
   },
@@ -44,6 +44,44 @@ const userController = {
     req.flash('success_messages', '登出成功！')
     req.logout()
     res.redirect('/signin')
+  },
+
+  // 不知道為什麼使用以下 promise 寫法跑 test 明明同樣的程式碼，有時候pass 有時候 Error: Timeout of 2000ms exceeded.
+  // 後來將 Timeout 等久一點就正常了，不知道是不是電腦太舊跑太慢？
+  getUser: (req, res, next) => {
+    const id = req.params.id
+    return User.findByPk(id, { raw: true })
+      .then(user => {
+        if (!user) throw new Error("user didn't exist!")
+        return res.render('users/profile', { user })
+      })
+      .catch(err => next(err))
+  },
+
+  editUser: (req, res, next) => {
+    const id = req.params.id
+    return User.findByPk(id, { raw: true })
+      .then(user => {
+        if (!user) throw new Error("user didn't exist!")
+        return res.render('users/edit', { user })
+      })
+      .catch(err => next(err))
+  },
+
+  putUser: (req, res, next) => {
+    const id = req.params.id
+    const { name } = req.body
+    const { file } = req
+    return Promise.all([User.findByPk(id), imgurFileHandler(file)])
+      .then(([user, filePath]) => {
+        if (!user) throw new Error("user didn't exist!")
+        return user.update({ name: name || user.name, image: filePath || user.image })
+      })
+      .then(() => {
+        req.flash('success_messages', '使用者資料編輯成功')
+        return res.redirect(`/users/${id}`)
+      })
+      .catch(err => next(err))
   }
 }
 module.exports = userController
--- routes/index.js
+++ routes/index.js
@@ -3,15 +3,17 @@ const router = express.Router()
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
 const commentController = require('../controllers/​​comment-controller')
+const upload = require('../middleware/multer')
 const { generalErrorHandler } = require('../middleware/error-handler')
 const passport = require('../config/passport')
 const admin = require('./modules/admin')
+
 const { authenticated, authenticatedAdmin } = require('../middleware/auth')
 
 router.use('/admin', authenticatedAdmin, admin)
 
 router.get('/signup', userController.signUpPage)
-router.post('/signup', userController.signUp) // 注意用 post
+router.post('/signup', userController.signUp)
 router.get('/logout', userController.logout)
 
 router.get('/signin', userController.signInPage)
@@ -19,7 +21,11 @@ router.post(
   '/signin',
   passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }),
   userController.signIn
-) // 注意是 post
+)
+
+router.get('/users/:id/edit', authenticated, userController.editUser)
+router.get('/users/:id', authenticated, userController.getUser)
+router.put('/users/:id', authenticated, upload.single('image'), userController.putUser)
 
 router.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard)
 router.get('/restaurants/:id', authenticated, restController.getRestaurant)
