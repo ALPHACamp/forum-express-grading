b9895501e92851c19aff7dd7014fed547ad2ac04
InzooC <bluefreaks@gmail.com>
2022-07-13 10:13:59 +0800

    feat: show comments on restaurant page

--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -1,7 +1,11 @@
 const dayjs = require('dayjs') //載入 dayjs 套件
+const relativeTime = require('dayjs/plugin/relativeTime') //Day.js本身的relativeTime 的 plugin
+dayjs.extend(relativeTime)
+
 module.exports = {
   currentYear: () => dayjs().year(), //取得當年年份作為 currentYear 的屬性值，並導出
-  ifCond: function(a, b, options) {
+  relativeTimeFromNow: a => dayjs(a).fromNow(),
+  ifCond: function (a, b, options) {
     return a === b ? options.fn(this) : options.inverse(this)
   }
 }
