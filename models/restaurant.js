1b001c5446ae59e54894e660e47896398f35ffb3
singingw <op0408io1998@gmail.com>
2022-07-13 23:22:47 +0800

    feat: create favorite model

--- models/restaurant.js
+++ models/restaurant.js
@@ -13,6 +13,11 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
       Restaurant.belongsTo(models.Category, { foreignKey: 'categoryId' })
       Restaurant.hasMany(models.Comment, { foreignKey: 'restaurantId' })
+      Restaurant.belongsToMany(models.User, {
+        through: models.Favorite, // 透過 Favorite 表來建立關聯
+        foreignKey: 'restaurantId',
+        as: 'FavoritedUsers' // 幫這個關聯取個名稱
+      })
     }
   };
   Restaurant.init({
