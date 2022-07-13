7ae670c55615b64a19318c094d34d27507b71e4f
ChillDiver <yumingchang1991@gmail.com>
2022-07-13 16:06:01 +0800

    feat: add like/unlike feature (R04)

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,5 +1,5 @@
 const bcrypt = require('bcryptjs')
-const { User, Restaurant, Comment, Favorite } = require('../models')
+const { User, Restaurant, Comment, Favorite, Like } = require('../models')
 const { imgurFileHandler } = require('../helpers/file-helpers')
 
 const userController = {
@@ -131,6 +131,48 @@ const userController = {
         if (!favorite) throw new Error("You haven't favorited this restaurant")
 
         return favorite.destroy()
+      })
+      .then(() => res.redirect('back'))
+      .catch(err => next(err))
+  },
+  addLike: (req, res, next) => {
+    const userId = req.user.id
+    const { restaurantId } = req.params
+
+    return Promise
+      .all([
+        Restaurant.findByPk(restaurantId),
+        Like.findOne({
+          where: {
+            userId,
+            restaurantId
+          }
+        })
+      ])
+      .then(([restaurant, like]) => {
+        if (!restaurant) throw new Error('restaurant does not exist')
+        if (like) throw new Error('You have liked this restaurant')
+        return Like.create({
+          userId,
+          restaurantId
+        })
+      })
+      .then(() => res.redirect('back'))
+      .catch(err => next(err))
+  },
+  removeLike: (req, res, next) => {
+    const userId = req.user.id
+    const { restaurantId } = req.params
+    return Like
+      .findOne({
+        where: {
+          userId,
+          restaurantId
+        }
+      })
+      .then(like => {
+        if (!like) throw new Error('You haven\'t like this restaurant')
+        return like.destroy()
       })
       .then(() => res.redirect('back'))
       .catch(err => next(err))
