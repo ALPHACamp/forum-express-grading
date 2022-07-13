dcfe45dc72655575830f7c6595d8785efd48c061
Rita Chien <ritachien929@gmail.com>
2022-07-13 16:07:41 +0800

    feat: show comments on restaurant page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,4 +1,4 @@
-const { Restaurant, Category } = require('../models')
+const { Restaurant, Category, Comment, User } = require('../models')
 const { getOffset, getPagination } = require('../helpers/pagination-helper')
 
 const restaurantController = {
@@ -38,7 +38,11 @@ const restaurantController = {
   getRestaurant: async (req, res, next) => {
     try {
       const restaurant = await Restaurant.findByPk(req.params.id, {
-        include: Category,
+        include: [
+          Category,
+          { model: Comment, include: User }
+        ],
+        order: [[Comment, 'createdAt', 'DESC']],
         nest: true
       })
       if (!restaurant) throw new Error('Restaurant does not exist!')
