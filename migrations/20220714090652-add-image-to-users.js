8291766a2825179fa1d020e25179817a553513ce
yaahsin <brillanteoboe@gmail.com>
2022-07-14 23:25:49 +0800

    heroku test

--- migrations/20220714090652-add-image-to-users.js
+++ migrations/20220714090652-add-image-to-users.js
@@ -3,9 +3,7 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.addColumn('Users', 'image', {
-      type: Sequelize.STRING,
-      // todo: 沒有成功顯示隨機圖片, 改在seed裡面
-      defaultValue: `https://loremflickr.com/320/240/people/?random=${Math.random() * 100}`
+      type: Sequelize.STRING
     })
   },
   down: async (queryInterface, Sequelize) => {
