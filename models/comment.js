0979075c6e1c2d425a1d9cceaec10bb3dfb489ef
rubylo718 <rubylo718@gmail.com>
2022-07-14 17:20:19 +0800

    feat: add comment model

---
+++ models/comment.js
@@ -0,0 +1,22 @@
+'use strict'
+const {
+  Model
+} = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+  class Comment extends Model {
+    static associate (models) {
+      Comment.belongsTo(models.Restaurant, { foreignKey: 'restaurantId' })
+      Comment.belongsTo(models.User, { foreignKey: 'userId' })
+    }
+  };
+  Comment.init({
+    text: DataTypes.STRING,
+    userId: DataTypes.INTEGER,
+    restaurantId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Comment',
+    underscored: true
+  })
+  return Comment
+}
