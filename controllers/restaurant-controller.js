273edbbfcf5e75f53a68e04141e8b70fa36de9f1
cincinfish <96610786+cincinfish@users.noreply.github.com>
2022-07-15 01:29:01 +0800

    feat: add view counts& dashboard

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -19,12 +19,26 @@ const restaurantController = {
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
+      include: Category
+    }).then(restaurant => {
+      if (!restaurant) throw new Error("Restaurant didn't exist!")
+      return restaurant.increment('viewCounts')
+    })
+      .then(restaurant => {
+        return res.render('restaurant', {
+          restaurant: restaurant.toJSON()
+        })
+      })
+      .catch(err => next(err))
+  },
+  getDashboard: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, {
       include: Category,
       nest: true,
       raw: true
     }).then(restaurant => {
       if (!restaurant) throw new Error("Restaurant didn't exist!")
-      return res.render('restaurant', {
+      return res.render('dashboard', {
         restaurant
       })
     })
