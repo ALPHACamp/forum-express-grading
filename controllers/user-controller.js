9296471f6b3f17095ebe58bedf3ad2e5ee3bf926
InzooC <bluefreaks@gmail.com>
2022-07-14 16:25:21 +0800

    merge R03 to main

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcryptjs')
-const db = require('../models')
-const { User } = db
+const { Restaurant, Comment, User } = require('../models')
+const { imgurFileHandler } = require('../helpers/file-helpers')
+
 const userController = {
   signUpPage: (req, res) => {
     res.render('signup')
@@ -36,6 +37,50 @@ const userController = {
     req.flash('success_messages', '登出成功！')
     req.logout()
     res.redirect('/signin')
+  },
+  getUser: (req, res, next) => {
+    const id = Number(req.params.id)
+    return User.findByPk(id, {
+      include: [
+        { model: Comment, include: Restaurant }
+      ]
+    })
+      .then(user => {
+        if (!user) throw new Error("user didn't exist!")
+        return res.render('users/profile', { user: user.toJSON() })
+      })
+      .catch(err => next(err))
+  },
+  editUser: (req, res, next) => {
+    const id = Number(req.params.id)
+    return User.findByPk(id, { raw: true })
+      .then(user => {
+        if (!user) throw new Error("user didn't exist!")
+        return res.render('users/edit', { user })
+      })
+      .catch(err => next(err))
+  },
+  putUser: (req, res, next) => {
+    const { name } = req.body
+    const { file } = req
+    const id = Number(req.params.id)
+    if (!name) throw new Error('user name is required!')
+    return Promise.all([
+      User.findByPk(id),
+      imgurFileHandler(file)
+    ])
+      .then(([user, filePath]) => {
+        if (!user) throw new Error("user didn't exist!")
+        return user.update({
+          name,
+          image: filePath || user.image
+        })
+      })
+      .then(() => {
+        req.flash('success_messages', '使用者資料編輯成功')
+        return res.redirect(`/users/${id}`)
+      })
+      .catch(err => next(err))
   }
 }
 module.exports = userController
--- seeders/20220708092321-categories-seed-file.js
+++ seeders/20220708092321-categories-seed-file.js
@@ -16,4 +16,3 @@ module.exports = {
     await queryInterface.bulkDelete('Categories', null, {})
   }
 }
- 
