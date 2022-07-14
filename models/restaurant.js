58d1e15d59d0fd6fe591c29f629348bf60fc62c0
singingw <op0408io1998@gmail.com>
2022-07-14 23:22:48 +0800

    Like / Unlike

--- models/restaurant.js
+++ models/restaurant.js
@@ -18,6 +18,11 @@ module.exports = (sequelize, DataTypes) => {
         foreignKey: 'restaurantId',
         as: 'FavoritedUsers' // 幫這個關聯取個名稱
       })
+      Restaurant.belongsToMany(models.User, {
+        through: models.Like,
+        foreignKey: 'restaurantId',
+        as: 'LikedUsers'
+      })
     }
   };
   Restaurant.init({
