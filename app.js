6eb78cdc7de8575b879cb5b169f0ef537c4c2ff9
robert1074004 <abc50113@yahoo.com.tw>
2022-07-15 13:45:19 +0800

    feat: add restaurants page

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const routes = require('./routes')
 const handlebars = require('express-handlebars')
+
 const app = express()
 const port = process.env.PORT || 3000
 app.engine('hbs', handlebars({ extname: '.hbs' }))
