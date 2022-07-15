398fffbc9b2e5df1292fa83c48afcb4a9f5b84fb
cincinfish <96610786+cincinfish@users.noreply.github.com>
2022-07-15 15:31:26 +0800

    feat: show comments on restaurant page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,4 +1,4 @@
-const { Restaurant, Category } = require('../models')
+const { Restaurant, Category, Comment, User } = require('../models')
 const { getOffset, getPagination } = require('../helpers/pagination-helper')
 
 const restaurantController = {
@@ -37,7 +37,10 @@ const restaurantController = {
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
-      include: Category
+      include: [Category,
+        { model: Comment, include: User }
+      ],
+      order: [[Comment, 'updatedAt', 'DESC']]
     }).then(restaurant => {
       if (!restaurant) throw new Error("Restaurant didn't exist!")
       return restaurant.increment('viewCounts')
