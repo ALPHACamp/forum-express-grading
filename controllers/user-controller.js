f7fa3a347ad9ffa73c012255865bbd2ef1b7e9d6
coldfish5630 <layan.schicksal@gmail.com>
2022-07-15 22:57:35 +0900

    feat: comments link to user profile
    feat: add return to user controller
    
    feat: delete getUser group by

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,5 +1,5 @@
 const bcrypt = require('bcryptjs')
-const { User } = require('../models')
+const { User, Restaurant, Comment } = require('../models')
 const { imgurFileHandler } = require('../helpers/file-helpers')
 
 const userController = {
@@ -46,11 +46,23 @@ const userController = {
     res.redirect('/signin')
   },
   getUser: (req, res, next) => {
-    const login = req.user
-    return User.findByPk(req.params.id, { raw: true })
-      .then(user => {
+    return Promise.all([
+      User.findByPk(req.params.id, { raw: true }),
+      Comment.findAndCountAll({
+        where: { userId: req.params.id },
+        include: Restaurant,
+        raw: true,
+        nest: true
+      })
+    ])
+      .then(([user, comment]) => {
+        const restaurants = comment.rows.map(r => r.Restaurant)
         if (!user) throw new Error("User didn't exist")
-        res.render('users/profile', { user, login })
+        return res.render('users/profile', {
+          user,
+          count: comment.count,
+          restaurants
+        })
       })
       .catch(err => next(err))
   },
