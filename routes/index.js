3c221725401229bed2c20e3b2a252e3c9ad662d2
AlvinLee66 <ewqaz001@gmail.com>
2022-07-17 00:53:51 +0800

    feat: add Top 10 restaurants page

--- routes/index.js
+++ routes/index.js
@@ -26,6 +26,7 @@ router.put('/users/:id', authenticated, upload.single('image'), userController.p
 router.get('/users/:id', authenticated, userController.getUser)
 
 // about restaurants
+router.get('/restaurants/top', authenticated, restController.getTopRestaurants)
 router.get('/restaurants/feeds', authenticated, restController.getFeeds)
 router.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard)
 router.get('/restaurants/:id', authenticated, restController.getRestaurant)
