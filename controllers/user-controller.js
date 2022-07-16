021b1966eb6c0d25b1b6908604dabfbb885b4360
smile19439 <smile19439@gmail.com>
2022-07-16 09:59:33 +0800

    R03: User Profile

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -2,6 +2,8 @@ const bcrypt = require('bcryptjs')
 const db = require('../models')
 const { User } = db
 
+const { imgurFileHelper } = require('../helpers/file-helpers')
+
 const userController = {
   signUpPage: (req, res) => {
     res.render('signup')
@@ -42,6 +44,59 @@ const userController = {
     req.logout()
     req.flash('success_messages', '成功登出!')
     res.redirect('/signin')
+  },
+  getUser: (req, res, next) => {
+    const id = req.params.id
+    return User.findByPk(id, {
+      raw: true,
+      nest: true
+    })
+      .then(user => {
+        if (!user) throw new Error("User dosen't exist!")
+
+        return res.render('users/profile', { user })
+      })
+      .catch(err => next(err))
+  },
+  editUser: (req, res, next) => {
+    const id = req.params.id
+
+    return User.findByPk(id, {
+      raw: true,
+      nest: true
+    })
+      .then(user => {
+        if (!user) throw new Error("User doesn't exist!")
+
+        return res.render('users/edit', { user })
+      })
+      .catch(err => next(err))
+  },
+  putUser: (req, res, next) => {
+    const id = req.params.id
+    const { name } = req.body
+    const { file } = req
+
+    if (!name) throw new Error('User name is required!')
+    if (Number(id) !== req.user.id) throw new Error('不能編輯其他人的資料')
+
+    return Promise.all([
+      User.findByPk(id),
+      imgurFileHelper(file)
+    ])
+      .then(([user, filePath]) => {
+        if (!user) throw new Error("User doesn't exist!")
+
+        return user.update({
+          name,
+          image: filePath || user.image
+        })
+      })
+      .then(() => {
+        req.flash('success_messages', '使用者資料編輯成功')
+        return res.redirect(`/users/${id}`)
+      })
+      .catch(err => next(err))
   }
 }
 
