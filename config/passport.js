2d23a459c35fc30a56a0d00c15041f97ab0799d6
june850713@gmail.com <june850713@gmail.com>
2022-07-16 14:20:09 +0800

    feat: create followship model

--- config/passport.js
+++ config/passport.js
@@ -28,7 +28,9 @@ passport.deserializeUser((id, cb) => {
   User.findByPk(id, {
     include: [
       { model: Restaurant, as: 'FavoritedRestaurants' },
-      { model: Restaurant, as: 'LikedRestaurants' }
+      { model: Restaurant, as: 'LikedRestaurants' },
+      { model: User, as: 'Followers' },
+      { model: User, as: 'Following' }
     ]
   })
     .then(user => cb(null, user.toJSON()))
