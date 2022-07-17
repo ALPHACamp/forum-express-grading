bea4c7492d9fa75f81043e418cb8834bd960f5c6
choyz1024 <nagatoyuki1024@gmail.com>
2022-07-17 18:45:01 +0800

    fix: adjust restaurant comment counts seed

---
+++ seeders/20220717020451-adjust-restaurant-comment-counts.js
@@ -0,0 +1,30 @@
+'use strict'
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const datas = await queryInterface.sequelize.query(
+      'SELECT restaurants.id, COUNT(comments.restaurant_id) AS comment_counts FROM restaurants JOIN forum.comments ON restaurants.id = comments.restaurant_id GROUP BY comments.restaurant_id ORDER BY comment_counts DESC;',
+      {
+        type: queryInterface.sequelize.QueryTypes.SELECT
+      }
+    )
+
+    await Promise.all(
+      datas.map(async data => {
+        await queryInterface.bulkUpdate(
+          'Restaurants',
+          {
+            comment_counts: data.comment_counts
+          },
+          {
+            id: data.id
+          }
+        )
+      })
+    )
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkUpdate('Restaurants', { comment_counts: 0 })
+  }
+}
