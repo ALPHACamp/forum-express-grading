c0955420dacd3a0bca4a0592dcb65065549506f2
Jason Chan <jasonchan19981017@hotmail.com>
2022-07-17 22:12:13 +0800

    feat: add authenticated to restaurants top and feeds route

--- routes/index.js
+++ routes/index.js
@@ -21,8 +21,8 @@ router.post('/signin', passport.authenticate('local',
   { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
 router.get('/logout', userController.logout)
 
-router.get('/restaurants/top', restController.getTopRestaurants)
-router.get('/restaurants/feeds', restController.getFeeds)
+router.get('/restaurants/top', authenticated, restController.getTopRestaurants)
+router.get('/restaurants/feeds', authenticated, restController.getFeeds)
 router.get('/restaurants/:id', authenticated, restController.getRestaurant)
 router.get('/restaurants', authenticated, restController.getRestaurants)
 router.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard)
