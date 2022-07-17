a3709d422125ef97700cde238652d67897be52af
SnowWuChiKuo <x9x9x8x8x7x7x6x6x@gmail.com>
2022-07-17 23:10:45 +0800

    feat: modify R05

--- routes/index.js
+++ routes/index.js
@@ -20,6 +20,7 @@ router.post('/signin', passport.authenticate('local', { failureRedirect: '/signi
 
 router.get('/logout', userController.logout)
 
+router.get('/restaurants/top', authenticated, restController.getTopRestaurants)
 router.get('/restaurants/feeds', authenticated, restController.getFeeds)
 router.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard)
 router.get('/restaurants/:id', authenticated, restController.getRestaurant)
