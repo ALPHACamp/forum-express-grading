fdfcb45cb94520c4d5100ab698a4f59ea944d48a
SnowWuChiKuo <x9x9x8x8x7x7x6x6x@gmail.com>
2022-07-17 11:09:15 +0800

    R03 : add getUser & editUser & putUser

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,4 +1,5 @@
 const bcrypt = require('bcryptjs')
+const { imgurFileHandler } = require('../helpers/file-helpers')
 const db = require('../models')
 const User = db.User
 
@@ -38,6 +39,42 @@ const userController = {
     req.flash('success_messages', '登出成功！')
     req.logout()
     res.redirect('/signin')
+  },
+  getUser: async (req, res, next) => {
+    await User.findByPk(req.params.id, { raw: true })
+      .then(user => {
+        if (!user) throw new Error('使用者不存在!')
+        return res.render('users/profile', { user })
+      })
+      .catch(err => next(err))
+  },
+  editUser: async (req, res, next) => {
+    await User.findByPk(req.params.id, { raw: true })
+      .then(user => {
+        if (!user) throw new Error('使用者不存在!')
+        return res.render('users/edit', { user })
+      })
+      .catch(err => next(err))
+  },
+  putUser: async (req, res, next) => {
+    const { name } = req.body
+    const { file } = req
+    await Promise.all([
+      User.findByPk(req.params.id),
+      imgurFileHandler(file)
+    ])
+      .then(([user, filePath]) => {
+        if (!user) throw new Error('使用者不存在!')
+        return user.update({
+          name: name || user.name,
+          image: filePath || user.image
+        })
+      })
+      .then(() => {
+        req.flash('success_messages', '使用者資料編輯成功')
+        res.redirect(`/users/${req.params.id}`)
+      })
+      .catch(err => next(err))
   }
 }
 module.exports = userController
