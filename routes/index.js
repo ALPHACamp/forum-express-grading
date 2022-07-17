caafe63798b025deda62ed2b21ba9262604f9200
忍者哈特利 <ks0dcongra@gmail.com>
2022-07-17 14:46:35 +0800

    feat: add favorite/unfavorite button at index page

--- routes/index.js
+++ routes/index.js
@@ -24,6 +24,8 @@ router.post('/comments', authenticated, commentController.postComment)
 router.get('/users/:id', authenticated, userController.getUser)
 router.get('/users/:id/edit', authenticated, userController.editUser)
 router.put('/users/:id', upload.single('image'), authenticated, userController.putUser)
+router.post('/favorite/:restaurantId', authenticated, userController.addFavorite)
+router.delete('/favorite/:restaurantId', authenticated, userController.removeFavorite)
 router.get('/', (req, res) => res.redirect('/restaurants'))
 router.use('/', generalErrorHandler)
 
