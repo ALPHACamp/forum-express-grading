7035e2db9a4b474a9f774d33a0be314ceda8dd13
Jason Chan <jasonchan19981017@hotmail.com>
2022-07-17 14:42:50 +0800

    feat: user profile function

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,6 +1,9 @@
 const bcrypt = require('bcryptjs')
 const db = require('../models')
-const { User } = db
+const { User, Comment, Restaurant } = db
+const { imgurFileHandler } = require('../helpers/file-helper')
+const Sequelize = require('sequelize')
+const { raw } = require('express')
 
 const userController = {
   signUpPage: (req, res) => {
@@ -37,6 +40,55 @@ const userController = {
     req.flash('success_messages', 'Logout successfully!')
     req.logout()
     res.redirect('/signin')
+  },
+  getUser: (req, res, next) => {
+    return User.findByPk(req.params.id, {
+      nest: true,
+      include: [
+        { model: Comment, include: Restaurant }
+      ]
+    })
+      .then(user => {
+        if (!user) throw new Error('User does not exist!')
+        user = user.toJSON()
+        // 針對test所做的調整，測試檔沒有Comments attribute
+        const numberOfComments = user.Comments ? user.Comments.length : 0
+        res.render('users/profile', { user: { ...user, numberOfComments } })
+      })
+      .catch(err => next(err))
+  },
+  editUser: (req, res, next) => {
+    return User.findByPk(req.params.id, { raw: true })
+      .then(user => {
+        if (!user) throw new Error('User does not exist!')
+
+        res.render('users/edit', { user })
+      })
+      .catch(err => next(err))
+  },
+  putUser: (req, res, next) => {
+    const { name } = req.body
+    const { id } = req.params
+    if (!name) throw new Error('Name is required!')
+
+    const { file } = req
+    return Promise.all([
+      User.findByPk(id),
+      imgurFileHandler(file)
+    ])
+      .then(([user, filePath]) => {
+        if (!user) throw new Error('User does not exist')
+
+        return user.update({
+          name,
+          image: filePath || user.image
+        })
+      })
+      .then(() => {
+        req.flash('success_messages', '使用者資料編輯成功')
+        res.redirect(`/users/${id}`)
+      })
+      .catch(err => next(err))
   }
 }
 
