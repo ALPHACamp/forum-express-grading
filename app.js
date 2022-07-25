a8b31de6275ed476fef829d617b407fe4f844537
rungo6 <t24331279@gmail.com>
2022-07-25 19:15:15 +0800

    add users migrate

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const handlebars = require('express-handlebars')
 const app = express()
 const port = process.env.PORT || 3000
 
+// eslint-disable-next-line no-unused-vars
 const db = require('./models')
 
 app.use(routes)
