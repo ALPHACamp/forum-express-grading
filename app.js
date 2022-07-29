219aedaa3bd7c383b3403c343efbc4ccdf81f191
simonho1017 <sungppppp1017@gmail.com>
2022-07-29 17:39:54 +0900

    feat add header & footer

--- app.js
+++ app.js
@@ -3,19 +3,26 @@ const routes = require('./routes')
 const handlebars = require('express-handlebars')
 const flash = require('connect-flash')
 const session = require('express-session')
+const passport = require('passport')
+const handlebarsHelpers = require('./helpers/handlebars-helpers') // 引入 handlebars-helpers
+const { getUser } = require('./helpers/auth-helpers')
 const app = express()
 const port = process.env.PORT || 3000
 const SESSION_SECRET = 'secret' // 新增這行
 
-app.engine('hbs', handlebars({ extname: 'hbs' }))
+app.engine('hbs', handlebars({ extname: 'hbs', helpers: handlebarsHelpers }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 
 app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
+app.use(passport.initialize())
+app.use(passport.session())
+
 app.use(flash())
 app.use((req, res, next) => {
-  res.locals.success_messagse = req.flash('success_messages')
+  res.locals.success_messages = req.flash('success_messages')
   res.locals.error_messages = req.flash('error_messages')
+  res.locals.user = getUser(req)
   next()
 })
 
