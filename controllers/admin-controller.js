4502e5621f9c4e24dec83ae3ae4bcb3dfbc5c27d
pusheng <sdf753741789@gmail.com>
2022-07-30 10:50:16 +0800

    fix code

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,17 +1,22 @@
-const { Restaurant } = require('../models')
-const { User } = require('../models')
+const { Restaurant, User, Category } = require('../models')
 const { imgurFileHandler } = require('../helpers/file-helpers')
 const adminController = {
   getRestaurants: (req, res, next) => {
     Restaurant.findAll({
-      raw: true
+      raw: true,
+      nest: true,
+      include: [Category]
     })
       .then(restaurants => res.render('admin/restaurants', { restaurants }))
       .catch(err => next(err))
   },
-  createRestaurant: (req, res) => { return res.render('admin/create-restaurant') },
+  createRestaurant: (req, res, next) => {
+    return Category.findAll({ raw: true }).then(categories => {
+      res.render('admin/create-restaurant', { categories })
+    }).catch(err => next(err))
+  },
   postRestaurant: (req, res, next) => {
-    const { name, tel, address, openingHours, description } = req.body
+    const { name, tel, address, openingHours, description, categoryId } = req.body
     const { file } = req
     if (!name) throw new Error('Restaurant name is required!')
     imgurFileHandler(file).then(filePath => Restaurant.create({
@@ -20,7 +25,8 @@ const adminController = {
       address,
       openingHours,
       description,
-      image: filePath || null
+      image: filePath || null,
+      categoryId
     })
       .then(() => {
         req.flash('success_messages', 'restaurant was successfully created')
@@ -30,7 +36,9 @@ const adminController = {
   },
   getRestaurant: (req, res, next) => {
     Restaurant.findByPk(req.params.id, {
-      raw: true
+      raw: true,
+      nest: true,
+      include: [Category]
     })
       .then(restaurant => {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
@@ -39,17 +47,18 @@ const adminController = {
       .catch(err => next(err))
   },
   editRestaurant: (req, res, next) => {
-    Restaurant.findByPk(req.params.id, {
-      raw: true
-    })
-      .then(restaurant => {
+    Promise.all([Restaurant.findByPk(req.params.id, {
+      raw: true,
+      nest: true
+    }), Category.findAll({ raw: true })])
+      .then(([restaurant, categories]) => {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
-        res.render('admin/edit-restaurant', { restaurant })
+        res.render('admin/edit-restaurant', { restaurant, categories })
       })
       .catch(err => next(err))
   },
   putRestaurant: (req, res, next) => {
-    const { name, tel, address, openingHours, description } = req.body
+    const { name, tel, address, openingHours, description, categoryId } = req.body
     if (!name) throw new Error('Restaurant name is required!')
     const { file } = req
     Promise.all([Restaurant.findByPk(req.params.id), imgurFileHandler(file)]).then(([restaurant, filePath]) => {
@@ -60,7 +69,8 @@ const adminController = {
         address,
         openingHours,
         description,
-        image: filePath || restaurant.image
+        image: filePath || restaurant.image,
+        categoryId
       })
     }).then(() => {
       req.flash('success_messages', 'restaurant was successfully to update')
--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,6 +1,122 @@
+const { Restaurant, Category, Comment, User } = require('../models')
+const { getOffset, getPagination } = require('../helpers/pagination-helper')
+
 const restaurantController = {
-  getRestaurants: (req, res) => {
-    return res.render('restaurants')
+  getRestaurants: (req, res, next) => {
+    const categoryId = Number(req.query.categoryId) || ''
+    const DEFAULT_LIMIT = 9
+
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || DEFAULT_LIMIT
+    const offset = getOffset(limit, page)
+
+    Promise.all([Restaurant.findAndCountAll({
+      include: Category,
+      where: {
+        ...categoryId ? { categoryId } : {}
+      },
+      limit,
+      offset,
+      nest: true,
+      raw: true
+    }),
+    Category.findAll({ raw: true })]).then(([restaurants, categories]) => {
+      const favoritedRestaurantsId = req.user && req.user.FavoritedRestaurants.map(fr => fr.id)
+      const LikeRestaurantId = req.user && req.user.LikeRestaurants.map(like => like.id)
+      const data = restaurants.rows.map(r => ({
+        ...r,
+        description: r.description.substring(0, 50),
+        isFavorited: favoritedRestaurantsId.includes(r.id),
+        isLike: LikeRestaurantId.includes(r.id)
+      }))
+      return res.render('restaurants', {
+        restaurants: data,
+        categories,
+        categoryId,
+        pagination: getPagination(limit, page, restaurants.count)
+      })
+    }).catch(err => next(err))
+  },
+  getRestaurant: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, {
+
+      include: [Category, {
+        model: Comment,
+        include: User
+      },
+      { model: User, as: 'FavoritedUsers' },
+      { model: User, as: 'LikeUsers' }],
+      order: [[{ model: Comment }, 'updatedAt', 'DESC']]
+
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        const isFavorited = restaurant.FavoritedUsers.some(f => f.id === req.user.id)
+        const isLike = restaurant.LikeUsers.some(l => l.id === req.user.id)
+        return restaurant.increment(
+          'viewCounts', { by: 1 }
+        ).then(restaurant => {
+          res.render('restaurant', {
+            restaurant: restaurant.toJSON(),
+            isFavorited,
+            isLike
+          })
+        })
+      })
+      .catch(err => next(err))
+  },
+  getDashboard: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, {
+      include: [Category, { model: Comment }]
+    }).then(restaurant => {
+      res.render('dashboard', {
+        restaurant: restaurant.toJSON()
+      })
+    }).catch(err => next(err))
+  },
+  getFeeds: (req, res, next) => {
+    return Promise.all([
+      Restaurant.findAll({
+        limit: 10,
+        order: [['createdAt', 'DESC']],
+        include: [Category],
+        raw: true,
+        nest: true
+      }),
+      Comment.findAll({
+        limit: 10,
+        order: [['createdAt', 'DESC']],
+        include: [User, Restaurant],
+        raw: true,
+        nest: true
+      })
+    ])
+      .then(([restaurants, comments]) => {
+        res.render('feeds', {
+          restaurants,
+          comments
+        })
+      })
+      .catch(err => next(err))
+  },
+  getTopRestaurants: (req, res, next) => {
+    return Restaurant.findAll({
+      include: [
+        {
+          model: User, as: 'FavoritedUsers'
+        }
+      ]
+    }).then(restaurants => {
+      const data = restaurants.map(r => ({
+        ...r.toJSON(),
+        isFavorited: req.user && req.user.FavoritedRestaurants.some(fr => fr.id === r.id),
+        favoritedCount: r.FavoritedUsers.length,
+        description: r.description.substring(0, 50)
+      })).sort((a, b) => {
+        return b.favoritedCount - a.favoritedCount
+      }).slice(0, 10)
+      return res.render('top-restaurants', { restaurants: data })
+    }).catch(err => next(err))
   }
 }
 module.exports = restaurantController
