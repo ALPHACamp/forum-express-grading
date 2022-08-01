21cb2c8838550ada44c0693aa86570303232e8fc
Yen <p12427@gmail.com>
2022-08-01 16:52:00 +0800

    feat: add admin restaurants page and router

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const router = require('express').Router()
 const restController = require('../controllers/restaurant-controller')
+const admin = require('./modules/admin')
 
+router.use('/admin', admin)
 router.get('/restaurants', restController.getRestaurants)
 router.use('/', (req, res) => res.redirect('restaurants'))
 
