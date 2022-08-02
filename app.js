4d3cdec22d85bdf1c72a31c4cf36737fda2be444
Yen <p12427@gmail.com>
2022-08-02 13:45:36 +0800

    feat: add user signup page and router

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const methodOverride = require('method-override')
 const exphbs = require('express-handlebars')
 const routes = require('./routes')
 
@@ -8,6 +9,10 @@ const port = process.env.PORT || 3000
 // template engine: handlebars
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+// middleware: body-parser, method-override
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 
 app.use(routes)
 
