bfa6bb50bd77657d8116d01a65b44681e1aa9a8c
Yen <p12427@gmail.com>
2022-08-05 15:49:38 +0800

    feat: add edit restaurant page and update restaurant feature

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -33,6 +33,32 @@ const adminController = {
         res.render('admin/restaurant', { restaurant })
       })
       .catch(e => next(e))
+  },
+
+  getEditRestaurantPage: (req, res, next) => {
+    Restaurant.findByPk(req.params.id, {
+      raw: true
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        res.render('admin/edit-restaurant', { restaurant })
+      })
+      .catch(e => next(e))
+  },
+
+  putRestaurant: (req, res, next) => {
+    const { name } = req.body
+    if (!name) throw new Error('Restaurant name is required!')
+    Restaurant.findByPk(req.params.id)
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        return restaurant.update(req.body)
+      })
+      .then(() => {
+        req.flash('success_messages', 'Restaurant was successfully updated.')
+        res.redirect('/admin/restaurants')
+      })
+      .catch(e => next(e))
   }
 }
 module.exports = adminController
