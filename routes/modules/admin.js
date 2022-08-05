aa3c3c7ab3e475d2c0b72f980a90d2ad0f38874e
rungo6 <t24331279@gmail.com>
2022-08-06 02:00:20 +0800

    feat: R01 done

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -12,5 +12,8 @@ router.delete('/restaurants/:id', adminController.deleteRestaurant)
 router.get('/restaurants', authenticated, adminController.getRestaurants)
 router.post('/restaurants', upload.single('image'), adminController.postRestaurant)
 
+router.patch('/users/:id', adminController.patchUser)
+router.get('/users', adminController.getUsers)
+
 router.get('', (req, res) => res.redirect('/admin/restaurants'))
 module.exports = router
