33aae179c2294baa604d34dbb4a95f4f344b12bb
rungo6 <t24331279@gmail.com>
2022-08-10 15:10:00 +0800

    feat: add categories navbar and pagination on restaurant index page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,18 +1,40 @@
 const { Restaurant, Category } = require('../models')
+const { getOffset, getPagination } = require('../helpers/pagination-helper')
+const restaurantController = {
 
-const adminController = {
+  getRestaurants: (req, res, next) => {
+    const DEFAULT_LIMIT = 9
 
-  getRestaurants: (req, res) => {
-    Restaurant.findAll({
-      include: Category,
-      nest: true,
-      raw: true
-    }).then(restaurants => {
-      const data = restaurants.map(r => ({
-        ...r, description: r.description.substring(0, 50)
-      }))
-      return res.render('restaurants', { restaurants: data })
-    })
+    const categoryId = Number(req.query.categoryId) || ''
+
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || DEFAULT_LIMIT
+    const offset = getOffset(limit, page)
+
+    return Promise.all([
+      Restaurant.findAndCountAll({
+        include: Category,
+        nest: true,
+        where: {
+          ...categoryId ? { categoryId } : {}
+        },
+        limit,
+        offset,
+        raw: true
+      }),
+      Category.findAll({ raw: true })
+    ])
+      .then(([restaurants, categories]) => {
+        const data = restaurants.rows.map(r => ({
+          ...r, description: r.description.substring(0, 50)
+        }))
+        return res.render('restaurants', {
+          restaurants: data,
+          categories,
+          categoryId,
+          pagination: getPagination(limit, page, restaurants.count)
+        })
+      }).catch(err => next(err))
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
@@ -37,4 +59,4 @@ const adminController = {
   }
 }
 
-module.exports = adminController
+module.exports = restaurantController
