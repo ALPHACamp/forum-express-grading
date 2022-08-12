576984ea3198c8cd8911a2dddaac087b1ce1d246
Yen <p12427@gmail.com>
2022-08-12 16:44:00 +0800

    feat: add admin create category router

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -7,6 +7,17 @@ const categoryController = {
     })
       .then(categories => res.render('admin/categories', { categories }))
       .catch(err => next(err))
+  },
+
+  postCategory: (req, res, next) => {
+    const name = req.body.name.trim()
+    if (!name) throw new Error('Category name is required!')
+    return Category.create({ name })
+      .then(() => {
+        req.flash('success_messages', 'Category was successfully created.')
+        res.redirect('/admin/categories')
+      })
+      .catch(e => next(e))
   }
 }
 module.exports = categoryController
