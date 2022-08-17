b9a93c69dc7ce679a51e3d4d260f52bd406e9997
Yen <p12427@gmail.com>
2022-08-17 19:40:58 +0800

    feat: add admin update category router

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -1,11 +1,14 @@
 const { Category } = require('../models')
 
 const categoryController = {
+  // show all categories, and create/edit input according to req.params.id
   getCategories: (req, res, next) => {
-    return Category.findAll({
-      raw: true
-    })
-      .then(categories => res.render('admin/categories', { categories }))
+    return Promise
+      .all([
+        Category.findAll({ raw: true }),
+        req.params.id ? Category.findByPk(req.params.id, { raw: true }) : null
+      ])
+      .then(([categories, category]) => res.render('admin/categories', { categories, category }))
       .catch(err => next(err))
   },
 
@@ -18,6 +21,19 @@ const categoryController = {
         res.redirect('/admin/categories')
       })
       .catch(e => next(e))
+  },
+
+  putCategory: (req, res, next) => {
+    const name = req.body.name.trim()
+    if (!name) throw new Error('Category name is required!')
+    return Category.findByPk(req.params.id)
+      .then(category => {
+        if (!category) throw new Error("Category doesn't exist!")
+        return category.update({ name })
+      })
+      .then(() => res.redirect('/admin/categories'))
+      .catch(err => next(err))
   }
 }
+
 module.exports = categoryController
