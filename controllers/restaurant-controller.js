18d13ae070b131b5e8609b77fce383492c13fd72
Yen <p12427@gmail.com>
2022-08-18 12:29:06 +0800

    feat: add restaurants index page and router

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,6 +1,21 @@
+const { Restaurant, Category } = require('../models')
+
 const restaurantController = {
-  getRestaurants: (req, res) => {
-    res.render('restaurants')
+  // show all restaurants with 'category name'
+  getRestaurants: (req, res, next) => {
+    return Restaurant.findAll({
+      include: Category,
+      nest: true,
+      raw: true
+    }).then(restaurants => {
+      restaurants = restaurants.map(restaurant => {
+        if (restaurant.description.length >= 50) {
+          restaurant.description = restaurant.description.substring(0, 47) + '...'
+        }
+        return restaurant
+      })
+      return res.render('restaurants', { restaurants })
+    }).catch(e => next(e))
   }
 }
 module.exports = restaurantController
