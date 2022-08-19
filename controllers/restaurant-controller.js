50f290c7597753664d84cda577e8a8e18e645c03
Yen <p12427@gmail.com>
2022-08-19 16:54:28 +0800

    feat: add pagination on restaurants index page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,30 +1,40 @@
 const { Restaurant, Category } = require('../models')
+const { getOffset, getPagination } = require('../helpers/pagination-helper')
 
 const restaurantController = {
-  // show restaurants according to the selected category
+  // show restaurants according to the selected category and page
   getRestaurants: (req, res, next) => {
+    const DEFAULT_LIMIT_ITEMS_PER_PAGE = 9
     // no query (NaN) and All (0): categoryId = ''
     const categoryId = Number(req.query.categoryId) || ''
-    return Promise
-      .all([
-        Restaurant.findAll({
-          include: Category,
-          where: categoryId ? { categoryId } : {},
-          nest: true,
-          raw: true
-        }),
-        Category.findAll({ raw: true })
-      ])
-      .then(([restaurants, categories]) => {
-        restaurants = restaurants.map(restaurant => {
-          if (restaurant.description.length >= 50) {
-            restaurant.description = restaurant.description.substring(0, 47) + '...'
-          }
-          return restaurant
-        })
-        return res.render('restaurants', { restaurants, categories, categoryId })
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || DEFAULT_LIMIT_ITEMS_PER_PAGE
+    const offset = getOffset(limit, page)
+
+    return Promise.all([
+      Restaurant.findAndCountAll({
+        include: Category,
+        where: categoryId ? { categoryId } : {},
+        limit,
+        offset,
+        nest: true,
+        raw: true
+      }),
+      Category.findAll({ raw: true })
+    ]).then(([restaurants, categories]) => {
+      const data = restaurants.rows.map(restaurant => {
+        if (restaurant.description.length >= 50) {
+          restaurant.description = restaurant.description.substring(0, 47) + '...'
+        }
+        return restaurant
       })
-      .catch(e => next(e))
+      return res.render('restaurants', {
+        restaurants: data, // show restaurants
+        categories, // show categories navbar
+        categoryId, // show the selected category
+        pagination: getPagination(limit, page, restaurants.count) // show pagination
+      })
+    }).catch(e => next(e))
   },
 
   getRestaurant: async (req, res, next) => {
