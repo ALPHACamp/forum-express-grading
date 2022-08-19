6c2abeab2ed9542a4d9fc78a696785069a643bd3
rungo6 <t24331279@gmail.com>
2022-08-19 13:45:39 +0800

    feat:fix bug try4

--- migrations/20220805070236-add-price-to-restaurants.js
+++ migrations/20220805070236-add-price-to-restaurants.js
@@ -2,11 +2,11 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn('Restaurants', 'image', {
+    await queryInterface.addColumn('Restaurants', 'price', {
       type: Sequelize.STRING
     })
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.removeColumn('Restaurants', 'image')
+    await queryInterface.removeColumn('Restaurants', 'price')
   }
 }
