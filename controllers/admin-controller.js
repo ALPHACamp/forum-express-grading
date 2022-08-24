f69c2d7cba1cd5dabe58009d0b197452838d465c
unknown <harrychien@ecmaker.com>
2022-08-24 15:18:56 +0800

    feat: create restaurant

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -5,6 +5,27 @@ const adminController = {
     Restaurant.findAll({ raw: true })
       .then((restaurants) => res.render('admin/restaurants', { restaurants }))
       .catch((err) => next(err))
+  },
+  createRestaurant: (req, res) => {
+    res.render('admin/create-restaurant')
+  },
+  postRestaurant: (req, res, next) => {
+    const { name, tel, address, openingHours, description } = req.body
+
+    if (!name) throw new Error('餐廳名稱為必填')
+
+    Restaurant.create({
+      name,
+      tel,
+      address,
+      openingHours,
+      description
+    })
+      .then(() => {
+        req.flash('success_messages', '餐廳建立成功')
+        res.redirect('/admin/restaurants')
+      })
+      .catch((err) => next(err))
   }
 }
 
