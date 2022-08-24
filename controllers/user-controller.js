bb903b55fdef92d07f8b1cb6c5f63049d450d0ed
unknown <harrychien@ecmaker.com>
2022-08-24 12:21:51 +0800

    feat: flash msg && signup verification

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -6,17 +6,26 @@ const userController = {
   signUpPage: (req, res) => {
     res.render('signup')
   },
-  signUp: (req, res) => {
-    const { name, email, password } = req.body
-    bcrypt.hash(password, 10)
-      .then(hash => User.create({
-        name,
-        email,
-        password: hash
-      }))
+  signUp: (req, res, next) => {
+    const { name, email, password, passwordCheck } = req.body
+    if (password !== passwordCheck) throw new Error('密碼與驗證密碼不相同')
+    User.findOne({ where: { email } })
+      .then((user) => {
+        if (user) throw new Error('Email已註冊過')
+        return bcrypt.hash(password, 10)
+      })
+      .then((hash) =>
+        User.create({
+          name,
+          email,
+          password: hash
+        })
+      )
       .then(() => {
+        req.flash('success_messages', '帳號註冊成功')
         res.redirect('/signin')
       })
+      .catch((err) => next(err))
   }
 }
 
