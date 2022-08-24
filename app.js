64c75ed914b88a1a0979706e004dec9f79f301a5
unknown <harrychien@ecmaker.com>
2022-08-24 14:02:04 +0800

    feat: passport init & signin

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const routes = require('./routes')
 const hbs = require('express-handlebars')
 const flash = require('connect-flash')
 const session = require('express-session')
-
+const passport = require('./config/passport')
 const app = express()
 const port = process.env.PORT || 3000
 const SESSION_SECRET = 'secret'
@@ -15,6 +15,8 @@ app.use(express.urlencoded({ extended: true }))
 app.use(
   session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false })
 )
+app.use(passport.initialize())
+app.use(passport.session())
 app.use(flash())
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
---
+++ config/passport.js
@@ -0,0 +1,67 @@
+const passport = require('passport')
+const LocalStrategy = require('passport-local')
+const bcrypt = require('bcryptjs')
+const db = require('../models')
+const User = db.User
+
+passport.use(
+  // 選用本地端認證策略
+  new LocalStrategy(
+    /*
+    設定客製化選項
+    帳號用email
+    密碼用password
+    passReqToCallback: true，才可以把callback的第一個參數拿到req裡
+    這樣才可以呼叫req.flash()客製化訊息
+    */
+    {
+      usernameField: 'email',
+      passwordField: 'password',
+      passReqToCallback: true
+    },
+    // callback function 驗證使用者
+    (req, email, password, cb) => {
+      // 查詢User資料庫帳號是否存在
+      User.findOne({ where: { email } }).then((user) => {
+        // 帳號不存在處理
+        if (!user) {
+          return cb(
+            null,
+            false,
+            req.flash('error_messages', '帳號或密碼輸入錯誤')
+          )
+        }
+
+        bcrypt.compare(password, user.password).then((res) => {
+          // 密碼錯誤處理
+          if (!res) {
+            return cb(
+              null,
+              false,
+              req.flash('error_messages', '帳號或密碼輸入錯誤')
+            )
+          }
+          // 驗證通過
+          return cb(null, user)
+        })
+      })
+    }
+  )
+)
+
+// 序列化 - 將資料轉為可儲存形式的過程(process)
+passport.serializeUser((user, cb) => {
+  cb(null, user.id)
+})
+
+// 反序列化 - 在需要的時候，可以將資料恢復原先狀態
+passport.deserializeUser((id, cb) => {
+  User.findByPk(id).then((user) => {
+    // FIXME: 暫時添加console
+    user = user.toJSON()
+    console.log(user)
+    return cb(null, user)
+  })
+})
+
+module.exports = passport
