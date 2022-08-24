ef012165ae2d8f356798e872c15cdb3d57993066
unknown <harrychien@ecmaker.com>
2022-08-24 14:19:36 +0800

    feat: add header & footer

--- app.js
+++ app.js
@@ -4,11 +4,13 @@ const hbs = require('express-handlebars')
 const flash = require('connect-flash')
 const session = require('express-session')
 const passport = require('./config/passport')
+const handlebarsHelpers = require('./helpers/handlebars-helpers')
+const { getUser } = require('./helpers/auth-helpers')
 const app = express()
 const port = process.env.PORT || 3000
 const SESSION_SECRET = 'secret'
 
-app.engine('hbs', hbs({ extname: '.hbs' }))
+app.engine('hbs', hbs({ extname: '.hbs', helpers: handlebarsHelpers }))
 
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
@@ -21,6 +23,7 @@ app.use(flash())
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
   res.locals.error_messages = req.flash('error_messages')
+  res.locals.user = getUser(req)
   next()
 })
 app.use(routes)
