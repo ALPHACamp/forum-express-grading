1f1f7e2b3a00215606458472316e0e26f1a9434f
naluwan <street.baller06@gmail.com>
2022-08-25 22:37:38 +0800

    feat: add patchUser

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -4,8 +4,8 @@ const { imgurFileHandler } = require('../helpers/file-helpers')
 const adminController = {
   getRestaurants: (req, res, next) => {
     Restaurant.findAll({ raw: true })
-      .then((restaurants) => res.render('admin/restaurants', { restaurants }))
-      .catch((err) => next(err))
+      .then(restaurants => res.render('admin/restaurants', { restaurants }))
+      .catch(err => next(err))
   },
   createRestaurant: (req, res) => {
     res.render('admin/create-restaurant')
@@ -15,7 +15,7 @@ const adminController = {
     if (!name) throw new Error('餐廳名稱為必填')
     const { file } = req
     imgurFileHandler(file)
-      .then((filePath) =>
+      .then(filePath =>
         Restaurant.create({
           name,
           tel,
@@ -29,25 +29,25 @@ const adminController = {
         req.flash('success_messages', '餐廳建立成功')
         res.redirect('/admin/restaurants')
       })
-      .catch((err) => next(err))
+      .catch(err => next(err))
   },
   getRestaurant: (req, res, next) => {
     const { id } = req.params
     Restaurant.findByPk(id, { raw: true })
-      .then((restaurant) => {
+      .then(restaurant => {
         if (!restaurant) throw new Error('查無此餐廳')
         res.render('admin/restaurant', { restaurant })
       })
-      .catch((err) => next(err))
+      .catch(err => next(err))
   },
   editRestaurant: (req, res, next) => {
     const { id } = req.params
     Restaurant.findByPk(id, { raw: true })
-      .then((restaurant) => {
+      .then(restaurant => {
         if (!restaurant) throw new Error('查無此餐廳')
         res.render('admin/edit-restaurant', { restaurant })
       })
-      .catch((err) => next(err))
+      .catch(err => next(err))
   },
   putRestaurant: (req, res, next) => {
     const { id } = req.params
@@ -70,22 +70,38 @@ const adminController = {
         req.flash('success_messages', '更新餐廳資料成功')
         res.redirect('/admin/restaurants')
       })
-      .catch((err) => next(err))
+      .catch(err => next(err))
   },
   deleteRestaurant: (req, res, next) => {
     const { id } = req.params
     return Restaurant.findByPk(id)
-      .then((restaurant) => {
+      .then(restaurant => {
         if (!restaurant) throw new Error('查無此餐廳')
         return restaurant.destroy()
       })
       .then(() => res.redirect('/admin/restaurants'))
-      .catch((err) => next(err))
+      .catch(err => next(err))
   },
   getUsers: (req, res, next) => {
-    User.findAll({ raw: true })
-      .then((users) => res.render('admin/restaurants', { users }))
-      .catch((err) => next(err))
+    return User.findAll({ raw: true })
+      .then(users => res.render('admin/restaurants', { users }))
+      .catch(err => next(err))
+  },
+  patchUser: (req, res, next) => {
+    const { id } = req.params
+    return User.findByPk(id)
+      .then(user => {
+        if (user.name === 'admin') {
+          req.flash('error_messages', '禁止變更 root 權限')
+          return res.redirect('back')
+        }
+        user.update({ isAdmin: !user.isAdmin })
+          .then(() => {
+            req.flash('success_messages', '使用者權限變更成功')
+            res.redirect('/admin/users')
+          })
+      })
+      .catch(err => next(err))
   }
 }
 
--- helpers/file-helpers.js
+++ helpers/file-helpers.js
@@ -4,24 +4,24 @@ const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
 
 imgur.setClientId(IMGUR_CLIENT_ID)
 
-const localFileHandler = (file) => {
+const localFileHandler = file => {
   return new Promise((resolve, reject) => {
     if (!file) return resolve(null)
     const fileName = `upload/${file.originalname}`
     return fs.promises
       .readFile(file.path)
-      .then((data) => fs.promises.writeFile(fileName, data))
+      .then(data => fs.promises.writeFile(fileName, data))
       .then(() => resolve(`/${fileName}`))
-      .catch((err) => reject(err))
+      .catch(err => reject(err))
   })
 }
 
 const imgurFileHandler = file => {
-return new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
     if (!file) return resolve(null)
     return imgur.uploadFile(file.path)
-      .then((img) => resolve(img?.link || null))
-      .catch((err) => reject(err))
+      .then(img => resolve(img?.link || null))
+      .catch(err => reject(err))
   })
 }
 
