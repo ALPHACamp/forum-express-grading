428ab9127e3d92bad47a156f621cfb4eb039e1ab
naluwan <street.baller06@gmail.com>
2022-08-29 17:12:32 +0800

    feat: add ifCond hbs helper & update category selector

--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -1,5 +1,9 @@
 const dayjs = require('dayjs')
 module.exports = {
   // 取得當年年份作為currentYear屬性值，並導出
-  currentYear: () => dayjs().year()
+  currentYear: () => dayjs().year(),
+  // 因為在function裡面會用到this，所以不能用箭頭韓式，否則this的對象會有錯誤
+  ifCond: function(a, b, options) {
+    return a === b ? options.fn(this) : options.inverse(this)
+  }
 }
