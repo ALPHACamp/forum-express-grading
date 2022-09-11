28f1c7072be54a7a53d137079dd330604b9dbb78
Eric LIAO <d1044171009@gmail.com>
2022-09-12 01:03:48 +0800

    feat: add admin index page

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,9 @@ const express = require('express')
 const router = express.Router()
 
 const restaurantController = require('../controllers/restaurant-controller')
+const admin = require('./modules/admin')
 
-// router.get('/', (req, res) => {
-//   res.send('Hello World!')
-// })
-
+router.use('/admin', admin)
 router.get('/restaurants', restaurantController.getRestaurants)
 // fallback 路由
 router.use('/', (req, res) => res.redirect('/restaurants'))
