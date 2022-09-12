cc2f30eeb82a39a665a6726f830e5918f8c903bc
parker3216 <parker3216@hotmail.com>
2022-09-12 15:57:31 +0800

    feat:user signup

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const router = express.Router()
-// 載入 controller
-const restController = require('../controllers/restaurant-controller')
-// 載入 admin.js
-const admin = require('./modules/admin')
+const admin = require('./modules/admin')// 載入 admin.js
+const restController = require('../controllers/restaurant-controller')// 載入 controller
+const userController = require('../controllers/user-controller')
 
 router.use('/admin', admin)
+
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
+
 // 新增前台網址路由//匹配條件多的路由要寫在前面
 router.get('/restaurants', restController.getRestaurants)
 router.use('/', (req, res) => res.redirect('/restaurants'))
