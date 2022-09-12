ba79833abcca97110490842a4b57529a4e9cadb9
DESKTOP-2C80JIU\highk <ckai.trans@gmail.com>
2022-09-12 18:42:01 +0800

    feat: add admin index page

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const admin = require('./modules/admin')
 const restController = require('../controllers/restaurant-controller')
 const router = express.Router()
+
+router.use('/admin', admin)
 
 router.get('/restaurants', restController.getRestaurants)
 
