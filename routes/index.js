6a8ce73f20225ecc3c5f33de033940c353f4f9f2
parker3216 <parker3216@hotmail.com>
2022-09-12 11:45:31 +0800

    feat:add index page

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
+// 載入 controller
+const restController = require('../controllers/restaurant-controller')
 
-router.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// 新增前台網址路由//匹配條件多的路由要寫在前面
+router.get('/restaurants', restController.getRestaurants)
+router.use('/', (req, res) => res.redirect('/restaurants'))
 
 module.exports = router
