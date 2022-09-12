5f15b9807141b04e63496c200a67302d86435b83
AlleyChang <yifanchang0602@gmail.com>
2022-09-12 22:46:29 +0800

    feat:add flash messages & signup verification

--- app.js
+++ app.js
@@ -1,15 +1,31 @@
 const express = require('express')
-const routes = require('./routes')
 const engine = require('express-handlebars')
+const session = require('express-session')
+const flash = require('connect-flash')
+const routes = require('./routes')
+
 const app = express()
 const port = process.env.PORT || 3000
 const db = require('./models')
-
+const SESSION_SECRET = 'MySecret'
 app.engine('hbs', engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
-app.use(routes)
 
+
+app.use(session({
+  secret: SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
+app.use(routes)
 app.listen(port, () => {
   console.info(`Example app listening on port ${port}!`)
 })
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const restController = require('../controllers/restaurant-controller')
 const admin = require('./modules/admin')
 const userController = require('../controllers/user-controller')
+const { generalErrorHandler } = require('../middleware/error-handler')
 
 router.use('/admin', admin)
 router.get('/signup', userController.signUpPage)
@@ -10,8 +11,6 @@ router.post('/signup', userController.signUp)
 // 若接收到的請求路徑=restaurants則交由controller處理
 router.get('/restaurants', restController.getRestaurants)
 
-router.use('/', (req, res) => {
-  res.redirect('/restaurants')
-})
-
+router.use('/', (req, res) => res.redirect('/restaurants'))
+router.use('/', generalErrorHandler)
 module.exports = router
