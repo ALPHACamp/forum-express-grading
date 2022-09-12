23237681447b7bc768beb644ea55e15aa5a8a8ea
parker3216 <parker3216@hotmail.com>
2022-09-12 21:15:29 +0800

    feat: user model add is_admin

--- middleware/auth.js
+++ middleware/auth.js
@@ -10,6 +10,8 @@ const authenticatedAdmin = (req, res, next) => {
   // if (req.isAuthenticated)
   if (ensureAuthenticated(req)) {
     if (getUser(req).isAdmin) return next()
+    res.redirect('/')
+  } else {
     res.redirect('/signin')
   }
 }
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -6,6 +6,6 @@ const { authenticatedAdmin } = require('../../middleware/auth') // 引入 auth.j
 
 // 新增後台網址路由//匹配條件多的路由要寫在前面
 router.get('/restaurants', authenticatedAdmin, adminController.getRestaurants)
-router.use('', (req, res) => res.redirect('/admin/restaurants'))
+router.use('/', (req, res) => res.redirect('/admin/restaurants'))
 
 module.exports = router
