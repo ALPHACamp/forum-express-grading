b345f05d04e1b13b3286551386bae5dc09ba3330
AlleyChang <yifanchang0602@gmail.com>
2022-09-12 15:05:37 +0800

    feat: add admin route

---
+++ controllers/admin-controller.js
@@ -0,0 +1,6 @@
+const adminController = {
+  getRestaurants: (req, res) => {
+    res.render('admin/restaurants')
+  }
+}
+module.exports = adminController
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const router = express.Router()
-const restaurantController = require('../controllers/restaurant-controller')
+const restController = require('../controllers/restaurant-controller')
+const admin = require('./modules/admin')
 
-router.get('/restaurants', restaurantController.getRestaurants) //若接收到的請求路徑=restaurants則交由controller處理
+router.use('/admin', admin)
+// 若接收到的請求路徑=restaurants則交由controller處理
+router.get('/restaurants', restController.getRestaurants)
 
 router.use('/', (req, res) => {
   res.redirect('/restaurants')
 })
+
 module.exports = router
