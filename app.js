1e1d3e450f12df868cd7568ccb101c962cdfba54
Kris <krischiang0301@gmail.com>
2022-09-13 17:26:35 +0800

    feat: passport init & signin

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const routes = require('./routes')
 const exphbs = require('express-handlebars')
 const flash = require('connect-flash')
 const session = require('express-session')
+const passport = require('passport')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -17,6 +18,9 @@ app.use(session({
   resave: false,
   saveUninitialized: false
 }))
+app.use(passport.initialize())
+app.use(passport.session())
+
 app.use(flash())
 app.use((req, res, next) => {
   res.locals.success_msg = req.flash('success_msg')
