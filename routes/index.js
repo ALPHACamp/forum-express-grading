1714aa398237fef970d5b628e16b4cd5d5173f6c
ccj777 <002ntou@gmail.com>
2022-09-13 15:37:54 +0800

    feat:add index page

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
+const restController = require('../controllers/restaurant-controller')
 
-router.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+router.get('/restaurants', restController.getRestaurants)
+
+// fallback route
+router.use('/', (req, res) => res.redirect('/restaurants'))
 
 module.exports = router
