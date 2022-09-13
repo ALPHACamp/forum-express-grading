f74e79b347ec6c8cafdc51f0559defa6bd601e0e
Kris <krischiang0301@gmail.com>
2022-09-13 15:11:40 +0800

    feat: flash msg & signup verificaion

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 const express = require('express')
 const routes = require('./routes')
 const exphbs = require('express-handlebars')
+const flash = require('connect-flash')
+const session = require('express-session')
 
 const app = express()
 const port = process.env.PORT || 3000
+const SESSION_SECRET = 'secret'
 
 app.engine('hbs', exphbs({ extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(express.urlencoded({ extended: true }))
-
+app.use(session({
+  secret: SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.error_msg = req.flash('error_msg')
+  next()
+})
 app.use(routes)
 
 app.listen(port, () => {
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -7,8 +7,14 @@ const userController = {
   signUpPage: (req, res) => {
     res.render('signup')
   },
-  signUp: (req, res) => {
-    bcrypt.hash(req.body.password, 10)
+  signUp: (req, res, next) => {
+    if (req.body.password !== req.body.passwordCheck) { throw new Error('Password 跟 Password Check 不一樣!') }
+    User.findOne({ where: { email: req.body.email } })
+      .then(user => {
+        if (user) throw new Error('Email 已經註冊過了')
+        return bcrypt.hash(req.body.password, 10)
+      })
+
       .then(hash => {
         User.create({
           name: req.body.name,
@@ -17,8 +23,10 @@ const userController = {
         })
       })
       .then(() => {
+        req.flash('success_msg', '成功註冊帳號')
         res.redirect('/signin')
       })
+      .catch(err => next(err))
   }
 }
 
