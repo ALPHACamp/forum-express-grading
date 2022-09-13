16e97e41fc5be61b26a418b05838f04ec885be82
Eagle1630 <eagle163013@gmail.com>
2022-09-13 16:56:02 +0800

    feat: add header & footer

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const port = process.env.PORT || 3000
 const session = require('express-session')
 const flash = require('connect-flash')
 const passport = require('./config/passpot ')
-
+const { getUser } = require('./helpers/auth-helper')
+const { currentYear } = require('./helpers/handlebars-helpers')
 // 註冊 Handlebars 樣板引擎，並指定副檔名為 .hbs
 app.engine('hbs', handlebars({ extname: '.hbs' }))
 // 設定使用 Handlebars 做為樣板引擎
@@ -20,16 +21,20 @@ app.use(session({
   resave: false,
   saveUninitialized: false
 }))
+
 // 啟用connect-flash
 app.use(flash())
-app.use((req, res, next) => {
-  res.locals.success_messages = req.flash('success_messages')
-  res.locals.error_messages = req.flash('error_messages')
-  next()
-})
 // 設定 passport
 app.use(passport.initialize())
 app.use(passport.session())
+
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  res.locals.user = getUser(req)
+  res.locals.currentYear = currentYear
+  next()
+})
 
 app.use(routes)
 
