271ea69a511578ae94649b14811c36fe1e54c17c
DESKTOP-2C80JIU\highk <ckai.trans@gmail.com>
2022-09-13 22:29:54 +0800

    feat: flash msg & signup verification

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const flash = require('connect-flash')
 const routes = require('./routes')
+const session = require('express-session')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -9,6 +15,20 @@ app.engine('hbs', exphbs({ extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(express.urlencoded({ extended: true }))
+
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}))
+
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
+
 app.use(routes)
 
 app.listen(port, () => {
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -5,14 +5,24 @@ const userController = {
   signUpPage: (req, res) => {
     res.render('signup')
   },
-  signUp: (req, res) => {
-    bcrypt.hash(req.body.password, 10)
+  signUp: (req, res, next) => {
+    const { name, email, password, passwordCheck } = req.body
+    if (password !== passwordCheck) throw new Error('Passwords do not match!')
+    User.findOne({ where: { email: req.body.email } })
+      .then(user => {
+        if (user) throw new Error('Email already exists!')
+        return bcrypt.hash(password, 10)
+      })
       .then(hash => User.create({
-        name: req.body.name,
-        email: req.body.email,
+        name: name,
+        email: email,
         password: hash
       }))
-      .then(() => res.redirect('/signin'))
+      .then(() => {
+        req.flash('success_messages', '成功註冊帳號！')
+        res.redirect('/signin')
+      })
+      .catch(err => next(err))
   }
 }
 
