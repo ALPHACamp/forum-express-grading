44b03277957487ef2422443b7b456da4fadf4cb9
ccj777 <002ntou@gmail.com>
2022-09-15 02:22:39 +0800

    change routes order

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,9 @@ const restController = require('../controllers/restaurant-controller')
 const admin = require('./modules/admin')
 const UserController = require('../controllers/user-controller')
 
+router.use('/admin', admin)
 router.get('/signup', UserController.signUpPage)
 router.post('/signup', UserController.signUp)
-router.use('/admin', admin)
 router.get('/restaurants', restController.getRestaurants)
 router.use('/', (req, res) => res.redirect('/restaurants')) // fallback route
 
