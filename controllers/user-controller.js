416eab0b55a4c7d66401adbf51ece31308e737fd
ccj777 <002ntou@gmail.com>
2022-09-15 04:55:31 +0800

    feat: flsh messages & signup verification

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -5,16 +5,26 @@ const UserController = {
   signUpPage: (req, res) => {
     res.render('signup')
   },
-  signUp: (req, res) => {
-    bcrypt.hash(req.body.password, 10)
+  signUp: (req, res, next) => {
+    //  如果兩次輸入的密碼不同，就建立一個 Error 物件並拋出
+    if (req.body.password !== req.body.passwordCheck) throw new Error('password do not match!')
+
+    User.findOne({ where: { email: req.body.email } })
+      .then(user => {
+        console.log(req.body.email)
+        if (user) throw new Error('Email already exist!') // 若用此email註冊的user已存在，建立一個Error物件並拋出
+        return bcrypt.hash(req.body.password, 10)
+      })
       .then(hash => User.create({
         name: req.body.name,
-        email: req.body.name,
+        email: req.body.email,
         password: hash
       }))
       .then(() => {
+        req.flash('success_messages', '成功註冊帳號!')
         res.redirect('/signin')
       })
+      .catch(err => next(err))
   }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,13 @@ const router = express.Router()
 const restController = require('../controllers/restaurant-controller')
 const admin = require('./modules/admin')
 const UserController = require('../controllers/user-controller')
+const { generalErrorHandler } = require('../middleware/error-handeler')
 
 router.use('/admin', admin)
 router.get('/signup', UserController.signUpPage)
 router.post('/signup', UserController.signUp)
 router.get('/restaurants', restController.getRestaurants)
 router.use('/', (req, res) => res.redirect('/restaurants')) // fallback route
+router.use('/', generalErrorHandler)
 
 module.exports = router
