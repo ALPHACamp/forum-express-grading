64433867576f7f4aac0d23af14d21bf93aa88e4f
Raven <reevan0613@gmail.com>
2022-09-14 18:49:57 +0800

    feat: update seed files

--- migrations/20220914085048-add-category-id-to-restaurant.js
+++ migrations/20220914085048-add-category-id-to-restaurant.js
@@ -4,7 +4,7 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.addColumn('Restaurants', 'category_id', {
       type: Sequelize.INTEGER,
-      allowNull: true,
+      allowNull: false,
       references: {
         model: 'Categories',
         key: 'id'
