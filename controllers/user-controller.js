5b2faa499d459ac4145703584ea9732391643d97
Nancy-Hsu <yutinghsu0411@gmail.com>
2022-09-14 16:29:25 +0800

    feat: add flash message and passport

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -4,13 +4,35 @@ const { User } = db
 
 const userController = {
   signUpPage: (req, res) => res.render('signUp'),
-  signUp: (req, res) => {
-    const { name, email, password } = req.body
-    return User.create({
-      name,
-      email,
-      password: bcrypt.hashSync(password, bcrypt.genSaltSync(10))
-    }).then(() => res.redirect('/signin'))
+  signUp: (req, res, next) => {
+    const { name, email, password, passwordCheck } = req.body
+    if (password !== passwordCheck) throw new Error('Passwords do not match!')
+    User.findOne({ where: { email } })
+      .then(user => {
+        if (user) throw new Error('Email already exists!')
+        return User.create({
+          name,
+          email,
+          password: bcrypt.hashSync(password, bcrypt.genSaltSync(10))
+        })
+      })
+      .then(() => {
+        req.flash('success_msg', '成功註冊帳號！')
+        res.redirect('/signin')
+      })
+      .catch(err => next(err))
+  },
+  signInPage: (req, res) => {
+    res.render('signin')
+  },
+  signIn: (req, res) => {
+    req.flash('success_msg', '成功登入！')
+    res.redirect('/restaurants')
+  },
+  logout: (req, res) => {
+    req.flash('success_msg', '登出成功！')
+    req.logout()
+    res.redirect('/signin')
   }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,20 @@ const router = express.Router()
 const resController = require('../controllers/restaurant-controller')
 const admin = require('./modules/admin')
 const userController = require('../controllers/user-controller')
+const { generalErrorHandler } = require('../middleware/error-handler')
+const passport = require('../config/passport')
 
 router.use('/admin', admin)
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
+router.get('/signin', userController.signInPage)
+router.post('/signin', passport.authenticate('local', {
+  failureRedirect: '/signin', failureFlash: true
+}), userController.signIn)
+router.get('/logout', userController.logout)
+
 router.get('/restaurants', resController.getRestaurants)
-router.use('/', (req, res) => res.redirect('/restaurants'))
+router.get('/', (req, res) => res.redirect('/restaurants'))
+router.use('/', generalErrorHandler)
 
 module.exports = router
