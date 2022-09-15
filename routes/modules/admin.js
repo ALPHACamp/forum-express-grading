fd132fea351538b58d8563722806523fccacfd2b
stu96534 <stu96534@gmail.com>
2022-09-16 01:26:10 +0800

    feat: delete restaurant

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -8,8 +8,10 @@ router.get('/restaurants/create', adminController
 router.get('/restaurants/:id/edit', adminController.editRestaurant)
 router.get('/restaurants/:id', adminController.getRestaurant)
 router.put('/restaurants/:id', adminController.putRestaurant)
+router.delete('/restaurants/:id', adminController.deleteRestaurant)
 router.get('/restaurants', adminController.getRestaurants)
 router.post('/restaurants', adminController.postRestaurant)
+
 router.use('/', (req, res) => res.redirect('/admin/restaurants'))
 
 module.exports = router
