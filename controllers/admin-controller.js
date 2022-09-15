a7d381963414ee2bf6033a1f630fb31df2415d99
robert1074004 <abc50113@yahoo.com.tw>
2022-09-15 22:44:37 +0800

    feat: admin restaurant page

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -17,6 +17,14 @@ const adminController = {
         res.redirect('/admin/restaurants')
       })
       .catch(err => next(err))
+  },
+  getRestaurant: (req, res, next) => {
+    Restaurant.findByPk(req.params.id, { raw: true })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        res.render('admins/restaurant', { restaurant })
+      })
+      .catch(err => next(err))
   }
 }
 module.exports = adminController
