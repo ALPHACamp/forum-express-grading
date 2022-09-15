27f805dba853ffd4ddd53e67c2863fda5da98fe2
LiyLi1122 <teppei.252033@gmail.com>
2022-09-15 17:55:23 +0800

    feat: modify admin restaurants page

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,6 +1,12 @@
+const { Restaurant } = require('../models') // === require('../models/index')
+
 const adminController = {
-  getRestaurants: (req, res) => {
-    res.render('admin')
+  getRestaurants: (req, res, next) => {
+    Restaurant.findAll({ // [{}, {}]
+      raw: true // 轉成單純 JS 物件，不轉也可以但要在.dataValues 取值
+    })
+      .then(restaurants => res.render('admin/restaurants', { restaurants }))
+      .catch(error => next(error))
   }
 }
 
