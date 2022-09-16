c223385b8e5d187dfe32fc9da1c2ec934a39548d
Nancy-Hsu <yutinghsu0411@gmail.com>
2022-09-16 15:49:44 +0800

    feat: reinsatll faker

--- seeders/20220916035009-restaurants-seed-file.js
+++ seeders/20220916035009-restaurants-seed-file.js
@@ -1,14 +1,14 @@
 'use strict'
-// const { faker } = require('@faker-js/faker')
+const faker = require('faker')
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('Restaurants', Array.from({ length: 50 }, () => ({
-      name: 'restaurant',
-      tel: '09171653993',
-      address: 'USA',
-      opening_hours: '10:10',
+      name: faker.name.findName(),
+      tel: faker.phone.phoneNumber(),
+      address: faker.address.streetAddress(),
+      opening_hours: '08:00',
       image: `https://loremflickr.com/320/240/restaurant,food/?random=${Math.random() * 100}`,
-      description: 'good place',
+      description: faker.lorem.text(),
       created_at: new Date(),
       updated_at: new Date()
     }))
