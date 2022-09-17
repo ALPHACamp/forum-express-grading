4117790d2b6eaae355b5f9208856dd35f0435125
Eagle1630 <eagle163013@gmail.com>
2022-09-17 16:03:04 +0800

    fix migration

--- migrations/20220916024112-add-image-to-restaurants.js
+++ migrations/20220916024112-add-image-to-restaurants.js
@@ -8,6 +8,6 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.removeColumn('Restaurnats', ' image')
+    await queryInterface.removeColumn('Restaurants', ' image')
   }
 }
