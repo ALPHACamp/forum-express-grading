c2ca407dcb51ba41aaa9a8af525eac4091b4eb6c
YangYGYang <ginayoung82@gmail.com>
2022-09-17 13:29:08 +0800

    flash msg & signup verification

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const routes = require('./routes')
 const handlebars = require('express-handlebars')
+const flash = require('connect-flash')
+const session = require('express-session')
 
 const app = express()
 const port = process.env.PORT || 3000
+const SESSION_SECRET = 'secret'
 
 const db = require('./models')
+app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
+app.use(flash())
 
 app.engine('hbs', handlebars({ extname: '.hbs' }))
 app.set('view engine', 'hbs')
--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,14 @@ const router = express.Router()
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
 const admin = require('./modules/admin')
+const { generalErrorHandler } = require('../middleware/error-handler')
 
 router.use('/admin', admin)
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
 router.get('/restaurant', restController.getRestaurants)
 router.use('/', (req, res) => { res.redirect('/restaurant') })
+
+router.use('/', generalErrorHandler)
 
 module.exports = router
