1ce6294b3ae526380ce672b3304918c11ed440a3
LoisChen68 <a8283626@gmail.com>
2022-09-17 18:54:23 +0800

    feat: show comments on user page
    feat: update comments seed files

--- routes/index.js
+++ routes/index.js
@@ -23,6 +23,7 @@ router.get('/restaurants', authenticated, restController.getRestaurants)
 router.delete('/comments/:id', authenticatedAdmin, commentController.deleteComment)
 router.post('/comments', authenticated, commentController.postComment)
 router.use('/', (req, res) => res.redirect('/restaurants'))
+
 router.use('/', generalErrorHandler)
 
 module.exports = router
