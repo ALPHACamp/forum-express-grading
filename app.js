81fe2b5aa2b0f7f3aa086d659a3413ba5f10cdce
kany <kany102030@gmail.com>
2022-09-17 20:45:26 +0800

    feat: add login

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const routes = require('./routes')
 const exphbs = require('express-handlebars')
 const flash = require('connect-flash')
 const session = require('express-session')
+const passport = require('./config/passport')
 const app = express()
 const port = process.env.PORT || 3000
 const db = require('./models')
@@ -13,7 +14,8 @@ app.use(express.urlencoded({ extended: true }))
 
 const SESSION_SECRET = 'secret'
 app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
-
+app.use(passport.initialize())
+app.use(passport.session())
 app.use(flash())
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const passport = require('../config/passport')
 const router = express.Router()
 const restController = require('../controllers/restaurant-controller')
 const admin = require('./modules/admin')
@@ -8,6 +9,11 @@ router.use('/admin', admin)
 
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
+
+router.get('/signin', userController.signInPage)
+router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn) // 注意是 post
+router.get('/logout', userController.logout)
+
 router.get('/restaurants', restController.getRestaurants)
 router.use('/', (req, res) => res.redirect('/restaurants'))
 
