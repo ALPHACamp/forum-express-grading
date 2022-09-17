e9f803f2bdcaab7d68e2a7118f682cfba13524cc
parker3216 <parker3216@hotmail.com>
2022-09-17 20:02:42 +0800

    feat:edit Users

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -93,7 +93,7 @@ const adminController = {
       .catch(err => next(err))
   },
   getUsers: (req, res, next) => {
-    User.findAll(
+    return User.findAll(
       { raw: true }
     )
       .then(users => res.render('admin/users', { users }))
