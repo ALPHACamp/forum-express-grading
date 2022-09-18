95751765544c790e2a6d301c96bbaade22638072
darrenjon <darrenlin23@gmail.com>
2022-09-18 19:23:28 +0800

    feat: category create

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -1,4 +1,5 @@
 const { Category } = require('../models')
+
 const categoryController = {
   getCategories: (req, res, next) => {
     return Category.findAll({
@@ -6,6 +7,15 @@ const categoryController = {
     })
       .then(categories => res.render('admin/categories', { categories }))
       .catch(err => next(err))
+  },
+  postCategory: (req, res, next) => {
+    const { name } = req.body
+    if (!name) throw new Error('Category name is required!')
+
+    return Category.create({ name })
+      .then(() => res.redirect('/admin/categories'))
+      .catch(err => next(err))
   }
 }
+
 module.exports = categoryController
