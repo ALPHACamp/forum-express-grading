2939e304a3ddd9aad2324e300c26b92d277e7d55
darrenjon <darrenlin23@gmail.com>
2022-09-18 22:25:45 +0800

    feat: add restaurant detail page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -11,10 +11,20 @@ const restaurantController = {
         ...r,
         description: r.description.substring(0, 50)
       }))
-      return res.render('restaurants', {
-        restaurants: data
-      })
+      return res.render('restaurants', { restaurants: data })
     })
+  },
+  getRestaurant: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, {
+      raw: true,
+      nest: true,
+      include: Category
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        res.render('restaurant', { restaurant })
+      })
+      .catch(err => next(err))
   }
 }
 
