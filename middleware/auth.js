78d6d50f797c317323752b227c63786d52067cb2
Eagle1630 <eagle163013@gmail.com>
2022-09-18 20:39:55 +0800

    pass local test

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,8 +1,8 @@
 
-const { getUser, ensureAuthenticated } = require('../helpers/auth-helper')
+const helpers = require('../helpers/auth-helper')
 
 const authenticated = (req, res, next) => {
-  if (ensureAuthenticated(req)) {
+  if (helpers.ensureAuthenticated(req)) {
     return next()
   }
   req.flash('error_messages', '請先進行登入')
@@ -11,9 +11,9 @@ const authenticated = (req, res, next) => {
 
 const authenticatedAdmin = (req, res, next) => {
   // 是否有登入
-  if (ensureAuthenticated(req)) {
+  if (helpers.ensureAuthenticated(req)) {
     // 是否是管理者
-    if (getUser(req).isAdmin) return next()
+    if (helpers.getUser(req).isAdmin) return next()
 
     return res.redirect('/restaurants')
   }
