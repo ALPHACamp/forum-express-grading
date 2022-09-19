875b877565fdae7a2a53c58ef27dc1c580401e16
Yen <p12427@gmail.com>
2022-09-19 14:11:50 +0800

    feat: create favorite model

---
+++ migrations/20220919040428-create-favorite.js
@@ -0,0 +1,40 @@
+'use strict'
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('Favorites', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      user_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Users',
+          key: 'id'
+        }
+      },
+      restaurant_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Restaurants',
+          key: 'id'
+        }
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('Favorites')
+  }
+}
