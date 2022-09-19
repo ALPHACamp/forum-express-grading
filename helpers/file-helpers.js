db179ba7c721114bf00383b15af7b6b438d49354
robert1074004 <abc50113@yahoo.com.tw>
2022-09-19 17:10:59 +0800

    feat: add imgur function

--- helpers/file-helpers.js
+++ helpers/file-helpers.js
@@ -1,4 +1,17 @@
 const fs = require('fs')
+const imgur = require('imgur')
+const IMGUR_CLIENT_ID = process.env.IMGUR_CLIENT_ID
+imgur.setClientId(IMGUR_CLIENT_ID)
+const imgurFileHandler = file => {
+  return new Promise((resolve, reject) => {
+    if (!file) return resolve(null)
+    return imgur.uploadFile(file.path)
+      .then(img => {
+        resolve(img?.link || null)
+      })
+      .catch(err => reject(err))
+  })
+}
 const localFileHandler = file => {
   return new Promise((resolve, reject) => {
     if (!file) return resolve(null)
@@ -10,5 +23,6 @@ const localFileHandler = file => {
   })
 }
 module.exports = {
+  imgurFileHandler,
   localFileHandler
 }
