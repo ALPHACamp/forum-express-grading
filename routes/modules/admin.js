b30a3e2530b96b203b7e65dbb7a579b05001192b
m790101 <m790101@hotmail.com>
2022-09-19 23:54:09 +0800

    feat: modify config for heroku

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,6 +1,15 @@
 const express = require('express')
 const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
+// const upload = require('../../middleware/multer')
+const upload = require('../../middleware/multer')
+
 router.get('/restaurants', adminController.getRestaurants)
-router.use('/', (req, res) => res.redirect('/admin/restaurants'))
+router.get('', (req, res) => res.redirect('/admin/restaurants'))
+router.get('/restaurants/create', adminController.createRestaurant)
+router.post('/restaurants', upload.single('image'), adminController.postRestaurant)
+router.get('/restaurants/:id/edit', adminController.editRestaurant)
+router.delete('/restaurants/:id', adminController.deleteRestaurant)
+router.put('/restaurants/:id', upload.single('image'), adminController.putRestaurant)
+router.get('/restaurants/:id', adminController.getRestaurant)
 module.exports = router
