b2690ec14b61e4417f804ea2dea81a375e35e57e
Eagle1630 <eagle163013@gmail.com>
2022-09-19 16:20:43 +0800

    feat: update seed date

--- seeders/20220919072810-restaurants-seed-file.js
+++ seeders/20220919072810-restaurants-seed-file.js
@@ -3,6 +3,12 @@ const faker = require('faker')
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    // 獲取 Categories 表單資料
+    const categories = await queryInterface.sequelize.query(
+      'SELECT id FROM Categories;',
+      { type: queryInterface.sequelize.QueryTypes.SELECT }
+    )
+
     await queryInterface.bulkInsert('Restaurants',
       Array.from({ length: 50 }, () => ({
         name: faker.name.findName(),
@@ -11,7 +17,8 @@ module.exports = {
         opening_hours: '08:00',
         image: `https://loremflickr.com/320/240/restaurant,food/?random=${Math.random() * 100}`,
         created_at: new Date(),
-        updated_at: new Date()
+        updated_at: new Date(),
+        category_id: categories[Math.floor(Math.random() * categories.length)].id
       })))
   },
 
