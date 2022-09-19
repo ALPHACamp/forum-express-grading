f5ab8030dd3bc5301d7b6bf7237f0bef9d35b0cf
Raven <reevan0613@gmail.com>
2022-09-19 21:02:14 +0800

    feat: add dashboard page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -47,7 +47,26 @@ const restaurantController = {
     })
       .then(restaurant => {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
-        res.render('restaurant', {
+        return restaurant.increment('viewCounts')
+      })
+      .then(restaurant => {
+        return res.render('restaurant', {
+          restaurant: restaurant.toJSON()
+        })
+      })
+      .catch(err => next(err))
+  },
+  getDashboard: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, {
+      include: [
+        Category,
+        { model: Comment }
+      ],
+      nest: true
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        return res.render('dashboard', {
           restaurant: restaurant.toJSON()
         })
       })
