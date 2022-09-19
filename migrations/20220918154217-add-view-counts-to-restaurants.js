3e96cbb72f92f589c2a5cd963c783cf39155863f
darrenjon <darrenlin23@gmail.com>
2022-09-19 21:50:59 +0800

    feat: add restaurant dashboard & page view counts

---
+++ migrations/20220918154217-add-view-counts-to-restaurants.js
@@ -0,0 +1,14 @@
+'use strict'
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('Restaurants', 'view_counts', {
+      type: Sequelize.INTEGER,
+      defaultValue: 0
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('Restaurants', 'view_counts')
+  }
+}
