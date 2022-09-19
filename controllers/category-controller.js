2ee2883c3939b953b720c8f7b8fbd0eeba1804ab
Chen Yi-Cheng <art787472@gmail.com>
2022-09-19 13:54:22 +0800

    feat: edit category

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -4,10 +4,12 @@ exports.getCategories = async (req, res, next) => {
  try {
     const pageSize = 15
     const currPage = +req.query.page || 1
-    const { count, rows } = await Category.findAndCountAll({
-      offset: (currPage - 1) * pageSize,
-      limit: pageSize
-    })
+    const [{ count, rows }, category] = await Promise.all([Category.findAndCountAll({
+        offset: (currPage - 1) * pageSize,
+        limit: pageSize
+      }),
+      req.params.id ? Category.findByPk(req.params.id, { raw: true }) : null
+    ])
     if (currPage > Math.ceil(count / pageSize)) {
       req.flash('error_messages', '頁面不存在')
       return res.redirect('/admin/categories')
@@ -16,7 +18,7 @@ exports.getCategories = async (req, res, next) => {
     const categories = rows.map(({ dataValues }) => dataValues)
     const nextPage = currPage === pages.length ? 0 : currPage + 1
     const prevPage = currPage - 1
-    return res.render('admin/categories', { categories, pages, nextPage, prevPage, currPage })
+    return res.render('admin/categories', { categories, category, pages, nextPage, prevPage, currPage })
   } catch (err) {
     next(err)
   }
@@ -25,12 +27,30 @@ exports.getCategories = async (req, res, next) => {
 exports.postCategory = async (req, res, next) => {
   try {
     const { name } = req.body
+    if (!name) throw new Error('請輸入名稱')
     const findCategories = await Category.findOne({where: { name }})
     if (findCategories) {
       throw new Error('類別已存在')
     }
     const newCategory = new Category({ name })
     await newCategory.save()
+    req.flash('success_messages','新增類別成功')
+    return res.redirect('/admin/categories')
+
+  } catch (err) {
+    next(err)
+  }
+}
+
+exports.putCategory = async (req, res, next) => {
+  try {
+    const { name } = req.body
+    const { id } = req.params
+    if (!name) throw new Error('請輸入名稱')
+
+    const category =  await Category.findByPk(id)
+    if (!category) throw new Error('找不到此類別')
+    await category.update({ name })
     req.flash('success_messages','新增類別成功')
     return res.redirect('/admin/categories')
 
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -13,8 +13,11 @@ router.get('/restaurants', adminController.getRestaurants)
 router.post('/restaurants', upload.single('image'), adminController.postRestaurant)
 router.get('/users', adminController.getUsers)
 router.patch('/users/:userId', adminController.patchUser)
+router.get('/categories/:id', categoryController.getCategories)
+router.put('/categories/:id', categoryController.putCategory)
 router.get('/categories', categoryController.getCategories)
 router.post('/categories', categoryController.postCategory)
+
 router.get('', (req, res) => res.redirect('/admin/restaurants'))
 
 module.exports = router
