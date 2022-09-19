6954572a6887b64c04c1e6b93de9020bec7d9b09
Nancy-Hsu <yutinghsu0411@gmail.com>
2022-09-19 17:07:25 +0800

    feat: add hbs helper idCond & update category selector

--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -1,4 +1,9 @@
 const dayjs = require('dayjs')
 module.exports = {
-  currentYear: () => dayjs().year()
+  currentYear: () => dayjs().year(),
+  ifCond: function (a, b, options) {
+    if (a === b) {
+      return options.fn(this)
+    }
+  }
 }
