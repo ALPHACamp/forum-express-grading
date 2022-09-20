61f215ff900706704dc645a314100aefc9e3a127
Chen Yi-Cheng <art787472@gmail.com>
2022-09-20 10:51:51 +0800

    feat: add comment model

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -4,19 +4,40 @@ exports.getRestaurants = async (req, res, next) => {
   try {
     const pageSize = 15
     const currPage = +req.query.page || 1
+    const categoryId = +req.query.categoryId || ''
     const { count, rows } = await Restaurant.findAndCountAll({
       offset: (currPage - 1) * pageSize,
-      limit: pageSize
+      limit: pageSize,
+      where: {
+        ...categoryId ? { categoryId } : {}
+      },
+      include: Category,
+      nest: true
+    })
+    const categories = await Category.findAll({
+      raw: true
     })
     if (currPage > Math.ceil(count / pageSize)) {
       req.flash('error_messages', '頁面不存在')
       return res.redirect('/restaurants')
     }
     const pages = Array.from({ length: Math.ceil(count / pageSize) }, (_, i) => Number(i + 1))
-    const restaurants = rows.map(({ dataValues }) => ({ ...dataValues, description: dataValues.description.substring(0, 50) }))
+    const restaurants = rows.map(({ dataValues }) => ({
+      ...dataValues,
+      description: dataValues.description.substring(0, 50),
+      Category: dataValues.Category.dataValues
+    }))
     const nextPage = currPage === pages.length ? 0 : currPage + 1
     const prevPage = currPage - 1
-    return res.render('restaurants', { restaurants, pages, nextPage, prevPage, currPage })
+    return res.render('restaurants', {
+      restaurants,
+      pages,
+      nextPage,
+      prevPage,
+      currPage,
+      categories,
+      categoryId
+    })
   } catch (err) {
     next(err)
   }
@@ -30,7 +51,6 @@ exports.getRestaurant = async (req, res, next) => {
     }
 
     return res.render('restaurant', { restaurant: restaurant.toJSON() })
-
   } catch (error) {
     next(error)
   }
@@ -42,7 +62,7 @@ exports.getDashboard = async (req, res, next) => {
     if (!restaurant) {
       throw new Error('Restaurant not found')
     }
-    const result = await restaurant.update({viewCount: restaurant.viewCount + 1})
+    const result = await restaurant.update({ viewCount: restaurant.viewCount + 1 })
     return res.render('dashboard', { restaurant: result.toJSON() })
   } catch (error) {
     next(error)
