50a45efce463ec55339153e7e3a29f55e8bda1d5
WIlson <eaz73727@gmail.com>
2022-09-21 00:01:15 +0800

    feat: setup dashboard and add view-count to restaurants model

--- migrations/20220919052530-add-category-id-to-restaurants.js
+++ migrations/20220919052530-add-category-id-to-restaurants.js
@@ -6,7 +6,7 @@ module.exports = {
       type: Sequelize.INTEGER,
       allowNull: false,
       references: {
-        model: 'categories',
+        model: 'Categories',
         key: 'id'
       }
     })
