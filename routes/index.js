1bd9bb1cbcd5fe1a18219725b55645706e2ace26
weizi0328 <ankewang26@gmail.com>
2022-09-20 14:41:42 +0800

    feat: add index page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,6 @@ const router = express.Router()
 
 const restController = require('../controllers/restaurant-controller')
 
-
 router.get('/restaurants', restController.getRestaurants)
 
 // fallback 路由。其他路由條件都不符合時，會通過這個路由。
