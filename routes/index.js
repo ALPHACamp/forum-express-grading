7610f484b87b8be544ff64ab995db9f010ffa871
Raven <reevan0613@gmail.com>
2022-09-21 14:52:00 +0800

    feat: add like restaurant

--- routes/index.js
+++ routes/index.js
@@ -33,6 +33,9 @@ router.get('/restaurants', authenticated, restController.getRestaurants)
 router.post('/comments', authenticated, commentController.postComment)
 router.delete('/comments/:id', authenticatedAdmin, commentController.deleteComment)
 
+router.post('/like/:restaurantId', authenticated, userController.addLike)
+router.delete('/like/:restaurantId', authenticated, userController.removeLike)
+
 router.post('/favorite/:restaurantId', authenticated, userController.addFavorite)
 router.delete('/favorite/:restaurantId', authenticated, userController.removeFavorite)
 
