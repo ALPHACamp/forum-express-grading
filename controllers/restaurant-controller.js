86615647191c0ff9e429a9ad6ecd96d24e9ec653
how200284 <how200284@gmail.com>
2022-09-21 11:29:11 +0800

    feat: add pagination on restaurants index page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,28 +1,40 @@
 const { Restaurant, Category } = require('../models')
+const { getOffset, getPagination } = require('../helpers/pagination-helper')
 
 const restController = {
   getRestaurants: (req, res, next) => {
+    const DEFAULT_LIMIT = 9
     const categoryId = Number(req.query.categoryId) || ''
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || DEFAULT_LIMIT
+    const offset = getOffset(limit, page)
 
-    Promise.all([
-      Restaurant.findAll({
+    return Promise.all([
+      Restaurant.findAndCountAll({
         include: Category,
         where: {
           ...categoryId ? { categoryId } : {}
           // The sever will process Ternary operator, and then spread operator.
           // The reason we have to put spread operator ahead is that categoryId and {} are OBJECT, but what we need to put inside "where: { }" is a STRING.
         },
+        limit,
+        offset,
         nest: true,
         raw: true
       }),
       Category.findAll({ raw: true })
     ])
       .then(([restaurant, categories]) => {
-        const data = restaurant.map(r => ({
+        const data = restaurant.rows.map(r => ({
           ...r,
           description: r.description.substring(0, 50)
         }))
-        return res.render('restaurants', { restaurants: data, categories, categoryId })
+        return res.render('restaurants', {
+          restaurants: data,
+          categories,
+          categoryId,
+          pagination: getPagination(limit, page, restaurant.count)
+        })
       })
       .catch(err => next(err))
   },
