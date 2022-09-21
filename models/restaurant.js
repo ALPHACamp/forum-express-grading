3e1729df0d25d6046c88ea2b0eede1c3e07f6d73
Eric LIAO <d1044171009@gmail.com>
2022-09-22 04:07:31 +0800

    feat: create favorite model

--- models/restaurant.js
+++ models/restaurant.js
@@ -12,6 +12,11 @@ module.exports = (sequelize, DataTypes) => {
     static associate (models) {
       Restaurant.belongsTo(models.Category, { foreignKey: 'categoryId' })
       Restaurant.hasMany(models.Comment, { foreignKey: 'restaurantId' })
+      Restaurant.belongsToMany(models.User, {
+        through: models.Favorite, // 透過Favorite 表來建立關聯
+        foreignKey: 'restaurantId', // 對 Favorite 表設定ＦＫ
+        as: 'FavoritedUsers' // 幫這個關聯取個名字
+      })
     }
   };
   Restaurant.init({
