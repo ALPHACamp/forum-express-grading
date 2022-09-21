3843000c96d2a828385ea72010566e0e6a45aaa6
robert1074004 <abc50113@yahoo.com.tw>
2022-09-21 22:36:45 +0800

    feat: add create category function

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -4,6 +4,16 @@ const categoryController = {
     return Category.findAll({ raw: true })
       .then(categories => res.render('admins/categories', { categories }))
       .catch(err => next(err))
+  },
+  postCategory: (req, res, next) => {
+    const name = req.body.name.trim()
+    if (!name) throw new Error('CategoryName must be required')
+    return Category.create({ name })
+      .then(() => {
+        req.flash('success_messages', 'Category is created')
+        res.redirect('/admin/categories')
+      })
+      .catch(err => next(err))
   }
 }
 
