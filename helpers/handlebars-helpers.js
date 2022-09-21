cf7eb46e4672617e48dcaf9862f406775d12dbfd
LiyLi1122 <teppei.252033@gmail.com>
2022-09-21 15:07:33 +0800

    feat: add ifCond hbs helper & update category selector

--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -1,4 +1,7 @@
 const dayjs = require('dayjs')
 module.exports = {
-  currentYear: dayjs().year() // 目前日期的總數字.只要年並轉成易讀型 dayjs() === new date()
+  currentYear: dayjs().year(), // 目前日期的總數字.只要年並轉成易讀型 dayjs() === new date()
+  ifCond: function (a, b, options) { // 這邊是故意不使用箭頭函式
+    return a === b ? options.fn(this) : options.inverse(this) // true or false ? 執行 if : 執行 else
+  }
 }
