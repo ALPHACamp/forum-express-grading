bda7a0eda836a0fa70ab5c3a1c7f3a7656be928e
robert1074004 <abc50113@yahoo.com.tw>
2022-09-21 22:19:55 +0800

    feat: create add category page

---
+++ controllers/category-controller.js
@@ -0,0 +1,10 @@
+const { Category } = require('../models')
+const categoryController = {
+  getCategories: (req, res, next) => {
+    return Category.findAll({ raw: true })
+      .then(categories => res.render('admins/categories', { categories }))
+      .catch(err => next(err))
+  }
+}
+
+module.exports = categoryController
