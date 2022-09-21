1bf88442ceef6995109a04566badb2223e394fb5
AlleyChang <yifanchang0602@gmail.com>
2022-09-21 09:22:49 +0800

    feat: add comment on restaurant page

--- routes/index.js
+++ routes/index.js
@@ -4,27 +4,29 @@ const passport = require('../config/passport')
 const admin = require('./modules/admin')
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
+const commentController = require('../controllers/comment-controller')
 
 const { generalErrorHandler } = require('../middleware/error-handler')
 const { authenticated, authenticatedAdmin } = require('../middleware/auth')
 
+// 後台
 router.use('/admin', authenticatedAdmin, admin)
 
+// 登入畫面
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
-
 router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', {
   failureRedirect: '/signin',
   failureFlash: true
 }), userController.signIn)
-
 router.get('/logout', userController.logout)
-// 若接收到的請求路徑'/restaurants'則交由controller處理
 
-router.get('/restaurants/:id', authenticated, restController.getRestaurant)
-router.get('/restaurants', authenticated, restController.getRestaurants)
-router.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard)
+// 前台
+router.get('/restaurants/:id', authenticated, restController.getRestaurant) // detail
+router.get('/restaurants', authenticated, restController.getRestaurants) // browse all
+router.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard) // dashboard
+router.post('/comments', authenticated, commentController.postComment) // comment
 router.use('/', (req, res) => res.redirect('/restaurants'))
 router.use('/', generalErrorHandler)
 
