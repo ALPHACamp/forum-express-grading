9dc930483aac3d871a313211da3bae6ecbe48c10
ccj777 <002ntou@gmail.com>
2022-09-21 11:28:18 +0800

    refactor: refactor handlebars-helpers.isSuperuser

--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -6,11 +6,7 @@ if (process.env.NODE_ENV !== 'production') {
 module.exports = {
   currentYear: () => dayjs().year(),
   isSuperuser: (email, options) => {
-    if (email === process.env.SUPERUSER_EMAIL) {
-      return options.fn(this)
-    } else {
-      return options.inverse(this)
-    }
+    return email === process.env.SUPERUSER_EMAIL ? options.fn(this) : options.inverse(this)
   },
   ifCond: function (a, b, options) {
     return a === b ? options.fn(this) : options.inverse(this)
