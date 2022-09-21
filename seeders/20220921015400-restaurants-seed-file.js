41b8f326a6acec492369954f6eb4c63f25b58452
LiyLi1122 <teppei.252033@gmail.com>
2022-09-21 11:12:26 +0800

    feat: updaye seed files

--- seeders/20220921015400-restaurants-seed-file.js
+++ seeders/20220921015400-restaurants-seed-file.js
@@ -2,6 +2,12 @@
 const faker = require('faker')
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    // 先取得目前 cate 表單內現有的資料(此先決條件是 cate 種子資料要先建立)
+    const categories = await queryInterface.sequelize.query(
+      'SELECT id FROM Categories',
+      { type: queryInterface.sequelize.QueryTypes.SELECT }
+    ) // [[object Object], ....]
+
     await queryInterface.bulkInsert('Restaurants', Array.from({ length: 50 }, () => ({
       name: faker.name.findName(),
       tel: faker.phone.phoneNumber(),
@@ -10,7 +16,8 @@ module.exports = {
       image: `https://loremflickr.com/320/240/restaurant,food/?random=${Math.random() * 100}`,
       description: faker.lorem.text(),
       created_at: new Date(),
-      updated_at: new Date()
+      updated_at: new Date(),
+      category_id: categories[Math.floor((Math.random() * categories.length))].id // 製作隨機下標(0 ~ categories.length-1)，到 categories 用下標後再 key 取值
     })))
   },
 
