951a88f1133dd3b6284cea975ca5f14a81ffbe7a
Eagle1630 <eagle163013@gmail.com>
2022-09-22 10:19:10 +0800

    feat: add pagination on restaurants index page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,22 +1,40 @@
 
 const { Restaurant, Category } = require('../models')
-
+const { getOffSet, getOpagination } = require('../helpers/pagination-helper')
 const restaurantController = {
 
   getRestaurants: async (req, res, next) => {
+    const DEFAULT_LIMIT = 10
     const categoryId = Number(req.query.categoryId) || ''
-    console.log('categoryId', categoryId)
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || DEFAULT_LIMIT
+    const offset = getOffSet(page, limit)
     try {
       const [restaurants, categories] = await Promise.all([
-        Restaurant.findAll({ raw: true, nest: true, include: [Category], where: { ...categoryId ? { categoryId } : {} } }),
+        Restaurant.findAndCountAll({
+          raw: true,
+          nest: true,
+          limit: limit,
+          offset: offset,
+          include: [Category],
+          where: { ...categoryId ? { categoryId } : {} }
+        }),
         Category.findAll({ raw: true })
       ])
-      const data = restaurants.map(r => {
+      const data = restaurants.rows.map(r => {
         return {
           ...r, description: r.description.substring(0, 50)
         }
       })
-      return res.render('restaurants', { restaurants: data, categories, categoryId })
+
+      return res.render('restaurants',
+        {
+          restaurants: data,
+          categories,
+          categoryId,
+          pagination: getOpagination(page, limit, restaurants.count)
+
+        })
     } catch (error) {
       next(error)
     }
