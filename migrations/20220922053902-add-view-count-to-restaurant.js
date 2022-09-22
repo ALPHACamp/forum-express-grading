29de527a66471ff8da1fe8f574cad93d06f0e390
LiyLi1122 <teppei.252033@gmail.com>
2022-09-22 17:46:09 +0800

    feat: modify dashbord view

--- migrations/20220922053902-add-view-count-to-restaurant.js
+++ migrations/20220922053902-add-view-count-to-restaurant.js
@@ -1,9 +1,8 @@
 'use strict'
-const { Restaurant } = require('../models')
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    Restaurant.queryInterface.addColumn('Restaurants', 'view_count', {
+    queryInterface.addColumn('Restaurants', 'view_count', {
       type: Sequelize.INTEGER,
       allowNull: false,
       defaultValue: 0 // 因為不允許空值，所以這邊就要先設定預設值，或最後再執行此 migration 就不用設定 0
@@ -11,6 +10,6 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    Restaurant.queryInterface.removeColumn('Restaurant', 'view_count')
+    queryInterface.removeColumn('Restaurant', 'view_count')
   }
 }
