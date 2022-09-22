f46819ee6eeaa50c773b77666285d8f3c73f82de
Eagle1630 <eagle163013@gmail.com>
2022-09-22 11:36:56 +0800

    feat: add comment model

--- models/restaurant.js
+++ models/restaurant.js
@@ -12,6 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate (models) {
       // 建立與Category的關聯
       Restaurant.belongsTo(models.Category, { foreignKey: 'categoryId' })
+      Restaurant.hasMany(models.Comment, { foreignKey: 'restaurantId' })
     }
   };
   Restaurant.init({
