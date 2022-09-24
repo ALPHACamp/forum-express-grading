eb3839b7d1526a838770feea1b3fb8a87dde04f8
ccj777 <002ntou@gmail.com>
2022-09-25 04:46:14 +0800

    feat: add post comment on restaurant page

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
+const commentController = require('../controllers/comment-controller')
 const admin = require('./modules/admin')
 const { generalErrorHandler } = require('../middleware/error-handeler')
 const { authenticated, authenticatedAdmin } = require('../middleware/auth')
@@ -12,10 +13,14 @@ router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
 router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
+
 router.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard)
 router.get('/restaurants/:id', authenticated, restController.getRestaurant)
 router.get('/restaurants', authenticated, restController.getRestaurants)
 router.get('/logout', userController.logout)
+
+router.post('/comments', authenticated, commentController.postComment)
+
 router.use('/', (req, res) => res.redirect('/restaurants')) // fallback route
 router.use('/', generalErrorHandler)
 
