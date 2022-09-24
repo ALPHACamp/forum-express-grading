c56125ac071635e37623ef2719d4d9f4d189e1e5
m790101 <m790101@hotmail.com>
2022-09-24 12:15:21 +0800

    add dashboard

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,8 @@ const { generalErrorHandler } = require('../middleware/error-handler')
 const passport = require('../config/passport')
 const { authenticated, authenticatedAdmin } = require('../middleware/auth')
 
+router.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard)
+router.get('/restaurants/:id', authenticated, restController.getRestaurant)
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
 router.use('/admin', authenticatedAdmin, admin)
