58c0e263dc797a26b5cfb7e49116ec0e81629aac
m790101 <m790101@hotmail.com>
2022-09-24 12:06:14 +0800

    add dashboard

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,6 +1,49 @@
+const { Restaurant, Category } = require('../models')
 const restaurantController = {
   getRestaurants: (req, res) => {
-    return res.render('restaurants')
+    return Restaurant.findAll({
+      include: Category,
+      nest: true,
+      raw: true
+    }).then(restaurants => {
+      const data = restaurants.map(r => ({
+        ...r,
+        description: r.description.substring(0, 50)
+      }))
+      return res.render('restaurants', {
+        restaurants: data
+      })
+    })
+  },
+  getRestaurant: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, {
+      include: Category, // 拿出關聯的 Category model
+      nest: true
+    })
+      .then(restaurant => restaurant.increment('viewCounts'))
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        restaurant = restaurant.toJSON()
+        res.render('restaurant', {
+          restaurant
+        })
+        // return restaurant.increment('viewCounts', { by: 1 })
+      })
+      .catch(err => next(err))
+  },
+  getDashboard: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, {
+      include: Category, // 拿出關聯的 Category model
+      nest: true,
+      raw: true
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        res.render('dashboard', {
+          restaurant
+        })
+      })
+      .catch(err => next(err))
   }
 }
 module.exports = restaurantController
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
+const categoryController = require('../../controllers/category-controller')
 // const upload = require('../../middleware/multer')
 const upload = require('../../middleware/multer')
 
@@ -13,6 +14,11 @@ router.put('/restaurants/:id', upload.single('image'), adminController.putRestau
 router.get('/restaurants/:id', adminController.getRestaurant)
 router.patch('/users/:id', adminController.patchUser)
 router.get('/users', adminController.getUsers)
+router.get('/categories/:id', categoryController.getCategories) // 新增這行
+router.put('/categories/:id', categoryController.putCategory)
+router.delete('/categories/:id', categoryController.deleteCategory)
+router.post('/categories', categoryController.postCategory)
+router.get('/categories', categoryController.getCategories)
 router.get('/', (req, res) => res.redirect('/admin/restaurants'))
 
 module.exports = router
