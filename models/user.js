8f3e7849c53ab02403a2b15b8a67bc8734c37bac
Elliot <elliotcs30@gmail.com>
2022-09-24 17:41:54 +0800

    feat: create favorite model

--- models/user.js
+++ models/user.js
@@ -6,6 +6,12 @@ module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     static associate (models) {
       User.hasMany(models.Comment, { foreignKey: 'userId' })
+
+      User.belongsToMany(models.Restaurant, {
+        through: models.Favorite,
+        foreignKey: 'userId',
+        as: 'FavoritedRestaurants'
+      })
     }
   }
   User.init({
