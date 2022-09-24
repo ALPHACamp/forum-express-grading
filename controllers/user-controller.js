0e6622c0fe310a365fd093405a02bffec40a0241
Ckai <ckai.trans@gmail.com>
2022-09-24 23:12:25 +0800

    feat: addFollowing & removeFollowing function

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -151,6 +151,22 @@ const userController = {
       })
       .then(() => res.redirect('back'))
       .catch(err => next(err))
+  },
+  getTopUsers: (req, res, next) => {
+    return User.findAll({
+      include: [{ model: User, as: 'Followers' }]
+    })
+      .then(users => {
+        users = users.map(user => ({
+          ...user.toJSON(),
+          // 計算追蹤者人數
+          followerCount: user.Followers.length,
+          // 判斷目前登入使用者是否已追蹤該 user 物件
+          isFollowed: req.user.Followings.some(f => f.id === user.id)
+        }))
+        res.render('top-users', { users: users })
+      })
+      .catch(err => next(err))
   }
 }
 
--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
+const { authenticated } = require('../../middleware/auth')
 const upload = require('../../middleware/multer')
 const userController = require('../../controllers/user-controller')
 
+router.get('/top', authenticated, userController.getTopUsers)
 router.get('/:id/edit', userController.editUser)
 router.put('/:id', upload.single('image'), userController.putUser)
 router.get('/:id', userController.getUser)
