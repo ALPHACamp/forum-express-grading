4e53808f6ac220eefc3c7b30fadcedf3c6e33e48
LiyLi1122 <teppei.252033@gmail.com>
2022-09-26 00:27:00 +0800

    test

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -52,7 +52,7 @@ const userController = {
             { model: Restaurant, attributes: ['name', 'image'] }
           ],
           where: { userId: user.id, text: { [Op.not]: null } },
-          order: [['created_at', 'DESC']],
+          order: [['createdAt', 'DESC']],
           raw: true,
           nest: true,
           attributes: [[Sequelize.fn('DISTINCT', Sequelize.col('restaurant_id')), 'restaurantId']
