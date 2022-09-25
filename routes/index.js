b0d9d9412ad13c0b020c68c232ca5df54d3766ec
Ckai <ckai.trans@gmail.com>
2022-09-25 10:29:16 +0800

    feat: addFollowing & removeFollowing function

--- routes/index.js
+++ routes/index.js
@@ -30,6 +30,9 @@ router.delete('/favorite/:restaurantId', authenticated, userController.removeFav
 // like / unlike
 router.post('/like/:restaurantId', authenticated, userController.addLike)
 router.delete('/like/:restaurantId', authenticated, userController.removeLike)
+// follow / unfollow
+router.post('/following/:userId', authenticated, userController.addFollowing)
+router.delete('/following/:userId', authenticated, userController.removeFollowing)
 
 router.use('/', (req, res) => res.redirect('/restaurants'))
 router.use('/', generalErrorHandler)
