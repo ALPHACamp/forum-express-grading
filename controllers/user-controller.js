709817e02af7fb79db16956b669bdf21f77eff52
LiyLi1122 <teppei.252033@gmail.com>
2022-09-25 23:43:02 +0800

    feat: modify getUser controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -52,7 +52,7 @@ const userController = {
             { model: Restaurant, attributes: ['name', 'image'] }
           ],
           where: { userId: user.id, text: { [Op.not]: null } },
-          group: [['restaurantId']],
+          group: [['restaurant_id']],
           order: [['createdAt', 'DESC']],
           raw: true,
           nest: true,
