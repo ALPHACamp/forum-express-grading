d9b3ec858f65f3a3046621a26c0fcd43e096971e
parker3216 <parker3216@hotmail.com>
2022-09-25 16:06:54 +0800

    feat: add post comment on restaurant page

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const router = express.Router()
+
 const passport = require('../config/passport') // 引入Passport,透過他幫忙做驗證
 const admin = require('./modules/admin')// 載入 admin.js
+
 const restController = require('../controllers/restaurant-controller')// 載入 controller
 const userController = require('../controllers/user-controller')
+const commentController = require('../controllers/​​comment-controller')
+
 const { authenticated, authenticatedAdmin } = require('../middleware/auth') // 引入 auth.js
 const { generalErrorHandler } = require('../middleware/error-handler')
 
@@ -15,11 +19,13 @@ router.post('/signup', userController.signUp)
 router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
 router.get('/logout', userController.logout)
+
 router.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard)
 router.get('/restaurants/:id', authenticated, restController.getRestaurant)
-
 // 新增前台網址路由//匹配條件多的路由要寫在前面
 router.get('/restaurants', authenticated, restController.getRestaurants)
+router.post('/comments', authenticated, commentController.postComment)
+
 router.use('/', (req, res) => res.redirect('/restaurants'))
 router.use('/', generalErrorHandler)
 
