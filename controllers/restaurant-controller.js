27e84715670a54239397dfa37bb38ea6786cde11
kany <kany102030@gmail.com>
2022-09-26 20:04:44 +0800

    feat: add favorite restaurant function

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -20,9 +20,11 @@ const restaurantController = {
       }),
       Category.findAll({ raw: true })
     ]).then(([restaurants, categories]) => {
+      const favoritedRestaurantsId = req.user && req.user.FavoritedRestaurants.map(fr => fr.id)
       const data = restaurants.rows.map(r => ({
         ...r,
-        description: r.description.substring(0, 50)
+        description: r.description.substring(0, 50),
+        isFavorited: favoritedRestaurantsId.includes(r.id)
       }))
       return res.render('restaurants', {
         restaurants: data,
@@ -35,15 +37,17 @@ const restaurantController = {
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
-      include: [Category, { model: Comment, include: User }],
+      include: [Category, { model: Comment, include: User }, { model: User, as: 'FavoritedUsers' }],
       nest: true
     })
       .then(restaurant => {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
+        const isFavorited = restaurant.FavoritedUsers.some(f => f.id === req.user.id)
         return restaurant.increment('viewCounts')
           .then(() => {
             return res.render('restaurant', {
-              restaurant: restaurant.toJSON()
+              restaurant: restaurant.toJSON(),
+              isFavorited
             })
           })
       })
