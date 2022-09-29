6294c8eba027fe9feb531b01ea87bd02a4012286
stu96534 <stu96534@gmail.com>
2022-09-29 16:13:39 +0800

    feat: add admin-services

--- controllers/pages/admin-controller.js
+++ controllers/pages/admin-controller.js
@@ -1,15 +1,10 @@
 const { Restaurant, User, Category } = require('../../models')
 const { imgurFileHandler } = require('../../helpers/file-helpers')
+const adminServices = require('../../services/admin-services')
 
 const adminController = {
   getRestaurants: (req, res, next) => {
-    Restaurant.findAll({
-      raw: true,
-      nest: true,
-      include: [Category]
-    })
-      .then(restaurants => res.render('admin/restaurants', { restaurants }))
-      .catch(err => next(err))
+    adminServices.getRestaurants(req, (err, data) => err ? next(err) : res.render('admin/restaurants', data))
   },
   createRestaurants: (req, res, next) => {
     return Category.findAll({
---
+++ services/admin-services.js
@@ -0,0 +1,15 @@
+const { Restaurant, Category } = require('../models')
+
+const adminServices = {
+  getRestaurants: (req, cb) => {
+    Restaurant.findAll({
+      raw: true,
+      nest: true,
+      include: [Category]
+    })
+      .then(restaurants => cb(null, { restaurants }))
+      .catch(err => cb(err))
+  }
+}
+
+module.exports = adminServices
