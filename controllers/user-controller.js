a680451be2500ef04dd0d1dfa440f63aff5dc099
robert1074004 <abc50113@yahoo.com.tw>
2022-10-02 10:39:43 +0800

    feat: add following function

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,5 +1,5 @@
 const bcrypt = require('bcryptjs')
-const { User, Comment, Restaurant, Favorite, Like } = require('../models')
+const { User, Comment, Restaurant, Favorite, Like, Followship } = require('../models')
 const { imgurFileHandler } = require('../helpers/file-helpers')
 const userController = {
   signUpPage: (req, res) => {
@@ -149,12 +149,40 @@ const userController = {
       include: [{ model: User, as: 'Followers' }]
     })
       .then(users => {
-        users = users.map(user => ({
+        const result = users.map(user => ({
           ...user.toJSON(),
           followerCount: user.Followers.length,
           isFollowed: req.user.Followings.some(f => f.id === user.id)
-        }))
-        res.render('top-users', { users })
+        })).sort((a, b) => b.followerCount - a.followerCount)
+        res.render('top-users', { users: result })
+      })
+      .catch(err => next(err))
+  },
+  addFollowing: (req, res, next) => {
+    const { userId } = req.params
+    return User.findByPk(userId, { include: [{ model: User, through: Followship, as: 'Followers' }] })
+      .then(user => {
+        const isFollowed = user.Followers.some(u => u.id === req.user.id)
+        if (!user) throw new Error("User didn't exist !")
+        if (isFollowed) throw new Error('You have followed this user')
+        return Followship.create({ followingId: userId, followerId: req.user.id })
+      })
+      .then(() => {
+        req.flash('success_messages', 'User is successfully followed')
+        res.redirect('back')
+      })
+      .catch(err => next(err))
+  },
+  removeFollowing: (req, res, next) => {
+    const { userId } = req.params
+    return Followship.findOne({ where: { followerId: req.user.id, followingId: userId } })
+      .then(followship => {
+        if (!followship) throw new Error('You have not followed this user')
+        return followship.destroy()
+      })
+      .then(() => {
+        req.flash('success_messages', 'User is successfully remove from followed')
+        res.redirect('back')
       })
       .catch(err => next(err))
   }
