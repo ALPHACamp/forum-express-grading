233114900012367c8da1f6fc98ea84b3ccc5afbb
weizi0328 <ankewang26@gmail.com>
2022-10-05 17:51:46 +0800

    feat: user signup

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const router = express.Router()
-
-const restController = require('../controllers/restaurant-controller')
 const admin = require('./modules/admin')
 
-router.use('/admin', admin)
-router.get('/restaurants', restController.getRestaurants)
+const restController = require('../controllers/restaurant-controller')
+const userController = require('../controllers/user-controller')
 
+router.use('/admin', admin)
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
+router.get('/restaurants', restController.getRestaurants)
 // fallback 路由。其他路由條件都不符合時，會通過這個路由。
 router.use('/', (req, res) => res.redirect('/restaurants'))
 
