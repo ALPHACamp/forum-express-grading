69187607175762e322d6a6aa16e1a615302f3167
naluwan <street.baller06@gmail.com>
2022-10-09 15:06:41 +0800

    feat: add Like/unLike button & switch button at index page

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcryptjs')
 const db = require('../models')
-const { User, Comment, Restaurant, Favorite } = db
+const { User, Comment, Restaurant, Favorite, Like } = db
 const { imgurFileHandler } = require('../helpers/file-helpers')
 
 const userController = {
@@ -145,6 +145,45 @@ const userController = {
         if (!favorite) throw new Error('還未將此餐廳加入最愛清單')
 
         return favorite.destroy()
+      })
+      .then(() => res.redirect('back'))
+      .catch(err => next(err))
+  },
+  addLike: (req, res, next) => {
+    const { restaurantId } = req.params
+    return Promise.all([
+      Restaurant.findByPk(restaurantId),
+      Like.findOne({
+        where: {
+          userId: req.user.id,
+          restaurantId
+        }
+      })
+    ])
+      .then(([restaurant, like]) => {
+        if (!restaurant) throw new Error('查無此餐廳')
+        if (like) throw new Error('此餐廳已在喜愛清單')
+
+        return Like.create({
+          userId: req.user.id,
+          restaurantId
+        })
+      })
+      .then(() => res.redirect('back'))
+      .catch(err => next(err))
+  },
+  removeLike: (req, res, next) => {
+    const { restaurantId } = req.params
+    return Like.findOne({
+      where: {
+        userId: req.user.id,
+        restaurantId
+      }
+    })
+      .then(like => {
+        if (!like) throw new Error('還未將此餐廳加入喜愛清單')
+
+        return like.destroy()
       })
       .then(() => res.redirect('back'))
       .catch(err => next(err))
