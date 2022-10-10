a24e04d98e40b36aeac46d244a6d16da0b9d5d18
Howhowchen <debby8698@gmail.com>
2022-10-10 22:47:32 +0800

    feat: flash msg & signup verification

--- app.js
+++ app.js
@@ -1,20 +1,38 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
+const flash = require('connect-flash')
+const session = require('express-session')
+
 const routes = require('./routes')
 
 const app = express()
 const port = process.env.PORT || 3000
+const SESSION_SECRET = 'secret'
 
 //  確認與資料庫連線
 require('./models')
 
-// 註冊 Handlebars 樣板引擎，並指定副檔名為 .hbs
+//  handlebars
 app.engine('hbs', handlebars({ extname: '.hbs' }))
-// 設定使用 Handlebars 做為樣板引擎
 app.set('view engine', 'hbs')
 
+//  middleware: body-parser
 app.use(express.urlencoded({ extended: true }))
 
+//  middleware: session
+app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
+
+//  middleware: flash message
+app.use(flash())
+
+//  middleware: locals argument
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
+
+//  middleware: routes
 app.use(routes)
 
 app.listen(port, () => {
