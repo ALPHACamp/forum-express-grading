30ff8fe28c32e3d41c83fcc27d29ba7830127825
Howhowchen <debby8698@gmail.com>
2022-10-10 16:52:50 +0800

    feat: add user model

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const routes = require('./routes')
 const app = express()
 const port = process.env.PORT || 3000
 
+//  確認與資料庫連線
+require('./models')
+
 // 註冊 Handlebars 樣板引擎，並指定副檔名為 .hbs
 app.engine('hbs', handlebars({ extname: '.hbs' }))
 // 設定使用 Handlebars 做為樣板引擎
--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const restController = require('../controllers/restaurant-controller')
 const admin = require('./modules/admin')
 
 router.use('/admin', admin)
-router.get('/restaurants', restController.getRestaurants) 
+router.get('/restaurants', restController.getRestaurants)
 router.use('/', (req, res) => res.redirect('/restaurants'))
 
 module.exports = router
