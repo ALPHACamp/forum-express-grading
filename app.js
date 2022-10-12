da6f3b341cbe32350f2c3b2a3d99740c95f41065
HowardWu5566 <ggg1237890@gmail.com>
2022-10-12 14:26:47 +0800

    feat: passport init & signin

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const handlebars = require('express-handlebars')
 const session = require('express-session')
 const flash = require('connect-flash')
+const passport = require('./config/passport')
 const routes = require('./routes')
 // const db = require('./models')
 const app = express()
@@ -13,10 +14,12 @@ app.set('view engine', 'hbs')
 
 app.use(express.urlencoded({ extended: true }))
 app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
-app.use(flash()) // 掛載套件
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
 app.use((req, res, next) => {
-  res.locals.success_messages = req.flash('success_messages') // 設定 success_msg 訊息
-  res.locals.error_messages = req.flash('error_messages') // 設定 warning_msg 訊息
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
   next()
 })
 
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -22,6 +22,18 @@ const userController = {
         res.redirect('/signin')
       })
       .catch(err => next(err))
+  },
+  signInPage: (req, res) => {
+    res.render('signin')
+  },
+  signIn: (req, res) => {
+    req.flash('success_messages', '成功登入！')
+    res.redirect('/restaurants')
+  },
+  logout: (req, res) => {
+    req.flash('success_messages', '登出成功！')
+    req.logout()
+    res.redirect('/signin')
   }
 }
 
