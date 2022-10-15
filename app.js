963e17d31343fba1f5a9c3c0d8b7ac8b3ce68fc1
HowardWu5566 <ggg1237890@gmail.com>
2022-10-15 14:30:36 +0800

    feat: add imgur api

--- app.js
+++ app.js
@@ -8,10 +8,13 @@ const passport = require('./config/passport')
 const { getUser } = require('./helpers/auth-helpers')
 const handlebarHelpers = require('./helpers/handlebars-helpers')
 const routes = require('./routes')
-// const db = require('./models')
 const app = express()
 const port = process.env.PORT || 3000
 const SESSION_SECRET = 'secret'
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
 app.engine('hbs', handlebars({ extname: '.hbs', helpers: handlebarHelpers }))
 app.set('view engine', 'hbs')
