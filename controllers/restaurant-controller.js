15a5915a2f20e510ae0354e93d2805f620253b58
Howhowchen <debby8698@gmail.com>
2022-10-19 02:59:44 +0800

    feat: add user profile & edit page to edit & show comments on profile page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -42,7 +42,8 @@ const restaurantController = {
       include: [
         Category,
         { model: Comment, include: User }
-      ]
+      ],
+      order: [[Comment, 'createdAt', 'DESC']]
     })
       .then(restaurant => {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
