9bbc12a55a54e3721b6252f627698877b49a6dcb
HowardWu5566 <ggg1237890@gmail.com>
2022-10-19 00:47:46 +0800

    feat: category create

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -11,9 +11,10 @@ router.put('/restaurants/:id', upload.single('image'), adminController.putRestau
 router.delete('/restaurants/:id', adminController.deleteRestaurant)
 router.get('/restaurants', adminController.getRestaurants)
 router.post('/restaurants', upload.single('image'), adminController.postRestaurant)
-router.get('/categories', categoryController.getCategories)
 router.get('/users', adminController.getUsers)
 router.patch('/users/:id', adminController.patchUser)
+router.get('/categories', categoryController.getCategories)
+router.post('/categories', categoryController.postCategory)
 router.get('/', (req, res) =>
   res.redirect('/admin/restaurants'))
 
