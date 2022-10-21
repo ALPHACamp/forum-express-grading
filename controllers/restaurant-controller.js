2a0bd4a93d61655d120a40d71207880ec2908c39
HowardWu5566 <ggg1237890@gmail.com>
2022-10-21 21:18:12 +0800

    feat: add pagination on restaurants index page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,31 +1,40 @@
 const { Restaurant, Category } = require('../models')
+const { getOffset, getPagination } = require('../helpers/pagination-helper')
 const restaurantController = {
   getRestaurants: (req, res, next) => {
-    const categoryId = Number(req.query.categoryId || '')
+    const DEFAULT_LIMIT = 9
+    const categoryId = Number(req.query.categoryId) || ''
+
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || DEFAULT_LIMIT
+    const offset = getOffset(limit, page)
     return Promise.all([
-      Restaurant.findAll({
+      Restaurant.findAndCountAll({
         raw: true,
         nest: true,
         include: Category,
-        where: { ...categoryId ? { categoryId } : {} }
+        where: { ...categoryId ? { categoryId } : {} },
+        limit,
+        offset
       }),
       Category.findAll({ raw: true })
     ])
       .then(([restaurants, categories]) => {
-        const data = restaurants.map(r => ({
+        const data = restaurants.rows.map(r => ({
           ...r,
           description: r.description.substring(0, 50)
         }))
         return res.render('restaurants', {
           restaurants: data,
-          categories
+          categories,
+          categoryId,
+          pagination: getPagination(limit, page, restaurants.count)
         })
       })
       .catch(err => next(err))
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
-      // raw: true,
       nest: true,
       include: Category
     })
