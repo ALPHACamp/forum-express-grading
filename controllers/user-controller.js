bcd58f69e3c8d8c63ea2a198e416bbaac1a03717
HowardWu5566 <ggg1237890@gmail.com>
2022-10-23 16:14:37 +0800

    feat: add user profile page

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,5 +1,6 @@
 const bcrypt = require('bcryptjs')
 const db = require('../models')
+const { imgurFileHandler } = require('../helpers/file-helpers')
 const { User } = db
 const userController = {
   signUpPage: (req, res) => {
@@ -34,6 +35,39 @@ const userController = {
     req.flash('success_messages', '登出成功！')
     req.logout()
     res.redirect('/signin')
+  },
+  getUser: (req, res) => {
+    return User.findByPk(req.params.id, { raw: true })
+      .then(user => {
+        if (!user) throw new Error("User didn't exist!")
+        res.render('users/profile', { user })
+      })
+  },
+  editUser: (req, res) => {
+    const { id } = req.params
+    return User.findByPk(id, { raw: true })
+      .then(user => {
+        if (!user) throw new Error("User didn't exist!")
+        res.render('users/edit', { user })
+      })
+  },
+  putUser: (req, res, next) => {
+    const { id } = req.params
+    const { name } = req.body
+    const { file } = req
+    return Promise.all([User.findByPk(id), imgurFileHandler(file)])
+      .then(([user, filePath]) => {
+        if (!user) throw new Error("User didn't exist!")
+        return user.update({
+          name,
+          image: filePath || user.image
+        })
+      })
+      .then(() => {
+        req.flash('success_messages', '使用者資料編輯成功')
+        res.redirect(`/users/${id}`)
+      })
+      .catch(err => next(err))
   }
 }
 
