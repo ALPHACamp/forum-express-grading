a001cae3080a434c79d07713c05ff890e85a5f81
weizi0328 <ankewang26@gmail.com>
2022-10-25 16:55:48 +0800

    feat: lower passport version to 0.4.1 to prevent error message

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -4,6 +4,8 @@ const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
 const upload = require('../../middleware/multer')
 
+router.patch('/users/:id', adminController.patchUser)
+router.get('/users', adminController.getUsers)
 router.get('/restaurants/create', adminController.createRestaurant)
 router.get('/restaurants/:id/edit', adminController.editRestaurant)
 router.get('/restaurants/:id', adminController.getRestaurant)
@@ -11,7 +13,6 @@ router.put('/restaurants/:id', upload.single('image'), adminController.putRestau
 router.delete('/restaurants/:id', adminController.deleteRestaurant)
 router.get('/restaurants', adminController.getRestaurants)
 router.post('/restaurants', upload.single('image'), adminController.postRestaurant)
-router.get('/users', adminController.getUsers)
 
 router.get('/', (req, res) => res.redirect('/admin/restaurants'))
 
