10b11a75e0ce01c570fe14b94974a3f02bfc7017
zephyr Lin <zephyrlin33@gmail.com>
2022-11-19 13:36:38 +0800

    add user table

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express')
+const handlebars = require('express-handlebars')
 const routes = require('./routes')
-const handlebars = require('express-handlebars') 
-
+const db = require('./models') // 暫時新增這行，引入資料庫，檢查完可刪
 const app = express()
 const port = process.env.PORT || 3000
 
 app.engine('hbs', handlebars({ extname: '.hbs' }))
-app.set('view engine', 'hbs')
+app.set('view engine', 'hb。s')
 
 app.use(routes)
 
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express')
-const restController = require('../controllers/restaurant-controller') 
+const restController = require('../controllers/restaurant-controller')
 const router = express.Router()
+const admin = require('./modules/admin')
 
-router.get('/restaurants', restController.getRestaurants) 
+router.use('/admin', admin)
+router.get('/restaurants', restController.getRestaurants)
 
 router.use('/', (req, res) => res.redirect('/restaurants'))
 
