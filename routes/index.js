8ffd05c8ed03105621f8cda80ce17ecf18f6ef90
miaout11 <miaout.11@gmail.com>
2022-11-19 14:27:34 +0800

    feat: add index page

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const router = express.Router()
+// 載入 controller
+const restController = require('../controllers/restaurant-controller')
 
-router.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+router.get('/restaurants', restController.getRestaurants)
+
+// 設定 fallback 路由(其他路由條件都不符合時，最終會通過的路由)
+router.use('/', (req, res) => res.redirect('/restaurants'))
 
 module.exports = router
