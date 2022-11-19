a4be99882f1d2231ce4ff04af1174a216fda7f3e
zephyr Lin <zephyrlin33@gmail.com>
2022-11-19 15:54:05 +0800

    flash msg & singup verification

--- app.js
+++ app.js
@@ -1,12 +1,25 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
 const routes = require('./routes')
-const db = require('./models') // 暫時新增這行，引入資料庫，檢查完可刪
+require('./models') // 暫時新增這行，引入資料庫，檢查完可刪
+const flash = require('connect-flash')
+const session = require('express-session')
+
 const app = express()
 const port = process.env.PORT || 3000
+const SESSION_SECRET = 'secret' // 新增這行
 
 app.engine('hbs', handlebars({ extname: '.hbs' }))
-app.set('view engine', 'hb。s')
+app.set('view engine', 'hbs')
+
+app.use(express.urlencoded({ extended: true })) // 使用POST方法，配合body-parson
+app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
+app.use(flash()) // 掛載套件
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages') // 設定 success_msg 訊息
+  res.locals.error_messages = req.flash('error_messages') // 設定 warning_msg 訊息
+  next()
+})
 
 app.use(routes)
 
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,16 @@
 const express = require('express')
 const restController = require('../controllers/restaurant-controller')
+const userController = require('../controllers/user-controller') // 新增這行
+const { generalErrorHandler } = require('../middleware/error-handler')
 const router = express.Router()
 const admin = require('./modules/admin')
 
 router.use('/admin', admin)
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp) // 注意用 post
 router.get('/restaurants', restController.getRestaurants)
 
 router.use('/', (req, res) => res.redirect('/restaurants'))
+router.use('/', generalErrorHandler) // 加入這行
 
 module.exports = router
