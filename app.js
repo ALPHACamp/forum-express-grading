f0464909cfe0aabc047373fbea53c86d9fd143f4
zephyr Lin <zephyrlin33@gmail.com>
2022-11-19 17:03:08 +0800

    passport& signin

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const routes = require('./routes')
 require('./models') // 暫時新增這行，引入資料庫，檢查完可刪
 const flash = require('connect-flash')
 const session = require('express-session')
+const passport = require('./config/passport')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -14,6 +15,8 @@ app.set('view engine', 'hbs')
 
 app.use(express.urlencoded({ extended: true })) // 使用POST方法，配合body-parson
 app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
+app.use(passport.initialize()) // 增加這行，初始化 Passport
+app.use(passport.session()) // 增加這行，啟動 session 功能
 app.use(flash()) // 掛載套件
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages') // 設定 success_msg 訊息
--- routes/index.js
+++ routes/index.js
@@ -2,15 +2,18 @@ const express = require('express')
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller') // 新增這行
 const { generalErrorHandler } = require('../middleware/error-handler')
+const passport = require('../config/passport')
 const router = express.Router()
 const admin = require('./modules/admin')
 
 router.use('/admin', admin)
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp) // 注意用 post
+router.get('/signin', userController.signInPage)
+router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn) // 注意是 post
 router.get('/restaurants', restController.getRestaurants)
 
-router.use('/', (req, res) => res.redirect('/restaurants'))
+router.get('/', (req, res) => res.redirect('/restaurants'))
 router.use('/', generalErrorHandler) // 加入這行
 
 module.exports = router
