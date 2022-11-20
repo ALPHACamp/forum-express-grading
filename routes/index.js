40f4f0aa98b7e224cb8fe7a9753b4bffbc0f1c37
zephyr Lin <zephyrlin33@gmail.com>
2022-11-20 19:45:06 +0800

    add create rest func

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,13 @@ const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller') // 新增這行
 
 const { generalErrorHandler } = require('../middleware/error-handler')
-const { authenticated } = require('../middleware/auth')
+const { authenticated, authenticatedAdmin } = require('../middleware/auth')
 
 const passport = require('../config/passport')
 const router = express.Router()
 const admin = require('./modules/admin')
 
-router.use('/admin', admin)
+router.use('/admin', authenticatedAdmin, admin)
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp) // 注意用 post
 router.get('/signin', userController.signInPage)
