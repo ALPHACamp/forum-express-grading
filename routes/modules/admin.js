7d055d208f917f1583ef0cb072c7d61e5d73de01
OneZerocococo <ylflyfly@gmail.com>
2022-11-20 22:24:49 +0800

    feat: user model add is_admin and add logout feature

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,6 +1,7 @@
 const express = require('express')
+const { authenticatedAdmin } = require('../../middleware/auth')
 const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
-router.get('/restaurants', adminController.getRestaurants)
+router.get('/restaurants', authenticatedAdmin, adminController.getRestaurants)
 router.use('/', (req, res) => res.redirect('/admin/restaurants'))
 module.exports = router
