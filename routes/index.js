7d66dd2a93c3979888df640f6772ef3310ccdf0e
zephyr Lin <zephyrlin33@gmail.com>
2022-11-20 12:39:03 +0800

    user model add isAdmin column & func

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller') // 新增這行
+
 const { generalErrorHandler } = require('../middleware/error-handler')
+const { authenticated } = require('../middleware/auth')
+
 const passport = require('../config/passport')
 const router = express.Router()
 const admin = require('./modules/admin')
@@ -11,7 +14,8 @@ router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp) // 注意用 post
 router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn) // 注意是 post
-router.get('/restaurants', restController.getRestaurants)
+router.get('/logout', userController.logout)
+router.get('/restaurants', authenticated, restController.getRestaurants)
 
 router.get('/', (req, res) => res.redirect('/restaurants'))
 router.use('/', generalErrorHandler) // 加入這行
