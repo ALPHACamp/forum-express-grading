9dc6ad84745413ba15a705eac681db5e8d26fd53
Coli-co <awestlifeblue@gmail.com>
2022-11-21 23:49:11 +0800

    feat: user signup

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const db = require('./models')
 app.engine('hbs', handlebars({ extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(express.urlencoded({ extended: true }))
+
 app.use(routes)
 
 app.listen(port, () => {
