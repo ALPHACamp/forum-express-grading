9b8d350dd695876aafaef5e6cc2c64df70f2e90b
linway035 <wayneck0138@gmail.com>
2022-11-21 14:02:22 +0800

    feat: add index page

---
+++ controllers/restaurant-controller.js
@@ -0,0 +1,10 @@
+// restaurantController 是一個物件 (object)。
+// restaurantController 有不同的方法，例如 getRestaurants
+const restaurantController = {
+  getRestaurants: (req, res) => {
+    return res.render("restaurants");
+  },
+};
+
+// 匯出之後才能在其他檔案裡使用。
+module.exports = restaurantController;
