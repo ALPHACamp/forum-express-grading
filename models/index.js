d6f37e628c0e60b06d583e3bbe7b2f9e1e36b892
linway035 <wayneck0138@gmail.com>
2022-11-21 14:28:06 +0800

    docs: add comment on models index

--- models/index.js
+++ models/index.js
@@ -9,21 +9,32 @@ const config = require(path.resolve(__dirname, '../config/config.json'))[env]
 const db = {}
 
 // 資料庫連線
+// 優先根據環境變數來決定連線資料庫的參數，若無的話則 config.json 寫的那些設定
 let sequelize
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config)
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config)
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config
+  )
 }
 
 // 動態引入其他 models
-fs
-  .readdirSync(__dirname)
+// 例如，假設有個 Model檔案 User.js，被 fs 掃描到且運用 sequelize引入進來，之後就可以用 db.User 來存取這個 Model。
+fs.readdirSync(__dirname)
   .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js')
+    return (
+      file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js'
+    )
   })
   .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes)
+    const model = require(path.join(__dirname, file))(
+      sequelize,
+      Sequelize.DataTypes
+    )
     db[model.name] = model
   })
 
@@ -35,7 +46,7 @@ Object.keys(db).forEach(modelName => {
 })
 
 // 匯出需要的物件
-db.sequelize = sequelize
-db.Sequelize = Sequelize
+db.sequelize = sequelize // 代表連線資料庫的 instance。這個 instance 擁有一些屬性如 queryInterface、config 等
+db.Sequelize = Sequelize // 存取到 Sequelize 這個 class，代表 Sequelize 函式庫本身。這個 class 有一些屬性如 DataType、Validator、Model
 
 module.exports = db
