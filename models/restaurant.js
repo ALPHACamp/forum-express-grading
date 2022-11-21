e1cdcb17ccf69ff102824ecee4adaa36929d300e
SimonHung8 <simon850226@gmail.com>
2022-11-21 23:53:53 +0800

    feat: add category model

--- models/restaurant.js
+++ models/restaurant.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate (models) {
       // define association here
+      Restaurant.belongsTo(models.Category, { foreignKey: 'categoryId' })
     }
   };
   Restaurant.init({
@@ -23,6 +24,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'Restaurant',
+    tableName: 'Restaurants',
     underscored: true
   })
   return Restaurant
