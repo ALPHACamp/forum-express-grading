d4222ab3d38711b636ac00f53d242ae49a417d85
Hua0720 <stella573021@gmail.com>
2022-11-22 00:31:28 +0800

    project init

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,10 @@ const express = require('express')
 const router = express.Router()
 // 新增，載入 controller
 const restController = require('../controllers/restaurant-controller')
+// 載入 admin.js
+const admin = require('./modules/admin')
+
+router.use('/admin', admin)
 
 router.get('/', (req, res) => {
   res.send('Hello World!')
