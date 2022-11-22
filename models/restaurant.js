7ef30ece0be58b0482021f98baa6175892458cf0
JoyWanddrr <m81y102@msn.com>
2022-11-22 20:15:50 +0800

    add Restaurant model

--- models/restaurant.js
+++ models/restaurant.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
+  class Restaurant extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -13,16 +13,17 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   };
-  User.init({
+  Restaurant.init({
     name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    isAdmin: DataTypes.BOOLEAN
+    tel: DataTypes.STRING,
+    address: DataTypes.STRING,
+    openingHours: DataTypes.STRING,
+    description: DataTypes.TEXT
   }, {
     sequelize,
-    modelName: 'User',
-    tableName: 'Users',
+    modelName: 'Restaurant',
+    tableName: 'Restaurants',
     underscored: true
   })
-  return User
+  return Restaurant
 }
