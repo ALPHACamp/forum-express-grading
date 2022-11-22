f7e9242d73c2dbb793169bdbe544a0431e15ba91
linway035 <wayneck0138@gmail.com>
2022-11-22 20:59:03 +0800

    feat: delete restaurant

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -7,6 +7,7 @@ router.get("/restaurants/create", adminController.createRestaurant);
 router.get("/restaurants/:id/edit", adminController.editRestaurant); //冒號後面名稱跟隨controller中的命名
 router.get("/restaurants/:rest_id", adminController.getRestaurant); //冒號後面名稱跟隨controller中的命名
 router.put("/restaurants/:id", adminController.putRestaurant); // 修改這一行為 put，冒號後面名稱跟隨controller中的命名
+router.delete("/restaurants/:id", adminController.deleteRestaurant);
 router.get("/restaurants", adminController.getRestaurants);
 router.post("/restaurants", adminController.postRestaurant);
 router.get("", (req, res) => res.redirect("/admin/restaurants"));
