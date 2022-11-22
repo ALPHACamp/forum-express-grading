288a6444ec391958975058bc927dbbb0c45edf6f
Rae7771653 <nicole831220@gmail.com>
2022-11-22 23:34:11 +0800

    feat:add user model and signup function

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
+const db = require('./models')
 const routes = require('./routes')
 
 const app = express()
 const port = process.env.PORT || 3000
 app.engine('hbs', handlebars({ extname: '.hbs' }))
 app.set('view engine', 'hbs')
+app.use(express.urlencoded({ extended: true }))
 app.use(routes)
 
 app.listen(port, () => {
---
+++ controllers/user-controller.js
@@ -0,0 +1,17 @@
+const bcrypt = require('bcryptjs')
+const db = require('../models')
+const { User } = db
+module.exports = {
+  signUpPage: (req, res) => res.render('signup'),
+  signUp: (req, res) => {
+    bcrypt.hash(req.body.password, 10)
+      .then(hash => User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: hash
+      }))
+      .then(() => {
+        res.redirect('/signin')
+      })
+  }   
+}
