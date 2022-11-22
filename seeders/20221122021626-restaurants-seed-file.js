bb65356abfbe91e05a2768a14e4d269d1583e6e1
SimonHung8 <simon850226@gmail.com>
2022-11-22 10:32:18 +0800

    feat: update seed files

--- seeders/20221122021626-restaurants-seed-file.js
+++ seeders/20221122021626-restaurants-seed-file.js
@@ -2,16 +2,21 @@
 const { faker } = require('@faker-js/faker/locale/zh_TW')
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    const categories = await queryInterface.sequelize.query(
+      'SELECT id FROM Categories;',
+      { type: queryInterface.sequelize.QueryTypes.SELECT }
+    )
     await queryInterface.bulkInsert('Restaurants',
       Array.from({ length: 50 }, () => ({
-        name: faker.name.findName(),
-        tel: faker.phone.phoneNumber(),
+        name: faker.name.fullName(),
+        tel: faker.phone.number(),
         address: faker.address.streetAddress(),
         opening_hours: '08:00',
         image: `https://loremflickr.com/320/240/restaurant,food/?random=${Math.random() * 100}`,
         description: faker.lorem.text(),
         created_at: new Date(),
-        updated_at: new Date()
+        updated_at: new Date(),
+        category_id: categories[Math.floor(Math.random() * categories.length)].id
       }))
     )
   },
