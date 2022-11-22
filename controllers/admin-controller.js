8ae4b492775203c0afa4e8cf0a9d720cc947aa30
linway035 <wayneck0138@gmail.com>
2022-11-22 18:05:36 +0800

    feat: modify admin restaurants page

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,6 +1,15 @@
+// const db=require('../models')
+// const Restaurant=db.Restaurant
+const { Restaurant } = require("../models");
+
 const adminController = {
-  getRestaurants: (req, res) => {
-    return res.render('admin/restaurants')
-  }
-}
-module.exports = adminController
+  getRestaurants: (req, res, next) => {
+    Restaurant.findAll({
+      raw: true, //把 Sequelize 包裝過的一大包物件轉換成格式比較單純的 JS 原生物件
+    })
+      .then((restaurants) => res.render("admin/restaurants", { restaurants }))
+      .catch((err) => next(err));
+  },
+};
+
+module.exports = adminController;
