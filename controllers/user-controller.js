eb342e10bbef8b1cd16abfaef418fc1df33594ed
JoyWanddrr <m81y102@msn.com>
2022-11-22 15:17:21 +0800

    add error handeler & error message page

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -9,14 +9,27 @@ const userController = {
     res.render('signup')
   },
   // 負責實際處理註冊的行為
-  signUp: (req, res) => {
-    bcrypt.hash(req.body.password, 10)
+  signUp: (req, res, next) => {
+    // 判斷兩次密碼是否相符
+    if (req.body.password !== req.body.passwordCheck) throw new Error('Passwords do not match!')
+    // 判斷email是否已註冊
+    User.findOne({ where: { email: req.body.email } })
+      .then(user => {
+        // 若已有使用者，回傳錯誤訊息
+        if (user) throw new Error('Email already exists!')
+        return bcrypt.hash(req.body.password, 10)
+        // 在這裡return可丟到下一個then函式
+      })
       .then(hash => User.create({
         name: req.body.name,
         email: req.body.email,
         password: hash
       }))
-      .then(() => res.redirect('/signin'))
+      .then(() => {
+        req.flash('success_messages', '成功註冊帳號！')
+        res.redirect('/signin')
+      })
+      .catch(err => next(err))
   }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,9 @@ const router = express.Router()
 const restController = require('../controllers/restaurant-controller')
 const admin = require('./modules/admin')
 const userController = require('../controllers/user-controller')
+const { generalErrorHandler } = require('../middleware/error-handler') // 須將middleware加入
 
-// 現在收到的請求如果帶有 /admin 的路徑，就一律丟給後台專用的 admin 這個 module 去處理，若是其他情況再依序往下判斷。(使用router.use)
+// 現在收到的請求如果帶有 /admin 的路徑，就一律丟給後台專用的 admin 這個 module 去處理，若是其他情況再依序往下判斷。(使用router.use )
 router.use('/admin', admin)
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)// 使用post處理signup的動作
@@ -13,5 +14,6 @@ router.get('/restaurants', restController.getRestaurants)
 
 // fallback路由。當其他路由條件都不符合時，最終會通過的路由。論此 request 是用哪個 HTTP method 發出的，都會匹配到這一行，將使用者重新導回 / restaurants。所以要注意順序。所以就是不論你網址亂打甚麼，他都會跳去/restaurants。
 router.use('/', (req, res) => { res.redirect('/restaurants') })
+router.use('/', generalErrorHandler)  // 須將middleware加入，不然無法顯示
 
 module.exports = router
