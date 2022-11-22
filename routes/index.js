3cf3ee6439fdb28873ee82bbf7b5a6fd3a56df1b
LAPTOP-TTILM0SK\Catherine <phes10217@gmail.com>
2022-11-22 23:17:07 +0800

    feat: add back and front's entry

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const router = express.Router()
+//新增，載入 controller
 
-router.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const restController = require('../controllers/restaurant-controller')
+//新增
+const admin = require('./modules/admin') //新增這行，載入 admin.js
+router.use('/admin', admin) //新增這行
+router.get('/restaurants', restController.getRestaurants)
+
+router.use('/', (req, res) => res.redirect('/restaurants'))
+
+module.exports = router
+
 
 module.exports = router
