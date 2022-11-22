944b9de64d622ef5b173597c31db2e8067f84fe3
Hua0720 <stella573021@gmail.com>
2022-11-22 23:00:29 +0800

    feat: user signup

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 // 新增，載入 controller
 const restController = require('../controllers/restaurant-controller')
+const userController = require('../controllers/user-controller') // 新增這行
 // 載入 admin.js
 const admin = require('./modules/admin')
 
@@ -11,8 +12,9 @@ router.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp) // 注意用 post
 // 新增
-
 router.get('/restaurants', restController.getRestaurants)
 
 router.use('/', (req, res) => res.redirect('/restaurants'))
