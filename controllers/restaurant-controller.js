4c4cb39039aa4c2de103f7c6f31c2d3774a12015
SimonHung8 <simon850226@gmail.com>
2022-11-22 22:18:50 +0800

    feat: add viewCounts to Restaurants and dashboard

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -19,16 +19,27 @@ const restaurantController = {
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
       include: Category,
-      nest: true,
-      raw: true
+      nest: true
     })
       .then(restaurant => {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
-        res.render('restaurant', {
-          restaurant
-        })
+        return restaurant.increment('viewCounts', { by: 1 })
+      })
+      .then(restaurant => {
+        return res.render('restaurant', { restaurant: restaurant.toJSON() })
       })
       .catch(err => next(err))
+  },
+  getDashboard: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, {
+      include: Category,
+      nest: true
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        return res.render('dashboard', { restaurant: restaurant.toJSON() })
+      })
+      .catch(next)
   }
 }
 module.exports = restaurantController
