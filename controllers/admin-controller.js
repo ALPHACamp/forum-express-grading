79c497c4b26eefbefaa83c545810d10a01fd321a
JoyWanddrr <m81y102@msn.com>
2022-11-22 20:38:41 +0800

    update admin restauant feature

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,8 +1,15 @@
 // 後台
 
+// 導入model
+const { Restaurant } = require('../models')
+
 const adminController = {
-  getRestaurants: (req, res) => {
-    return res.render('admin/restaurants')
+  getRestaurants: (req, res, next) => {
+    // 渲染所有餐廳
+    Restaurant.findAll({ raw: true })
+      // 不使用raw:true，會回傳sequelize的instance。在沒有要用Sequelize做後續操作時，可以轉成JS原生物件。
+      .then(restaurants => res.render('admin/restaurants', { restaurants }))
+      .catch(err => next(err))
   }
 }
 
