25a6d11979b722154d7f490256e6d193a2e7f05e
Hua0720 <stella573021@gmail.com>
2022-11-23 17:54:36 +0800

    feat: flash msg & signup verification

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router()
 // 新增，載入 controller
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller') // 新增這行
+const { generalErrorHandler } = require('../middleware/error-handler')
 // 載入 admin.js
 const admin = require('./modules/admin')
 
@@ -18,5 +19,7 @@ router.post('/signup', userController.signUp) // 注意用 post
 router.get('/restaurants', restController.getRestaurants)
 
 router.use('/', (req, res) => res.redirect('/restaurants'))
+
+router.use('/', generalErrorHandler)
 
 module.exports = router
