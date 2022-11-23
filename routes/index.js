5157b41c15264e8e3644e482051e7f48b14c8a6e
LAPTOP-TTILM0SK\Catherine <phes10217@gmail.com>
2022-11-24 01:42:01 +0800

    feat:user signup

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,13 @@ const router = express.Router()
 const restController = require('../controllers/restaurant-controller')
 //新增
 const admin = require('./modules/admin') //新增這行，載入 admin.js
+const userController = require('../controllers/user-controller') //新增這行
 router.use('/admin', admin) //新增這行
+
+//新增下面這兩行，注意順序
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp) //注意用 post
+
 router.get('/restaurants', restController.getRestaurants)
 
 router.use('/', (req, res) => res.redirect('/restaurants'))
