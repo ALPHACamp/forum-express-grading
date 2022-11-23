729ae62ea9b55791d5f3867da3ed1159aec9fe1b
cosine0720 <cosine0720@gmail.com>
2022-11-23 11:21:16 +0800

    feat: create restaurant

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,10 @@ const passport = require('../config/passport')
 const admin = require('./modules/admin')
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
-const { authenticated } = require('../middleware/auth')
+const { authenticated, authenticatedAdmin } = require('../middleware/auth')
 const { generalErrorHandler } = require('../middleware/error-handler')
 
-router.use('/admin', admin)
+router.use('/admin', authenticatedAdmin, admin)
 
 // 註冊
 router.get('/signup', userController.signUpPage)
