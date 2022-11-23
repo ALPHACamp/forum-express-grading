785676140c93ffaaa944262806255f7d680473b1
LAPTOP-TTILM0SK\Catherine <phes10217@gmail.com>
2022-11-24 04:39:36 +0800

    feat: admin restaurant page

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
 
 router.get('/restaurants/create', adminController.createRestaurant) // 新增這行
+router.get('/restaurants/:id', adminController.getRestaurant) //新增這一行
 router.get('/restaurants', adminController.getRestaurants)
 router.post('/restaurants', adminController.postRestaurant) // 新增這行
 router.get('/', (req, res) => res.redirect('/admin/restaurants'))
