a960c8bc710f069413c30ee1d646ebef91da346a
Gincoolwant <soulbox790326@gmail.com>
2022-11-23 17:31:21 +0800

    feat: flash msg & signup verification

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
+const flash = require('connect-flash')
+const session = require('express-session')
 
 const routes = require('./routes')
 
 const app = express()
 const port = process.env.PORT || 3000
+const SESSION_SECRET = 'secret'
 
 app.engine('hbs', handlebars({ extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
+app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
+app.use(flash())
 
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.error_msg = req.flash('error_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
+  next()
+})
 app.use(routes)
 
 app.listen(port, () => {
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -6,8 +6,13 @@ const userController = {
   signUpPage: (req, res) => {
     return res.render('signup')
   },
-  signUp: (req, res) => {
-    bcrypt.hash(req.body.password, 10)
+  signUp: (req, res, next) => {
+    if (req.body.password !== req.body.passwordCheck) throw new Error('Password do not match!')
+    User.findOne({ where: { email: req.body.email } })
+      .then(user => {
+        if (user) throw new Error('The email already exist.')
+        return bcrypt.hash(req.body.password, 10)
+      })
       .then(hash => {
         return User.create({
           name: req.body.name,
@@ -16,8 +21,10 @@ const userController = {
         })
       })
       .then(() => {
+        req.flash('success_msg','您已成功註冊。')
         return res.redirect('/signin')
       })
+      .catch(err => next(err))
   }
 }
 
