d674860013fcc80c2454fb1bf535ea575e8d4f7e
Hua0720 <stella573021@gmail.com>
2022-11-23 18:27:25 +0800

    feat: passport init & signin

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const router = express.Router()
+
+const passport = require('../config/passport') // 引入 Passport，需要他幫忙做驗證
+
 // 新增，載入 controller
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller') // 新增這行
@@ -15,6 +18,11 @@ router.get('/', (req, res) => {
 
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp) // 注意用 post
+
+router.get('/signin', userController.signInPage)
+router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn) // 注意是 post
+router.get('/logout', userController.logout)
+
 // 新增
 router.get('/restaurants', restController.getRestaurants)
 
