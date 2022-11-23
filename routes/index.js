8dc3cda7ec767d306582ddaeae887646b80f426c
Ting-Yu, Lin <tingyulin68@gmail.com>
2022-11-23 13:17:26 +0800

    feat: user model add is_admin

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const router = express.Router()
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
 const { generalErrorHandler } = require('../middleware/error-handler')
+const { authenticated } = require('../middleware/auth')
 const admin = require('./modules/admin')
 
 router.use('/admin', admin)
@@ -19,7 +20,7 @@ router.post(
   userController.signIn
 )
 router.get('/logout', userController.logout)
-router.get('/restaurants', restController.getRestaurants)
+router.get('/restaurants', authenticated, restController.getRestaurants)
 router.use('/', (req, res) => res.redirect('/restaurants'))
 router.use('/', generalErrorHandler)
 module.exports = router
