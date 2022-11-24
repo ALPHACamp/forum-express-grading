81785054a991f8e39333ed859cba7c61104d5d24
Ting-Yu, Lin <tingyulin68@gmail.com>
2022-11-24 17:19:40 +0800

    feat: add ifCond hbs helper & update category selector

--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -2,6 +2,10 @@ const dayjs = require('dayjs')
 const { isSuperUser } = require('./users-helper')
 module.exports = {
   currentYear: () => dayjs().year(),
-  disabledRoot: (user, options) =>
-    isSuperUser(user) ? options.fn(this) : options.inverse(this)
+  disabledRoot: function (user, options) {
+    return isSuperUser(user) ? options.fn(this) : options.inverse(this)
+  },
+  ifCond: function (a, b, options) {
+    return a === b ? options.fn(this) : options.inverse(this)
+  }
 }
