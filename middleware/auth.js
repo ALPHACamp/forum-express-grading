26b4687606d09942d6092395781657e68e8a2dcc
Tzuminn <valuesu0326@gmail.com>
2022-11-24 21:06:49 +0800

    feat: user model add is_admin

---
+++ middleware/auth.js
@@ -0,0 +1,29 @@
+const { ensureAuthenticated, getUser } = require('../helpers/auth-helpers')
+
+// 驗證登入狀態
+const authenticated = (req, res, next) => {
+  // if (req.isAuthenticated)
+  if (ensureAuthenticated(req)) {
+    // 有登入=>往下走
+    return next()
+  }
+  // 未登入=>丟回登入頁
+  res.redirect('/signin')
+}
+
+// 驗證管理員身份
+const authenticatedAdmin = (req, res, next) => {
+  if (ensureAuthenticated(req)) {
+    // 有登入且為管理員=>往下走
+    if (getUser(req).isAdmin) return next()
+    // 有登入但非管理員=>丟回首頁
+    res.redirect('/')
+  } else {
+    res.redirect('/signin')
+  }
+}
+
+module.exports = {
+  authenticated,
+  authenticatedAdmin
+}
