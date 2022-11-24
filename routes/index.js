ce7dbc1e7e60e7deaa53360324e33e7c2bc83c09
Victor Cheng <weitungstyle@gmail.com>
2022-11-24 14:26:32 +0800

    feat: create restaurant

--- routes/index.js
+++ routes/index.js
@@ -7,8 +7,9 @@ const restaurantController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
 const { authenticated } = require('../middleware/auth')
 const { generalErrorHandler } = require('../middleware/error-handler')
+const { authenticatedAdmin } = require('../middleware/auth')
 
-router.use('/admin', admin)
+router.use('/admin', authenticatedAdmin, admin)
 
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
