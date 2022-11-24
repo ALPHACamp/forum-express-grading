971d28e9fe87e9d4b0cb2b1124d8656a39c6a0ec
Tzuminn <valuesu0326@gmail.com>
2022-11-25 01:09:49 +0800

    feat:adjust middleware/auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,9 +1,9 @@
-const { ensureAuthenticated, getUser } = require('../helpers/auth-helpers')
+const helpers = require('../helpers/auth-helpers')
 
 // 驗證登入狀態
 const authenticated = (req, res, next) => {
   // if (req.isAuthenticated)
-  if (ensureAuthenticated(req)) {
+  if (helpers.ensureAuthenticated(req)) {
     // 有登入=>往下走
     return next()
   }
@@ -13,9 +13,9 @@ const authenticated = (req, res, next) => {
 
 // 驗證管理員身份
 const authenticatedAdmin = (req, res, next) => {
-  if (ensureAuthenticated(req)) {
+  if (helpers.ensureAuthenticated(req)) {
     // 有登入且為管理員=>往下走
-    if (getUser(req).isAdmin) return next()
+    if (helpers.getUser(req).isAdmin) return next()
     // 有登入但非管理員=>丟回首頁
     res.redirect('/')
   } else {
