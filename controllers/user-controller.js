2a14bde62d4067539ca137ad611a22e5f4bfddd3
seanlin1125 <sean821125@gmail.com>
2022-11-24 11:27:01 +0800

    feat: adjust user-controller and users page

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -5,18 +5,19 @@ const userController = {
   signUpPage: (req, res) => {
     res.render('signup')
   },
-  signUp: (req, res, next) => { // 修改這裡
+  signUp: (req, res, next) => {
+    const { name, email, password, passwordCheck } = req.body
     // 如果兩次輸入的密碼不同，就建立一個 Error 物件並拋出
-    if (req.body.password !== req.body.passwordCheck) throw new Error('Passwords do not match!')
+    if (password !== passwordCheck) throw new Error('Passwords do not match!')
     // 確認資料裡面沒有一樣的 email，若有，就建立一個 Error 物件並拋出
-    User.findOne({ where: { email: req.body.email } })
+    User.findOne({ where: { email } })
       .then(user => {
         if (user) throw new Error('Email already exists!')
-        return bcrypt.hash(req.body.password, 10) // 前面加 return
+        return bcrypt.hash(password, 10) // 前面加 return
       })
       .then(hash => User.create({ // 上面錯誤狀況都沒發生，就把使用者的資料寫入資料庫
-        name: req.body.name,
-        email: req.body.email,
+        name,
+        email,
         password: hash
       }))
       .then(() => {
