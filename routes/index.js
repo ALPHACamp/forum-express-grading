337e5a4ce493cfea2f849eb2332b4dba49f554b3
Hua0720 <stella573021@gmail.com>
2022-11-24 23:11:00 +0800

    feat: create restaurant

--- routes/index.js
+++ routes/index.js
@@ -6,16 +6,16 @@ const passport = require('../config/passport') // 引入 Passport，需要他幫
 // 新增，載入 controller
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller') // 新增這行
-const { authenticated } = require('../middleware/auth') // 引入 auth.js
+const { authenticated, authenticatedAdmin } = require('../middleware/auth')// 引入 auth.js
 const { generalErrorHandler } = require('../middleware/error-handler')
 // 載入 admin.js
 const admin = require('./modules/admin')
 
-router.use('/admin', admin)
+router.use('/admin', authenticatedAdmin, admin)
 
-router.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// router.get('/', (req, res) => {
+//   res.send('Hello World!')
+// })
 
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp) // 注意用 post
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
-const { authenticatedAdmin } = require('../../middleware/auth') // 引入 auth.js
-router.get('/restaurants', authenticatedAdmin, adminController.getRestaurants) // 修改這行，新增 authenticatedAdmin 參數
+// const { authenticatedAdmin } = require('../../middleware/auth') // 引入 auth.js
+router.get('/restaurants/create', adminController.createRestaurant) // 把 authenticatedAdmin 參數拿出去到index內，由index頁傳入
+router.get('/restaurants', adminController.getRestaurants)
+router.post('/restaurants', adminController.postRestaurant)
+
 router.use('/', (req, res) => res.redirect('/admin/restaurants'))
 module.exports = router
