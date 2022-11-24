349b7293533e452e4795d116cd4809a8b6ec6c28
Coli-co <awestlifeblue@gmail.com>
2022-11-24 10:27:36 +0800

    user persmission message fixed

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -100,13 +100,13 @@ const adminController = {
         if (!user) throw new Error("User doesn't exist !")
 
         if (user.email === 'root@example.com') {
-          req.flash('error_messages', "User permission can't be changed !")
+          req.flash('error_messages', '禁止變更 root 權限')
           return res.redirect('back')
         }
         return user.update({ isAdmin: !user.isAdmin })
       })
       .then(() => {
-        req.flash('success_messages', 'User role was successfully updated !')
+        req.flash('success_messages', '使用者權限變更成功')
         return res.redirect('/admin/users')
       })
       .catch((err) => next(err))
