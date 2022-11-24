f366718a6c5db66ef6f7847dbee5313711b9ec58
Tzuminn <valuesu0326@gmail.com>
2022-11-24 20:54:40 +0800

    feat: passport init & signin

---
+++ config/passport.js
@@ -0,0 +1,42 @@
+const passport = require('passport')
+const LocalStrategy = require('passport-local')
+const bcrypt = require('bcryptjs')
+
+const db = require('../models')
+const User = db.User
+
+// 設定Strategies (登入策略)
+passport.use(new LocalStrategy(
+  // 參數1:設定客製化選項:帳號用email 密碼用password
+  {
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true // 拿到req，供flash使用
+  },
+  // 參數2:一個 callback function，內容是驗證使用者。
+  (req, email, password, cb) => {
+    User.findOne({ where: { email } })
+      .then(user => {
+        // 使用者不存在
+        if (!user) return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤！'))
+        // 使用者存在，驗證密碼
+        bcrypt.compare(password, user.password).then(res => {
+          // 密碼錯誤
+          if (!res) return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤！'))
+          // 驗證通過
+          return cb(null, user)
+        })
+      })
+  }
+))
+// 設定序列化、反序列化
+passport.serializeUser((user, cb) => {
+  cb(null, user.id)
+})
+passport.deserializeUser((id, cb) => {
+  User.findByPk(id).then(user => {
+    user = user.toJSON()
+    return cb(null, user)
+  })
+})
+module.exports = passport
