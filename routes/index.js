3440a8bf4bec4e59135efe7199c5e7a7c10adee9
wujia777 <cwu005104@gmail.com>
2022-11-25 23:28:34 +0800

    feat: add user SignUp page

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,10 @@ const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
 const admin = require('./modules/admin')
 
-router.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 router.use('/admin', admin)
+
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
 router.get('/restaurants', restController.getRestaurants)
-router.use('/', (req, res) => res.redirect('/restaurants'))
+router.get('/', (req, res) => res.redirect('/restaurants'))
 module.exports = router
