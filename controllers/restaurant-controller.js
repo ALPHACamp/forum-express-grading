adb0a0537544d572def700722c54fe463f89a0bd
Ting-Yu, Lin <tingyulin68@gmail.com>
2022-11-25 11:03:17 +0800

    feat: add restaurants index page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,6 +1,16 @@
+const { Restaurant, Category } = require('../models')
 const restaurantController = {
-  getRestaurants: (req, res) => {
-    return res.render('restaurants')
+  getRestaurants: (req, res, next) => {
+    return Restaurant.findAll({ include: Category, nest: true, raw: true })
+      .then(restaurants => {
+        if (!restaurants.length) return req.flash('error_messages', 'No restaurant is found!')
+        const datas = restaurants.map(r => ({
+          ...r,
+          description: r.description.substring(0, 50)
+        }))
+        return res.render('restaurants', { restaurants: datas })
+      })
+      .catch(err => next(err))
   }
 }
 module.exports = restaurantController
