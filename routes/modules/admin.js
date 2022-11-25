9b8f4f74819b5d85d2e049843259fbb94b66a51d
ping8601 <wanping86701@gmail.com>
2022-11-25 22:04:16 +0800

    feat: admin restaurant page

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -3,11 +3,13 @@ const router = express.Router()
 
 const adminController = require('../../controllers/admin-controller')
 
-// get all restaurants
-router.get('/restaurants', adminController.getRestaurants)
-
 // create restaurant
 router.get('/restaurants/create', adminController.createRestaurant)
+// view restaurant
+router.get('/restaurants/:id', adminController.getRestaurant)
+// get all restaurants
+router.get('/restaurants', adminController.getRestaurants)
+// post restaurant
 router.post('/restaurants', adminController.postRestaurant)
 
 router.get('', (req, res) => res.redirect('/admin/restaurants'))
