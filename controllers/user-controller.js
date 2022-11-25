20a7ed1b9bed10266c1727de9e0b5354e79a8bdd
wujia777 <cwu005104@gmail.com>
2022-11-26 01:07:45 +0800

    feat: add flash msg & signup verification

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -6,13 +6,25 @@ const userController = {
   signUpPage: (req, res) => {
     res.render('signup')
   },
-  signUp: (req, res) => {
-    const { name, email, password } = req.body
-    User.create({
-      name,
-      email,
-      password: bcrypt.hashSync(password, bcrypt.genSaltSync(10), null)
-    }).then(() => res.redirect('/signin'))
+  signUp: (req, res, next) => {
+    const { name, email, password, passwordCheck } = req.body
+    if (password !== passwordCheck) throw new Error('Password do not match!')
+
+    User.findOne({ where: { email } })
+      .then(user => {
+        if (user) throw new Error('Email already exists!')
+        return bcrypt.hash(password, 10) // 可以少一層巢狀結構
+      })
+      .then(hash => User.create({
+        name,
+        email,
+        password: hash
+      }))
+      .then(() => {
+        req.flash('success_messages', '成功註冊帳號!')
+        res.redirect('/signin')
+      })
+      .catch(err => next(err))
   }
 }
 
