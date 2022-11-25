ebf7aff9e24d649b646ef3e19912e7c00a9b6986
Ting-Yu, Lin <tingyulin68@gmail.com>
2022-11-25 11:17:27 +0800

    feat: add restaurant page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,14 +1,29 @@
+const assert = require('assert')
 const { Restaurant, Category } = require('../models')
 const restaurantController = {
   getRestaurants: (req, res, next) => {
     return Restaurant.findAll({ include: Category, nest: true, raw: true })
       .then(restaurants => {
-        if (!restaurants.length) return req.flash('error_messages', 'No restaurant is found!')
+        if (!restaurants.length) {
+          return req.flash('error_messages', 'No restaurant is found!')
+        }
         const datas = restaurants.map(r => ({
           ...r,
           description: r.description.substring(0, 50)
         }))
         return res.render('restaurants', { restaurants: datas })
+      })
+      .catch(err => next(err))
+  },
+  getRestaurant: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, {
+      include: Category,
+      nest: true,
+      raw: true
+    })
+      .then(restaurant => {
+        assert(restaurant, "Restaurant did't exist!")
+        return res.render('restaurant', { restaurant })
       })
       .catch(err => next(err))
   }
