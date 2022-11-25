5be3159a34edfab11fa4a43c720c59c987a19d3e
ping8601 <wanping86701@gmail.com>
2022-11-25 15:18:07 +0800

    feat: flash msg & signup verification

--- app.js
+++ app.js
@@ -1,13 +1,29 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
+const session = require('express-session')
+const flash = require('connect-flash')
 const routes = require('./routes')
 
 const app = express()
 const port = process.env.PORT || 3000
+const SESSION_SECRET = 'secret'
 
 // set express-handlebars as view engine
 app.engine('hbs', handlebars({ extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+// use session
+app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
+
+// use flash
+app.use(flash())
+
+// add a middleware to add variables
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 
 // use body parser to handle all the request
 app.use(express.urlencoded({ extended: true }))
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -5,18 +5,23 @@ const userController = {
   signUpPage: (req, res) => {
     res.render('signup')
   },
-  signUp: (req, res) => {
-    bcrypt.hash(req.body.password, 10)
+  signUp: (req, res, next) => {
+    if (req.body.password !== req.body.passwordCheck) throw new Error('Passwords do not match!')
+
+    User.findOne({ where: { email: req.body.email } })
+      .then(user => {
+        if (user) throw new Error('User already exists!')
+        return bcrypt.hash(req.body.password, 10)
+      })
       .then(hash => User.create({
         name: req.body.name,
         email: req.body.email,
         password: hash
-      })
-        .catch(error => console.error(error))
-      )
+      }))
       .then(() => {
         res.redirect('/signin')
       })
+      .catch(err => next(err))
   }
 }
 
