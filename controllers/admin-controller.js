78cf7bddbc41569d969a947af231f6221de68720
linway035 <wayneck0138@gmail.com>
2022-11-25 16:57:40 +0800

    feat: add user rights management

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,6 +1,6 @@
 // const db=require('../models')
 // const Restaurant=db.Restaurant
-const { Restaurant } = require('../models')
+const { Restaurant, User } = require('../models')
 const {
   localFileHandler,
   imgurFileHandler
@@ -8,7 +8,7 @@ const {
 
 const adminController = {
   getRestaurants: (req, res, next) => {
-    Restaurant.findAll({
+    return Restaurant.findAll({
       raw: true // 把 Sequelize 包裝過的一大包物件轉換成格式比較單純的 JS 原生物件
     })
       .then(restaurants => res.render('admin/restaurants', { restaurants }))
@@ -22,7 +22,7 @@ const adminController = {
     if (!name) throw new Error('Restaurant name is required!') // name 是必填，若發先是空值就會終止程式碼，並在畫面顯示錯誤提示。即使前端有required，但前後端分離，後端必須自己也要控管。throw 讓我們可以在程式出錯時，終止執行此區塊的程式碼，並拋出客製化的錯誤訊息。
 
     const file = req.file // 把檔案取出來
-    imgurFileHandler(file) // 把取出的檔案傳給 file-helper 處理後。要用本地的話改成 localFileHandler這詞就好，下面put亦同
+    return imgurFileHandler(file) // 把取出的檔案傳給 file-helper 處理後。要用本地的話改成 localFileHandler這詞就好，下面put亦同
       .then(filePath =>
         Restaurant.create({
           // 產生一個新的 Restaurant 物件實例，並存入資料庫
@@ -41,7 +41,7 @@ const adminController = {
       .catch(err => next(err))
   },
   getRestaurant: (req, res, next) => {
-    Restaurant.findByPk(req.params.rest_id, {
+    return Restaurant.findByPk(req.params.rest_id, {
       // 去資料庫用 id(PK) 找一筆資料
       raw: true // 換成格式比較單純的 JS 原生物件
     })
@@ -52,7 +52,7 @@ const adminController = {
       .catch(err => next(err))
   },
   editRestaurant: (req, res, next) => {
-    Restaurant.findByPk(req.params.id, {
+    return Restaurant.findByPk(req.params.id, {
       raw: true
     })
       .then(restaurant => {
@@ -94,6 +94,27 @@ const adminController = {
         return restaurant.destroy()
       })
       .then(() => res.redirect('/admin/restaurants'))
+      .catch(err => next(err))
+  },
+  getUsers: (req, res, next) => {
+    return User.findAll({ raw: true })
+      .then(users => res.render('admin/users', { users }))
+      .catch(err => next(err))
+  },
+  patchUser: (req, res, next) => {
+    return User.findByPk(req.params.id)
+      .then(user => {
+        if (!user) throw new Error("User didn't exist!")
+        if (user.name === 'admin' && user.email === 'root@example.com') {
+          req.flash('error_messages', '禁止變更 root 權限')
+          return res.redirect('back')
+        }
+        return user.update({ isAdmin: !user.isAdmin }) // 處理boolean
+      })
+      .then(() => {
+        req.flash('success_messages', '使用者權限變更成功')
+        res.redirect('/admin/users')
+      })
       .catch(err => next(err))
   }
 }
