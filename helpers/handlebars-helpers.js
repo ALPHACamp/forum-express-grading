f2d3c6bae3482c3124e53ad5ff5c449d2e0f0232
LAPTOP-TTILM0SK\Catherine <phes10217@gmail.com>
2022-11-25 10:59:43 +0800

    feat:add 使用者權限調整

--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -1,4 +1,16 @@
-const dayjs = require('dayjs') // 載入 dayjs 套件
+
+
+const dayjs = require('dayjs')
+const { isSuperuser } = require('./superuser-helper')
+const relativeTime = require('dayjs/plugin/relativeTime')
+
+dayjs.extend(relativeTime)
+
 module.exports = {
-  currentYear: () => dayjs().year() // 取得當年年份作為 currentYear 的屬性值，並導出
+  currentYear: () => dayjs().year(),
+  relativeTimeFromNow: a => dayjs(a).fromNow(),
+  fixRoleSetting: email => isSuperuser(email) ? 'disabled' : null,
+  ifCond: function (a, b, options) {
+    return a === b ? options.fn(this) : options.inverse(this)
+  }
 }
