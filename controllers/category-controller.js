f32b05536ef1ffcc86eb7767c85a0d10ac2205ec
cosine0720 <cosine0720@gmail.com>
2022-11-26 20:44:16 +0800

    feat: category update

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -2,20 +2,42 @@ const { Category } = require('../models')
 
 const categoryController = {
   // [餐廳類別]============================
+  // 查：瀏覽全部類別 ＋ 新增/編輯功能
   getCategories: (req, res, next) => {
-    return Category.findAll({
-      raw: true,
-      nest: true
-    })
-      .then(categories => res.render('admin/categories', { categories }))
+    return Promise.all([
+      Category.findAll({ raw: true }),
+      req.params.id ? Category.findByPk(req.params.id, { raw: true }) : null
+    ])
+      .then(([categories, category]) => {
+        res.render('admin/categories', {
+          categories,
+          category
+        })
+      })
       .catch(err => next(err))
   },
+  // 增：
   postCategory: (req, res, next) => {
     const { name } = req.body
     if (!name) throw new Error('Category name is required!')
     Category.create({ name })
       .then(() => {
         req.flash('success_messages', 'Category was successfully created')
+        res.redirect('/admin/categories')
+      })
+      .catch(err => next(err))
+  },
+  // 改：
+  putCategory: (req, res, next) => {
+    const { name } = req.body
+    if (!name) throw new Error('Category name is required!')
+    return Category.findByPk(req.params.id)
+      .then(category => {
+        if (!category) throw new Error("Category didn't exist!")
+        return category.update({ name })
+      })
+      .then(() => {
+        req.flash('success_messages', 'Category was successfully to update')
         res.redirect('/admin/categories')
       })
       .catch(err => next(err))
