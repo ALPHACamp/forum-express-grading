f324e23705cd98388b32805b01eb82cbeff78576
xxirenee <charice0709@gmail.com>
2022-11-26 14:37:28 +0800

    feat: user model add is_admin

--- migrations/20221125151848-add-is-admin-to-users.js
+++ migrations/20221125151848-add-is-admin-to-users.js
@@ -1,21 +1,14 @@
-'use strict';
+'use strict'
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+    await queryInterface.addColumn('Users', 'is_admin', {
+      type: Sequelize.BOOLEAN,
+      defaultValue: false
+    })
   },
 
   down: async (queryInterface, Sequelize) => {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+    await queryInterface.removeColumn('Users', 'is_admin')
   }
-};
+}
