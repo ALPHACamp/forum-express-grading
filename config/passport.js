a4355bd9079682d1997bc2bb4330a5472a7b663e
wujia777 <cwu005104@gmail.com>
2022-11-26 11:30:47 +0800

    feat: add header & footer

--- config/passport.js
+++ config/passport.js
@@ -6,34 +6,34 @@ const User = db.User
 
 // setup passport strategy
 passport.use(new LocalStrategy(
-    { // customize user Field
-        usernameField: 'email',
-        passwordField: 'password',
-        passReqToCallback: true
-    },
-    // authenticate user
-    (req, email, password, cb) => {
-        User.findOne({ where: { email } })
-            .then(user => {
-                if (!user) return cb(null, false, req.flash('error_messages', '帳號或密碼錯誤!'))
-                bcrypt.compare(password, user.password).then(res => {
-                    if (res) return cb(null, false, req.flash('error_messages', '帳號或密碼錯誤!'))
-                })
-                return cb(null, user)
-            })
-    }
+  { // customize user Field
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true
+  },
+  // authenticate user
+  (req, email, password, cb) => {
+    User.findOne({ where: { email } })
+      .then(user => {
+        if (!user) return cb(null, false, req.flash('error_messages', '帳號或密碼錯誤!'))
+        bcrypt.compare(password, user.password).then(res => {
+          if (res) return cb(null, false, req.flash('error_messages', '帳號或密碼錯誤!'))
+        })
+        return cb(null, user)
+      })
+  }
 ))
 
 // serialize and deserialize user
 passport.serializeUser((user, cb) => {
-    cb(null, user.id)
+  cb(null, user.id)
 })
 
 passport.deserializeUser((id, cb) => {
-    User.findByPk(id).then(user => {
-        user = user.toJSON()
-        return cb(null, user)
-    })
+  User.findByPk(id).then(user => {
+    user = user.toJSON()
+    return cb(null, user)
+  })
 })
 
 module.exports = passport
