51761531d31aa21399ecb0663f7451f6eeaf2673
zephyr Lin <zephyrlin33@gmail.com>
2022-11-26 19:05:49 +0800

    add dashcoard page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,6 +1,22 @@
 const { Restaurant, Category } = require('../models')
 const restaurantController = {
 
+  // 顯示每間餐廳的統計數據
+  getDashboard: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, {
+      include: Category, // 拿出關聯的 Category model
+      nest: true,
+      raw: true
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        res.render('dashboard', {
+          restaurant
+        })
+      })
+      .catch(err => next(err))
+  },
+
   // 顯示首頁index page
   getRestaurants: (req, res) => {
     return Restaurant.findAll({
@@ -22,13 +38,14 @@ const restaurantController = {
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
       include: Category, // 拿出關聯的 Category model
-      nest: true,
-      raw: true
+      nest: true
+      // raw: true
     })
       .then(restaurant => {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
+        restaurant.increment('viewCounts')
         res.render('restaurant', {
-          restaurant
+          restaurant: restaurant.toJSON()
         })
       })
       .catch(err => next(err))
