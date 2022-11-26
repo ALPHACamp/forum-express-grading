c26dd0074d281c99e57cb05a088f29e767e5023a
wujia777 <cwu005104@gmail.com>
2022-11-26 20:47:08 +0800

    feat: add admin update rstaurant function

--- models/restaurant.js
+++ models/restaurant.js
@@ -17,7 +17,7 @@ module.exports = (sequelize, DataTypes) => {
     name: DataTypes.STRING,
     tel: DataTypes.STRING,
     address: DataTypes.STRING,
-    opening_hours: DataTypes.STRING,
+    openingHours: DataTypes.STRING,
     description: DataTypes.TEXT
   }, {
     sequelize,
