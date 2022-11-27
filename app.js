277b822ebf6a277286f7d3d1c3773a26c29b87a3
Leon-Chun <s1235753@gmail.com>
2022-11-27 12:27:40 +0800

    feat: user signup

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ const routes = require('./routes')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(routes)
-
 app.engine('hbs', handlebars({ extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(express.urlencoded({ extended: true }))
+app.use(routes)
 
 app.listen(port, () => {
   console.info(`Example app listening on port ${port}!`)
--- routes/index.js
+++ routes/index.js
@@ -2,12 +2,12 @@ const express = require('express')
 const router = express.Router()
 const restController = require('../controllers/restaurant-controller')
 const admin = require('./modules/admin')
+const userController = require('../controllers/user-controller')
 
 router.use('/admin', admin)
-router.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
 router.get('/restaurants', restController.getRestaurants)
 
 router.use('/', (req, res) => res.redirect('/restaurants'))
