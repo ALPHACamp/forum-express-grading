6dc5e029e7abaf21f7548fffb0c5065cbceda3ee
SimonHung8 <simon850226@gmail.com>
2022-11-27 13:14:45 +0800

    feat: add top-restaurants page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -31,7 +31,6 @@ const restaurantController = {
           isFavorited: favoritedRestaurantsId.includes(r.id),
           isLiked: likedRestaurantsId.includes(r.id)
         }))
-        console.log(data)
         return res.render('restaurants', {
           restaurants: data,
           categories,
@@ -97,6 +96,25 @@ const restaurantController = {
         })
       })
       .catch(err => next(err))
+  },
+  getTopRestaurants: (req, res, next) => {
+    return Restaurant.findAll({
+      include: [{
+        model: User, as: 'FavoritedUsers'
+      }]
+    })
+      .then(restaurants => {
+        const result = restaurants.map(r => ({
+          ...r.dataValues,
+          description: r.dataValues.description.substring(0, 50),
+          favoritedCount: r.FavoritedUsers.length,
+          isFavorited: req.user && req.user.FavoritedRestaurants.some(f => f.id === r.id)
+        }))
+          .sort((a, b) => b.favoritedCount - a.favoritedCount).slice(0, 10)
+        res.render('top-restaurants', { restaurants: result })
+      })
+      .catch(next)
   }
 }
+
 module.exports = restaurantController
