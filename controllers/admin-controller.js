01128cf4a194c6a09de6b77c28dc573a66495283
Leon-Chun <s1235753@gmail.com>
2022-11-27 14:58:59 +0800

    feat: add imgur api

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,4 +1,5 @@
 const { Restaurant } = require('../models')
+const { imgurFileHandler } = require('../helpers/file-helpers')
 const { localFileHandler } = require('../helpers/file-helpers')
 
 const adminController = {
@@ -17,6 +18,7 @@ const adminController = {
     if (!name) throw new Error('Restaurant name is required!')
     const { file } = req
     localFileHandler(file)
+    return imgurFileHandler(file)
       .then(filePath => Restaurant.create({
         name,
         tel,
@@ -54,9 +56,9 @@ const adminController = {
     const { name, tel, address, openingHours, description } = req.body
     if (!name) throw new Error('Restaurant name is required!')
     const { file } = req
-    Promise.all([
+    return Promise.all([
       Restaurant.findByPk(req.params.id),
-      localFileHandler(file)
+      imgurFileHandler(file)
     ])
       .then(([restaurant, filePath]) => {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
