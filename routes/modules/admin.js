53b20085253f2b13d6c74b720ecab847e051a6b3
AlanSquid <squid78313@gmail.com>
2022-11-27 13:44:41 +0800

    feat: category create

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -11,8 +11,12 @@ router.put('/restaurants/:id', upload.single('image'), adminController.putRestau
 router.delete('/restaurants/:id', adminController.deleteRestaurant)
 router.get('/restaurants', adminController.getRestaurants)
 router.post('/restaurants', upload.single('image'), adminController.postRestaurant)
+
 router.get('/users', adminController.getUsers)
 router.patch('/users/:id', adminController.patchUser)
+
 router.get('/categories', categoryController.getCategories)
+router.post('/categories', categoryController.postCategory)
+
 router.get('/', (req, res) => res.redirect('/admin/restaurants'))
 module.exports = router
