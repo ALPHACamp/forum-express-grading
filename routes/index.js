21481081f83476d5ecbb0566ada3e60d644edd75
cheeeeeryou <cheryl9463@gmail.com>
2022-11-28 00:19:02 +0800

    finished AC:R01

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const router = express.Router()
-//新增，載入 controller
+const passport = require('../config/passport')
+const admin = require('./modules/admin')
 const restController = require('../controllers/restaurant-controller')
-const admin = require('./modules/admin') //新增這行，載入 admin.js
+const userController = require('../controllers/user-controller')
+const { authenticated, authenticatedAdmin } = require('../middleware/auth')
+const { generalErrorHandler } = require('../middleware/error-handler')
 
-router.use('/admin', admin) //新增這行
-router.get('/restaurants', restController.getRestaurants)
-router.use('/', (req, res) => res.redirect('/restaurants'))
-
+router.use('/admin', authenticatedAdmin, admin)
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp) //注意用 post
+router.get('/signin', userController.signInPage)
+router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn) // 注意是 post
+router.get('/logout', userController.logout)
+router.get('/restaurants', authenticated, restController.getRestaurants)
+router.get('/', (req, res) => res.redirect('/restaurants'))
+router.use('/', generalErrorHandler)
 module.exports = router
