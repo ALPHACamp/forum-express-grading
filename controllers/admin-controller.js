5816e36476d085aea9d2e5563d22dcd4b2860f6c
programmerP22 <chiangcw19@gmail.com>
2022-11-27 20:05:27 +0800

    merge R01 to main

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,5 +1,8 @@
-const { Restaurant } = require('../models')
+const { Restaurant, User } = require('../models')
 const { localFileHandler } = require('../helpers/file-helpers')
+
+const superUserEmails = ['root@example.com']
+
 const adminController = {
   getRestaurants: (req, res, next) => {
     Restaurant.findAll({
@@ -83,8 +86,32 @@ const adminController = {
       })
       .then(() => res.redirect('/admin/restaurants'))
       .catch(err => next(err))
+  },
+  getUsers: (req, res, next) => {
+    return User.findAll({
+      raw: true
+    })
+      .then(users => res.render('admin/users', { users }))
+      .catch(err => next(err))
+  },
+  patchUser: (req, res, next) => {
+    const id = req.params.id
+    return User.findByPk(id)
+      .then(user => {
+        if (!user) throw new Error("User doesn't exists!")
+        if (superUserEmails.includes(user.email)) {
+          req.flash('error_messages', '禁止變更 root 權限')
+          // 這邊因為測試檔的 User.name 為 'admin', 用 ${user.name} 會顯示錯誤，故直接以 'root' 取代
+          return res.redirect('back')
+        }
+        return user.update({ isAdmin: !user.isAdmin })
+      })
+      .then(user => {
+        req.flash('success_messages', '使用者權限變更成功')
+        res.redirect('/admin/users')
+      })
+      .catch(err => next(err))
   }
-
 }
 
 module.exports = adminController
