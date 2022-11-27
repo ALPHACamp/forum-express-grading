03ba97bb2386bf857d23afe0dbcf2a597d42976d
wujia777 <cwu005104@gmail.com>
2022-11-27 11:55:50 +0800

    feat: add imgur api

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,5 +1,5 @@
 const { Restaurant } = require('../models')
-const { localFileHandler } = require('../helpers/file-helpers')
+const { imgurFileHandler } = require('../helpers/file-helpers')
 
 const adminController = {
   getRestaurants: (req, res, next) => {
@@ -14,7 +14,7 @@ const adminController = {
     const { name, tel, address, openingHours, description } = req.body
     if (!name) { throw new Error('Restaurant name is required!') } // 前後端不同步所以都要設定
     const { file } = req
-    localFileHandler(file)
+    imgurFileHandler(file)
       .then(filePath => Restaurant.create({
         name, tel, address, openingHours, description, image: filePath || null
       }))
@@ -46,7 +46,7 @@ const adminController = {
     const { file } = req
     Promise.all([
       Restaurant.findByPk(req.params.id),
-      localFileHandler(file)])
+      imgurFileHandler(file)])
       .then(([restaurant, filePath]) => {
         if (!restaurant) { throw new Error("Restaurant didn't exist!") }
         restaurant.update({
--- helpers/file-helpers.js
+++ helpers/file-helpers.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const imgur = require('imgur')
 const localFileHandler = file => {
   return new Promise((resolve, reject) => {
     if (!file) return resolve(null)
@@ -10,4 +11,15 @@ const localFileHandler = file => {
   })
 }
 
-module.exports = { localFileHandler }
+const imgurFileHandler = file => {
+  return new Promise((resolve, reject) => {
+    if (!file) return resolve(null)
+    return imgur.uploadFile(file.path)
+      .then(img => {
+        resolve(img?.link || null)
+      })
+      .catch(err => reject(err))
+  })
+}
+
+module.exports = { localFileHandler, imgurFileHandler }
