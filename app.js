d15a839a479cbf9ef30b9c5038a92ae7c9262a75
lomo007 <eragon66997@gmail.com>
2022-11-27 09:09:27 +0800

    feat: passport init & signin

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const handlenars = require('express-handlebars')
 const routes = require('./routes')
 const flash = require('connect-flash')
 const session = require('express-session')
+const passport = require('./config/passport')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -12,10 +13,12 @@ app.engine('hbs', handlenars({ extname: '.hbs' })) //*
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
-app.use(flash()) // 掛載套件
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
 app.use((req, res, next) => {
-  res.locals.success_messages = req.flash('success_messages')  // 設定 success_msg 訊息
-  res.locals.error_messages = req.flash('error_messages')  // 設定 warning_msg 訊息
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
   next()
 })
 app.use(routes)
