8dc32197afbe29b577a48d6a8b753f4df24eacbd
zephyr Lin <zephyrlin33@gmail.com>
2022-11-27 17:37:43 +0800

    add comment area to rest detail pages

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller') // 新增這行
+const commentController = require('../controllers/comment-controller')
 
 const { generalErrorHandler } = require('../middleware/error-handler')
 const { authenticated, authenticatedAdmin } = require('../middleware/auth')
@@ -19,6 +20,8 @@ router.get('/logout', userController.logout)
 router.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard)
 router.get('/restaurants/:id', authenticated, restController.getRestaurant)
 router.get('/restaurants', authenticated, restController.getRestaurants)
+
+router.post('/comments', authenticated, commentController.postComment) 
 
 router.get('/', (req, res) => res.redirect('/restaurants'))
 router.use('/', generalErrorHandler) // 加入這行
