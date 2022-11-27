5bbc9d6fcfd73a6f1dad739e66958e0afae6ee26
cosine0720 <cosine0720@gmail.com>
2022-11-28 00:20:00 +0800

    feat: finish R02

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -21,13 +21,25 @@ const restaurantController = {
   // 瀏覽：個別餐廳
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
-      raw: true,
-      nest: true,
       include: Category
     })
       .then(restaurant => {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
-        res.render('restaurant', { restaurant })
+        return restaurant.increment('viewCount')
+      })
+      .then(restaurant => {
+        return res.render('restaurant', { restaurant: restaurant.toJSON() })
+      })
+      .catch(err => next(err))
+  },
+  // Dashboard：
+  getDashboard: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, {
+      include: Category
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        res.render('dashboard', { restaurant: restaurant.toJSON() })
       })
       .catch(err => next(err))
   }
