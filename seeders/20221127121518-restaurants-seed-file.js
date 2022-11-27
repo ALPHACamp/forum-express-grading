8387ac3fe1bdec215967665f6913e0b71ba5f3c4
JoyWanddrr <m81y102@msn.com>
2022-11-27 20:17:39 +0800

    add catergory seed and connect with restaurant model

--- seeders/20221127121518-restaurants-seed-file.js
+++ seeders/20221127121518-restaurants-seed-file.js
@@ -3,6 +3,8 @@ const faker = require('faker')
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    // 從queryInterface用SQL語法請求categories裡的id資料
+    const categories = await queryInterface.sequelize.query('SELECT id FROM Categories', { type: queryInterface.sequelize.QueryTypes.SELECT })// 請求的資料類型
     await queryInterface.bulkInsert('Restaurants',
       Array.from({ length: 50 }, () => ({
         name: faker.name.findName(),
@@ -12,7 +14,9 @@ module.exports = {
         image: `https://loremflickr.com/320/240/restaurant,food/?random=${Math.random() * 100}`,
         description: faker.lorem.text(),
         created_at: new Date(),
-        updated_at: new Date()
+        updated_at: new Date(),
+        category_id: categories[Math.floor(Math.random() * categories.length)].id
+        // 根據categories長度決定取出的亂數號碼=categories元素位置
       }))
       , {})
   },
