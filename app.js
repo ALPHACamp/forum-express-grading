afe2f7243c9b0a6d96070cff78ff37daac840ca2
Aither <cheese736@hotmail.com>
2022-11-29 02:24:54 +0800

    project init

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const routes = require('./routes')
+const handlebars = require('express-handlebars')
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(routes)
+app.engine('hbs', handlebars({extname: '.hbs'}))
+app.set('view engine', 'hbs')
 
 app.listen(port, () => {
   console.info(`Example app listening on port ${port}!`)
