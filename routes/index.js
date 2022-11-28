08aa4252477661ed998dd24893ea960569f0d833
seanlin1125 <sean821125@gmail.com>
2022-11-28 15:35:10 +0800

    feat: add post comment on restaurant page

--- routes/index.js
+++ routes/index.js
@@ -4,21 +4,27 @@ const passport = require('../config/passport')
 const admin = require('./modules/admin')
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
+const commentController = require('../controllers/​​comment-controller')
 const { authenticated, authenticatedAdmin } = require('../middleware/auth')
 const { generalErrorHandler } = require('../middleware/error-handler')
 
+// admin
 router.use('/admin', authenticatedAdmin, admin)
+// user
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
 router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn) // 注意是 post
 router.get('/logout', userController.logout)
-
+// restaurant
 router.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard)
 router.get('/restaurants/:id', authenticated, restController.getRestaurant)
 router.get('/restaurants', authenticated, restController.getRestaurants)
-router.use('/', (req, res) => res.redirect('/restaurants'))
+// comment
+router.post('/comments', authenticated, commentController.postComment)
 
+router.use('/', (req, res) => res.redirect('/restaurants'))
+// error
 router.use('/', generalErrorHandler)
 
 module.exports = router
