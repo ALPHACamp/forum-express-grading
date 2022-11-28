a6d08c9e5fd20a2dbe44cb1024bc133ed4bc1d0c
miaout11 <miaout.11@gmail.com>
2022-11-28 15:54:50 +0800

    feat: add admin categories page

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 // 載入 controller
 const adminController = require('../../controllers/admin-controller')
+const categoryController = require('../../controllers/category-controller')
 const upload = require('../../middleware/multer') // 載入 multer
 
 router.get('/restaurants/create', adminController.createRestaurant) // 新增餐廳表單頁面
@@ -13,6 +14,7 @@ router.get('/restaurants', adminController.getRestaurants) // 瀏覽全部餐廳
 router.post('/restaurants', upload.single('image'), adminController.postRestaurant) // post新增餐廳表單
 router.patch('/users/:id', adminController.patchUser) // 修改User的權限
 router.get('/users', adminController.getUsers) // 瀏覽全部User
+router.get('/categories', categoryController.getCategories) // 瀏覽categories頁面
 
 // 設定 fallback 路由(其他路由條件都不符合時，最終會通過的路由)
 router.get('/', (req, res) => res.redirect('/admin/restaurants'))
