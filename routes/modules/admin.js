7097ba1676157d781fb6340f04b9786465fcac27
miaout11 <miaout.11@gmail.com>
2022-11-28 16:08:39 +0800

    feat: category create

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -6,15 +6,16 @@ const categoryController = require('../../controllers/category-controller')
 const upload = require('../../middleware/multer') // 載入 multer
 
 router.get('/restaurants/create', adminController.createRestaurant) // 新增餐廳表單頁面
-router.get('/restaurants/:id/edit', adminController.editRestaurant) // 編輯1間餐廳的資訊
-router.get('/restaurants/:id', adminController.getRestaurant) // 瀏覽1間餐廳的資訊
-router.put('/restaurants/:id', upload.single('image'), adminController.putRestaurant) // put編輯1間餐廳的資訊
-router.delete('/restaurants/:id', adminController.deleteRestaurant) // 刪除1間餐廳
-router.get('/restaurants', adminController.getRestaurants) // 瀏覽全部餐廳的資訊
-router.post('/restaurants', upload.single('image'), adminController.postRestaurant) // post新增餐廳表單
+router.get('/restaurants/:id/edit', adminController.editRestaurant) // 編輯餐廳表單頁面
+router.get('/restaurants/:id', adminController.getRestaurant) // 瀏覽1間餐廳
+router.put('/restaurants/:id', upload.single('image'), adminController.putRestaurant) // 編輯餐廳
+router.delete('/restaurants/:id', adminController.deleteRestaurant) // 刪除餐廳
+router.get('/restaurants', adminController.getRestaurants) // 瀏覽全部餐廳
+router.post('/restaurants', upload.single('image'), adminController.postRestaurant) // 新增餐廳
 router.patch('/users/:id', adminController.patchUser) // 修改User的權限
 router.get('/users', adminController.getUsers) // 瀏覽全部User
 router.get('/categories', categoryController.getCategories) // 瀏覽categories頁面
+router.post('/categories', categoryController.postCategory) // 新增category
 
 // 設定 fallback 路由(其他路由條件都不符合時，最終會通過的路由)
 router.get('/', (req, res) => res.redirect('/admin/restaurants'))
