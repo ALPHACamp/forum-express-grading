395acfa29998936c177d727fcddbeccc68d705f6
Coli-co <awestlifeblue@gmail.com>
2022-11-28 10:09:39 +0800

    feat: add restaurant page

--- routes/index.js
+++ routes/index.js
@@ -21,6 +21,7 @@ router.post(
   userController.signIn
 )
 router.get('/logout', userController.logout)
+router.get('/restaurants/:id', authenticated, restController.getRestaurant)
 router.get('/restaurants', authenticated, restController.getRestaurants)
 router.get('/', (req, res) => res.redirect('/restaurants'))
 router.use('/', generalErrorHandler)
