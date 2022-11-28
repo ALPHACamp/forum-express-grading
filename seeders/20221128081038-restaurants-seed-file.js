2a358cb611fd52ff07b48647cbcc4fb7121a2acc
Victor Cheng <weitungstyle@gmail.com>
2022-11-28 16:14:41 +0800

    feat: update seed files

--- seeders/20221128081038-restaurants-seed-file.js
+++ seeders/20221128081038-restaurants-seed-file.js
@@ -2,6 +2,11 @@
 const faker = require('faker')
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    // 取出categories物件
+    const categories = await queryInterface.sequelize.query(
+      'SELECT id FROM Categories;',
+      { type: queryInterface.sequelize.QueryTypes.SELECT }
+    )
     await queryInterface.bulkInsert('Restaurants',
       Array.from({ length: 50 }, () => ({
         name: faker.name.findName(),
@@ -11,7 +16,9 @@ module.exports = {
         image: `https://loremflickr.com/320/240/restaurant,food/?random=${Math.random() * 100}`,
         description: faker.lorem.text(),
         created_at: new Date(),
-        updated_at: new Date()
+        updated_at: new Date(),
+        // 用Math.random生成對應categories物件長度的隨機數=>放入對應的id（因應不同SQL生成id可能是跳號）
+        category_id: categories[Math.floor(Math.random() * categories.length)].id
       }))
     )
   },
