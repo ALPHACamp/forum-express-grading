93fcd3f80d47ce2ab0ccc22b8bd24c8e98bab424
miaout11 <miaout.11@gmail.com>
2022-11-28 18:22:17 +0800

    feat: add restaurants index page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,6 +1,21 @@
+const { Restaurant, Category } = require('../models')
+
 const restaurantController = {
   getRestaurants: (req, res) => {
-    return res.render('restaurants')
+    return Restaurant.findAll({
+      raw: true,
+      nest: true,
+      include: Category
+    }).then(restaurants => {
+      // map 整理出來的新陣列，需要多設一個變數 data 來接住回傳值，最後我們要使用的資料會放在 data
+      const data = restaurants.map(r => ({
+        ...r,
+        description: r.description.substring(0, 50) // 把敘述截斷為50個字，截取字串可用 substring()
+      }))
+      return res.render('restaurants', {
+        restaurants: data // 要使用的資料會放在 data
+      })
+    })
   }
 }
 module.exports = restaurantController
