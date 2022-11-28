5e625d843e0927a9a749b4be5fd5790ca3a450b9
Elio <eliochen34@gmail.com>
2022-11-28 16:47:40 +0800

    feat: add categories selector on admin create and edit page

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -12,11 +12,15 @@ const adminController = { // 修改這裡
       .then(restaurants => res.render('admin/restaurants', { restaurants }))
       .catch(err => next(err))
   },
-  createRestaurant: (req, res) => {
-    res.render('admin/create-restaurant')
+  createRestaurant: (req, res, next) => {
+    Category.findAll({
+      raw: true
+    })
+      .then(categories => res.render('admin/create-restaurant', { categories }))
+      .catch(err => next(err))
   },
   postRestaurant: (req, res, next) => {
-    const { name, tel, address, openingHours, description } = req.body // 從 req.body 拿出表單裡的資料
+    const { name, tel, address, openingHours, description, categoryId } = req.body // 從 req.body 拿出表單裡的資料
     if (!name) throw new Error('Restaurant name is required!') // name 是必填，若發先是空值就會終止程式碼，並在畫面顯示錯誤提示
 
     const { file } = req // 取出檔案給file-helper處理後再create餐廳資料
@@ -27,7 +31,8 @@ const adminController = { // 修改這裡
         address,
         openingHours,
         description,
-        image: filePath || null // filePath ? filePath : null三元運算子的寫法
+        image: filePath || null, // filePath ? filePath : null三元運算子的寫法
+        categoryId
       }))
       .then(() => {
         req.flash('success_messages', 'restaurant was successfully created') // 在畫面顯示成功提示
@@ -48,17 +53,18 @@ const adminController = { // 修改這裡
       .catch(err => next(err))
   },
   editRestaurant: (req, res, next) => {
-    Restaurant.findByPk(req.params.id, {
-      raw: true
-    })
-      .then(restaurant => {
+    Promise.all([
+      Restaurant.findByPk(req.params.id, { raw: true }),
+      Category.findAll({ raw: true })
+    ])
+      .then(([restaurant, categories]) => {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
-        res.render('admin/edit-restaurant', { restaurant })
+        res.render('admin/edit-restaurant', { restaurant, categories })
       })
       .catch(err => next(err))
   },
   putRestaurant: (req, res, next) => {
-    const { name, tel, address, openingHours, description } = req.body
+    const { name, tel, address, openingHours, description, categoryId } = req.body
     if (!name) throw new Error('Restaurant name is required!')
 
     const { file } = req
@@ -76,7 +82,8 @@ const adminController = { // 修改這裡
           address,
           openingHours,
           description,
-          image: filePath || restaurant.image // 如果使用者有更改圖片，則使用filePath，如果沒有，就使用原本的image
+          image: filePath || restaurant.image, // 如果使用者有更改圖片，則使用filePath，如果沒有，就使用原本的image
+          categoryId
         })
       })
       .then(() => {
