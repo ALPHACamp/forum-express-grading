fedee40b7735330a14a17322fad3185e1ea36cb6
wujia777 <cwu005104@gmail.com>
2022-11-28 21:44:33 +0800

    feat: add category model

--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,7 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
 const bcrypt = require('bcryptjs')
-const { User, Restaurant } = require('../models')
+const { User } = require('../models')
 
 // setup passport strategy
 passport.use(new LocalStrategy(
---
+++ migrations/20221128133053-add-categoryId-to-restaurant.js
@@ -0,0 +1,19 @@
+'use strict'
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.addColumn('Restaurants', 'category_id', {
+      type: Sequelize.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'Categories',
+        key: 'id'
+      }
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.removeColumn('Restaurants', 'category_id')
+  }
+}
