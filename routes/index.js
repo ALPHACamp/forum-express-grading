071f0c83499dd38f04433f17fb804f55de6fe5f1
Aither <cheese736@hotmail.com>
2022-11-30 03:08:55 +0800

    add express handlebars and routes

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const router = express.Router()
+const restaurantController = require('../controllers/restaurant-controller')
+const admin = require('./modules/admin')
 
-router.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+router.use('/admin', admin)
+router.get('/restaurants', restaurantController.getRestaurants)
+
+router.get('/', (req, res) => res.redirect('/restaurants'))
 
 module.exports = router
