e5f0e501ba0020f2427aa2861845b103578f1760
ping8601 <wanping86701@gmail.com>
2022-11-29 15:10:39 +0800

    feat: merge R02 with main

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -19,12 +19,25 @@ const restaurantController = {
     const { id } = req.params
     return Restaurant.findByPk(id, {
       include: Category,
-      raw: true,
       nest: true
     })
       .then(restaurant => {
         if (!restaurant) throw new Error("Restaurant doesn't exist!")
-        return res.render('restaurant', { restaurant })
+        return restaurant.increment('viewCounts', { by: 1 })
+      })
+      .then(restaurant => res.render('restaurant', { restaurant: restaurant.toJSON() }))
+      .catch(err => next(err))
+  },
+  getDashboard: (req, res, next) => {
+    const { id } = req.params
+    return Restaurant.findByPk(id, {
+      raw: true,
+      include: Category,
+      nest: true
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant doesn't exist!")
+        return res.render('dashboard', { restaurant: restaurant })
       })
       .catch(err => next(err))
   }
