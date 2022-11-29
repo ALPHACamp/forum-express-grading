03ec78c54fd75d361b69918c93ce2c48dea66f8f
Tzuminn <valuesu0326@gmail.com>
2022-11-29 22:54:58 +0800

    feat: add ifCond hbs helper & update category selector

--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -1,5 +1,9 @@
 const dayjs = require('dayjs')
 
 module.exports = {
-  currentYear: () => dayjs().year()
+  currentYear: () => dayjs().year(),
+  ifCond: function (a, b, options) {
+    // 若 a 和 b 相等，會回傳 options.fn(this)，不相等則回傳 options.inverse(this)
+    return a === b ? options.fn(this) : options.inverse(this)
+  }
 }
