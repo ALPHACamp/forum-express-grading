ea6ea6aa0e20c258ee3ca63a2c4a7989211aa78c
LAPTOP-TTILM0SK\Catherine <phes10217@gmail.com>
2022-11-29 14:31:30 +0800

    feat: category update

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -15,7 +15,8 @@ router.post('/restaurants', upload.single('image'), adminController.postRestaura
 router.patch('/users/:id', adminController.patchUser)
 router.get('/users', adminController.getUsers)
 
-
+router.get('/categories/:id', categoryController.getCategories) // 新增這行
+router.put('/categories/:id', categoryController.putCategory) // 新增這行
 router.get('/categories', categoryController.getCategories)
 router.post('/categories', categoryController.postCategory)
 router.get('', (req, res) => res.redirect('/admin/restaurants'))
