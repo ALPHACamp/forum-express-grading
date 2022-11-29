625737f1ea43c69e06c6a41fd26e3a9cf7dce4f9
OneZerocococo <ylflyfly@gmail.com>
2022-11-29 17:44:06 +0800

    Merge R03 to main

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,6 +1,8 @@
 const bcrypt = require('bcryptjs')
 const db = require('../models')
-const { User } = db
+const { imgurFileHandler } = require('../helpers/file-helpers')
+const { getUser } = require('../helpers/auth-helpers')
+const { User, Restaurant, Comment } = db
 const userController = {
   signUpPage: (req, res) => {
     res.render('signup')
@@ -34,6 +36,56 @@ const userController = {
     req.flash('success_messages', '登出成功！')
     req.logout()
     res.redirect('/signin')
+  },
+  getUser: (req, res, next) => {
+    return Promise.all([
+      User.findByPk(req.params.id, { raw: true }),
+      Comment.findAll({
+        where: { userId: req.params.id },
+        include: Restaurant,
+        raw: true,
+        nest: true
+      })
+    ])
+      .then(([userProfile, comments]) => {
+        if (!userProfile) throw new Error("User doesn't exist.")
+        res.render('users/profile', {
+          user: getUser(req),
+          userProfile,
+          comments
+        })
+      })
+      .catch(err => next(err))
+  },
+  editUser: (req, res, next) => {
+    return User.findByPk(req.params.id, { raw: true })
+      .then(user => {
+        if (!user) throw new Error("User doesn't exist!")
+        res.render('users/edit', { user })
+      })
+      .catch(err => next(err))
+  },
+  putUser: (req, res, next) => {
+    const { name } = req.body
+    const { file } = req
+    const { id } = req.params
+    if (!name) throw new Error('User name is required!')
+    return Promise.all([
+      User.findByPk(id),
+      imgurFileHandler(file)
+    ])
+      .then(([user, filePath]) => {
+        if (!user) throw new Error("User didn't exist!")
+        return user.update({
+          name,
+          image: filePath || user.image
+        })
+      })
+      .then(() => {
+        req.flash('success_messages', '使用者資料編輯成功')
+        res.redirect(`/users/${id}`)
+      })
+      .catch(err => next(err))
   }
 }
 module.exports = userController
