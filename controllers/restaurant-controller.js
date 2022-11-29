8d73845a9931c202c1bb8831328e7a5893be0147
LAPTOP-TTILM0SK\Catherine <phes10217@gmail.com>
2022-11-30 07:31:07 +0800

    feat: add categories navbar on restaurants index page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,19 +1,31 @@
 const { Restaurant, Category } = require('../models')
 
 const restaurantController = {
-  getRestaurants: (req, res) => {
-    return Restaurant.findAll({
-      include: Category,
-      raw: true,
-      nest: true
-    })
-      .then(restaurants => {
+  getRestaurants: (req, res,next) => {
+    const categoryId = parseInt(req.query.categoryId) || '' // 新增這裡，從網址上拿下來的參數是字串，先轉成 Number 再操作
+    return Promise.all([
+      Restaurant.findAll({
+        include: Category,
+        where: {  // 新增查詢條件
+          ...categoryId ? { categoryId } : {} // 檢查 categoryId 是否為空值
+        },
+        nest: true,
+        raw: true
+      }),
+      Category.findAll({ raw: true })
+    ])
+      .then(([restaurants, categories]) => {
         const data = restaurants.map(r => ({
           ...r,
           description: r.description.substring(0, 50)
         }))
-        return res.render('restaurants', { restaurants: data })
+        return res.render('restaurants', {
+          restaurants: data,
+          categories,
+          categoryId // 新增這裡
+        })
       })
+      .catch(err => next(err)) // 補上
   },
   getRestaurant: (req, res, next) => {
     const { id } = req.params
@@ -23,7 +35,7 @@ const restaurantController = {
     })
       .then(restaurant => {
         if (!restaurant) throw new Error("Restaurant doesn't exist!")
-        return restaurant.increment('viewCounts', { by: 1 })
+        return restaurant.increment('viewCounts', { by: 1 })       
       })
       .then(restaurant => res.render('restaurant', { restaurant: restaurant.toJSON() }))
       .catch(err => next(err))
