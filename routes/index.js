49574e9f8f550145103e095e346c2ab5938284e9
LAPTOP-TTILM0SK\Catherine <phes10217@gmail.com>
2022-11-30 10:57:30 +0800

    feat: add post comment on restaurant page

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,15 @@ const express = require('express')
 const router = express.Router()
 const passport = require('../config/passport') // 引入 Passport，需要他幫忙做驗證
 // 新增，載入 controller
+const userController = require('../controllers/user-controller') // 新增這行
 const restController = require('../controllers/restaurant-controller')
 // 新增
+const commentController = require('../controllers/comment-controller') // 引入 controller
+//....
+
+
 const { authenticated, authenticatedAdmin } = require('../middleware/auth') // 修改這一行
-const userController = require('../controllers/user-controller') // 新增這行
+
  const admin = require('./modules/admin') 
 const { generalErrorHandler } = require('../middleware/error-handler') // 加入這行
 
@@ -28,7 +33,10 @@ router.get(
 router.get('/restaurants/:id', authenticated, restController.getRestaurant) // 新增這行
 router.get('/restaurants', authenticated, restController.getRestaurants)
 
-router.use('/', (req, res) => res.redirect('/restaurants'))
+router.get('/', (req, res) => res.redirect('/restaurants'))
+
+router.post('/comments', authenticated, commentController.postComment) // 加入路由設定
+
 router.use('/', generalErrorHandler) // 加入這行
 module.exports = router
 
