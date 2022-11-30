4319de57ac3600b58560b6660bb6374f0485a878
programmerP22 <chiangcw19@gmail.com>
2022-11-30 19:17:12 +0800

    feat: add dashboard page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -28,6 +28,19 @@ const restaurantController = {
         })
       })
       .catch(err => next(err))
+  },
+  getDashboard: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, {
+      raw: true,
+      nest: true,
+      include: Category
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        res.render('dashboard', { restaurant })
+      })
+      .catch(err => next(err))
   }
+
 }
 module.exports = restaurantController
