19828651e6dea8209cef8eb31462f50a95c8768f
linway035 <wayneck0138@gmail.com>
2022-11-30 19:29:47 +0800

    feat: add pagination on restaurants index page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,63 +1,72 @@
 // restaurantController 是一個物件 (object)。
 // restaurantController 有不同的方法，例如 getRestaurants
-const { Restaurant, Category } = require('../models')
+const { Restaurant, Category } = require("../models");
+const { getOffset, getPagination } = require("../helpers/pagination-helper");
 const restaurantController = {
   getRestaurants: (req, res) => {
-    const categoryId = Number(req.query.categoryId) || '' // query網址列是字串所以要轉數字
+    const shortDescriptionLength = 50;
+    const DEFAULT_LIMIT = 9;
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || DEFAULT_LIMIT; //若以後想在query設計成可供使用者選擇每頁幾個
+    const offset = getOffset(limit, page);
+    const categoryId = Number(req.query.categoryId) || ""; // query網址列是字串所以要轉數字
     return Promise.all([
-      Restaurant.findAll({
+      Restaurant.findAndCountAll({
         include: Category,
         where: { ...(categoryId ? { categoryId } : {}) }, // 優先級為:先判斷是否，再展開...
+        limit,
+        offset,
         nest: true,
-        raw: true
+        raw: true,
       }),
-      Category.findAll({ raw: true })
+      Category.findAll({ raw: true }),
     ])
       .then(([restaurants, categories]) => {
-        const data = restaurants.map(r => ({
+        const data = restaurants.rows.map((r) => ({
           ...r,
-          description: r.description.substring(0, 50)
-        }))
-        return res.render('restaurants', {
+          description: r.description.substring(0, shortDescriptionLength),
+        }));
+        return res.render("restaurants", {
           restaurants: data,
           categories,
-          categoryId // 才可以判斷active
-        })
+          categoryId, // 才可以判斷active
+          pagination: getPagination(limit, page, restaurants.count),
+        });
       })
-      .catch(err => next(err))
+      .catch((err) => next(err));
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
-      include: Category // 拿出關聯的 Category model
+      include: Category, // 拿出關聯的 Category model
     })
-      .then(restaurant => {
-        if (!restaurant) throw new Error("Restaurant doesn't exist!")
-        return restaurant.increment('viewCounts', { by: 1 }) // 小寫restaurant
+      .then((restaurant) => {
+        if (!restaurant) throw new Error("Restaurant doesn't exist!");
+        return restaurant.increment("viewCounts", { by: 1 }); // 小寫restaurant
       })
-      .then(restaurant => {
+      .then((restaurant) => {
         // console.log(restaurant,restaurant.toJSON());
-        res.render('restaurant', {
-          restaurant: restaurant.toJSON() // toJSON大小寫要全對
-        })
+        res.render("restaurant", {
+          restaurant: restaurant.toJSON(), // toJSON大小寫要全對
+        });
       })
-      .catch(err => next(err))
+      .catch((err) => next(err));
   },
   getDashboard: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
       include: Category, // 拿出關聯的 Category model
       nest: true,
-      raw: true
+      raw: true,
     })
-      .then(restaurant => {
+      .then((restaurant) => {
         // console.log(restaurant);
-        if (!restaurant) throw new Error("Restaurant doesn't exist!")
-        res.render('dashboard', {
-          restaurant
-        })
+        if (!restaurant) throw new Error("Restaurant doesn't exist!");
+        res.render("dashboard", {
+          restaurant,
+        });
       })
-      .catch(err => next(err))
-  }
-}
+      .catch((err) => next(err));
+  },
+};
 
 // 匯出之後才能在其他檔案裡使用。
-module.exports = restaurantController
+module.exports = restaurantController;
