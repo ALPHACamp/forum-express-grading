4a807a53e0060588bbfe7a06195d8f0dece02985
Aither <cheese736@hotmail.com>
2022-12-01 04:29:19 +0800

    auth.js

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const restaurantController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
 const admin = require('./modules/admin')
 const { generalErrorHandler } = require('../middleware/error-handler') 
+const { authenticated } = require('../middleware/auth') 
 
 router.use('/admin', admin)
 router.get('/signup', userController.signUpPage)
@@ -12,7 +13,7 @@ router.post('/signup', userController.signUp)
 router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn) // 注意是 post
 router.get('/logout', userController.logout)
-router.get('/restaurants', restaurantController.getRestaurants)
+router.get('/restaurants',authenticated, restaurantController.getRestaurants)
 router.get('/', (req, res) => res.redirect('/restaurants'))
 router.use('/', generalErrorHandler) 
 
