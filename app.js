1cb2d70ad84be5b60a0442431f4e17cdd01b6586
Aither <cheese736@hotmail.com>
2022-12-01 03:49:16 +0800

    feat: add header & footer

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const flash = require('connect-flash')
 const session = require('express-session')
 const handlebars = require('express-handlebars')
 const passport = require('./config/passport')
+const { getUser } = require('./helpers/auth-helpers')
+const handlebarsHelpers = require('./helpers/handlebars-helpers')
 const app = express()
 const port = process.env.PORT || 3000
 const SESSION_SECRET = 'secret'
@@ -14,11 +16,12 @@ app.use(flash()) // 掛載套件
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')  // 設定 success_msg 訊息
   res.locals.error_messages = req.flash('error_messages')  // 設定 warning_msg 訊息
+  res.locals.user = getUser(req)
   next()
 })
 app.use(passport.initialize())
 app.use(passport.session())
-app.engine('hbs', handlebars({ extname: '.hbs' }))
+app.engine('hbs', handlebars({ extname: '.hbs', helpers: handlebarsHelpers }))
 app.set('view engine', 'hbs')
 app.use(routes)
 
