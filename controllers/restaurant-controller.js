4115d5fb14dddefa7a016b1cf0a0d53278825417
JoyWanddrr <m81y102@msn.com>
2022-11-30 20:09:45 +0800

    Add view page category feature

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -3,15 +3,22 @@
 const { Restaurant, Category } = require('../models')
 
 const restaurantController = {
-  getRestaurants: (req, res) => {
-    return Restaurant.findAll({
+  getRestaurants: (req, res, next) => {
+    // 將categoryId轉成數字或者為空(為全部做準備)
+    const categoryId = Number(req.query.categoryId) || ''
+    return Promise.all([Restaurant.findAll({
       include: Category,
+      // 當使用者點選的是「全部」這個頁籤時，categoryId 會是空值。設定 where 查詢條件時，撈出全部where: {}
+      // categoryId=true=...{categoryId}
+      where: { ...categoryId ? { categoryId } : {} },
       nest: true,
       raw: true
-    }).then(restaurants => {
-      const data = restaurants.map(r => ({ ...r, description: r.description.substring(0, 50) }))
-      return res.render('restaurants', { restaurants: data })
-    })
+    }), Category.findAll({ raw: true })])
+      .then(([restaurants, categories]) => {
+        const data = restaurants.map(r => ({ ...r, description: r.description.substring(0, 50) }))
+        return res.render('restaurants', { restaurants: data, categories, categoryId })// 回傳給hbs是否需要active
+      })
+      .catch(err => next(err))
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, { include: Category, nest: true, raw: true })
