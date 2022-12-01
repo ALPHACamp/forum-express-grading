b844b423ba82e2f3bdf45d9aeaae442a47e4ec54
xxirenee <charice0709@gmail.com>
2022-12-01 15:59:33 +0800

    feat: category view & create

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
+const categoryController = require('../../controllers/category-controller')
 const upload = require('../../middleware/multer')
 router.patch('/users/:id', adminController.patchUser)
 router.get('/users', adminController.getUsers)
@@ -11,5 +12,7 @@ router.put('/restaurants/:id', upload.single('image'), adminController.putRestau
 router.delete('/restaurants/:id', adminController.deleteRestaurant)
 router.get('/restaurants', adminController.getRestaurants)
 router.post('/restaurants', upload.single('image'), adminController.postRestaurant)
+router.get('/categories', categoryController.getCategories)
+router.post('/categories', categoryController.postCategory)
 router.use('/', (req, res) => res.redirect('/admin/restaurants'))
 module.exports = router
