43bf2328e3c849a97977a1cb79f7df1e2d663945
LAPTOP-TTILM0SK\Catherine <phes10217@gmail.com>
2022-12-01 18:30:33 +0800

    feat:add top-users page

--- routes/index.js
+++ routes/index.js
@@ -25,7 +25,7 @@ router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn) // 注意是 post
 
 router.get('/logout', userController.logout)
-
+router.get('/users/top', authenticated, userController.getTopUsers) //新增這一行
 router.get('/users/:id/edit', authenticated, userController.editUser)
 router.get('/users/:id', authenticated, userController.getUser)
 router.put('/users/:id', authenticated, upload.single('image'), userController.putUser)
@@ -38,7 +38,7 @@ router.get(
 router.get('/restaurants/:id', authenticated, restController.getRestaurant) // 新增這行
 router.get('/restaurants', authenticated, restController.getRestaurants)
 
-router.get('/', (req, res) => res.redirect('/restaurants'))
+
 
 router.delete('/comments/:id', authenticatedAdmin, commentController.deleteComment) // 加入這行
 router.post('/comments', authenticated, commentController.postComment) // 加入路由設定
@@ -48,7 +48,7 @@ router.delete('/favorite/:restaurantId', authenticated, userController.removeFav
 router.post('/like/:restaurantId', authenticated, userController.addLike)
 router.delete('/like/:restaurantId', authenticated, userController.removeLike)
 
-
+router.get('/', (req, res) => res.redirect('/restaurants'))
 router.use('/', generalErrorHandler) // 加入這行
 module.exports = router
 
