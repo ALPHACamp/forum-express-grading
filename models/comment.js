73d15fa4b6cc8b7ce1419ab371816d83a03ae250
henrysmile2071 <henrychen.chc@gmail.com>
2022-12-02 04:45:40 +0800

    fix: typo for  tableName

--- models/comment.js
+++ models/comment.js
@@ -22,7 +22,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'Comment',
-    tablename: 'Comments',
+    tableName: 'Comments',
     underscored: true
   })
   return Comment
