369a3eec43a18ab685dbdb17507e680c6e3c1350
Aither <cheese736@hotmail.com>
2022-12-01 21:24:50 +0800

    creat seeds with faker

--- seeders/20221201131718-users-seed-file.js
+++ seeders/20221201131718-users-seed-file.js
@@ -1,24 +1,31 @@
-'use strict';
-
+'use strict'
+const bcrypt = require('bcryptjs')
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    await queryInterface.bulkInsert('Users', [{ // 一次新增三筆資料
+      email: 'root@example.com',
+      password: await bcrypt.hash('12345678', 10),
+      is_admin: true,
+      name: 'root',
+      created_at: new Date(),
+      updated_at: new Date()
+    }, {
+      email: 'user1@example.com',
+      password: await bcrypt.hash('12345678', 10),
+      is_admin: false,
+      name: 'user1',
+      created_at: new Date(),
+      updated_at: new Date()
+    }, {
+      email: 'user2@example.com',
+      password: await bcrypt.hash('12345678', 10),
+      is_admin: false,
+      name: 'user2',
+      created_at: new Date(),
+      updated_at: new Date()
+    }], {})
   },
-
-  down: async (queryInterface, Sequelize) => {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+  down: async (queryInterface, Sequelize) => { // 清空資料表中所有資料
+    await queryInterface.bulkDelete('Users', {})
   }
-};
+}
