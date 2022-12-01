bbc3a3ea4cc32518863cb4ca45c518e9f14e7a88
Coli-co <awestlifeblue@gmail.com>
2022-12-01 18:15:30 +0800

    feat: add following sort function

--- routes/index.js
+++ routes/index.js
@@ -68,8 +68,14 @@ router.put(
   userController.putUser
 )
 
-router.get('/', (req, res) => res.redirect('/restaurants'))
+router.post('/following/:userId', authenticated, userController.addFollowing)
+router.delete(
+  '/following/:userId',
+  authenticated,
+  userController.removeFollowing
+)
 
+router.get('/', (req, res) => res.redirect('/restaurants'))
 router.use('/', generalErrorHandler)
 
 module.exports = router
