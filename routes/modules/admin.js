382d41652df56906ca78dd438f2d9ee2c59d73dd
Aither <cheese736@hotmail.com>
2022-12-01 19:13:14 +0800

    feat: add show restaurant function

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -4,10 +4,10 @@ const adminController = require('../../controllers/admin-controller')
 
 // route
 router.get('/restaurants/create', adminController.createRestaurant)
+router.get('/restaurants/:id', adminController.getRestaurant)
 router.get('/restaurants', adminController.getRestaurants)
 router.post('/restaurants', adminController.postRestaurant)
 router.use('/', (req, res) => res.redirect('/admin/restaurants'))
-
 
 // export
 module.exports = router
