0c25b56e2f677220f4661f80d57a1a1026cd89b9
Victor Cheng <weitungstyle@gmail.com>
2022-12-01 21:46:45 +0800

    feat: add post comment on restaurant page

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const admin = require('./modules/admin')
 
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
+const commentController = require('../controllers/comment-controller')
 const { authenticated } = require('../middleware/auth')
 const { generalErrorHandler } = require('../middleware/error-handler')
 const { authenticatedAdmin } = require('../middleware/auth')
@@ -22,6 +23,8 @@ router.get('/restaurants/:id/dashboard', authenticated, restController.getDashbo
 router.get('/restaurants/:id', authenticated, restController.getRestaurant)
 router.get('/restaurants', authenticated, restController.getRestaurants)
 router.use('/', (req, res) => res.redirect('/restaurants'))
+
+router.post('/comments', authenticated, commentController.postComment)
 
 router.use('/', generalErrorHandler)
 
