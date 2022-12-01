25779245018577af37ec4d3aecb1ec45eb2c4c75
programmerP22 <chiangcw19@gmail.com>
2022-12-02 01:07:44 +0800

    feat: finish user profile & imgurFileHandler

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const passport = require('../config/passport')
 const admin = require('./modules/admin')
+const upload = require('../middleware/multer')
 
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
@@ -20,6 +21,7 @@ router.get('/logout', userController.logout)
 
 router.get('/users/:id/edit', authenticated, userController.editUser)
 router.get('/users/:id', authenticated, userController.getUser)
+router.put('/users/:id', authenticated, upload.single('image'), userController.putUser)
 
 router.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard)
 router.get('/restaurants/:id', authenticated, restController.getRestaurant)
