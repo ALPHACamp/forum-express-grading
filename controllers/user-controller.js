7079e9aa5a029f1952c64cd5eeabbca909ab9942
ping8601 <wanping86701@gmail.com>
2022-12-01 17:40:50 +0800

    feat: add user profile view & edit page

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,5 +1,7 @@
 const bcrypt = require('bcryptjs')
 const db = require('../models')
+const { imgurFileHandler } = require('../helpers/file-helpers')
+const { getUser } = require('../helpers/auth-helpers')
 const { User } = db
 const userController = {
   signUpPage: (req, res) => {
@@ -34,6 +36,49 @@ const userController = {
     req.flash('success_messages', '成功登出！')
     req.logout()
     res.redirect('/signin')
+  },
+  getUser: (req, res, next) => {
+    const { id } = req.params
+    const user = getUser(req)
+    return User.findByPk(id, { raw: true })
+      .then(viewedUser => {
+        if (!viewedUser) throw new Error("User doesn't exist!")
+        res.render('users/profile', { user, viewedUser })
+      })
+      .catch(err => next(err))
+  },
+  editUser: (req, res, next) => {
+    const { id } = req.params
+    return User.findByPk(id, { raw: true })
+      .then(user => {
+        if (!user) throw new Error("User doesn't exist!")
+        if (user.id !== getUser(req).id) throw new Error('Permission denied!')
+        res.render('users/edit', { user })
+      })
+      .catch(err => next(err))
+  },
+  putUser: (req, res, next) => {
+    const { id } = req.params
+    const { name } = req.body
+    if (!name) throw new Error('User name is required!')
+    const { file } = req
+    return Promise.all([
+      User.findByPk(id),
+      imgurFileHandler(file)
+    ])
+      .then(([user, filePath]) => {
+        if (!user) throw new Error("User doesn't exist!")
+        if (req.user.id !== user.id) throw new Error('Permission denied!')
+        return user.update({
+          name,
+          image: filePath || user.image
+        })
+      })
+      .then(user => {
+        req.flash('success_messages', '使用者資料編輯成功')
+        res.redirect(`/users/${id}`)
+      })
+      .catch(err => next(err))
   }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,7 @@ const commentController = require('../controllers/comment-controller')
 const { generalErrorHandler } = require('../middleware/error-handler')
 const { authenticated, authenticatedAdmin } = require('../middleware/auth')
 const passport = require('../config/passport')
+const upload = require('../middleware/multer')
 
 // admin
 router.use('/admin', authenticatedAdmin, admin)
@@ -18,8 +19,11 @@ router.post('/signup', userController.signUp)
 router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
 router.get('/logout', userController.logout)
+router.get('/users/:id/edit', authenticated, userController.editUser)
+router.put('/users/:id', authenticated, upload.single('image'), userController.putUser)
+router.get('/users/:id', authenticated, userController.getUser)
 
-// user
+// restaurant
 router.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard)
 router.get('/restaurants/:id', authenticated, restController.getRestaurant)
 router.get('/restaurants', authenticated, restController.getRestaurants)
