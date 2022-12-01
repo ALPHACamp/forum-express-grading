c389be4980ed24da71f52cce78cd5e9a4fc46df0
programmerP22 <chiangcw19@gmail.com>
2022-12-01 10:23:38 +0800

    feat: add viewcounts increment function

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -18,13 +18,13 @@ const restaurantController = {
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
       include: Category,
-      nest: true,
-      raw: true
+      nest: true
     })
       .then(restaurant => {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
+        restaurant.increment('viewCounts')
         res.render('restaurant', {
-          restaurant
+          restaurant: restaurant.toJSON()
         })
       })
       .catch(err => next(err))
