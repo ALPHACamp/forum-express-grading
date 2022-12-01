62bf6070a9e13cad8201945a10724e2ae5f394ad
Gincoolwant <soulbox790326@gmail.com>
2022-12-01 10:56:32 +0800

    feat: add dashboard viewCounts feature

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -16,14 +16,25 @@ const restaurantController = {
       })
   },
   getRestaurant: (req, res, next) => {
-    Restaurant.findByPk(req.params.id, {
+    return Restaurant.findByPk(req.params.id, {
+      include: [Category]
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        restaurant.increment('viewCounts', { by: 1 })
+        return res.render('admin/restaurant', { restaurant: restaurant.toJSON() })
+      })
+      .catch(err => next(err))
+  },
+  getDashboard: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, {
       raw: true,
       nest: true,
       include: [Category]
     })
       .then(restaurant => {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
-        return res.render('admin/restaurant', { restaurant })
+        return res.render('dashboard', { restaurant })
       })
       .catch(err => next(err))
   }
