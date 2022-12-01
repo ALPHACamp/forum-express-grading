59edda61ae66082942146b396403c6b8a28333cf
Aither <cheese736@hotmail.com>
2022-12-01 14:57:39 +0800

    feat: add admin/restaurant.hbs

--- app.js
+++ app.js
@@ -10,21 +10,23 @@ const app = express()
 const port = process.env.PORT || 3000
 const SESSION_SECRET = 'secret'
 
-
+app.engine('hbs', handlebars({ extname: '.hbs', helpers: handlebarsHelpers }))
+app.set('view engine', 'hbs')
 
 app.use(express.urlencoded({ extended: true }))
+
 app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
 app.use(passport.initialize())
 app.use(passport.session())
 app.use(flash()) // 掛載套件
+
 app.use((req, res, next) => {
-  res.locals.success_messages = req.flash('success_messages')  // 設定 success_msg 訊息
-  res.locals.error_messages = req.flash('error_messages')  // 設定 warning_msg 訊息
+  res.locals.success_messages = req.flash('success_messages') // 設定 success_msg 訊息
+  res.locals.error_messages = req.flash('error_messages') // 設定 warning_msg 訊息
   res.locals.user = getUser(req)
   next()
 })
-app.engine('hbs', handlebars({ extname: '.hbs', helpers: handlebarsHelpers }))
-app.set('view engine', 'hbs')
+
 app.use(routes)
 
 app.listen(port, () => {
--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ const passport = require('../config/passport')
 const restaurantController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
 const admin = require('./modules/admin')
-const { generalErrorHandler } = require('../middleware/error-handler') 
-const { authenticated } = require('../middleware/auth') 
+const { generalErrorHandler } = require('../middleware/error-handler')
+const { authenticated } = require('../middleware/auth')
 
 router.use('/admin', admin)
 router.get('/signup', userController.signUpPage)
@@ -13,8 +13,8 @@ router.post('/signup', userController.signUp)
 router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn) // 注意是 post
 router.get('/logout', userController.logout)
-router.get('/restaurants',authenticated, restaurantController.getRestaurants)
+router.get('/restaurants', authenticated, restaurantController.getRestaurants)
 router.get('/', (req, res) => res.redirect('/restaurants'))
-router.use('/', generalErrorHandler) 
+router.use('/', generalErrorHandler)
 
 module.exports = router
