267e8b66ab11a795559eeb532f6778aa52798c84
ZHENG-YOU-LU <yowyow222@gmail.com>
2022-12-01 11:29:59 +0800

    feat: category create

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -11,9 +11,13 @@ router.put('/restaurants/:id', upload.single('image'), adminController.putRestau
 router.delete('/restaurants/:id', adminController.deleteRestaurant)
 router.get('/restaurants', adminController.getRestaurants)
 router.post('/restaurants', upload.single('image'), adminController.postRestaurant)
+
 router.get('/users', adminController.getUsers)
 router.patch('/users/:id', adminController.patchUser)
+
 router.get('/categories', categoryController.getCategories)
+router.post('/categories', categoryController.postCategory)
+
 router.use('/', (req, res) => res.redirect('/admin/restaurants'))
 
 module.exports = router
