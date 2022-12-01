b9260da98615e9db311dfd07476c84f7f7a5767a
wujia777 <cwu005104@gmail.com>
2022-12-02 01:19:20 +0800

    feat: update R02-changeplace

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use((req, res, next) => {
 app.use(routes)
 
 app.listen(port, () => {
-  console.info(`Example app listening on port ${port}!`)
+  console.info(`Example app listening on port http://localhost:${port}`)
 })
 
 module.exports = app
