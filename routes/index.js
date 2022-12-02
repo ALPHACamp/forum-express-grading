a1e2ea33c6a8c20eb94f9d4b2f5cd38cd80bedec
Hua0720 <stella573021@gmail.com>
2022-12-03 02:31:39 +0800

    feat: add post comment on restaurant page

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ const passport = require('../config/passport') // 引入 Passport，需要他幫
 // 新增，載入 controller
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller') // 新增這行
+const commentController = require('../controllers/​​comment-controller') // 引入 controller
 const { authenticated, authenticatedAdmin } = require('../middleware/auth')// 引入 auth.js
 const { generalErrorHandler } = require('../middleware/error-handler')
 // 載入 admin.js
@@ -27,6 +28,8 @@ router.get('/logout', userController.logout)
 router.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard)
 router.get('/restaurants/:id', authenticated, restController.getRestaurant)
 router.get('/restaurants', authenticated, restController.getRestaurants) // 修改這行，新增 authenticated 參數
+
+router.post('/comments', authenticated, commentController.postComment) // 加入路由設定
 
 router.use('/', (req, res) => res.redirect('/restaurants'))
 
