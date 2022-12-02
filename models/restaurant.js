4ff3075bee66bec0f1c05a1389eda1a1124ae8c7
linway035 <wayneck0138@gmail.com>
2022-12-02 16:10:52 +0800

    feat: create favorite model

--- models/restaurant.js
+++ models/restaurant.js
@@ -11,6 +11,11 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
       Restaurant.belongsTo(models.Category, { foreignKey: 'categoryId' })
       Restaurant.hasMany(models.Comment, { foreignKey: 'restaurantId' })
+      Restaurant.belongsToMany(models.User, {
+        through: models.Favorite, // 透過 Favorite 表來建立關聯
+        foreignKey: 'restaurantId', // 對 Favorite 表設定 FK
+        as: 'FavoritedUsers' // 幫這個關聯取個名稱（例如動態設定有分好友和摯友，但都是多對多，所以用名稱幫忙區分）
+      })
     }
   }
   Restaurant.init(
