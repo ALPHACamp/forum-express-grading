528a230c1597d4339b4fcf5844af3576b1a03545
linway035 <wayneck0138@gmail.com>
2022-12-02 18:16:02 +0800

    feat: get user's favorited restaurants & switch button

--- config/passport.js
+++ config/passport.js
@@ -1,90 +1,142 @@
-const passport = require('passport')
-const LocalStrategy = require('passport-local')
-const bcrypt = require('bcryptjs')
-const db = require('../models')
-const User = db.User
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const bcrypt = require("bcryptjs");
+const { User, Restaurant } = require("../models");
+
 // set up Passport strategy
 passport.use(
   new LocalStrategy(
     // 第一個參數傳入客製化的選項: customize user field
     {
-      usernameField: 'email',
-      passwordField: 'password',
-      passReqToCallback: true // 因為flash需要req，true後才可以給下面用req
+      usernameField: "email",
+      passwordField: "password",
+      passReqToCallback: true, // 因為flash需要req，true後才可以給下面用req
     },
     // 第二個參數是一個 callback function: authenticate user
     // email也可以自取，例如改成username，where {email:username}。官方文件用done，教案TA習慣用cb。
     (req, email, password, cb) => {
-      User.findOne({ where: { email } }).then(user => {
+      User.findOne({ where: { email } }).then((user) => {
         if (!user) {
           return cb(
             null,
             false,
-            req.flash('error_messages', '帳號或密碼輸入錯誤！')
-          )
+            req.flash("error_messages", "帳號或密碼輸入錯誤！")
+          );
         }
-        bcrypt.compare(password, user.password).then(res => {
+        bcrypt.compare(password, user.password).then((res) => {
           if (!res) {
             return cb(
               null,
               false,
-              req.flash('error_messages', '帳號或密碼輸入錯誤！')
-            )
+              req.flash("error_messages", "帳號或密碼輸入錯誤！")
+            );
           }
-          return cb(null, user)
-        })
-      })
+          return cb(null, user);
+        });
+      });
     }
   )
-)
+);
 // serialize and deserialize user
 passport.serializeUser((user, cb) => {
-  cb(null, user.id)
-})
+  cb(null, user.id);
+});
 passport.deserializeUser((id, cb) => {
-  User.findByPk(id).then(user => {
-    // console.log(user);
-    // User {
-    //   dataValues: {
-    //     id: 1,
-    //     name: 'test',
-    //     email: 'test1@tt.tt',
-    //     password: '$2a$10$S.RBcI0QnouTaxrkvcKmtOPCA0x.pHnoQzrKS1THl.6NhIdZU7nba',
-    //     createdAt: 2022-11-22T03:48:44.000Z,
-    //     updatedAt: 2022-11-22T03:48:44.000Z
-    //   },
-    //   _previousDataValues: {
-    //     id: 1,
-    //     name: 'test',
-    //     email: 'test1@tt.tt',
-    //     password: '$2a$10$S.RBcI0QnouTaxrkvcKmtOPCA0x.pHnoQzrKS1THl.6NhIdZU7nba',
-    //     createdAt: 2022-11-22T03:48:44.000Z,
-    //     updatedAt: 2022-11-22T03:48:44.000Z
-    //   },
-    //   uniqno: 1,
-    //   _changed: Set(0) {},
-    //   _options: {
-    //     isNewRecord: false,
-    //     _schema: null,
-    //     _schemaDelimiter: '',
-    //     raw: true,
-    //     attributes: [ 'id', 'name', 'email', 'password', 'createdAt', 'updatedAt' ]
-    //   },
-    //   isNewRecord: false
-    // }
-    user = user.toJSON()
-    //  console.log("toJSON後", user);
-    //  toJSON後 {
-    //   id: 1,
-    //   name: 'test',
-    //   email: 'test1@tt.tt',
-    //   password: '$2a$10$S.RBcI0QnouTaxrkvcKmtOPCA0x.pHnoQzrKS1THl.6NhIdZU7nba',
-    //   createdAt: 2022-11-22T03:48:44.000Z,
-    //   updatedAt: 2022-11-22T03:48:44.000Z
-    // }
-    return cb(null, user)
+  return User.findByPk(id, {
+    include: [{ model: Restaurant, as: "FavoritedRestaurants" }], //as名來源自model中的設定
   })
-})
-module.exports = passport
+    .then((user) => {
+      cb(null, user.toJSON());
+      // 登入後產生console.log(user.toJSON());
+      // {
+      //   id: 1,
+      //   name: 'root',
+      //   email: 'root@example.com',
+      //   password: '$2***',
+      //   isAdmin: true,
+      //   image: 'https://i.imgur.com/d07pwY9.jpeg',
+      //   createdAt: 2022-12-01T08:22:54.000Z,
+      //   updatedAt: 2022-12-01T14:21:49.000Z,
+      //   FavoritedRestaurants: [
+      //     {
+      //       id: 2,
+      //       name: 'Ernest Parker',
+      //       tel: '1-750-252-2082 x185',
+      //       address: '2732 Boehm Track',
+      //       openingHours: '08:00',
+      //       description: 'et',
+      //       image: 'https://loremflickr.com/320/240/restaurant,food/?random=27.46213842538325',
+      //       viewCounts: 4,
+      //       createdAt: 2022-12-01T08:22:55.000Z,
+      //       updatedAt: 2022-12-01T15:23:17.000Z,
+      //       categoryId: 7,
+      //       Favorite: [Object]
+      //     },
+      //     {
+      //       id: 3,
+      //       name: 'Karla Collins',
+      //       tel: '503.760.7391 x84655',
+      //       address: '861 McLaughlin Roads',
+      //       openingHours: '08:00',
+      //       description: 'Culpa dolor accusamus.',
+      //       image: 'https://loremflickr.com/320/240/restaurant,food/?random=19.41431277567167',
+      //       viewCounts: 3,
+      //       createdAt: 2022-12-01T08:22:55.000Z,
+      //       updatedAt: 2022-12-01T14:35:04.000Z,
+      //       categoryId: 2,
+      //       Favorite: [Object]
+      //     }
+      //   ]
+      // }
+    })
+    .catch((err) => cb(err));
+});
+
+// 以下console範例
+// passport.deserializeUser((id, cb) => {
+//   User.findByPk(id).then(user => {
+//     // console.log(user);
+//     // User {
+//     //   dataValues: {
+//     //     id: 1,
+//     //     name: 'test',
+//     //     email: 'test1@tt.tt',
+//     //     password: '$2a$10$S.RBcI0QnouTaxrkvcKmtOPCA0x.pHnoQzrKS1THl.6NhIdZU7nba',
+//     //     createdAt: 2022-11-22T03:48:44.000Z,
+//     //     updatedAt: 2022-11-22T03:48:44.000Z
+//     //   },
+//     //   _previousDataValues: {
+//     //     id: 1,
+//     //     name: 'test',
+//     //     email: 'test1@tt.tt',
+//     //     password: '$2a$10$S.RBcI0QnouTaxrkvcKmtOPCA0x.pHnoQzrKS1THl.6NhIdZU7nba',
+//     //     createdAt: 2022-11-22T03:48:44.000Z,
+//     //     updatedAt: 2022-11-22T03:48:44.000Z
+//     //   },
+//     //   uniqno: 1,
+//     //   _changed: Set(0) {},
+//     //   _options: {
+//     //     isNewRecord: false,
+//     //     _schema: null,
+//     //     _schemaDelimiter: '',
+//     //     raw: true,
+//     //     attributes: [ 'id', 'name', 'email', 'password', 'createdAt', 'updatedAt' ]
+//     //   },
+//     //   isNewRecord: false
+//     // }
+//     user = user.toJSON()
+//     //  console.log("toJSON後", user);
+//     //  toJSON後 {
+//     //   id: 1,
+//     //   name: 'test',
+//     //   email: 'test1@tt.tt',
+//     //   password: '$2a$10$S.RBcI0QnouTaxrkvcKmtOPCA0x.pHnoQzrKS1THl.6NhIdZU7nba',
+//     //   createdAt: 2022-11-22T03:48:44.000Z,
+//     //   updatedAt: 2022-11-22T03:48:44.000Z
+//     // }
+//     return cb(null, user)
+//   })
+// })
+module.exports = passport;
 
 // Sequalize 打包後的物件，多包裝了幾層，並且裡面加上一些 Sequalize 內建的參數與方法，讓我們可以直接透過 Sequalize 操作這筆資料，例如刪除user.delete()或 更新user.date(...) 或簡化user.toJSON()
--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,15 +1,15 @@
 // restaurantController 是一個物件 (object)。
 // restaurantController 有不同的方法，例如 getRestaurants
-const { Restaurant, Category, User, Comment } = require('../models')
-const { getOffset, getPagination } = require('../helpers/pagination-helper')
+const { Restaurant, Category, User, Comment } = require("../models");
+const { getOffset, getPagination } = require("../helpers/pagination-helper");
 const restaurantController = {
   getRestaurants: (req, res) => {
-    const shortDescriptionLength = 50
-    const DEFAULT_LIMIT = 9
-    const page = Number(req.query.page) || 1
-    const limit = Number(req.query.limit) || DEFAULT_LIMIT // 若以後想在query設計成可供使用者選擇每頁幾個
-    const offset = getOffset(limit, page)
-    const categoryId = Number(req.query.categoryId) || '' // query網址列是字串所以要轉數字
+    const shortDescriptionLength = 50;
+    const DEFAULT_LIMIT = 9;
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || DEFAULT_LIMIT; // 若以後想在query設計成可供使用者選擇每頁幾個
+    const offset = getOffset(limit, page);
+    const categoryId = Number(req.query.categoryId) || ""; // query網址列是字串所以要轉數字
     return Promise.all([
       Restaurant.findAndCountAll({
         include: Category,
@@ -17,88 +17,120 @@ const restaurantController = {
         limit,
         offset,
         nest: true,
-        raw: true
+        raw: true,
       }),
-      Category.findAll({ raw: true })
+      Category.findAll({ raw: true }),
     ])
       .then(([restaurants, categories]) => {
-        const data = restaurants.rows.map(r => ({
+        const favoritedRestaurantsId =
+          req.user && req.user.FavoritedRestaurants.map((fr) => fr.id); //防req.user是空的。map會產生一個陣列。若放在data裡面，一頁有9家，這動作就要跑9次，但結果都相同，會沒效率，所以獨立出來。
+        const data = restaurants.rows.map((r) => ({
           ...r,
-          description: r.description.substring(0, shortDescriptionLength)
-        }))
-        return res.render('restaurants', {
+          description: r.description.substring(0, shortDescriptionLength),
+          isFavorited: favoritedRestaurantsId.includes(r.id),
+        }));
+        // console.log("favoritedRestaurantsId", favoritedRestaurantsId); // favoritedRestaurantsId [ 2, 3 ]
+        // console.log("data", data);
+        // data [
+        //   {
+        //     id: 1,
+        //     name: 'Lauren Strosin',
+        //     tel: '(854) 746-2410 x635',
+        //     address: '9067 Gracie Forest',
+        //     openingHours: '08:00',
+        //     description: 'Saepe qui qui dolore ullam qui. Molestiae qui offi',
+        //     image: 'https://loremflickr.com/320/240/restaurant,food/?random=98.19748241873347',
+        //     viewCounts: 7,
+        //     createdAt: 2022-12-01T08:22:55.000Z,
+        //     updatedAt: 2022-12-02T07:55:41.000Z,
+        //     categoryId: 5,
+        //     Category: {
+        //       id: 5,
+        //       name: '素食料理',
+        //       createdAt: 2022-12-01T08:22:54.000Z,
+        //       updatedAt: 2022-12-01T08:22:54.000Z
+        //     },
+        //     isFavorited: false
+        //   },{},{}...頁面上9個
+        // ]
+        return res.render("restaurants", {
           restaurants: data,
           categories,
           categoryId, // 才可以判斷active
-          pagination: getPagination(limit, page, restaurants.count)
-        })
+          pagination: getPagination(limit, page, restaurants.count),
+        });
       })
-      .catch(err => next(err))
+      .catch((err) => next(err));
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
-      include: [Category, { model: Comment, include: User }], // 拿出關聯的 Category model及 預先加載
-      // 找Restaurant的Category、找Restaurant的Comment、找Restaurant的Comment的User
-      order: [[Comment, 'createdAt', 'desc']]
+      include: [
+        Category,
+        { model: Comment, include: User },
+        { model: User, as: "FavoritedUsers" },
+      ], // 拿出關聯的 Category model及 預先加載
+      // 找Restaurant的Category、找Restaurant的Comment、找Restaurant的Comment的User。以此類推
+      order: [[Comment, "createdAt", "desc"]],
     })
-      .then(restaurant => {
-        if (!restaurant) throw new Error("Restaurant doesn't exist!")
-        return restaurant.increment('viewCounts', { by: 1 }) // 小寫restaurant
+      .then((restaurant) => {
+        if (!restaurant) throw new Error("Restaurant doesn't exist!");
+        return restaurant.increment("viewCounts", { by: 1 }); // 小寫restaurant
       })
-      .then(restaurant => {
-        // console.log("A-", restaurant, "B-", restaurant.toJSON());
-        // const allComments = restaurant.toJSON().Comments;
-        // const sortedDescComments = [...allComments].sort(
-        //   (objA, objB) => Number(objB.updatedAt) - Number(objA.updatedAt)
-        // );
-        // console.log(sortedDesc);
-        res.render('restaurant', {
-          restaurant: restaurant.toJSON() // toJSON大小寫要全對
-        })
+      .then((restaurant) => {
+        // console.log(restaurant.FavoritedUsers); 回傳一陣列，假設這餐廳有兩個收藏者，如下
+        // [User{},User{}]，內容為此2使用者的user表資料
+        //some測試陣列中是否至少有一個元素，一找到就結束(回傳布林值)，map則會全部跑完
+        const isFavorited = restaurant.FavoritedUsers.some(
+          (f) => f.id === req.user.id
+        );
+        res.render("restaurant", {
+          restaurant: restaurant.toJSON(), // toJSON大小寫要全對
+          isFavorited,
+        });
       })
-      .catch(err => next(err))
+      .catch((err) => next(err));
   },
   getDashboard: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
       include: Category, // 拿出關聯的 Category model
       nest: true,
-      raw: true
+      raw: true,
     })
-      .then(restaurant => {
+      .then((restaurant) => {
         // console.log(restaurant);
-        if (!restaurant) throw new Error("Restaurant doesn't exist!")
-        res.render('dashboard', {
-          restaurant
-        })
+        if (!restaurant) throw new Error("Restaurant doesn't exist!");
+        res.render("dashboard", {
+          restaurant,
+        });
       })
-      .catch(err => next(err))
+      .catch((err) => next(err));
   },
   getFeeds: (req, res, next) => {
     return Promise.all([
       Restaurant.findAll({
         limit: 10,
-        order: [['createdAt', 'DESC']], // 陣列包陣列，要多組條件排序的話為[[],[],[]...]
+        order: [["createdAt", "DESC"]], // 陣列包陣列，要多組條件排序的話為[[],[],[]...]
         include: [Category],
         raw: true,
-        nest: true
+        nest: true,
       }),
       Comment.findAll({
         limit: 10,
-        order: [['createdAt', 'DESC']],
+        order: [["createdAt", "DESC"]],
         include: [User, Restaurant],
         raw: true,
-        nest: true
-      })
+        nest: true,
+      }),
     ])
       .then(([restaurants, comments]) => {
-        res.render('feeds', {
+        res.render("feeds", {
           restaurants,
-          comments
-        })
+          comments,
+        });
       })
-      .catch(err => next(err))
-  }
-}
+      .catch((err) => next(err));
+  },
+};
 
 // 匯出之後才能在其他檔案裡使用。
-module.exports = restaurantController
+module.exports = restaurantController;
