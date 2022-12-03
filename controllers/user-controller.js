ed8d27ba8ba071b14b313532ac06f5d67a9baed0
linway035 <wayneck0138@gmail.com>
2022-12-03 10:08:59 +0800

    feat: addFollowing & removeFollowing function

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -6,10 +6,10 @@ const {
 const {
   Restaurant,
   User,
-  Category,
   Comment,
   Favorite,
   Like,
+  Followship,
 } = require("../models");
 const { getUser } = require("../helpers/auth-helpers");
 const userController = {
@@ -211,16 +211,54 @@ const userController = {
     })
       .then((users) => {
         // 整理 users 資料，把每個 user 項目都拿出來處理一次，並把新陣列儲存在 users 裡
-        users = users.map((user) => ({
-          // 整理格式
-          ...user.toJSON(),
-          // 計算追蹤者人數
-          followerCount: user.Followers.length,
-          // 判斷目前登入使用者的追蹤清單是否已包含該 user 物件
-          isFollowed: req.user.Followings.some((f) => f.id === user.id),
-        }));
-        res.render("top-users", { users: users });
+        const result = users
+          .map((user) => ({
+            // 整理格式
+            ...user.toJSON(),
+            // 計算追蹤者人數
+            followerCount: user.Followers.length,
+            // 判斷目前登入使用者的追蹤清單是否已包含該 user 物件
+            isFollowed: req.user.Followings.some((f) => f.id === user.id),
+          }))
+          .sort((a, b) => b.followerCount - a.followerCount); //由大到小
+        res.render("top-users", { users: result });
       })
+      .catch((err) => next(err));
+  },
+  addFollowing: (req, res, next) => {
+    const { userId } = req.params;
+    Promise.all([
+      User.findByPk(userId),
+      Followship.findOne({
+        where: {
+          followerId: req.user.id,
+          followingId: req.params.userId,
+        },
+      }),
+    ])
+      .then(([user, followship]) => {
+        if (!user) throw new Error("User doesn't exist!");
+        if (followship) throw new Error("You are already following this user!");
+        return Followship.create({
+          followerId: req.user.id,
+          followingId: userId,
+        });
+      })
+      .then(() => res.redirect("back"))
+      .catch((err) => next(err));
+  },
+  removeFollowing: (req, res, next) => {
+    Followship.findOne({
+      where: {
+        followerId: req.user.id,
+        followingId: req.params.userId,
+      },
+    })
+      .then((followship) => {
+        if (!followship) throw new Error("You haven't followed this user!");
+        return followship.destroy();
+      })
+      .then(() => res.redirect("back"))
       .catch((err) => next(err));
   },
 };
