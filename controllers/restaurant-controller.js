db383094ae3cebd036b71fe6c26551c18542197f
Hua0720 <stella573021@gmail.com>
2022-12-04 02:18:10 +0800

    feat: add resttop restaurants page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -101,6 +101,24 @@ const restaurantController = {
         })
       })
       .catch(err => next(err))
+  },
+  getTopRestaurants: (req, res, next) => {
+    return Restaurant.findAll({
+      include: [{
+        model: User, as: 'FavoritedUsers'
+      }]
+    })
+      .then(restaurants => {
+        const result = restaurants.map(r => ({
+          ...r.dataValues,
+          description: r.dataValues.description.substring(0, 50),
+          favoritedCount: r.FavoritedUsers.length,
+          isFavorited: req.user && req.user.FavoritedRestaurants.some(f => f.id === r.id)
+        }))
+          .sort((a, b) => b.favoritedCount - a.favoritedCount).slice(0, 10)
+        res.render('top-restaurants', { restaurants: result })
+      })
+      .catch(next)
   }
 }
 module.exports = restaurantController
