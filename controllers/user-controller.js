7826868291950792084c3eca78fbf3842cbc4e96
wujia777 <cwu005104@gmail.com>
2022-12-03 21:58:27 +0800

    feat: pass R04 tests

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,5 +1,5 @@
 const bcrypt = require('bcryptjs')
-const { Restaurant, User, Favorite } = require('../models')
+const { Restaurant, User, Favorite, Like } = require('../models')
 
 const userController = {
   signUpPage: (req, res) => {
@@ -75,7 +75,41 @@ const userController = {
       })
       .then(() => res.redirect('back'))
       .catch(err => next(err))
+  },
+  addLike: (req, res, next) => {
+    const { restaurantId } = req.params
+    return Promise.all([
+      Restaurant.findByPk(restaurantId),
+      Like.findOne({
+        where: {
+          userId: req.user.id,
+          restaurantId
+        }
+      })
+    ]).then(([restaurant, like]) => {
+      if (!restaurant) throw new Error("Restaurant didn't exist!")
+      if (like) throw new Error('You Liked this!')
+
+      return Like.create({
+        userId: req.user.id,
+        restaurantId
+      })
+    }).then(() => res.redirect('back'))
+      .catch(err => next(err))
+  },
+  removeLike: (req, res, next) => {
+    const { restaurantId } = req.params
+    return Like.findOne({
+      where: {
+        userId: req.user.id,
+        restaurantId
+      }
+    }).then(like => {
+      if (!like) throw new Error("You didn't like this!")
+
+      return like.destroy()
+    }).then(() => res.redirect('back'))
+      .catch(err => next(err))
   }
 }
-
 module.exports = userController
