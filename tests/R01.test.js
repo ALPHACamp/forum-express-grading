017f4a7e9ee427d9a026ebd46d219d1b7a76197e
ZHENG-YOU-LU <yowyow222@gmail.com>
2022-12-03 11:05:41 +0800

    feat: create flike model

--- tests/R01.test.js
+++ tests/R01.test.js
@@ -186,10 +186,13 @@ describe('# R01', () => {
         res.redirect.calledWith('/admin/users').should.be.true
 
         // patchUser 執行完畢後，假資料中 id:2 使用者的應該要是 isAdmin：false
+        // patchUser 執行完畢後，假資料中 id:2 使用者的應該要是 isAdmin：true
         // 將假資料撈出，比對確認有成功修改到
         const user = await this.UserMock.findOne({ where: { id: 2 } })
         user.isAdmin.should.equal(false)
       })
     })
   })
+  
 })
+
