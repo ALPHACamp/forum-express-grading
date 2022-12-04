be54c0b3225026ba03d5362376eee7efa69574b2
Verna0214 <yuhsichen0214@gmail.com>
2022-12-04 09:49:04 +0800

    feat: add feeds page

--- routes/index.js
+++ routes/index.js
@@ -17,6 +17,7 @@ router.post('/signin', passport.authenticate('local', { failureRedirect: '/signi
 router.get('/logout', userController.logout)
 router.get('/users/:id/edit', authenticated, userController.editUser)
 router.put('/users/:id', authenticated, upload.single('image'), userController.putUser)
+router.get('/restaurants/feeds', authenticated, restController.getFeeds)
 router.get('/users/:id', authenticated, userController.getUser)
 router.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard)
 router.get('/restaurants/:id', authenticated, restController.getRestaurant)
