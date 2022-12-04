eb5b98ac6555371639e09783cb210566f2b2e888
Benson.YBS <yehbo0921@gmail.com>
2022-12-05 01:52:06 +0800

    feat: add imgur api

--- app.js
+++ app.js
@@ -8,7 +8,9 @@ const passport = require('./config/passport')
 const handlebarsHelpers = require('./helpers/handlebars-helpers')
 const { getUser } = require('./helpers/auth-helpers')
 const routes = require('./routes')
-
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const app = express()
 const port = process.env.PORT || 3000
 const SESSION_SECRET = 'secret'
