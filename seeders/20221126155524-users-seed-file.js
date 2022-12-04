801384012d7bde219b35940573d2e8c89f5343a5
wujia777 <cwu005104@gmail.com>
2022-12-04 23:32:48 +0800

    feat: restart cmd change

--- seeders/20221126155524-users-seed-file.js
+++ seeders/20221126155524-users-seed-file.js
@@ -1,7 +1,7 @@
 'use strict'
 const bcrypt = require('bcryptjs')
 module.exports = {
-  async up(queryInterface, Sequelize) {
+  async up (queryInterface, Sequelize) {
     await queryInterface.bulkInsert('Users', [{ // 一次新增三筆資料
       email: 'root@example.com',
       password: await bcrypt.hash('12345678', 10),
@@ -26,7 +26,7 @@ module.exports = {
     }], {})
   },
 
-  async down(queryInterface, Sequelize) {
+  async down (queryInterface, Sequelize) {
     await queryInterface.bulkDelete('Users', null, {})
     /**
      * Add commands to revert seed here.
