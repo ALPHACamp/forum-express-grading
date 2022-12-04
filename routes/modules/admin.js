0c778ec0e51526e7035268e9b0ac5ed0c4ce1c1d
Aither <cheese736@hotmail.com>
2022-12-04 16:00:27 +0800

    merge R01 into main

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
+const categoryController = require('../../controllers/category-controller')
 const upload = require('../../middleware/multer')
 
 // route
+// restaurant
 router.get('/restaurants/create', adminController.createRestaurant)
 router.get('/restaurants/:id/edit', adminController.editRestaurant)
 router.get('/restaurants/:id', adminController.getRestaurant)
@@ -11,6 +13,15 @@ router.put('/restaurants/:id', upload.single('image'), adminController.putRestau
 router.delete('/restaurants/:id', adminController.deleteRestaurant)
 router.get('/restaurants', adminController.getRestaurants)
 router.post('/restaurants', upload.single('image'), adminController.postRestaurant)
+// user
+router.get('/users', adminController.getUsers)
+router.patch('/users/:id', adminController.patchUser)
+// category
+router.get('/categories', categoryController.getCategories)
+router.post('/categories', categoryController.postCategory)
+router.get('/categories/:id', categoryController.getCategories) // 新增這行
+router.put('/categories/:id', categoryController.putCategory)
+// fallback
 router.use('/', (req, res) => res.redirect('/admin/restaurants'))
 
 // export
