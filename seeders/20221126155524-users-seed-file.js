c653226ffbda0ea386908fc83aaaeae0ac9df45f
wujia777 <cwu005104@gmail.com>
2022-12-04 23:09:03 +0800

    feat: delete user img migration

--- seeders/20221126155524-users-seed-file.js
+++ seeders/20221126155524-users-seed-file.js
@@ -2,35 +2,32 @@
 const bcrypt = require('bcryptjs')
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
+  async up(queryInterface, Sequelize) {
     await queryInterface.bulkInsert('Users', [{ // 一次新增三筆資料
       email: 'root@example.com',
       password: await bcrypt.hash('12345678', 10),
       is_admin: true,
       name: 'root',
       created_at: new Date(),
-      updated_at: new Date(),
-      image: `https://loremflickr.com/320/240/human/?random=${Math.random() * 100}`
+      updated_at: new Date()
     }, {
       email: 'user1@example.com',
       password: await bcrypt.hash('12345678', 10),
       is_admin: false,
       name: 'user1',
       created_at: new Date(),
-      updated_at: new Date(),
-      image: `https://loremflickr.com/320/240/human/?random=${Math.random() * 100}`
+      updated_at: new Date()
     }, {
       email: 'user2@example.com',
       password: await bcrypt.hash('12345678', 10),
       is_admin: false,
       name: 'user2',
       created_at: new Date(),
-      updated_at: new Date(),
-      image: `https://loremflickr.com/320/240/human/?random=${Math.random() * 100}`
+      updated_at: new Date()
     }], {})
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     await queryInterface.bulkDelete('Users', null, {})
     /**
      * Add commands to revert seed here.
