66f67e512ae3c29bf07078c19e6ce77e3b48c88d
weizi0328 <ankewang26@gmail.com>
2022-12-04 08:44:11 +0800

    feat: add imgur api to profile

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcryptjs')
-const { User, Restaurant, Favorite, Like } = require('../models')
-const { localFileHandler } = require('../helpers/file-helpers')
+const { User, Restaurant, Comment, Favorite, Like } = require('../models')
+const { imgurFileHandler } = require('../helpers/file-helpers')
 
 const userController = {
   signUpPage: (req, res) => {
@@ -45,11 +45,15 @@ const userController = {
   },
   getUser: (req, res, next) => {
     return User.findByPk(req.params.id, {
-      raw: true
+      // raw: true
+      include: {
+        model: Comment, include: Restaurant
+      }
     })
       .then(user => {
         if (!user) throw new Error("User didn't exist!")
-        res.render('users/profile', { user })
+        console.log(user.toJSON())
+        res.render('users/profile', { user: user.toJSON() })
       })
       .catch(err => next(err))
   },
@@ -68,7 +72,7 @@ const userController = {
     const { file } = req
     return Promise.all([
       User.findByPk(req.params.id),
-      localFileHandler(file)
+      imgurFileHandler(file)
     ])
       .then(([user, filePath]) => {
         if (!user) throw new Error("User didn't exist!")
