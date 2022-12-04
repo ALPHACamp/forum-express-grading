bfde9dcff6ddba7017f045925b081c7bfd1d5454
lomo007 <eragon66997@gmail.com>
2022-12-04 22:55:44 +0800

    feat: modify Comment table name

--- models/comment.js
+++ models/comment.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate (models) {
+    static associate(models) {
       Comment.belongsTo(models.Restaurant, { foreignKey: 'restaurantId' })
       Comment.belongsTo(models.User, { foreignKey: 'userId' })
     }
@@ -20,7 +20,8 @@ module.exports = (sequelize, DataTypes) => {
     restaurantId: DataTypes.INTEGER
   }, {
     sequelize,
-    modelName: 'Comments',
+    modelName: 'Comment',
+    tableName: 'Comments',
     underscored: true
   })
   return Comment
