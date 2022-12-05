dea40b5611544e68ffc9e036da7b0cb927747684
lomo007 <eragon66997@gmail.com>
2022-12-05 21:45:05 +0800

    feat: create followship model, addFollowing & removeFollowing function

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,7 @@ router.post('/signup', userController.signUp) // 注意用 post
 router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
 router.get('/logout', userController.logout)
+router.get('/users/top', authenticated, userController.getTopUsers)
 router.get('/restaurants/feeds', authenticated, restController.getFeeds) //*
 router.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard)
 router.get('/restaurants/:id', authenticated, restController.getRestaurant)
@@ -25,6 +26,7 @@ router.post('/favorite/:restaurantId', authenticated, userController.addFavorite
 router.delete('/favorite/:restaurantId', authenticated, userController.removeFavorite)
 router.post('/like/:restaurantId', authenticated, userController.addLike)
 router.delete('/like/:restaurantId', authenticated, userController.removeLike)
+router.get('/users/top', authenticated, userController.getTopUsers)
 router.get('/users/:id/edit', authenticated, userController.editUser)
 router.put('/users/:id', authenticated, upload.single('image'), userController.putUser)
 router.get('/users/:id', authenticated, userController.getUser)
