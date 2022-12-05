e9c1d9b0c2cbf3bf98de9ea803da79569b34c2ae
wujia777 <cwu005104@gmail.com>
2022-12-05 21:16:06 +0800

    feat: R03 Q5 merge to see what happend

--- migrations/20221204084304-add-image-to-users.js
+++ migrations/20221204084304-add-image-to-users.js
@@ -1,6 +1,5 @@
 'use strict'
-
-'use strict'
+/** @type {import('sequelize-cli').Migration} */
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.addColumn('Users', 'image', {
--- seeders/20221126155524-users-seed-file.js
+++ seeders/20221126155524-users-seed-file.js
@@ -1,6 +1,5 @@
 'use strict'
 const bcrypt = require('bcryptjs')
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
     await queryInterface.bulkInsert('Users', [{ // 一次新增三筆資料
@@ -9,21 +8,24 @@ module.exports = {
       is_admin: true,
       name: 'root',
       created_at: new Date(),
-      updated_at: new Date()
+      updated_at: new Date(),
+      image: `https://loremflickr.com/320/240/male,female/?lock=${Math.random() * 100}`
     }, {
       email: 'user1@example.com',
       password: await bcrypt.hash('12345678', 10),
       is_admin: false,
       name: 'user1',
       created_at: new Date(),
-      updated_at: new Date()
+      updated_at: new Date(),
+      image: `https://loremflickr.com/320/240/male,female/?lock=${Math.random() * 100}`
     }, {
       email: 'user2@example.com',
       password: await bcrypt.hash('12345678', 10),
       is_admin: false,
       name: 'user2',
       created_at: new Date(),
-      updated_at: new Date()
+      updated_at: new Date(),
+      image: `https://loremflickr.com/320/240/male,female/?lock=${Math.random() * 100}`
     }], {})
   },
 
