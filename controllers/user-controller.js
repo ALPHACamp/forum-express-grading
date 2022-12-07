5670b7b47311965b2e45082ad23be48ea5aee988
Aither <cheese736@hotmail.com>
2022-12-07 18:07:10 +0800

    establish user profile (R03)

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcryptjs') // 載入 bcrypt
-const db = require('../models')
-const { User } = db
+const { User, Comment, Restaurant } = require('../models')
+const { imgurFileHandler } = require('../helpers/file-helpers')
+const { getUser } = require('../helpers/auth-helpers')
 const userController = {
   signUpPage: (req, res) => {
     res.render('signup')
@@ -8,7 +9,7 @@ const userController = {
   signUp: (req, res, next) => { // 修改這裡
     if (req.body.password !== req.body.passwordCheck) throw new Error('Passwords do not match!')
 
-    User.findOne({ where: { email: req.body.email } })
+    return User.findOne({ where: { email: req.body.email } })
       .then(user => {
         if (user) throw new Error('Email already exists!')
         return bcrypt.hash(req.body.password, 10) // 前面加 return
@@ -35,6 +36,55 @@ const userController = {
     req.flash('success_messages', '登出成功！')
     req.logout()
     res.redirect('/signin')
+  },
+  getUser: (req, res, next) => {
+    const userId = getUser(req).id
+    if (userId !== Number(req.params.id)) throw new Error('Permission denied!')
+    return Promise.all([
+      User.findByPk(userId, { raw: true }),
+      Comment.findAndCountAll({
+        include: [Restaurant],
+        nest: true,
+        where: { userId: req.params.id }
+      })
+    ])
+      .then(([user, comments]) => {
+        res.render('users/profile', { user, comments })
+      })
+      .catch(err => next(err))
+  },
+
+  editUser: (req, res, next) => {
+    const userId = getUser(req).id
+    if (userId !== Number(req.params.id)) throw new Error('Permission denied!')
+    return User.findByPk(userId, { raw: true })
+      .then(user => {
+        res.render('users/edit', { user })
+      })
+      .catch(err => next(err))
+  },
+
+  putUser: (req, res, next) => {
+    const userId = req.user.id
+    const { name } = req.body
+    const { file } = req
+    if (!name) throw new Error('User name is required!')
+    if (userId !== Number(req.params.id)) throw new Error('Permission denied!')
+    return Promise.all([
+      User.findByPk(userId),
+      imgurFileHandler(file)
+    ])
+      .then(([user, filepath]) => {
+        return user.update({
+          name,
+          avatar: filepath || user.avatar
+        })
+      })
+      .then(() => {
+        req.flash('success_messages', '使用者資料編輯成功')
+        res.redirect(`/users/${userId}`)
+      })
+      .catch(err => next(err))
   }
 }
 module.exports = userController
