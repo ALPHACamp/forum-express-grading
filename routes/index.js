ad0e7e03fd1adbc44745340388af7899dedd6b31
lurulu156 <kzzlin9@gmail.com>
2022-12-13 17:44:05 +0800

    feat: add header & footer

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,7 @@ router.post('/signup', userController.signUp)
 router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
 router.get('/logout', userController.logout)
+
 router.use('/admin', admin)
 router.get('/restaurants', restController.getRestaurants)
 router.use('/', (req, res) => res.redirect('/restaurants'))
