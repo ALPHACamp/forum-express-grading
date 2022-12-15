cddd9ba18975a5c0aa292da4634ba5f500e12fff
lurulu156 <kzzlin9@gmail.com>
2022-12-15 18:45:23 +0800

    feat: add pagination on admin restaurants index page

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,14 +1,25 @@
 const { Restaurant, User, Category } = require('../models')
 const { imgurFileHandler } = require('../helpers/file-helpers')
+const { getOffset, getPagination } = require('../helpers/pagination-helpers')
 
 const adminController = {
   getRestaurants: (req, res, next) => {
-    Restaurant.findAll({
+    const DEFAULT_LIMIT = 10
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || DEFAULT_LIMIT
+    const offset = getOffset(limit, page)
+
+    Restaurant.findAndCountAll({
       raw: true,
       nest: true,
-      include: [Category]
+      include: [Category],
+      limit,
+      offset
     })
-      .then(restaurants => res.render('admin/restaurants', { restaurants }))
+      .then(restaurants => res.render('admin/restaurants', {
+        restaurants: restaurants.rows,
+        pagination: getPagination(limit, page, restaurants.count)
+      }))
       .catch(err => next(err))
   },
   createRestaurant: (req, res, next) => {
