c5662a9e86228f98246dfe00c88c1d40bb2cfdae
lurulu156 <kzzlin9@gmail.com>
2022-12-20 10:45:50 +0800

    feat: add getRestaurants api

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const session = require('express-session')
 const passport = require('./config/passport')
 const handlebarsHelpers = require('./helpers/handlebars-helpers')
 const { getUser } = require('./helpers/auth-helpers')
-const { pages } = require('./routes')
+const { pages, apis } = require('./routes')
 
 if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
@@ -35,6 +35,7 @@ app.use((req, res, next) => {
   res.locals.user = getUser(req)
   next()
 })
+app.use('/api', apis)
 app.use(pages)
 
 app.listen(port, () => {
