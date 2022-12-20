a00f0ca4243e299787569eb2cfb3a779e99a2532
lurulu156 <kzzlin9@gmail.com>
2022-12-20 17:19:41 +0800

    feat: add api-auth middleware

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const path = require('path')
 const express = require('express')
 const handlebars = require('express-handlebars')
@@ -8,10 +12,6 @@ const passport = require('./config/passport')
 const handlebarsHelpers = require('./helpers/handlebars-helpers')
 const { getUser } = require('./helpers/auth-helpers')
 const { pages, apis } = require('./routes')
-
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()
-}
 
 const app = express()
 const port = process.env.PORT || 3000
--- routes/apis/index.js
+++ routes/apis/index.js
@@ -4,10 +4,11 @@ const passport = require('../../config/passport')
 const admin = require('./modules/admin')
 const restController = require('../../controllers/apis/restaurant-controller')
 const userController = require('../../controllers/apis/user-controller')
+const { authenticated, authenticatedAdmin } = require('../../middleware/api-auth')
 const { apiErrorHandler } = require('../../middleware/error-handler')
 
-router.use('/admin', admin)
+router.use('/admin', authenticated, authenticatedAdmin, admin)
+router.get('/restaurants', authenticated, restController.getRestaurants)
 router.post('/signin', passport.authenticate('local', { session: false }), userController.signIn)
-router.get('/restaurants', restController.getRestaurants)
 router.use('/', apiErrorHandler)
 module.exports = router
