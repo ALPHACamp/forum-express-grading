fcf14db40bc4003352e040cf4a879eef3569bf1b
yichennn <yichennn1010@gmail.com>
2023-01-03 17:19:08 +0800

    feat: modify session secret

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ if (process.env.NODE_ENV !== 'production') {
 }
 const app = express()
 const port = process.env.PORT || 3000
+const SESSION_SECRET = 'secret'
 
 // 套件
 const exphbs = require('express-handlebars')
@@ -23,7 +24,7 @@ app.engine('hbs', exphbs({ extname: '.hbs', helpers: hbsHelpers })) // 註冊 Ha
 app.set('view engine', 'hbs')// 設定使用 Handlebars 做為樣板引擎
 app.use(express.urlencoded({ extended: true }))
 app.use(session({
-  secret: process.env.SESSION_SECRET,
+  secret: SESSION_SECRET,
   resave: false,
   saveUninitialized: false
 }))
