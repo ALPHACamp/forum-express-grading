9364268a7a148f25cc9f58ea4a89b2345d015bbf
yichennn <yichennn1010@gmail.com>
2023-01-05 17:30:52 +0800

    feat: add Category model & add category_id to Restaurant model

---
+++ models/category.js
@@ -0,0 +1,21 @@
+'use strict'
+const {
+  Model
+} = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+  class Category extends Model {
+    static associate (models) {
+      // define association here
+      Category.hasMany(models.Restaurant, { foreignKey: 'categoryID' })
+    }
+  };
+  Category.init({
+    name: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Category',
+    tableName: 'Categories',
+    underscored: true
+  })
+  return Category
+}
