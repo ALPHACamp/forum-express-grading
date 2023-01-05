47b747c634d82b6f84749988e252c5608469ecbe
daniel155158 <daniel155158@gmail.com>
2023-01-05 17:57:32 +0800

    feat: flash msg & signup verification

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
 const express = require('express')
 const routes = require('./routes')
 const handlebars = require('express-handlebars')
+const flash = require('connect-flash')
+const session = require('express-session')
 
 const app = express()
 const port = process.env.PORT || 3000
+const SESSION_SECRET = 'secret'
+
 // template engine
 app.engine('hbs', handlebars({ extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 // middleware
 app.use(express.urlencoded({ extended: true }))
+app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
+app.use(flash()) // 掛載套件
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 app.use(routes)
 
 app.listen(port, () => {
