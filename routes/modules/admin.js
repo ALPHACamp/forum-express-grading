04bfd5af6e3564eaeac74e02bb3d9229c4203cd9
daniel155158 <daniel155158@gmail.com>
2023-01-05 21:52:52 +0800

    feat: user model add is_admin

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
-const adminController = require('../../controllers/admin-controller')
 
-router.get('/restaurants', adminController.getRestaurants)
+const adminController = require('../../controllers/admin-controller')
+const { authenticatedAdmin } = require('../../middleware/auth')
+
+router.get('/restaurants', authenticatedAdmin, adminController.getRestaurants)
 router.use('/', (req, res) => res.redirect('/admin/restaurants'))
 
 module.exports = router
