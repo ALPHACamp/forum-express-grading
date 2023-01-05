66f058773321ea892746b5b157c541a53d0e3680
Johnny77585 <jkuo2188@gmail.com>
2023-01-05 23:09:46 +0800

    feat: addFollowing & removeFollowing function

--- routes/index.js
+++ routes/index.js
@@ -28,6 +28,8 @@ router.delete('/favorite/:restaurantId', authenticated, userController.removeFav
 router.get('/users/:id/edit', authenticatedUser, userController.editUser)
 router.get('/users/:id', authenticated, userController.getUser)
 router.put('/users/:id', authenticatedUser, upload.single('image'), userController.putUser)
+router.post('/following/:userId', authenticated, userController.addFollowing)
+router.delete('/following/:userId', authenticated, userController.removeFollowing)
 
 router.get('/', (req, res) => res.redirect('/restaurants'))
 router.use('/', generalErrorHandler)
