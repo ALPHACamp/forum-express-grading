d9c25522496a07f0a9a057a3b39f162a6455fc94
daniel155158 <daniel155158@gmail.com>
2023-01-05 18:45:44 +0800

    feat: passport init & signin

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const routes = require('./routes')
 const handlebars = require('express-handlebars')
 const flash = require('connect-flash')
 const session = require('express-session')
+const passport = require('./config/passport')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -15,7 +16,9 @@ app.set('view engine', 'hbs')
 // middleware
 app.use(express.urlencoded({ extended: true }))
 app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
-app.use(flash()) // 掛載套件
+app.use(passport.initialize()) // 初始化Passport
+app.use(passport.session()) // 啟動session功能
+app.use(flash())
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
   res.locals.error_messages = req.flash('error_messages')
