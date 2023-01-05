63392a6959387a4f8b6b020901df5142c67ffbfd
daniel155158 <daniel155158@gmail.com>
2023-01-05 13:27:39 +0800

    feat: user signup

--- app.js
+++ app.js
@@ -4,10 +4,12 @@ const handlebars = require('express-handlebars')
 
 const app = express()
 const port = process.env.PORT || 3000
-
+// template engine
 app.engine('hbs', handlebars({ extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+// middleware
+app.use(express.urlencoded({ extended: true }))
 app.use(routes)
 
 app.listen(port, () => {
