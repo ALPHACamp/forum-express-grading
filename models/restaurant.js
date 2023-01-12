b8f42e005d1c32dab04fa33dfd4f3d0c9388c1f4
Dinsky21 <dinsky21@gmail.com>
2023-01-12 12:54:35 +0800

    feat: add dashboard and viewCount

--- models/restaurant.js
+++ models/restaurant.js
@@ -19,7 +19,9 @@ module.exports = (sequelize, DataTypes) => {
     address: DataTypes.STRING,
     openingHours: DataTypes.STRING,
     description: DataTypes.TEXT,
-    image: DataTypes.STRING
+    image: DataTypes.STRING,
+    viewCount: DataTypes.INTEGER
+
   }, {
     sequelize,
     modelName: 'Restaurant',
