86093adaafeb4ed9fb888d881eee00b5154bae0b
wenliangsu <wenliangsu65018@gmail.com>
2023-01-12 15:25:31 +0800

    Feat: project initialized

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
-const express = require('express')
-const routes = require('./routes')
+const express = require('express');
+const handlebars = require('express-handlebars');
+const routes = require('./routes');
 
-const app = express()
-const port = process.env.PORT || 3000
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.use(routes)
+app.engine('hbs', handlebars({ extname: '.hbs' }));
+app.set('view engine', 'hbs');
+
+app.use(routes);
 
 app.listen(port, () => {
-  console.info(`Example app listening on port ${port}!`)
-})
+  console.info(`Example app listening on port ${port}!`);
+});
 
-module.exports = app
+// note 導入自動化測試後會用到，所以要寫這段
+module.exports = app;
