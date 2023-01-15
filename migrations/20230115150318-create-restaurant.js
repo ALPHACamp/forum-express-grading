94efb4544ca5f6104aa65bedcc1a9381544a2904
wenliangsu <wenliangsu65018@gmail.com>
2023-01-16 00:00:15 +0800

    Feat: add model of restaurant in MySQL

---
+++ migrations/20230115150318-create-restaurant.js
@@ -0,0 +1,37 @@
+// !!建立後還是要檢查一下是否有用到駝峰式寫法，需要修改
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Restaurants', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      address: {
+        type: Sequelize.STRING
+      },
+      opening_hours: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.TEXT
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Restaurants');
+  }
+};
