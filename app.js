5748d257a67270663551be294fa37e66048ce53a
thk61159 <tina0757023.bt07@nycu.edu.tw>
2023-01-23 02:43:19 +0800

    feat: add index page

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const express = require('express')
 const routes = require('./routes')
-const handlebars = require('express-handlebars');
+const handlebars = require('express-handlebars')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.engine('hbs', handlebars({ extname: '.hbs' }));
+app.engine('hbs', handlebars({ extname: '.hbs' }))
 // 設定使用 Handlebars 做為樣板引擎
-app.set('view engine', 'hbs');
-app.use(routes);
+app.set('view engine', 'hbs')
+app.use(routes)
 
 app.listen(port, () => {
   console.info(`Example app listening on port ${port}!`)
