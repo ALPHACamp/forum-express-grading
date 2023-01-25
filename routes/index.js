ec41b86fe11c725507407ff2878f0897e27667ca
Isis Lin <qweb32114@gmail.com>
2023-01-25 16:42:23 +0800

    feat: user signup

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,13 @@ const express = require('express')
 const router = express.Router()
 
 const restController = require('../controllers/restaurant-controller')
+const userController = require('../controllers/user-controller')
 const admin = require('./modles/admin.js')
 
 router.use('/admin', admin)
+
+router.get('/signup', userController.signUppage)
+router.post('/signup', userController.signUp)
 router.get('/restaurants', restController.getRestaurants)
 
 router.use('/', (req, res) => res.redirect('/restaurants'))
