f28ac0866640af5d256c642d6554cf90894f596a
LAPTOP-QJBRI58J\Anne <koala1995610@gmail.com>
2023-01-27 14:15:06 +0800

    feat: add user signup

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 3000
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
+app.use(express.urlencoded({ extended: true }))
 app.use(routes)
 
 app.listen(port, () => {
