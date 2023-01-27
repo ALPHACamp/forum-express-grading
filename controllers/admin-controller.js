bc3e11d73dc2f5e60b99ae262f34e2f07e59a64a
thk61159 <tina0757023.bt07@nycu.edu.tw>
2023-01-27 21:22:09 +0800

    feat: fix user authorization

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -89,7 +89,7 @@ const adminController = {
       .catch(err => next(err))
   },
   getUsers: (req, res, next) => {
-    User.findAll({
+    return User.findAll({
       raw: true
     })
       .then(users => res.render('admin/users', { users }))
@@ -97,18 +97,18 @@ const adminController = {
   },
   patchUser: (req, res, next) => {
     const { id } = req.params
-    User.findByPk(id)
+    return User
+      .findByPk(id)
       .then(user => {
-        if (user.name === 'root') throw new Error('禁止變更 root 權限')
-        if (user.isAdmin) {
-          user.isAdmin = false
-        } else {
-          user.isAdmin = true
+        if (user.email === 'root@example.com') {
+          req.flash('error_messages', '禁止變更 root 權限')
+          return res.redirect('back')
         }
-        return user.save()
+        return user.update({ isAdmin: !user.isAdmin })
       })
       .then(() => {
-        res.redirect('/admin/users')
+        req.flash('success_messages', '使用者權限變更成功')
+        return res.redirect('/admin/users')
       })
       .catch(err => next(err))
   }
