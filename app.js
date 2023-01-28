e7310f57e749a6adb4273be65219fd16f8026eb3
LAPTOP-QJBRI58J\Anne <koala1995610@gmail.com>
2023-01-28 19:26:57 +0800

    feat: flash msg & signup verification

--- app.js
+++ app.js
@@ -1,13 +1,24 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const routes = require('./routes')
+const flash = require('connect-flash')
+const session = require('express-session')
 
 const app = express()
 const port = process.env.PORT || 3000
+const SESSION_SECRET = 'secret'
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
+app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
+
 app.use(routes)
 
 app.listen(port, () => {
