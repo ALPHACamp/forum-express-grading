b5f72cf83cecd96daf576696cd9ea9e6e3798000
Dinsky21 <dinsky21@gmail.com>
2023-01-28 21:16:09 +0800

    feat: add getRestaurants api

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const path = require('path')
 const express = require('express')
-const { pages } = require('./routes')
+const { pages, apis } = require('./routes')
 const handlebars = require('express-handlebars')
 const app = express()
 const port = process.env.PORT || 3000
@@ -29,6 +29,7 @@ app.use((req, res, next) => {
   res.locals.user = getUser(req)
   next()
 })
+app.use('/api', apis)
 app.use(pages)
 
 app.listen(port, () => {
