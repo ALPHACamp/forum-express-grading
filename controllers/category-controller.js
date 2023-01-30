b34fb99193bf97fd88e5a1ea7863722863a0408b
thk61159 <tina0757023.bt07@nycu.edu.tw>
2023-01-30 18:19:05 +0800

    feat: category update

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -1,14 +1,32 @@
 const { Category } = require('../models')
 const categoryController = {
   getCategories: (req, res, next) => {
-    return Category.findAll({ raw: true })
-      .then(categories => res.render('admin/categories', { categories }))
+    const { id } = req.params
+    return Promise.all([
+      Category.findAll({ raw: true }),
+      id ? Category.findByPk(id, { raw: true }) : null
+    ])
+      .then(([categories, category]) =>
+        res.render('admin/categories', { categories, category })
+      )
       .catch(err => next(err))
   },
   postCategory: (req, res, next) => {
     const { name } = req.body
     if (!name) throw new Error('Category name is required!')
     return Category.create({ name })
+      .then(() => res.redirect('/admin/categories'))
+      .catch(err => next(err))
+  },
+  putCategory: (req, res, next) => {
+    const { id } = req.params
+    const { name } = req.body
+    if (!name) throw new Error('Category name is required!')
+    return Category.findByPk(id)
+      .then(category => {
+        if (!category) throw new Error("Category doesn't exist!")
+        return category.update({ name })
+      })
       .then(() => res.redirect('/admin/categories'))
       .catch(err => next(err))
   }
