22cd7bd696af8688e75fe4fbe4b01e800c3d1e0c
thk61159 <tina0757023.bt07@nycu.edu.tw>
2023-01-31 01:42:14 +0800

    feat: add restaurants index page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,6 +1,22 @@
+const { Category, Restaurant } = require('../models')
 const restaurantController = {
-  getRestaurants: (req, res) => {
-    return res.render('restaurants')
+  getRestaurants: (req, res, next) => {
+    return Restaurant.findAll({
+      include: Category,
+      raw: true,
+      nest: true
+    })
+      .then(restaurants => {
+        // map後的()是IIFE嗎？
+        const data = restaurants.map(r => ({
+          ...r,
+          description: r.description.substring(0, 50)
+        }))
+        return res.render('restaurants', {
+          restaurants: data
+        })
+      })
+      .catch(err => next(err))
   }
 }
 
