ef7cb1dd36ccd0661dade68b3489c054f8ef4b5b
Isis Lin <qweb32114@gmail.com>
2023-01-30 16:19:38 +0800

    feat: admin restaurant page

--- routes/modles/admin.js
+++ routes/modles/admin.js
@@ -3,9 +3,10 @@ const express = require('express')
 const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
 
-router.get('/restaurants', adminController.getRestaurants)
 router.get('/restaurants/create', adminController.createRestaurant)
-router.post('/restaurants/', adminController.postRestaurant)
+router.get('/restaurants/:id', adminController.getRestaurant)
+router.get('/restaurants', adminController.getRestaurants)
+router.post('/restaurants', adminController.postRestaurant)
 router.use('/', (req, res) => res.redirect('/admin/restaurants'))
 
 module.exports = router
