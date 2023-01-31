79568a3111ac6abb782852c8bee10947e729e96d
cayang <cayang@simenvi.com.tw>
2023-01-31 12:01:38 +0800

    feat: add restaurant image

--- routes/index.js
+++ routes/index.js
@@ -12,7 +12,7 @@ router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
 router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
-router.get('logout', userController.logOut)
+router.get('/logout', userController.logOut)
 router.get('/restaurants', authenticated, restController.getRestaurants)
 router.use('/', (req, res) => res.redirect('/restaurants'))
 
