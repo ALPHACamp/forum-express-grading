cabe4b3d312d28e2d8e0266f3afd86ad383f8ed4
Isis Lin <qweb32114@gmail.com>
2023-02-01 14:27:04 +0800

    feat: add imgur api

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ const path = require('path')
 const { getUser } = require('./helpers/auth-helpers')
 const handlebarsHelpers = require('./helpers/handlebars-helpers')
 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const app = express()
 const port = process.env.PORT || 3000
 const db = require('./models')
