28b675fa1db7aa7aa84d28b0cd927d50d4d7993a
Isis Lin <qweb32114@gmail.com>
2023-02-01 15:03:30 +0800

    feat: add category model

--- seeders/20230131093332-restaurants-seed-file.js
+++ seeders/20230131093332-restaurants-seed-file.js
@@ -18,6 +18,6 @@ module.exports = {
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkDelete('Restaurant', {})
+    await queryInterface.bulkDelete('Restaurants', {})
   }
 }
