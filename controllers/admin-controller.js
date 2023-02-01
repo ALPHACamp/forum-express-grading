9b40fb43c056e17ffd89b54b1348cf1c35e0d289
wenliangsu <wenliangsu65018@gmail.com>
2023-02-02 00:52:47 +0800

    Feat: add categories selector on admin create and edit page

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -3,7 +3,7 @@ const { Restaurant, User, Category } = require('../models');
 const { imgurFileHandler } = require('../helpers/file-helpers');
 
 const adminController = {
-  // the object
+  // TODO  Read all restaurants
   getRestaurants: (req, res, next) => {
     //  note raw 是讓資料呈現簡化為單純的JS物件，若是不加的話，則會是原生Sequelize的instance，除非需要對後續取得的資料操作，才不加。
     // note nest 因取用兩個model，所以未加入的話則會變成'Category.name': 'XXX', 加入後可以變成Category: {xx:xx, xx:xx}，較方便操作
@@ -13,21 +13,25 @@ const adminController = {
       include: [Category] // note 帶入另一個model
     })
       .then(restaurants => {
-        // console.log(restaurants);
         res.render('admin/restaurants', { restaurants });
       })
       .catch(err => next(err));
   },
-  createRestaurant: (req, res) => {
-    return res.render('admin/create-restaurant');
+  // TODO Create the new restaurant data
+  createRestaurant: (req, res, next) => {
+    return Category.findAll({
+      raw: true
+    })
+      .then(categories => res.render('admin/create-restaurant', { categories }))
+      .catch(err => next(err))
   },
   postRestaurant: (req, res, next) => {
     // !! note 雖然前端有用required來驗證，但是容易被修改，所以要在後端做一次驗證，避免被直接修改或侵入
-    const { name, tel, address, openingHours, description } = req.body;
+    const { name, tel, address, openingHours, description, categoryId } = req.body;
     if (!name) throw new Error('Restaurant name is required');
 
     const file = req.file;
-    console.log(file);
+
     imgurFileHandler(file) // 先經multer處理後再給下面繼續
       .then(filePath => {
         return Restaurant.create({
@@ -36,7 +40,8 @@ const adminController = {
           address,
           openingHours,
           description,
-          image: filePath || null
+          image: filePath || null,
+          categoryId
         });
       })
       .then(() => {
@@ -45,6 +50,7 @@ const adminController = {
       })
       .catch(err => next(err));
   },
+  // TODO Edit and Update the restaurant data
   getRestaurant: (req, res, next) => {
     // note 單筆資料操作可以不加入
     Restaurant.findByPk(req.params.id, { raw: true, nest: true, include: [Category] })
@@ -57,21 +63,24 @@ const adminController = {
       .catch(err => next(err));
   },
   editRestaurant: (req, res, next) => {
-    Restaurant.findByPk(req.params.id, { raw: true })
-      .then(restaurant => {
+    // notice 注意parameter數量，Promise.all([a, b]).then([a, b])
+    return Promise.all([
+      Restaurant.findByPk(req.params.id, { raw: true }),
+      Category.findAll({ raw: true })
+    ])
+      .then(([restaurant, categories]) => {
         if (!restaurant) throw new Error("Restaurant didn't exist");
 
-        res.render('admin/edit-restaurant', { restaurant });
+        res.render('admin/edit-restaurant', { restaurant, categories });
       })
       .catch(err => next(err));
   },
   putRestaurant: (req, res, next) => {
-    const { name, tel, address, openingHours, description } = req.body;
+    const { name, tel, address, openingHours, description, categoryId } = req.body;
     if (!name) throw new Error('Restaurant name is required');
 
     const { file } = req;
     // Thinking 需要找到餐廳資料，在處理相片的檔案，會有時間差為非同步，需要做同步處理
-    // notice 注意parameter數量，Promise.all([a, b]).then([a, b])
     Promise.all([
       Restaurant.findByPk(req.params.id), // note 不使用raw是因為還需要利用Sequelize的instance進行資料操作，若是轉成JS則無法使用update()
       imgurFileHandler(file)
@@ -86,7 +95,8 @@ const adminController = {
           address,
           openingHours,
           description,
-          image: filePath || restaurant.image
+          image: filePath || restaurant.image,
+          categoryId
         });
       })
       .then(() => {
@@ -95,6 +105,7 @@ const adminController = {
       })
       .catch(err => next(err));
   },
+  // TODO Delete the restaurant data
   deleteRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id)
       .then(restaurant => {
