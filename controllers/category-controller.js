9f8ad9a27ca779da1a956a4396da6cd9ccb6436c
Isis Lin <qweb32114@gmail.com>
2023-02-02 13:05:52 +0800

    refactor: combine create and edit category in the same route

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -2,10 +2,11 @@ const { Category } = require('../models')
 
 const categoryController = {
   getCategories: (req, res, next) => {
-    Category.findAll({
-      raw: true
-    })
-      .then(categories => res.render('admin/categories', { categories }))
+    return Promise.all([
+      Category.findAll({ raw: true }),
+      req.params.id ? Category.findByPk(req.params.id, { raw: true }) : null
+    ])
+      .then(([categories, category]) => res.render('admin/categories', { categories, category }))
       .catch(err => next(err))
   },
   postCategories: (req, res, next) => {
@@ -16,16 +17,6 @@ const categoryController = {
       name
     })
       .then(() => res.redirect('/admin/categories'))
-      .catch(err => next(err))
-  },
-  editCategory: (req, res, next) => {
-    Promise.all([
-      Category.findAll({ raw: true }),
-      Category.findByPk(req.params.id, { raw: true })
-    ])
-      .then(([categories, category]) => {
-        return res.render('admin/edit-category', { categories, category })
-      })
       .catch(err => next(err))
   },
   putCategory: (req, res, next) => {
@@ -43,6 +34,7 @@ const categoryController = {
         req.flash('success_messages', 'Category was successfully to update')
         res.redirect('/admin/categories')
       })
+      .catch(err => next(err))
   },
   deleteCategory: (req, res, next) => {
     return Category.findByPk(req.params.id)
