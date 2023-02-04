56490dd780b7726d526d4dceb4fce3abec11f1d6
WilliamChen15 <turelight13@gmail.com>
2023-02-05 02:52:24 +0800

    feat: passport init & signin

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 
 const router = express.Router()
 
+const passport = require('../config/passport')
+
 // 新增，載入 controller
 
 const restController = require('../controllers/restaurant-controller')
@@ -17,6 +19,10 @@ router.use('/admin', admin)
 router.get('/signup', userController.signUpPage)
 
 router.post('/signup', userController.signUp)
+
+router.get('/signin', userController.signInPage)
+router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
+router.get('/logout', userController.logout)
 
 router.get('/restaurants', restController.getRestaurants)
 
