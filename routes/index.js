0717031c9cb4d3c41f79cbc582881fd68585ef04
Isis Lin <qweb32114@gmail.com>
2023-02-04 16:25:26 +0800

    feat: addFollowing & removeFollowing function

--- routes/index.js
+++ routes/index.js
@@ -29,6 +29,9 @@ router.delete('/favorite/:restaurantId', authenticated, userController.removeFav
 router.post('/like/:restaurantId', authenticated, userController.addLike)
 router.delete('/like/:restaurantId', authenticated, userController.removeLike)
 
+router.post('/following/:userId', authenticated, userController.addFollowing)
+router.delete('/following/:userId', authenticated, userController.removeFollowing)
+
 router.delete('/comments/:id', authenticated, commentController.deleteComment)
 router.post('/comments', authenticated, commentController.postComment)
 
