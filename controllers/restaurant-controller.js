e233fa456d32208737a01ebf7ca8baa8b130b419
wenliangsu <wenliangsu65018@gmail.com>
2023-02-04 10:43:48 +0800

    Feat: add categories navigator on restaurants index page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -3,20 +3,32 @@
 const { Restaurant, Category } = require('../models')
 
 const restaurantController = {
-  getRestaurants: (req, res) => {
-    return Restaurant.findAll({
-      raw: true,
-      nest: true,
-      include: [Category]
-    }).then(restaurants => {
-      const data = restaurants.map(r => ({
-        // note spread operator可以把重複的項目已後面的為基準取代前面的，所以description變成所要的50個字以內
-        ...r,
-        description: r.description.substring(0, 50)
-      }))
+  getRestaurants: (req, res, next) => {
+    const categoryId = Number(req.query.categoryId) || ''
 
-      return res.render('restaurants', { restaurants: data })
-    })
+    // Thinking 使用where時，因前面有ifCond做條件判斷，所以用三元運算子來進行，而多加個spread operator則是若傳進來的為物件，則可以個別使用。
+    return Promise.all([
+      Restaurant.findAll({
+        raw: true,
+        nest: true,
+        include: [Category],
+        where: {
+          // notice where這個做法可以在後面多加好幾個條件，以便於未來擴充查詢條件
+          ...categoryId ? { categoryId } : {}
+        }
+      }),
+      Category.findAll({ raw: true })
+    ])
+      .then(([restaurants, categories]) => {
+        const data = restaurants.map(r => ({
+          // note spread operator可以把重複的項目已後面的為基準取代前面的，所以description變成所要的50個字以內
+          ...r,
+          description: r.description.substring(0, 50)
+        }))
+
+        return res.render('restaurants', { restaurants: data, categories, categoryId })
+      })
+      .catch(err => next(err))
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
