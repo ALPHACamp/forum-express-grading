3d12c1012e107185f7e172d377642552cc36ed2e
WilliamChen15 <turelight13@gmail.com>
2023-02-04 16:43:50 +0800

    feat: user signup

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,15 @@ const router = express.Router()
 
 const restController = require('../controllers/restaurant-controller')
 
+const userController = require('../controllers/user-controller')
+
 const admin = require('./modules/admin')
 
 router.use('/admin', admin)
+
+router.get('/signup', userController.signUpPage)
+
+router.post('/signup', userController.signUp)
 
 router.get('/restaurants', restController.getRestaurants)
 
