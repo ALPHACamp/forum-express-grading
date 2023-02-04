5ce3ba4a537ccff8c79c479ff62c6049d0decc98
Isis Lin <qweb32114@gmail.com>
2023-02-04 15:32:17 +0800

    feat: add topUser page

--- routes/models/user.js
+++ routes/models/user.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const userController = require('../../controllers/user-controller')
 const upload = require('../../middleware/multer')
 
+router.get('/top', userController.getTopUsers)
 router.get('/:id/edit', userController.editUser)
 router.put('/:id', upload.single('image'), userController.putUser)
 router.get('/:id', userController.getUser)
