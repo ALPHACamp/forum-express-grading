7eabe8686cf7c6b82a1ca5a78e22b1ea767bf886
Isis Lin <qweb32114@gmail.com>
2023-02-04 12:08:46 +0800

    feat: add favorite/unfavorite button at index page

--- models/favorite.js
+++ models/favorite.js
@@ -19,7 +19,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'Favorite',
-    tableName: 'Favorities',
+    tableName: 'Favorites',
     underscored: true
   })
   return Favorite
