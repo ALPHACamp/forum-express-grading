fa0906ccdd827eebd35cbc4860df627bbefc78b2
WilliamChen15 <turelight13@gmail.com>
2023-02-06 00:13:40 +0800

    feat: user model add is_admin

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,8 @@ const restController = require('../controllers/restaurant-controller')
 
 const userController = require('../controllers/user-controller')
 
+const { authenticated } = require('../middleware/auth')
+
 const { generalErrorHandler } = require('../middleware/error-handler')
 
 const admin = require('./modules/admin')
@@ -24,7 +26,7 @@ router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
 router.get('/logout', userController.logout)
 
-router.get('/restaurants', restController.getRestaurants)
+router.get('/restaurants', authenticated, restController.getRestaurants)
 
 router.use('/', (req, res) => res.redirect('/restaurants'))
 
