a908fe1557cd567487fc55fcd0742870a6b7f893
Isis Lin <qweb32114@gmail.com>
2023-02-05 13:21:43 +0800

    feat: api signin add jwt token

---
+++ controllers/apis/user-controller.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken')
+const userController = {
+  signIn: (req, res, next) => {
+    try {
+      const userData = req.user.toJSON() // 前面因為要使用token驗證，把session設定改為false，在passport設定中不會進入反序列化
+      delete userData.password // 刪除敏感資料
+      const token = jwt.sign(userData, process.env.JWT_SECRET, { expiresIn: '30d' }) // 簽發 JWT，效期為 30 天
+      res.json({
+        status: 'success',
+        data: {
+          token,
+          user: userData
+        }
+      })
+    } catch (err) {
+      next(err)
+    }
+  }
+}
+module.exports = userController
--- routes/apis/index.js
+++ routes/apis/index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const router = express.Router()
+const passport = require('../../config/passport')
 const { apiErrorHandler } = require('../../middleware/error-handler')
 const restController = require('../../controllers/apis/restaurant-controller')
+const userController = require('../../controllers/apis/user-controller')
 const admin = require('./modules/admin')
 
 router.use('/admin', admin)
 router.get('/restaurants', restController.getRestaurants)
+router.post('/signin', passport.authenticate('local', { session: false }), userController.signIn)
 
 router.use('/', apiErrorHandler)
 
