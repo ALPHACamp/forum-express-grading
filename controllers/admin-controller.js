c1a90b7f6c62572c15ac615c0325a65225774381
yichennn <yichennn1010@gmail.com>
2023-02-21 00:16:09 +0800

    feat: update restaurants page

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,8 +1,13 @@
 // 後台專用
+const { sequelize } = require('../models')
+const { QueryTypes } = require('sequelize')
+// const { Restaurant } = require('../models')
+
 const adminController = {
-  getRestaurants: (req, res) => {
-    res.render('admin/restaurants')
+  getRestaurants: (req, res, next) => {
+    sequelize.query('SELECT id, name FROM restaurants', { type: QueryTypes.SELECT })
+      .then(restaurants => res.render('admin/restaurants', { restaurants }))
+      .catch(err => next(err))
   }
 }
-
 module.exports = adminController
