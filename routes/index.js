54cc23923e74147ac44b0eccd3836f60024f18c6
WilliamChen15 <turelight13@gmail.com>
2023-02-24 01:50:57 +0800

    feat: modify admin restaurants page

--- routes/index.js
+++ routes/index.js
@@ -23,7 +23,7 @@ router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
 
 router.get('/signin', userController.signInPage)
-router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
+router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', badRequestMessage: '信箱及密碼皆不得為空', failureFlash: true }), userController.signIn)
 router.get('/logout', userController.logout)
 
 router.get('/restaurants', authenticated, restController.getRestaurants)
