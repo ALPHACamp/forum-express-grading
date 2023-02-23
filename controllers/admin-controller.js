7b458c253672e1a004022cb7bfffb19b3396548a
sd880428 <sd660428@gmail.com>
2023-02-24 02:15:17 +0800

    feat: set user permissions

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,4 +1,4 @@
-const { Restaurant } = require('../models')
+const { Restaurant, User } = require('../models')
 const { imgurFileHandler } = require('../helpers/file-helpers')
 const adminController = {
   getRestaurants: (req, res, next) => {
@@ -62,7 +62,7 @@ const adminController = {
       })
       .catch(err => next(err))
   },
-  deleteRestaurant: async (req, res, next) => {
+  deleteRestaurant: async (req, res, next) => { // async await練習
     try {
       const restaurant = await Restaurant.findByPk(req.params.id)
       if (!restaurant) throw new Error("Restaurant didn't exist!")
@@ -71,6 +71,29 @@ const adminController = {
     } catch (err) {
       next(err)
     }
+  },
+  getUsers: (req, res, next) => {
+    return User.findAll({ raw: true })
+      .then(users => {
+        res.render('admin/users', { users })
+      })
+      .catch(err => next(err))
+  },
+  patchUser: (req, res, next) => {
+    return User.findByPk(req.params.id)
+      .then(users => {
+        if (users.email === 'root@example.com') {
+          req.flash('error_messages', '禁止變更 root 權限')
+          return res.redirect('back')
+        }
+        const isAdmin = !users.isAdmin
+        return users.update({ isAdmin })
+      })
+      .then(() => {
+        req.flash('success_messages', '使用者權限變更成功')
+        res.redirect('/admin/users')
+      })
+      .catch(err => next(err))
   }
 }
 module.exports = adminController
