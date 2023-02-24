e42e82f1e6150b9ebc1adfc180a555da591bcbf1
WilliamChen15 <turelight13@gmail.com>
2023-02-24 12:14:36 +0800

    feat: create restaurant

--- routes/index.js
+++ routes/index.js
@@ -10,13 +10,13 @@ const restController = require('../controllers/restaurant-controller')
 
 const userController = require('../controllers/user-controller')
 
-const { authenticated } = require('../middleware/auth')
+const { authenticated, authenticatedAdmin } = require('../middleware/auth')
 
 const { generalErrorHandler } = require('../middleware/error-handler')
 
 const admin = require('./modules/admin')
 
-router.use('/admin', admin)
+router.use('/admin', authenticatedAdmin, admin)
 
 router.get('/signup', userController.signUpPage)
 
