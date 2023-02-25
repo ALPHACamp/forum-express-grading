da2cb0e30a1d0617552e894ae531f0177abb2b7a
freeway26tw <freeway26tw@gmail.com>
2023-02-25 20:16:20 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 const express = require('express')
+const handlebars = require('express-handlebars')
 const routes = require('./routes')
 
 const app = express()
 const port = process.env.PORT || 3000
+
+// 註冊 Handlebars engine，副檔名設定hbs
+app.engine('hbs', handlebars({ extname: 'hbs'}))
+// 設定使用 Handlebars作為template engine
+app.set('view engine', 'hbs')
 
 app.use(routes)
 
