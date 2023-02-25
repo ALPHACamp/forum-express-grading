93968e844db54707c9b89e067b9339e82e5c9e1f
ray199229 <ray199229@gmail.com>
2023-02-25 21:44:39 +0800

    feat: add admin home page and route

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,10 @@ const express = require('express')
 const router = express.Router()
 
 const restController = require('../controllers/restaurant-controller')
+const admin = require('./modules/admin')
 
+router.use('/admin', admin)
 router.get('/restaurants', restController.getRestaurants)
-
-router.get('/', (req, res) => {
-  res.redirect('/restaurants')
-})
+router.use('/', (req, res) => res.redirect('/restaurants'))
 
 module.exports = router
