f019856ea5b714d1dec93048d8a785a6d583b16c
thk61159 <tina0757023.bt07@nycu.edu.tw>
2023-02-27 04:31:04 +0800

    feat: change viewCount column setting

--- migrations/20230131043438-add-view-Counts-to-restaurant.js
+++ migrations/20230131043438-add-view-Counts-to-restaurant.js
@@ -4,7 +4,7 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.addColumn('Restaurants', 'view_Counts', {
       type: Sequelize.INTEGER,
-      allowNull: false
+      defaultValue: 0
     })
   },
   down: async (queryInterface, Sequelize) => {
