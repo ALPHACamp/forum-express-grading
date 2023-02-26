b4c78f53da791105f1652e900d55737879664e0f
Noelle-KH <noelletang86@gmail.com>
2023-02-26 17:01:46 +0800

    feat: flash message and sign up verification

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const express = require('express')
 const handlebars = require('express-handlebars')
+const session = require('express-session')
+const flash = require('connect-flash')
 
 const routes = require('./routes')
 const app = express()
@@ -9,6 +15,17 @@ app.engine('hbs', handlebars({ extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(express.urlencoded({ extended: true }))
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 app.use(routes)
 
 app.listen(port, () => {
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -6,15 +6,26 @@ const userController = {
   signUpPage: (req, res) => {
     return res.render('signup')
   },
-  signUp: (req, res) => {
-    return bcrypt.hash(req.body.password, 10)
+  signUp: (req, res, next) => {
+    if (!req.body.name || !req.body.email || !req.body.password || !req.body.passwordCheck) throw new Error('All field is required.')
+
+    if (req.body.password !== req.body.passwordCheck) throw new Error('Passwords do not match.')
+
+    return User.findOne({ where: { email: req.body.email } })
+      .then(user => {
+        if (user) throw new Error('Email already exists.')
+        return bcrypt.hash(req.body.password, 10)
+      })
       .then(hash => User.create({
         name: req.body.name,
         email: req.body.email,
         password: hash
       }))
-      .then(() => res.redirect('/signin'))
-      .catch(error => console.log(error))
+      .then(() => {
+        req.flash('success_message', 'Registered successfully')
+        return res.redirect('/signin')
+      })
+      .catch(error => next(error))
   }
 }
 
