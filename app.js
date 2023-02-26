c5a1c76fddea7facfdd8c8e0c4d12fb329bcb6ff
ray199229 <ray199229@gmail.com>
2023-02-26 16:13:44 +0800

    feat: user signup

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const port = process.env.PORT || 3000
 app.engine('hbs', handlebars({ extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
+app.use(express.urlencoded({ extended: true }))
+
 app.use(routes)
 
 app.listen(port, () => {
