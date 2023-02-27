2bb867077f0c71692881d7d9514d5952d97dcf2c
freeway26tw <freeway26tw@gmail.com>
2023-02-27 17:29:25 +0800

    feat: add header & footer

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const handlebars = require('express-handlebars')
 const flash = require('connect-flash')
 const session = require('express-session')
 const passport = require('./config/passport')
+const handlebarsHelpers = require('./helpers/handlebars-helpers')
 
+const { getUser } = require('./helpers/auth-helpers')
 const routes = require('./routes')
 
 const app = express()
@@ -12,7 +14,7 @@ const port = process.env.PORT || 3000
 const SESSION_SECRET = 'secret'
 
 // 註冊 Handlebars engine，副檔名設定hbs
-app.engine('hbs', handlebars({ extname: 'hbs' }))
+app.engine('hbs', handlebars({ extname: 'hbs', helpers: handlebarsHelpers }))
 // 設定使用 Handlebars作為template engine
 app.set('view engine', 'hbs')
 
@@ -24,6 +26,7 @@ app.use(flash())
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
   res.locals.error_messages = req.flash('error_messages')
+  res.locals.user = getUser(req)
   next()
 })
 app.use(routes)
