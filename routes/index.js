cb7666e35fe88c969a2123779bad2bb4df4f22b0
Lilynews <lilinews2021@gmail.com>
2023-02-27 12:05:10 -0500

    feat: add user model is_admin & add auth

--- routes/index.js
+++ routes/index.js
@@ -7,11 +7,12 @@ const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
 // error msg
 const { generalErrorHandler } = require('../middleware/error-handler')
+const { authenticated } = require('../middleware/auth')
 
 router.use('/admin', admin)
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
-router.get('/restaurants', restController.getRestaurants)
+router.get('/restaurants', authenticated, restController.getRestaurants)
 router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
 router.get('/logout', userController.logout)
