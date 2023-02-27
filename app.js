aeddb0be62a8971a4424b652bc760bc0f1b47b16
chikunmark <chikunmark@gmail.com>
2023-02-27 23:29:18 +0800

    feat: add restaurants route

--- app.js
+++ app.js
@@ -11,14 +11,8 @@ app.set('view engine', 'hbs')
 app.use(routes)
 
 app.listen(port, () => {
-  console.info(`Example app listening on port ${port}!`)
+  console.info(`Example app listening on port http://localhost:${port}`)
 })
 
 // 導入自動化測試以後，由於測試環境會用到 app，所以需要在文件最下方輸出 app
 module.exports = app
-
-console.log('aaa')
-console.info('aaa')
-console.error('aaa')
-console.warn('aaa')
-console.debug('aaa')
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const restController = require('../controllers/restaurant-controller')
+
+//! 之後來試試調整上下順序 (教案說只要符合條件就會停，我以前試的狀況好像要 "完全符合")
+// router.get('/', (req, res) => res.send('I am so happy'))
+router.get('/restaurants', restController.getRestaurants)
+// router.get('/', (req, res) => res.redirect('/restaurants'))
+router.use('/', (req, res) => res.redirect('/restaurants')) //! 教案說要改成這樣，我先試試上面的
 
 module.exports = router
