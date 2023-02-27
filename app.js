c9bd445dd12f745ddb569a0da481706464224f0b
chikunmark <chikunmark@gmail.com>
2023-02-27 16:13:56 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const handlebars = require('express-handlebars')
 const routes = require('./routes')
 
 const app = express()
 const port = process.env.PORT || 3000
+
+app.engine('hbs', handlebars({ extname: '.hbs' }))
+app.set('view engine', 'hbs')
 
 app.use(routes)
 
@@ -10,4 +14,11 @@ app.listen(port, () => {
   console.info(`Example app listening on port ${port}!`)
 })
 
+// 導入自動化測試以後，由於測試環境會用到 app，所以需要在文件最下方輸出 app
 module.exports = app
+
+console.log('aaa')
+console.info('aaa')
+console.error('aaa')
+console.warn('aaa')
+console.debug('aaa')
