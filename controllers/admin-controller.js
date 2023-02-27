75bd97fbe64ba425fe8c3d93f88c38a324ad3ffd
FrankHuang <frankcool321@gmail.com>
2023-02-27 16:10:52 +0800

    feat: add users tab & add function of  management on user's authorization

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,5 +1,5 @@
 // - 處理屬於restaurant路由的相關請求
-const { Restaurant } = require('../models')
+const { Restaurant, User } = require('../models')
 const { imgurFileHandler } = require('../helpers/file-helpers')
 const adminController = {
   getRestaurants: async (req, res, next) => {
@@ -28,7 +28,7 @@ const adminController = {
         description,
         image: filePath || null
       })
-      req.flash('success_msg', '成功建立新餐廳!')
+      req.flash('success_messages', '成功建立新餐廳!')
       return res.redirect('/admin/restaurants')
     } catch (error) {
       return next(error)
@@ -76,7 +76,7 @@ const adminController = {
         // - 否則就用原先DB的image
         image: filePath || restaurant.image
       })
-      req.flash('success_msg', '成功更新餐廳資料!')
+      req.flash('success_messages', '成功更新餐廳資料!')
       return res.redirect('/admin/restaurants')
     } catch (error) {
       return next(error)
@@ -89,6 +89,39 @@ const adminController = {
       if (!restaurant) return new Error('此餐廳不存在!')
       await restaurant.destroy()
       return res.redirect('/admin/restaurants')
+    } catch (error) {
+      return next(error)
+    }
+  },
+  getUsers: async (req, res, next) => {
+    try {
+      const users = await User.findAll({ raw: true })
+      return res.render('admin/users', { users })
+    } catch (error) {
+      return next(error)
+    }
+  },
+  patchUser: async (req, res, next) => {
+    const { id } = req.params
+    try {
+      const foundUser = await User.findByPk(id)
+      if (!foundUser) return new Error('此用戶不存在')
+      if (foundUser.email === 'root@example.com') {
+        req.flash('error_messages', '禁止變更 root 權限')
+        return res.redirect('back')
+      }
+      // -切換一般管理員與使用者權限
+      if (foundUser.isAdmin) {
+        await foundUser.update({
+          isAdmin: false
+        })
+      } else {
+        await foundUser.update({
+          isAdmin: true
+        })
+      }
+      req.flash('success_messages', '使用者權限變更成功')
+      return res.redirect('/admin/users')
     } catch (error) {
       return next(error)
     }
--- middleware/auth.js
+++ middleware/auth.js
@@ -2,7 +2,7 @@ const helpers = require('../helpers/auth-helpers')
 //! 定義驗證一般使用者與管理員的認證middleware
 const authenticated = (req, res, next) => {
   if (helpers.ensureAuthenticated(req)) return next()
-  req.flash('warning_msg', '需先登入才能使用!')
+  req.flash('warning_messages', '需先登入才能使用!')
   return res.redirect('/signin')
 }
 const authenticatedAdmin = (req, res, next) => {
@@ -10,7 +10,7 @@ const authenticatedAdmin = (req, res, next) => {
     if (helpers.getUser(req).isAdmin) return next() //! 若為管理員則繼續往後走
     return res.redirect('/restaurants')
   }
-  req.flash('warning_msg', '需先登入才能使用!')
+  req.flash('warning_messages', '需先登入才能使用!')
   return res.redirect('/signin')
 }
 
