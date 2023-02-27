7bc7bbff3c08563e6ea840e12f4ca62769d996e0
vdiefn <ddhkxxhxx@hotmail.com>
2023-02-27 14:25:41 +0800

    feat: add ifCond hbs helper & update category selector

--- helpers/handlerbars-helper.js
+++ helpers/handlerbars-helper.js
@@ -1,5 +1,8 @@
 const dayjs = require('dayjs')
 module.exports = {
   // 取得當年年份作為 currentYear 的屬性值，並導出
-  currentYear: () => dayjs().year()
+  currentYear: () => dayjs().year(),
+  ifCond: function (a, b, options) {
+    return a === b ? options.fn(this) : options.inverse(this)
+  }
 }
