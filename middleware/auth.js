b081d9158604f1f57cbd2103c38e2a67fa3dd805
FrankHuang <frankcool321@gmail.com>
2023-02-27 14:11:50 +0800

    feat: adjust middleware/auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,13 +1,13 @@
-const { getUser, ensureAuthenticated } = require('../helpers/auth-helpers')
+const helpers = require('../helpers/auth-helpers')
 //! 定義驗證一般使用者與管理員的認證middleware
 const authenticated = (req, res, next) => {
-  if (ensureAuthenticated(req)) return next()
+  if (helpers.ensureAuthenticated(req)) return next()
   req.flash('warning_msg', '需先登入才能使用!')
   return res.redirect('/signin')
 }
 const authenticatedAdmin = (req, res, next) => {
-  if (ensureAuthenticated(req)) {
-    if (getUser(req).isAdmin) return next() //! 若為管理員則繼續往後走
+  if (helpers.ensureAuthenticated(req)) {
+    if (helpers.getUser(req).isAdmin) return next() //! 若為管理員則繼續往後走
     return res.redirect('/restaurants')
   }
   req.flash('warning_msg', '需先登入才能使用!')
