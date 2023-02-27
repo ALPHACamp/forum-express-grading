28e6f4dedc358efe42f8e65af7eb23ef9cafe889
vdiefn <ddhkxxhxx@hotmail.com>
2023-02-27 16:36:54 +0800

    feat:category update

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -11,10 +11,15 @@ router.put('/restaurants/:id', upload.single('image'), adminController.putRestau
 router.delete('/restaurants/:id', adminController.deleteRestaurant)
 router.post('/restaurants', upload.single('image'), adminController.postRestaurant)
 router.get('/restaurants', adminController.getRestaurants)
+
 router.patch('/users/:id', adminController.patchUser)
 router.get('/users', adminController.getUsers)
+
+router.get('/categories/:id', categoryController.getCategories)
+router.put('/categories/:id', categoryController.putCategory)
 router.get('/categories', categoryController.getCategories)
 router.post('/categories', categoryController.postCategory)
+
 router.use('/', (req, res) => res.redirect('/admin/restaurants'))
 
 module.exports = router
