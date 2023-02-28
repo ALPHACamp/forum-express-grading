63a0c8a1e7e77a646d7ef018341a1a05d5759fdd
Leon180 <lioung860302@gmail.com>
2023-02-28 23:28:14 +0800

    feat create restaurant

--- controllers/admin-contoller.js
+++ controllers/admin-contoller.js
@@ -10,6 +10,26 @@ const adminController = {
     } catch (err) {
       next(err)
     }
+  },
+  createRestaurant: (req, res) => {
+    return res.render('admin/create-restaurant')
+  },
+  postRestaurant: async (req, res, next) => {
+    try {
+      const { name, tel, address, openingHours, description } = req.body
+      if (!name) throw new Error('Restaurant name is required!')
+      await Restaurant.create({
+        name,
+        tel,
+        address,
+        openingHours,
+        description
+      })
+      req.flash('success_messages', 'restaurant was successfully created')
+      res.redirect('/admin/restaurants')
+    } catch (err) {
+      next(err)
+    }
   }
 }
 
