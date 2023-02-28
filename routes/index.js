66fa7b5ba87c19a9667f6ef0beab2ef82d50f06d
chikunmark <chikunmark@gmail.com>
2023-02-28 20:53:11 +0800

    feat: add signup page

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,16 @@ const express = require('express')
 const router = express.Router()
 
 const restController = require('../controllers/restaurant-controller')
+const userController = require('../controllers/user-controller')
 const admin = require('./modules/admin')
 
 //! 之後來試試調整上下順序 (教案說只要符合條件就會停，我以前試的狀況好像要 "完全符合")
 // router.get('/', (req, res) => res.send('I am so happy'))
 router.use('/admin', admin)
+
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
+
 
 router.get('/restaurants', restController.getRestaurants)
 // router.get('/', (req, res) => res.redirect('/restaurants'))
