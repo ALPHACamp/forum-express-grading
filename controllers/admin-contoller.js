ec6218abacec76843e4d2a152647f8cc05dc919b
Leon180 <lioung860302@gmail.com>
2023-02-28 23:46:28 +0800

    feat: admin update restaurant

--- controllers/admin-contoller.js
+++ controllers/admin-contoller.js
@@ -41,6 +41,36 @@ const adminController = {
     } catch (err) {
       next(err)
     }
+  },
+  editRestaurant: async (req, res, next) => {
+    try {
+      const restaurant = await Restaurant.findByPk(req.params.id, {
+        raw: true
+      })
+      if (!restaurant) throw new Error("Restaurant didn't exist!")
+      res.render('admin/edit-restaurant', { restaurant })
+    } catch (err) {
+      next(err)
+    }
+  },
+  putRestaurant: async (req, res, next) => {
+    try {
+      const { name, tel, address, openingHours, description } = req.body
+      if (!name) throw new Error('Restaurant name is required!')
+      const restaurant = await Restaurant.findByPk(req.params.id)
+      if (!restaurant) throw new Error("Restaurant didn't exist!")
+      await restaurant.update({
+        name,
+        tel,
+        address,
+        openingHours,
+        description
+      })
+      req.flash('success_messages', 'restaurant was successfully to update')
+      res.redirect('/admin/restaurants')
+    } catch (err) {
+      next(err)
+    }
   }
 }
 
