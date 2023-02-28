4708828f5498c877d302265ff8431d987eacb740
frank <frankcool321@gmail.com>
2023-02-28 22:52:01 +0800

    feat: update seed files

--- seeders/20230226142442-restaurants-seed-file.js
+++ seeders/20230226142442-restaurants-seed-file.js
@@ -2,6 +2,10 @@
 const faker = require('faker')
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    const categories = await queryInterface.sequelize.query(
+      'SELECT id FROM Categories',
+      { type: queryInterface.sequelize.QueryTypes.SELECT }
+    ) // -傳入 type 物件，標明是 SELECT query，使回傳值不包含 metadata，為單純的 SELECT 結果集( an array of results )
     await queryInterface.bulkInsert(
       'Restaurants',
       Array.from({ length: 50 }, () => ({
@@ -14,7 +18,9 @@ module.exports = {
         }`,
         description: faker.lorem.text(),
         created_at: new Date(),
-        updated_at: new Date()
+        updated_at: new Date(),
+        category_id:
+          categories[Math.floor(Math.random() * categories.length)].id
       }))
     )
   },
