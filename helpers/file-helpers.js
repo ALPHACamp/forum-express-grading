6520cdd0a4ded11da50fbefd7f0d1439fa7ad77d
Noelle-KH <noelletang86@gmail.com>
2023-02-28 12:18:54 +0800

    feat: add imgur api

--- helpers/file-helpers.js
+++ helpers/file-helpers.js
@@ -1,4 +1,6 @@
 const fs = require('fs')
+const imgur = require('imgur')
+imgur.setClientId(process.env.IMGUR_CLIENT_ID)
 
 const localFileHandler = file => {
   return new Promise((resolve, reject) => {
@@ -11,4 +13,15 @@ const localFileHandler = file => {
   })
 }
 
-module.exports = { localFileHandler }
+const imgurFileHandler = file => {
+  return new Promise((resolve, reject) => {
+    if (!file) return resolve(null)
+    return imgur.uploadFile(file.path)
+      .then(img => {
+        resolve(img?.link || null)
+      })
+      .catch(error => reject(error))
+  })
+}
+
+module.exports = { localFileHandler, imgurFileHandler }
