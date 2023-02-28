e71d1865a89b2ae2f1197b83800afd5c546ea36a
sd880428 <sd660428@gmail.com>
2023-02-28 22:02:16 +0800

    feat: add ifCond hbs helper & update category selector

--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -1,5 +1,9 @@
 const dayjs = require('dayjs')
+// const handlebars = require('express-handlebars')
 
 module.exports = {
-  currentYear: () => dayjs().year()
+  currentYear: () => dayjs().year(),
+  ifCond: function (a, b, options) {
+    return a === b ? options.fn(this) : options.inverse(this)
+  }
 }
