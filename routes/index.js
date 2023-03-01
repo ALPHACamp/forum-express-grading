83d3c9b9d6d6652bba207ca09bf0198c921b9acb
Christal Chen <christalchen1209@gmail.com>
2023-03-01 21:53:20 +0800

    feat: flash msg & signup verification

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,8 @@ const router = express.Router()
 const restController = require('../controllers/restaurant-controller')
 const admin = require('./modules/admin')
 const userController = require('../controllers/user-controller')
+const { generalErrorHandler } = require('../middleware/error-handler')
+
 router.use('/admin', admin)
 
 router.get('/signup', userController.signUpPage)
@@ -12,4 +14,5 @@ router.get('/restaurants', restController.getRestaurants)
 
 router.use('/', (req, res) => res.redirect('/restaurants'))
 
+router.use('/', generalErrorHandler)
 module.exports = router
