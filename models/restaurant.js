7aa4c6cfb615043b3424b861628f53100def1113
cayang <cayang@simenvi.com.tw>
2023-03-01 10:31:17 +0800

    feat: add comment model

--- models/restaurant.js
+++ models/restaurant.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       Restaurant.belongsTo(models.Category, { foreignKey: 'categoryId' })
+      Restaurant.hasMany(models.Comment, { foreignKey: 'restaurantId' })
     }
   }
   Restaurant.init({
