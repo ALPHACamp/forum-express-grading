669096ea76fe05a9b164753524e778f66bb4ce44
chachagof <chacha0932@gmail.com>
2023-03-01 16:48:45 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express')
+const handlebars = require('express-handlebars')
 const routes = require('./routes')
 
 const app = express()
 const port = process.env.PORT || 3000
+
+app.engine('hbs', handlebars({ extname: '.hbs' }))
+app.use('view engine', 'hbs')
 
 app.use(routes)
 
