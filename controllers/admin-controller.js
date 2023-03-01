d9490b1fd2d54cffce569c44fb1a5d5394352822
onlyyou000u@gmail.com <onlyyou000u@gmail.com>
2023-03-01 23:39:28 +0800

    feat: delete restaurant

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -68,6 +68,17 @@ const adminController = {
         res.redirect('/admin/restaurants')
       })
       .catch(err => next(err))
+  },
+  deleteRestaurant: (req, res, next) => {
+    const { id } = req.params
+    return Restaurant.findByPk(id)
+      .then(restaurant => {
+        if (!restaurant) throw new Error('Restaurant did not exists!')
+
+        return restaurant.destroy()
+      })
+      .then(() => res.redirect('/admin/restaurants'))
+      .catch(err => next(err))
   }
 }
 
