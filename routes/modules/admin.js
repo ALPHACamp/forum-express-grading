7de64c78ef5c5f177004837a5064db0cb030d238
Shiba-Patrick <liu21407@gmail.com>
2023-03-02 19:26:38 +0800

    feat: admin restaurant page

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -4,8 +4,10 @@ const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
 
 router.get('/restaurants/create', adminController.createRestaurant)
+router.get('/restaurants/:id', adminController.getRestaurant)
 router.get('/restaurants', adminController.getRestaurants) // 導入登入狀態驗證
 router.post('/restaurants', adminController.postRestaurant)
+
 router.get('/', (req, res) => res.redirect('/admin/restaurants'))
 
 module.exports = router
