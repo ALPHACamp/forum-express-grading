6abb7bad0b8ff9a6ea55eb4e3d25ca51bc87f2c9
frank <frankcool321@gmail.com>
2023-03-02 20:08:08 +0800

    feat: add delete category function

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -43,7 +43,18 @@ const categoryController = {
       return next(error)
     }
   },
-  deleteCategory: async (req, res, next) => {}
+  deleteCategory: async (req, res, next) => {
+    const { id } = req.params
+    try {
+      const category = await Category.findByPk(id)
+      if (!category) throw new Error('類別不存在!')
+      await category.destroy()
+      req.flash('success_messages', '刪除類別成功!')
+      return res.redirect('/admin/categories')
+    } catch (error) {
+      return next(error)
+    }
+  }
 }
 
 module.exports = categoryController
