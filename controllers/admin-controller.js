3a6348f6bed4223467a19a00199373b1bed82864
ray199229 <ray199229@gmail.com>
2023-03-02 16:20:40 +0800

    feat: add getCategories page

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -122,6 +122,12 @@ const adminController = {
         res.redirect('/admin/users')
       })
       .catch(err => next(err))
+  },
+  getCategories: (req, res, next) => {
+    return Category.findAll({
+      raw: true
+    })
+      .then(categories => res.render('admin/categories', { categories }))
   }
 }
 module.exports = adminController
