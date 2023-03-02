6d890d7e3c6c831d320c8f0c8ed9fcf46572bdc6
vdiefn <ddhkxxhxx@hotmail.com>
2023-03-02 14:31:25 +0800

    feat:add feeds page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -46,6 +46,31 @@ const restaurantController = {
       res.render('restaurant', { restaurant: restaurant.toJSON() })
     })
       .catch(err => next(err))
+  },
+  getFeeds: (req, res, next) => {
+    return Promise.all([
+      Restaurant.findAll({
+        limit: 10,
+        order: [['createdAt', 'DESC']],
+        include: [Category],
+        raw: true,
+        nest: true
+      }),
+      Comment.findAll({
+        limit: 10,
+        order: [['createdAt', 'DESC']],
+        include: [User, Restaurant],
+        raw: true,
+        nest: true
+      })
+    ])
+      .then(([restaurants, comments]) => {
+        res.render('feeds', {
+          restaurants,
+          comments
+        })
+      })
+      .catch(err => next(err))
   }
 }
 module.exports = restaurantController
