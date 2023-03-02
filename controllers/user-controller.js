af01c6817981d3eaa88b342a3d09f4a38f99cae0
may huang <mayno1024@gmail.com>
2023-03-02 21:24:35 +0800

    feat: flash msg & signup verification

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -7,16 +7,24 @@ const UserController = {
   signUpPage: (req, res) => {
     res.render('signup')
   },
-  singUp: (req, res) => {
-    bcrypt.hash(req.body.password, 10)
+  singUp: (req, res, next) => {
+    if (req.body.password !== req.body.passwordCheck) throw new Error('Password do not match')
+
+    User.findOne({ where: { email: req.body.email } })
+      .then(user => {
+        if (user) throw new Error('Email already exists')
+        return bcrypt.hash(req.body.password, 10)
+      })
       .then(hash => User.create({
         name: req.body.name,
         email: req.body.email,
         password: hash
       }))
       .then(() => {
+        req.flash('success_messages', '成功註冊帳號')
         res.redirect('/singin')
       })
+      .catch(err => next(err))
   }
 }
 module.exports = UserController
