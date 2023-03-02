c40d36002ecf04fa3a7ea8f350cbf3a60a060d32
yy933 <emilyyu025@gmail.com>
2023-03-02 22:06:02 +0800

    feat: admin update restaurant

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -6,8 +6,9 @@ router.get(
   '/restaurants/create',
   adminController.createRestaurant
 )
+router.get("/restaurants/:id/edit", adminController.editRestaurant); 
 router.get('/restaurants/:id', adminController.getRestaurant)
 router.get('/restaurants', adminController.getRestaurants)
-router.post('/restaurants', adminController.postRestaurant)
+router.put('/restaurants/:id', adminController.putRestaurant)
 router.use('/', (req, res) => res.redirect('/admin/restaurants'))
 module.exports = router
