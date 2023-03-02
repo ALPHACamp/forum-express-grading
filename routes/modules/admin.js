db5b3f8c3f24ebaccb027c6a504a87b17dfb4b93
yy933 <emilyyu025@gmail.com>
2023-03-02 18:37:21 +0800

    feat: admin restaurant page

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -6,7 +6,8 @@ router.get(
   '/restaurants/create',
   adminController.createRestaurant
 )
+router.get('/restaurants/:id', adminController.getRestaurant)
 router.get('/restaurants', adminController.getRestaurants)
-router.post("/restaurants", adminController.postRestaurant);
+router.post('/restaurants', adminController.postRestaurant)
 router.use('/', (req, res) => res.redirect('/admin/restaurants'))
 module.exports = router
