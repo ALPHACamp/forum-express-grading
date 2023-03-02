3976e345aed1702f4cd8c9802363803391cc3f89
jjerry1225 <i183801225@gmail.com>
2023-03-02 15:49:54 +0800

    feat: add restaurant image

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,4 +1,5 @@
 const { Restaurant } = require('../models')
+const { localFileHandler } = require('../helpers/file-helpers')
 
 const adminController = {
   getRestaurants: (req, res, next) => {
@@ -15,13 +16,15 @@ const adminController = {
     const { name, tel, address, openingHours, description } = req.body
     if (!name) throw new Error('Restaurant name is required!')
 
-    Restaurant.create({
+    const { file } = req // 這裡是解構賦值，也可以寫成 const file = req.file
+    localFileHandler(file).then(filePath => Restaurant.create({
       name,
       tel,
       address,
       openingHours,
-      description
-    })
+      description,
+      image: filePath || null // 若有上傳圖片則存檔，沒有上傳圖片則為null。
+    }))
       .then(() => {
         req.flash('success_messages', 'restaurant was successfully created')
         res.redirect('/admin/restaurants')
@@ -54,15 +57,20 @@ const adminController = {
     const { name, tel, address, openingHours, description } = req.body
     if (!name) throw new Error('Restaurant name is required!')
 
-    Restaurant.findByPk(req.params.id)
-      .then(restaurant => {
+    const { file } = req
+    Promise.all([
+      Restaurant.findByPk(req.params.id),
+      localFileHandler(file)
+    ])
+      .then(([restaurant, filePath]) => {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
         return restaurant.update({
           name,
           tel,
           address,
           openingHours,
-          description
+          description,
+          image: filePath || restaurant.image // 如果 filePath 是 Truthy (使用者有上傳新照片) 就用 filePath，是 Falsy (使用者沒有上傳新照片) 就沿用原本資料庫內的值
         })
       })
       .then(() => {
