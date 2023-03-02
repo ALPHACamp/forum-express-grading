49f880f279c1379f7a7978fe2b42e0debd2e6670
freeway26tw <freeway26tw@gmail.com>
2023-03-02 14:37:36 +0800

    user admin change permission

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,4 +1,4 @@
-const { Restaurant } = require('../models')
+const { Restaurant, User } = require('../models')
 const { imgurFileHandler } = require('../helpers/file-helpers')
 const adminController = {
   getRestaurants: (req, res, next) => {
@@ -82,6 +82,25 @@ const adminController = {
         return restaurant.destroy()
       })
       .then(() => res.redirect('/admin/restaurants'))
+      .catch(err => next(err))
+  },
+  getUsers: (req, res, next) => {
+    return User.findAll({
+      raw: true
+    })
+      .then(users => res.render('admin/users', { users }))
+      .catch(err => next(err))
+  },
+  patchUser: (req, res, next) => {
+    return User.findByPk(req.params.id)
+      .then(user => {
+        if (user.dataValues.email === 'root@example.com') throw new Error('禁止變更 root 權限')
+        user.update({ isAdmin: !user.isAdmin })
+      })
+      .then(() => {
+        req.flash('success_messages', 'user authority was successfully to update')
+        res.redirect('/admin/users')
+      })
       .catch(err => next(err))
   }
 }
