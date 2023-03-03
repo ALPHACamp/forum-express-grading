8bd6cf3b85e8a399ec5d68b92206c37d4b001a62
chikunmark <chikunmark@gmail.com>
2023-03-03 09:43:29 +0800

    feat: add user permission management

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,9 +1,9 @@
-const { Restaurant } = require('../models')
+const { Restaurant, User } = require('../models')
 const { imgurFileHandler } = require('../helpers/file-helpers')
 
 const adminController = {
   getRestaurants: (req, res, next) => {
-    Restaurant.findAll({ raw: true })
+    return Restaurant.findAll({ raw: true })
     // (上1) 因為接下來沒有要對找到的資料進行 CRUD，所以用 {raw: true} 把他們轉成 JS object，準備渲染
       .then(restaurants => res.render('admin/restaurants', { restaurants }))
       .catch(err => next(err))
@@ -14,7 +14,7 @@ const adminController = {
     if (!name) throw new Error('Restaurant name is required!') // name 是必填，若發先是空值就會終止程式碼，並在畫面顯示錯誤提示
 
     const { file } = req // 圖片資料會存這，而非 req.body (multipart/form-data)
-    imgurFileHandler(file).then(filePath => Restaurant.create({
+    return imgurFileHandler(file).then(filePath => Restaurant.create({
       name,
       tel,
       address,
@@ -29,7 +29,7 @@ const adminController = {
       .catch(err => next(err))
   },
   getRestaurant: (req, res, next) => {
-    Restaurant.findByPk(req.params.id, { raw: true })
+    return Restaurant.findByPk(req.params.id, { raw: true })
       .then(restaurant => {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
         res.render('admin/restaurant', { restaurant })
@@ -37,10 +37,10 @@ const adminController = {
       .catch(err => next(err))
   },
   editRestaurant: (req, res, next) => {
-    Restaurant.findByPk(req.params.id, { raw: true })
+    return Restaurant.findByPk(req.params.id, { raw: true })
       .then(restaurant => {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
-        res.render('admin/edit-restaurant', { restaurant })
+        return res.render('admin/edit-restaurant', { restaurant })
       })
       .catch(err => next(err))
   },
@@ -59,7 +59,7 @@ const adminController = {
       })
       .then(() => {
         req.flash('success_messages', 'restaurant was successfully to update')
-        res.redirect('/admin/restaurants')
+        return res.redirect('/admin/restaurants')
       })
       .catch(err => next(err))
   },
@@ -69,7 +69,35 @@ const adminController = {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
         return restaurant.destroy()
       })
-      .then(() => res.redirect('admin/restaurants'))
+      .then(() => res.redirect('/admin/restaurants'))
+      .catch(err => next(err))
+  },
+  getUsers: (req, res, next) => {
+    return User.findAll({ raw: true })
+    // (上1) 因為接下來沒有要對找到的資料進行 CRUD，所以用 {raw: true} 把他們轉成 JS object，準備渲染
+      .then(users => res.render('admin/users', { users }))
+      .catch(err => next(err))
+  },
+  patchUser: (req, res, next) => {
+    // if (req.params.id === '1') throw new Error('禁止變更 root 權限')
+    // if (req.params.id === '1') {
+    //   req.flash('error_messages', '禁止變更 root 權限')
+    //   return res.redirect('back')
+    // }
+    return User.findByPk(req.params.id)
+      .then(user => {
+        if (!user) throw new Error("Restaurant didn't exist!")
+        if (user.email === 'root@example.com') {
+          req.flash('error_messages', '禁止變更 root 權限')
+          return res.redirect('back')
+        }
+        const isAdmin = !user.isAdmin // 改變權限 (布林值)
+        return user.update({ isAdmin })
+      })
+      .then(() => {
+        req.flash('success_messages', '使用者權限變更成功')
+        return res.redirect('/admin/users')
+      })
       .catch(err => next(err))
   }
 }
