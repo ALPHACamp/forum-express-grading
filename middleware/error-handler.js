0280c71675a9ef230a56cfb0c126b0872a770e77
chikunmark <chikunmark@gmail.com>
2023-03-03 09:25:42 +0800

    feat: add user permission management

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,8 +1,17 @@
 module.exports = {
   generalErrorHandler (err, req, res, next) {
     if (err instanceof Error) {
-      req.flash('error_messages', `${err.name}: ${err.message}`)
+      console.log('yyyyy顯示err')
+      console.log('yyyyy顯示err')
+      console.log('yyyyy顯示err')
+      console.log(err)
+      req.flash('error_messages', `${err.message}`)
+      // req.flash('error_messages', `${err.name}: ${err.message}`)
     } else {
+      console.log('顯示err')
+      console.log('顯示err')
+      console.log('顯示err')
+      console.log(err)
       req.flash('error_messages', `${err}`)
     }
     res.redirect('back') // 丟回前一頁
