b39b6c0082567ab22042e4f2fa6fc9b2b9cda449
Ian_Lin <ian920511@gmail.com>
2023-03-03 15:31:20 +0800

    feat:admin restaurant page and admin update restaurant

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -30,6 +30,53 @@ const adminController = {
         res.redirect('/admin/restaurants')
       })
       .catch(error => next(error))
+  },
+
+  getRestaurant: (req, res, next) => {
+    Restaurant.findByPk(req.params.id, {
+      raw: true
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+
+        res.render('admin/restaurant', { restaurant })
+      })
+      .catch(error => next(error))
+  },
+
+  editRestaurant: (req, res, next) => {
+    Restaurant.findByPk(req.params.id, {
+      raw: true
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+
+        res.render('admin/edit-restaurant', { restaurant })
+      })
+  },
+
+  putRestaurant: (req, res, next) => {
+    const { name, tel, address, openingHours, description } = req.body
+
+    if (!name) throw new Error('Restaurant name is required')
+
+    Restaurant.findByPk(req.params.id)
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+
+        return restaurant.update({
+          name,
+          tel,
+          address,
+          openingHours,
+          description
+        })
+      })
+      .then(() => {
+        req.flash('success_messages', 'restaurant was successfully to update')
+        res.redirect('/admin/restaurants')
+      })
+      .catch(error => next(error))
   }
 }
 
