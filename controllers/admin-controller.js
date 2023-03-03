bd6c0195ddf57139ac3cc9a98a0961197e16e699
Ellen <ellen31725@gmail.com>
2023-03-03 18:58:51 +0800

    Squashed commit of the following:
    commit 511cbbac7781cce755b8309eb5f857a740d87a18
    Author: Ellen <ellen31725@gmail.com>
    Date:   Fri Mar 3 18:00:57 2023 +0800
    
        users authorization Management
    
    commit e3e31c212a1a9197649990e849d229d87e162fc2
    Author: Ellen <ellen31725@gmail.com>
    Date:   Fri Mar 3 17:57:47 2023 +0800
    
        add search function of restaurnats
    
    commit bfe806ce493fbf2c8223b79419e3b5a3be006d31
    Author: Ellen <ellen31725@gmail.com>
    Date:   Fri Mar 3 01:56:33 2023 +0800
    
        check and meet the test
    
    commit 33c7e55dd88e323fdd22ca3247428b39cc9fa487
    Author: Ellen <ellen31725@gmail.com>
    Date:   Fri Mar 3 00:01:05 2023 +0800
    
        feat:adjust middleware/auth.js
    
    commit 3fc99964bcbf1cd02b761aebeff9a17e8a4c3af3
    Merge: 4c3d70b 03c14c0
    Author: Ellen <ellen31725@gmail.com>
    Date:   Thu Mar 2 21:15:06 2023 +0800
    
        Merge remote-tracking branch 'upstream/R01-test' into R01
    
    commit 03c14c01ab2502f04a830604c0e49974d3e3f654
    Author: zjzheng17 <90173539+zjzheng17@users.noreply.github.com>
    Date:   Mon Jul 18 12:56:31 2022 +0800
    
        correcting typo in comment
    
    commit 79ff15885ef6a386efcfd22160781a580049ada1
    Author: zjzheng17 <90173539+zjzheng17@users.noreply.github.com>
    Date:   Mon Jul 18 12:36:56 2022 +0800
    
        註解 typo 修正
    
    commit 7ece79ef4c63264ebac70974f8c2bbce3c80e14c
    Author: Amber Yan <freehourse2sh@gmail.com>
    Date:   Wed Mar 2 15:23:34 2022 +0800
    
        add R01.test.js && unit-test-helper.js

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,6 +1,36 @@
-const { Restaurant } = require('../models')
+const { Restaurant, User } = require('../models')
 const { imgurFileHandler } = require('../helpers/file-helpers')
 const adminController = {
+  getUsers: (req, res, next) => {
+    return User.findAll({
+      // where: {
+      //   name: 'admin'
+      // },
+      raw: true
+    })
+      .then(users => res.render('admin/users', { users }))
+      .catch(err => next(err))
+  },
+  patchUser: (req, res, next) => {
+    return User.findByPk(req.params.id)
+      .then(user => {
+        if (user.email === 'root@example.com') {
+          req.flash('error_messages', '禁止變更 root 權限')
+          res.redirect('back')
+          return
+        }
+
+        if (user.isAdmin) {
+          user.update({ isAdmin: false })
+        } else {
+          user.update({ isAdmin: true })
+        }
+
+        req.flash('success_messages', '使用者權限變更成功')
+        res.redirect('/admin/users')
+      })
+      .catch(err => next(err))
+  },
   getRestaurants: (req, res, next) => {
     Restaurant.findAll({
       raw: true
--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,6 +1,29 @@
+const { Op } = require('sequelize')
+const { Restaurant } = require('../models')
 const restaurantController = {
-  getRestaurants: (req, res) => {
-    return res.render('restaurants')
+  getRestaurants: (req, res, next) => {
+    Restaurant.findAll({
+      raw: true
+    })
+      .then(restaurants => res.render('restaurants', { restaurants }))
+      .catch(err => next(err))
+  },
+  getRestaurantsByName: (req, res, next) => {
+    const keyword = req.query.keyword
+
+    if (keyword) {
+      return Restaurant.findAll({
+        where: {
+          name: {
+            [Op.like]: `%${keyword}%`
+          }
+        },
+        raw: true
+      })
+        .then(restaurants => res.render('restaurants', { restaurants, keyword }))
+    } else {
+      res.render('restaurants')
+    }
   }
 }
 module.exports = restaurantController
