57b5ba49b760d444916bf53b88a12a3f3e07d419
ray199229 <ray199229@gmail.com>
2023-03-03 16:01:48 +0800

    try to commit

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -2,21 +2,31 @@ const assert = require('assert')
 const { Restaurant, Category } = require('../models')
 
 const restaurantController = {
-  getRestaurants: (req, res) => {
-    return Restaurant.findAll({
-      include: Category,
-      raw: true,
-      nest: true
-    })
-      .then(restaurants => {
+  getRestaurants: (req, res, next) => {
+    const categoryId = Number(req.query.categoryId) || ''
+    return Promise.all([
+      Restaurant.findAll({
+        include: Category,
+        where: {
+          ...categoryId ? { categoryId } : {}
+        },
+        raw: true,
+        nest: true
+      }),
+      Category.findAll({ raw: true })
+    ])
+      .then(([restaurants, categories]) => {
         const data = restaurants.map(r => ({
           ...r,
           description: r.description.substring(0, 50)
         }))
         return res.render('restaurants', {
-          restaurants: data
+          restaurants: data,
+          categories,
+          categoryId
         })
       })
+      .catch(err => next(err))
   },
   getRestaurant: (req, res, next) => {
     const { id } = req.params
