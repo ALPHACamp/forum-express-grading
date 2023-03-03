9c18490bcddbffab4601a8cc4186c3ef0c4b0bee
chachagof <chacha0932@gmail.com>
2023-03-03 16:22:56 +0800

    feat:passport init & singin

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const handlebars = require('express-handlebars')
 const routes = require('./routes')
 const session = require('express-session')
 const flash = require('connect-flash')
+const passport = require('passport')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -15,6 +16,8 @@ app.set('view engine', 'hbs')
 
 app.use(express.urlencoded({ extended: true }))
 app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
+app.use(passport.initialize())
+app.use(passport.session())
 app.use(flash())
 
 app.use((req, res, next) => {
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 
+const passport = require('../config/passport')
 const admin = require('./modules/admin')
 
 const restController = require('../controllers/restaurant-controller')
@@ -9,13 +10,18 @@ const userController = require('../controllers/user-controller')
 const { generalErrorHandler } = require('../middleware/error-handler')
 
 router.use('/admin', admin)
-
+// signup
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
+// signin
+router.get('/signin', userController.signInPage)
+router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
+// logout
+router.get('/logout', userController.logout)
 
 router.get('/restaurants', restController.getRestaurants)
 
-router.use('/', (req, res) => res.redirect('/restaurants'))
+router.get('/', (req, res) => res.redirect('/restaurants'))
 router.use('/', generalErrorHandler)
 
 module.exports = router
