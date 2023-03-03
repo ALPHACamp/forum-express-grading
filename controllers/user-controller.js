45664b94b9cebebb2a53c5fce5a04feedacfe6e0
daniel155158 <daniel155158@gmail.com>
2023-03-03 23:48:58 +0800

    add commented restaurants to user profile

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcryptjs')
 const db = require('../models')
-const { User } = db
+const { User, Comment, Restaurant } = db
 const { imgurFileHandler } = require('../helpers/file-helpers')
 
 const userController = {
@@ -40,12 +40,21 @@ const userController = {
     res.redirect('/signin')
   },
   getUser: (req, res, next) => {
-    return User.findByPk(req.params.id, {
-      raw: true
-    })
-      .then(user => {
+    return Promise.all([
+      User.findByPk(req.params.id, {
+        raw: true
+      }),
+      Comment.findAll({
+        raw: true,
+        nest: true,
+        where: { userId: req.params.id },
+        include: Restaurant
+      })
+    ])
+      .then(([user, comments]) => {
         if (!user) throw new Error("User didn't exist")
-        res.render('users/profile', { user })
+        const commentCounts = comments.length
+        res.render('users/profile', { user, comments, commentCounts })
       })
       .catch(err => next(err))
   },
