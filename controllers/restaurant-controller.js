07f89106b4bd6b4cb4e214cf0a03457a3ff9c37f
frank <frankcool321@gmail.com>
2023-03-03 23:07:03 +0800

    feat: add restaurants index page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,7 +1,18 @@
-// - 處理屬於restaurant路由的相關請求
+// - 處理屬於前台restaurant路由的相關請求
+const { Restaurant, Category } = require('../models')
 const restaurantController = {
-  getRestaurants: (req, res) => {
-    return res.render('restaurants')
+  getRestaurants: async (req, res) => {
+    const restaurants = await Restaurant.findAll({
+      raw: true,
+      nest: true,
+      include: [Category]
+    })
+    // - 對原有description進行字數刪減
+    const data = restaurants.map(r => ({
+      ...r,
+      description: r.description.substring(0, 50)
+    }))
+    return res.render('restaurants', { restaurants: data })
   }
 }
 
