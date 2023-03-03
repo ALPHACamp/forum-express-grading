44b318564865a35da32b7cb02c9184a293287418
yy933 <emilyyu025@gmail.com>
2023-03-03 14:17:50 +0800

    feat: add restaurant image

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
+const upload = require('../../middleware/multer')
 
 router.get(
   '/restaurants/create',
@@ -8,8 +9,17 @@ router.get(
 )
 router.get('/restaurants/:id/edit', adminController.editRestaurant)
 router.get('/restaurants/:id', adminController.getRestaurant)
-router.get('/restaurants', adminController.getRestaurants)
-router.put('/restaurants/:id', adminController.putRestaurant)
+router.put(
+  '/restaurants/:id',
+  upload.single('image'),
+  adminController.putRestaurant
+)
 router.delete('/restaurants/:id', adminController.deleteRestaurant)
+router.get('/restaurants', adminController.getRestaurants)
+router.post(
+  '/restaurants',
+  upload.single('image'),
+  adminController.postRestaurant
+)
 router.use('/', (req, res) => res.redirect('/admin/restaurants'))
 module.exports = router
