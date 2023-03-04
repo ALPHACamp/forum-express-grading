5015d4a2a7a9228e96bdd14934c42b32e2770a23
Joshua <j2h5068312@gmail.com>
2023-03-05 01:45:22 +0800

    feat: add index paaage

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,21 @@
 const express = require('express')
+
 const router = express.Router()
+
+// 新增，載入 controller
+
+const restController = require('../controllers/restaurant-controller')
+
+// 刪除
 
 router.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+// 新增
+
+router.get('/restaurants', restController.getRestaurants)
+
+router.use('/', (req, res) => res.redirect('/restaurants'))
 
 module.exports = router
