16eda177a9ca8277dcd154011725dc3ff075ab1b
yy933 <emilyyu025@gmail.com>
2023-03-04 21:39:18 +0800

    feat: add admin user permission patch function and page
    feat: add user permission patch function
    
    fix: fix adminController.getUser

--- helpers/auth-helpers.js
+++ helpers/auth-helpers.js
@@ -1,7 +1,7 @@
 const getUser = req => {
   return req.user || null // 等同於 req.user ? req.user : null
 }
-const ensureAuthenticated = (req) => {
+const ensureAuthenticated = req => {
   return req.isAuthenticated()
 }
 module.exports = {
