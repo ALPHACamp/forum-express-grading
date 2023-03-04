36cf05146220f346060c859ce8b998868c280acb
Joshua <j2h5068312@gmail.com>
2023-03-05 02:11:26 +0800

    feat: passport init & signin

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 
 const router = express.Router()
+const passport = require('../config/passport')
 const admin = require('./modules/admin')
 
 // 新增，載入 controller
@@ -12,6 +13,8 @@ const { generalErrorHandler } = require('../middleware/error-handler')
 router.use('/admin', admin)
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
+router.get('/signin', userController.signInPage)
+router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
 
 router.get('/restaurants', restController.getRestaurants)
 
