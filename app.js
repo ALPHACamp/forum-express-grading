043744cfd7fdd38a0f1ac2f29b8c10b0983394a0
chachagof <chacha0932@gmail.com>
2023-03-04 13:33:58 +0800

    feat: add restaurant image

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const session = require('express-session')
 const flash = require('connect-flash')
 const passport = require('passport')
 const methodOverride = require('method-override')
+const path = require('path')
 
 const routes = require('./routes')
 const { getUser } = require('./helpers/auth-helpers')
@@ -24,6 +25,7 @@ app.use(passport.initialize())
 app.use(passport.session())
 app.use(flash())
 app.use(methodOverride('_method'))
+app.use('/upload', express.static(path.join(__dirname, 'upload')))
 
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
