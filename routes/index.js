644edea93bbb264f0421e648fe7d3eac73eee164
sd880428 <sd660428@gmail.com>
2023-03-04 20:34:49 +0800

    feat: add feeds page

--- routes/index.js
+++ routes/index.js
@@ -28,6 +28,7 @@ router.delete('/comments/:id', authenticatedAdmin, commentController.deleteComme
 router.post('/comments', authenticated, commentController.postComment)
 
 router.get('/restaurants', authenticated, restController.getRestaurants)
+router.get('/restaurants/feeds', authenticated, restController.getFeeds)
 router.get('/restaurants/:id', authenticated, restController.getRestaurant)
 router.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard)
 
