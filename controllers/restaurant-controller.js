e18639e9e5821623d4fb8cd751effa633188c697
Chloe905 <cchloehsieh@gmail.com>
2023-03-04 23:58:15 +0800

    feat:finish R02, add dashboard viewCounts

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -17,13 +17,26 @@ const restaurantController = {
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
+      include: [Category]
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        return restaurant.increment('viewCounts') // 每點擊一次+1，若+2->('viewCounts', { by: 2 })
+      })
+      .then(restaurant => {
+        res.render('restaurant', { restaurant: restaurant.toJSON() })
+      })
+      .catch(err => next(err))
+  },
+  getDashboard: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, {
       raw: true,
       nest: true,
       include: [Category]
     })
       .then(restaurant => {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
-        res.render('restaurant', { restaurant })
+        return res.render('dashboard', { restaurant })
       })
       .catch(err => next(err))
   }
