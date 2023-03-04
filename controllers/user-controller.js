80e7516d3797b809220e8bfb99ed12e4435f8e4a
Joshua <j2h5068312@gmail.com>
2023-03-05 01:58:42 +0800

    feat: user signup

---
+++ controllers/user-controller.js
@@ -0,0 +1,20 @@
+const bcrypt = require('bcryptjs') // 載入 bcrypt
+const db = require('../models')
+const { User } = db
+const userController = {
+  signUpPage: (req, res) => {
+    res.render('signup')
+  },
+  signUp: (req, res) => {
+    bcrypt.hash(req.body.password, 10)
+      .then(hash => User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: hash
+      }))
+      .then(() => {
+        res.redirect('/signin')
+      })
+  }
+}
+module.exports = userController
