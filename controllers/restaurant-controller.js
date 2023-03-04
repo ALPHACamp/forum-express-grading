4c8e08455d7069db396f42205aaa309fe8363596
frank <frankcool321@gmail.com>
2023-03-04 11:23:38 +0800

    feat: add restaurant page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,18 +1,36 @@
 // - 處理屬於前台restaurant路由的相關請求
 const { Restaurant, Category } = require('../models')
 const restaurantController = {
-  getRestaurants: async (req, res) => {
-    const restaurants = await Restaurant.findAll({
-      raw: true,
-      nest: true,
-      include: [Category]
-    })
-    // - 對原有description進行字數刪減
-    const data = restaurants.map(r => ({
-      ...r,
-      description: r.description.substring(0, 50)
-    }))
-    return res.render('restaurants', { restaurants: data })
+  getRestaurants: async (req, res, next) => {
+    try {
+      const restaurants = await Restaurant.findAll({
+        raw: true,
+        nest: true,
+        include: [Category]
+      })
+      // - 對原有description進行字數刪減
+      const data = restaurants.map(r => ({
+        ...r,
+        description: r.description.substring(0, 50)
+      }))
+      return res.render('restaurants', { restaurants: data })
+    } catch (error) {
+      return next(error)
+    }
+  },
+  getRestaurant: async (req, res, next) => {
+    const { id } = req.params
+    try {
+      const restaurant = await Restaurant.findByPk(id, {
+        raw: true,
+        nest: true,
+        include: [Category]
+      })
+      if (!restaurant) throw new Error('此餐廳不存在')
+      return res.render('restaurant', { restaurant })
+    } catch (error) {
+      return next(error)
+    }
   }
 }
 
