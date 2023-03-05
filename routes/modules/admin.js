52a130de0533bb286c6933c0d25585389bc66372
Joshua <j2h5068312@gmail.com>
2023-03-05 13:22:33 +0800

    git merge

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -3,6 +3,9 @@ const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
 const upload = require('../../middleware/multer')
 
+router.get('/users', adminController.getUsers)
+router.patch('/users/:id', adminController.patchUser)
+
 router.get('/restaurants/create', adminController.createRestaurant)
 router.get('/restaurants/:id/edit', adminController.editRestaurant)
 router.get('/restaurants/:id', adminController.getRestaurant)
@@ -10,5 +13,6 @@ router.put('/restaurants/:id', upload.single('image'), adminController.putRestau
 router.delete('/restaurants/:id', adminController.deleteRestaurant)
 router.get('/restaurants', adminController.getRestaurants)
 router.post('/restaurants', upload.single('image'), adminController.postRestaurant)
+
 router.use('/', (req, res) => res.redirect('/admin/restaurants'))
 module.exports = router
