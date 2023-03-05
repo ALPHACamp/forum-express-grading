704678133430c1f03b39bb3e9bf3884fa18b9db6
MerryHao <mq6721@gmail.com>
2023-03-05 11:46:37 +0800

    feat: modify router method

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,6 @@ router.use('/admin', admin)
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
 router.get('/restaurants', restController.getRestaurants)
-router.use('/', (req, res) => res.redirect('/restaurants'))
+router.get('/', (req, res) => res.redirect('/restaurants'))
 router.use('/', generalErrorHandler)
 module.exports = router
