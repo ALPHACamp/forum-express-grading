85037d1324421eaf0cd16125e0308dfc5fe03b98
HKMark <HongKongMark@Gmail.com>
2023-03-05 16:25:26 +0800

    feat: user signup

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const router = express.Router()
-
-// 新增，載入 controller
 const restController = require('../controllers/restaurant-controller')
-const admin = require('./modules/admin') // 載入 admin.js
+const admin = require('./modules/admin')
+const userController = require('../controllers/user-controller')
 
 router.use('/admin', admin)
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
 router.get('/restaurants', restController.getRestaurants)
-router.use('/', (req, res) => res.redirect('/restaurants'))
+router.get('/', (req, res) => res.redirect('/restaurants'))
 
 module.exports = router
