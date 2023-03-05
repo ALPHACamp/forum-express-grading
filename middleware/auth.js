19c8d4be2cf5c261c9dfbbc62de8222a2849f512
JingYi120 <l2898168@yahoo.com.tw>
2023-03-05 08:44:17 +0800

    (RO1)feat: add admin user page

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,8 +1,8 @@
-const { ensureAuthenticated, getUser } = require('../helpers/auth-helpers')
+const helpers = require('../helpers/auth-helpers')
 const authenticated = (req, res, next) => {
   // 使用這是否有登入?
   // if (req.isAuthenticated)
-  if (ensureAuthenticated(req)) {
+  if (helpers.ensureAuthenticated(req)) {
     // 有登入，可以往下走
     return next()
   }
@@ -11,9 +11,9 @@ const authenticated = (req, res, next) => {
 }
 const authenticatedAdmin = (req, res, next) => {
   // if (req.isAuthenticated)
-  if (ensureAuthenticated(req)) {
+  if (helpers.ensureAuthenticated(req)) {
     // 是admin可以往下走
-    if (getUser(req).isAdmin) return next()
+    if (helpers.getUser(req).isAdmin) return next()
     res.redirect('/')
   } else {
     res.redirect('/signin')
