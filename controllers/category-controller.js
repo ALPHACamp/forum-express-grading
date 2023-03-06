7ccb2731faced26b22dd7e182550a3d2f82eb950
Ian_Lin <ian920511@gmail.com>
2023-03-06 20:07:14 +0800

    feat: category update

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -2,10 +2,11 @@ const { Category } = require('../models')
 
 const categoryController = {
   getCategories: (req, res, next) => {
-    Category.findAll({
-      raw: true
-    })
-      .then(categories => res.render('admin/categories', { categories }))
+    Promise.all([
+      Category.findAll({ raw: true }),
+      req.params.id ? Category.findByPk(req.params.id, { raw: true }) : null
+    ])
+      .then(([categories, category]) => res.render('admin/categories', { categories, category }))
       .catch(error => next(error))
   },
 
@@ -15,6 +16,19 @@ const categoryController = {
     if (!name) throw new Error('Category name is required!')
 
     return Category.create({ name })
+      .then(() => res.redirect('/admin/categories'))
+      .catch(error => next(error))
+  },
+
+  putCategory: (req, res, next) => {
+    const { name } = req.body
+    if (!name) throw new Error('Category name is required!')
+
+    return Category.findByPk(req.params.id)
+      .then(category => {
+        if (!category) throw new Error("Category doesn't exist!")
+        return category.update({ name })
+      })
       .then(() => res.redirect('/admin/categories'))
       .catch(error => next(error))
   }
