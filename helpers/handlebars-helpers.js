1cbf7be90ebbfeebd4fdf2c17137a2b0a1cbd2b9
Lilynews <lilinews2021@gmail.com>
2023-03-05 21:13:00 -0500

    feat: show comments on restaurant page

--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -1,9 +1,13 @@
 // 載入 dayjs 套件
 const dayjs = require('dayjs')
+// day.js 延伸套件，顯示為”隔“多久
+const relativeTime = require('dayjs/plugin/relativeTime')
+dayjs.extend(relativeTime)
 
 module.exports = {
   // 取得當年年份作為 currentYear 的屬性值，並導出
   currentYear: () => dayjs().year(),
+  relativeTimeFromNow: a => dayjs(a).fromNow(),
   ifCond: function (a, b, options) {
     return a === b ? options.fn(this) : options.inverse(this)
   }
