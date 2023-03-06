d3b8c3cc25d7e8411c9ae5342a96ae0d7cfa9032
Lilynews <lilinews2021@gmail.com>
2023-03-05 21:19:20 -0500

    feat: add delete comment for admin

--- routes/index.js
+++ routes/index.js
@@ -19,6 +19,7 @@ router.post('/signin', passport.authenticate('local', { failureRedirect: '/signi
 router.get('/logout', userController.logout)
 router.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard)
 router.get('/restaurants/:id', authenticated, restController.getRestaurant)
+router.delete('/comments/:id', authenticatedAdmin, commentController.deleteComment)
 router.post('/comments', authenticated, commentController.postComment)
 
 router.get('/', (req, res) => res.redirect('/restaurants'))
