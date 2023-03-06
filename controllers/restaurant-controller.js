51832b94fbbeb6260ab60e0f38093846a5fdaa06
abbie930 <abbiecheng930@gmail.com>
2023-03-06 15:23:59 +0800

    feat: add restaurant dashboard view page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -31,6 +31,18 @@ const restaurantController = {
         })
       })
       .catch(err => next(err))
+  },
+  getDashboard: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, {
+      include: Category
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        res.render('dashboard', {
+          restaurant: restaurant.toJSON()
+        })
+      })
+      .catch(err => next(err))
   }
 }
 module.exports = restaurantController
