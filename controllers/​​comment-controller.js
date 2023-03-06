47dee21ea5e0e300e39cd10fabe99916f913da97
Chloe905 <cchloehsieh@gmail.com>
2023-03-06 20:30:27 +0800

    feat: add post comment on restaurant page

---
+++ controllers/​​comment-controller.js
@@ -0,0 +1,26 @@
+const { Comment, User, Restaurant } = require('../models')
+const commentController = {
+  postComment: (req, res, next) => {
+    const { restaurantId, text } = req.body // 剛放在restaurnt.hbs form裡的name值
+    const userId = req.user.id // 存在passport serializeUser session中
+    if (!text) throw new Error('Comment text is required!')
+    return Promise.all([
+      User.findByPk(userId),
+      Restaurant.findByPk(restaurantId)
+    ])
+      .then(([user, restaurant]) => {
+        if (!user) throw new Error("User didn't exist!")
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        return Comment.create({
+          text,
+          restaurantId,
+          userId
+        })
+      })
+      .then(() => {
+        res.redirect(`/restaurants/${restaurantId}`)
+      })
+      .catch(err => next(err))
+  }
+}
+module.exports = commentController
