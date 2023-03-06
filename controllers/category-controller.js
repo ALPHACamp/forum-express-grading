1e02b557c3d34fd4c3fee307a186ad7825d12a67
Leon180 <lioung860302@gmail.com>
2023-03-07 00:30:41 +0800

    feat: category delete

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -17,6 +17,10 @@ const categoryController = {
     try {
       const { name } = req.body
       if (!name) throw new Error('Category name is required!')
+      const compareCategory = await Category.findAll({ where: { name }, raw: true })
+      console.log('000000000000000')
+      console.log(compareCategory)
+      if (compareCategory.id !== req.params.id) { throw new Error('Category already exist!') }
       const category = await Category.findByPk(req.params.id)
       if (!category) throw new Error("Category doesn't exist!")
       await category.update({ name })
@@ -30,12 +34,20 @@ const categoryController = {
       const { name } = req.body
       if (!name) throw new Error('Category name is required!')
       const categories = await Category.findAll({ raw: true })
-      categories.forEach(category => {
-        if (category.name === name) {
-          throw new Error('Category already exist!')
-        }
-      })
+      if (categories.filter(category => category.name === name).length > 0) {
+        throw new Error('Category already exist!')
+      }
       await Category.create({ name })
+      res.redirect('/admin/categories')
+    } catch (err) {
+      next(err)
+    }
+  },
+  deleteCategory: async (req, res, next) => {
+    try {
+      const category = await Category.findByPk(req.params.id)
+      if (!category) throw new Error("Category didn't exist!")
+      await category.destroy()
       res.redirect('/admin/categories')
     } catch (err) {
       next(err)
