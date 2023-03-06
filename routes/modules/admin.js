48732a7f2fdc1301a9a36c7e3415f63c36031622
HKMark <HongKongMark@Gmail.com>
2023-03-07 00:29:36 +0800

    feat: category create

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -7,9 +7,9 @@ const upload = require('../../middleware/multer')
 
 // access management
 router.get('/users', adminController.getUsers)
-router.get('/categories', categoryController.getCategories)
 router.patch('/users/:id', adminController.patchUser)
-
+router.get('/categories', categoryController.getCategories)
+router.post('/categories', categoryController.postCategory)
 router.get('/restaurants/create', adminController.createRestaurant)
 router.get('/restaurants/:id/edit', adminController.editRestaurant)
 router.get('/restaurants/:id', adminController.getRestaurant)
