6cbaf8965d6676e73f0aaf2d337a2f8fa5d47675
hl94vul3h6 <gary6780gary6780@gmail.com>
2023-03-06 13:56:55 -0800

    feat: add user profile

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,22 +1,27 @@
 const bcrypt = require('bcryptjs') // 載入 bcrypt
 const db = require('../models')
+const assert = require('assert')
 const { User } = db
+const { imgurFileHandler } = require('../helpers/file-helpers')
 const userController = {
   signUpPage: (req, res) => {
     res.render('signup')
   },
   signUp: (req, res, next) => {
-    if (req.body.password !== req.body.passwordCheck) throw new Error('Passwords do not match!')
+    if (req.body.password !== req.body.passwordCheck) { throw new Error('Passwords do not match!') }
     User.findOne({ where: { email: req.body.email } })
       .then(user => {
         if (user) throw new Error('Email already exists!')
         return bcrypt.hash(req.body.password, 10) // 前面加 return
       })
-      .then(hash => User.create({ // 上面錯誤狀況都沒發生，就把使用者的資料寫入資料庫
-        name: req.body.name,
-        email: req.body.email,
-        password: hash
-      }))
+      .then(hash =>
+        User.create({
+          // 上面錯誤狀況都沒發生，就把使用者的資料寫入資料庫
+          name: req.body.name,
+          email: req.body.email,
+          password: hash
+        })
+      )
       .then(() => {
         req.flash('success_messages', '成功註冊帳號！') // 並顯示成功訊息
         res.redirect('/signin')
@@ -34,6 +39,43 @@ const userController = {
     req.flash('success_messages', '登出成功！')
     req.logout()
     res.redirect('/signin')
+  },
+  getUser: (req, res, next) => {
+    return User.findByPk(req.params.id, { raw: true })
+      .then(user => {
+        if (!user) throw new Error("User didn't exist!")
+        return res.render('users/profile', { user })
+      })
+      .catch(err => next(err))
+  },
+  editUser: (req, res, next) => {
+    return User.findByPk(req.params.id, { raw: true })
+      .then(user => {
+        if (!user) throw new Error("User didn't exist!")
+        assert.equal(user.id, req.user.id, "User can't modify others profile")
+        return res.render('users/edit', { user })
+      })
+      .catch(err => next(err))
+  },
+  putUser: (req, res, next) => {
+    const { name } = req.body
+    if (!name) throw new Error('Restaurant name is required!')
+    const { id } = req.params
+    const { file } = req
+    return Promise.all([User.findByPk(id), imgurFileHandler(file)])
+      .then(([user, filePath]) => {
+        if (!user) throw new Error("User didn't exist!")
+        // assert.equal(user.id, req.user.id, "User can't modify others profile")
+        return user.update({
+          name,
+          image: filePath || user.image
+        })
+      })
+      .then(() => {
+        req.flash('success_messages', '使用者資料編輯成功')
+        return res.redirect(`/users/${id}`)
+      })
+      .catch(err => next(err))
   }
 }
 module.exports = userController
