051c5ee882f95164cf7f53940cbfa76bc20cd86a
chachagof <chacha0932@gmail.com>
2023-03-06 20:20:28 +0800

    feat:add ifCond handlebars helper & update category selector

--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -1,5 +1,8 @@
 const day = require('dayjs')
 
 module.exports = {
-  currentYear: () => day().year()
+  currentYear: () => day().year(),
+  ifCond: function (a, b, options) {
+    return a === b ? options.fn(this) : options.inverse(this)
+  }
 }
