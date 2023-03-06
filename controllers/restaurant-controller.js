dc7106ba44ad14ce99392ed7f12e717dccdddd21
freeway26tw <freeway26tw@gmail.com>
2023-03-06 14:47:51 +0800

    add view count on each restaurant dashboard

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -17,15 +17,26 @@ const restaurantController = {
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
+      include: Category
+    })
+      .then(restaurant => {
+        restaurant.increment('view_counts')
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        res.render('restaurant', {
+          restaurant: restaurant.toJSON()
+        })
+      })
+      .catch(err => next(err))
+  },
+  getDashboard: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, {
       include: Category,
       nest: true,
       raw: true
     })
       .then(restaurant => {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
-        res.render('restaurant', {
-          restaurant
-        })
+        return res.render('dashboard', { restaurant })
       })
       .catch(err => next(err))
   }
