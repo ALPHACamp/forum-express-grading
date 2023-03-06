f5f90936306bfffca92a9c788a93823a7194aa55
ray199229 <ray199229@gmail.com>
2023-03-06 15:19:20 +0800

    refactor: getUser to adjust to be findAndCountAll

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,9 +1,8 @@
 const bcrypt = require('bcryptjs')
 const assert = require('assert')
 const { imgurFileHandler } = require('../helpers/file-helpers')
-const db = require('../models')
-
-const { User, Comment, Restaurant } = db
+const { User, Comment, Restaurant } = require('../models')
+const helpers = require('../helpers/auth-helpers')
 
 const userController = {
   signUpPage: (req, res) => {
@@ -41,33 +40,32 @@ const userController = {
   },
   getUser: (req, res, next) => {
     const userId = req.params.id
+    const reqUser = helpers.getUser(req) // 滿足測試檔所增加的
+    const checkUser = (Number(userId) === reqUser.id)
     return Promise.all([
       User.findByPk(userId),
-      Comment.findAll({
+      // 也可以分開用 finAll 和 count 來完成計數
+      Comment.findAndCountAll({
         include: Restaurant,
         where: {
           ...userId ? { userId } : {}
         },
         raw: true,
         nest: true
-      }),
-      Comment.count({
-        where: {
-          ...userId ? { userId } : {}
-        }
       })
     ])
-      .then(([user, comments, totalComments]) => {
-        // assert.equal(user.id, req.user.id, 'Only can update your own profile!')
+      // { rows } 顯示下一層內容，不會只返回[object]，{ count } 計算數量。
+      .then(([user, { rows: comments, count: totalComments }]) => {
         assert(user, "User didn't exist!")
-        return res.render('users/profile', { user: user.toJSON(), comments, totalComments })
+        return res.render('users/profile', { user: user.toJSON(), comments, totalComments, checkUser })
       })
       .catch(err => next(err))
   },
   editUser: (req, res, next) => {
+    const reqUser = helpers.getUser(req) // 滿足測試檔所增加的
     return User.findByPk(req.params.id)
       .then(user => {
-        // assert.equal(user.id, req.user.id, 'Only can update your own profile!')
+        assert.equal(user.id, reqUser.id, 'Only can update your own profile!')
         assert(user, "User didn't exist!")
         return res.render('users/edit', { user: user.toJSON() })
       })
