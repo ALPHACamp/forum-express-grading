dc8acf460f4db4897f16c418a40c245b9f3fbfb1
Shiba-Patrick <liu21407@gmail.com>
2023-03-07 20:06:36 +0800

    feat: add pagination on restaurants index page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,33 +1,41 @@
 const { Restaurant, Category } = require('../models')
+const { getOffset, getPagination } = require('../helpers/pagination-helper')
 
 const restaurantController = {
   getRestaurants: (req, res, next) => { // 瀏覽所有餐廳頁面:運用展開運算子 ...
+    const DEFAULT_LIMIT = 9 // 依照devTools來觀察適合一頁放幾筆
+
     const categoryId = Number(req.query.categoryId) || '' // 從網址上拿下來的參數是字串，先轉成 Number 再操作
 
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || DEFAULT_LIMIT
+    const offset = getOffset(limit, page)
     // const where = {}
     // if (categoryId) where.categoryId = categoryId
-
     return Promise.all([
-      Restaurant.findAll({
+      Restaurant.findAndCountAll({
         include: Category,
         // where: where,
         where: { // 新增查詢條件
           ...categoryId ? { categoryId } : {} // 檢查 categoryId 是否為空值
         },
+        limit,
+        offset,
         nest: true,
         raw: true
       }),
       Category.findAll({ raw: true })
     ])
       .then(([restaurants, categories]) => {
-        const data = restaurants.map(r => ({
+        const data = restaurants.rows.map(r => ({
           ...r,
           description: r.description.substring(0, 50)
         }))
         return res.render('restaurants', {
           restaurants: data,
           categories,
-          categoryId
+          categoryId,
+          pagination: getPagination(limit, page, restaurants.count)
         })
       })
       .catch(err => next(err))
