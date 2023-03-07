3eb4a2de4f94950359696ec69112e44e921a6441
Shiba-Patrick <liu21407@gmail.com>
2023-03-07 18:49:05 +0800

    feat: add categories navbar on restaurants index page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,18 +1,36 @@
 const { Restaurant, Category } = require('../models')
 
 const restaurantController = {
-  getRestaurants: (req, res) => { // 瀏覽所有餐廳頁面:運用展開運算子 ...
-    return Restaurant.findAll({
-      include: Category,
-      nest: true,
-      raw: true
-    }).then(restaurants => {
-      const data = restaurants.map(r => ({
-        ...r,
-        description: r.description.substring(0, 50)
-      }))
-      return res.render('restaurants', { restaurants: data })
-    })
+  getRestaurants: (req, res, next) => { // 瀏覽所有餐廳頁面:運用展開運算子 ...
+    const categoryId = Number(req.query.categoryId) || '' // 從網址上拿下來的參數是字串，先轉成 Number 再操作
+
+    // const where = {}
+    // if (categoryId) where.categoryId = categoryId
+
+    return Promise.all([
+      Restaurant.findAll({
+        include: Category,
+        // where: where,
+        where: { // 新增查詢條件
+          ...categoryId ? { categoryId } : {} // 檢查 categoryId 是否為空值
+        },
+        nest: true,
+        raw: true
+      }),
+      Category.findAll({ raw: true })
+    ])
+      .then(([restaurants, categories]) => {
+        const data = restaurants.map(r => ({
+          ...r,
+          description: r.description.substring(0, 50)
+        }))
+        return res.render('restaurants', {
+          restaurants: data,
+          categories,
+          categoryId
+        })
+      })
+      .catch(err => next(err))
   },
   getRestaurant: (req, res, next) => { // 瀏覽單筆餐廳頁面
     return Restaurant.findByPk(req.params.id, {
