a8050fc3396a96e8e1eb2314a3b1f0dec0d8baa7
freeway26tw <freeway26tw@gmail.com>
2023-03-07 12:42:36 +0800

    feat: add delete comment for admin

--- routes/index.js
+++ routes/index.js
@@ -26,6 +26,7 @@ router.get('/restaurants/:id/dashboard', authenticated, restController.getDashbo
 router.get('/restaurants/:id', authenticated, restController.getRestaurant)
 router.get('/restaurants', authenticated, restController.getRestaurants)
 
+router.delete('/comments/:id', authenticatedAdmin, commentController.deleteComment)
 router.post('/comments', authenticated, commentController.postComment)
 
 router.use('/', (req, res) => res.redirect('/restaurants'))
