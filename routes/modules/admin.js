8b1de5701e1573087845b26a13f5018824d779e4
yy933 <emilyyu025@gmail.com>
2023-03-07 17:12:58 +0800

    feat: category create

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -5,7 +5,8 @@ const categoryController = require('../../controllers/category-controller')
 const upload = require('../../middleware/multer')
 
 router.get('/users', adminController.getUsers)
-router.get("/categories", categoryController.getCategories);
+router.get('/categories', categoryController.getCategories)
+router.post('/categories', categoryController.postCategory)
 router.patch('/users/:id', adminController.patchUser)
 router.get(
   '/restaurants/create',
