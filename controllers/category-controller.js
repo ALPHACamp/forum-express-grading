784948e914ce5d58bfa086a3a1a59e57b5821524
chachagof <chacha0932@gmail.com>
2023-03-07 12:52:11 +0800

    feat: category create

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -5,6 +5,17 @@ const categoryController = {
     Category.findAll({ raw: true })
       .then(categories => res.render('admin/categories', { categories }))
       .catch(err => next(err))
+  },
+  postCategory: (req, res, next) => {
+    const { name } = req.body
+    if (!name) throw new Error('Category name is required!')
+    Category.findOne({ where: { name } })
+      .then(category => {
+        if (category) throw new Error('Category is already exist!')
+        Category.create({ name })
+      })
+      .then(() => res.redirect('categories'))
+      .catch(err => next(err))
   }
 }
 
