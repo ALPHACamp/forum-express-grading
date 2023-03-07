bc5f210cf17d24ed98e96c20e549cd1847115bc1
Shiba-Patrick <liu21407@gmail.com>
2023-03-07 14:37:06 +0800

    feat: add restaurants index page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,7 +1,19 @@
+const { Restaurant, Category } = require('../models')
+
 const restaurantController = {
-  getRestaurants: (req, res) => {
-    return res.render('restaurants')
+  getRestaurants: (req, res) => { // 瀏覽所有餐廳頁面:運用展開運算子 ...
+    return Restaurant.findAll({
+      include: Category,
+      nest: true,
+      raw: true
+    }).then(restaurants => {
+      const data = restaurants.map(r => ({
+        ...r,
+        description: r.description.substring(0, 50)
+      }))
+      console.log('data', data)
+      return res.render('restaurants', { restaurants: data})
+    })
   }
 }
-
 module.exports = restaurantController
