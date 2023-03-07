1c115838851537fbcac375b21ea9598fbc191ed1
chachagof <chacha0932@gmail.com>
2023-03-07 13:15:48 +0800

    feat: category update

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -2,8 +2,11 @@ const { Category } = require('../models')
 
 const categoryController = {
   getCategories: (req, res, next) => {
-    Category.findAll({ raw: true })
-      .then(categories => res.render('admin/categories', { categories }))
+    Promise.all([
+      Category.findAll({ raw: true }),
+      req.params.id ? Category.findByPk(req.params.id, { raw: true }) : null
+    ])
+      .then(([categories, category]) => res.render('admin/categories', { categories, category }))
       .catch(err => next(err))
   },
   postCategory: (req, res, next) => {
@@ -13,6 +16,17 @@ const categoryController = {
       .then(category => {
         if (category) throw new Error('Category is already exist!')
         Category.create({ name })
+      })
+      .then(() => res.redirect('categories'))
+      .catch(err => next(err))
+  },
+  putCategory: (req, res, next) => {
+    const { name } = req.body
+    if (!name) throw new Error('Category name is required!')
+    return Category.findByPk(req.params.id)
+      .then(category => {
+        if (!category) throw new Error("Category doesn't exist!")
+        return category.update({ name })
       })
       .then(() => res.redirect('categories'))
       .catch(err => next(err))
