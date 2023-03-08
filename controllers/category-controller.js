34d99ec2e618c952c82aadc1e3f4aa37bfc9a5d5
Christal Chen <christalchen1209@gmail.com>
2023-03-08 23:40:41 +0800

    feat: category update

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -1,11 +1,17 @@
 const { Category } = require('../models')
 const categoryController = {
   getCategories: (req, res, next) => {
-    Category.findAll({
-      raw: true
-    })
-      .then(categories =>
-        res.render('admin/categories', { categories }))
+    return Promise.all([
+      Category.findAll({
+        raw: true
+      }),
+      req.params.id
+        ? Category.findByPk(req.params.id, { raw: true })
+        : null
+    ])
+
+      .then(([categories, category]) =>
+        res.render('admin/categories', { categories, category }))
       .catch(err => next(err))
   },
   postCategory: (req, res, next) => {
@@ -13,6 +19,25 @@ const categoryController = {
     if (!name) throw new Error('Category name is required!')
     Category.create({ name })
       .then(() => res.redirect('/admin/categories'))
+      .catch(err => next(err))
+  },
+  putCategory: (req, res, next) => {
+    const { name } = req.body
+    if (!name) throw new Error('Category name is required!')
+    Category.findByPk(req.params.id)
+
+      .then(category => {
+        if (!category) throw new Error('Category does not exist!')
+
+        return category.update({
+          name
+
+        })
+      })
+      .then(() => {
+        // req.flash('success_messages', 'category was successfully to update')
+        res.redirect('/admin/categories')
+      })
       .catch(err => next(err))
   }
 }
