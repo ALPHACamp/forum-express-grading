6e0fb76242174a5bb3e4ccc26fca4cdf8ad8c7ec
WilliamChen15 <turelight13@gmail.com>
2023-03-08 19:44:52 +0800

    feat: add topUser page

--- routes/index.js
+++ routes/index.js
@@ -32,6 +32,8 @@ router.post('/signin', passport.authenticate('local', { failureRedirect: '/signi
 
 router.get('/logout', userController.logout)
 
+router.get('/users/top', authenticated, userController.getTopUsers)
+
 router.get('/users/:id/edit', authenticated, authenticatedOwner, userController.editUser)
 
 router.get('/users/:id', authenticated, userController.getUser)
