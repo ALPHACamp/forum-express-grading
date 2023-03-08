e9345e9b6072f69488e4e4134d0f54befd5e1565
chikunmark <chikunmark@gmail.com>
2023-03-08 15:22:55 +0800

    feat: add home page pagination

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,20 +1,29 @@
 const { Restaurant, Category } = require('../models')
+const { getOffset, getPagination } = require('../helpers/pagination-helper')
 
 const restaurantController = {
   getRestaurants: (req, res, next) => {
+    const DEFAULT_LIMIT = 9
+
     const categoryId = Number(req.query.categoryId) || '' // 還不知 或 當空字元的原因
     // const categoryId = +req.params.categoryId || '' // 與上同意，之後查 + 的用處
     console.log(categoryId)
+
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || DEFAULT_LIMIT // 前面的 Number(...)，只是留著，未來要用可用
+    const offset = getOffset(limit, page)
 
     // const where = {}
 
     // if (categoryId) where.categoryId = categoryId
 
     Promise.all([
-      Restaurant.findAll({
+      Restaurant.findAndCountAll({
         nest: true,
         raw: true,
         include: [Category],
+        limit,
+        offset,
         where: { // 這個 where 的 value，物件包物件，原來能這樣
           ...categoryId ? { categoryId } : {} // 這裡的 ...，我猜不是展開運算子，畢竟下面實驗失敗，而且，categoryId 明明就是"一個"數字，為啥要展開？
           // categoryId ? { categoryId } : {}
@@ -25,14 +34,15 @@ const restaurantController = {
       .then(([restaurants, categories]) => {
       // console.log(restaurants) // 觀察 include
       // (下1) 不錯，運用展開運算子跟箭頭函式，直接改 object 內容
-        const data = restaurants.map(r => ({
+        const data = restaurants.rows.map(r => ({
           ...r,
           description: r.description.substring(0, 50)
         }))
         return res.render('restaurants', {
           restaurants: data,
           categories,
-          categoryId
+          categoryId,
+          pagination: getPagination(limit, page, restaurants.count)
         })
       })
       .catch(err => next(err))
