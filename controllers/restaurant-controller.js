0dadefd609a5d440faf12cfd870896eddd6e543a
Lilynews <lilinews2021@gmail.com>
2023-03-08 15:01:46 -0500

    feat: get user's favorited & switch button

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -24,9 +24,12 @@ const restaurantController = {
       Category.findAll({ raw: true })
     ])
       .then(([restaurants, categories]) => {
+        const favoritedRestaurantsId = req.user && req.user.FavoritedRestaurants.map(fr => fr.id)
+
         const data = restaurants.rows.map(r => ({
           ...r,
-          description: r.description.substring(0, 50)
+          description: r.description.substring(0, 50),
+          isFavorited: favoritedRestaurantsId.includes(r.id)
         }))
         return res.render('restaurants', {
           restaurants: data,
@@ -41,16 +44,22 @@ const restaurantController = {
     return Restaurant.findByPk(req.params.id, {
       include: [
         Category,
-        { model: Comment, include: User }
+        { model: Comment, include: User },
+        { model: User, as: 'FavoritedUsers' }
       ],
       nest: true
     })
       .then(restaurant => {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
         // 每進來一次就 increment viewCounts 這欄位
-        restaurant.increment('viewCounts')
-        res.render('restaurant', {
-          restaurant: restaurant.toJSON()
+        return restaurant.increment('viewCounts')
+      })
+      .then(restaurant => {
+        const isFavorited = restaurant.FavoritedUsers.some(f => f.id === req.user.id)
+
+        return res.render('restaurant', {
+          restaurant: restaurant.toJSON(),
+          isFavorited
         })
       })
       .catch(err => next(err))
