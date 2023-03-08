3076d635b67e1f61f9045873cc96b76a9e19abe5
chikunmark <chikunmark@gmail.com>
2023-03-08 14:20:16 +0800

    feat: add sort Fn.

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,22 +1,41 @@
 const { Restaurant, Category } = require('../models')
 
 const restaurantController = {
-  getRestaurants: (req, res) => {
-    return Restaurant.findAll({
-      include: [Category],
-      nest: true,
-      raw: true
-    }).then(restaurants => {
+  getRestaurants: (req, res, next) => {
+    const categoryId = Number(req.query.categoryId) || '' // 還不知 或 當空字元的原因
+    // const categoryId = +req.params.categoryId || '' // 與上同意，之後查 + 的用處
+    console.log(categoryId)
+
+    // const where = {}
+
+    // if (categoryId) where.categoryId = categoryId
+
+    Promise.all([
+      Restaurant.findAll({
+        nest: true,
+        raw: true,
+        include: [Category],
+        where: { // 這個 where 的 value，物件包物件，原來能這樣
+          ...categoryId ? { categoryId } : {} // 這裡的 ...，我猜不是展開運算子，畢竟下面實驗失敗，而且，categoryId 明明就是"一個"數字，為啥要展開？
+          // categoryId ? { categoryId } : {}
+        }
+      }),
+      Category.findAll({ raw: true })
+    ])
+      .then(([restaurants, categories]) => {
       // console.log(restaurants) // 觀察 include
       // (下1) 不錯，運用展開運算子跟箭頭函式，直接改 object 內容
-      const data = restaurants.map(r => ({
-        ...r,
-        description: r.description.substring(0, 50)
-      }))
-      return res.render('restaurants', {
-        restaurants: data
+        const data = restaurants.map(r => ({
+          ...r,
+          description: r.description.substring(0, 50)
+        }))
+        return res.render('restaurants', {
+          restaurants: data,
+          categories,
+          categoryId
+        })
       })
-    })
+      .catch(err => next(err))
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
