e66e447c84ae4395a9626fbb0b88529ee457d533
chachagof <chacha0932@gmail.com>
2023-03-08 22:33:53 +0800

    feat: show comments on restaurant page

--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -1,7 +1,11 @@
-const day = require('dayjs')
+const dayjs = require('dayjs')
+const relativeTime = require('dayjs/plugin/relativeTime')
+
+dayjs.extend(relativeTime)
 
 module.exports = {
-  currentYear: () => day().year(),
+  currentYear: () => dayjs().year(),
+  relativeTimeFromNow: a => dayjs(a).fromNow(),
   ifCond: function (a, b, options) {
     return a === b ? options.fn(this) : options.inverse(this)
   }
