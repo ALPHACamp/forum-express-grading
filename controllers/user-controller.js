f5a89f73297e1f3d329baec16bc4e403d7359620
FrankHuang <frankcool321@gmail.com>
2023-03-10 00:05:20 +0800

    feat: addFollowing & removeFollowing function

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,6 +1,13 @@
 // - 處理屬於user路由的相關請求
 const bcrypt = require('bcryptjs')
-const { User, Comment, Restaurant, Favorite, Like } = require('../models')
+const {
+  User,
+  Comment,
+  Restaurant,
+  Favorite,
+  Like,
+  Followship
+} = require('../models')
 const { getUser } = require('../helpers/auth-helpers')
 const { imgurFileHandler } = require('../helpers/file-helpers')
 const userController = {
@@ -185,18 +192,65 @@ const userController = {
   },
   getTopUsers: async (req, res, next) => {
     try {
-      let users = await User.findAll({
+      const users = await User.findAll({
         include: [{ model: User, as: 'Followers' }]
       })
 
-      users = users.map(user => ({
-        ...user.toJSON(), // -整理格式
-        followerCount: user.Followers.length,
-        // - 判斷目前登入者的是否有追蹤 user 物件
-        isFollowed: getUser(req).Followings.some(f => f.id === user.id)
-      }))
+      const result = users
+        .map(user => ({
+          ...user.toJSON(), // -整理格式
+          followerCount: user.Followers.length,
+          // - 判斷目前登入者的是否有追蹤 user 物件
+          isFollowed: getUser(req).Followings.some(f => f.id === user.id)
+        }))
+        .sort((a, b) => b.followerCount - a.followerCount) // -按照追蹤者人數作排序
 
-      return res.render('top-users', { users })
+      return res.render('top-users', { users: result })
+    } catch (error) {
+      return next(error)
+    }
+  },
+  addFollowing: async (req, res, next) => {
+    const { userId } = req.params // -取得欲追蹤者 id
+    try {
+      const [user, followship] = await Promise.all([
+        User.findByPk(userId),
+        Followship.findOne({
+          where: {
+            followerId: getUser(req).id,
+            followingId: userId
+          }
+        })
+      ])
+
+      if (!user) throw new Error('此使用者不存在!')
+      if (followship) throw new Error('您已追蹤此使用者!')
+
+      await Followship.create({
+        followerId: getUser(req).id,
+        followingId: userId
+      })
+      req.flash('success_messages', '追蹤成功!')
+      return res.redirect('back')
+    } catch (error) {
+      return next(error)
+    }
+  },
+  removeFollowing: async (req, res, next) => {
+    const { userId } = req.params
+    try {
+      const followship = await Followship.findOne({
+        where: {
+          followerId: getUser(req).id,
+          followingId: userId
+        }
+      })
+
+      if (!followship) throw new Error('您尚未追蹤過此使用者!')
+
+      await followship.destroy()
+      req.flash('success_messages', '取消追蹤成功!')
+      return res.redirect('back')
     } catch (error) {
       return next(error)
     }
