3c91c201271258a6851801cf6bf749dbfd81137f
freeway26tw <freeway26tw@gmail.com>
2023-03-09 15:47:42 +0800

    create like model
    add like/unlike button at index page
    
    get user's liked restaurants and switch button

--- models/restaurant.js
+++ models/restaurant.js
@@ -21,6 +21,11 @@ module.exports = (sequelize, DataTypes) => {
         foreignKey: 'restaurantId',
         as: 'FavoritedUsers'
       })
+      Restaurant.belongsToMany(models.User, {
+        through: models.Like,
+        foreignKey: 'restaurantId',
+        as: 'LikedUsers'
+      })
     }
   };
   Restaurant.init({
