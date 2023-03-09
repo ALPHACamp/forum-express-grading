658ef12ea077eca8c329eb9ca571f627aa701bd6
may huang <mayno1024@gmail.com>
2023-03-09 11:36:26 +0800

    feat: admin restaurant page

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -3,7 +3,8 @@ const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
 
 router.get('/restaurants/create', adminController.createRestaurant)
+router.get('/restaurants/:id', adminController.getRestaurant)
 router.get('/restaurants', adminController.getRestaurants)
 router.post('/restaurants', adminController.postRestaurant)
-router.get('/', (req, res) => res.redirect('/admin/restaurants')) // 這行有點不大懂
+router.get('/', (req, res) => res.redirect('/admin/restaurants')) // 這裡有點不大懂
 module.exports = router
