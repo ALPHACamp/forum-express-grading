308ed39a98fb372fddef41ef9bfc1c4b00ddd25a
onlyyou000u@gmail.com <onlyyou000u@gmail.com>
2023-03-09 23:04:16 +0800

    feat: add like function

--- routes/index.js
+++ routes/index.js
@@ -28,6 +28,9 @@ router.delete('/comments/:id', authenticated, commentController.deleteComment)
 router.post('/favorite/:restaurantId', authenticated, userController.addFavorite)
 router.delete('/favorite/:restaurantId', authenticated, userController.removeFavorite)
 
+router.post('/like/:restaurantId', authenticated, userController.addLike)
+router.delete('/like/:restaurantId', authenticated, userController.removeLike)
+
 router.get('/users/:id', authenticated, userController.getUser)
 router.get('/users/:id/edit', authenticated, userController.editUser)
 router.put('/users/:id', authenticated, upload.single('image'), userController.putUser)
