c2b0f8555b560feb025c7b9f75c553a9906073c2
Ellen <ellen31725@gmail.com>
2023-03-09 12:02:35 +0800

    Squashed commit of the following:
    commit 45aed421d2be988ed3abaf5c72f5d98bb5e61847
    Author: Ellen <ellen31725@gmail.com>
    Date:   Thu Mar 9 10:55:36 2023 +0800
    
        feat:get restaurants user like or unlike & switch button
    
    commit 227368eb920b1b7a420b76b07f1a4b93f63b4e58
    Author: Ellen <ellen31725@gmail.com>
    Date:   Wed Mar 8 23:53:03 2023 +0800
    
        feat: get user's favorited restaurants & switch button
    
    commit 7c410fa45a1cc537e7c3889a959cfe07782ebc82
    Author: Ellen <ellen31725@gmail.com>
    Date:   Wed Mar 8 22:31:44 2023 +0800
    
        feat: add favorite/unfavorite button at index page
    
    commit edba5fecd25ede58d5257137c34d594f3507703d
    Author: Ellen <ellen31725@gmail.com>
    Date:   Wed Mar 8 22:11:27 2023 +0800
    
        feat: create favorite model
    
    commit f48be4440a6aec85c402cd04dabbfe283f39a728
    Author: Ellen <ellen31725@gmail.com>
    Date:   Wed Mar 8 21:35:59 2023 +0800
    
        feat: add feeds page
    
    commit 560d6f3d804a8d76f452e96d8ffab734f6ccf0e8
    Merge: 66b04bc 1ead393
    Author: Ellen <ellen31725@gmail.com>
    Date:   Wed Mar 8 21:10:15 2023 +0800
    
        Merge remote-tracking branch 'upstream/R04-test' into R04
    
    commit 1ead393948309b86423652cac6712a1e19485384
    Author: Amber Yan <freehourse2sh@gmail.com>
    Date:   Wed Mar 2 15:28:28 2022 +0800
    
        add R04.test.js
    
    commit 37322b4d2a72d4b3c495ce00294561093b055cb7
    Author: Carrot7712 <carrot7712@gmail.com>
    Date:   Thu Jan 13 18:03:44 2022 +0800
    
        feat:add R03.test.js
    
    commit 96da4bee5176c5e643633f5bc462405ff38c6660
    Author: Carrot7712 <carrot7712@gmail.com>
    Date:   Thu Jan 13 18:01:55 2022 +0800
    
        feat:add R02.test.js
    
    commit fca7f3bc73dc9fa9bfadef198304b1ba88e6a44b
    Author: Carrot7712 <carrot7712@gmail.com>
    Date:   Thu Jan 13 16:48:07 2022 +0800
    
        feat: add R01.test.js and unit-test-helpers.js

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -21,9 +21,14 @@ const restaurantController = {
       Category.findAll({ raw: true })
     ])
       .then(([restaurants, categories]) => {
+        const favoritedRestaurantsId = req.user && req.user.FavoritedRestaurants.map(fr => fr.id)
+        const likedRestaurantsId = req.user && req.user.LikedRestaurants.map(lr => lr.id)
+
         const data = restaurants.rows.map(r => ({
           ...r,
-          description: r.description.substring(0, 50)
+          description: r.description.substring(0, 50),
+          isFavorited: favoritedRestaurantsId.includes(r.id),
+          isLiked: likedRestaurantsId.includes(r.id)
         }))
         return res.render('restaurants', {
           restaurants: data,
@@ -37,28 +42,62 @@ const restaurantController = {
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
       include: [Category,
-        { model: Comment, include: User }
+        { model: Comment, include: User },
+        { model: User, as: 'FavoritedUsers' },
+        { model: User, as: 'LikedUsers' }
       ]
     })
       .then(restaurant => {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
-        return restaurant.increment('view_counts')
+        return restaurant.increment('viewCounts')
       })
       .then(restaurant => {
+        const isFavorited = restaurant.FavoritedUsers.some(f => f.id === req.user.id)
+        const isLiked = restaurant.LikedUsers.some(l => l.id === req.user.id)
+
         res.render('restaurant', {
-          restaurant: restaurant.toJSON()
+          restaurant: restaurant.toJSON(),
+          isFavorited,
+          isLiked
         })
       })
       .catch(err => next(err))
   },
   getDashboard: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
-      include: Category
+      include: [
+        Category
+      ]
     })
       .then(restaurant => {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
 
         res.render('dashboard', { restaurant: restaurant.toJSON() })
+      })
+      .catch(err => next(err))
+  },
+  getFeeds: (req, res, next) => {
+    return Promise.all([
+      Restaurant.findAll({
+        limit: 10,
+        order: [['createdAt', 'DESC']],
+        include: [Category],
+        raw: true,
+        nest: true
+      }),
+      Comment.findAll({
+        limit: 10,
+        order: [['createdAt', 'DESC']],
+        include: [User, Restaurant],
+        raw: true,
+        nest: true
+      })
+    ])
+      .then(([restaurants, comments]) => {
+        res.render('feeds', {
+          restaurants,
+          comments
+        })
       })
       .catch(err => next(err))
   }
--- routes/index.js
+++ routes/index.js
@@ -16,6 +16,12 @@ router.post('/signup', userController.signUp)
 router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn) // 注意是 post
 router.get('/logout', userController.logout)
+
+router.get('/users/:id/edit', authenticated, userController.editUser)
+router.get('/users/:id', authenticated, userController.getUser)
+router.put('/users/:id', authenticated, upload.single('image'), userController.putUser)
+
+router.get('/restaurants/feeds', authenticated, restController.getFeeds)
 router.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard)
 router.get('/restaurants/:id', authenticated, restController.getRestaurant)
 router.get('/restaurants', authenticated, restController.getRestaurants)
@@ -23,9 +29,11 @@ router.get('/restaurants', authenticated, restController.getRestaurants)
 router.delete('/comments/:id', authenticatedAdmin, commentController.deleteComment)
 router.post('/comments', authenticated, commentController.postComment)
 
-router.get('/users/:id/edit', authenticated, userController.editUser)
-router.get('/users/:id', authenticated, userController.getUser)
-router.put('/users/:id', authenticated, upload.single('image'), userController.putUser)
+router.post('/favorite/:restaurantId', authenticated, userController.addFavorite)
+router.delete('/favorite/:restaurantId', authenticated, userController.removeFavorite)
+
+router.post('/like/:restaurantId', authenticated, userController.addLike)
+router.delete('/like/:restaurantId', authenticated, userController.removeLike)
 
 router.get('/', (req, res) => res.redirect('/restaurants'))
 router.use('/', generalErrorHandler)
