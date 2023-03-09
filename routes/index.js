90b18a972326d232deac6ef0209a2227740fd457
onlyyou000u@gmail.com <onlyyou000u@gmail.com>
2023-03-09 17:52:41 +0800

    feat:add favorite/unfavorite button ad index page

--- routes/index.js
+++ routes/index.js
@@ -25,6 +25,9 @@ router.get('/restaurants', authenticated, restController.getRestaurants)
 router.post('/comments', authenticated, commentController.postComment)
 router.delete('/comments/:id', authenticated, commentController.deleteComment)
 
+router.post('/favorite/:restaurantId', authenticated, userController.addFavorite)
+router.delete('/favorite/:restaurantId', authenticated, userController.removeFavorite)
+
 router.get('/users/:id', authenticated, userController.getUser)
 router.get('/users/:id/edit', authenticated, userController.editUser)
 router.put('/users/:id', authenticated, upload.single('image'), userController.putUser)
