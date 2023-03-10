d0f8988dde6c5a2824070c246c750ecbaaa5f3b0
may huang <mayno1024@gmail.com>
2023-03-10 09:46:59 +0800

    Squashed commit of the following:
    commit b6e42ef7c74fdb21311226b892eeeeb709e75ae8
    Author: may huang <mayno1024@gmail.com>
    Date:   Fri Mar 10 09:24:35 2023 +0800
    
        feat: show and patch admin
    
    commit 1b99f38f5cafdb7b13f4b6fcbb1679904dce79e9
    Author: may huang <mayno1024@gmail.com>
    Date:   Thu Mar 9 17:13:25 2023 +0800
    
        feat:adjust middleware/auth.js
    
    commit 161b4a651530645a7686a009854fb6f401b96e2c
    Merge: 84490be 03c14c0
    Author: may huang <mayno1024@gmail.com>
    Date:   Thu Mar 9 17:00:17 2023 +0800
    
        Merge remote-tracking branch 'origin/R01-test' into R01

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,16 +1,16 @@
-const { ensureAuthenticated, getUser } = require('../helpers/auth-helpers')
+const helpers = require('../helpers/auth-helpers')
 const authenticated = (req, res, next) => {
   // 是否有登入
-  if (ensureAuthenticated(req)) {
+  if (helpers.ensureAuthenticated(req)) {
     return next()
   }
   res.redirect('/signin')
 }
 const authenticatedAdmin = (req, res, next) => {
   // 是否有登入
-  if (ensureAuthenticated(req)) {
+  if (helpers.ensureAuthenticated(req)) {
     // 是否是admin
-    if (getUser(req).isAdmin) return next()
+    if (helpers.getUser(req).isAdmin) return next()
     res.redirect('/')
   } else {
     res.redirect('/signin')
