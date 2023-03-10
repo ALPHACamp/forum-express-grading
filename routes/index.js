41a1c181cac008790b6231d28e2b7268a6161e25
LinTzuAn <a22341254@gmail.com>
2023-03-10 13:19:50 +0800

    feat: add topUser page

--- routes/index.js
+++ routes/index.js
@@ -26,6 +26,7 @@ router.get('/restaurants', authenticated, restController.getRestaurants)
 router.delete('/comments/:id', authenticated, commentController.deleteComment)
 router.post('/comments', authenticated, commentController.postComment)
 
+router.get('/users/top', authenticated, userController.getTopUsers)
 router.get('/users/:id/edit', authenticated, userController.editUser)
 router.put('/users/:id', authenticated, upload.single('image'), userController.putUser)
 router.get('/users/:id', authenticated, userController.getUser)
