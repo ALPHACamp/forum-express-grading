404eb10ea2099fd84dc66f9c3d3c16b8169c2730
MerryHao <mq6721@gmail.com>
2023-03-10 23:22:31 +0800

    feat: modify getDashboard

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -59,13 +59,13 @@ const restaurantController = {
   getDashboard: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
       include: Category,
-      nest: true,
-      raw: true
+      // nest: true,
+      // raw: true
     })
       .then(restaurant => {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
         res.render('dashboard', {
-          restaurant
+          restaurant: restaurant.toJSON()
         })
       })
       .catch(err => next(err))
