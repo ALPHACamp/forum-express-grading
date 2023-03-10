7e1e8fd78e557357daa465630c106eb1eb476c0a
jjerry1225 <i183801225@gmail.com>
2023-03-10 11:24:12 +0800

    #

--- routes/index.js
+++ routes/index.js
@@ -22,20 +22,6 @@ router.get('/restaurants/feeds', authenticated, restController.getFeeds)
 router.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard)
 router.get('/restaurants/:id', authenticated, restController.getRestaurant)
 router.get('/restaurants', authenticated, restController.getRestaurants)
-
-router.delete('/comments/:id', authenticated, commentController.deleteComment)
-router.post('/comments', authenticated, commentController.postComment)
-
-router.post('/favorite/:restaurantId', authenticated, userController.addFavorite)
-router.delete('/favorite/:restaurantId', authenticated, userController.removeFavorite)
-
-router.post('/like/:restaurantId', authenticated, userController.addLike)
-router.delete('/like/:restaurantId', authenticated, userController.removeLike)
-
-router.get('/users/:id/edit', authenticated, userController.editUser)
-router.get('/users/:id', authenticated, userController.getUser)
-router.put('/users/:id', authenticated, upload.single('image'), userController.putUser)
-
 router.get('/', (req, res) => res.redirect('/restaurants'))
 
 router.use('/', generalErrorHandler)
