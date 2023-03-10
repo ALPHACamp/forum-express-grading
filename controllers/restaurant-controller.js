79bbbf6bce1a2238dd5df6ee623a9d4d57fa7ac4
Christal Chen <christalchen1209@gmail.com>
2023-03-10 19:23:57 +0800

    fix

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,44 +1,61 @@
 const { Restaurant, Category } = require('../models')
+
 const restaurantController = {
-  getRestaurants: (req, res) => {
-    return Restaurant.findAll({
-      include: Category,
-      nest: true,
-      raw: true
-    }).then(restaurants => {
-      const data = restaurants.map(r => ({
-        ...r,
-        description: r.description.substring(0, 50)
-      }))
-      return res.render('restaurants', {
-        restaurants: data
+
+  getRestaurants: (req, res, next) => {
+    const categoryId = Number(req.query.categoryId) || ''
+
+    return Promise.all([
+      Restaurant.findAll({
+        include: Category,
+        where: {
+          ...categoryId ? { categoryId } : {}
+        },
+        nest: true,
+        raw: true
+      }),
+      Category.findAll({ raw: true })
+    ])
+      .then(([restaurants, categories]) => {
+        const data = restaurants.map(r => ({
+          ...r,
+          description: r.description.substring(0, 50)
+        }))
+        return res.render('restaurants', {
+          restaurants: data,
+          categories,
+          categoryId
+        })
       })
-    })
+
+      .catch(err => next(err))
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
-
       include: Category
-    }).then(restaurant => {
-      if (!restaurant) throw new Error("Restaurant didn't exist!")
-
-      return restaurant.increment('viewCounts')
-    }).then(restaurant => {
-      res.render('restaurant', { restaurant: restaurant.toJSON() })
     })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        return restaurant.increment('viewCount')
+      })
+      .then(restaurant => {
+        res.render('restaurant', {
+          restaurant: restaurant.toJSON()
+        })
+      })
       .catch(err => next(err))
   },
   getDashboard: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
       include: Category,
+      nest: true,
       raw: true
     })
       .then(restaurant => {
-        if (!restaurant) throw new Error('Restaurant does not exist!')
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
         res.render('dashboard', { restaurant })
       })
       .catch(err => next(err))
   }
-
 }
 module.exports = restaurantController
