4aa890e491f7d5e92b47222d4993e6e81af32cef
peggy8865 <peggy8865@gmail.com>
2023-03-10 21:41:42 +0800

    feat: show comments on restaurant page

--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -1,6 +1,10 @@
 const dayjs = require('dayjs')
+const relativeTime = require('dayjs/plugin/relativeTime')
+dayjs.extend(relativeTime)
+
 module.exports = {
   currentYear: () => dayjs().year(),
+  relativeTimeFromNow: a => dayjs(a).fromNow(),
   role: isAdmin => {
     if (isAdmin) return 'admin'
     return 'user'
