98e8597e5347feaa6e53391790ed08a0001995da
Christal Chen <christalchen1209@gmail.com>
2023-03-10 13:54:37 +0800

    R02 fixing

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -29,7 +29,7 @@ const restaurantController = {
       .catch(err => next(err))
   },
   getDashboard: (req, res, next) => {
-    Restaurant.findByPk(req.params.id, {
+    return Restaurant.findByPk(req.params.id, {
       include: Category,
       raw: true
     })
