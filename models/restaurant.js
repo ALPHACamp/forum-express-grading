b47f6ac58371e32998c2a226e32680048fde5b19
Leon180 <lioung860302@gmail.com>
2023-03-10 12:03:04 +0800

    feat: create favorite model

--- models/restaurant.js
+++ models/restaurant.js
@@ -15,6 +15,11 @@ module.exports = (sequelize, DataTypes) => {
         foreignKey: 'categoryId'
       })
       Restaurant.hasMany(models.Comment, { foreignKey: 'restaurantId' })
+      Restaurant.belongsToMany(models.User, {
+        through: models.Favorite,
+        foreignKey: 'restaurantId',
+        as: 'FavoritedUsers'
+      })
     }
   };
   Restaurant.init({
