abb810c1a1a9ddb09d525ac44b912494245d767b
Christal Chen <christalchen1209@gmail.com>
2023-03-10 18:52:29 +0800

    fix

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,19 +1,24 @@
 const { Restaurant, Category } = require('../models')
 const restaurantController = {
-  getRestaurants: (req, res) => {
-    return Restaurant.findAll({
-      include: Category,
-      nest: true,
-      raw: true
-    }).then(restaurants => {
+  getRestaurants: (req, res, next) => {
+    return Promise.all([
+      Restaurant.findAll({
+        include: Category,
+        nest: true,
+        raw: true
+      }),
+      Category.findAll({ raw: true })
+    ]).then(([restaurants, categories]) => {
       const data = restaurants.map(r => ({
         ...r,
         description: r.description.substring(0, 50)
       }))
       return res.render('restaurants', {
-        restaurants: data
+        restaurants: data,
+        categories
       })
     })
+      .catch(err => next(err))
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,24 +1,24 @@
 const bcrypt = require('bcryptjs')
 const db = require('../models')
-const { User } = db
+const User = db.User
+
 const userController = {
   signUpPage: (req, res) => {
     res.render('signup')
   },
   signUp: (req, res, next) => {
     if (req.body.password !== req.body.passwordCheck) throw new Error('Passwords do not match!')
-    User.findOne({ where: { email: req.body.email } })
-      .then(user => {
-        if (user) throw new Error('Email already exists!')
-        return bcrypt.hash(req.body.password, 10)
-      })
+    User.findOne({ where: { email: req.body.email } }).then(user => {
+      if (user) throw new Error('Email already exists!')
+      return bcrypt.hash(req.body.password, 10)
+    })
       .then(hash => User.create({
         name: req.body.name,
         email: req.body.email,
         password: hash
       }))
       .then(() => {
-        req.flash('success_messages', '成功註冊帳號！')
+        req.flash('success_message', '成功註冊帳號！')
         res.redirect('/signin')
       })
       .catch(err => next(err))
@@ -35,7 +35,5 @@ const userController = {
     req.logout()
     res.redirect('/signin')
   }
-
 }
-
 module.exports = userController
