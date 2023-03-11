c2860e6ae23f102159fe4567038c711c0419b97b
may huang <mayno1024@gmail.com>
2023-03-11 15:33:36 +0800

    Squashed commit of the following:
    commit 382b0caf1b5e893db69901da43c0b9dd140ec01f
    Author: may huang <mayno1024@gmail.com>
    Date:   Sat Mar 11 15:21:21 2023 +0800
    
        feat: dashboard
    
    commit 461ffb1eaebd55f08c54ba0cde264d7d1b33bb20
    Merge: 2e5bdd5 9d566b6
    Author: may huang <mayno1024@gmail.com>
    Date:   Fri Mar 10 21:27:57 2023 +0800
    
        Merge remote-tracking branch 'upstream/R02-test' into R02
    
    commit 9d566b6e0de4de3ba4dc33137230dc8cce77a37b
    Author: zjzheng17 <90173539+zjzheng17@users.noreply.github.com>
    Date:   Mon Jul 18 12:43:45 2022 +0800
    
        correcting typo in comment
    
    commit b54a2e99ab2d9f4d1a6d4fe0393213518626296c
    Author: Amber Yan <freehourse2sh@gmail.com>
    Date:   Wed Mar 2 15:27:03 2022 +0800
    
        add R02.test.js file
    
    commit fca7f3bc73dc9fa9bfadef198304b1ba88e6a44b
    Author: Carrot7712 <carrot7712@gmail.com>
    Date:   Thu Jan 13 16:48:07 2022 +0800
    
        feat: add R01.test.js and unit-test-helpers.js

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,31 +1,42 @@
 const { Restaurant, Category } = require('../models')
-const restaurantController = {
+const restController = {
   getRestaurants: (req, res, next) => {
     return Restaurant.findAll({
-      include: Category,
+      include: [Category],
       nest: true,
       raw: true
     })
       .then(restaurants => {
-        console.log(restaurants)
         const data = restaurants.map(r => ({
           ...r,
           description: r.description.substring(0, 50)
         }))
         return res.render('restaurants', { restaurants: data })
       })
+      .catch(err => next(err))
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
-      include: Category,
-      nest: true,
-      raw: true
+      include: Category
     })
       .then(restaurant => {
         if (!restaurant) throw new Error("restaurant didn't exist")
-        res.render('restaurant', { restaurant })
+        restaurant.increment('viewCounts')
+        res.render('restaurant', { restaurant: restaurant.toJSON() })
+      })
+      .catch(err => next(err))
+  },
+  getDashboard: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, {
+      include: Category,
+      raw: true,
+      nest: true
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("restaurant didn't exist")
+        return res.render('dashboard', { restaurant })
       })
       .catch(err => next(err))
   }
 }
-module.exports = restaurantController
+module.exports = restController
