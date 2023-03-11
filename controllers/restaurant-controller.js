ca1a325b314308753f9c51a4b671975685c5c2c0
Ian_Lin <ian920511@gmail.com>
2023-03-11 11:59:09 +0800

    feat: add TopRestaurant page & function

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -108,6 +108,25 @@ const restaurantsController = {
         })
       })
       .catch(error => next(error))
+  },
+
+  getTopRestaurants: (req, res, next) => {
+    return Restaurant.findAll({
+      include: [{ model: User, as: 'FavoritedUsers' }]
+    })
+      .then(restaurants => {
+        const result = restaurants.map(r => ({
+          ...r.toJSON(),
+          description: r.description.substring(0, 50),
+          favoritedCount: r.FavoritedUsers.length,
+          isFavorited: req.user && req.user.FavoritedRestaurants.some(fr => fr.id === r.id)
+        }))
+          .sort((a, b) => (b.favoritedCount - a.favoritedCount))
+          .slice(0, 10)
+
+        return res.render('top-restaurants', { restaurants: result })
+      })
+      .catch(error => next(error))
   }
 }
 
