d5f53848986d4954e5d9ca34d3d4e0e61e5f915e
MerryHao <mq6721@gmail.com>
2023-03-11 21:41:07 +0800

    feat: add feeds page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -69,6 +69,31 @@ const restaurantController = {
         })
       })
       .catch(err => next(err))
+  },
+  getFeeds: (req, res, next) => {
+    return Promise.all([
+      Restaurant.findAll({
+        limit: 10,
+        order: [['createdAt', 'DESC']],
+        include: [Category],
+        raw: true,
+        nest: true
+      }),
+      Comment.findAll({
+        limit: 10,
+        order: [['createdAt', 'DESC']],
+        include: [User, Restaurant],
+        raw: true,
+        nest: true
+      })
+    ])
+      .then(([restaurants, comments]) => {
+        res.render('feeds', {
+          restaurants,
+          comments
+        })
+      })
+      .catch(err => next(err))
   }
 }
 module.exports = restaurantController
