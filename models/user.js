ffd0faa09ddab0ed74007082121ad75620efdaa5
abbie930 <abbiecheng930@gmail.com>
2023-03-11 15:24:43 +0800

    feat: create followship model

--- models/user.js
+++ models/user.js
@@ -22,6 +22,18 @@ module.exports = (sequelize, DataTypes) => {
         foreignKey: 'userId',
         as: 'LikedRestaurants'
       })
+      // User的追蹤者
+      User.belongsToMany(User, {
+        through: models.Followship,
+        foreignKey: 'followingId',
+        as: 'Followers'
+      })
+      // User追蹤中的 user
+      User.belongsToMany(User, {
+        through: models.Followship,
+        foreignKey: 'followerId',
+        as: 'Followings'
+      })
     }
   };
   User.init(
