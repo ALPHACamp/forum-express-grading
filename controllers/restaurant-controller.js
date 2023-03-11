3ccf9a232da0677c6389fa23aaa3ee2495947ec2
FrankHuang <frankcool321@gmail.com>
2023-03-11 12:07:07 +0800

    feat: add getTopRestaurant page and modify restaurant controller

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,5 +1,5 @@
 // - 處理屬於前台restaurant路由的相關請求
-const { Restaurant, Category, User, Comment } = require('../models')
+const { Restaurant, Category, User, Comment, sequelize } = require('../models')
 const { getUser } = require('../helpers/auth-helpers')
 const { getOffset, getPagination } = require('../helpers/pagination-helper')
 const restaurantController = {
@@ -122,6 +122,57 @@ const restaurantController = {
     } catch (error) {
       return next(error)
     }
+  },
+  getTopRestaurants: async (req, res, next) => {
+    // - 作法一 - 在資料庫撈取全部資料再處理
+    try {
+      const restaurants = await Restaurant.findAll({
+        include: [{ model: User, as: 'FavoritedUsers', required: true }]
+      })
+      const result = restaurants
+        .map(r => ({
+          ...r.toJSON(),
+          description: r.description.substring(0, 50),
+          favoritedCount: r.FavoritedUsers.length,
+          isFavorited:
+            getUser(req) &&
+            getUser(req).FavoritedRestaurants.some(f => f.id === r.id)
+        }))
+        .sort((a, b) => b.favoritedCount - a.favoritedCount)
+      return res.render('top-restaurants', { restaurants: result })
+    } catch (error) {
+      return next(error)
+    }
+    // - 作法二 - 在資料庫處理資料再回傳
+    // try {
+    //   const restaurants = await Restaurant.findAll({
+    //     subQuery: false, // - 當有 limit 屬性時需加上取消子查詢屬性
+    //     // -改為inner join 篩選有 FavoritedUsers 餐廳
+    //     include: [{ model: User, as: 'FavoritedUsers', required: true }],
+    //     // -額外查詢COUNT(`FavoritedUsers.id`) 作為排序標準
+    //     attributes: {
+    //       include: [
+    //         [
+    //           sequelize.fn('COUNT', sequelize.col('FavoritedUsers.id')),
+    //           'favoritedCount'
+    //         ]
+    //       ]
+    //     },
+    //     group: 'Restaurant.id', // -GROUP BY `Restaurant.id`
+    //     order: [['favoritedCount', 'DESC']],
+    //     limit: 10
+    //   })
+    //   const result = restaurants.map(r => ({
+    //     ...r.toJSON(),
+    //     description: r.description.substring(0, 50),
+    //     isFavorited:
+    //       getUser(req) &&
+    //       getUser(req).FavoritedRestaurants.some(f => f.id === r.id)
+    //   }))
+    //   return res.render('top-restaurants', { restaurants: result })
+    // } catch (error) {
+    //   return next(error)
+    // }
   }
 }
 
--- routes/index.js
+++ routes/index.js
@@ -33,6 +33,7 @@ router.post(
 )
 router.get('/logout', userController.logout)
 
+router.get('/restaurants/top', authenticated, restController.getTopRestaurants)
 router.get('/restaurants/feeds', authenticated, restController.getFeeds)
 router.get(
   '/restaurants/:id/dashboard',
