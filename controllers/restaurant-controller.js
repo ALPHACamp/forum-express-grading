4bcfbff60186689ac4b58ef14c3343c4ab5d3537
DESKTOP-NE7KTJ6\zell0 <zell0323@hotmail.com>
2023-03-12 11:50:18 +0800

    new

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,21 +1,39 @@
+const { getOffset, getPagination } = require('../helpers/pagination-helper') // 加入這行
 const { Restaurant, Category, Comment, User } = require('../models')
 const restaurantController = {
-  getRestaurants: (req, res, next) => {
-    return Restaurant.findAll({
-      include: Category,
-      nest: true,
-      raw: true
-    }).then(restaurants => {
-      const data = restaurants.map(r => ({
-        ...r,
-        description: r.description.substring(0, 50)
-      }))
-      return res.render('restaurants', {
-        restaurants: data
+  getRestaurants: (req, res) => {
+    const DEFAULT_LIMIT = 9 // 加入這行
+    const categoryId = Number(req.query.categoryId) || '' // 新增這裡，從網址上拿下來的參數是字串，先轉成 Number 再操作
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || DEFAULT_LIMIT
+    const offset = getOffset(limit, page)
+
+    return Promise.all([
+      Restaurant.findAndCountAll({
+        include: Category,
+        where: { // 新增查詢條件
+          ...categoryId ? { categoryId } : {} // 檢查 categoryId 是否為空值
+        },
+        limit, // 增加這裡
+        offset, // 增加這裡
+        nest: true,
+        raw: true
+      }),
+      Category.findAll({ raw: true })
+    ])
+      .then(([restaurants, categories]) => {
+        const data = restaurants.rows.map(r => ({
+          ...r,
+          description: r.description.substring(0, 50)
+        }))
+        return res.render('restaurants', {
+          restaurants: data,
+          categories,
+          categoryId, // 新增這裡
+          pagination: getPagination(limit, page, restaurants.count)
+        })
       })
-    })
-      .catch(err => next(err))
-  }, // 加逗號，新增以下
+  },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
       include: [
