0d5ab8838fb329bcfd9d543bfede831e492ae08c
peggy8865 <peggy8865@gmail.com>
2023-03-12 17:40:40 +0800

    feat: create followship model

--- models/user.js
+++ models/user.js
@@ -21,6 +21,16 @@ module.exports = (sequelize, DataTypes) => {
         foreignKey: 'userId',
         as: 'LikedRestaurants'
       })
+      User.belongsToMany(User, { // 我可以被很多人追蹤
+        through: models.Followship,
+        foreignKey: 'followingId', // 我的主鍵 = 別人的外鍵 = 'followingId'
+        as: 'Followers' // 呼叫 user(我).Followers 將回傳所有追蹤我的人 => user(別人).followingId === user(我).id
+      })
+      User.belongsToMany(User, { // 我可以追蹤很多人
+        through: models.Followship,
+        foreignKey: 'followerId', // 我的主鍵 = 別人的外鍵 = 'followerId'
+        as: 'Followings' // 呼叫 user(我).Followings 將回傳所有我追蹤的人 => user(別人).followerId === user(我).id
+      })
       // define association here
     }
   };
