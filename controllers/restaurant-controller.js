24dc7176fc8f94a2d7ae06066abefbc05a097979
MerryHao <mq6721@gmail.com>
2023-03-12 11:24:24 +0800

    feat: get user's favorited restaurants & switch button

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -23,9 +23,16 @@ const restaurantController = {
     ])
     .then(([restaurants, categories]) => {
       //console.log(restaurants)
+      const favoritedRestaurantsId = req.user && req.user.FavoritedRestaurants.map(fr => fr.id)
       const data = restaurants.rows.map(r => ({
         ...r,
-        description: r.description.substring(0, 50)
+        description: r.description.substring(0, 50),
+        isFavorited: favoritedRestaurantsId.includes(r.id)
+        /** 
+         * 取出使用者的收藏清單，然後map成id清單，之後用Array的includes方法進行比對，最後會回傳布林值。
+         * 不過因為req.user可能是空的，謹慎起見我們要寫成 req.user && req.user.FavoritedRestaurants... 先做檢查。
+         * 整段程式碼的意思就是說要來看看現在這間餐廳是不是有被使用者收藏，有的話 isFavorited 就會是 true，否則會是 false。
+         **/
       }))
       return res.render('restaurants', {
         restaurants: data,
@@ -39,7 +46,8 @@ const restaurantController = {
     return Restaurant.findByPk(req.params.id, {
       include: [
         Category,
-          { model: Comment, include: User }
+        { model: Comment, include: User },
+        { model: User, as: 'FavoritedUsers' }
       ] // 拿出關聯的 Category model
       // nest: true,
       // raw: true
@@ -50,8 +58,14 @@ const restaurantController = {
         return restaurant.increment('viewCount')
       })
       .then(restaurant => {
+        const isFavorited = restaurant.FavoritedUsers.some(f => f.id === req.user.id)
+        /**
+         * 這裡我們是使用 some 方法來操作陣列。使用 some 的好處是只要帶迭代過程中找到一個符合條件的項目後，就會立刻回傳 true，後面的項目不會繼續執行。
+         * 比起 map 方法無論如何都會從頭到尾把陣列裡的項目執行一次，some 因為加入了判斷條件 f.id === req.user.id，可以有效減少執行次數。
+         **/
         res.render('restaurant', {
-          restaurant: restaurant.toJSON()
+          restaurant: restaurant.toJSON(),
+          isFavorited
         })
       })
       .catch(err => next(err))
