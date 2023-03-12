f2cffd24f2cb9bbe78d463709d1fb0905327a532
chachagof <chacha0932@gmail.com>
2023-03-12 16:57:10 +0800

    fix restaurant-controller for test

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -115,22 +115,20 @@ const restaurantController = {
   },
   getTopRestaurants: (req, res, next) => {
     return Restaurant.findAll({
-      include: {
-        model: User,
-        as: 'FavoritedUsers'
-      }
+      include: [{
+        model: User, as: 'FavoritedUsers'
+      }]
     })
       .then(restaurants => {
-        const data = restaurants
-          .map(r => ({
-            ...r.dataValues,
-            description: r.dataValues.description.substring(0, 50),
-            favoritedCount: r.FavoritedUsers.length,
-            isFavorited: req.user && req.user.FavoritedRestaurants.map(d => d.id).includes(r.id)
-          }))
-          .sort((a, b) => b.favoritedCount - a.favoritedCount)
-          .slice(0, 10)
-        return res.render('top-restaurants', { restaurants: data })
+        restaurants = restaurants.map(r => ({
+          ...r.dataValues,
+          description: r.dataValues.description.substring(0, 50),
+          favoritedCount: r.FavoritedUsers.length,
+          isFavorited: req.user && req.user.FavoritedRestaurants.map(d => d.id).includes(r.id)
+        }))
+        restaurants.sort((a, b) => b.favoritedCount - a.favoritedCount)
+        restaurants = restaurants.slice(0, 10)
+        res.render('top-restaurants', { restaurants })
       })
       .catch(err => next(err))
   }
