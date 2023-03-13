6cb2e27ffaa026348fdcb03b86524080cff08f99
yy933 <emilyyu025@gmail.com>
2023-03-14 00:20:04 +0800

    fix migration

---
+++ migrations/20230312060506-add-image-to-user.js
@@ -0,0 +1,16 @@
+'use strict'
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('Users', 'image', {
+      type: Sequelize.STRING,
+      allowNull: false,
+      defaultValue:
+        'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png'
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('Users', 'image')
+  }
+}
