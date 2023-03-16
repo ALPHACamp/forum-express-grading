bc050a427a9634de03f8f694aad3c78356bb6d29
Christal Chen <christalchen1209@gmail.com>
2023-03-16 14:33:15 +0800

    feat:  create favorite model

--- models/user.js
+++ models/user.js
@@ -11,6 +11,11 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate (models) {
       User.hasMany(models.Comment, { foreignKey: 'userId' })
+      User.belongsToMany(models.Restaurant, {
+        through: models.Favorite,
+        foreignKey: 'userId',
+        as: 'FavoritedRestaurants'
+      })
     }
   };
   User.init({
