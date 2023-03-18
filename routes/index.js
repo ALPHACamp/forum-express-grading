8cfa8b5ed1a6a44ed7999c7fbedbcdf18b98d389
ImAmyKe <xiahamy@gmail.com>
2023-03-18 11:37:06 +0800

    feat: flash msg & signup verification&feat: passport init & signin

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,26 @@
 const express = require('express')
 const router = express.Router()
+const passport = require('../config/passport')
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
 const admin = require('./modules/admin')
+const { generalErrorHandler } = require('../middleware/error-handler')
 
 // admin
 router.use('/admin', admin)
 
-// sign up
+// sign up, sign in, logout
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
+router.get('/signin', userController.signInPage)
+router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
+router.get('/logout', userController.logout)
 
 // restaurants
 router.get('/restaurants', restController.getRestaurants)
 router.use('/', (req, res) => res.redirect('/restaurants'))
+
+// error
+router.use('/', generalErrorHandler)
 
 module.exports = router
