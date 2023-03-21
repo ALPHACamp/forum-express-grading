130c71d2a113d62b304e4b8ed72a08a9871ee862
ImAmyKe <xiahamy@gmail.com>
2023-03-21 20:23:18 +0800

    feat: add restaurant page

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,7 @@ router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
 router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
+router.get('/restaurants/:id', authenticated, restController.getRestaurant)
 router.get('/logout', userController.logout)
 
 // restaurants
