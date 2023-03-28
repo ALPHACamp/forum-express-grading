c573a44a040395ab039d2176530b3a9e475207df
ImAmyKe <xiahamy@gmail.com>
2023-03-28 23:02:05 +0800

    feat: add feeds page

--- routes/index.js
+++ routes/index.js
@@ -23,6 +23,7 @@ router.post('/signin', passport.authenticate('local', { failureRedirect: '/signi
 router.get('/logout', userController.logout)
 // restaurants
 router.get('/restaurants', authenticated, restController.getRestaurants)
+router.get('/restaurants/feeds', authenticated, restController.getFeeds)
 router.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard)
 router.get('/restaurants/:id', authenticated, restController.getRestaurant)
 // comments
