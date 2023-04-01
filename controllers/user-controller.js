327deeb5b081442380ee9176016a2eb83a0ed3b0
YuZheLin1987 <billlinxx@gmail.com>
2023-04-01 14:46:56 +0800

    Squashed commit of the following:
    commit d56eb26ffbe48f79017b7201df81ab307818cec6
    Author: YuZheLin1987 <billlinxx@gmail.com>
    Date:   Sat Apr 1 14:22:02 2023 +0800
    
        feat: add commented restaurant to profile page
    
    commit 473c5e369be3e5462ca5a14d6ca26f69dc082e1c
    Author: YuZheLin1987 <billlinxx@gmail.com>
    Date:   Tue Mar 28 21:48:55 2023 +0800
    
        feat: add user profile page & edit profile
    
    commit 97ed23fabb8fa6593308781097a4bb7aefbdd6ef
    Merge: 822c6ae 4fe60b0
    Author: YuZheLin1987 <billlinxx@gmail.com>
    Date:   Sun Mar 26 22:22:12 2023 +0800
    
        Merge remote-tracking branch 'origin/R03-test' into R03
    
    commit 4fe60b0a0771173e7a6d4a57404c645237bb7d7e
    Author: zjzheng17 <90173539+zjzheng17@users.noreply.github.com>
    Date:   Mon Jul 18 12:48:56 2022 +0800
    
        correcting typo in comment
    
    commit ef44c3efaf42025c138378e9f42035f25c330526
    Author: Amber Yan <freehourse2sh@gmail.com>
    Date:   Wed Mar 2 15:27:56 2022 +0800
    
        add R03.test.js file
    
    commit 96da4bee5176c5e643633f5bc462405ff38c6660
    Author: Carrot7712 <carrot7712@gmail.com>
    Date:   Thu Jan 13 18:01:55 2022 +0800
    
        feat:add R02.test.js
    
    commit fca7f3bc73dc9fa9bfadef198304b1ba88e6a44b
    Author: Carrot7712 <carrot7712@gmail.com>
    Date:   Thu Jan 13 16:48:07 2022 +0800
    
        feat: add R01.test.js and unit-test-helpers.js

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcryptjs')
 const db = require('../models')
-const { User } = db
+const { User, Restaurant, Comment } = db
+const { imgurFileHandler } = require('../helpers/file-helpers')
 const userController = {
   signUpPage: (req, res) => {
     res.render('signup')
@@ -37,6 +38,57 @@ const userController = {
     req.flash('success_messages', '登出成功！')
     req.logout()
     res.redirect('/signin')
+  },
+  getUser: (req, res, next) => {
+    return Promise.all([
+      User.findByPk(req.params.id, {
+        raw: true
+      }),
+      Comment.findAll({
+        where: { userId: req.params.id },
+        attributes: ['restaurantId'],
+        group: ['restaurantId'],
+        include: [Restaurant],
+        raw: true,
+        nest: true
+      })
+    ])
+      .then(([user, comments]) => {
+        if (!user) throw new Error("User doesn't exist!")
+        return res.render('users/profile', { user, comments })
+      })
+      .catch(err => next(err))
+  },
+  editUser: (req, res, next) => {
+    return User.findByPk(req.params.id, {
+      raw: true
+    })
+      .then(user => {
+        if (!user) throw new Error("User didn't exist!")
+        return res.render('users/edit', { user })
+      })
+      .catch(err => next(err))
+  },
+  putUser: (req, res, next) => {
+    const { name } = req.body
+    const { file } = req
+    return Promise.all([
+      User.findByPk(req.user.id),
+      imgurFileHandler(file)
+    ])
+      .then(([user, filePath]) => {
+        if (!user) throw new Error("User didn't exist!")
+        if (user.id !== Number(req.params.id)) throw new Error('Edit self profile only!')
+        return user.update({
+          name,
+          image: filePath || user.image
+        })
+      })
+      .then(() => {
+        req.flash('success_messages', '使用者資料編輯成功')
+        return res.redirect(`/users/${req.user.id}`)
+      })
+      .catch(err => next(err))
   }
 }
 module.exports = userController
