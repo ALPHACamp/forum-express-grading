9e6a61f1dcdd319ae67f5d58ebd316c8a8f71dbd
LAPTOP-DHHLQT9N\wert7 <wert723jim@gmail.com>
2023-04-06 11:06:15 +0800

    feat:add admin index page

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.send('Hello World!')
+const restController = require('../controllers/restaurant-controller')
+const admin = require('./modules/admin')
+router.use('/admin', admin)
+// 注意路由擺放順序，匹配條件多的寫在前面
+router.get('/restaurants', restController.getRestaurants)
+// fallback 路由，若req都沒有匹配到，最後就會進入這
+router.use('/', (req, res) => {
+  res.redirect('/restaurants')
 })
 
 module.exports = router
