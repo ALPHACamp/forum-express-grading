c50fcd22f81837b6496ff3f4b3125bc339d66464
LAPTOP-DHHLQT9N\wert7 <wert723jim@gmail.com>
2023-04-12 10:48:11 +0800

    feat: add footer

--- app.js
+++ app.js
@@ -5,12 +5,13 @@ const flash = require('connect-flash')
 const session = require('express-session')
 const passport = require('passport')
 const { getUser } = require('./helpers/auth-helpers')
+const handlebarsHelper = require('./helpers/handlebars-helper')
 
 const app = express()
 const port = process.env.PORT || 3000
 const SESSION_SECRET = 'secret'
 
-app.engine('hbs', handlebars({ extname: '.hbs' }))
+app.engine('hbs', handlebars({ extname: '.hbs', helpers: handlebarsHelper }))
 
 app.use(express.urlencoded({ extended: true }))
 
