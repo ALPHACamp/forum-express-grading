c63ae10fe675e8bcbc996f2ac61a8212aaf7f3bd
CHUCHUDAN <daniel870217@gmail.com>
2023-04-20 14:13:19 +0800

    feat: flash msg & signup verification

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
 const routes = require('./routes')
+const session = require('express-session')
+const flash = require('connect-flash')
+const SESSION_SECRET = 'secret'
 const app = express()
 const port = process.env.PORT || 3000
 
 app.engine('hbs', handlebars({ extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
+app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
+app.use(flash()) // 掛載套件
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages') // 設定 success_msg 訊息
+  res.locals.error_messages = req.flash('error_messages') // 設定 warning_msg 訊息
+  next()
+})
 app.use(routes)
 app.listen(port, () => {
   console.info(`Example app listening on port ${port}!`)
