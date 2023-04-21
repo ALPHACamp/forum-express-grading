e4e41996d54e4e05941b9f79c71cf086b11c66b9
LAPTOP-DHHLQT9N\wert7 <wert723jim@gmail.com>
2023-04-21 14:41:07 +0800

    feat: modify admin users page & add nav tab in restaurants page

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,4 +1,4 @@
-const { Restaurant } = require('../models')
+const { Restaurant, User } = require('../models')
 const { imgurFileHandler } = require('../helpers/file-helper')
 const adminController = {
   getRestaurants: (req, res, next) => {
@@ -93,6 +93,33 @@ const adminController = {
         res.redirect('/admin/restaurants')
       })
       .catch(err => next(err))
+  },
+  getUsers: (req, res, next) => {
+    return User.findAll({
+      raw: true
+    })
+      .then(users => res.render('admin/users', { users }))
+      .catch(err => next(err))
+  },
+  patchUser: (req, res, next) => {
+    const id = req.params.id
+    return User.findByPk(id)
+      .then(user => {
+        if (!user) throw new Error("User didn't exist!")
+        const { isAdmin } = user
+        if (user.name === 'admin') throw new Error('禁止變更 root 權限')
+        return user.update({
+          isAdmin: !isAdmin
+        })
+      })
+      .then(() => {
+        req.flash('success_messages', '使用者權限變更成功')
+        res.redirect('/admin/users')
+      })
+      .catch(err => {
+        req.flash('error_messages', err.message)
+        res.redirect('back')
+      })
   }
 }
 
