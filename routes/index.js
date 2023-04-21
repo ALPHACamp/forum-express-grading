370dfbc773d71d726dc265eb220e92a1950d9466
LongXiangL <bfh1104@gmail.com>
2023-04-22 03:09:32 +0800

    fea: add admin index paget

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const restaurantController = require('../controllers/restaurant-controller')
 
+const restaurantController = require('../controllers/restaurant-controller')
+const admin = require('./modules/admin') // 新增這行，載入 admin.js
+router.use('/admin', admin) // 新增這行
 router.get('/', (req, res) => res.redirect('/restaurant'))
 
 router.get('/restaurant', restaurantController.getRestaurants)
