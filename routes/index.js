0c5f89161c8fc7f25d2476ee9e140379654f0832
popojk <c.alexwu@gmail.com>
2023-04-23 23:49:13 +0800

    feat:add index page

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const router = express.Router()
+// load controller
+const restController = require('../controllers/restaurant-controller')
 
 router.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+router.get('/restaurants', restController.getRestaurants)
+router.use('/', (req, res) => res.redirect('/restaurants'))
 
 module.exports = router
