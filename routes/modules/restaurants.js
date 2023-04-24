b124bebe951f055bc2a4323163e64bc0c44a4faa
LinZH-1995 <lin84610@gmail.com>
2023-04-24 22:08:36 +0800

    merge --squash R02

--- routes/modules/restaurants.js
+++ routes/modules/restaurants.js
@@ -3,6 +3,8 @@ const router = express.Router()
 
 const restController = require('../../controllers/restaurant-controller.js')
 
+router.get('/:id/dashboard', restController.getDashboard)
+
 router.get('/:id', restController.getRestaurant)
 
 router.get('/', restController.getRestaurants)
