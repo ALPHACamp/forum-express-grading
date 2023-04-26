6a55ab19f6e629922f0ceca770d5e322ec4059e6
LinZH-1995 <lin84610@gmail.com>
2023-04-26 20:41:44 +0800

    feat: fix files

--- models/user.js
+++ models/user.js
@@ -16,6 +16,11 @@ module.exports = (sequelize, DataTypes) => {
         foreignKey: 'userId',
         as: 'FavoritedRestaurants'
       })
+      User.belongsToMany(models.Restaurant, {
+        through: models.Like,
+        foreignKey: 'userId',
+        as: 'LikedRestaurants'
+      })
     }
   };
   User.init({
