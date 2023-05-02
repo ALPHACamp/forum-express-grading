806145461aafbb90f519e6422340f66751f7e3c3
LAPTOP-DHHLQT9N\wert7 <wert723jim@gmail.com>
2023-05-02 11:11:43 +0800

    feat: show comment on restaurant page

--- models/restaurant.js
+++ models/restaurant.js
@@ -12,7 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate (models) {
       // define association here
       Restaurant.belongsTo(models.Category, { foreignKey: 'categoryId' })
-      Restaurant.hasMany(models.Comment, { foreignKey: 'RestaurantId' })
+      Restaurant.hasMany(models.Comment, { foreignKey: 'restaurantId' })
     }
   };
   Restaurant.init({
