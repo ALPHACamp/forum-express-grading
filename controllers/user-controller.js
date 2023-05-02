f28f1395e134053c7c8c737081ce538424c4c67a
LAPTOP-DHHLQT9N\wert7 <wert723jim@gmail.com>
2023-05-02 17:29:42 +0800

    feat: add update user

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcryptjs')
 const db = require('../models')
 const { User } = db
+const { imgurFileHandler } = require('../helpers/file-helper')
 const userController = {
   signUpPage: (req, res) => {
     return res.render('signup')
@@ -40,7 +41,7 @@ const userController = {
     res.redirect('/signin')
   },
   getUser: (req, res, next) => {
-    User.findByPk(req.params.id, { raw: true })
+    return User.findByPk(req.params.id, { raw: true })
       .then(user => {
         if (!user) throw new Error("User doesn't exist!")
         res.render('users/profile', { user })
@@ -48,11 +49,36 @@ const userController = {
       .catch(err => next(err))
   },
   editUser: (req, res, next) => {
-    User.findByPk(req.params.id, { raw: true })
+    return User.findByPk(req.params.id, { raw: true })
       .then(user => {
         if (!user) throw new Error("User doesn't exist!")
         res.render('users/edit', { user })
       })
+  },
+  putUser: (req, res, next) => {
+    const { name, email } = req.body
+    if (!name) throw new Error('User name is required!')
+    // if (!email) throw new Error('User email is required!')
+
+    const { file } = req
+
+    return Promise.all([
+      imgurFileHandler(file),
+      User.findByPk(req.params.id)
+    ])
+      .then(([filePath, user]) => {
+        if (!user) throw new Error("User doesn't exist!")
+        return user.update({
+          name,
+          email: email || user.email,
+          image: filePath || user.image
+        })
+      })
+      .then(() => {
+        req.flash('success_messages', '使用者資料編輯成功')
+        res.redirect(`/users/${req.params.id}`)
+      })
+      .catch(err => next(err))
   }
 }
 
