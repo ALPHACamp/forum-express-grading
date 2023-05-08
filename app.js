9b8e71ae6547a88eb704192dccffd495faf4a211
popojk <c.alexwu@gmail.com>
2023-05-08 19:21:41 +0800

    feat: add user model

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const handlebars = require('express-handlebars')
 const routes = require('./routes')
 const app = express()
 const port = process.env.PORT || 3000
+const db = require('./models')
 app.engine('hbs', handlebars({ extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(routes)
-
 app.listen(port, () => {
   console.info(`Example app listening on port ${port}!`)
 })
