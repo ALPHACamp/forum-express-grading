a1d6eea4eb94c403181377f6b8c7013512becdf2
Edward Kuan <edward880814@gmail.com>
2023-05-09 17:26:59 +0800

    feat: passport init and sigin

--- app.js
+++ app.js
@@ -2,19 +2,20 @@ const express = require('express')
 const handlebars = require('express-handlebars')
 const flash = require('connect-flash')
 const session = require('express-session')
+const passport = require('./config/passport')
 
 const routes = require('./routes')
 
 const app = express()
 const port = process.env.PORT || 3000
 const SESSION_SECRET = 'secret'
-
 app.engine('hbs', handlebars({ extname: '.hbs' }))
 app.set('view engine', 'hbs')
-
 app.use(express.urlencoded({ extended: true }))
 
 app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
+app.use(passport.initialize())
+app.use(passport.session())
 app.use(flash())
 
 app.use((req, res, next) => {
@@ -22,9 +23,7 @@ app.use((req, res, next) => {
   res.locals.error_messages = req.flash('error_messages')
   next()
 })
-
 app.use(routes)
-
 app.listen(port, () => {
   console.info(`Example app listening on port ${port}!`)
 })
