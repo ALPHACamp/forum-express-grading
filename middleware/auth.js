dbfdb3910eea0d0b7158aecebefa984ab33444dc
tschiang23 <tien23@gmail.com>
2023-05-09 11:04:13 +0800

    feat:user model add is_admin

---
+++ middleware/auth.js
@@ -0,0 +1,26 @@
+// middleware/auth.js
+const { ensureAuthenticated, getUser } = require('../helpers/auth-helpers')
+
+// 驗證使用者是否有登入
+const authenticated = (req, res, next) => {
+  // if (req.isAuthenticated)
+  if (ensureAuthenticated(req)) {
+    return next()
+  }
+  res.redirect('/signin')
+}
+
+// 驗證使用者是否為管理員
+const authenticatedAdmin = (req, res, next) => {
+  // if (req.isAuthenticated)
+  if (ensureAuthenticated(req)) {
+    if (getUser(req).isAdmin) return next()
+    res.redirect('/')
+  } else {
+    res.redirect('/signin')
+  }
+}
+module.exports = {
+  authenticated,
+  authenticatedAdmin
+}
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const restController = require('../controllers/restaurant-controller')
 const admin = require('./modules/admin')
 const userController = require('../controllers/user-controller')
+const { authenticated } = require('../middleware/auth')
 const { generalErrorHandler } = require('../middleware/error-handler')
 const passport = require('../config/passport')
 
@@ -14,7 +15,7 @@ router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
 router.get('/logout', userController.logout)
 
-router.get('/restaurants', restController.getRestaurants)
+router.get('/restaurants', authenticated, restController.getRestaurants)
 
 // 設定fallback 路由
 router.get('/', (req, res) => res.redirect('/restaurants'))
