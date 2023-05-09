cfd9a77dd6131b972a9aa4b2c17935e465c43cbf
Edward Kuan <edward880814@gmail.com>
2023-05-09 14:22:26 +0800

    feat: user signup

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,17 @@
 const express = require('express')
 const router = express.Router()
-
-// 新增，載入 controller
+const admin = require('./modules/admin')
 
 const restController = require('../controllers/restaurant-controller')
-const admin = require('./modules/admin') // 新增這行，載入 admin.js
 
-router.use('/admin', admin) // 新增這行
+const userController = require('../controllers/user-controller')
 
-// 新增
+router.use('/admin', admin)
+
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
+
 router.get('/restaurants', restController.getRestaurants)
-
 router.use('/', (req, res) => res.redirect('/restaurants'))
 
 module.exports = router
