9e7aa2cf3ef87570d4b66daf28d8b2e44b4fd694
CHUCHUDAN <daniel870217@gmail.com>
2023-05-09 15:49:18 +0800

    feat: add restaurant image

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,19 +1,20 @@
 const express = require('express')
 const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
+const upload = require('../../middleware/multer')
 
 // 新增餐廳頁面
 router.get('/restaurants/create', adminController.createRestaurant)
 // 編輯餐廳頁面
 router.get('/restaurants/:id/edit', adminController.editRestaurant)
 // 編輯餐廳功能
-router.put('/restaurants/:id', adminController.putRestaurant)
+router.put('/restaurants/:id', upload.single('image'), adminController.putRestaurant)
 // 瀏覽單一餐廳
 router.get('/restaurants/:id', adminController.getRestaurant)
 // 刪除餐廳功能
 router.delete('/restaurants/:id', adminController.deleteRestaurant)
 // 新增餐廳功能
-router.post('/restaurants', adminController.postRestaurant)
+router.post('/restaurants', upload.single('image'), adminController.postRestaurant)
 // 瀏覽全部頁面
 router.get('/restaurants', adminController.getRestaurants)
 // 無法匹配路由皆會丟到這
