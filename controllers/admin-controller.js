9f82c6dc64b62fd7cb9d5dd6c5a39898dfba9ffc
tschiang23 <tien23@gmail.com>
2023-05-09 13:26:24 +0800

    feat:admin update restaurant

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -36,6 +36,33 @@ const adminController = {
 
       res.render('admin/restaurant', { restaurant })
     } catch (err) { next(err) }
+  },
+  editRestaurant: async (req, res, next) => {
+    try {
+      const restaurant = await Restaurant.findByPk(req.params.id, { raw: true })
+      if (!restaurant) throw new Error("Restaurant didn't exist!")
+
+      res.render('admin/edit-restaurant', { restaurant })
+    } catch (err) { next(err) }
+  },
+  putRestaurant: async (req, res, next) => {
+    try {
+      const { name, tel, address, openingHours, description } = req.body
+      if (!name) throw new Error('Restaurant name is required!')
+
+      const restaurant = await Restaurant.findByPk(req.params.id)
+      if (!restaurant) throw new Error("Restaurant didn't exist!")
+
+      await restaurant.update({
+        name,
+        tel,
+        address,
+        openingHours,
+        description
+      })
+      req.flash('success_messages', 'restaurant was successfully to update')
+      res.redirect('/admin/restaurants')
+    } catch (err) { next(err) }
   }
 }
 module.exports = adminController
