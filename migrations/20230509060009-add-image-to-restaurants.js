20c7c4460c657ab85882703e21bd5f621890cb9f
tschiang23 <tien23@gmail.com>
2023-05-10 11:40:03 +0800

    feat: modify config for heroku
    add 20230509060009-migreate file

---
+++ migrations/20230509060009-add-image-to-restaurants.js
@@ -0,0 +1,11 @@
+'use strict'
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    // await queryInterface.addColumn('Restaurants', 'image', {
+    //   type: Sequelize.STRING
+    // })cl
+  },
+  down: async (queryInterface, Sequelize) => {
+    // await queryInterface.removeColumn('Restaurants', 'image')
+  }
+}
