7be93bf9ce574f7a0c26df46ecfb4953158ab35b
qpooqpooqp <kkk3kkk3kkk3@yahoo.com.tw>
2023-05-11 15:23:23 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express')
+const handlebars = require('express-handlebars') // 引入 express-handlebars
 const routes = require('./routes')
-
 const app = express()
 const port = process.env.PORT || 3000
-
-app.use(routes)
+// 註冊 Handlebars 樣板引擎，並指定副檔名為 .hbs
+app.engine('hbs', handlebars({ extname: '.hbs' }))
+// 設定使用 Handlebars 做為樣板引擎
+app.set('view engine', 'hbs')
 
 app.listen(port, () => {
   console.info(`Example app listening on port ${port}!`)
