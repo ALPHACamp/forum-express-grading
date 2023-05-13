b386a67e203a69262aeb5763f50625b46c57e148
AllisonLin8 <doging8@gmail.com>
2023-05-13 15:03:27 +0800

    feat: passport init & signin

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
 const session = require('express-session')
+const passport = require('./config/passport')
 const flash = require('connect-flash')
 const routes = require('./routes')
 
@@ -13,6 +14,8 @@ app.set('view engine', 'hbs') // 設定使用 Handlebars 做為樣板引擎
 
 app.use(express.urlencoded({ extended: true }))
 app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
+app.use(passport.initialize()) // 初始化 Passport
+app.use(passport.session()) // 啟動 session 功能
 app.use(flash()) // 掛載套件
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages') // 設定 success_msg 訊息
