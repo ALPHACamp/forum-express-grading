c378bb78cbcf3d0a0d1889d561a80a9a5bf855ed
s1030905 <s1030905@outlook.com>
2023-05-14 23:34:27 +0800

    feat: passport init & signin

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
 const session = require('express-session')
+const passport = require('./config/passport')
 const flash = require('connect-flash')
 const routes = require('./routes')
 
@@ -16,6 +17,8 @@ app.use(session({
   resave: false,
   saveUninitialized: true
 }))
+app.use(passport.initialize())
+app.use(passport.session())
 app.use(flash())
 app.use((req, res, next) => {
   // res.locals.user = req.user
