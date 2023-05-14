e0e16073f70656700fa36581da787b85d8279244
azx79115 <azx79115@gmail.com>
2023-05-14 16:32:37 +0800

    完成前台、後台網站入口

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+// 新增，載入controller
+
+const restaurantController = require('../controllers/restaurant-controller')
+const admin = require('./modules/admin')
+
+router.use('/admin', admin)
+router.get('/restaurants', restaurantController.getRestaurants)
+router.use('/', (req, res) => res.redirect('/restaurants'))
 
 module.exports = router
