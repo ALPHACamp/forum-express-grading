84934e41d93d254a94997dec18cd67640d01fcb1
s1030905 <s1030905@outlook.com>
2023-05-14 17:08:45 +0800

    feat: flash msg & signup verification

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
+const session = require('express-session')
+const flash = require('connect-flash')
 const routes = require('./routes')
 
 const app = express()
 const port = process.env.PORT || 3000
-
-const db = require('./models')
+const SESSION_SECRET = 'secret'
 
 app.set('view engine', 'hbs')
 app.engine('hbs', handlebars({ extname: '.hbs' }))
 app.use(express.urlencoded({ extended: true }))
+app.use(session({
+  secret: SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true
+}))
+app.use(flash())
+app.use((req, res, next) => {
+  // res.locals.user = req.user
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.error_msg = req.flash('error_msg')
+  next()
+})
 app.use(routes)
 
 app.listen(port, () => {
