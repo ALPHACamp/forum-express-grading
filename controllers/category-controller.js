0acdd21bdf428349f34d8f8668586d406dddcebd
klu0926 <lukuoyu@gmail.com>
2023-05-15 17:31:36 +0800

    feat: delete category

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -1,14 +1,24 @@
-const { Category } = require('../models')
+const { Category, Restaurant } = require('../models')
 
 const categoryController = {
-  getCategories: (req, res, next) => {
-    Category.findAll({
-      raw: true
-    })
-      .then(categories => {
-        res.render('admin/categories', { categories })
-      })
-      .catch(err => next(err))
+  getCategories: async (req, res, next) => {
+    try {
+      // 拿到全部categories
+      const categories = await Category.findAll({ raw: true })
+
+      await Promise.all(categories.map(async cat => {
+        const res = await Restaurant.findAll({ where: { categoryId: cat.id }, raw: true })
+        cat.restaurantsAmount = res.length
+      }))
+
+      // 拿到 params.id 沒有就不用
+      const category = req.params.id ? await Category.findByPk(req.params.id) : null
+
+      // response
+      res.render('admin/categories', { categories, category })
+    } catch (err) {
+      next(err)
+    }
   },
   postCategories: (req, res, next) => {
     const { name } = req.body
@@ -17,6 +27,47 @@ const categoryController = {
     return Category.create({ name })
       .then(() => res.redirect('/admin/categories'))
       .catch(err => next(err))
+  },
+
+  putCategory: (req, res, next) => {
+    const { name } = req.body
+    if (!name) throw new Error('Category name is required!')
+
+    Category.findByPk(req.params.id)
+      .then(category => {
+        if (!category) throw new Error('Can not find category!')
+
+        return category.update({ name })
+      })
+      .then(() => res.redirect('/admin/categories'))
+      .catch(err => next(err))
+  },
+  // 刪除某類型的餐廳，保留類型
+  deleteCategoryRestaurants: (req, res, next) => {
+    Category.findByPk(req.params.id)
+      .then(category => {
+        if (!category) throw new Error('Can not find category!')
+        return category.getRestaurants({})
+      })
+      .then(restaurants => {
+        return Promise.all(restaurants.map(res => res.destroy()))
+      })
+      .then(() => res.redirect('/admin/categories'))
+      .catch(err => next(err))
+  },
+  // 刪除某類型，跟其餐廳
+  deleteCategory: async (req, res, next) => {
+    try {
+      const category = await Category.findByPk(req.params.id)
+      const restaurants = await category.getRestaurants({})
+
+      await Promise.all(restaurants.map(res => res.destroy()))
+      await category.destroy()
+
+      res.redirect('/admin/categories')
+    } catch (err) {
+      next(err)
+    }
   }
 }
 
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -17,6 +17,10 @@ router.post('/restaurants', upload.single('image'), adminController.postRestaura
 router.get('/users', adminController.getUsers)
 router.patch('/users/:id', adminController.patchUser)
 // categories
+router.get('/categories/:id', categoryController.getCategories)
+router.put('/categories/:id', categoryController.putCategory)
+router.delete('/categories/:id/restaurants', categoryController.deleteCategoryRestaurants)
+router.delete('/categories/:id', categoryController.deleteCategory)
 router.get('/categories', categoryController.getCategories)
 router.post('/categories', categoryController.postCategories)
 // home
