c20c7b699479cb0a8595ea859adf6d6d6a653a59
alvin <ckuvn1854@gmail.com>
2023-05-15 10:39:59 +0800

    feat: add users page and toggle role function

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,15 +1,15 @@
-const { getUser, ensureAuthenticated } = require('../helpers/auth-helpers')
+const helpers = require('../helpers/auth-helpers')
 
 const authenticated = (req, res, next) => {
-  if (ensureAuthenticated(req)) return next()
+  if (helpers.ensureAuthenticated(req)) return next()
 
   req.flash('error_messages', '請先登入!')
   res.redirect('/signin')
 }
 
 const adminAuthenticated = (req, res, next) => {
-  if (ensureAuthenticated(req)) {
-    if (getUser(req).isAdmin) {
+  if (helpers.ensureAuthenticated(req)) {
+    if (helpers.getUser(req).isAdmin) {
       return next()
     }
     res.redirect('/')
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -20,6 +20,8 @@ router.post(
   upload.single('image'),
   adminController.postRestaurant
 )
+router.patch('/users/:id', adminController.patchUser)
+router.get('/users', adminController.getUsers)
 router.use('/', (req, res) => res.redirect('/admin/restaurants'))
 
 module.exports = router
