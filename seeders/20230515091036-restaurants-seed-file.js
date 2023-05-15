07a39a54183a6f9c45f463f1910f0a29a8fde95c
kim1037 <pan913063@gmail.com>
2023-05-15 17:11:38 +0800

    feat: update seed files-- add categoryid reference

--- seeders/20230515091036-restaurants-seed-file.js
+++ seeders/20230515091036-restaurants-seed-file.js
@@ -3,6 +3,11 @@ const faker = require('faker')
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    // 先查詢categories的id共有那些
+    const categories = await queryInterface.sequelize.query(
+      'SELECT id FROM Categories;',
+      { type: queryInterface.sequelize.QueryTypes.SELECT }
+    )
     await queryInterface.bulkInsert(
       'Restaurants',
       Array.from({ length: 50 }, () => ({
@@ -11,11 +16,14 @@ module.exports = {
         address: faker.address.streetAddress(),
         opening_hours: '08:00',
         image: `https://loremflickr.com/320/240/restaurant,food/?random=${
-         Math.random() * 100
-       }`,
+          Math.random() * 100
+        }`,
         description: faker.lorem.text(),
         created_at: new Date(),
-        updated_at: new Date()
+        updated_at: new Date(),
+        // 考量到雲端的database, 產生資料時id可能會跳號
+        category_id:
+          categories[Math.floor(Math.random() * categories.length)].id
       }))
     )
   },
