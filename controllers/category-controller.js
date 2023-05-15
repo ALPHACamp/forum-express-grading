09597628315a3a838378715ca137277d70dedaf2
klu0926 <lukuoyu@gmail.com>
2023-05-15 15:16:04 +0800

    feat: cvategory create

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -9,6 +9,14 @@ const categoryController = {
         res.render('admin/categories', { categories })
       })
       .catch(err => next(err))
+  },
+  postCategories: (req, res, next) => {
+    const { name } = req.body
+    if (!name) throw new Error('Category name is required!')
+
+    return Category.create({ name })
+      .then(() => res.redirect('/admin/categories'))
+      .catch(err => next(err))
   }
 }
 
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -18,6 +18,7 @@ router.get('/users', adminController.getUsers)
 router.patch('/users/:id', adminController.patchUser)
 // categories
 router.get('/categories', categoryController.getCategories)
+router.post('/categories', categoryController.postCategories)
 // home
 router.get('', (req, res) => res.redirect('/admin/restaurants'))
 
