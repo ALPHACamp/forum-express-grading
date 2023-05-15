b0b8ac2e46b84d53b88b0bd73022270607d4d96b
klu0926 <lukuoyu@gmail.com>
2023-05-15 14:23:10 +0800

    feat: add ifCond hbs helper & update category selector

--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -3,10 +3,13 @@ const dayjs = require('dayjs')
 module.exports = {
   currentYear: () => {
     return dayjs().year()
+  },
+  isCond: function (a, b, options) {
+    return a === b ? options.fn(this) : options.inverse(this)
   }
 }
 
-/* 客製的helper不需要 # 開頭，以及 / 結尾，可直接使用
+/* 基本的 helper不需要 # 開頭，以及 / 結尾，可直接使用
 當然你仍可以用一般的helper方式，並在中間夾入你要的HTML結構
 使用 # 代表是 block，這樣就必須使用 / 把來結尾
 以addUp為例:
@@ -14,4 +17,15 @@ module.exports = {
 {{ this }}
 {{/addUp}}
 傳入的arguments用 空格 隔開而非一般的逗號
+*/
+
+/*
+helper parameter (inputA, inputB, options)
+options.fn(this) : if condition is true, use the current block
+options.inverse(this) : if condition is false, use the alternative block
+*/
+
+/*
+使用 箭頭函式會把 this 訂到外層的 this, 所以在使用 options.fn(this) 的時候必能使用
+必須要寫成普通的 function
 */
