0e0e56fdf83a827ae7fc1c8991351ac00c5b733c
s1030905 <s1030905@outlook.com>
2023-05-15 11:44:07 +0800

    feat: create restaurant

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -3,17 +3,23 @@ const { Restaurant } = require('../models') // 新增這裡 採用解構賦值
 const adminController = { // 修改這裡
 
   getRestaurants: (req, res, next) => {
-    Restaurant.findAll({
-
-      raw: true
-
-    })
-
+    Restaurant.findAll({ raw: true })
       .then(restaurants => res.render('admin/restaurants', { restaurants }))
-
+      .catch(err => next(err))
+  },
+  createRestaurant: (req, res) => {
+    res.render('admin/create-restaurant')
+  },
+  postRestaurant: (req, res, next) => {
+    const { name, tel, address, openingHours, description } = req.body
+    if (!name) throw new Error('Restaurant is required')
+    Restaurant.create({ name, tel, address, openingHours, description })
+      .then(() => {
+        req.flash('success_msg', 'Restaurant was successfully created')
+        res.redirect('/admin/restaurants')
+      })
       .catch(err => next(err))
   }
-
 }
 
 module.exports = adminController
