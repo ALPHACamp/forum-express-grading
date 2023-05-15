578a67d2ba7c1083897e8b62c56cba43c6b86644
goodjobhot401 <goodjobhot@gmail.com>
2023-05-15 18:05:11 +0800

    feat: modify admin restaurants page

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,6 +1,11 @@
+const { Restaurants } = require('../models')
+
 const adminController = {
-  getRestaurants: (req, res) => {
-    return res.render('admin/restaurants')
+  getRestaurants: (req, res, next) => {
+    Restaurants.findAll({ raw: true })
+      .then(restaurants => res.render('admin/restaurants', { restaurants }))
+      .catch(err => next(err))
   }
 }
+
 module.exports = adminController
