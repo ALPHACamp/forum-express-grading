7bcbcf77bca0bc30ecc9868c6a58f3b1792b161e
Chingsan0722 <rory85722@gmail.com>
2023-05-15 16:26:47 +0800

    Revert "feat: create MVC and restaurant page"
    This reverts commit 9d54336d59840849de3b0e9cd76280aa0911ee00.

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const router = express.Router()
-const restController = require('../controllers/restaurant-controller')
 
-router.get('/restaurants', restController.getRestaurants)
-router.use('/', (req, res) => { res.redirect('/restaurants') })
+router.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
 module.exports = router
