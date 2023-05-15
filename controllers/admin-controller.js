a8f100a4ebac56c8608998278623d985f7b29e45
klu0926 <lukuoyu@gmail.com>
2023-05-15 13:46:04 +0800

    feat: add categories selector on admin create and edit page

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -28,12 +28,18 @@ const adminController = {
       .catch(err => next(err))
   },
 
-  createRestaurant: (req, res) => {
-    res.render('admin/create-restaurant')
+  createRestaurant: (req, res, next) => {
+    Category.findAll({
+      raw: true
+    })
+      .then(categories => {
+        res.render('admin/create-restaurant', { categories })
+      })
+      .catch(err => next(err))
   },
 
   postRestaurant: (req, res, next) => {
-    const { name, tel, address, openingHours, description } = req.body
+    const { name, tel, address, openingHours, description, categoryId } = req.body
     // 驗證：沒有填名字
     if (!name) throw new Error('Name can not be empty!')
 
@@ -47,29 +53,31 @@ const adminController = {
           address,
           openingHours,
           description,
-          image: filePath || null
+          image: filePath || null,
+          categoryId
         })
       })
       .then(() => {
         req.flash('success_messages', `${name} was successfully created!`)
-        res.redirect('admin/restaurants')
+        res.redirect('/admin/restaurants')
       })
       .catch(err => next(err))
   },
 
   editRestaurant: (req, res, next) => {
-    return Restaurant.findByPk(req.params.id, {
-      raw: true
-    })
-      .then(restaurant => {
-        if (!restaurant) throw new Error('Can not find restaurant to edit!')
-        res.render('admin/edit-restaurant', { restaurant })
+    return Promise.all([
+      Restaurant.findByPk(req.params.id, { raw: true }),
+      Category.findAll({ raw: true })
+    ])
+      .then(([restaurant, categories]) => {
+        if (!restaurant) throw new Error("Restaurant doesn't exist")
+        res.render('admin/edit-restaurant', { restaurant, categories })
       })
       .catch(err => next(err))
   },
 
   putRestaurant: (req, res, next) => {
-    const { name, tel, address, openingHours, description } = req.body
+    const { name, tel, address, openingHours, description, categoryId } = req.body
     // 驗證：沒有填名字
     if (!name) throw new Error('Name can not be empty!')
 
@@ -87,7 +95,8 @@ const adminController = {
           address,
           openingHours,
           description,
-          image: filePath || restaurant.image
+          image: filePath || restaurant.image,
+          categoryId
         })
       })
       .then(() => {
