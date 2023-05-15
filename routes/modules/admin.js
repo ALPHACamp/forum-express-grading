8f2fe9179dd663e16e6204c96fcdb09dc8074b81
king610160 <king610160@gmail.com>
2023-05-15 13:35:43 +0800

    feat: build up sign-in and restaurant page and function. add helper and controller

---
+++ routes/modules/admin.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+const adminController = require('../../controllers/admin-controller')
+const { authenticatedAdmin } = require('../../middleware/auth') // 引入 auth.js
+
+router.get('/restaurants', authenticatedAdmin, adminController.getRestaurants)
+router.use('/', (req, res) => res.redirect('/admin/restaurants'))
+
+module.exports = router
