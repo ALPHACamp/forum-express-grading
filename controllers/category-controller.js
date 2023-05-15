be626204d2bff4da44328b9b018d1fcbfb9cc969
tschiang23 <tien23@gmail.com>
2023-05-15 13:22:45 +0800

    feat:category update

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -3,8 +3,15 @@ const { Category } = require('../models')
 const categoryController = {
   getCategories: async (req, res, next) => {
     try {
-      const categories = await Category.findAll({ raw: true })
-      res.render('admin/categories', { categories })
+      const [categories, category] = await Promise.all([
+        Category.findAll({ raw: true }),
+        // 檢查 req.params.id 是否存在
+        req.params.id ? Category.findByPk(req.params.id, { raw: true }) : null
+      ])
+      res.render('admin/categories', {
+        categories,
+        category
+      })
     } catch (err) { next(err) }
   },
   postCategory: async (req, res, next) => {
@@ -13,6 +20,20 @@ const categoryController = {
       if (!name) throw new Error('Category name is required!')
 
       await Category.create({ name })
+      res.redirect('/admin/categories')
+    } catch (err) { next(err) }
+  },
+  putCategory: async (req, res, next) => {
+    try {
+      const { name } = req.body
+      if (!name) throw new Error('Category name is required!')
+
+      const category = await Category.findByPk(req.params.id)
+
+      if (!category) throw new Error("Category doesn't exist!")
+
+      await category.update({ name })
+
       res.redirect('/admin/categories')
     } catch (err) { next(err) }
   }
