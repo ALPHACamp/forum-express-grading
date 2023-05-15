8604ca1102d581b0a036b294a01051936a1e5664
aking4511 <aking4511@gmail.com>
2023-05-15 15:08:14 +0800

    feat: add index page

--- app.js
+++ app.js
@@ -3,6 +3,10 @@ const routes = require('./routes')
 
 const app = express()
 const port = process.env.PORT || 3000
+const handlebars = require('express-handlebars')
+
+app.engine('hbs', handlebars({ extname: '.hbs' }))
+app.set('view engine', 'hbs')
 
 app.use(routes)
 
