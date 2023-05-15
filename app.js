3fd48d3509a641e6bce9eface06bb22b4a0158a1
eunicebibi <agjay410@gmail.com>
2023-05-15 15:53:53 +0800

    feat:  passport init & signin

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const handlebars = require('express-handlebars')
 const routes = require('./routes')
 const flash = require('connect-flash')
 const session = require('express-session')
+const passport = require('./config/passport') 
 // 暫時新增這行，引入資料庫，檢查完可刪 const db = require('./models')
 const app = express()
 const port = process.env.PORT || 3000
@@ -13,6 +14,8 @@ app.engine('hbs', handlebars({ extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
+app.use(passport.initialize())// 初始化passport
+app.use(passport.session()) // 啟動session功能
 app.use(flash()) // 掛載套件
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
---
+++ config/passport.js
@@ -0,0 +1,38 @@
+const passport = require('passport')
+const LocalStrategy = require('passport-local')
+const bcrypt = require('bcryptjs')
+const db = require('../models')
+const User = db.User
+// set up Passport strategy
+passport.use(new LocalStrategy(
+  // customize user field
+  {
+    usernameField: 'email',
+    passwordField: 'password',
+    // 設定passReqToCallback: true， 可以把 callback 的第一個參數拿到 req 裡，這麼一來我們就可以呼叫 req.flash() 把想要客製化的訊息放進去。
+    passReqToCallback: true
+  },
+  // authenticate user
+  // cb 對應到官方文件裡的 done
+  (req, email, password, cb) => {
+    User.findOne({ where: { email } })
+      .then(user => {
+        if (!user) return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤！'))
+        bcrypt.compare(password, user.password).then(res => {
+          if (!res) return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤！'))
+          return cb(null, user)
+        })
+      })
+  }
+))
+// serialize and deserialize user
+passport.serializeUser((user, cb) => {
+  cb(null, user.id)
+})
+passport.deserializeUser((id, cb) => {
+  User.findByPk(id).then(user => {
+    // console.log(user) // 暫時添加
+    return cb(null, user)
+  })
+})
+module.exports = passport
