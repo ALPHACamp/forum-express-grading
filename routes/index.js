70640de7dca6625e091d9f62b356d726d8d6b0ca
king610160 <king610160@gmail.com>
2023-05-15 18:47:40 +0800

    feat: add signup function and hbs, modified controller with bcrypt function.

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const router = express.Router()
-const restController = require('../controllers/restaurant-controller')
-const admin = require('./modules/admin') // 新增這行，載入 admin.js
+const admin = require('./modules/admin') // 載入 admin.js
+const restController = require('../controllers/restaurant-controller') // 載入restaurant
+const userController = require('../controllers/user-controller') // 載入user
+const { generalErrorHandler } = require('../middleware/error-handler') // 載入error-handler
 
-router.use('/admin', admin) // 新增這行
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp) // 用post
+router.use('/admin', admin) // 使用admin
 router.get('/restaurants', restController.getRestaurants)
 router.use('/', (req, res) => res.redirect('/restaurants'))
+router.use('/', generalErrorHandler) // 加入這行
 
 module.exports = router
