507e63fb9869bc32f85450dcc519175b32727c7d
Ariel <ariel040132@gmail.com>
2023-05-16 18:15:44 +0800

    feat: add website backend

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
-const express = require("express");
-const restaurantController = require("../controllers/restaurant-controller");
-const router = express.Router();
+const express = require('express')
+const restaurantController = require('../controllers/restaurant-controller')
+const router = express.Router()
+const admin = require('./modules/admin')
 
-router.get("/restaurants", restaurantController.getRestaurants);
-router.use("/", (req, res) => res.redirect("/restaurants"));
+router.use('./admin', admin)
+router.get('/restaurants', restaurantController.getRestaurants)
+router.use('/', (req, res) => res.redirect('/restaurants'))
 
-module.exports = router;
+module.exports = router
