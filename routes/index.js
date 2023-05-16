5417774e2d747fd7e39bdb75834412d7b72ce66d
aking4511 <aking4511@gmail.com>
2023-05-16 13:47:19 +0800

    feat: user signup

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,12 @@ const express = require('express')
 const router = express.Router()
 const restController = require('../controllers/restaurant-controller')
 const admin = require('./modules/admin')
+const userController = require('../controllers/user-controller') // 新增這行
 
 router.use('/admin', admin)
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp) // 注意用 post
 router.get('/restaurants', restController.getRestaurants)
-router.use('/', (req, res) => res.redirect('/restaurants'))
+router.get('/', (req, res) => res.redirect('/restaurants'))
 
 module.exports = router
