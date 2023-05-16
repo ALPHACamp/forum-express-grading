ca40c50cb783037620d499ca7403a0ac48f44e59
Wei <awei1127@hotmail.com>
2023-05-17 00:07:31 +0800

    feat: add flash msg & signup verification

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
+const session = require('express-session')
+const flash = require('connect-flash')
 const routes = require('./routes')
 
 const app = express()
 const port = process.env.PORT || 3000
+const SESSION_SECRET = 'secret'
 
 app.engine('hbs', handlebars({ extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
+app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 app.use(routes)
 
 app.listen(port, () => {
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -5,16 +5,34 @@ const userController = {
   signUpPage: (req, res) => {
     res.render('signup')
   },
-  signUp: (req, res) => {
-    bcrypt.hash(req.body.password, 10)
+  signUp: (req, res, next) => {
+    const { name, email, password, passwordCheck } = req.body
+    // 如果有空欄，丟錯誤
+    if (!name || !email || !password || !passwordCheck) {
+      throw new Error('All fields are required.')
+    }
+    // 如果密碼不同，丟錯誤
+    if (password !== passwordCheck) {
+      throw new Error('Passwords do not match!')
+    }
+    // 如果email已存在，丟錯誤
+    User.findOne({ where: { email } })
+      .then(user => {
+        if (user) {
+          throw new Error('Email already exists!')
+        }
+        return bcrypt.hash(req.body.password, 10)
+      })
       .then(hash => User.create({
-        name: req.body.name,
-        email: req.body.email,
+        name,
+        email,
         password: hash
       }))
       .then(() => {
+        req.flash('success_messages', '成功註冊帳號！')
         res.redirect('/signin')
       })
+      .catch(err => next(err))
   }
 }
 module.exports = userController
