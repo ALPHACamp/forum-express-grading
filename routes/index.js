be86ea059979d78683cc31a053cf7e5054b2cf07
CatYunnnn <marco20001114@gmail.com>
2023-05-16 19:38:56 +0800

    flash msg&signup verification

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,14 @@
 const express = require("express");
-
 const router = express.Router();
-
-//新增，載入 controller
-
 const restController = require("../controllers/restaurant-controller");
 const admin = require("./modules/admin");
 const userController = require("../controllers/user-controller");
+const { generalErrorHandler } = require("../middleware/error-handler");
 router.use("/admin", admin);
 router.get("/signup", userController.signUpPage);
 router.post("/signup", userController.signUp);
 router.get("/restaurants", restController.getRestaurants);
 
-router.use("/", (req, res) => res.redirect("/restaurants"));
-
+router.get("/", (req, res) => res.redirect("/restaurants"));
+router.use("/", generalErrorHandler);
 module.exports = router;
