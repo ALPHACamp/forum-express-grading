553788f872f1e12a45ad9842f215c152df3abd87
kim1037 <pan913063@gmail.com>
2023-05-16 14:45:17 +0800

    feat: category update and delete

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -2,9 +2,15 @@ const { Category } = require('../models')
 
 const categoryController = {
   getCategories: (req, res, next) => {
-    Category.findAll({ raw: true }).then(categories => {
-      res.render('admin/categories', { categories })
-    })
+    const id = req.params.id
+    Promise.all([
+      Category.findAll({ raw: true }),
+      id ? Category.findByPk(id, { raw: true }) : null
+    ])
+      .then(([categories, category]) => {
+        res.render('admin/categories', { categories, category })
+      })
+      .catch(e => next(e))
   },
   postCategory: (req, res, next) => {
     const { name } = req.body
@@ -13,8 +19,29 @@ const categoryController = {
     return Category.create({ name }).then(() => res.redirect('/admin/categories'))
       .catch(e => next(e))
   },
-  putCategory: (req, res, next) => {},
-  deleteCategory: (req, res, next) => {}
+  putCategory: (req, res, next) => {
+    const { name } = req.body
+    if (!name) throw new Error('Category name is required!')
+    return Category.findByPk(req.params.id)
+      .then(category => {
+        if (!category) throw new Error("Category didn't exist!")
+        return category.update({ name })
+      })
+      .then(() => res.redirect('/admin/categories'))
+      .catch(e => next(e))
+  },
+  deleteCategory: (req, res, next) => {
+    return Category.findByPk(req.params.id)
+      .then(category => {
+        if (!category) throw new Error("Category didn't exist!")
+        return category.destroy()
+      })
+      .then(() => {
+        req.flash('success_messages', 'Category was successfully deleted')
+        res.redirect('/admin/categories')
+      })
+      .catch(e => next(e))
+  }
 }
 
 module.exports = categoryController
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -21,9 +21,9 @@ router.post(
   adminController.postRestaurant
 )
 // admin categories
-// router.get('/categories/:id', categoryController.getCategories)
-// router.put('/categories/:id', categoryController.putCategory)
-// router.delete('/categories/:id', categoryController.deleteCategory)
+router.get('/categories/:id', categoryController.getCategories)
+router.put('/categories/:id', categoryController.putCategory)
+router.delete('/categories/:id', categoryController.deleteCategory)
 router.get('/categories', categoryController.getCategories)
 router.post('/categories', categoryController.postCategory)
 
