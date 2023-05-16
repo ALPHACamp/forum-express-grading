231fa4831d6aa72c0f2fb83a68ab68a48ef1c400
Lanways <mcbluedd@gmail.com>
2023-05-17 04:01:56 +0800

    feat: passport init&signin

--- app.js
+++ app.js
@@ -6,12 +6,14 @@ const flash = require('connect-flash')
 const app = express()
 const port = process.env.PORT || 3000
 const SESSION_SECRET = 'secret'
-const db = require('./models')
+const passport = require('passport')
 
 app.engine('hbs', handlebars({ extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
+app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
+app.use(passport.initialize())
 app.use(flash())
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
