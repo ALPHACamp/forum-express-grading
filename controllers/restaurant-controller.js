79806ebc39ebe38d5e2c9aeb4042c5a6ef120f4d
tschiang23 <tien23@gmail.com>
2023-05-16 14:11:42 +0800

    feat:add categories navbar on restaurants index page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -2,11 +2,20 @@ const { Restaurant, Category } = require('../models')
 const restaurantController = {
   getRestaurants: async (req, res, next) => {
     try {
-      const restaurants = await Restaurant.findAll({
-        include: Category,
-        nest: true,
-        raw: true
-      })
+      // 從網址上拿下來的參數是字串，先轉成 Number 再操作
+      const categoryId = Number(req.query.categoryId) || ''
+      const [restaurants, categories] = await Promise.all([
+        Restaurant.findAll({
+          include: Category,
+          where: { // 查詢條件
+            ...categoryId ? { categoryId } : {} // 檢查 categoryId 是否為空值
+          },
+          nest: true,
+          raw: true
+        }),
+        Category.findAll({ raw: true })
+      ])
+
       const data = restaurants.map(r => {
         r.description = r.description.substring(0, 50)
         return r
@@ -17,7 +26,11 @@ const restaurantController = {
       //   description: r.description.substring(0, 50)
       // }))
 
-      return res.render('restaurants', { restaurants: data })
+      return res.render('restaurants', {
+        restaurants: data,
+        categories,
+        categoryId
+      })
     } catch (err) {
       next(err)
     }
