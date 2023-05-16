cfcba7cde2f85871fd64d3e446406f7f5a802a94
pleasesailas <pleasesailas@gmail.com>
2023-05-16 15:12:49 +0800

    feat: add admin route & view page

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const restController = require('../controllers/restaurant-controller')
+const admin = require('./modules/admin')
 const router = express.Router()
 
+router.use('/admin', admin)
 router.get('/restaurants', restController.getRestaurants)
 router.use('/', (req, res) => { res.redirect('/restaurants') })
 
