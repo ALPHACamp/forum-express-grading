aef94b84587b117aee9a88a4e78ec21fa2d0de86
Leechiayu <gary86442@gmail.com>
2023-05-16 14:10:03 +0800

    feat:add front entrance

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const router = express.Router()
+//* 新增，載入 controller
 
-router.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const restController = require('../controllers/restaurant-controller')
+
+router.get('/restaurants', restController.getRestaurants)
+
+router.use('/', (req, res) => res.redirect('/restaurants'))
 
 module.exports = router
