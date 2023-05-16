45e18296e8d33e4e7324071e505a708fd6089750
Lanways <mcbluedd@gmail.com>
2023-05-17 03:10:49 +0800

    feat: flash msg&signup verification

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -6,15 +6,28 @@ const userController = {
   signUpPage: (req, res) => {
     res.render('signup')
   },
-  signUp: (req, res) => {
-    bcrypt.hash(req.body.password, 10)
-      .then(hash => User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: hash
-      }))
-      .then(() => res.redirect('/signin'))
+  signUp: (req, res, next) => {
+    if (req.body.password != req.body.passwordCheck) throw new Error('Password do not mathc')
+
+    User.findOne({ where: { email: req.body.email } })
+      .then(user => {
+        if (user) throw new Error('Email already exists!')
+        return bcrypt.hash(req.body.password, 10)
+      })
+      .then(hash => {
+        return User.create({
+          name: req.body.name,
+          email: req.body.email,
+          password: hash
+        })
+      })
+      .then((user) => {
+        req.flash('success_messages', '成功註冊帳號!')
+        res.redirect('/signin')
+      })
+      .catch(err => next(err))
   }
 }
+
 
 module.exports = userController
