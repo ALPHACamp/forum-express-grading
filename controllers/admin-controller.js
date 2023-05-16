e8c62913dfa50658fb1b2a0d78878a9318f53ee4
s1030905 <s1030905@outlook.com>
2023-05-16 20:34:21 +0800

    feat: redirect(back)

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -87,27 +87,52 @@ const adminController = { // 修改這裡
       .catch(err => next(err))
   },
   getUsers: (req, res, next) => {
-    User.findAll({ raw: true })
-      .then(users => {
-        console.log('..................這裡是第一個user名稱' + users[0].name)
-        res.render('admin/users', { users })
-      })
-      // .then(users => res.render('admin/users', { users }))
+    return User.findAll({ raw: true })
+      .then(users => res.render('admin/users', { users }))
       .catch(err => next(err))
   },
   patchUser: async (req, res, next) => {
-    const id = req.params.id
-    const user = await User.findByPk(id)
-    if (!user) throw new Error('Restaurant is required')
-    if (user.email === 'root@example.com') {
-      req.flash('error_messages', '禁止變更 root 權限')
+    try {
+      const id = req.params.id
+      const user = await User.findByPk(id)
+      if (!user) throw new Error('Restaurant is required')
+      if (user.email === 'root@example.com') {
+        req.flash('error_messages', '禁止變更 root 權限')
+        return res.redirect('back')
+      }
+      await user.update({
+        isAdmin: !user.isAdmin
+      })
+      req.flash('success_messages', '使用者權限變更成功')
       return res.redirect('/admin/users')
+    } catch (error) {
+      console.log(error)
     }
-    await user.update({
-      isAdmin: !user.isAdmin
-    })
-    req.flash('success_messages', '使用者權限變更成功')
-    return res.redirect('/admin/users')
   }
+
+  // patchUser: (req, res, next) => {
+  //   const id = req.params.id
+  //   return User.findByPk(id)
+  //     .then(user => {
+  //       if (!user) {
+  //         throw new Error('Restaurant is required')
+  //       }
+  //       if (user.email === 'root@example.com') {
+  //         req.flash('error_messages', '禁止變更 root 權限')
+  //         return res.redirect('/admin/users')
+  //       }
+  //       return user
+  //     })
+  //     .then(user => { return user.update({ isAdmin: !user.isAdmin }) })
+  //     .then(() => {
+  //       req.flash('success_messages', '使用者權限變更成功')
+  //       return res.redirect('/admin/users')
+  //     })
+  //     .catch(error => {
+  //       console.log(error)
+  //       next(error)
+  //     })
+  // }
+
 }
 module.exports = adminController
