0bac4df826f389982e35c7d0a9ce689f43d6588e
CatYunnnn <marco20001114@gmail.com>
2023-05-16 20:35:24 +0800

    user model add is_admin

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,11 @@ const restController = require("../controllers/restaurant-controller");
 const admin = require("./modules/admin");
 const userController = require("../controllers/user-controller");
 const { generalErrorHandler } = require("../middleware/error-handler");
+const { authenticated } = require("../middleware/auth");
 router.use("/admin", admin);
 router.get("/signup", userController.signUpPage);
 router.post("/signup", userController.signUp);
-router.get("/restaurants", restController.getRestaurants);
+router.get("/restaurants", authenticated, restController.getRestaurants);
 router.get("/signin", userController.signInPage);
 router.post(
   "/signin",
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,16 +1,12 @@
 const express = require("express");
-
 const router = express.Router();
-
-//新增，載入 controller
-
 const adminController = require("../../controllers/admin-controller");
-
+const { authenticatedAdmin } = require("../../middleware/auth");
 //刪除
 
 //新增
 
-router.get("/restaurants", adminController.getRestaurants);
+router.get("/restaurants", authenticatedAdmin, adminController.getRestaurants);
 
 router.use("/", (req, res) => res.redirect("/admin/restaurants"));
 
