95d923998c6fc859beff28753021a57f8340b005
Nero <karta0706566@gmail.com>
2023-05-17 10:22:21 +0800

    feat: add header & footer

--- app.js
+++ app.js
@@ -7,12 +7,14 @@ const exphbs = require('express-handlebars')
 const flash = require('connect-flash')
 const session = require('express-session')
 const passport = require('./config/passport')
+const hbsHelpers = require('./helpers/handlebars-helpers')
+const { getUser } = require('./helpers/auth-helpers')
 const routes = require('./routes')
 const app = express()
 const port = process.env.PORT || 3000
 
 // handlebars設定
-app.engine('hbs', exphbs({ extname: '.hbs' }))
+app.engine('hbs', exphbs({ extname: '.hbs', helpers: hbsHelpers }))
 app.set('view engine', 'hbs')
 
 // body-parser設定
@@ -30,6 +32,7 @@ app.use(flash())
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
   res.locals.error_messages = req.flash('error_messages')
+  res.locals.user = getUser(req)
   next()
 })
 
