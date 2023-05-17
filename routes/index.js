08a4bcee75db0bf5eb3d69462c60216a1b73c633
tschiang23 <tien23@gmail.com>
2023-05-17 11:28:29 +0800

    feat:add user profile
    update user profile

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ const commentController = require('../controllers/​​comment-controller')
 const { authenticated, authenticatedAdmin } = require('../middleware/auth')
 const { generalErrorHandler } = require('../middleware/error-handler')
 const passport = require('../config/passport')
+const upload = require('../middleware/multer')
 
 router.use('/admin', authenticatedAdmin, admin)
 // user routes
@@ -15,6 +16,10 @@ router.post('/signup', userController.signUp)
 router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
 router.get('/logout', userController.logout)
+
+router.get('/users/:id/edit', authenticated, userController.editUser)
+router.get('/users/:id', authenticated, userController.getUser)
+router.put('/users/:id', authenticated, upload.single('image'), userController.putUser)
 
 router.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard)
 router.get('/restaurants/:id', authenticated, restController.getRestaurant)
