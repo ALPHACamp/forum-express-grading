a08199752df9ab0521070fd0584dc91ccf54a726
king610160 <king610160@gmail.com>
2023-05-17 14:01:07 +0800

    feat: build resaturant model, migrate to sql.

---
+++ models/restaurants.js
@@ -0,0 +1,29 @@
+'use strict'
+const {
+  Model
+} = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+  class Restaurants extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate (models) {
+      // define association here
+    }
+  };
+  Restaurants.init({
+    name: DataTypes.STRING,
+    tel: DataTypes.STRING,
+    openingHour: DataTypes.STRING,
+    address: DataTypes.STRING,
+    description: DataTypes.TEXT
+  }, {
+    sequelize,
+    modelName: 'Restaurant',
+    tableName: 'Restaurants',
+    underscored: true
+  })
+  return Restaurants
+}
