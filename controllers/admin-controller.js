08c5a5dc7d4c511bbe7122da194d28448a71db68
king610160 <king610160@gmail.com>
2023-05-17 14:38:11 +0800

    feat:add admin board in hbs, modified admin controller for get data, fix database column name.

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,6 +1,11 @@
+const { Restaurant } = require('../models')
 const adminController = {
-  getRestaurants: (req, res) => {
-    return res.render('admin/restaurants')
+  getRestaurants: (req, res, next) => {
+    Restaurant.findAll({
+      raw: true // 讓拿到的資料是最簡單的javascript資料
+    })
+      .then(restaurants => res.render('admin/restaurants', { restaurants }))
+      .catch(err => next(err))
   }
 }
 module.exports = adminController
