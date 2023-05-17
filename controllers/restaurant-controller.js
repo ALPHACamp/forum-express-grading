7251b3c8fe2cf1392268096a92366fad6fd503c6
kim1037 <pan913063@gmail.com>
2023-05-17 13:26:08 +0800

    feat: add restaurants index page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,5 +1,20 @@
+const { Restaurant, Category } = require('../models')
+
 const restaruantController = {
-  getRestaurants: (req, res) => {
+  getRestaurants: async (req, res, next) => {
+    try {
+      const restaurants = await Restaurant.findAll({
+        raw: true,
+        next: true,
+        include: Category
+      })
+      const data = restaurants.map(r => ({ ...r, description: r.description.substring(0, 50) }))
+      res.render('restaurants', { restaurants: data })
+    } catch (e) {
+      next(e)
+    }
+  },
+  getRestaurant: (req, res) => {
     return res.render('restaurants')
   }
 }
