faf210ce0863825118520caffdafe688b3a1ef90
alvin <ckuvn1854@gmail.com>
2023-05-17 16:26:51 +0800

    feat: add edit function, not to database

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -2,9 +2,12 @@ const { Category } = require('../models')
 
 const categoryController = {
   getCategories: async (req, res, next) => {
+    // 確認有無category，沒有為null
+    const { id } = req.params
     try {
       const categories = await Category.findAll({ raw: true })
-      return res.render('admin/categories', { categories })
+      const category = await Category.findByPk(id, { raw: true })
+      return res.render('admin/categories', { categories, category })
     } catch (err) {
       next(err)
     }
@@ -18,6 +21,17 @@ const categoryController = {
       await Category.create({ name })
       // redirect
       res.redirect('/admin/categories')
+    } catch (err) {
+      next(err)
+    }
+  },
+  putCategory: async (req, res, next) => {
+    // 取得category
+    try {
+      // 找出對應的
+      // 沒有就報錯
+      // 有就更新
+      // redirect
     } catch (err) {
       next(err)
     }
