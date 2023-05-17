43a2c3b9270bf2c546ac99f6fad3793426240487
kim1037 <pan913063@gmail.com>
2023-05-18 00:18:23 +0800

    feat: add dashboard and viewCounts - R02 pass

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -8,24 +8,39 @@ const restaruantController = {
         next: true,
         include: Category
       })
-      const data = restaurants.map(r => ({ ...r, description: r.description.substring(0, 50) }))
+      const data = restaurants.map(r => ({
+        ...r,
+        description: r.description.substring(0, 50)
+      }))
       res.render('restaurants', { restaurants: data })
     } catch (e) {
       next(e)
     }
   },
-  getRestaurant: async (req, res, next) => {
+  getRestaurant: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, {
+      include: Category, // 拿出關聯的 Category model
+      nest: true
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        res.render('restaurant', {
+          restaurant: restaurant.toJSON()
+        })
+        return restaurant.increment('viewCounts')
+      })
+      .catch(err => next(err))
+  },
+  getDashboard: async (req, res, next) => {
     try {
       const restaurant = await Restaurant.findByPk(req.params.id, {
+        include: [Category],
         raw: true,
-        next: true,
-        include: Category
+        nest: true
       })
       if (!restaurant) throw new Error("Restaurant didn't exist!")
-      res.render('restaurant', { restaurant })
-    } catch (e) {
-      next(e)
-    }
+      res.render('dashboard', { restaurant })
+    } catch (e) { next(e) }
   }
 }
 
