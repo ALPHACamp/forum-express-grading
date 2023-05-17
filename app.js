362b4bc3b6834fbaa2f3512c2be9be551a3f1511
Ariel <ariel040132@gmail.com>
2023-05-18 00:31:41 +0800

    feat: passport init & signin

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
-const express = require("express");
-const handlebars = require("express-handlebars");
-const routes = require("./routes");
+const express = require('express')
+const handlebars = require('express-handlebars')
+const flash = require('connect-flash')
+const session = require('express-session')
+const routes = require('./routes')
+const app = express()
+const port = process.env.PORT || 3000
+const SESSION_SECRET = 'secret'
+const passport = require('./config/passport')
 
-const app = express();
-const port = process.env.PORT || 3000;
-app.set("view engine", "hbs");
-app.engine("hbs", handlebars({ extname: ".hbs" }));
-app.use(routes);
+app.set('view engine', 'hbs')
+app.engine('hbs', handlebars({ extname: '.hbs' }))
+app.use(express.urlencoded({ extended: true }))
+app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
+app.use(routes)
 
 app.listen(port, () => {
-  console.info(`Example app listening on port ${port}!`);
-});
+  console.info(`Example app listening on port ${port}!`)
+})
 
-module.exports = app;
+module.exports = app
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const restaurantController = require('../controllers/restaurant-controller')
 const router = express.Router()
+const passport = require('../config/passport')
 const admin = require('./modules/admin')
+const userController = require('../controllers/user-controller')
+const { generalErrorHandler } = require('../middleware/error-handler')
 
 router.use('./admin', admin)
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
+router.get('/signin', userController.signInPage)
+router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
+router.get('/logout', userController.logout)
 router.get('/restaurants', restaurantController.getRestaurants)
 router.use('/', (req, res) => res.redirect('/restaurants'))
+router.use('/', generalErrorHandler)
 
 module.exports = router
