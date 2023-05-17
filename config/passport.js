3d1fc4c5896f4b78db09fde2b41c7587aaf1db2d
jiasyuanchu <jiasyuanchu@gmail.com>
2023-05-17 16:29:25 +0800

    feat: passport init & signin

---
+++ config/passport.js
@@ -0,0 +1,36 @@
+const passport = require('passport')
+const LocalStrategy = require('passport-local')
+const bcrypt = require('bcryptjs')
+const db = require('../models')
+const User = db.User
+// set up Passport strategy
+passport.use(new LocalStrategy(
+  // customize user field
+  {
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true // 將 HTTP 請求物件（req）傳遞給驗證回調函式（cb）
+  },
+  // authenticate user
+  (req, email, password, cb) => {
+    User.findOne({ where: { email } })
+      .then(user => {
+        if (!user) return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤！'))
+        bcrypt.compare(password, user.password).then(res => {
+          if (!res) return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤！'))
+          return cb(null, user)
+        })
+      })
+  }
+))
+// serialize and deserialize user
+passport.serializeUser((user, cb) => {
+  cb(null, user.id)
+})
+passport.deserializeUser((id, cb) => {
+  User.findByPk(id).then(user => {
+    user = user.toJSON()// 把資料簡化
+    return cb(null, user)
+  })
+})
+module.exports = passport
