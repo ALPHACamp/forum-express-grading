f7eb9651d98d6f2d1420eb41f97ed0f31908bb08
king610160 <king610160@gmail.com>
2023-05-17 11:28:41 +0800

    feat:add header, footer.hbs, add helper for auth, handlebars.

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const passport = require('../config/passport') // 引入 Passport，需要他幫忙做驗證
 const admin = require('./modules/admin') // 載入 admin.js
 const restController = require('../controllers/restaurant-controller') // 載入restaurant
 const userController = require('../controllers/user-controller') // 載入user
@@ -7,6 +8,9 @@ const { generalErrorHandler } = require('../middleware/error-handler') // 載入
 
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp) // 用post
+router.get('/signin', userController.signInPage)
+router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn) // 注意是 post
+router.get('/logout', userController.logout)
 router.use('/admin', admin) // 使用admin
 router.get('/restaurants', restController.getRestaurants)
 router.use('/', (req, res) => res.redirect('/restaurants'))
