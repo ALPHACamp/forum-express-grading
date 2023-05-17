2c9781ce7fede348cf1a35a751280a38a3346919
Nero <karta0706566@gmail.com>
2023-05-17 08:51:17 +0800

    feat: sequelize init

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const routes = require('./routes')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(routes)
+
+app.listen(port, () => {
+  console.info(`Example app listening on port ${port}!`)
+})
+
+module.exports = app
