9527ffd58281378f0a1fafa9834536298b6395f2
Wei <awei1127@hotmail.com>
2023-05-17 19:18:39 +0800

    feat: user model add is_admin

---
+++ middleware/auth.js
@@ -0,0 +1,25 @@
+const { getUser, ensureAuthenticated } = require('../helpers/auth-helpers')
+
+const authenticated = (req, res, next) => {
+  if (ensureAuthenticated(req)) {
+    next()
+  } else {
+    res.redirect('/signin')
+  }
+}
+
+const authenticatedAdmin = (req, res, next) => {
+  if (ensureAuthenticated(req)) {
+    if (getUser(req).isAdmin) {
+      return next()
+    }
+    res.redirect('/')
+  } else {
+    res.redirect('/signin')
+  }
+}
+
+module.exports = {
+  authenticated,
+  authenticatedAdmin
+}
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const passport = require('passport')
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
 const { generalErrorHandler } = require('../middleware/error-handler')
+const { authenticated } = require('../middleware/auth')
 const admin = require('./modules/admin')
 
 router.use('/admin', admin)
@@ -19,7 +20,7 @@ router.post('/signin', passport.authenticate('local', { failureRedirect: '/signi
 
 router.get('/logout', userController.logout)
 
-router.get('/restaurants', restController.getRestaurants)
+router.get('/restaurants', authenticated, restController.getRestaurants)
 
 router.use('/', (req, res) => res.redirect('/restaurants'))
 
