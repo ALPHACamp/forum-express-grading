40607bca39883fc254d3b498b7b066936fa2d417
HuangYiLun <wk5018102@gmail.com>
2023-05-17 14:46:21 +0800

    feat: add user model

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const routes = require('./routes')
-
 const app = express()
 const port = process.env.PORT || 3000
 
