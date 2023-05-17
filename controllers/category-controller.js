3deb6a45fdb396c5688ec2a85c45809286da992f
alvin <ckuvn1854@gmail.com>
2023-05-17 15:49:38 +0800

    feat: add category page and route

---
+++ controllers/category-controller.js
@@ -0,0 +1,14 @@
+const { Category } = require('../models')
+
+const categoryController = {
+  getCategories: async (req, res, next) => {
+    try {
+      const categories = await Category.findAll({ raw: true })
+      return res.render('admin/categories', { categories })
+    } catch (err) {
+      next(err)
+    }
+  }
+}
+
+module.exports = categoryController
