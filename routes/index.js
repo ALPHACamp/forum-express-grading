b8188af46ac558fb7a48ae9629218986cc0b3f89
Leechiayu <gary86442@gmail.com>
2023-05-17 15:11:08 +0800

    feat: add singup page, controller, route.

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,14 @@ const router = express.Router()
 
 const admin = require('./modules/admin')
 //* 新增，載入 controller
-
 const restController = require('../controllers/restaurant-controller')
-
+const userController = require('../controllers/user-controller')
+//* 後台路由
 router.use('/admin', admin)
+//* 使用者路由
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
+
 router.get('/restaurants', restController.getRestaurants)
 
 router.use('/', (req, res) => res.redirect('/restaurants'))
