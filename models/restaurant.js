1c77054965303a488e9629a25309c5908a259a5b
tschiang23 <tien23@gmail.com>
2023-05-18 15:24:06 +0800

    feat: create favorite model

--- models/restaurant.js
+++ models/restaurant.js
@@ -10,8 +10,13 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate (models) {
-      Restaurant.belongsTo(models.Category, { foreignKey: 'categoryId' })
-      Restaurant.hasMany(models.Comment, { foreignKey: 'restaurantId' })
+      Restaurant.belongsTo(models.Category, { foreignKey: 'categoryId' }) // 多對1
+      Restaurant.hasMany(models.Comment, { foreignKey: 'restaurantId' }) // 1對多
+      Restaurant.belongsToMany(models.User, { // 多對多 透過join table
+        through: models.Favorite, // 透過 Favorite 表來建立關聯
+        foreignKey: 'restaurantId', // 對 Favorite 表設定 FK
+        as: 'FavoritedUsers' // 幫這個關聯取個名稱
+      })
     }
   };
   Restaurant.init({
