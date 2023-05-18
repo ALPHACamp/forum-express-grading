011f5a2d253097f913c78ec8c6fd54a80e110396
king610160 <king610160@gmail.com>
2023-05-18 11:28:24 +0800

    feat:add fake seed for testing.

---
+++ seeders/20230518031415-restaurant-seed-file.js
@@ -0,0 +1,22 @@
+'use strict'
+const faker = require('faker')
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('Restaurants',
+      Array.from({ length: 50 }, () => ({
+        name: faker.name.findName(),
+        tel: faker.phone.phoneNumber(),
+        address: faker.address.streetAddress(),
+        opening_hour: '08:00',
+        image: `https://loremflickr.com/320/240/restaurant,food/?random=${Math.random() * 100}`, // for not to produce the same picture, need to use random function.
+        description: faker.lorem.text(),
+        created_at: new Date(),
+        updated_at: new Date()
+      }))
+    )
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Restaurants', {})
+  }
+}
