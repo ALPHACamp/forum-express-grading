26f1ef8c375a6d168816aac3585fe0aa3b6310a0
jiasyuanchu <jiasyuanchu@gmail.com>
2023-05-18 14:11:40 +0800

    feat: user model add is_admin

--- app.js
+++ app.js
@@ -23,8 +23,8 @@ app.use(flash())
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
   res.locals.error_messages = req.flash('error_messages')
-  next()
   res.locals.user = getUser(req)
+  next()
 })
 app.use(routes)
 
