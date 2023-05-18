45098e18e0a3b48d91619b784a9f7091df442488
Leechiayu <gary86442@gmail.com>
2023-05-18 16:01:28 +0800

    feat:show detail of restaurant page.

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,9 +1,17 @@
 const db = require('../models')
 const { Restaurant } = db
 const adminController = {
+  //* 讀取全部
   getRestaurants: (req, res) => {
-    return res.render('admin/restaurants')
+    Restaurant.findAll({
+      raw: true
+    })
+
+      .then(restaurants => res.render('admin/restaurants', { restaurants }))
+
+      .catch(err => next(err))
   },
+  //* 新增餐廳
   createRestaurant: (req, res) => {
     return res.render('admin/create-restaurant')
   },
@@ -25,6 +33,20 @@ const adminController = {
         req.flash('success_messages', 'restaurant was successfully created')
         //* 新增完成後導回後台首頁
         res.redirect('/admin/restaurants')
+      })
+      .catch(err => next(err))
+  },
+  //* 讀取餐廳詳細
+  getRestaurant: (req, res, next) => {
+    //* 去資料庫用 id 找一筆資料
+    Restaurant.findByPk(req.params.id, {
+      //* 找到以後整理格式再回傳
+      raw: true
+    })
+      .then(restaurant => {
+        //*  如果找不到，回傳錯誤訊息，後面不執行
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        res.render('admin/restaurant', { restaurant })
       })
       .catch(err => next(err))
   }
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -3,9 +3,12 @@ const router = express.Router()
 
 //* 新增，載入 controller
 const adminController = require('../../controllers/admin-controller')
-
+//* 新增餐廳路由
 router.get('/restaurants/create', adminController.createRestaurant)
 router.post('/restaurants', adminController.postRestaurant)
+//* 讀取餐廳詳細
+router.get('/restaurants/:id', adminController.getRestaurant)
+//* 讀取全部餐廳
 router.get('/restaurants', adminController.getRestaurants)
 
 router.use('/', (req, res) => res.redirect('/admin/restaurants'))
