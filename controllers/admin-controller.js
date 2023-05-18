5be275b7f2c9463798b21fe884913a1a46cdb058
Chingsan0722 <rory85722@gmail.com>
2023-05-18 21:25:39 +0800

    feat: add module patchUser

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -8,16 +8,6 @@ const adminController = {
       .then(restaurants => res.render('admin/restaurants', { restaurants }))
       .catch(err => next(err))
   },
-  getUsers: (req, res, next) => {
-    User.findAll({
-      raw: true
-    })
-      .then(users => res.render('admin/users', { users }))
-      .catch(err => next(err))
-  },
-  patchUser: (req, res, next) => {
-    //
-  },
   createRestaurant: (req, res) => {
     return res.render('admin/create-restaurant')
   },
@@ -92,6 +82,32 @@ const adminController = {
         return restaurant.destroy()
       })
       .then(() => res.redirect('/admin/restaurants'))
+      .catch(err => next(err))
+  },
+  getUsers: (req, res, next) => {
+    return User.findAll({
+      raw: true
+    })
+      .then(users => res.render('admin/users', { users }))
+      .catch(err => next(err))
+  },
+  patchUser: (req, res, next) => {
+    const userId = req.params.id
+    return User.findByPk(userId)
+      .then(user => {
+        if (!user) throw new Error("Restaurant didn't exist!")
+        if (user.dataValues.email === 'root@example.com') {
+          req.flash('error_messages', '禁止變更 root 權限')
+          res.redirect('back')
+        } else {
+          const convert = !user.isAdmin
+          user.update({ isAdmin: convert })
+          req.flash('success_messages', '使用者權限變更成功')
+        }
+      })
+      .then(() => {
+        res.redirect('/admin/users')
+      })
       .catch(err => next(err))
   }
 }
