47bb56f56eb181f779651d516ad3e8791adf2e8f
AllisonLin8 <doging8@gmail.com>
2023-05-18 14:24:17 +0800

    feat: add user profile page and edit profile

--- routes/index.js
+++ routes/index.js
@@ -9,6 +9,7 @@ const userController = require('../controllers/user-controller')
 const commentController = require('../controllers/comment-controller')
 const { generalErrorHandler } = require('../middleware/error-handler')
 const { authenticated, authenticatedAdmin } = require('../middleware/auth')
+const upload = require('../middleware/multer')
 
 router.use('/admin', authenticatedAdmin, admin)
 
@@ -19,6 +20,10 @@ router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
 
 router.get('/logout', userController.logout)
+
+router.get('/users/:id/edit', userController.editUser)
+router.put('/users/:id', upload.single('image'), userController.putUser)
+router.get('/users/:id', userController.getUser)
 
 router.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard)
 router.get('/restaurants/:id', authenticated, restController.getRestaurant)
