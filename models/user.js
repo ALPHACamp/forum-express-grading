4c62b52c8a30358956a7231e10848a424a4d520f
klu0926 <lukuoyu@gmail.com>
2023-05-18 20:56:34 +0800

    feat:create followship model

--- models/user.js
+++ models/user.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate (models) {
+    static associate(models) {
       User.hasMany(models.Comment, { foreignKey: 'userId' })
       // 多對多
       User.belongsToMany(models.Restaurant, {
@@ -21,6 +21,18 @@ module.exports = (sequelize, DataTypes) => {
         through: models.Like,
         foreignKey: 'userId',
         as: 'LikedRestaurants'
+      })
+      // 用被追的人的身份 (followingId) 去查追我的人
+      User.belongsToMany(models.User, {
+        through: models.Followship,
+        foreignKey: 'followingId',
+        as: 'Followers'
+      })
+      // 用追人的身份 (followerId) 去查被我追的人
+      User.belongsToMany(models.User, {
+        through: models.Followship,
+        foreignKey: 'followerId',
+        as: 'Followings'
       })
     }
   };
