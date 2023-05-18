668cccc6a259326e2c8caa05b4dd6b262caffb29
etandmouse <etandmouse@gmail.copm>
2023-05-18 20:55:04 +0800

    feat: add dashboard page and viewCounts for restaurant

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -18,16 +18,33 @@ const restaurantController = {
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
       include: Category,
+      nest: true
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        return restaurant.increment('viewCounts', { by: 1 })
+          .then(restaurant => {
+            res.render('restaurant', {
+              restaurant: restaurant.toJSON()
+            })
+          })
+      })
+      .catch(err => next(err))
+  },
+  getDashboard: (req, res, next) => {
+    const id = req.params.id
+
+    return Restaurant.findByPk(id, {
+      include: Category,
       nest: true,
       raw: true
     })
       .then(restaurant => {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
-        res.render('restaurant', {
+        res.render('dashboard', {
           restaurant
         })
       })
-      .catch(err => next(err))
   }
 }
 
