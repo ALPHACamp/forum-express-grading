604324c834a3d7f581e4e5d40aeb010181994a47
LongXiangL <bfh1104@gmail.com>
2023-05-18 19:49:44 +0800

    feat: update seed files

--- models/category.js
+++ models/category.js
@@ -8,7 +8,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate (models) {
-      Category.hasMany(models.Restaurant, { foreignKey: 'categoryId' }) // 新增這裡
+      Category.hasMany(models.Restaurant, { foreignKey: 'categoryId' })
     }
   };
   Category.init({
