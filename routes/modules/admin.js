1e0bde5b98cd6e7eb19efdfc9bf53f0f08a265c8
aking4511 <aking4511@gmail.com>
2023-05-18 14:41:10 +0800

    feat: delete restaurant

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -6,6 +6,7 @@ router.get('/restaurants/create', adminController.createRestaurant)
 router.get('/restaurants/:id/edit', adminController.editRestaurant)
 router.get('/restaurants/:id', adminController.getRestaurant) // create 字串會先被解讀成 :id, 所以create放上面
 router.put('/restaurants/:id', adminController.putRestaurant)
+router.delete('/restaurants/:id', adminController.deleteRestaurant)
 router.get('/restaurants', adminController.getRestaurants)
 router.post('/restaurants', adminController.postRestaurant)
 router.use('/', (req, res) => res.redirect('/admin/restaurants'))
