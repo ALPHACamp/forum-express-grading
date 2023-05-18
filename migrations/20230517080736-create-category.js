a34211c90b1f636aa0723b4bd64731b9023a5f64
qpooqpooqp <kkk3kkk3kkk3@yahoo.com.tw>
2023-05-18 15:27:36 +0800

    feat: add category model & upsate seed file

--- migrations/20230517080736-create-category.js
+++ migrations/20230517080736-create-category.js
@@ -1,7 +1,7 @@
 'use strict'
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Users', {
+    await queryInterface.createTable('Categories', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -9,12 +9,6 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       name: {
-        type: Sequelize.STRING
-      },
-      email: {
-        type: Sequelize.STRING
-      },
-      password: {
         type: Sequelize.STRING
       },
       created_at: {
@@ -28,6 +22,6 @@ module.exports = {
     })
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Users')
+    await queryInterface.dropTable('Categories')
   }
 }
