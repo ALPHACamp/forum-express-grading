bc577497b780a2bfc624f4a82df29810fea463b8
Wei <awei1127@hotmail.com>
2023-05-19 17:40:24 +0800

    feat: adjust middleware/auth.js
    feat: add users page
    
    fix bugs to pass test

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,7 @@
-const { getUser, ensureAuthenticated } = require('../helpers/auth-helpers')
+const helpers = require('../helpers/auth-helpers')
 
 const authenticated = (req, res, next) => {
-  if (ensureAuthenticated(req)) {
+  if (helpers.ensureAuthenticated(req)) {
     next()
   } else {
     res.redirect('/signin')
@@ -9,8 +9,8 @@ const authenticated = (req, res, next) => {
 }
 
 const authenticatedAdmin = (req, res, next) => {
-  if (ensureAuthenticated(req)) {
-    if (getUser(req).isAdmin) {
+  if (helpers.ensureAuthenticated(req)) {
+    if (helpers.getUser(req).isAdmin) {
       return next()
     }
     res.redirect('/')
