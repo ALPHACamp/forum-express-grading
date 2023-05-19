6da43fc88356fe1e30d98e8ccc10b1b0971dd6bd
playcsgo <sam@plum.wine>
2023-05-20 06:05:49 +0800

    feat: front entry

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express')
 const routes = require('./routes')
-
 const app = express()
 const port = process.env.PORT || 3000
+const handlebars = require('express-handlebars')
+
+app.engine('hbs', handlebars({ extname: '.hbs' }))
+app.set('view engine', 'hbs')
 
 app.use(routes)
 
---
+++ controllers/restaurant-controller.js
@@ -0,0 +1,7 @@
+const restaurantColler = {
+  getRestaurants: (req, res) => {
+    return res.render('restaurant')
+  }
+}
+
+module.exports = restaurantColler
