111e95bbae10d07a32f42dca7ce323efbda7614b
Lanways <mcbluedd@gmail.com>
2023-05-19 21:11:15 +0800

    feat: delete restaurant function

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -8,6 +8,7 @@ router.get('/restaurants/:id/edit', adminController.editRestaurant)
 router.get('/restaurants/:id',
   adminController.getRestaurant)
 router.put('/restaurants/:id', adminController.putRestaurant)
+router.delete('/restaurants/:id', adminController.deleteRestaurant)
 router.get('/restaurants',
   adminController.getRestaurants)
 
