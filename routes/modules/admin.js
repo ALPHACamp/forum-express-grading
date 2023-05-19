aac32c9c42e0a032ba748d4bb690b7cf6bcb4855
azx79115 <azx79115@gmail.com>
2023-05-19 16:13:12 +0800

    更改test指令 (+1 squashed commits)
    Squashed commits:
    
    [f54d873] 增加後臺管理權限功能

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -8,6 +8,8 @@ router.get('/restaurants/:id/edit', adminController.editRestaurant)
 router.put('/restaurants/:id', upload.single('image'), adminController.putRestaurant) // 修改餐廳
 router.get('/restaurants/:id', adminController.getRestaurant)
 router.delete('/restaurants/:id', adminController.deleteRestaurant)
+router.patch('/users/:id', adminController.patchUser)
+router.get('/users', adminController.getUsers)
 router.get('/restaurants', adminController.getRestaurants)
 router.post('/restaurants', upload.single('image'), adminController.postRestaurant) // 新增餐廳
 router.use('/', (req, res) => res.redirect('/admin/restaurants'))
