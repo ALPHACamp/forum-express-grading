c1ccd721293d64ab3c3caabbeaf0d442149d8e14
Chingsan0722 <rory85722@gmail.com>
2023-05-19 11:34:10 +0800

    copy previous

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
+const path = require('path')
 const express = require('express')
+const handlebars = require('express-handlebars')
+const handlebarsHelpers = require('./helpers/handlebars-helpers')
+const flash = require('connect-flash')
+const session = require('express-session')
+const methodOverride = require('method-override')
+const passport = require('passport')
+const { getUser } = require('./helpers/auth-helpers')
 const routes = require('./routes')
-
 const app = express()
 const port = process.env.PORT || 3000
-
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+const SESSION_SECRET = 'secret'
+app.engine('hbs', handlebars({ extname: '.hbs', helpers: handlebarsHelpers }))
+app.set('view engine', 'hbs')
+app.use(express.urlencoded({ extended: true }))
+app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
+app.use(methodOverride('_method'))
+app.use('/upload', express.static(path.join(__dirname, 'upload')))
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  res.locals.user = getUser(req)
+  next()
+})
 app.use(routes)
 
 app.listen(port, () => {
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,27 @@
 const express = require('express')
+const passport = require('../config/passport')
 const router = express.Router()
+// 引入controller
+const restController = require('../controllers/restaurant-controller')
+const userController = require('../controllers/user-controller')
+// 引入middleware
+const { authenticated, authenticatedAdmin } = require('../middleware/auth')
+const { generalErrorHandler } = require('../middleware/error-handler')
+// 後台
+const admin = require('./modules/admin')
+router.use('/admin', authenticatedAdmin, admin)
+// 註冊
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
+// 登入
+router.get('/signin', userController.signInPage)
+router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
+// 登出
+router.get('/logout', userController.logout)
+// 餐廳頁
+router.get('/restaurants', authenticated, restController.getRestaurants)
 
-router.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+router.use('/', (req, res) => { res.redirect('/restaurants') })
+router.use('/', generalErrorHandler)
 
 module.exports = router
