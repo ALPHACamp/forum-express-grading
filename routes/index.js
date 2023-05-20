282615b0a0f7a992758b9741e88904be45872f43
JimmyLin <disillusioned2077@gmail.com>
2023-05-20 16:25:59 +0800

    feat: create admin router

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,10 @@ const router = express.Router()
 // 新增，載入 controller
 const restController = require('../controllers/restaurant-controller')
 
-router.get('/restaurants', restController.getRestaurants)
+const admin = require('./modules/admin')
 
+router.use('/admin', admin)
+router.get('/restaurants', restController.getRestaurants)
 router.use('/', (req, res) => res.redirect('/restaurants'))
 
 module.exports = router
