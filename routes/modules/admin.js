4e5316abd49eb8e93f3ddb20c6432bce4f176a02
KenYuChang <justin0915justin0915@gmail.com>
2023-05-20 13:12:47 +0800

    done

---
+++ routes/modules/admin.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const upload = require("../../middleware/multer");
+const router = express.Router();
+const adminController = require("../../controllers/admin-controller");
+router.get("/restaurants/create", adminController.createRestaurant);
+router.get("/restaurants/:id/edit", adminController.editRestaurant);
+router.get("/restaurants/:id", adminController.getRestaurant);
+router.put(
+  "/restaurants/:id",
+  upload.single("image"),
+  adminController.putRestaurant
+);
+router.delete("/restaurants/:id", adminController.deleteRestaurant);
+router.get("/restaurants", adminController.getRestaurants);
+router.post(
+  "/restaurants",
+  upload.single("image"),
+  adminController.postRestaurant
+);
+router.use("/", (req, res) => res.redirect("/admin/restaurants"));
+module.exports = router;
