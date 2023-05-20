0b0e9bbd8b78e205797b39c1322e104b81e3fdbb
Anna0118 <annachu0988@gmail.com>
2023-05-20 17:55:15 +0800

    feat: create restaurant

--- routes/index.js
+++ routes/index.js
@@ -5,10 +5,10 @@ const passport = require('../config/passport') // @Add
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
 const { generalErrorHandler } = require('../middleware/error-handler')
-const { authenticated } = require('../middleware/auth') // 引入 auth.js
+const { authenticated, authenticatedAdmin } = require('../middleware/auth') // 引入 auth.js
 
 const admin = require('./modules/admin')
-router.use('/admin', admin)
+router.use('/admin', authenticatedAdmin, admin)
 
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
