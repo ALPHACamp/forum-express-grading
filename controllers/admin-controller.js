0e94d78b67e47f63f24cb334266e195392450fbc
king610160 <king610160@gmail.com>
2023-05-20 15:31:56 +0800

    add patchUser controller, and pass the testing.

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -86,7 +86,7 @@ const adminController = {
       .catch(err => next(err))
   },
   getUsers: (req, res, next) => {
-    User.findAll({
+    return User.findAll({
       raw: true // 讓拿到的資料是最簡單的javascript資料
     })
       .then(users => {
@@ -104,21 +104,20 @@ const adminController = {
       .then(users => res.render('admin/all-users-authority', { users }))
       .catch(err => next(err))
   },
-  putUsers: (req, res, next) => {
-    User.findAll({
-      raw: true // 讓拿到的資料是最簡單的javascript資料
-    })
-      .then(users => {
-        users.forEach(element => {
-          if (element.isAdmin === '1') {
-            element.isAdmin = 'admin'
-          }
-          element.isAdmin = 'user'
+  patchUser: (req, res, next) => {
+    return User.findByPk(req.params.id)
+      .then(user => {
+        if (!user) throw new Error("User didn't exist!") // 沒有資料就拋錯
+        if (user.dataValues.email === 'root@example.com') { // 若email = root@example.com則提示錯誤
+          req.flash('error_messages', '禁止變更 root 權限')
+          return res.redirect('back')
+        }
+        user.update({ // 只要email不是root@example.com, 就admin和user身分對換
+          isAdmin: !(user.dataValues.isAdmin)
         })
-        console.log(users)
-        return users
+        return req.flash('success_messages', '使用者權限變更成功')
       })
-      .then(users => res.render('admin/all-users-authority', { users }))
+      .then(() => res.redirect('/admin/users'))
       .catch(err => next(err))
   }
 }
