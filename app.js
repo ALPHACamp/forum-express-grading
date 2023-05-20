b9aacdf32e8e7ad81048cf3c4ff0f1b191c335b2
playcsgo <sam@plum.wine>
2023-05-21 05:10:45 +0800

    feat: flash msg & signup verification

--- app.js
+++ app.js
@@ -3,10 +3,20 @@ const routes = require('./routes')
 const app = express()
 const port = process.env.PORT || 3000
 const handlebars = require('express-handlebars')
+const session = require('express-session')
+const SESSION_SECRET = 'secret'
+const flash = require('connect-flash')
 
 app.use(express.urlencoded({ extended: true }))
 app.engine('hbs', handlebars({ extname: '.hbs' }))
 app.set('view engine', 'hbs')
+app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 
 app.use(routes)
 
--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
-'use strict';
+'use strict'
 const {
   Model
-} = require('sequelize');
+} = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    static associate (models) {
       // define association here
     }
   };
@@ -22,6 +22,6 @@ module.exports = (sequelize, DataTypes) => {
     modelName: 'User',
     tableName: 'Users',
     underscored: true
-  });
-  return User;
-};
+  })
+  return User
+}
