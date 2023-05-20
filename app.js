c9e17aefd4df8c45ad855ea9f08d857b32e1c464
vinchiengithub <jianfan0114@gmail.com>
2023-05-20 19:34:57 +0800

    補上 require handlbars

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const routes = require('./routes')
+const handlebars = require('express-handlebars')
 
 const app = express()
 const port = process.env.PORT || 3000
