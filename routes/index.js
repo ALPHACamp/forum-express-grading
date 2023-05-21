d10f0cb7ccc2c87610a44ed8e8ddfb20fac49d62
WeiWayne1030 <wayne10032004@gmail.com>
2023-05-21 21:57:21 +0800

    fix: test render route

--- routes/index.js
+++ routes/index.js
@@ -22,7 +22,7 @@ router.get('/logout', userController.logout)
 
 router.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard)
 router.get('/restaurants/:id', authenticated, restController.getRestaurant)
-router.get('/restaurants', authenticated, restController.getRestaurants)
+//router.get('/restaurants', authenticated, restController.getRestaurants)
 
 router.use('/', generalErrorHandler)
 router.use('/', (req, res) => res.redirect('/restaurants'))
