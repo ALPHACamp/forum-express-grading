0fdeecdc7696998fed9bf6874f29c27cc14998c5
larrytherealboy <abclarryabclarry@gmail.com>
2023-05-21 13:31:17 +0800

    combine R01 branch to main

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,12 +1,33 @@
-const { Restaurant } = require('../models')
+const { Restaurant, User } = require('../models')
 const { imgurFileHandler } = require('../helpers/file-helpers')
 
 const adminController = {
   getRestaurants: (req, res, next) => {
-    Restaurant.findAll({
+    return Restaurant.findAll({
       raw: true
     })
       .then(restaurants => res.render('admin/restaurants', { restaurants }))
+      .catch(err => next(err))
+  },
+  getUsers: (req, res, next) => {
+    return User.findAll({
+      raw: true
+    })
+      .then(users => res.render('admin/users', { users }))
+      .catch(err => next(err))
+  },
+  patchUser: (req, res, next) => {
+    return User.findByPk(req.params.id)
+      .then(user => {
+        if (!user) throw new Error("User didn't exist!")
+        if (user.email === 'root@example.com') {
+          req.flash('error_messages', '禁止變更 root 權限')
+          res.redirect('back')
+        }
+        user.update({ isAdmin: !(user.dataValues.isAdmin) })
+        req.flash('success_messages', '使用者權限變更成功')
+        res.redirect('/admin/users')
+      })
       .catch(err => next(err))
   },
   createRestaurant: (req, res) => {
