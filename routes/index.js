8f6dd14ba7e77eec4066b51548127549930d321c
Ariel <ariel040132@gmail.com>
2023-05-22 03:09:10 +0800

    feat: add admin.getUsers & admin.patchUser

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 const express = require('express')
-const restaurantController = require('../controllers/restaurant-controller')
 const router = express.Router()
 const passport = require('../config/passport')
 const admin = require('./modules/admin')
+
+const restaurantController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
 const { authenticated, authenticatedAdmin } = require('../middleware/auth')
 const { generalErrorHandler } = require('../middleware/error-handler')
@@ -13,6 +14,7 @@ router.post('/signup', userController.signUp)
 router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
 router.get('/logout', userController.logout)
+
 router.get('/restaurants', authenticated, restaurantController.getRestaurants)
 router.use('/', (req, res) => res.redirect('/restaurants'))
 router.use('/', generalErrorHandler)
