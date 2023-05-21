5ae70fb5d28f6533e3af81684bb77764978351a7
Suz033 <susanamy2324@gmail.com>
2023-05-21 22:11:10 +0800

    feat: create restaurant

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -9,6 +9,30 @@ const adminController = {
     })
       .then(restaurants => res.render('admin/restaurants', { restaurants }))
       .catch(err => next(err))
+  },
+  createRestaurant: (req, res) => {
+    return res.render('admin/create-restaurant')
+  },
+  postRestaurant: (req, res, next) => {
+    // 從 req.body 拿出表單裡的資料
+    const { name, tel, address, openingHours, description } = req.body
+    // name 是必填，若發先是空值就會終止程式碼，並在畫面顯示錯誤提示
+    if (!name) throw new Error('Restaurant name is required!')
+    // 產生一個新的 Restaurant 物件實例，並存入資料庫
+    Restaurant.create({
+      name,
+      tel,
+      address,
+      openingHours,
+      description
+    })
+      .then(() => {
+        // 在畫面顯示成功提示
+        req.flash('success_messages', 'restaurant was successfully created')
+        // 新增完成後導回後台首頁
+        res.redirect('/admin/restaurants')
+      })
+      .catch(err => next(err))
   }
 }
 
