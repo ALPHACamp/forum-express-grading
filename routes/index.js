7556f027b9992570d5089ce5786dd9fcf78abbc0
JimmyLin <disillusioned2077@gmail.com>
2023-05-22 00:55:10 +0800

    feat: add create restaurant page and feature

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,12 @@ const passport = require('../config/passport')
 // 新增，載入 controller
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
-const { authenticated } = require('../middleware/auth')
+const { authenticated, authenticatedAdmin } = require('../middleware/auth')
 const { generalErrorHandler } = require('../middleware/error-handler') // 使用{}是為了提取../middleware/error-handler內的解構賦值(function跑完後的值)，若沒加上{}會直接匯入整個function
 
 const admin = require('./modules/admin')
 
-router.use('/admin', admin)
+router.use('/admin', authenticatedAdmin, admin)
 // 註冊
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
-const { authenticatedAdmin } = require('../../middleware/auth')
 
-router.get('/restaurants', authenticatedAdmin, adminController.getRestaurants) // 進入/restaurants前先驗證是否為adm，是才能帶入adm畫面
+router.get('/restaurants/create', adminController.createRestaurant) // 新增餐廳頁面
+router.get('/restaurants', adminController.getRestaurants) // 進入/restaurants前先驗證是否為adm，是才能帶入adm畫面
+router.post('/restaurants', adminController.postRestaurant) // 新增餐廳資訊
 router.use('/', (req, res) => res.redirect('/admin/restaurants'))
 
 module.exports = router
