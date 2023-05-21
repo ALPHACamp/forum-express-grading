0ff8d51ead51a78a281aa55a3130d24db05bb323
shaochenn <hnhngtb1111@gmail.com>
2023-05-21 18:49:07 +0800

    feat: create restaurant

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -2,14 +2,29 @@ const { Restaurant } = require('../models')
 
 const adminController = {
   getRestaurants: (req, res, next) => {
-    Restaurant.findAll({
-
-      raw: true
-
+    return Restaurant.findAll({ raw: true })
+      .then(restaurants => {
+        return res.render('admin/restaurants', { restaurants })
+      })
+      .catch(err => next(err))
+  },
+  createRestaurant: (req, res) => {
+    return res.render('admin/create-restaurant')
+  },
+  postRestaurant: (req, res, next) => {
+    const { name, tel, address, openingHours, description } = req.body
+    if (!name) throw new Error('Restaurant name is required!') // name 是必填，若發先是空值就會終止程式碼，並在畫面顯示錯誤提示
+    Restaurant.create({
+      name,
+      tel,
+      address,
+      openingHours,
+      description
     })
-
-      .then(restaurants => res.render('admin/restaurants', { restaurants }))
-
+      .then(() => {
+        req.flash('success_messages', 'restaurant was successfully created')
+        res.redirect('/admin/restaurants')
+      })
       .catch(err => next(err))
   }
 }
