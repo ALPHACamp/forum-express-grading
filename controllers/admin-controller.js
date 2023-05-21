ad0e06d0e1399796e5e09df5dafb3aa2ed0a922d
Anna0118 <annachu0988@gmail.com>
2023-05-21 11:34:43 +0800

    feat: add imgur api

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,5 +1,7 @@
 const { Restaurant } = require('../models')
 const { localFileHandler } = require('../helpers/file-helpers')
+const { imgurFileHandler } = require('../helpers/file-helpers')
+
 const adminController = {
   getRestaurants: (req, res, next) => {
     Restaurant.findAll({
@@ -17,7 +19,7 @@ const adminController = {
     const { name, tel, address, openingHours, description } = req.body
     if (!name) throw Error('Restaurant name is required!')
     const { file } = req // const file = req.file
-    localFileHandler(file)
+    imgurFileHandler(file)
       .then(filePath =>
         Restaurant.create({
           name,
@@ -26,7 +28,8 @@ const adminController = {
           openingHours,
           description,
           Image: filePath || null
-        }))
+        })
+      )
       .then(() => {
         req.flash('success_messages', 'restaurant was successfully created')
         res.redirect('/admin/restaurants')
@@ -62,13 +65,16 @@ const adminController = {
     const { name, tel, address, openingHours, description } = req.body
     if (!name) throw new Error('Restaurant name is required!')
     const { file } = req // 把檔案取出來
-    Promise.all([ // 非同步處理
+    Promise.all([
+      // 非同步處理
       Restaurant.findByPk(req.params.id), // 去資料庫查有沒有這間餐廳
-      localFileHandler(file) // 把檔案傳到 file-helper 處理
+      imgurFileHandler(file) // 把檔案傳到 file-helper 處理
     ])
-      .then(([restaurant, filePath]) => { // 以上兩樣事都做完以後
+      .then(([restaurant, filePath]) => {
+        // 以上兩樣事都做完以後
         if (!restaurant) throw new Error("Restaurant didn't exist!")
-        return restaurant.update({ // 修改這筆資料
+        return restaurant.update({
+          // 修改這筆資料
           name,
           tel,
           address,
