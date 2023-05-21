adb50f23681857c323eb564ae0b4422bc82be784
JimmyLin <disillusioned2077@gmail.com>
2023-05-21 14:31:24 +0800

    feat: careate signup function,add bcrypt and seesion,add flash message feature

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,17 @@ const express = require('express')
 const router = express.Router()
 // 新增，載入 controller
 const restController = require('../controllers/restaurant-controller')
+const userController = require('../controllers/user-controller')
+const { generalErrorHandler } = require('../middleware/error-handler') // 使用{}是為了提取../middleware/error-handler內的解構賦值(function跑完後的值)，若沒加上{}會直接匯入整個function
 
 const admin = require('./modules/admin')
 
 router.use('/admin', admin)
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
 router.get('/restaurants', restController.getRestaurants)
 router.use('/', (req, res) => res.redirect('/restaurants'))
+
+router.use('/', generalErrorHandler)
 
 module.exports = router
