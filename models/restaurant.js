91cd6b584db208b0a406f91793daa1f6c1f7515a
LongXiangL <bfh1104@gmail.com>
2023-05-22 00:44:40 +0800

    finsh R02

--- models/restaurant.js
+++ models/restaurant.js
@@ -19,11 +19,11 @@ module.exports = (sequelize, DataTypes) => {
     address: DataTypes.STRING,
     openingHours: DataTypes.STRING,
     description: DataTypes.TEXT,
-    image: DataTypes.STRING
+    image: DataTypes.STRING,
+    viewCount: DataTypes.INTEGER
   }, {
     sequelize,
     modelName: 'Restaurant',
-    tableName: 'Restaurants',
     underscored: true
   })
   return Restaurant
