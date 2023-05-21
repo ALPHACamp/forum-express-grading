f8aa5884c7f867da5c829f4246fb9111f99eaaf6
jasmine <jasmine12122005@yahoo.com.tw>
2023-05-21 22:23:16 +0800

    feat: add user model

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
-const routes = require('./routes')
 
 const app = express()
 const port = process.env.PORT || 3000
+const db = require('./models')
 
 app.engine('hbs', handlebars({ extname: '.hbs' }))
 app.set('view engine', 'hbs')
