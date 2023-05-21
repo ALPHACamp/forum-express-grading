c03eea60d43f1b92d589acd88981c3f9ccb3d60c
goodjobhot401 <goodjobhot@gmail.com>
2023-05-21 16:21:17 +0800

    feat: add categories selector on admin creat and edit pade & modify admin-restaurants-view-page

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -13,12 +13,14 @@ const adminController = {
       })
   },
 
-  createRestaurant: (req, res) => {
-    return res.render('admin/create-restaurant')
+  createRestaurant: (req, res, next) => {
+    return Category.findAll({ raw: true })
+      .then(categories => res.render('admin/create-restaurant', { categories }))
+      .catch(err => next(err))
   },
 
   postRestaurant: (req, res, next) => {
-    const { name, tel, address, openingHours, description } = req.body
+    const { name, tel, address, openingHours, description, categoryId } = req.body
 
     if (!name) throw new Error('Restaurant name is required!')
 
@@ -30,6 +32,7 @@ const adminController = {
         address,
         openingHours,
         description,
+        categoryId,
         image: filePath || null
       }))
       .then(() => {
@@ -49,16 +52,19 @@ const adminController = {
   },
 
   editRestaurant: (req, res, next) => {
-    Restaurant.findByPk(req.params.id, { raw: true })
-      .then(restaurant => {
+    return Promise.all([
+      Restaurant.findByPk(req.params.id, { raw: true }),
+      Category.findAll({ raw: true })
+    ])
+      .then(([restaurant, categories]) => {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
-        return res.render('admin/edit-restaurant', { restaurant })
+        return res.render('admin/edit-restaurant', { restaurant, categories })
       })
       .catch(err => next(err))
   },
 
   putRestaurant: (req, res, next) => {
-    const { name, tel, address, openingHours, description } = req.body
+    const { name, tel, address, openingHours, description, categoryId } = req.body
     if (!name) throw new Error('Restaurant name is required!')
 
     const { file } = req
@@ -75,6 +81,7 @@ const adminController = {
           address,
           openingHours,
           description,
+          categoryId,
           image: filePath || restaurant.image
         })
       })
