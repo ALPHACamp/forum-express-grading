27846e06132a6ee5fd6d03b515be9bdc5c50ce61
thpss91103 <a0975142932@gmail.com>
2023-05-21 20:25:14 +0800

    feat: merge R02

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -18,14 +18,26 @@ const restaurantController = {
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
       include: Category, // 拿出關聯的 Category model
+      nest: true
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        return restaurant.increment('viewCounts', { by: 1 })
+      })
+      .then(restaurant => {
+        return res.render('restaurant', { restaurant: restaurant.toJSON() })
+      })
+      .catch(err => next(err))
+  },
+  getDashboard: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, {
+      include: Category, // 拿出關聯的 Category model
       nest: true,
       raw: true
     })
       .then(restaurant => {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
-        res.render('restaurant', {
-          restaurant
-        })
+        res.render('dashboard', { restaurant })
       })
       .catch(err => next(err))
   }
