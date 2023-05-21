4dabb107b6e8a8721dab9afbb92cdf7b9f52a033
jasmine <jasmine12122005@yahoo.com.tw>
2023-05-21 22:25:39 +0800

    feat: user signup

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
+const routes = require('./routes')
 
 const app = express()
 const port = process.env.PORT || 3000
-const db = require('./models')
 
 app.engine('hbs', handlebars({ extname: '.hbs' }))
 app.set('view engine', 'hbs')
+app.use(express.urlencoded({ extended: true }))
 app.use(routes)
 app.listen(port, () => {
   console.info(<code>Example app listening on port ${port}!</code>)
