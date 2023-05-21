ef0996b66f6882dbc91b3a2f9019f1513d558460
eunicebibi <agjay410@gmail.com>
2023-05-21 15:54:43 +0800

    feat: add dashboard

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -23,13 +23,29 @@ const restaurantController = {
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, { // Pk 是 primary key 的簡寫，也就是餐廳的 id，去資料庫用 id 找一筆資料
-      raw: true, // 找到以後整理格式再回傳
-      nest: true,
       include: [Category]
     })
       .then(restaurant => {
         if (!restaurant) throw new Error("Restaurant didn't exist!") //  如果找不到，回傳錯誤訊息，後面不執行
-        res.render('restaurant', { restaurant })
+        restaurant.increment('viewCount', { by: 1 })
+        return restaurant
+      })
+      .then(restaurant => {
+        res.render('restaurant', {
+          restaurant: restaurant.toJSON()
+        })
+      })
+      .catch(err => next(err))
+  },
+  getDashboard: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, {
+      raw: true,
+      nest: true,
+      include: [Category]
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        return res.render('dashboard', { restaurant })
       })
       .catch(err => next(err))
   }
