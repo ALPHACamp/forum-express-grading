f80765f0df48245a8af7d383a7ed0032ba4e0582
rr37 <ay870527@gmail.com>
2023-05-22 19:03:58 +0800

    feat: add restaurant page

--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,19 @@ const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
 const { authenticated, authenticatedAdmin } = require('../middleware/auth')
 const { generalErrorHandler } = require('../middleware/error-handler')
+
+// admin 總路由
 router.use('/admin', authenticatedAdmin, admin)
+
+// 註冊、登入、登出
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
 router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
 router.get('/logout', userController.logout)
+
+// 前台
+router.get('/restaurants/:id', authenticated, restController.getRestaurant)
 router.get('/restaurants', authenticated, restController.getRestaurants)
 router.use('/', (req, res) => res.redirect('/restaurants'))
 router.use('/', generalErrorHandler)
