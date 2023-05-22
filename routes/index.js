630b0b5110fa07e004c8dcfc792989bbc283776e
vinchiengithub <jianfan0114@gmail.com>
2023-05-22 19:52:12 +0800

    feat: finish R02

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const router = express.Router()
 const passport = require('../config/passport')
+const admin = require('./modules/admin')
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
 const { authenticated, authenticatedAdmin } = require('../middleware/auth')
 const { generalErrorHandler } = require('../middleware/error-handler')
-const admin = require('./modules/admin')
 
 router.use('/admin', authenticatedAdmin, admin)
 router.get('/signup', userController.signUpPage)
@@ -18,13 +18,16 @@ router.post(
     failureFlash: true
   }),
   userController.signIn
-) // 注意是 post
+)
 router.get('/logout', userController.logout)
+router.get(
+  '/restaurants/:id/dashboard',
+  authenticated,
+  restController.getDashboard
+)
 router.get('/restaurants/:id', authenticated, restController.getRestaurant)
-router.get('/restaurants', authenticated, restController.getRestaurants) // 修改這行，新增 authenticated 參數
-
-router.use('/', (req, res) => res.redirect('/restaurants'))
-
+router.get('/restaurants', authenticated, restController.getRestaurants)
+router.get('/', (req, res) => res.redirect('/restaurants'))
 router.use('/', generalErrorHandler)
 
 module.exports = router
