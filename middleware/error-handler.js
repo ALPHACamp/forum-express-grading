9b929114a29fcfba3a1d62f04021c34979886fd5
arubakingpikachu <85471527+arubakingpikachu@users.noreply.github.com>
2023-05-22 23:06:13 +0800

    feat: flash msg & signup verification

---
+++ middleware/error-handler.js
@@ -0,0 +1,11 @@
+module.exports = {
+  generalErrorHandler (err, req, res, next) {
+    if (err instanceof Error) { // 判斷傳入的 err 是不是一個 Error 物件
+      req.flash('error_messages', `${err.name}: ${err.message}`)// 有屬性 name 和 message，那麼就利用快閃訊息把值印出來給使用者看
+    } else {
+      req.flash('error_messages', `${err}`)// 如果不是 Error 物件，可能傳進來一堆錯誤報告，直接把字串印出來即可
+    }
+    res.redirect('back')
+    next(err)
+  }
+}
