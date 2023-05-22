68403df06459fce395904cdfcf7f1144da0ae379
sherryylin0719 <sherryylin0719@gmail.com>
2023-05-22 23:37:19 +0900

    feat: add R01 test request

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -3,6 +3,8 @@ const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
 const upload = require('../../middleware/multer')
 
+router.patch('/users/:id', adminController.patchUser)
+router.get('/users', adminController.getUsers)
 router.get('/restaurants/create', adminController.createRestaurant)
 router.get('/restaurants/:id/edit', adminController.editRestaurant)
 router.get('/restaurants/:id', adminController.getRestaurant)
@@ -10,5 +12,6 @@ router.put('/restaurants/:id', upload.single('image'), adminController.putRestau
 router.delete('/restaurants/:id', adminController.deleteRestaurant)
 router.get('/restaurants', adminController.getRestaurants)
 router.post('/restaurants', upload.single('image'), adminController.postRestaurant)
+
 router.use('/', (req, res) => res.redirect('/admin/restaurants'))
 module.exports = router
