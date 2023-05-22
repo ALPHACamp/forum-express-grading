8bf73cc70ba6f82b5d82091eecd8648d87fe0fc3
WeiWayne1030 <wayne10032004@gmail.com>
2023-05-22 14:14:11 +0800

    feat:show comments on restaurant page

--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -1,6 +1,10 @@
 const dayjs = require('dayjs') // 載入 dayjs 套件
+const relativeTime = require('dayjs/plugin/relativeTime') // ���入 relativeTime ���
+dayjs.extend(relativeTime)
+
 module.exports = {
   currentYear: () => dayjs().year(), // 取得當年年份作為 currentYear 的屬性值，並導出
+  relativeTimeFromNow: a => dayjs(a).fromNow(), // 取得��年年份作��
   ifCond: function (a, b, options) {
     return a === b ? options.fn(this) : options.inverse(this)
   }
