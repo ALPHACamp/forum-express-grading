42cf517e03f2a696134b24d775b664dfa3c9da5d
s1030905 <s1030905@outlook.com>
2023-05-22 11:19:24 +0800

    feat: add ifCond hbs helper & update category selector

--- helpers/handlebars-helper.js
+++ helpers/handlebars-helper.js
@@ -1,5 +1,8 @@
 const dayjs = require('dayjs')
 
 module.exports = {
-  currentYear: () => dayjs().year()
+  currentYear: () => dayjs().year(),
+  ifCond: function (a, b, option) {
+    return a === b ? option.fn(this) : option.inverse(this)
+  }
 }
