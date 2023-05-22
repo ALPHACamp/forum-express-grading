458bb44e285ecb1cb6b3b05c217b186598732aff
newm1n <fbsns20@gmail.com>
2023-05-22 14:00:04 +0800

    feat: create restaurant

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,15 @@
 const express = require('express')
 const router = express.Router()
 const passport = require('../config/passport')
+const admin = require('./modules/admin')
 
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
 
-const { authenticated } = require('../middleware/auth')
+const { authenticated, authenticatedAdmin } = require('../middleware/auth')
 const { generalErrorHandler } = require('../middleware/error-handler')
 
-const admin = require('./modules/admin')
-
-router.use('/admin', admin)
+router.use('/admin', authenticatedAdmin, admin)
 
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
@@ -24,10 +23,10 @@ router.post(
   }),
   userController.signIn
 )
+
 router.get('/logout', userController.logout)
 
 router.get('/restaurants', authenticated, restController.getRestaurants)
-
 router.use('/', (req, res) => res.redirect('/restaurants'))
 
 router.use('/', generalErrorHandler)
