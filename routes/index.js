874617fd2460a98b3fb23dc08d0162e0633e1906
Leechiayu <gary86442@gmail.com>
2023-05-22 14:43:26 +0800

    feat:add auth and all helpers

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,35 @@
 const express = require('express');
 
 const router = express.Router();
+const passport = require('../config/passport');
+
+//* 載入middleware
+const { authenticated, authenticatedAdmin } = require('../middleware/auth');
 //* 載入子路由
 const admin = require('./modules/admin');
 
 //* 載入controller
 const restController = require('../controllers/restaurant-controller');
+const userController = require('../controllers/user-controller');
+const { generalErrorHandler } = require('../middleware/error-handler');
+//* 後台
+router.use('/admin', authenticatedAdmin, admin);
 
-router.use('/admin', admin);
-router.get('/restaurants', restController.getRestaurants);
+//* 使用者
+router.get('/signup', userController.signUpPage);
+router.post('/signup', userController.signUp);
+router.get('/signin', userController.signInPage);
+router.post(
+  '/signin',
+  passport.authenticate('local', {
+    failureRedirect: '/signin',
+    failureFlash: true
+  }),
+  userController.signIn
+);
+router.get('/restaurants', authenticated, restController.getRestaurants);
 
 router.use('/', (req, res) => res.redirect('/restaurants'));
+router.use('/', generalErrorHandler);
 
 module.exports = router;
