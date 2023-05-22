a5e5f5200df37284b0c5d3625ab58088a213c690
KenYuChang <justin0915justin0915@gmail.com>
2023-05-22 20:47:57 +0800

    edt route for categories

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -20,6 +20,9 @@ router.post(
 );
 router.patch("/users/:id", adminController.patchUser);
 router.get("/users", adminController.getUsers);
+router.get("/categories/:id", categoryController.getCategories);
+router.put("/categories/:id", categoryController.putCategory);
 router.get("/categories", categoryController.getCategories);
+router.post("/categories", categoryController.postCategories);
 router.use("/", (req, res) => res.redirect("/admin/restaurants"));
 module.exports = router;
