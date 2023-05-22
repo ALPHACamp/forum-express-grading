270089b74e9ebba7e1181a9c4093541c6d2bf18d
Leechiayu <gary86442@gmail.com>
2023-05-22 15:30:54 +0800

    feat:add CRUD

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const handlebars = require('express-handlebars');
 const flash = require('connect-flash');
 const session = require('express-session');
 const passport = require('./config/passport');
+const methodOverride = require('method-override');
 const { getUser } = require('./helpers/auth-helpers');
 const handlebarsHelpers = require('./helpers/handlebars-helpers'); // 引入 handlebars-helpers
 const app = express();
@@ -20,7 +21,8 @@ app.use(
 );
 app.use(passport.initialize()); // 增加這行，初始化 Passport
 app.use(passport.session()); // 增加這行，啟動 session 功能
-app.use(flash()); // 掛載套件
+app.use(flash());
+app.use(methodOverride('_method'));
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages'); // 設定 success_msg 訊息
   res.locals.error_messages = req.flash('error_messages'); // 設定 warning_msg 訊息
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,6 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const adminController = require('../../controllers/admin-controller');
+//* 新增餐廳
+router.get('/restaurants/create', adminController.createRestaurant);
+router.post('/restaurants', adminController.postRestaurant);
+
+//* 餐廳詳細
+router.get('/restaurants/:id', adminController.getRestaurant);
+//* 編輯餐廳資料
+router.get('/restaurants/:id/edit', adminController.editRestaurant);
+router.put('/restaurants/:id', adminController.putRestaurant);
+
+//* 刪除餐廳
+router.delete('/restaurants/:id', adminController.deleteRestaurant);
+
+//* 瀏覽全部餐廳
 router.get('/restaurants', adminController.getRestaurants);
-router.use('/', (req, res) => res.redirect('/admin/restaurants'));
+router.use('', (req, res) => res.redirect('/admin/restaurants'));
 module.exports = router;
