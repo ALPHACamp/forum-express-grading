3d3753b6b4419eaab07fa349e3c53973fed844d4
s1030905 <s1030905@outlook.com>
2023-05-22 14:04:19 +0800

    feat: category update

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -15,8 +15,10 @@ router.post('/restaurants', upload.single('image'), adminController.postRestaura
 router.get('/users', adminController.getUsers)
 router.patch('/users/:id', adminController.patchUser)
 
+router.get('/categories/:id', categoryController.getCategories)
+router.put('/categories/:id', categoryController.putCategory)
 router.get('/categories', categoryController.getCategories)
-router.post('/categories', categoryController.postCategories)
+router.post('/categories', categoryController.postCategory)
 
 router.use('', (req, res) => res.redirect('/admin/restaurants'))
 
