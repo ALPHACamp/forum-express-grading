1ba8c6687de3c3a09ec269e4592176a0140dda5b
vinchiengithub <jianfan0114@gmail.com>
2023-05-22 16:56:40 +0800

    feat: add create category on admin categories page

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -23,6 +23,7 @@ router.get('/users', adminController.getUsers)
 router.patch('/users/:id', adminController.patchUser)
 
 router.get('/categories', categoryController.getCategories)
+router.post('/categories', categoryController.postCategory)
 
 router.use('/', (req, res) => res.redirect('/admin/restaurants'))
 router.get('', (req, res) => res.redirect('/admin/restaurants'))
