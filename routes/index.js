8515a0e1742cc7d888f9cde068ee6ad10dff993f
sherryylin0719 <sherryylin0719@gmail.com>
2023-05-22 20:45:54 +0900

    feat: fix logout function

--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,7 @@ router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
 
 router.get('/restaurants', authenticated, restController.getRestaurants)
+router.get('/logout', userController.logout)
 
 router.get('/', (req, res) => res.redirect('/restaurants'))
 router.use('/', generalErrorHandler)
