ff373542132e9d114ca4198d498cc212b2df67b3
Leechiayu <gary86442@gmail.com>
2023-05-22 13:46:04 +0800

    feat:add back entrance

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 
 const router = express.Router();
+//* 載入子路由
+const admin = require('./modules/admin');
+
 //* 載入controller
 const restController = require('../controllers/restaurant-controller');
 
+router.use('/admin', admin);
 router.get('/restaurants', restController.getRestaurants);
 
 router.use('/', (req, res) => res.redirect('/restaurants'));
