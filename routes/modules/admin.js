09bebf2cfc67d1d21b45ecc410f6b9682492b8d0
Edward Kuan <edward880814@gmail.com>
2023-05-22 14:59:47 +0800

    feat: add admin categories page

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,7 +1,11 @@
 const express = require('express')
 const router = express.Router()
+
 const adminController = require('../../controllers/admin-controller')
+const categoryController = require('../../controllers/category-controller')
+
 const upload = require('../../middleware/multer')
+
 router.get('/restaurants/create', adminController.createRestaurant)
 router.get('/restaurants/:id/edit', adminController.editRestaurant)
 router.get('/restaurants/:id', adminController.getRestaurant)
@@ -9,10 +13,11 @@ router.put('/restaurants/:id', upload.single('image'), adminController.putRestau
 router.delete('/restaurants/:id', adminController.deleteRestaurant)
 router.get('/restaurants', adminController.getRestaurants)
 router.post('/restaurants', upload.single('image'), adminController.postRestaurant)
-// 處裡更新使用者權限的request
 router.patch('/users/:id', adminController.patchUser)
-// 處理獲取使用者名單的request
 router.get('/users', adminController.getUsers)
 
+router.get('/categories', categoryController.getCategories)
+
 router.get('/', (req, res) => res.redirect('/admin/restaurants'))
+
 module.exports = router
