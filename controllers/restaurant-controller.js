3355ec9413b03490082f8e209b92d875c2639fb6
jamiennn <jasper07165@hotmail.com>
2023-05-22 23:17:18 +0800

    feat: add dashboard page and viewCounts

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,6 +1,46 @@
+const { Restaurant, Category } = require('../models')
+
 const restaurantController = {
-  getRestaurants: (req, res) => {
-    return res.render('restaurants')
+  getRestaurants: (req, res, next) => {
+    return Restaurant.findAll({
+      raw: true,
+      nest: true,
+      include: [Category]
+    })
+      .then(restaurants => {
+        const data = restaurants.map(r => ({
+          ...r,
+          description: r.description.substring(0, 50)
+        }))
+        return res.render('restaurants', { restaurants: data })
+      })
+      .catch(err => next(err))
+  },
+  getRestaurant: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, {
+      include: [Category]
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error('Restaurant does not exist!')
+        return restaurant.increment('viewCounts')
+      })
+      .then(restaurant => {
+        restaurant = restaurant.toJSON()
+        res.render('restaurant', { restaurant })
+      })
+      .catch(err => next(err))
+  },
+  getDashboard: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, {
+      raw: true,
+      nest: true,
+      include: [Category]
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error('Restaurant does not exist!')
+        res.render('dashboard', { restaurant })
+      })
+      .catch(err => next(err))
   }
 }
 
