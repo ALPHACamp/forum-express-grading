d7364862c753523a5c58c2236c533752ca09f2d8
s1030905 <s1030905@outlook.com>
2023-05-22 22:55:24 +0800

    feat: show comments on restaurant page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,4 +1,4 @@
-const { Restaurant, Category } = require('../models')
+const { Restaurant, Category, Comment, User } = require('../models')
 const { getOffset, getPagination } = require('../helpers/pagination-helper')
 const restaurantController = {
   getRestaurants: (req, res) => {
@@ -41,13 +41,18 @@ const restaurantController = {
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
-      include: Category,
-      nest: true,
-      raw: true
+      include: [
+        Category,
+        { model: Comment, include: User }
+      ]
+      // nest: true,
+      // raw: true
     })
       .then(restaurant => {
         if (!restaurant) throw new Error("Restaurant didn't exist")
-        res.render('restaurant', { restaurant })
+        // console.log('--------------here---------------' + restaurant.Comments.text)
+        // res.render('restaurant', { restaurant })
+        res.render('restaurant', { restaurant: restaurant.toJSON() })
       })
       .catch(err => next(err))
   }
