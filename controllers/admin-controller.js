0c339c3eac7425d8e3ef52335a849635714f6a98
Leechiayu <gary86442@gmail.com>
2023-05-22 18:02:28 +0800

    feat: modify config for heroku

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,5 +1,7 @@
 const { Restaurant } = require('../models');
+const { localFileHandler } = require('../helpers/file-helpers');
 const adminController = {
+  //* 瀏覽全部餐廳
   getRestaurants: (req, res, next) => {
     Restaurant.findAll({
       raw: true
@@ -9,26 +11,33 @@ const adminController = {
 
       .catch(err => next(err));
   },
+  //*建立新餐廳
   createRestaurant: (req, res) => {
     return res.render('admin/create-restaurant');
   },
   postRestaurant: (req, res, next) => {
     const { name, tel, address, openingHours, description } = req.body; // 從 req.body 拿出表單裡的資料
     if (!name) throw new Error('Restaurant name is required!'); // name 是必填，若發先是空值就會終止程式碼，並在畫面顯示錯誤提示
-    Restaurant.create({
-      //產生一個新的 Restaurant 物件實例，並存入資料庫
-      name,
-      tel,
-      address,
-      openingHours,
-      description
-    })
+    const { file } = req;
+    localFileHandler(file)
+      .then(filePath => {
+        Restaurant.create({
+          //產生一個新的 Restaurant 物件實例，並存入資料庫
+          name,
+          tel,
+          address,
+          openingHours,
+          description,
+          image: filePath || null
+        });
+      })
       .then(() => {
         req.flash('success_messages', 'restaurant was successfully created'); // 在畫面顯示成功提示
         res.redirect('/admin/restaurants'); //新增完成後導回後台首頁
       })
       .catch(err => next(err));
   },
+  //* 餐廳詳細
   getRestaurant: (req, res, next) => {
     Restaurant.findByPk(req.params.id, {
       //去資料庫用 id 找一筆資料
@@ -40,6 +49,7 @@ const adminController = {
       })
       .catch(err => next(err));
   },
+  //* 編輯餐廳資料
   editRestaurant: (req, res, next) => {
     Restaurant.findByPk(req.params.id, {
       raw: true
@@ -52,16 +62,24 @@ const adminController = {
   },
   putRestaurant: (req, res, next) => {
     const { name, tel, address, openingHours, description } = req.body;
+    const { file } = req; // 把檔案取出來
     if (!name) throw new Error('Restaurant name is required!');
-    Restaurant.findByPk(req.params.id)
-      .then(restaurant => {
+    Promise.all([
+      // 非同步處理
+      Restaurant.findByPk(req.params.id), // 去資料庫查有沒有這間餐廳
+      localFileHandler(file) // 把檔案傳到 file-helper 處理
+    ])
+      .then(([restaurant, filePath]) => {
+        // 以上兩樣事都做完以後
         if (!restaurant) throw new Error("Restaurant didn't exist!");
         return restaurant.update({
+          // 修改這筆資料
           name,
           tel,
           address,
           openingHours,
-          description
+          description,
+          image: filePath || restaurant.image // 如果 filePath 是 Truthy (使用者有上傳新照片) 就用 filePath，是 Falsy (使用者沒有上傳新照片) 就沿用原本資料庫內的值
         });
       })
       .then(() => {
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const adminController = require('../../controllers/admin-controller');
+const upload = require('../../middleware/multer');
 //* 新增餐廳
 router.get('/restaurants/create', adminController.createRestaurant);
-router.post('/restaurants', adminController.postRestaurant);
+router.post(
+  '/restaurants',
+  upload.single('image'),
+  adminController.postRestaurant
+);
 
 //* 餐廳詳細
 router.get('/restaurants/:id', adminController.getRestaurant);
 //* 編輯餐廳資料
 router.get('/restaurants/:id/edit', adminController.editRestaurant);
-router.put('/restaurants/:id', adminController.putRestaurant);
+router.put(
+  '/restaurants/:id',
+  upload.single('image'),
+  adminController.putRestaurant
+);
 
 //* 刪除餐廳
 router.delete('/restaurants/:id', adminController.deleteRestaurant);
