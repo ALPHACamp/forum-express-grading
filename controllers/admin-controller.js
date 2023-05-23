ff092c2814f9e89b68dff7f4a5b9cd1027bc3521
Leechiayu <gary86442@gmail.com>
2023-05-23 16:09:20 +0800

    feat: add category on views.

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,24 +1,26 @@
-const { Restaurant, User } = require('../models');
-const { imgurFileHandler } = require('../helpers/file-helpers');
+const { Restaurant, User, Category } = require('../models')
+const { imgurFileHandler } = require('../helpers/file-helpers')
 const adminController = {
   //* 瀏覽全部餐廳
   getRestaurants: (req, res, next) => {
     Restaurant.findAll({
-      raw: true
+      raw: true,
+      nest: true,
+      include: [Category]
     })
 
       .then(restaurants => res.render('admin/restaurants', { restaurants }))
 
-      .catch(err => next(err));
+      .catch(err => next(err))
   },
   //*建立新餐廳
   createRestaurant: (req, res) => {
-    return res.render('admin/create-restaurant');
+    return res.render('admin/create-restaurant')
   },
   postRestaurant: (req, res, next) => {
-    const { name, tel, address, openingHours, description } = req.body; // 從 req.body 拿出表單裡的資料
-    if (!name) throw new Error('Restaurant name is required!'); // name 是必填，若發先是空值就會終止程式碼，並在畫面顯示錯誤提示
-    const { file } = req;
+    const { name, tel, address, openingHours, description } = req.body // 從 req.body 拿出表單裡的資料
+    if (!name) throw new Error('Restaurant name is required!') // name 是必填，若發先是空值就會終止程式碼，並在畫面顯示錯誤提示
+    const { file } = req
     imgurFileHandler(file)
       .then(filePath => {
         Restaurant.create({
@@ -29,25 +31,27 @@ const adminController = {
           openingHours,
           description,
           image: filePath || null
-        });
+        })
       })
       .then(() => {
-        req.flash('success_messages', 'restaurant was successfully created'); // 在畫面顯示成功提示
-        res.redirect('/admin/restaurants'); //新增完成後導回後台首頁
+        req.flash('success_messages', 'restaurant was successfully created') // 在畫面顯示成功提示
+        res.redirect('/admin/restaurants') //新增完成後導回後台首頁
       })
-      .catch(err => next(err));
+      .catch(err => next(err))
   },
   //* 餐廳詳細
   getRestaurant: (req, res, next) => {
     Restaurant.findByPk(req.params.id, {
       //去資料庫用 id 找一筆資料
-      raw: true // 找到以後整理格式再回傳
+      raw: true, // 找到以後整理格式再回傳
+      nest: true,
+      include: [Category]
     })
       .then(restaurant => {
-        if (!restaurant) throw new Error("Restaurant didn't exist!"); //  如果找不到，回傳錯誤訊息，後面不執行
-        res.render('admin/restaurant', { restaurant });
+        if (!restaurant) throw new Error("Restaurant didn't exist!") //  如果找不到，回傳錯誤訊息，後面不執行
+        res.render('admin/restaurant', { restaurant })
       })
-      .catch(err => next(err));
+      .catch(err => next(err))
   },
   //* 編輯餐廳資料
   editRestaurant: (req, res, next) => {
@@ -55,15 +59,15 @@ const adminController = {
       raw: true
     })
       .then(restaurant => {
-        if (!restaurant) throw new Error("Restaurant didn't exist!");
-        res.render('admin/edit-restaurant', { restaurant });
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        res.render('admin/edit-restaurant', { restaurant })
       })
-      .catch(err => next(err));
+      .catch(err => next(err))
   },
   putRestaurant: (req, res, next) => {
-    const { name, tel, address, openingHours, description } = req.body;
-    const { file } = req; // 把檔案取出來
-    if (!name) throw new Error('Restaurant name is required!');
+    const { name, tel, address, openingHours, description } = req.body
+    const { file } = req // 把檔案取出來
+    if (!name) throw new Error('Restaurant name is required!')
     Promise.all([
       // 非同步處理
       Restaurant.findByPk(req.params.id), // 去資料庫查有沒有這間餐廳
@@ -71,7 +75,7 @@ const adminController = {
     ])
       .then(([restaurant, filePath]) => {
         // 以上兩樣事都做完以後
-        if (!restaurant) throw new Error("Restaurant didn't exist!");
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
         return restaurant.update({
           // 修改這筆資料
           name,
@@ -80,22 +84,22 @@ const adminController = {
           openingHours,
           description,
           image: filePath || restaurant.image // 如果 filePath 是 Truthy (使用者有上傳新照片) 就用 filePath，是 Falsy (使用者沒有上傳新照片) 就沿用原本資料庫內的值
-        });
+        })
       })
       .then(() => {
-        req.flash('success_messages', 'restaurant was successfully to update');
-        res.redirect('/admin/restaurants');
+        req.flash('success_messages', 'restaurant was successfully to update')
+        res.redirect('/admin/restaurants')
       })
-      .catch(err => next(err));
+      .catch(err => next(err))
   },
   deleteRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id)
       .then(restaurant => {
-        if (!restaurant) throw new Error("Restaurant didn't exist!");
-        return restaurant.destroy();
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        return restaurant.destroy()
       })
       .then(() => res.redirect('/admin/restaurants'))
-      .catch(err => next(err));
+      .catch(err => next(err))
   }, //* 瀏覽所有帳號
   getUsers: (req, res, next) => {
     return User.findAll({
@@ -104,15 +108,15 @@ const adminController = {
       .then(users => {
         for (user of users) {
           if (user.isAdmin) {
-            user.role = 'admin';
+            user.role = 'admin'
           } else {
-            user.role = 'user';
+            user.role = 'user'
           }
         }
-        res.render('admin/users', { users });
+        res.render('admin/users', { users })
       })
 
-      .catch(err => next(err));
+      .catch(err => next(err))
   },
 
   //* 更改權限
@@ -120,18 +124,18 @@ const adminController = {
   patchUser: (req, res, next) => {
     return User.findByPk(req.params.id)
       .then(user => {
-        if (!user) throw new Error("User didn't exist!");
+        if (!user) throw new Error("User didn't exist!")
         if (user.email === 'root@example.com') {
-          req.flash('error_messages', '禁止變更 root 權限');
-          return res.redirect('back');
+          req.flash('error_messages', '禁止變更 root 權限')
+          return res.redirect('back')
         }
-        return user.update({ isAdmin: !user.isAdmin });
+        return user.update({ isAdmin: !user.isAdmin })
       })
       .then(() => {
-        req.flash('success_messages', '使用者權限變更成功');
-        res.redirect('/admin/users');
+        req.flash('success_messages', '使用者權限變更成功')
+        res.redirect('/admin/users')
       })
-      .catch(err => next(err));
+      .catch(err => next(err))
   }
-};
-module.exports = adminController;
+}
+module.exports = adminController
