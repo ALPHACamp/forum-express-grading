9f1a16f66637f9446c0e8639fe456feab2233136
CHUCHUDAN <daniel870217@gmail.com>
2023-05-23 13:34:42 +0800

    feat: create favorite model

--- models/restaurant.js
+++ models/restaurant.js
@@ -13,6 +13,11 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
       Restaurant.belongsTo(models.Category, { foreignKey: 'categoryId' })
       Restaurant.hasMany(models.Comment, { foreignKey: 'restaurantId' })
+      Restaurant.belongsToMany(models.User, {
+        through: models.Favorite,
+        foreignKey: 'restaurantId',
+        as: 'FavoritedUsers'
+      })
     }
   };
   Restaurant.init({
