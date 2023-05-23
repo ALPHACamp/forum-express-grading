6580f53d753566773e3fe18e0fdf842964cc2088
Ray7808 <yuwang7808@gmail.com>
2023-05-23 19:56:02 +0800

    feat: add users.hbs to provide user to revise the user state(admin or user)

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,4 +1,4 @@
-const { Restaurant } = require('../models')
+const { Restaurant, User } = require('../models')
 const { imgurFileHandler } = require('../helpers/file-helpers') // 將 file-helper 載進來
 
 const adminController = {
@@ -84,6 +84,38 @@ const adminController = {
       })
       .then(() => res.redirect('/admin/restaurants'))
       .catch(err => next(err))
+  },
+  getUsers: (req, res, next) => {
+    return User.findAll({
+      raw: true
+    })
+      .then(users => res.render('admin/users', { users }))
+      .catch(err => next(err))
+  },
+  patchUser: (req, res, next) => {
+    const userId = req.params.id
+    return User.findByPk(userId)
+      .then(user => {
+        if (!user) throw new Error("User didn't exist!")
+        if (user.isAdmin === true && user.email === 'root@example.com') {
+          req.flash('error_messages', '禁止變更 root 權限')
+          res.redirect('back')
+        } else if (user.isAdmin !== true) {
+          return user.update({
+            isAdmin: true
+          })
+        } else {
+          return user.update({
+            isAdmin: false
+          })
+        }
+      })
+      .then(() => {
+        req.flash('success_messages', '使用者權限變更成功')
+        res.redirect('/admin/users')
+      })
+      .catch(err => next(err))
   }
+
 }
 module.exports = adminController
