3fd3ea4c26345af52ac075df4a1951db65e39481
qpooqpooqp <kkk3kkk3kkk3@yahoo.com.tw>
2023-05-23 08:23:04 +0800

    feat: category delete

--- migrations/20230517083828-add-category-id-to-restaurant.js
+++ migrations/20230517083828-add-category-id-to-restaurant.js
@@ -1,17 +1,15 @@
 'use strict'
-
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.addColumn('Restaurants', 'category_id', {
       type: Sequelize.INTEGER,
       allowNull: false,
-      reference: {
+      references: {
         model: 'Categories',
         key: 'id'
       }
     })
   },
-
   down: async (queryInterface, Sequelize) => {
     await queryInterface.removeColumn('Restaurants', 'category_id')
   }
--- models/restaurant.js
+++ models/restaurant.js
@@ -24,7 +24,7 @@ module.exports = (sequelize, DataTypes) => {
   {
     sequelize,
     modelName: 'Restaurant',
-    tableName: 'RestaurantS',
+    tableName: 'Restaurants',
     underscored: true
   })
   return Restaurant
