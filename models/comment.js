1610cfeb63929f17d9c8c88148aad63bc7c1e2a5
vinchiengithub <jianfan0114@gmail.com>
2023-05-23 14:47:51 +0800

    feat: add comments model

---
+++ models/comment.js
@@ -0,0 +1,32 @@
+'use strict'
+const {
+  Model
+} = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+  class Comment extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate (models) {
+      // define association here
+      Comment.belongsTo(models.Restaurant, { foreignKey: 'restaurantId' })
+      Comment.belongsTo(models.User, { foreignKey: 'userId' })
+    }
+  }
+  Comment.init(
+    {
+      text: DataTypes.STRING,
+      // 改成小駝峰式的命名
+      userId: DataTypes.INTEGER,
+      restaurantId: DataTypes.INTEGER
+    },
+    {
+      sequelize,
+      modelName: 'Comment',
+      underscored: true
+    }
+  )
+  return Comment
+}
