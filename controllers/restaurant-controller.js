28b55999b47df59659ad8ac32b854d036ea8e0a8
playcsgo <sam@plum.wine>
2023-05-24 05:44:30 +0800

    feat: add restaurant index page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,6 +1,20 @@
+const { Restaurant, Category } = require('../models')
+
 const restaurantColler = {
   getRestaurants: (req, res) => {
-    return res.render('restaurant')
+    return Restaurant.findAll({
+      include: Category,
+      nest: true,
+      raw: true
+    })
+      .then(restaurant => {
+        const data = restaurant.map(r => ({
+          ...r,
+          description: r.description.substring(0, 50)
+          // 意思是 找出來的restaurant裡面有什麼 data就有什麼, 而且description是處理過的
+        }))
+        return res.render('restaurants', { restaurants: data })
+      })
   }
 }
 
