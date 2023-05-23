2a827dfba7cb02f22db847047ae4b2946a69d410
elviawu <elviawu@gmail.com>
2023-05-23 17:34:50 +0800

    feat: add dashboard

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -16,14 +16,29 @@ const restaurantController = {
     })
   },
   getRestaurant: (req, res, next) => {
+    const id = req.params.id
     return Restaurant.findByPk(req.params.id, {
-      include: Category, // 拿出關聯的 Category model
-      nest: true,
-      raw: true
+      include: Category
     })
       .then(restaurant => {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
-        res.render('restaurant', { restaurant })
+        Restaurant.increment('viewCounts', { where: { id } })
+        return restaurant
+      })
+      .then(restaurant => {
+        res.render('restaurant', { restaurant: restaurant.toJSON() })
+      })
+      .catch(err => next(err))
+  },
+  getDashboard: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, {
+      raw: true,
+      nest: true,
+      include: Category
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        res.render('dashboard', { restaurant })
       })
       .catch(err => next(err))
   }
