7e4dfd737d40c499fdf3f5911293a99b541ac639
azx79115 <azx79115@gmail.com>
2023-05-23 12:29:56 +0800

    add restaurant page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -6,9 +6,17 @@ const restaurantController = {
         const data = restaurants.map(r => ({
           ...r, description: r.description.substring(0, 50)
         }))
-        console.log(restaurants)
+        // console.log(restaurants)
         return res.render('restaurants', { restaurants: data })
       })
+  },
+  getRestaurant: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, { include: Category, nest: true, raw: true })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        res.render('restaurant', { restaurant })
+      })
+      .catch(err => next(err))
   }
 }
 
