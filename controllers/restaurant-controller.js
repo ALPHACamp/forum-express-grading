e8cfabeb6e5a41e5c9b56b13bd6c12bc09f22d43
azx79115 <azx79115@gmail.com>
2023-05-23 17:31:11 +0800

    R02完成

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,22 +1,39 @@
 const { Restaurant, Category } = require('../models')
+
 const restaurantController = {
-  getRestaurants: (req, res) => {
-    return Restaurant.findAll({ include: Category, nest: true, raw: true })
-      .then(restaurants => {
-        const data = restaurants.map(r => ({
-          ...r, description: r.description.substring(0, 50)
-        }))
-        // console.log(restaurants)
-        return res.render('restaurants', { restaurants: data })
-      })
+  getRestaurants: async (req, res) => {
+    try {
+      const restaurants = await Restaurant.findAll({ include: Category, nest: true, raw: true })
+      const data = restaurants.map(r => ({
+        ...r, description: r.description.substring(0, 50)
+      }))
+      res.render('restaurants', { restaurants: data })
+    } catch (err) {
+      console.log(err)
+    }
   },
-  getRestaurant: (req, res, next) => {
-    return Restaurant.findByPk(req.params.id, { include: Category, nest: true, raw: true })
-      .then(restaurant => {
-        if (!restaurant) throw new Error("Restaurant didn't exist!")
-        res.render('restaurant', { restaurant })
-      })
-      .catch(err => next(err))
+
+  getRestaurant: async (req, res, next) => {
+    try {
+      const restaurant = await Restaurant.findByPk(req.params.id, { include: Category, nest: true })
+      if (!restaurant) throw new Error("Restaurant didn't exist!")
+      await restaurant.increment('view_counts')
+      console.log(restaurant.view_counts)
+      res.render('restaurant', { restaurant: restaurant.toJSON() })
+    } catch (err) {
+      next(err)
+    }
+  },
+
+  getDashboard: async (req, res, next) => {
+    try {
+      console.log(req.params)
+      const restaurant = await Restaurant.findByPk(req.params.id, { include: Category, nest: true, raw: true })
+      if (!restaurant) throw new Error("Restaurant didn't exist!")
+      res.render('dashboard', { restaurant })
+    } catch (err) {
+      next(err)
+    }
   }
 }
 
