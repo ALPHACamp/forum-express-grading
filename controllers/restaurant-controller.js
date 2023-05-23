0e2013d23b89d23f93fc608266c7f339c3d8e3ea
kim1037 <pan913063@gmail.com>
2023-05-23 11:21:30 +0800

    feat: add filter on getTopRestaurants

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -109,6 +109,7 @@ const restaruantController = {
       .catch(err => next(err))
   },
   getTopRestaurants: (req, res, next) => {
+    const maxLength = 150
     // 目前註解掉的部分會使test fail
     return Restaurant.findAll({
       include: [
@@ -117,12 +118,17 @@ const restaruantController = {
       ]
     })
       .then(restaurants => {
-        restaurants = restaurants.map(rest => ({
-          ...rest.toJSON(),
-          favoritedCount: rest.FavoritedUsers.length,
-          isFavorited: req.user && req.user.FavoritedRestaurants.some(f => f.id === rest.id)
-          // ,isLiked: req.user && req.user.LikedRestaurants.some(l => l.id === rest.id)
-        }))
+        restaurants = restaurants
+          .map(rest => ({
+            ...rest.toJSON(),
+            description: rest.description.substring(0, maxLength) + '...',
+            favoritedCount: rest.FavoritedUsers.length,
+            isFavorited:
+              req.user &&
+              req.user.FavoritedRestaurants.some(f => f.id === rest.id)
+            // ,isLiked: req.user && req.user.LikedRestaurants.some(l => l.id === rest.id)
+          }))
+          .filter(r => r.favoritedCount > 0) // 只顯示收藏數>0的餐廳
           .sort((a, b) => b.favoritedCount - a.favoritedCount)
           .slice(0, 10)
         res.render('top-restaurants', { restaurants })
