4b95d2759c99d50c6139a8ebfe999072ae45d9ef
jamiennn <jasper07165@hotmail.com>
2023-05-23 15:28:45 +0800

    feat: add category-filter and pagination

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,18 +1,39 @@
 const { Restaurant, Category } = require('../models')
+const { getOffset, getPagination } = require('../helpers/pagination-helpers')
 
 const restaurantController = {
   getRestaurants: (req, res, next) => {
-    return Restaurant.findAll({
-      raw: true,
-      nest: true,
-      include: [Category]
-    })
-      .then(restaurants => {
-        const data = restaurants.map(r => ({
+    const DEFAULT_LIMIT = 9
+
+    const categoryId = Number(req.query.categoryId) || ''
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || DEFAULT_LIMIT
+    const offset = getOffset(limit, page)
+
+    return Promise.all([
+      Restaurant.findAndCountAll({
+        include: [Category],
+        where: {
+          ...categoryId ? { categoryId } : {}
+        },
+        offset,
+        limit,
+        raw: true,
+        nest: true
+      }),
+      Category.findAll({ raw: true })
+    ])
+      .then(([restaurants, categories]) => {
+        const data = restaurants.rows.map(r => ({
           ...r,
           description: r.description.substring(0, 50)
         }))
-        return res.render('restaurants', { restaurants: data })
+        return res.render('restaurants', {
+          restaurants: data,
+          categories,
+          categoryId,
+          pagination: getPagination(limit, page, restaurants.count)
+        })
       })
       .catch(err => next(err))
   },
