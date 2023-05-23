4b91e4cd8aa664293663a6bdf05aad439e32ffe9
jamiennn <jasper07165@hotmail.com>
2023-05-24 02:47:08 +0800

    feat: modify parameter 'loginUser' for handlebars

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(methodOverride('_method'))
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
   res.locals.error_messages = req.flash('error_messages')
-  res.locals.user = getUser(req)
+  res.locals.loginUser = getUser(req)
   next()
 })
 
