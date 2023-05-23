81290dfacac32e16417cf6e23981ea56e15b194c
vinchiengithub <jianfan0114@gmail.com>
2023-05-23 15:01:36 +0800

    feat: add browse reviews function

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,4 +1,4 @@
-const { Restaurant, Category } = require('../models')
+const { Restaurant, Category, Comment, User } = require('../models')
 const { getOffset, getPagination } = require('../helpers/pagination-helpers')
 const restaurantController = {
   getRestaurants: (req, res, next) => {
@@ -37,8 +37,7 @@ const restaurantController = {
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
-      include: Category // 拿出關聯的 Category model
-      // raw 要拿掉，不然無法取得 dashboard 需要的資料
+      include: [Category, { model: Comment, include: User }]
     })
       .then(restaurant => {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
