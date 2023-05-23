d801a3d04565e4c9ac21a10fde6233889da04285
JimmyLin <disillusioned2077@gmail.com>
2023-05-23 17:32:22 +0800

    feat: create users and restaurants seeder

---
+++ seeders/20230523085328-restaurants-seed-file.js
@@ -0,0 +1,21 @@
+'use strict'
+const faker = require('faker')
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('Restaurants',
+      Array.from({ length: 50 }, () => ({ // 產出長度50的陣列
+        name: faker.name.findName(), // 使用faker產出name資料
+        tel: faker.phone.phoneNumber(),
+        address: faker.address.streetAddress(),
+        opening_hours: '08:00',
+        image: `https://loremflickr.com/320/240/restaurant,food/?random=${Math.random() * 100}`, // 圖片產出網站，可修改restaurant,food產出其他圖片，並隨機從1-100張編號中選取
+        description: faker.lorem.text(500), // 用faker產出類文章
+        created_at: new Date(),
+        updated_at: new Date()
+      }))
+    )
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Restaurants', {})
+  }
+}
