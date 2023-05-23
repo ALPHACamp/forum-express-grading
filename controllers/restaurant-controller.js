94fe63a46a8546606a908a15f28c3dadbf5d9b7a
jamiennn <jasper07165@hotmail.com>
2023-05-24 00:26:00 +0800

    feat: add feeds

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -67,6 +67,28 @@ const restaurantController = {
         res.render('dashboard', { restaurant: restaurant.toJSON() })
       })
       .catch(err => next(err))
+  },
+  getFeeds: (req, res, next) => {
+    return Promise.all([
+      Restaurant.findAll({
+        limit: 10,
+        order: [['createdAt', 'DESC']],
+        include: [Category],
+        raw: true,
+        nest: true
+      }),
+      Comment.findAll({
+        limit: 10,
+        order: [['createdAt', 'DESC']],
+        include: [Restaurant, User],
+        raw: true,
+        nest: true
+      })
+    ])
+      .then(([restaurants, comments]) => {
+        res.render('feeds', { restaurants, comments })
+      })
+      .catch(err => next(err))
   }
 }
 
