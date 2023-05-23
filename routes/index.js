5df9973762a800caa93a49dd49b92aebb849f84c
vinchiengithub <jianfan0114@gmail.com>
2023-05-23 15:07:09 +0800

    feat: add admin delete comment function

--- routes/index.js
+++ routes/index.js
@@ -26,6 +26,11 @@ router.get(
   authenticated,
   restController.getDashboard
 )
+router.delete(
+  '/comments/:id',
+  authenticatedAdmin,
+  commentController.deleteComment
+)
 router.post('/comments', authenticated, commentController.postComment)
 router.get('/restaurants/:id', authenticated, restController.getRestaurant)
 router.get('/restaurants', authenticated, restController.getRestaurants)
