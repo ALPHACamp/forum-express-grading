863e2ae80f3220b8d5562876fc5166f5f5d1e830
CharisLai <griffin8418@gmail.com>
2023-05-23 20:32:41 +0800

    R02 test requestment

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -13,11 +13,22 @@ const restaurantController = {
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
-      include: Category, nest: true, raw: true
+      include: Category, nest: true
     })
       .then(restaurant => {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
-        res.render('restaurant', { restaurant })
+        return restaurant.increment('viewCounts', { by: 1 })
+      })
+      .then(restaurant => {
+        return res.render('restaurant', { restaurant: restaurant.toJSON() })
+      })
+      .catch(err => next(err))
+  },
+  getDashboard: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, { include: Category, nest: true, raw: true })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        res.render('dashboard', { restaurant })
       })
       .catch(err => next(err))
   }
