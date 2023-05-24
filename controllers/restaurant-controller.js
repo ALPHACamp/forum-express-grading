4cc58a15aaabd7ee39590ddae1e0779d99edf37a
playcsgo <sam@plum.wine>
2023-05-25 07:10:32 +0800

    feat: add category navbar on restaurats page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,20 +1,32 @@
 const { Restaurant, Category } = require('../models')
 
 const restaurantColler = {
-  getRestaurants: (req, res) => {
-    return Restaurant.findAll({
-      include: Category,
-      nest: true,
-      raw: true
-    })
-      .then(restaurant => {
-        const data = restaurant.map(r => ({
+  getRestaurants: (req, res, next) => {
+    const categoryId = Number(req.query.categoryId) || ''
+    console.log(categoryId)
+    return Promise.all([
+      Restaurant.findAll({
+        include: Category,
+        where: {
+          ...categoryId ? { categoryId } : {}
+        },
+        raw: true,
+        nest: true
+      }),
+      Category.findAll({ raw: true }) // 為了render category-Nav
+    ])
+      .then(([restaurants, categories]) => {
+        const data = restaurants.map(r => ({
           ...r,
           description: r.description.substring(0, 50)
-          // 意思是 找出來的restaurant裡面有什麼 data就有什麼, 而且description是處理過的
         }))
-        return res.render('restaurants', { restaurants: data })
+        return res.render('restaurants', {
+          restaurants: data,
+          categories,
+          categoryId
+        })
       })
+      .catch(err => next(err))
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
