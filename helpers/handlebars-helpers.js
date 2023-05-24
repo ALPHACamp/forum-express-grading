c9e30759a43935e896d14e0dfec86b2678e48976
elviawu <elviawu@gmail.com>
2023-05-25 03:04:50 +0800

    feat: show comments on restaurant page

--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -1,6 +1,9 @@
 const dayjs = require('dayjs')
+const relativeTime = require('dayjs/plugin/relativeTime') // 增加這裡
+dayjs.extend(relativeTime)
 module.exports = {
   currentYear: () => dayjs().year(), // 取得當年年份作為 currentYear 的屬性值，並導出
+  relativeTimeFromNow: a => dayjs(a).fromNow(),
   ifCond: function (a, b, options) {
     return a === b ? options.fn(this) : options.inverse(this)
   }
