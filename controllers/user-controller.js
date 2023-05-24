6ca9498b2561ddd340052caecb18da67f2933c67
LongXiangL <bfh1104@gmail.com>
2023-05-24 18:41:00 +0800

    finsh R03

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,30 +1,28 @@
 const bcrypt = require('bcryptjs')
-const db = require('../models')
-const { User } = db
+const { User, Restaurant, Comment } = require('../models')
+const { imgurFileHandler } = require('../helpers/file-helpers')
+
 const userController = {
   signUpPage: (req, res) => {
     res.render('signup')
   },
-  signUp: (req, res, next) => { // 修改這裡
-    // 如果兩次輸入的密碼不同，就建立一個 Error 物件並拋出
+  signUp: (req, res, next) => {
     if (req.body.password !== req.body.passwordCheck) throw new Error('Passwords do not match!')
-
-    // 確認資料裡面沒有一樣的 email，若有，就建立一個 Error 物件並拋出
     User.findOne({ where: { email: req.body.email } })
       .then(user => {
         if (user) throw new Error('Email already exists!')
-        return bcrypt.hash(req.body.password, 10) // 前面加 return
+        return bcrypt.hash(req.body.password, 10)
       })
-      .then(hash => User.create({ // 上面錯誤狀況都沒發生，就把使用者的資料寫入資料庫
+      .then(hash => User.create({
         name: req.body.name,
         email: req.body.email,
         password: hash
       }))
       .then(() => {
-        req.flash('success_messages', '成功註冊帳號！') // 並顯示成功訊息
+        req.flash('success_messages', '成功註冊帳號！')
         res.redirect('/signin')
       })
-      .catch(err => next(err)) // 接住前面拋出的錯誤，呼叫專門做錯誤處理的 middleware
+      .catch(err => next(err))
   },
   signInPage: (req, res) => {
     res.render('signin')
@@ -37,6 +35,61 @@ const userController = {
     req.flash('success_messages', '登出成功！')
     req.logout()
     res.redirect('/signin')
+  },
+  getUser: (req, res, next) => {
+    return User.findByPk(req.params.id, {
+      include: [
+        { model: Comment, include: Restaurant }
+      ]
+    })
+      .then(user => {
+        if (!user) throw new Error("User didn't exist!")
+
+        user = user.toJSON()
+        user.commentedRestaurants = user.Comments && user.Comments.reduce((accumulator, c) => {
+          if (!accumulator.some(r => r.id === c.restaurantId)) {
+            accumulator.push(c.Restaurant)
+          }
+          return accumulator
+        }, [])
+        res.render('users/profile', {
+          user
+        })
+      })
+      .catch(err => next(err))
+  },
+  editUser: (req, res, next) => {
+    return User.findByPk(req.params.id)
+      .then(user => {
+        if (!user) throw new Error("User didn't exist!")
+
+        res.render('users/edit', { user: user.toJSON() })
+      })
+      .catch(err => next(err))
+  },
+  putUser: (req, res, next) => {
+    if (Number(req.params.id) !== Number(req.user.id)) {
+      res.redirect(`/users/${req.params.id}`)
+    }
+    const { file } = req
+
+    return Promise.all([
+      User.findByPk(req.params.id),
+      imgurFileHandler(file)
+    ])
+      .then(([user, filePath]) => {
+        if (!user) throw new Error("User didn't exist!")
+
+        return user.update({
+          name: req.body.name,
+          image: filePath || user.image
+        })
+      })
+      .then(() => {
+        req.flash('success_messages', '使用者資料編輯成功')
+        res.redirect(`/users/${req.params.id}`)
+      })
+      .catch(err => next(err))
   }
 }
 module.exports = userController
