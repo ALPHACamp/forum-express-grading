fc21a64d20ca1a4cc1e525a825708f4a583785db
thpss91103 <a0975142932@gmail.com>
2023-05-24 19:08:11 +0800

    feat: add user profile page

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -34,6 +34,14 @@ const userController = {
     req.flash('success_messages', '登出成功！')
     req.logout()
     res.redirect('/signin')
+  },
+  getUser: (req, res, next) => {
+    return User.findByPk(req.params.id, { raw: true })
+      .then(user => {
+        if (!user) throw new Error("User didn't exist!")
+        return res.render('users/profile', { user })
+      })
+      .catch(err => next(err))
   }
 }
 module.exports = userController
