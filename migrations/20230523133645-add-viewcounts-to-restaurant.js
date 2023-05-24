0add495e9326161cd1256b06c408f960a82fb8cb
Chingsan0722 <rory85722@gmail.com>
2023-05-24 13:29:08 +0800

    update: dashbard page typo

--- migrations/20230523133645-add-viewcounts-to-restaurant.js
+++ migrations/20230523133645-add-viewcounts-to-restaurant.js
@@ -4,9 +4,7 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.addColumn('Restaurants', 'view_counts', {
       type: Sequelize.INTEGER,
-      allowNull: false,
-      undesigned: true,
-      zerofill: true
+      allowNull: false
     })
   },
 
