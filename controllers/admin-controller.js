a27e2e7d2cb8c2ac4c0c2729b53a268652b0781c
arubakingpikachu <85471527+arubakingpikachu@users.noreply.github.com>
2023-05-25 01:45:37 +0800

    feat: show category on admin restaurant pages

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,10 +1,10 @@
-const { Restaurant, User } = require('../models') // 新增這裡
+const { Restaurant, User, Category } = require('../models') // 新增這裡
 const { imgurFileHandler } = require('../helpers/file-helpers')
 
 const adminController = { // 修改這裡
 
   getRestaurants: (req, res, next) => {
-    Restaurant.findAll({ raw: true })
+    Restaurant.findAll({ raw: true, nest: true, include: [Category] })
       .then(restaurants => res.render('admin/restaurants', { restaurants }))
       .catch(err => next(err))
   },
@@ -33,7 +33,9 @@ const adminController = { // 修改這裡
   },
   getRestaurant: (req, res, next) => {
     Restaurant.findByPk(req.params.id, { // 去資料庫用 id 找一筆資料
-      raw: true // 找到以後整理格式再回傳
+      raw: true, // 找到以後整理格式再回傳
+      nest: true,
+      include: [Category]
     })
       .then(restaurant => {
         if (!restaurant) throw new Error("Restaurant didn't exist!") //  如果找不到，回傳錯誤訊息，後面不執行
