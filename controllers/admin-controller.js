7faee41a05f49fe6e3fd41ef7097cb6e12f57eab
KenYuChang <justin0915justin0915@gmail.com>
2023-05-24 16:53:08 +0800

    add pagimation

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,8 +1,6 @@
 const { Restaurant, User, Category } = require("../models");
 const { imgurFileHandler } = require("../helpers/file-helpers");
 const adminController = {
-  // 修改這裡
-
   getRestaurants: (req, res, next) => {
     Restaurant.findAll({
       raw: true,
--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,20 +1,40 @@
 const { Restaurant, Category } = require("../models");
-
+const { getOffset, getPagination } = require("../helpers/pagination-helper");
 const restaurantController = {
-  getRestaurants: (req, res) => {
-    return Restaurant.findAll({
-      include: Category,
-      nest: true,
-      raw: true,
-    }).then((restaurants) => {
-      const data = restaurants.map((r) => ({
-        ...r,
-        description: r.description.substring(0, 50),
-      }));
-      return res.render("restaurants", {
-        restaurants: data,
-      });
-    });
+  getRestaurants: (req, res, next) => {
+    const DEFAULT_LIMIT = 9;
+    const categoryId = Number(req.query.categoryId) || ""; // 從網址上拿下來的參數是字串，先轉成 Number 再操作
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || DEFAULT_LIMIT;
+    const offset = getOffset(limit, page);
+
+    return Promise.all([
+      Restaurant.findAndCountAll({
+        include: Category,
+        where: {
+          // 新增查詢條件
+          ...(categoryId ? { categoryId } : {}),
+        },
+        limit,
+        offset,
+        nest: true,
+        raw: true,
+      }),
+      Category.findAll({ raw: true }),
+    ])
+      .then(([restaurants, categories]) => {
+        const data = restaurants.rows.map((r) => ({
+          ...r,
+          description: r.description.substring(0, 50),
+        }));
+        return res.render("restaurants", {
+          restaurants: data,
+          categories,
+          categoryId,
+          pagination: getPagination(limit, page, restaurants.count),
+        });
+      })
+      .catch((err) => next(err));
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
