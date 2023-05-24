eed1da5905af382373c41d81a78e72b2d238b0a1
elviawu <elviawu@gmail.com>
2023-05-24 16:31:45 +0800

    feat: add post comment on restaurant page

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,9 @@ const router = express.Router()
 const passport = require('../config/passport')
 
 const admin = require('./modules/admin')
-
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
+const commentController = require('../controllers/comment-controller')
 
 const { authenticated, authenticatedAdmin } = require('../middleware/auth')
 
@@ -16,16 +16,16 @@ router.use('/admin', authenticatedAdmin, admin)
 
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
-
 router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
-
 router.get('/logout', userController.logout)
 
 router.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard)
 router.get('/restaurants/:id', authenticated, restController.getRestaurant)
 router.get('/restaurants', authenticated, restController.getRestaurants)
-router.use('/', (req, res) => res.redirect('/restaurants'))
+
+router.post('/comments', authenticated, commentController.postComment)
+router.get('/', (req, res) => res.redirect('/restaurants'))
 
 router.use('/', generalErrorHandler)
 
