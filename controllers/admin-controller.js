e0d09fa75ea22dc10cbd471358417b5d47c25c67
jiasyuanchu <jiasyuanchu@gmail.com>
2023-05-24 16:15:35 +0800

    merge R01

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,4 +1,5 @@
 const { Restaurant } = require('../models')
+const { User } = require('../models') // 取得user model
 const { imgurFileHandler } = require('../helpers/file-helpers')
 
 const adminController = {
@@ -83,6 +84,32 @@ const adminController = {
         return restaurant.destroy()
       })
       .then(() => res.redirect('/admin/restaurants'))
+      .catch(err => next(err))
+  },
+  getUsers: (req, res, next) => {
+    return User.findAll({
+      raw: true // 將Sequelize取回來的物件保留下JS原生物件就好
+    })
+      .then(users => res.render('admin/users', { users }))
+      .catch(err => next(err))
+  },
+  patchUser: (req, res, next) => {
+    return User.findByPk(req.params.id)
+      .then(user => {
+        if (!user) throw new Error("User didn't exist.")
+        if (user.email === 'root@example.com') {
+          req.flash('error_messages', '禁止變更 root 權限')
+          return res.redirect('back')
+        } else {
+          return user.update({
+            isAdmin: !user.isAdmin
+          })
+        }
+      })
+      .then(() => {
+        req.flash('success_messages', '使用者權限變更成功')
+        res.redirect('/admin/users')
+      })
       .catch(err => next(err))
   }
 }
