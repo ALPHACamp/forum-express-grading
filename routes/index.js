fd0eebb391f17a9afef620c1e4efe75c0fb6d022
vinchiengithub <jianfan0114@gmail.com>
2023-05-24 16:48:08 +0800

    feat: add feeds function

--- routes/index.js
+++ routes/index.js
@@ -31,7 +31,7 @@ router.put(
   upload.single('image'),
   userController.putUser
 )
-
+router.get('/restaurants/feeds', authenticated, restController.getFeeds)
 router.get(
   '/restaurants/:id/dashboard',
   authenticated,
