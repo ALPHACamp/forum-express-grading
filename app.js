98fca76e8bae9de51cd59cc31c118e876d95938f
newm1n <fbsns20@gmail.com>
2023-05-24 20:37:12 +0800

    feat: add imgur api

--- app.js
+++ app.js
@@ -11,9 +11,13 @@ const { getUser } = require('./helpers/auth-helpers')
 
 const routes = require('./routes')
 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const app = express()
 const port = process.env.PORT || 3000
-const SESSION_SECRET = 'secret'
+const SESSION_SECRET = process.env.SESSION_SECRET
 
 app.engine('hbs', handlebars({ extname: '.hbs', helpers: handlebarsHelpers }))
 app.set('view engine', 'hbs')
