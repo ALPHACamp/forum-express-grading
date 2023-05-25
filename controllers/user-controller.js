976b4d27817915bdb9762b160a72ef34a15cad51
CharisLai <griffin8418@gmail.com>
2023-05-25 09:09:15 +0800

    feat: add user rest-comment
    fix comment table

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcryptjs')
 const db = require('../models')
-const { User } = db
+const { User, Restaurant, Comment } = db
 const { imgurFileHandler } = require('../helpers/file-helpers')
 
 const userController = {
@@ -44,13 +44,20 @@ const userController = {
     },
     getUser: (req, res, next) => {
         const id = req.params.id
-        return User.findByPk(id, { raw: true })
-            .then(user => {
+        return Promise.all(
+            [User.findByPk(id, { raw: true }),
+            Comment.findAll({
+                include: [Restaurant],
+                raw: true,
+                nest: true,
+                where: { userId: req.params.id }
+            })]
+        )
+            .then(([user, comments]) => {
                 if (!user) throw new Error("User didn't exist!")
-                return res.render('users/profile', { user })
+                return res.render('users/profile', { user, comments })
             })
             .catch(err => next(err))
-        // res.render('users/profile')
     },
     editUser: (req, res, next) => {
         return User.findByPk(req.params.id, { raw: true })
