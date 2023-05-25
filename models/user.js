3243008c14b8e4676affd06e057b67f66c1acecf
Chingsan0722 <rory85722@gmail.com>
2023-05-25 22:44:42 +0800

    feat: create favorite model

--- models/user.js
+++ models/user.js
@@ -4,13 +4,13 @@ const {
 } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate (models) {
       User.hasMany(models.Comment, { foreignKey: 'userId' })
+      User.belongsToMany(models.Restaurant, {
+        through: models.Favorite, // 透過 Favorite 表來建立關聯
+        foreignKey: 'userId', // 對 Favorite 表設定 FK
+        as: 'FavoritedRestaurants' // 幫這個關聯取個名稱
+      })
     }
   };
   User.init({
