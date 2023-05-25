679339f28f2210ad046ee9299d97c863f8b31910
Anna0118 <annachu0988@gmail.com>
2023-05-26 02:02:09 +0800

    Squashed commit of the following:
    commit 0b88616df0c265bd53aa11af3351b6c8f95314c9
    Author: Anna0118 <annachu0988@gmail.com>
    Date:   Fri May 26 01:23:24 2023 +0800
    
        feat: add dashboard and viewcount
    
    commit e35847970aacd99afb8d92f4518efee10bac17e5
    Merge: 2976588 9d566b6
    Author: Anna0118 <annachu0988@gmail.com>
    Date:   Thu May 25 22:02:51 2023 +0800
    
        Merge remote-tracking branch 'upstream/R02-test' into R02
    
    commit 9d566b6e0de4de3ba4dc33137230dc8cce77a37b
    Author: zjzheng17 <90173539+zjzheng17@users.noreply.github.com>
    Date:   Mon Jul 18 12:43:45 2022 +0800
    
        correcting typo in comment
    
    commit b54a2e99ab2d9f4d1a6d4fe0393213518626296c
    Author: Amber Yan <freehourse2sh@gmail.com>
    Date:   Wed Mar 2 15:27:03 2022 +0800
    
        add R02.test.js file
    
    commit fca7f3bc73dc9fa9bfadef198304b1ba88e6a44b
    Author: Carrot7712 <carrot7712@gmail.com>
    Date:   Thu Jan 13 16:48:07 2022 +0800
    
        feat: add R01.test.js and unit-test-helpers.js

--- routes/index.js
+++ routes/index.js
@@ -17,7 +17,8 @@ router.post('/signin', passport.authenticate('local', { failureRedirect: '/signi
 router.get('/logout', userController.logout)
 
 router.get('/restaurants', authenticated, restController.getRestaurants)
-router.get('/restaurants/:id', authenticated, restController.getRestaurant) // @Add
+router.get('/restaurants/:id', authenticated, restController.getRestaurant)
+router.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard) // @Add
 
 router.get('/', (req, res) => res.redirect('/restaurants'))
 router.use('/', generalErrorHandler)
