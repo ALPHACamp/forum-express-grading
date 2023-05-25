fd24f549ea16681af02b7d4905f7de75c2394c99
newm1n <fbsns20@gmail.com>
2023-05-25 22:27:27 +0800

    feat: category update

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -25,6 +25,8 @@ router.post(
 router.patch('/users/:id', adminController.patchUser)
 router.get('/users', adminController.getUsers)
 
+router.get('/categories/:id', categoryController.getCategories)
+router.put('/categories/:id', categoryController.putCategory)
 router.get('/categories', categoryController.getCategories)
 router.post('/categories', categoryController.postCategory)
 
