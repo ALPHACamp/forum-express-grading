2de6487118cd600c22bf86f0d90bbca7f832b348
WeiWayne1030 <wayne10032004@gmail.com>
2023-05-25 15:48:41 +0800

    feat: add topUser page

--- routes/index.js
+++ routes/index.js
@@ -22,6 +22,7 @@ router.post('/signin', passport.authenticate('local', { failureRedirect: '/signi
 
 router.get('/logout', userController.logout)
 
+router.get('/users/top', authenticated, userController.getTopUsers)
 router.get('/users/:id/edit', authenticated, userController.editUser)
 router.put('/users/:id', authenticated, upload.single('image'), userController.putUser)
 router.get('/users/:id', authenticated, userController.getUser)
@@ -40,7 +41,7 @@ router.delete('/favorite/:restaurantId', authenticated, userController.removeFav
 router.post('/like/:restaurantId', authenticated, userController.addLike)
 router.delete('/like/:restaurantId', authenticated, userController.removeLike)
 
+router.get('/', (req, res) => res.redirect('/restaurants'))
 router.use('/', generalErrorHandler)
-router.use('/', (req, res) => res.redirect('/restaurants'))
 
 module.exports = router
