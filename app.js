38e0b5c1e4722685c36b73fa6bdf9453c17e0262
newm1n <fbsns20@gmail.com>
2023-05-25 19:37:05 +0800

    fix session secret

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ if (process.env.NODE_ENV !== 'production') {
 
 const app = express()
 const port = process.env.PORT || 3000
-const SESSION_SECRET = process.env.SESSION_SECRET
+const SESSION_SECRET = 'secret'
 
 app.engine('hbs', handlebars({ extname: '.hbs', helpers: handlebarsHelpers }))
 app.set('view engine', 'hbs')
