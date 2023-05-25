fb1b18914661964809f0354ff6797414af4e2634
av124773 <av124773@gmail.com>
2023-05-25 21:18:51 +0800

    feat: add restaurant page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -5,15 +5,30 @@ const restaurantController = {
       include: Category,
       nest: true,
       raw: true
-    }).then(restaurants => {
-      const data = restaurants.map(r => ({
-        ...r,
-        description: r.description.substring(0, 50)
-      }))
-      return res.render('restaurants', {
-        restaurants: data
-      })
     })
+      .then(restaurants => {
+        const data = restaurants.map(r => ({
+          ...r,
+          description: r.description.substring(0, 50)
+        }))
+        return res.render('restaurants', {
+          restaurants: data
+        })
+      })
+  },
+  getRestaurant: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, {
+      include: Category,
+      nest: true,
+      raw: true
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        res.render('restaurant', {
+          restaurant
+        })
+      })
+      .catch(err => next(err))
   }
 }
 module.exports = restaurantController
