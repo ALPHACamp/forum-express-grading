29765889aafe13d0ca9546e811bcf3bdedbab061
Anna0118 <annachu0988@gmail.com>
2023-05-25 21:52:46 +0800

    feat: add restaurant page

--- routes/index.js
+++ routes/index.js
@@ -12,11 +12,12 @@ router.use('/admin', authenticatedAdmin, admin)
 
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
-router.get('/signin', userController.signInPage) // @Add
+router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn) // 注意是 post , @Add
-router.get('/logout', userController.logout) // @Add
+router.get('/logout', userController.logout)
 
 router.get('/restaurants', authenticated, restController.getRestaurants)
+router.get('/restaurants/:id', authenticated, restController.getRestaurant) // @Add
 
 router.get('/', (req, res) => res.redirect('/restaurants'))
 router.use('/', generalErrorHandler)
