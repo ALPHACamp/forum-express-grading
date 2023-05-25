44833e6966de1d212768e2e28b59878ca65b5940
HuangYiLun <wk5018102@gmail.com>
2023-05-25 13:39:25 +0800

    fea: category hbs and CURD feature

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,7 +1,14 @@
 const express = require('express')
 const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
+const categoryController = require('../../controllers/category-controller')
 const upload = require('../../middleware/multer')
+
+router.get('/categories/:id', categoryController.getCategories)
+router.get('/categories', categoryController.getCategories)
+router.put('/categories/:id', categoryController.putCategory)
+router.delete('/categories/:id', categoryController.deleteCategory)
+router.post('/categories', categoryController.postCategory)
 
 router.get('/restaurants/create', adminController.createRestaurants)
 router.get('/restaurants/:id/edit', adminController.editRestaurant)
@@ -10,6 +17,7 @@ router.put('/restaurants/:id', upload.single('image'), adminController.putRestau
 router.delete('/restaurants/:id', adminController.deleteRestaurant)
 router.get('/restaurants', adminController.getRestaurants)
 router.post('/restaurants', upload.single('image'), adminController.postRestaurant)
+
 router.get('/users', adminController.getUsers)
 router.patch('/users/:id', adminController.patchUser)
 router.get('/', (req, res) => res.redirect('/admin/restaurants'))
