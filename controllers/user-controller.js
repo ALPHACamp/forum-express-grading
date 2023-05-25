4c84ebfa6e268c9b1cbf1e74b70b132439588c05
goodjobhot401 <goodjobhot@gmail.com>
2023-05-25 15:40:09 +0800

    feat: add like button at index page

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,5 +1,5 @@
 const bcrypt = require('bcryptjs')
-const { User, Comment, Restaurant, Favorite } = require('../models')
+const { User, Comment, Restaurant, Favorite, Like } = require('../models')
 const { imgurFileHandler } = require('../helper/file-helpers')
 
 const userController = {
@@ -136,6 +136,50 @@ const userController = {
       .then(favorite => {
         if (!favorite) throw new Error("You haven't favorted this restaurant")
         return favorite.destroy()
+      })
+      .then(() => res.redirect('back'))
+      .catch(err => next(err))
+  },
+
+  addLike: (req, res, next) => {
+    const { restaurantId } = req.params
+    return Promise.all([
+      Restaurant.findByPk(restaurantId),
+      Like.findOne({
+        where: {
+          userId: req.user.id,
+          restaurantId
+        }
+      })
+    ])
+      .then(([restaurant, like]) => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        if (like) throw new Error('You have liked this restaurant')
+
+        return Like.create({
+          userId: req.user.id,
+          restaurantId
+        })
+          .then(() => res.redirect('back'))
+          .catch(err => next(err))
+      })
+      .then(() => res.redirect('back'))
+      .catch(err => next(err))
+  },
+
+  removeLike: (req, res, next) => {
+    const { restaurantId } = req.params
+    return Like.findOne({
+      where: {
+        userId: req.user.id,
+        restaurantId
+      }
+    })
+      .then(like => {
+        if (!like) throw new Error("You haven't like this restaurant!")
+        return Like.destroy()
+          .then(() => res.redirect('back'))
+          .catch(err => next(err))
       })
       .then(() => res.redirect('back'))
       .catch(err => next(err))
