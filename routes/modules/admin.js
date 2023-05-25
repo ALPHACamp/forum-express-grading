7db98f77d7e7ce3ced21cfce10dc2d478f59bd09
Banwind <a84012807@gmail.com>
2023-05-26 01:12:30 +0800

    feat: add restaurants index page

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -14,7 +14,7 @@ router.post('/restaurants', upload.single('image'), adminController.postRestaura
 router.get('/users', adminController.getUsers)
 router.get('/categories/:id', categoryController.getCategories)
 router.put('/categories/:id', categoryController.putCategory)
-router.delete("/categories/:id", categoryController.deleteCategory)
+router.delete('/categories/:id', categoryController.deleteCategory)
 router.get('/categories', categoryController.getCategories)
 router.post('/categories', categoryController.postCategory)
 
