f7f3c6281f163e92faa9eb56f2756550016d4439
playcsgo <sam@plum.wine>
2023-05-25 10:35:01 +0800

    feat: add paginator

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,29 +1,37 @@
 const { Restaurant, Category } = require('../models')
+const { getOffset, getPagination } = require('../helpers/pagination-helper')
 
 const restaurantColler = {
   getRestaurants: (req, res, next) => {
+    const DEFAULT_LIMIT = 9
     const categoryId = Number(req.query.categoryId) || ''
-    console.log(categoryId)
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || DEFAULT_LIMIT
+    const offset = getOffset(limit, page)
     return Promise.all([
-      Restaurant.findAll({
+      Restaurant.findAndCountAll({
         include: Category,
         where: {
           ...categoryId ? { categoryId } : {}
         },
+        limit,
+        offset,
         raw: true,
         nest: true
       }),
       Category.findAll({ raw: true }) // 為了render category-Nav
     ])
       .then(([restaurants, categories]) => {
-        const data = restaurants.map(r => ({
+        console.log(restaurants)
+        const data = restaurants.rows.map(r => ({
           ...r,
           description: r.description.substring(0, 50)
         }))
         return res.render('restaurants', {
           restaurants: data,
           categories,
-          categoryId
+          categoryId,
+          pagination: getPagination(limit, page, restaurants.counts)
         })
       })
       .catch(err => next(err))
