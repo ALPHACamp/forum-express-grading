88bc6a7da11b46bea6b4cc972d59ef2aef9a0dc8
aking4511 <aking4511@gmail.com>
2023-05-25 17:15:43 +0800

    Squashed commit of the following:
    commit f20d4bbfa67210c0537f87926a7446d2e24cfcb7
    Author: aking4511 <aking4511@gmail.com>
    Date:   Thu May 25 17:05:28 2023 +0800
    
        feat: restaurant dashboard
    
    commit e155bdbbbe6b80ec519b2d2c17615fb565ef96cc
    Merge: 771f783 9d566b6
    Author: aking4511 <aking4511@gmail.com>
    Date:   Thu May 25 15:25:23 2023 +0800
    
        Merge remote-tracking branch 'upstream/R02-test' into R02
    
    commit 9d566b6e0de4de3ba4dc33137230dc8cce77a37b
    Author: zjzheng17 <90173539+zjzheng17@users.noreply.github.com>
    Date:   Mon Jul 18 12:43:45 2022 +0800
    
        correcting typo in comment
    
    commit b54a2e99ab2d9f4d1a6d4fe0393213518626296c
    Author: Amber Yan <freehourse2sh@gmail.com>
    Date:   Wed Mar 2 15:27:03 2022 +0800
    
        add R02.test.js file
    
    commit fca7f3bc73dc9fa9bfadef198304b1ba88e6a44b
    Author: Carrot7712 <carrot7712@gmail.com>
    Date:   Thu Jan 13 16:48:07 2022 +0800
    
        feat: add R01.test.js and unit-test-helpers.js

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -17,17 +17,27 @@ const restaurantController = {
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
-      include: Category, // 拿出關聯的 Category model
-      nest: true,
-      raw: true
+      include: Category // 拿出關聯的 Category model
     })
       .then(restaurant => {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
+        return restaurant.increment({ viewCounts: 1 })
+      })
+      .then(restaurant => {
         res.render('restaurant', {
-          restaurant
+          restaurant: restaurant.toJSON()
         })
       })
       .catch(err => next(err))
+  },
+  getDashboard: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, {
+      include: Category,
+      nest: true,
+      raw: true
+    }).then(restaurant => {
+      res.render('dashboard', { restaurant })
+    })
   }
 }
 module.exports = restaurantController
