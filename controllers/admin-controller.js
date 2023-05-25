5554cc929a7e617ac730fc64cdfbfb7d1f8ce5c0
yiyun <ccjhs70325@gmail.com>
2023-05-25 17:52:40 +0800

    fix users.hbs

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -87,10 +87,11 @@ const adminController = {
       .catch(err => next(err))
   },
   getUsers: (req, res, next) => {
-    User.findAll({
+    return User.findAll({
       raw: true
     })
       .then(users => res.render('admin/users', { users }))
+      .catch(err => next(err))
   }
 }
 
