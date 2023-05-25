348665085b52be74a45f701de48bd20e33335a94
JimmyLin <disillusioned2077@gmail.com>
2023-05-25 17:11:57 +0800

    feat: create category table

--- models/restaurant.js
+++ models/restaurant.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      Restaurant.belongsTo(models.Category, { foreignKey: 'categoryId' }) // 設定單方與category關聯，並設定categoryId為外鍵
     }
   };
   Restaurant.init({
