c2ae801bc4c8f45e3c82506e3357292505d5fa33
jiasyuanchu <jiasyuanchu@gmail.com>
2023-05-26 18:47:19 +0800

    main merged R03

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,13 +1,14 @@
 const bcrypt = require('bcryptjs')
-const db = require('../models')
-const { User } = db
+const { User, Comment, Restaurant } = require('../models')
+const { imgurFileHandler } = require('../helpers/file-helpers')
+
 const userController = {
   signUpPage: (req, res) => {
-    res.render('signup')
+    return res.render('signup')
   },
   signUp: (req, res, next) => {
     if (req.body.password !== req.body.passwordCheck) throw new Error('Passwords do not match!')
-    User.findOne({ where: { email: req.body.email } })
+    return User.findOne({ where: { email: req.body.email } })
       .then(user => {
         if (user) throw new Error('Email already exists!')
         return bcrypt.hash(req.body.password, 10)
@@ -19,21 +20,70 @@ const userController = {
       }))
       .then(() => {
         req.flash('success_messages', '成功註冊帳號！')
-        res.redirect('/signin')
+        return res.redirect('/signin')
       })
       .catch(err => next(err))
   },
   signInPage: (req, res) => {
-    res.render('signin')
+    return res.render('signin')
   },
   signIn: (req, res) => {
     req.flash('success_messages', '成功登入！')
-    res.redirect('/restaurants')
+    return res.redirect('/restaurants')
   },
   logout: (req, res) => {
     req.flash('success_messages', '登出成功！')
     req.logout()
-    res.redirect('/signin')
+    return res.redirect('/signin')
+  },
+  getUser: (req, res, next) => {
+    return User.findByPk(req.params.id,
+      {
+        include:
+        {
+          model: Comment,
+          include: Restaurant
+        },
+        nest: true
+      })
+      .then(user => {
+        if (!user) throw new Error("User didn't exist.")
+        const commentRestaurant = user.Comments.map(
+          comment => comment.Restaurant.dataValues
+        )
+        return res.render('users/profile', {
+          user: user.toJSON(),
+          commentRestaurant
+        })
+      })
+      .catch(err => next(err))
+  },
+  editUser: (req, res, next) => {
+    return User.findByPk(req.params.id, { raw: true })
+      .then(user => {
+        if (!user) throw new Error("User didn't exist.")
+        return res.render('users/edit', { user })
+      })
+      .catch(err => next(err))
+  },
+  putUser: (req, res, next) => {
+    const { name } = req.body
+    const { file } = req
+    if (!name) throw new Error('Name is required!')
+    return Promise.all([User.findByPk(req.params.id), imgurFileHandler(file)])
+      .then(([user, filePath]) => {
+        if (!user) throw new Error("User didn't exist.")
+        return user.update({
+          name,
+          image: filePath || user.image
+        })
+      })
+      .then(user => {
+        req.flash('success_messages', '使用者資料編輯成功')
+        return res.redirect(`/users/${req.params.id}`)
+      })
+      .catch(err => next(err))
   }
 }
+
 module.exports = userController
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const passport = require('../config/passport')
 const admin = require('./modules/admin')
+const upload = require('../middleware/multer')
 
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
@@ -22,8 +23,14 @@ router.get('/restaurants/:id', authenticated, restController.getRestaurant)
 router.get('/restaurants', authenticated, restController.getRestaurants)
 router.get('/restaurants', restController.getRestaurants)
 
+// comments
 router.delete('/comments/:id', authenticatedAdmin, commentController.deleteComment)
 router.post('/comments', authenticated, commentController.postComment)
+
+// users
+router.get('/users/:id/edit', authenticated, userController.editUser)
+router.get('/users/:id', authenticated, userController.getUser)
+router.put('/users/:id', upload.single('image'), authenticated, userController.putUser)
 
 router.use('/', (req, res) => res.redirect('/restaurants'))
 
