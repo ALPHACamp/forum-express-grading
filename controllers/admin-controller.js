2f1c11bef93565e8d0353e0d696408f7686bde2c
Suz033 <susanamy2324@gmail.com>
2023-05-26 09:55:24 +0800

    feat: show category on admin restaurant pages

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,13 +1,14 @@
 // files
-const { Restaurant } = require('../models')
-const { User } = require('../models')
+const { Restaurant, User, Category } = require('../models')
 const { imgurFileHandler } = require('../helpers/file-helpers')
 
 // controllers
 const adminController = {
   getRestaurants: (req, res, next) => {
     Restaurant.findAll({
-      raw: true
+      raw: true,
+      nest: true,
+      include: [Category]
     })
       .then(restaurants => res.render('admin/restaurants', { restaurants }))
       .catch(err => next(err))
@@ -42,7 +43,9 @@ const adminController = {
   getRestaurant: (req, res, next) => {
     // 去資料庫用 id 找一筆資料
     Restaurant.findByPk(req.params.id, {
-      raw: true // 找到以後整理格式再回傳
+      raw: true, // 找到以後整理格式再回傳
+      nest: true,
+      include: [Category]
     })
       .then(restaurant => {
         //  如果找不到，回傳錯誤訊息，後面不執行
