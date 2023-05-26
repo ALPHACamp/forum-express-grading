5df01a2e387fa5da5bb7cb0be909fb33cef23009
jiasyuanchu <jiasyuanchu@gmail.com>
2023-05-26 11:50:15 +0800

    fix: try to use asynchronous codes in restaurant-controller.js

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,50 +1,80 @@
 const { Restaurant, Category } = require('../models')
 
 const restaurantController = {
-  getRestaurants: (req, res) => {
-    return Restaurant.findAll({
-      include: Category,
-      nest: true,
-      raw: true
-    }).then(restaurants => {
+  getRestaurants: async (req, res) => {
+    try {
+      const restaurants = await Restaurant.findAll({
+        include: Category,
+        nest: true,
+        raw: true
+      })
+
       const data = restaurants.map(r => ({
         ...r,
         description: r.description.substring(0, 50)
       }))
+
       return res.render('restaurants', {
         restaurants: data
       })
-    })
+    } catch (err) {
+      console.error(err)
+      return res.status(500).send('Internal Server Error')
+    }
   },
-  getRestaurant: (req, res, next) => {
-    return Restaurant.findByPk(req.params.id, {
-      include: Category,
-      nest: true,
-      raw: true
-    })
-      .then(restaurant => {
-        if (!restaurant) throw new Error("Restaurant didn't exist!")
-        res.render('restaurant', {
-          restaurant
-        })
-        return Restaurant.increment('viewCounts', { where: { id: req.params.id } })
-      })
-      .catch(err => next(err))
-  },
-  getDashboard: (req, res, next) => {
-    return Restaurant.findByPk(req.params.id, {
-      include: Category,
-      nest: true,
-      raw: true
-    })
-      .then(restaurant => {
-        if (!restaurant) throw new Error("Restaurant didn't exist!")
-        res.render('dashboard', {
-          restaurant
-        })
-      })
-      .catch(err => next(err))
-  }
 
+  getRestaurant: async (req, res, next) => {
+    try {
+      const restaurant = await Restaurant.findByPk(req.params.id, {
+        include: Category,
+        nest: true,
+        raw: true
+      })
+
+      if (!restaurant) {
+        throw new Error("Restaurant doesn't exist!")
+      }
+
+      await Restaurant.increment('viewCounts', {
+        where: { id: req.params.id }
+      })
+
+      const updatedRestaurant = await Restaurant.findByPk(req.params.id, {
+        include: Category,
+        nest: true,
+        raw: true
+      })
+
+      res.render('restaurant', {
+        restaurant: updatedRestaurant
+      })
+      console.log(restaurant)
+    } catch (err) {
+      console.error(err)
+      return next(err)
+    }
+  },
+
+  getDashboard: async (req, res, next) => {
+    try {
+      const restaurant = await Restaurant.findByPk(req.params.id, {
+        include: Category,
+        nest: true,
+        raw: true
+      })
+
+      if (!restaurant) {
+        throw new Error("Restaurant doesn't exist!")
+      }
+
+      res.render('dashboard', {
+        restaurant
+      })
+    } catch (err) {
+      console.error(err)
+      return next(err)
+    }
+  }
 }
+
 module.exports = restaurantController
