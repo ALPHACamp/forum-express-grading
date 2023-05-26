bcec1dbe6cfcc97a18be503d46653a0129fa27fd
HuangYiLun <wk5018102@gmail.com>
2023-05-26 19:05:23 +0800

    feat: show comments on restaurant page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,4 +1,4 @@
-const { Restaurant, Category } = require('../models')
+const { Restaurant, Category, Comment, User } = require('../models')
 const { getOffset, getPagination } = require('../helpers/pagination-helper')
 
 const restaurantController = {
@@ -37,13 +37,15 @@ const restaurantController = {
   },
   getRestaurant: (req, res) => {
     return Restaurant.findByPk(req.params.id, {
-      raw: true,
-      nest: true,
-      include: Category
+      include: [
+        Category,
+        { model: Comment, include: User }
+      ]
     }).then(restaurant => {
+      console.log(restaurant.Comments[0].dataValues)
       if (!restaurant) throw new Error("Restaurant didn't exist!")
       return res.render('restaurant', {
-        restaurant
+        restaurant: restaurant.toJSON()
       })
     })
   }
