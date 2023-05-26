1aa75008b925b0fb0c1a7543bd7ef3be39b2ca7a
KenYuChang <justin0915justin0915@gmail.com>
2023-05-26 17:04:16 +0800

    ad followship models, view, and controllers

--- config/passport.js
+++ config/passport.js
@@ -43,6 +43,8 @@ passport.deserializeUser((id, cb) => {
     include: [
       { model: Restaurant, as: "FavoritedRestaurants" },
       { model: Restaurant, as: "LikedRestaurants" },
+      { model: User, as: "Followers" },
+      { model: User, as: "Followings" },
     ],
   })
 
--- routes/index.js
+++ routes/index.js
@@ -33,6 +33,7 @@ router.get("/restaurants/:id", authenticated, restController.getRestaurant);
 router.get("/restaurants", authenticated, restController.getRestaurants);
 
 //users
+router.get("/users/top", authenticated, userController.getTopUsers);
 router.get("/users/:id/edit", authenticated, userController.editUser);
 router.get("/users/:id", authenticated, userController.getUser);
 router.put(
@@ -63,6 +64,14 @@ router.delete(
 //Like
 router.post("/like/:restaurantId", authenticated, userController.addLike);
 router.delete("/like/:restaurantId", authenticated, userController.removeLike);
+
+//follow
+router.post("/following/:userId", authenticated, userController.addFollowing);
+router.delete(
+  "/following/:userId",
+  authenticated,
+  userController.removeFollowing
+);
 router.get("/", (req, res) => res.redirect("/restaurants"));
 router.use("/", generalErrorHandler);
 module.exports = router;
