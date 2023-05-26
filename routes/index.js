dbcaa1952d76a4dfcc064ba65950dcf3cc890040
jiasyuanchu <jiasyuanchu@gmail.com>
2023-05-26 16:12:00 +0800

    feat: add post comment on restaurant page

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,14 @@
 const express = require('express')
 const router = express.Router()
 const passport = require('../config/passport')
+const admin = require('./modules/admin')
 
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
+const commentController = require('../controllers/​​comment-controller')
+
 const { authenticated, authenticatedAdmin } = require('../middleware/auth') // 把驗證程序 authenticatedAdmin 向上抽離到index.js
-const admin = require('./modules/admin')
+
 const { generalErrorHandler } = require('../middleware/error-handler')
 
 router.use('/admin', authenticatedAdmin, admin)
@@ -18,7 +21,11 @@ router.get('/restaurants/:id/dashboard', authenticated, restController.getDashbo
 router.get('/restaurants/:id', authenticated, restController.getRestaurant)
 router.get('/restaurants', authenticated, restController.getRestaurants)
 router.get('/restaurants', restController.getRestaurants)
+
+router.post('/comments', authenticated, commentController.postComment)
+
 router.use('/', (req, res) => res.redirect('/restaurants'))
+
 router.use('/', generalErrorHandler)
 
 module.exports = router
