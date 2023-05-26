86a05f64ca5b500bf4ad3853c9209790fdefc1e1
Ray7808 <yuwang7808@gmail.com>
2023-05-26 20:47:04 +0800

    fix: revise the seeder and model of restaurant to show viewCounts
    feat: add viewCounts inside restaurant model and show on the dashboard.hbs

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -32,11 +32,19 @@ const restaurantController = {
       .catch(err => next(err))
   },
   getDashboard: (req, res, next) => {
-    return Restaurant.findByPk(req.params.id, {
-      include: Category, // 拿出關聯的 Category model
-      nest: true,
-      raw: true
-    })
+    const restaurantId = req.params.id
+    return Restaurant.findByPk(restaurantId)
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        return restaurant.increment('viewCounts', { by: 1 })
+      })
+      .then(() => {
+        return Restaurant.findByPk(req.params.id, {
+          include: Category, // 拿出關聯的 Category model
+          nest: true,
+          raw: true
+        })
+      })
       .then(restaurant => {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
         res.render('dashboard', { restaurant })
