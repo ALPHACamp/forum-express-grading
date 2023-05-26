34d7807b397102acde2bc865283de68ab4466a71
Wei <awei1127@hotmail.com>
2023-05-26 21:46:35 +0800

    add feeds page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -61,6 +61,32 @@ const restaurantController = {
         res.render('dashboard', { restaurant })
       })
       .catch(err => next(err))
+  },
+  getFeeds: (req, res, next) => {
+    return Promise.all([
+      Restaurant.findAll({
+        limit: 10,
+        order: [
+          ['createdAt', 'DESC']
+        ],
+        include: [Category],
+        nest: true,
+        raw: true
+      }),
+      Comment.findAll({
+        limit: 10,
+        order: [
+          ['createdAt', 'DESC']
+        ],
+        include: [Restaurant, User],
+        nest: true,
+        raw: true
+      })
+    ])
+      .then(([restaurants, comments]) => {
+        res.render('feeds', { restaurants, comments })
+      })
+      .catch(err => next(err))
   }
 }
 module.exports = restaurantController
