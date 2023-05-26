bcb8b6d6e92e2055c7081a5211c4e1a2ea78d8e2
Chingsan0722 <rory85722@gmail.com>
2023-05-26 14:39:11 +0800

    feat: add topUser page

--- routes/index.js
+++ routes/index.js
@@ -21,6 +21,10 @@ router.post('/signin', passport.authenticate('local', { failureRedirect: '/signi
 // 登出
 router.get('/logout', userController.logout)
 // 餐廳頁
+router.get('/users/top', authenticated, userController.getTopUsers)
+router.get('/users/:id/edit', authenticated, userController.editUser)
+router.put('/users/:id', authenticated, upload.single('image'), userController.putUser)
+router.get('/users/:id', authenticated, userController.getUser)
 router.get('/restaurants/feeds', authenticated, restController.getFeeds)
 router.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard)
 router.get('/restaurants/:id', authenticated, restController.getRestaurant)
@@ -31,10 +35,7 @@ router.post('/favorite/:restaurantId', authenticated, userController.addFavorite
 router.delete('/favorite/:restaurantId', authenticated, userController.removeFavorite)
 router.post('/like/:restaurantId', authenticated, userController.addLike)
 router.delete('/like/:restaurantId', authenticated, userController.removeLike)
-router.get('/users/:id/edit', authenticated, userController.editUser)
-router.put('/users/:id', authenticated, upload.single('image'), userController.putUser)
-router.get('/users/:id', authenticated, userController.getUser)
-router.use('/', (req, res) => { res.redirect('/restaurants') })
+router.get('/', (req, res) => { res.redirect('/restaurants') }) // get use 都能用?
 router.use('/', generalErrorHandler)
 
 module.exports = router
