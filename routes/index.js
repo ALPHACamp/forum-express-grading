a930dc407297dba30fdc8f4b3475fd7426cb5c01
rr37 <ay870527@gmail.com>
2023-05-26 21:27:00 +0800

    feat: add topUser page

--- routes/index.js
+++ routes/index.js
@@ -41,6 +41,7 @@ router.post('/like/:restaurantId', authenticated, userController.addLike)
 router.delete('/like/:restaurantId', authenticated, userController.removeLike)
 
 // 個人頁面
+router.get('/users/top', authenticated, userController.getTopUsers)
 router.get('/users/:id/edit', authenticated, userController.editUser)
 router.get('/users/:id', authenticated, userController.getUser)
 router.put('/users/:id', upload.single('image'), authenticated, userController.putUser)
