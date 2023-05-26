a14789918bc2dbd63fb5e9776d018bee4ff9cd2a
JimmyLin <disillusioned2077@gmail.com>
2023-05-26 21:05:07 +0800

    feat: add category filter feature

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,20 +1,31 @@
 const { Restaurant, Category } = require('../models') // 帶入資料庫
 const restaurantController = {
-  getRestaurants: (req, res) => {
+  getRestaurants: (req, res, next) => {
     // 首頁
-    return Restaurant.findAll({
-      include: Category, // 帶入關聯
-      nest: true, // include整理
-      raw: true
-    }).then(restaurants => {
-      const data = restaurants.map(r => ({
-        ...r, // 把 r 展開倒入 data 以便做資料修改
-        description: r.description.substring(0, 50) // 修改description，擷取 0-50 的文字內容
-      }))
-      return res.render('restaurants', {
-        restaurants: data
+    const categoryId = Number(req.query.categoryId) || '' // 抓出網址的id並字串轉數字
+    return Promise.all([
+      Restaurant.findAll({
+        include: Category, // 帶入關聯
+        where: { // 新增查詢條件
+          ...categoryId ? { categoryId } : {} // 檢查 categoryId 是否為空值，不是空值就帶入與網址相同的id並展開，是的話帶入{}空字串，才能使用hbs的#ifCond
+        },
+        nest: true, // include整理
+        raw: true
+      }),
+      Category.findAll({ raw: true }) // 抓Category資訊
+    ])
+      .then(([restaurants, categories]) => {
+        const data = restaurants.map(r => ({
+          ...r, // 把 r 展開倒入 data 以便做資料修改
+          description: r.description.substring(0, 50) // 修改description，擷取 0-50 的文字內容
+        }))
+        return res.render('restaurants', {
+          restaurants: data,
+          categories,
+          categoryId
+        })
       })
-    })
+      .catch(err => next(err))
   },
   // 詳情頁
   getRestaurant: (req, res, next) => {
