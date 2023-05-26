eddf297dfdb28ec6fbcfaeaa3edd2858078ca02f
JimmyLin <disillusioned2077@gmail.com>
2023-05-26 15:56:04 +0800

    feat: add restaurants page UI, and render data function

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,6 +1,19 @@
+const { Restaurant, Category } = require('../models') // 帶入資料庫
 const restaurantController = {
   getRestaurants: (req, res) => {
-    return res.render('restaurants')
+    return Restaurant.findAll({
+      include: Category, // 帶入關聯
+      nest: true, // include整理
+      raw: true
+    }).then(restaurants => {
+      const data = restaurants.map(r => ({
+        ...r, // 把 r 展開倒入 data 以便做資料修改
+        description: r.description.substring(0, 50) // 修改description，擷取 0-50 的文字內容
+      }))
+      return res.render('restaurants', {
+        restaurants: data
+      })
+    })
   }
 }
 module.exports = restaurantController
