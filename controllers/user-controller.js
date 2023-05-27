df96f652e18888175a8a6ca17d038d68756c863d
Pikapikapi <huang.paladin@gmail.com>
2023-05-27 15:46:38 +0800

    feat:passport init & signin

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -33,6 +33,18 @@ const userController = {
         res.redirect('/signin')
       })
       .catch(err => next(err)) // 目前會觸發Express內建的Error Handler
+  },
+  signInPage: (req, res) => {
+    res.render('signin')
+  },
+  signIn: (req, res) => {
+    req.flash('success_messages', '成功登入！')
+    res.redirect('/restaurants')
+  },
+  logout: (req, res) => {
+    req.flash('success_messages', '登出成功！')
+    req.logout()
+    res.redirect('/signin')
   }
 }
 module.exports = userController
