f0bfec3bfd087a4fb5ccedf8e1b8b0fd202513e3
goodjobhot401 <goodjobhot@gmail.com>
2023-05-27 19:56:41 +0800

    feat: adjust middleware

--- config/passport.js
+++ config/passport.js
@@ -28,7 +28,7 @@ passport.serializeUser((user, cb) => {
   cb(null, user.id)
 })
 passport.deserializeUser((id, cb) => {
-  User.findByPk(id, {
+  return User.findByPk(id, {
     include: [
       { model: Restaurant, as: 'FavoritedRestaurants' },
       { model: Restaurant, as: 'LikedRestaurants' },
