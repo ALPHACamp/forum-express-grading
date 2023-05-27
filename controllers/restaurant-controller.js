8216512f3497dfa4569bf07a446f47f54f612f93
Ray7808 <yuwang7808@gmail.com>
2023-05-27 12:01:59 +0800

    feat: show comments on restaurant page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,4 +1,4 @@
-const { Restaurant, Category } = require('../models')
+const { Restaurant, Category, Comment, User } = require('../models')
 const { getOffset, getPagination } = require('../helpers/pagination-helper')
 
 const restaurantController = {
@@ -37,14 +37,15 @@ const restaurantController = {
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
-      include: Category, // 拿出關聯的 Category model
-      nest: true,
-      raw: true
+      include: [
+        Category,
+        { model: Comment, include: User }
+      ]
     })
       .then(restaurant => {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
         res.render('restaurant', {
-          restaurant
+          restaurant: restaurant.toJSON()
         })
         return Restaurant.increment('viewCounts', { where: { id: req.params.id } })
       })
@@ -52,7 +53,6 @@ const restaurantController = {
   },
   getDashboard: (req, res, next) => {
     const restaurantId = req.params.id
-
     return Restaurant.findByPk(restaurantId, {
       include: Category, // 拿出關聯的 Category model
       nest: true,
