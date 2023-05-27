9709abdb5e274a80669490dc0e7712ecebb76f5e
JimmyLin <disillusioned2077@gmail.com>
2023-05-27 23:17:47 +0800

    modify database user model image

--- migrations/20230527120907-add-image-to-users.js
+++ migrations/20230527120907-add-image-to-users.js
@@ -2,8 +2,7 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.addColumn('Users', 'image', { // 在restaurants新增category_id欄位
-      type: Sequelize.STRING,
-      allowNull: false // 不予許空白
+      type: Sequelize.STRING
     })
   },
   down: async (queryInterface, Sequelize) => {
