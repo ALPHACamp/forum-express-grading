dacc4479f856b99f36e719c824e98501848153bc
Leechiayu <gary86442@gmail.com>
2023-05-27 13:03:17 +0800

    fix:merge conflict

--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,7 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
 const bcrypt = require('bcryptjs')
-const { User, Restaurant, Like } = require('../models')
+const { User, Restaurant } = require('../models')
 // set up Passport strategy
 passport.use(
   new LocalStrategy(
