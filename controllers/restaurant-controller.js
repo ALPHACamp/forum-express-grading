b9c660d54fee324a719cbeea229ea87399dce3ae
aking4511 <aking4511@gmail.com>
2023-05-28 03:53:48 +0800

    feat: add feeds page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -58,6 +58,31 @@ const restaurantController = {
     }).then(restaurant => {
       res.render('dashboard', { restaurant: restaurant.toJSON() })
     })
+  },
+  getFeeds: (req, res, next) => {
+    return Promise.all([
+      Restaurant.findAll({
+        limit: 10,
+        order: [['createdAt', 'DESC']],
+        include: [Category],
+        raw: true,
+        nest: true
+      }),
+      Comment.findAll({
+        limit: 10,
+        order: [['createdAt', 'DESC']],
+        include: [User, Restaurant],
+        raw: true,
+        nest: true
+      })
+    ])
+      .then(([restaurants, comments]) => {
+        res.render('feeds', {
+          restaurants,
+          comments
+        })
+      })
+      .catch(err => next(err))
   }
 }
 module.exports = restaurantController
