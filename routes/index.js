36f7a99fe3a2e8f841e2edc2bf6a7e3424a7a7a6
newm1n <fbsns20@gmail.com>
2023-05-27 23:42:10 +0800

    feat: add topUser page

--- routes/index.js
+++ routes/index.js
@@ -29,6 +29,8 @@ router.post(
 
 router.get('/logout', userController.logout)
 
+router.get('/users/top', authenticated, userController.getTopUsers)
+
 router.get('/users/:id/edit', authenticated, userController.editUser)
 router.get('/users/:id', authenticated, userController.getUser)
 router.put(
