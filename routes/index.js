d0d3c7238a91d0847e8dd2b2d0a8342544db7f83
vinchiengithub <jianfan0114@gmail.com>
2023-05-27 15:59:47 +0800

    feat: finish R04

--- routes/index.js
+++ routes/index.js
@@ -55,6 +55,8 @@ router.delete(
   authenticated,
   userController.removeFavorite
 )
+router.post('/like/:restaurantId', authenticated, userController.addLike)
+router.delete('/like/:restaurantId', authenticated, userController.removeLike)
 router.get('/', (req, res) => res.redirect('/restaurants'))
 router.use('/', generalErrorHandler)
 
