67267a3e26b2277460e1aebe3ad7d082549e8c9f
playcsgo <sam@plum.wine>
2023-05-28 05:14:34 +0800

    feat: add following and removeFollowing on top-users page

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,5 +1,5 @@
 const bcrypt = require('bcryptjs')
-const { User, Comment, Restaurant, Favorite, Like } = require('../models')
+const { User, Comment, Restaurant, Favorite, Like, Followship } = require('../models')
 const { imgurFileHandler } = require('../helpers/file-helpers')
 const userController = {
   signUpPage: (req, res) => {
@@ -164,14 +164,52 @@ const userController = {
     return User.findAll({
       include: [{ model: User, as: 'Followers' }]
     })
+    // 使用const result = xxxx  而不直接拿回傳users做處理 可以保留原始的資料
       .then(users => {
-        users = users.map(user => ({
+        const result = users.map(user => ({
           ...user.toJSON(),
           followerCount: user.Followers.length,
           isFollowed: req.user.Followings.some(f => f.id === user.id)
         }))
-        res.render('top-users', { users: users })
+          .sort((a, b) => b.followerCount - a.followerCount)
+        res.render('top-users', { users: result })
       })
+      .catch(err => next(err))
+  },
+  // 追蹤功能
+  addFollowing: (req, res, next) => {
+    return Promise.all([
+      User.findByPk(req.params.userId),
+      Followship.findOne({
+        where: {
+          followerId: req.user.id, // 自己就是A
+          followingId: req.params.userId
+        }
+      })
+    ])
+      .then(([user, followship]) => {
+        if (!user) throw new Error('沒這人')
+        if (followship) throw new Error('跟蹤狂喔')
+        return Followship.create({
+          followerId: req.user.id,
+          followingId: req.params.userId
+        })
+      })
+      .then(() => res.redirect('back'))
+      .catch(err => next(err))
+  },
+  removeFollowing: (req, res, next) => {
+    return Followship.findOne({
+      where: {
+        followerId: req.user.id,
+        followingId: req.params.userId
+      }
+    })
+      .then(followship => {
+        if (!followship) throw new Error('刪空氣?')
+        return followship.destroy()
+      })
+      .then(() => res.redirect('back'))
       .catch(err => next(err))
   }
 }
