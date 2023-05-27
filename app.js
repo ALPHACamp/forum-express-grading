25981131e02ee03a2223c1343f3217db9ea354aa
Pikapikapi <huang.paladin@gmail.com>
2023-05-27 22:15:16 +0800

    feat: admin update restaurant

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
 const flash = require('connect-flash')
+const methodOverride = require('method-override')
 const session = require('express-session')
 const passport = require('./config/passport')
 const handlebarsHelpers = require('./helpers/handlebars-helpers')
@@ -25,6 +26,7 @@ app.use(passport.initialize())
 // 啟動 session 功能
 app.use(passport.session())
 app.use(flash()) // 掛載套件
+app.use(methodOverride('_method'))
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages') // 設定 success_msg 訊息
   res.locals.error_messages = req.flash('error_messages') // 設定 warning_msg 訊息
