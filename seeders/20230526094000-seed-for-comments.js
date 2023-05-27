716f6c3a6229251519e5277da588d14e331c115f
jiasyuanchu <jiasyuanchu@gmail.com>
2023-05-27 23:45:45 +0800

    fix: seeder error fixed

--- seeders/20230526094000-seed-for-comments.js
+++ seeders/20230526094000-seed-for-comments.js
@@ -3,23 +3,23 @@ const faker = require('faker')
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    const maxTextLength = 50
-    // 先查詢資料庫並獲取 USERS 表中的所有 id
+    const maxTextLength = 80
     const users = await queryInterface.sequelize.query(
-      'SELECT id FROM USERS;',
+      'SELECT id FROM Users',
       { type: queryInterface.sequelize.QueryTypes.SELECT }
     )
-    // 先查詢資料庫並獲取 RESTAURANTS 表中的所有 id
     const restaurants = await queryInterface.sequelize.query(
-      'SELECT id FROM RESTAURANTS;',
+      'SELECT id FROM Restaurants',
       { type: queryInterface.sequelize.QueryTypes.SELECT }
     )
+
     await queryInterface.bulkInsert(
       'Comments',
-      Array.from({ length: 20 }, () => ({
+      Array.from({ length: 40 }, () => ({
         text: faker.lorem.text().slice(0, maxTextLength),
         created_at: new Date(),
         updated_at: new Date(),
+        // 考量到雲端的database, 產生資料時id可能會跳號
         user_id: users[Math.floor(Math.random() * (users.length))].id,
         restaurant_id:
           restaurants[Math.floor(Math.random() * (restaurants.length))].id
