c57e51206a9ae02fcc4721e0c6efcc07c5f5db9f
av124773 <av124773@gmail.com>
2023-05-27 22:25:08 +0800

    feat: add user profile page and edit page and putUser feature

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,13 +1,14 @@
 const bcrypt = require('bcryptjs')
 const db = require('../models')
 const { User } = db
+const { localFileHandler } = require('../helpers/file-helpers')
 const userController = {
   signUpPage: (req, res) => {
     res.render('signup')
   },
   signUp: (req, res, next) => {
     if (req.body.password !== req.body.passwordCheck) throw new Error('Passwords do not match!')
-    User.findOne({ where: { email: req.body.email } })
+    return User.findOne({ where: { email: req.body.email } })
       .then(user => {
         if (user) throw new Error('Email alread exists!')
         return bcrypt.hash(req.body.password, 10)
@@ -34,6 +35,49 @@ const userController = {
     req.flash('success_messages', '登出成功!')
     req.logout()
     res.redirect('/signin')
+  },
+  getUser: (req, res, next) => {
+    const userId = req.params.id
+    return User.findByPk(userId, {
+      raw: true
+    })
+      .then(user => {
+        if (!user) throw new Error("User didn't exists!")
+        return res.render('users/profile', { user })
+      })
+      .catch(err => next(err))
+  },
+  editUser: (req, res, next) => {
+    const userId = req.params.id
+    return User.findByPk(userId, {
+      raw: true
+    })
+      .then(user => {
+        if (!user) throw new Error("User didn't exists!")
+        return res.render('users/edit', { user })
+      })
+      .catch(err => next(err))
+  },
+  putUser: (req, res, next) => {
+    const userId = req.params.id
+    const { name } = req.body
+    const { file } = req
+    return Promise.all([
+      User.findByPk(userId),
+      localFileHandler(file)
+    ])
+      .then(([user, filePath]) => {
+        if (!user) throw new Error("User didn't exists!")
+        return user.update({
+          name,
+          image: filePath || user.image
+        })
+      })
+      .then(() => {
+        req.flash('success_messages', '使用者資料編輯成功')
+        res.redirect(`/users/${userId}`)
+      })
+      .catch(err => next(err))
   }
 }
 module.exports = userController
