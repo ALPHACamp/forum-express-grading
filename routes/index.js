15c64a0d9dd9b9dfedc9c160b9ca109693596364
Pikapikapi <huang.paladin@gmail.com>
2023-05-27 14:43:28 +0800

    feat: flash msg & verification

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,7 @@ const router = express.Router()
 // 新增，載入 controller
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
+const { generalErrorHandler } = require('../middleware/error-handler')
 const admin = require('./modules/admin')
 
 // 刪除
@@ -17,6 +18,8 @@ router.use('/admin', admin)
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
 router.get('/restaurants', restController.getRestaurants)
-router.use('/', (req, res) => res.redirect('/restaurants'))
+router.get('/', (req, res) => res.redirect('/restaurants'))
+router.use('/', generalErrorHandler)
+module.exports = router
 
 module.exports = router
