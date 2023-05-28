d1ec6a8132e7ab4af71abeda56bcce959eba0446
arubakingpikachu <85471527+arubakingpikachu@users.noreply.github.com>
2023-05-28 20:59:12 +0800

    feat: add favorite/unfavorite button at index page

--- models/restaurant.js
+++ models/restaurant.js
@@ -12,6 +12,12 @@ module.exports = (sequelize, DataTypes) => {
     static associate (models) {
       Restaurant.belongsTo(models.Category, { foreignKey: 'categoryId' })
       Restaurant.hasMany(models.Comment, { foreignKey: 'restaurantId' })
+
+      Restaurant.belongsToMany(models.User, {
+        through: models.Favorite, // 透過 Favorite 表來建立關聯
+        foreignKey: 'restaurantId', // 對 Favorite 表設定 FK
+        as: 'FavoritedUsers' // 幫這個關聯取個名稱，之後還有其他需求，例如想打造 Like/Unlike 功能時，就可以用同樣方法來建立不同的關係名稱
+      })
     }
   };
   Restaurant.init({
