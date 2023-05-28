32e7a4faa846614e75e9329fb6c3bd06be495ed0
Pikapikapi <huang.paladin@gmail.com>
2023-05-28 15:05:10 +0800

    feat:add imgur api

--- helpers/file-helpers.js
+++ helpers/file-helpers.js
@@ -1,4 +1,5 @@
 const fs = require('fs') // 引入 fs 模組
+const imgur = require('imgur')
 const localFileHandler = file => {
   // file 是 multer 處理完的檔案
   return new Promise((resolve, reject) => {
@@ -11,6 +12,18 @@ const localFileHandler = file => {
       .catch(err => reject(err))
   })
 }
+const imgurFileHandler = file => {
+  return new Promise((resolve, reject) => {
+    if (!file) return resolve(null)
+    return imgur
+      .uploadFile(file.path)
+      .then(img => {
+        resolve(img?.link || null) // 檢查 img 是否存在
+      })
+      .catch(err => reject(err))
+  })
+}
 module.exports = {
-  localFileHandler
+  localFileHandler,
+  imgurFileHandler
 }
