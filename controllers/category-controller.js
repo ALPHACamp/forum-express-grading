d19798c3d57d64637446fc5eac5a5467dca57f3f
Ray625 <wdvgy32@gmail.com>
2023-05-29 00:08:21 +0800

    feat: category create

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -9,6 +9,16 @@ const categoryController = {
         res.render('admin/categories', { categories })
       })
       .catch(err => next(err))
+  },
+  postCategory: (req, res, next) => {
+    const { name } = req.body
+    if (!name) throw new Error('Category name is required!')
+    return Category.create({ name })
+      .then(() => {
+        req.flash('success_messages', '成功新增餐廳類別')
+        res.redirect('/admin/categories')
+      })
+      .catch(err => next(err))
   }
 }
 
