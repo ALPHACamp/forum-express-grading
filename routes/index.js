fb4a17163d662e50c34247f491f3a144ee2ff947
ahung888 <yunhung2000@gmail.com>
2023-05-28 18:47:34 +0800

    test route

--- routes/index.js
+++ routes/index.js
@@ -1,56 +1,77 @@
 const express = require('express')
-const router = express.Router()
+const { generalErrorHandler } = require('../middleware/error-handler')
+const { authenticated, authenticatedAdmin } = require('../middleware/auth')
 const passport = require('../config/passport')
-const admin = require('./modules/admin')
+const router = express.Router()
+const upload = require('../middleware/multer')
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
 const commentController = require('../controllers/comment-controller')
+const admin = require('./modules/admin')
 
-const { authenticated, authenticatedAdmin } = require('../middleware/auth')
-const { generalErrorHandler } = require('../middleware/error-handler')
-const upload = require('../middleware/multer')
-
+// admin
 router.use('/admin', authenticatedAdmin, admin)
+
+// user signup
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
+
+// user signin
 router.get('/signin', userController.signInPage)
-router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
+router.post('/signin', passport.authenticate('local', {
+  failureRedirect: '/signin',
+  failureFlash: true
+}), userController.signIn)
+
+// user logout
 router.get('/logout', userController.logout)
 
-// feeds
-router.get('/restaurants/feeds', authenticated, restController.getFeeds)
-
-// top restaurant
+// restaurant top
 router.get('/restaurants/top', authenticated, restController.getTopRestaurants)
 
-// restaurant
-router.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard)
+// restaurant feeds
+router.get('/restaurants/feeds', authenticated, restController.getFeeds)
+
+// restaurant detail
 router.get('/restaurants/:id', authenticated, restController.getRestaurant)
+
+// restaurant dashboard
+router.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard)
+
+// restaurant index
 router.get('/restaurants', authenticated, restController.getRestaurants)
 
-// comments
-router.delete('/comments/:id', authenticatedAdmin, commentController.deleteComment)
+// comment create
 router.post('/comments', authenticated, commentController.postComment)
 
-// favorite
+// comment delete
+router.delete('/comments/:id', authenticated, commentController.deleteComment)
+
+// favorite create & delete
 router.post('/favorite/:restaurantId', authenticated, userController.addFavorite)
 router.delete('/favorite/:restaurantId', authenticated, userController.removeFavorite)
 
-// like
+// like create & delete
 router.post('/like/:restaurantId', authenticated, userController.addLike)
 router.delete('/like/:restaurantId', authenticated, userController.removeLike)
 
-// follow
+// following create & delete
 router.post('/following/:userId', authenticated, userController.addFollowing)
 router.delete('/following/:userId', authenticated, userController.removeFollowing)
 
-// users
+// user top-users
 router.get('/users/top', authenticated, userController.getTopUsers)
-router.get('/users/:id/edit', authenticated, userController.editUser)
+
+// user profile
 router.get('/users/:id', authenticated, userController.getUser)
+
+// user profile edit
+router.get('/users/:id/edit', authenticated, userController.editUser)
 router.put('/users/:id', authenticated, upload.single('image'), userController.putUser)
 
 router.get('/', (req, res) => res.redirect('/restaurants'))
+
+// error
 router.use('/', generalErrorHandler)
 
 module.exports = router
