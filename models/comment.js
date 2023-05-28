99106dffcca8a7008f824fc50b5dbdb098c80fd3
Lanways <mcbluedd@gmail.com>
2023-05-28 19:03:33 +0800

    feat: modify comment model

--- models/comment.js
+++ models/comment.js
@@ -21,7 +21,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'Comment',
-    tablueName: 'Comments',
+    tableName: 'Comments',
     underscored: true,
   });
   return Comment;
