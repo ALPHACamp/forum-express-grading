5a2da58315666e0c1aedcce4c367d508fff8b954
Ray625 <wdvgy32@gmail.com>
2023-05-29 06:18:06 +0800

    feat: category delete

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -1,4 +1,4 @@
-const { Category } = require('../models')
+const { Category, Restaurant } = require('../models')
 
 const categoryController = {
   getCategories: (req, res, next) => {
@@ -31,6 +31,30 @@ const categoryController = {
       })
       .then(() => res.redirect('/admin/categories'))
       .catch(err => next(err))
+  },
+  deleteCategory: async (req, res, next) => {
+    // 進階刪除
+    try {
+      const category = await Category.findByPk(req.params.id, {
+        nest: true,
+        include: [Restaurant]
+      })
+      if (!category) throw new Error("Category didn't exist!")
+      // 找出屬於此分類的餐廳有哪些, 並把id整合成新array例如 : [1, 15 , 23 , 25]
+      const changeRestaurantId = category.Restaurants.map(rest => rest.toJSON().id)
+      // 找出'未分類'的id並把這些餐廳的categoryId改成該id
+      const uncategorized = await Category.findOne({ where: { name: '未分類' } }, { raw: true })
+      if (req.params.id.toString() === uncategorized.id.toString()) throw new Error('未分類為預設欄位，無法刪除!')
+      await Restaurant.update({ categoryId: uncategorized.id }, { where: { id: changeRestaurantId } })
+
+      // 改完之後再刪除分類
+      await category.destroy()
+
+      req.flash('success_messages', 'Category was successfully deleted')
+      res.redirect('/admin/categories')
+    } catch (err) {
+      next(err)
+    }
   }
 }
 
--- seeders/20230528083403-categories-seed-file.js
+++ seeders/20230528083403-categories-seed-file.js
@@ -2,7 +2,7 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('Categories',
-      ['中式料理', '日本料理', '義大利料理', '墨西哥料理', '素食料理', '美式料理', '複合式料理']
+      ['未分類', '中式料理', '日本料理', '義大利料理', '墨西哥料理', '素食料理', '美式料理', '複合式料理']
         .map(item => {
           return {
             name: item,
