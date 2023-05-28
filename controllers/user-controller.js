3f492af8c9957b078e85b9357078ebc36a5191bd
av124773 <av124773@gmail.com>
2023-05-28 15:39:38 +0800

    feat: add favorite/unfavorite button at index page

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,7 +1,8 @@
 const bcrypt = require('bcryptjs')
 const db = require('../models')
-const { Restaurant, Comment, User } = db
+const { Restaurant, Comment, User, Favorite } = db
 const { imgurFileHandler } = require('../helpers/file-helpers')
+const restaurant = require('../models/restaurant')
 const userController = {
   signUpPage: (req, res) => {
     res.render('signup')
@@ -87,6 +88,42 @@ const userController = {
         req.flash('success_messages', '使用者資料編輯成功')
         res.redirect(`/users/${userId}`)
       })
+      .catch(err => next(err))
+  },
+  addFavorite: (req, res, next) => {
+    const { restaurantId } = req.params
+    return Promise.all([
+      Restaurant.findByPk(restaurantId),
+      Favorite.findOne({
+        where: {
+          userId: req.user.id,
+          restaurantId
+        }
+      })
+    ])
+      .then(([restaurant, favorite]) => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        if (favorite) throw new Error('You have favorited this restaurant!')
+        return Favorite.create({
+          userId: req.user.id,
+          restaurantId
+        })
+      })
+      .then(() => res.redirect('back'))
+      .catch(err => next(err))
+  },
+  removeFavorite: (req, res, next) => {
+    return Favorite.findOne({
+      where: {
+        userId: req.user.id,
+        restaurantId: req.params.restaurantId
+      }
+    })
+      .then(favorite => {
+        if (!favorite) throw new Error("You haven't favorited this restaurant")
+        return favorite.destroy()
+      })
+      .then(() => res.redirect('back'))
       .catch(err => next(err))
   }
 }
