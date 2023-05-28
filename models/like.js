4ec2b58af9003060e318ecfd3cce46ac5f022954
Lanways <mcbluedd@gmail.com>
2023-05-28 19:20:21 +0800

    feat: add like/unlike feature

--- models/like.js
+++ models/like.js
@@ -3,26 +3,24 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Comment extends Model {
+  class Like extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      Comment.belongsTo(models.Restaurant, { foreignKey: 'restaurantId' })
-      Comment.belongsTo(models.User, { foreignKey: 'userId' })
+      // define association here
     }
   };
-  Comment.init({
-    text: DataTypes.STRING,
+  Like.init({
     userId: DataTypes.INTEGER,
     restaurantId: DataTypes.INTEGER
   }, {
     sequelize,
-    modelName: 'Comment',
-    tableName: 'Comments',
+    modelName: 'Like',
+    tableName: 'Likes',
     underscored: true,
   });
-  return Comment;
+  return Like;
 };
