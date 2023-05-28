c363d5087e3db8bf04a9dd0a61a7ed88f79d8323
sherryylin0719 <sherryylin0719@gmail.com>
2023-05-28 11:16:02 +0900

    feat: add post comment on restaurant page

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const passport = require('../config/passport') // 引入 Passport，需要他幫忙做驗證
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
+const commentController = require('../controllers/comment-controller')
 const { authenticated, authenticatedAdmin } = require('../middleware/auth')
 const { generalErrorHandler } = require('../middleware/error-handler')
 const admin = require('./modules/admin')
@@ -17,6 +18,8 @@ router.post('/signin', passport.authenticate('local', { failureRedirect: '/signi
 router.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard)
 router.get('/restaurants/:id', authenticated, restController.getRestaurant)
 router.get('/restaurants', authenticated, restController.getRestaurants)
+
+router.post('/comments', authenticated, commentController.postComment)
 
 router.get('/logout', userController.logout)
 
