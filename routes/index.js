a3cde19f0db891f22b13d8442c378a3e9209d54e
HuangYiLun <wk5018102@gmail.com>
2023-05-28 23:22:37 +0800

    feat: add top 10 restaurants page

--- routes/index.js
+++ routes/index.js
@@ -19,6 +19,7 @@ router.post('/signin', passport.authenticate('local', { failureRedirect: '/signi
 
 router.get('/logout', userController.logOut)
 
+router.get('/restaurants/top', authenticated, restController.getTopRestaurants)
 router.get('/restaurants/feeds', authenticated, restController.getFeeds)
 router.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard)
 router.get('/restaurants/:id', authenticated, restController.getRestaurant)
