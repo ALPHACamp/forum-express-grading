7f91e03b8506a321f2177eae7598d96eb52c2cc5
qpooqpooqp <kkk3kkk3kkk3@yahoo.com.tw>
2023-05-30 10:34:54 +0800

    feat: add getRestaurants api

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const session = require('express-session')
 const passport = require('./config/passport')
 const handlebarsHelpers = require('./helpers/handlebars-helpers')
 const { getUser } = require('./helpers/auth-helpers')
-const { pages } = require('./routes')
+const { pages, apis } = require('./routes')
 const { parseArgs } = require('util')
 const app = express()
 const port = process.env.PORT || 3000
@@ -27,6 +27,7 @@ app.use((req, res, next) => {
   res.locals.user = req.user
   next()
 })
+app.use('/api', apis)
 app.use(pages)
 app.listen(port, () => {
   console.info(`Example app listening on port ${port}!`)
