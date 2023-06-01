4242e43a362bfd5ca9002aae46b0abcc594dd018
yiyun <ccjhs70325@gmail.com>
2023-06-01 20:49:29 +0800

    feat: category create

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -15,6 +15,7 @@ router.get('/users', adminController.getUsers)
 // patchUser
 router.patch('/users/:id', adminController.patchUser)
 router.get('/categories', categoryController.getCategories)
+router.post('/categories', categoryController.postCategory)
 router.post('/restaurants', upload.single('image'), adminController.postRestaurant)
 router.get('/', (req, res) => res.redirect('/admin/restaurants'))
 
