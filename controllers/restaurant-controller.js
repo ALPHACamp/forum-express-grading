866da0cfa9c7c3ad4436c18fd8c713e34eebc797
yiyun <ccjhs70325@gmail.com>
2023-06-03 15:15:31 +0800

    feat: add increment viewCounts function

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -17,13 +17,14 @@ const restaurantController = {
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
-      raw: true,
-      nest: true,
       include: Category
     })
       .then(restaurant => {
+        return restaurant.increment('view_counts')
+      })
+      .then(restaurant => {
         if (!restaurant) throw new Error("Restaurant didn't exist.")
-        res.render('restaurant', { restaurant })
+        res.render('restaurant', { restaurant: restaurant.toJSON() })
       })
       .catch(err => next(err))
   },
