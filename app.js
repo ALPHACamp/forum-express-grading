f610ce10090a72f486d07c1b0ff17dacff287663
qpooqpooqp <kkk3kkk3kkk3@yahoo.com.tw>
2023-06-06 09:52:14 +0800

    feat: add api-auth middleware

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+if (process.env.NODE_ENV !== 'production') { require('dotenv').config() }
 const path = require('path')
 const express = require('express')
 const handlebars = require('express-handlebars')
@@ -8,8 +9,6 @@ const passport = require('./config/passport')
 const handlebarsHelpers = require('./helpers/handlebars-helpers')
 const { getUser } = require('./helpers/auth-helpers')
 const { pages, apis } = require('./routes')
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config()}
 const { parseArgs } = require('util')
 const app = express()
 const port = process.env.PORT || 3000
--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,27 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
+const passportJWT = require('passport-jwt')
 const bcrypt = require('bcryptjs')
 const { User, Restaurant } = require('../models')
+const JWTStrategy = passportJWT.Strategy
+const ExtractJWT = passportJWT.ExtractJwt
+const jwtOptions = {
+  jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+  secretOrKey: process.env.JWT_SECRET
+}
+passport.use(new JWTStrategy(jwtOptions, (jwtPayload, cb) => {
+  User.findByPk(jwtPayload.id, {
+    include: [
+      { model: Restaurant, as: 'FavoritedRestaurants' },
+      // { model: Restaurant, as: 'LikedRestaurants' },
+      { model: User, as: 'Followers' },
+      { model: User, as: 'Followings' }
+    ]
+  })
+    .then(user => cb(null, user))
+    .catch(err => cb(err))
+}))
+
 // set up Passport strategy
 passport.use(new LocalStrategy(
   // customize user field
