9e20dd849dad2a65b2e7972ee8e4bbd715bb2b86
Sean Chen <seanchen112@gmail.com>
2023-06-14 15:11:51 +0800

    feat: flash msg & signup verification

--- routes/index.js
+++ routes/index.js
@@ -3,10 +3,14 @@ const router = express.Router()
 const admin = require('./modules/admin')
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller') // 新增這行
+const { generalErrorHandler } = require('../middleware/error-handler') // 加入這行
+
 router.use('/admin', admin)
 // 新增下面這兩行，注意順序
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp) // 注意用 post
 router.get('/restaurants', restController.getRestaurants)
 router.get('/', (req, res) => res.redirect('/restaurants'))
+router.use('/', generalErrorHandler) // 加入這行
+
 module.exports = router
