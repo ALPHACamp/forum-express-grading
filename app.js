50d40778232f5dadfa660381b10f842e57d8a8db
Sean Chen <seanchen112@gmail.com>
2023-06-15 10:26:37 +0800

    feat: add header & footer

--- app.js
+++ app.js
@@ -7,8 +7,10 @@ const passport = require('./config/passport')
 const app = express()
 const port = process.env.PORT || 3000
 const SESSION_SECRET = 'secret' // 新增這行
+const { getUser } = require('./helpers/auth-helpers') // 增加這行，引入自定義的 auth-helpers
+const handlebarsHelpers = require('./helpers/handlebars-helpers') // 引入 handlebars-helpers
 
-app.engine('hbs', handlebars({ extname: '.hbs' }))
+app.engine('hbs', handlebars({ extname: '.hbs', helpers: handlebarsHelpers }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true })) // 加入這行
 app.use(
@@ -20,6 +22,8 @@ app.use(flash()) // 掛載套件
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages') // 設定 success_msg 訊息
   res.locals.error_messages = req.flash('error_messages') // 設定 warning_msg 訊息
+  res.locals.user = getUser(req) // 增加這行
+
   next()
 })
 app.use(routes)
