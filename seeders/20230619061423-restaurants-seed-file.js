9048f9d2528d9d5f43d7b845296a0f665d4d50f9
Sean Chen <seanchen112@gmail.com>
2023-06-20 15:52:45 +0800

    fix: eslint

--- seeders/20230619061423-restaurants-seed-file.js
+++ seeders/20230619061423-restaurants-seed-file.js
@@ -1,24 +1,24 @@
-"use strict";
-const faker = require("faker");
+'use strict'
+const faker = require('faker')
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert(
-      "Restaurants",
+      'Restaurants',
       Array.from({ length: 50 }, () => ({
         name: faker.name.findName(),
         tel: faker.phone.phoneNumber(),
         address: faker.address.streetAddress(),
-        opening_hours: "08:00",
+        opening_hours: '08:00',
         image: `https://loremflickr.com/320/240/restaurant,food/?random=${
           Math.random() * 100
         }`,
         description: faker.lorem.text(),
         created_at: new Date(),
-        updated_at: new Date(),
+        updated_at: new Date()
       }))
-    );
+    )
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.bulkDelete("Restaurants", {});
-  },
-};
+    await queryInterface.bulkDelete('Restaurants', {})
+  }
+}
