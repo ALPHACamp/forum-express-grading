1d974aaa0c9e8b43bcf6eff85d0fa942b185c14d
Melodystart <start99start@gmail.com>
2023-06-22 13:29:04 +0800

    feat: modify admin restaurants page

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,6 +1,12 @@
+const { Restaurant } = require('../models') // 新增這裡
+
 const adminController = {
-  getRestaurants: (req, res) => {
-    return res.render('admin/restaurants')
+  getRestaurants: (req, res, next) => {
+    Restaurant.findAll({
+      raw: true // 把 Sequelize的一大包物件轉換成格式較單純JS 原生物件
+    })
+      .then(restaurants => res.render('admin/restaurants', { restaurants }))
+      .catch(err => next(err))
   }
 }
 module.exports = adminController
