c1fe451bff9c47331933d72e1eeb2309f1bcf1c3
Melodystart <start99start@gmail.com>
2023-06-24 00:15:58 +0800

    feat: add ifCond hbs helper & update category selector

--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -1,4 +1,8 @@
 const dayjs = require('dayjs') // 載入 dayjs 套件
 module.exports = {
-  currentYear: () => dayjs().year() // 取得當年年份作為 currentYear
+  currentYear: () => dayjs().year(), // 取得當年年份作為 currentYear
+
+  ifCond: function (a, b, options) {
+    return a === b ? options.fn(this) : options.inverse(this)
+  }
 }
