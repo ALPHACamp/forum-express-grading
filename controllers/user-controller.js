c075117a6affbf00179667fd43eb8f237932f928
user <carola1408@yahoo.com.tw>
2023-06-23 23:28:27 +0800

    feat: add passport init & signin

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -6,6 +6,7 @@ const userController = {
   signUpPage: (req, res) => {
     res.render('signup')
   },
+
   // 負責實際處理註冊的行為
   signUp: (req, res, next) => {
     // 如果兩次輸入的密碼不同，就建立一個 Error 物件並拋出
@@ -27,6 +28,18 @@ const userController = {
         res.redirect('/signin')
       })
       .catch(err => next(err)) // 接住前面拋出的錯誤，呼叫專門做錯誤處理的 middleware
+  },
+  signInPage: (req, res) => {
+    res.render('signin')
+  },
+  signIn: (req, res) => {
+    req.flash('success_messages', '成功登入！')
+    res.redirect('/restaurants')
+  },
+  logout: (req, res) => {
+    req.flash('success_messages', '登出成功！')
+    req.logout()
+    res.redirect('/signin')
   }
 }
 module.exports = userController
