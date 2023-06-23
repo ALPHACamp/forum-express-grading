6c02a6f09baf5501cdec20098a7d8150bc84beb6
Melodystart <start99start@gmail.com>
2023-06-23 13:48:52 +0800

    feat:add revise admin-controller.js

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -2,7 +2,7 @@ const { Restaurant, User } = require('../models') // 新增這裡
 const { imgurFileHandler } = require('../helpers/file-helpers')
 const adminController = {
   getRestaurants: (req, res, next) => {
-    Restaurant.findAll({
+    return Restaurant.findAll({
       raw: true // 把 Sequelize的一大包物件轉換成格式較單純JS 原生物件
     })
       .then(restaurants => res.render('admin/restaurants', { restaurants }))
@@ -15,7 +15,7 @@ const adminController = {
     const { name, tel, address, openingHours, description } = req.body
     if (!name) throw new Error('Restaurant name is required!')
     const { file } = req // 把檔案取出來，也可寫 const file = req.file
-    imgurFileHandler(file) // 把取出的檔案傳給 file-helper 處理後
+    return imgurFileHandler(file) // 把取出的檔案傳給 file-helper 處理後
       .then(filePath =>
         Restaurant.create({ // 產生一個新的 Restaurant 物件實例，並存入資料庫
           name,
@@ -32,7 +32,7 @@ const adminController = {
       .catch(err => next(err))
   },
   getRestaurant: (req, res, next) => {
-    Restaurant.findByPk(req.params.id, {
+    return Restaurant.findByPk(req.params.id, {
       raw: true // 把從資料庫傳來的資料轉換成 JS 原生物件
     })
       .then(restaurant => {
@@ -42,7 +42,7 @@ const adminController = {
       .catch(err => next(err))
   },
   editRestaurant: (req, res, next) => {
-    Restaurant.findByPk(req.params.id, {
+    return Restaurant.findByPk(req.params.id, {
       raw: true
     })
       .then(restaurant => {
@@ -86,23 +86,26 @@ const adminController = {
       .catch(err => next(err))
   },
   getUsers: (req, res, next) => {
-    User.findAll({
+    return User.findAll({
       raw: true // 把 Sequelize的一大包物件轉換成格式較單純JS 原生物件
     })
       .then(users => res.render('admin/users', { users }))
       .catch(err => next(err))
   },
   patchUser: (req, res, next) => {
-    User.findByPk(req.params.id)
+    return User.findByPk(req.params.id)
       .then(user => {
         if (!user) throw new Error("User didn't exist!")
-        if (user.name === 'root') throw new Error('禁止變更 root 權限')
+        if (user.email === 'root@example.com') {
+          req.flash('error_messages', '禁止變更 root 權限')
+          return res.redirect('back')
+        }
         return user.update({ // 除root外，點選此連結將調整user/admin狀態
           isAdmin: !user.isAdmin
         })
       })
       .then(() => {
-        req.flash('success_messages', 'user was successfully to update')
+        req.flash('success_messages', '使用者權限變更成功')
         res.redirect('/admin/users')
       })
       .catch(err => next(err))
