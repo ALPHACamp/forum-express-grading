cb225e4b8cceade51f55419fdae9028bed7395e5
user <carola1408@yahoo.com.tw>
2023-06-28 22:45:09 +0800

    feat: add admin-tabs.hbs

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const path = require('path')
 const app = express()
 const port = process.env.PORT || 3000
 const SESSION_SECRET = 'secret'
-const db = require('./models')
+
 // 註冊 Handlebars 樣板引擎，並指定副檔名為 .hbs
 app.engine('hbs', handlebars({ extname: '.hbs', helpers: handlebarsHelpers }))
 // 設定使用 Handlebars 做為樣板引擎
