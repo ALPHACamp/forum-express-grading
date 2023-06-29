b40251163d4d6a662ed87c8fd6176242a3fff61e
TCY1020 <a25023423@gmail.com>
2023-06-30 01:51:43 +0800

    feat:add index page

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.set('view engine', 'hbs')
 app.use(routes)
 
 app.listen(port, () => {
-  console.info(`Example app listening on port ${port}!`)
+  console.info(`Example app listening on http://localhost:${port}/`)
 })
 
 module.exports = app
