660bfb8b43d3a2ca077529dc9e118a17b39f5f40
LAI-Recycle <xfjioyhnplus@gmail.com>
2023-07-01 17:36:30 +0800

    feat: create restaurant

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,7 +1,8 @@
 const express = require('express')
-const { authenticatedAdmin } = require('../../middleware/auth')
 const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
-router.get('/restaurants', authenticatedAdmin, adminController.getRestaurants)
+router.get('/restaurants/create', adminController.createRestaurant)
+router.get('/restaurants', adminController.getRestaurants)
+router.post('/restaurants', adminController.postRestaurant)
 router.use('/', (req, res) => res.redirect('/admin/restaurants'))
 module.exports = router
