406b2c74f72a45884e0d5c23e0bc7bfece0e80ff
user <carola1408@yahoo.com.tw>
2023-07-01 22:54:08 +0800

    feat: category create

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -6,6 +6,14 @@ const categoryController = {
     })
       .then(categories => res.render('admin/categories', { categories }))
       .catch(err => next(err))
+  },
+  // 先檢查 name 是否為空，如果是空的話就回傳錯誤訊息，否則就建立一個新的分類並且導回瀏覽分類頁面。
+  postCategory: (req, res, next) => {
+    const { name } = req.body
+    if (!name) throw new Error('Category name is required!')
+    return Category.create({ name })
+      .then(() => res.redirect('/admin/categories'))
+      .catch(err => next(err))
   }
 }
 module.exports = categoryController
