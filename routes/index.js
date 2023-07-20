37b415dfece852063e7d1eadb20700f00a5dc76f
DannyHucc <dannyhu10011101@gmail.com>
2023-07-20 10:00:43 +0800

    feat: user model add is_admin

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
+const { authenticated } = require('../middleware/auth')
 const { generalErrorHandler } = require('../middleware/error-handler')
 const passport = require('../config/passport')
 const admin = require('./modules/admin')
@@ -10,7 +11,6 @@ router.use('/admin', admin)
 
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
-
 router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', {
   failureRedirect: '/signin',
@@ -18,8 +18,7 @@ router.post('/signin', passport.authenticate('local', {
 }), userController.signIn)
 router.get('/logout', userController.logout)
 
-router.get('/restaurants', restController.getRestaurants)
-
+router.get('/restaurants', authenticated, restController.getRestaurants)
 router.use('/', generalErrorHandler)
 
 module.exports = router
