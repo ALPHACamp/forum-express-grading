1a10f9eb2e215501d8972a3a967f116914c09b34
DannyHucc <dannyhu10011101@gmail.com>
2023-07-24 10:54:51 +0800

    feat: add user permission management

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,4 +1,4 @@
-const { Restaurant } = require('../models')
+const { Restaurant, User } = require('../models')
 const { imgurFileHandler } = require('../helpers/file-helpers')
 
 const adminController = {
@@ -10,6 +10,7 @@ const adminController = {
       return next(error)
     }
   },
+
   createRestaurant: async (req, res, next) => {
     try {
       return res.render('admin/create-restaurant')
@@ -17,6 +18,7 @@ const adminController = {
       return next(error)
     }
   },
+
   postRestaurant: async (req, res, next) => {
     try {
       const { name, tel, address, openingHours, description } = req.body
@@ -41,6 +43,7 @@ const adminController = {
       return next(error)
     }
   },
+
   getRestaurant: async (req, res, next) => {
     try {
       const restaurant = await Restaurant.findByPk(req.params.id, { raw: true })
@@ -51,6 +54,7 @@ const adminController = {
       return next(error)
     }
   },
+
   editRestaurant: async (req, res, next) => {
     try {
       const restaurant = await Restaurant.findByPk(req.params.id, { raw: true })
@@ -61,6 +65,7 @@ const adminController = {
       return next(error)
     }
   },
+
   putRestaurant: async (req, res, next) => {
     try {
       const { name, tel, address, openingHours, description } = req.body
@@ -92,6 +97,7 @@ const adminController = {
       return next(error)
     }
   },
+
   deleteRestaurant: async (req, res, next) => {
     try {
       const restaurant = await Restaurant.findByPk(req.params.id)
@@ -99,6 +105,34 @@ const adminController = {
 
       await restaurant.destroy()
       return res.redirect('/admin/restaurants')
+    } catch (error) {
+      return next(error)
+    }
+  },
+
+  getUsers: async (req, res, next) => {
+    try {
+      const users = await User.findAll({ raw: true, nest: true })
+      return res.render('admin/users', { users })
+    } catch (error) {
+      return next(error)
+    }
+  },
+
+  patchUser: async (req, res, next) => {
+    try {
+      const { id } = req.params
+
+      const user = await User.findByPk(id)
+      if (!user) throw new Error("User didn't exist!")
+      if (user.email === 'root@example.com') {
+        req.flash('error_messages', '禁止變更 root 權限')
+        return res.redirect('back')
+      }
+
+      await user.update({ isAdmin: !user.isAdmin })
+      req.flash('success_messages', '使用者權限變更成功')
+      return res.redirect('/admin/users')
     } catch (error) {
       return next(error)
     }
