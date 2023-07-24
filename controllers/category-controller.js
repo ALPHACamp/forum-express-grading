c3311a9e14bb6a381f60f881b73d96c72454fc7f
DannyHucc <dannyhu10011101@gmail.com>
2023-07-24 16:42:31 +0800

    feat: add admin categories page

---
+++ controllers/category-controller.js
@@ -0,0 +1,14 @@
+const { Category } = require('../models')
+
+const categoryController = {
+  getCategories: async (req, res, next) => {
+    try {
+      const categories = await Category.findAll({ raw: true })
+      return res.render('admin/categories', { categories })
+    } catch (error) {
+      return next(error)
+    }
+  }
+}
+
+module.exports = categoryController
