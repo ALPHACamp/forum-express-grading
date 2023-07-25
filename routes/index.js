0b591501af6afae6eddbfbc969db2bc989584ec9
Chiang Jou <tiffanyj.chiang@gmail.com>
2023-07-25 17:12:30 +0800

    feat: user model add is_admin

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ const admin = require('./modules/admin')
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
 
+const { authenticated } = require('../middleware/auth')
 const { generalErrorHandler } = require('../middleware/error-handler')
 
 router.use('/admin', admin)
@@ -18,7 +19,7 @@ router.post('/signin', passport.authenticate('local', { failureRedirect: '/signi
 
 router.get('/logout', userController.logout)
 
-router.get('/restaurants', restController.getRestaurants)
+router.get('/restaurants', authenticated, restController.getRestaurants)
 router.use('/', (req, res) => res.redirect('/restaurants'))
 
 router.use('/', generalErrorHandler)
