46eb5cf4cac98940184c115e4a4b7d62b6bc46a4
DannyHucc <dannyhu10011101@gmail.com>
2023-07-25 10:47:37 +0800

    feat: category update

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -3,8 +3,14 @@ const { Category } = require('../models')
 const categoryController = {
   getCategories: async (req, res, next) => {
     try {
-      const categories = await Category.findAll({ raw: true })
-      return res.render('admin/categories', { categories })
+      const promiseData = await Promise.all([
+        Category.findAll({ raw: true }),
+        req.params.id ? Category.findByPk(req.params.id, { raw: true }) : null
+      ])
+      const categories = promiseData[0]
+      const category = promiseData[1]
+
+      return res.render('admin/categories', { categories, category })
     } catch (error) {
       return next(error)
     }
@@ -17,6 +23,21 @@ const categoryController = {
       if (!name) throw new Error('Category name is required!')
 
       await Category.create({ name })
+      return res.redirect('/admin/categories')
+    } catch (error) {
+      return next(error)
+    }
+  },
+
+  putCategory: async (req, res, next) => {
+    try {
+      const { name } = req.body
+
+      if (!name) throw new Error('Category name is required!')
+
+      const category = await Category.findByPk(req.params.id)
+      await category.update({ name })
+
       return res.redirect('/admin/categories')
     } catch (error) {
       return next(error)
