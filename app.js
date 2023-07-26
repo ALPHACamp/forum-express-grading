6ed789c9cd6ef2bebbbfdde1936ee0b98bfcf70d
DannyHucc <dannyhu10011101@gmail.com>
2023-07-26 16:13:00 +0800

    feat: modify parameter and add .env.exp

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const routes = require('./routes')
 const SESSION_SECRET = 'secret'
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 8080
 
 app.engine('hbs', hbs({
   extname: '.hbs',
