a7b2d55efdc9e57c46bf9dd5ba621dd0edb1b9d7
yang haotse <ha91252@gmail.com>
2023-07-26 18:05:38 +0800

    feat: admin restaurant page

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -2,10 +2,13 @@ const express = require('express')
 const router = express.Router()
 const adminController = require('../../controllers/admin-controllers')
 
-
+// 新增一筆資料
 router.get('/restaurants/create', adminController.createRestaurant)
+// 瀏覽一筆資料
+router.get('/restaurants/:id', adminController.getRestaurant)
+// 新增送出
 router.post('/restaurants', adminController.postRestaurant)
-
+// 瀏覽全部資料
 router.get('/restaurants', adminController.getRestaurants)
 
 // 注意這邊在教案中是router.use('',(req, res)...)
