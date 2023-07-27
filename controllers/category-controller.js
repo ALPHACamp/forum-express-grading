bf9ada3e80c51b277002f910009d5cc9491fffd8
TCY1020 <a25023423@gmail.com>
2023-07-27 19:49:49 +0800

    feat: delete Category Restaurant's category will be 尚未分類

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -1,4 +1,4 @@
-const { Category } = require('../models')
+const { Category, Restaurant } = require('../models')
 
 const categoryController = {
   getCategories: (req, res, next) => {
@@ -24,6 +24,10 @@ const categoryController = {
     return Category.findByPk(req.params.id)
       .then(category => {
         if (!category) throw new Error("Category didn't exist!")
+        if (category.name === '尚未分類') {
+          req.flash('error_messages', '禁止修改[尚未分類]')
+          return res.redirect('back')
+        }
         return category.update({ name })
       })
       .then(() => {
@@ -32,14 +36,23 @@ const categoryController = {
       })
       .catch(err => next(err))
   },
-  deleteCategories: (req, res, next) => {
-    return Category.findByPk(req.params.id)
-      .then(category => {
-        if (!category) throw new Error("Category didn't exist!")
-        return category.destroy()
-      })
-      .then(() => res.redirect('/admin/categories'))
-      .catch(err => next(err))
+  deleteCategories: async (req, res, next) => {
+    try {
+      const category = await Category.findByPk(req.params.id)
+      if (!category) throw new Error("Category didn't exist!")
+      if (category.name === '尚未分類') {
+        req.flash('error_messages', '禁止刪除[尚未分類]')
+        return res.redirect('back')
+      }
+      const uncategorized = await Category.findOne({ where: { name: '尚未分類' } })
+      const uncategorizedId = uncategorized.id
+      const categoryId = category.id
+      await Restaurant.update({ categoryId: uncategorizedId }, { where: { categoryId } })
+      await category.destroy()
+      res.redirect('/admin/categories')
+    } catch (err) {
+      next(err)
+    }
   }
 }
 
