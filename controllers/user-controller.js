535091dbc0f103e76c3df9375362de2a2a9bc0dd
DannyHucc <dannyhu10011101@gmail.com>
2023-07-28 22:11:20 +0800

    feat: merge --squash R03

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcryptjs')
 const db = require('../models')
-const { User } = db
+const { imgurFileHandler } = require('../helpers/file-helpers')
+const { User, Restaurant, Comment } = db
 
 const userController = {
   signUpPage: async (req, res, next) => {
@@ -10,6 +11,7 @@ const userController = {
       return next(error)
     }
   },
+
   signUp: async (req, res, next) => {
     try {
       if (req.body.password !== req.body.passwordCheck) throw new Error('Passwords do not match!')
@@ -29,6 +31,7 @@ const userController = {
       return next(error)
     }
   },
+
   signInPage: async (req, res, next) => {
     try {
       return res.render('signin')
@@ -36,6 +39,7 @@ const userController = {
       return next(error)
     }
   },
+
   signIn: async (req, res, next) => {
     try {
       req.flash('success_messages', '成功登入！')
@@ -44,11 +48,75 @@ const userController = {
       return next(error)
     }
   },
+
   logout: async (req, res, next) => {
     try {
       req.flash('success_messages', '登出成功！')
       req.logout()
       return res.redirect('/signin')
+    } catch (error) {
+      return next(error)
+    }
+  },
+
+  getUser: async (req, res, next) => {
+    try {
+      const promiseData = await Promise.all([
+        User.findByPk(req.params.id, { raw: true }),
+        Comment.findAll({
+          raw: true,
+          nest: true,
+          where: { userId: req.params.id },
+          include: Restaurant
+        })
+      ])
+      const user = promiseData[0]
+      const comments = promiseData[1]
+
+      if (!user) throw new Error("User doesn't exists!")
+
+      return res.render('users/profile', { user, comments })
+    } catch (error) {
+      return next(error)
+    }
+  },
+
+  editUser: async (req, res, next) => {
+    try {
+      const user = await User.findByPk(req.params.id, {
+        raw: true
+      })
+
+      if (!user) throw new Error("User doesn't exists!")
+
+      return res.render('users/edit', { user: user })
+    } catch (error) {
+      return next(error)
+    }
+  },
+
+  putUser: async (req, res, next) => {
+    try {
+      const { name } = req.body
+      const { file } = req
+      if (!name) throw new Error('User name is required!')
+
+      const promiseData = await Promise.all([
+        User.findByPk(req.params.id),
+        imgurFileHandler(file)
+      ])
+      const user = promiseData[0]
+      const filePath = promiseData[1]
+
+      if (!user) throw new Error("User doesn't exists!")
+
+      await user.update({
+        name,
+        image: filePath || user.image
+      })
+
+      req.flash('success_messages', '使用者資料編輯成功')
+      return res.redirect(`/users/${req.params.id}`)
     } catch (error) {
       return next(error)
     }
