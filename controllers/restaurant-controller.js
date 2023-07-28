6d0fdf3e4c6e359163571bd6b94c9be3b8693da7
TCY1020 <a25023423@gmail.com>
2023-07-28 12:26:24 +0800

    feat: add restaurant page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -10,11 +10,24 @@ const restaurantController = {
         ...r,
         description: r.description.substring(0, 50)
       }))
-      console.log(data)
       return res.render('restaurants', {
         restaurants: data
       })
     })
+  },
+  getRestaurant: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, {
+      include: Category, // 拿出關聯的 Category model
+      nest: true,
+      raw: true
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        res.render('restaurant', {
+          restaurant
+        })
+      })
+      .catch(err => next(err))
   }
 }
 module.exports = restaurantController
