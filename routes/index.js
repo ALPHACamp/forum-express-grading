16284ccedbd3ef0772878d3e2db574c651b27928
DannyHucc <dannyhu10011101@gmail.com>
2023-07-28 11:11:28 +0800

    feat: merge --squash R02

--- routes/index.js
+++ routes/index.js
@@ -22,6 +22,7 @@ router.get('/logout', userController.logout)
 
 router.get('/restaurants', authenticated, restController.getRestaurants)
 router.get('/restaurants/:id', authenticated, restController.getRestaurant)
+router.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard)
 
 router.use('/', (req, res) => res.redirect('/restaurants'))
 
