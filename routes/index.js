823b96ce55d7f57de184a81c45e9994f96f5c7ae
taiwania <taiwaniajusto@gmail.com>
2023-07-29 14:49:39 +0800

    Set error messages

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const router = express.Router()
 // Import controllers
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
+const { generalErrorHandler } = require('../middleware/error-handler')
 
 const admin = require('./modules/admin')
 router.use('/admin', admin)
@@ -12,5 +13,6 @@ router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
 router.get('/restaurants', restController.getRestaurants)
 router.use('/', (req, res) => res.redirect('/restaurants'))
+router.use('/', generalErrorHandler)
 
 module.exports = router
