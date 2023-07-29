3b436c4f1f0e2d5258684c95c1b27205b802b95a
taiwania <taiwaniajusto@gmail.com>
2023-07-29 23:39:46 +0800

    Create restaurants

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,11 +1,26 @@
 const { Restaurant } = require('../models')
 const adminController = {
-  getRestaurants: (req, res, next) => {
-    Restaurant.findAll({
-      raw: true
+  getRestaurants: (req, res) => {
+    return Restaurant.findAll({ raw: true }).then(restaurants => {
+      return res.render('admin/restaurants', { restaurants: restaurants })
     })
-      .then(restaurants => {
-        res.render('admin/restaurants', { restaurants })
+  },
+  createRestaurant: (req, res) => {
+    return res.render('admin/create-restaurant')
+  },
+  postRestaurant: (req, res, next) => {
+    const { name, tel, address, openingHours, description } = req.body
+    if (!name) throw new Error('Restaurant name is required!')
+    Restaurant.create({
+      name,
+      tel,
+      address,
+      openingHours,
+      description
+    })
+      .then(() => {
+        req.flash('success_messages', 'restaurant was successfully created')
+        res.redirect('/admin/restaurants')
       })
       .catch(err => next(err))
   }
