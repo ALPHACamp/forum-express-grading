f53651395a5341fd9ebb4e50ef0f940ead4e1bf4
DannyHucc <dannyhu10011101@gmail.com>
2023-07-30 09:46:44 +0800

    feat: add allowNull false in favorite.js

--- migrations/20230729025341-create-favorite.js
+++ migrations/20230729025341-create-favorite.js
@@ -9,9 +9,11 @@ module.exports = {
         type: Sequelize.INTEGER
       },
       user_id: {
+        allowNull: false,
         type: Sequelize.INTEGER
       },
       restaurant_id: {
+        allowNull: false,
         type: Sequelize.INTEGER
       },
       created_at: {
