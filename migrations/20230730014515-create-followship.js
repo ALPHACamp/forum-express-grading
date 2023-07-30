087bed9cbd4ef40703ce36261069352199fcd939
DannyHucc <dannyhu10011101@gmail.com>
2023-07-30 10:25:12 +0800

    feat: create followship model

---
+++ migrations/20230730014515-create-followship.js
@@ -0,0 +1,32 @@
+'use strict'
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Followships', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      follower_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      following_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Followships')
+  }
+}
