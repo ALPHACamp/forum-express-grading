f808a39befff90bd79c0924367f4c87a2ce7e47b
hao0516hsu <chhsu0516@gmail.com>
2023-07-30 13:00:48 +0800

    feat: admin update restaurant

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const routes = require('./routes')
 const handlebars = require('express-handlebars')
 const flash = require('connect-flash')
 const session = require('express-session')
+const methodOverride = require('method-override')
 const passport = require('./config/passport')
 const { getUser } = require('./helpers/auth-helpers')
 const handlebarsHelpers = require('./helpers/handlebars-helpers')
@@ -18,6 +19,8 @@ app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: fals
 app.use(passport.initialize())
 app.use(passport.session())
 app.use(flash())
+app.use(methodOverride('_method'))
+
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
   res.locals.error_messages = req.flash('error_messages')
