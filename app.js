aa7f67a187672ee36213622d2b0c1773d7a0bacc
paul.wu <tease0623@gmail.com>
2023-07-31 14:33:09 +0800

    feat: add passport local strategy

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const flash = require('connect-flash')
+const session = require('express-session')
+const passport = require('./config/passport')
 const routes = require('./routes')
+
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -10,6 +17,21 @@ app.engine('hbs', exphbs({
   extname: 'hbs'
 }))
 app.set('view engine', 'hbs')
+app.use(express.urlencoded({ extended: true }))
+app.use(session({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
+  res.locals.err_msg = req.flash('err_msg')
+  next()
+})
 
 app.use(routes)
 
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,31 @@
 const express = require('express')
 const router = express.Router()
+const passport = require('../config/passport')
 
 // import controllers
 const restaurantController = require('../controllers/restaurant-controller')
+const userController = require('../controllers/user-controller')
+
+// import error handler
+const { generalErrorHandler } = require('../middleware/error-handler')
+
 const admin = require('./modules/admin')
 
 router.use('/admin', admin)
+
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
+router.get('/signin', userController.signInPage)
+router.post('/signin', passport.authenticate('local', {
+  failureRedirect: '/signin',
+  failureFlash: true
+}), userController.signIn)
+router.get('/logout', userController.logout)
+
 router.get('/restaurants', restaurantController.getRestaurants)
-router.use('/', (req, res) => res.redirect('/restaurants'))
+router.get('/', (req, res) => res.redirect('/restaurants'))
+
+// error handler
+router.use('/', generalErrorHandler)
 
 module.exports = router
