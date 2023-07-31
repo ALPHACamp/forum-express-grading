da136b6fc3bb6221625b495b3356e3f951c1e02d
Chiang Jou <tiffanyj.chiang@gmail.com>
2023-07-31 16:54:44 +0800

    feat: category update

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -16,6 +16,8 @@ router.post('/restaurants', upload.single('image'), adminController.postRestaura
 router.patch('/users/:id', adminController.patchUser)
 router.get('/users', adminController.getUsers)
 
+router.get('/categories/:id', categoryController.getCategories)
+router.put('/categories/:id', categoryController.putCategory)
 router.get('/categories', categoryController.getCategories)
 router.post('/categories', categoryController.postCategory)
 
