74be5dad643f09d71a741357f3a87a1a6d6f0018
hao0516hsu <chhsu0516@gmail.com>
2023-07-31 11:57:45 +0800

    R01 assignment

--- config/passport.js
+++ config/passport.js
@@ -16,7 +16,7 @@ passport.use(new LocalStrategy(
         if (!user) return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤！'))
         bcrypt.compare(password, user.password)
           .then(res => {
-            if (!res) return cb(null, false, res.flash('error_messages', '帳號或密碼輸入錯誤！'))
+            if (!res) return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤！'))
 
             return cb(null, user)
           })
