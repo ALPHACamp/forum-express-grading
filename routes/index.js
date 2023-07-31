96d810b0b567b0650ee09097bd0f07710456312c
go2Cshop <shell800808@gmail.com>
2023-07-31 14:37:28 +0800

    feat: add admin index page

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const router = express.Router()
-
 // 新增，載入 controller
 const restController = require('../controllers/restaurant-controller')
+const admin = require('./modules/admin')
 
+router.use('/admin', admin)
 router.get('/restaurants', restController.getRestaurants)
-
 router.use('/', (req, res) => res.redirect('/restaurants'))
 
 module.exports = router
