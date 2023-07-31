88c2955d4e4e14bbf9bc5bb152dfda16da3f8809
harrison0502 <harrison0502@gmail.com>
2023-07-31 20:11:09 +0800

    feat: flash msg & signup verification

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require('express')
 const routes = require('./routes')
 const handlebars = require('express-handlebars')
+const flash = require('connect-flash')
+const session = require('express-session')
 const app = express()
 const port = process.env.PORT || 3000
+const SESSION_SECRET = 'secret'
 
 app.engine('hbs', handlebars({ extname: '.hbs' }))
 app.use(express.urlencoded({ extended: true }))
 app.set('view engine', 'hbs')
+app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
+app.use(flash()) // 掛載套件
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 app.use(routes)
 
 app.listen(port, () => {
