49378b39d1fd8e6592265b7c2eab7b1e9b787402
TCY1020 <a25023423@gmail.com>
2023-08-01 13:58:07 +0800

    feat: add feeds page & profile page edit button

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -74,6 +74,32 @@ const restaurantController = {
     } catch (err) {
       next(err)
     }
+  },
+  getFeeds: async (req, res, next) => {
+    try {
+      const [restaurants, comments] = await Promise.all([
+        Restaurant.findAll({
+          limit: 10,
+          order: [['createdAt', 'DESC']],
+          include: [Category],
+          raw: true,
+          nest: true
+        }),
+        Comment.findAll({
+          limit: 10,
+          order: [['createdAt', 'DESC']],
+          include: [User, Restaurant],
+          raw: true,
+          nest: true
+        })
+      ])
+      res.render('feeds', {
+        restaurants,
+        comments
+      })
+    } catch (err) {
+      next(err)
+    }
   }
 }
 module.exports = restaurantController
--- routes/index.js
+++ routes/index.js
@@ -17,6 +17,7 @@ router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn) // 驗證是否登入成功  注意是 post
 router.get('/logout', userController.logout)
 router.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard)
+router.get('/restaurants/feeds', authenticated, restController.getFeeds)
 router.get('/restaurants/:id', authenticated, restController.getRestaurant)
 router.get('/restaurants', authenticated, restController.getRestaurants) // 驗證成功才可進入restaurants
 router.delete('/comments/:id', authenticated, commentController.deleteComment)
