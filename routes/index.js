ad4afafe63c9fe65ce4085411b739239623c3f4f
lilymichael87@gmail.com <lilymichael87@gmail.com>
2023-08-01 16:43:52 +0800

    feat: flash msg & signup verification

--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,13 @@ const router = express.Router()
 const resrController = require('../controllers/restaurant-controller')
 const admin = require('./modules/admin')
 const userController = require('../controllers/user-controller')
+const { generalErrorHandler } = require('../middleware/error-handler')
 
 router.use('/admin', admin)
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
 router.get('/restaurants', resrController.getRestaurants)
 router.get('/', (req, res) => res.redirect('/restaurants'))
+router.use('/', generalErrorHandler)
 
 module.exports = router
