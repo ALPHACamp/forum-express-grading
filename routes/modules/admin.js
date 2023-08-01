26760ad71584e16d1c24c4831f30c02834bce209
Yen Lin <lynn575938@outlook.com>
2023-08-01 14:12:30 +0800

    feat: add admin delete restaurant function

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -7,6 +7,7 @@ router.get('/restaurants/create', adminController.createRestaurant) // 新增餐
 router.get('/restaurants/:id/edit', adminController.editRestaurant) // 編輯一筆餐廳頁面
 router.get('/restaurants/:id', adminController.getRestaurant) // 瀏覽一筆餐廳資料
 router.put('/restaurants/:id', adminController.putRestaurant) // 編輯一筆餐廳資料
+router.delete('/restaurants/:id', adminController.deleteRestaurant) // 刪除一筆餐廳資料
 router.get('/restaurants', adminController.getRestaurants) // 餐廳首頁
 router.post('/restaurants', adminController.postRestaurant) // 新增餐廳
 
