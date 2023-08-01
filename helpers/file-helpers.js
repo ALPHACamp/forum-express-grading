963e155d11d17611c391d496ad4173d235c45847
Easonlu0425 <andy1234532@hotmail.com>
2023-08-01 17:27:52 +0800

    feat: add seed data

--- helpers/file-helpers.js
+++ helpers/file-helpers.js
@@ -1,6 +1,6 @@
 const fs = require('fs') // 引入 fs 模組
 const localFileHandler = file => {
-  // file 是 multer 處理完的檔案
+  // file 是 multer 處理完在temp的檔案
   return new Promise((resolve, reject) => {
     if (!file) return resolve(null)
     const fileName = `upload/${file.originalname}`
