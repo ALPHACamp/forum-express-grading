d9764d9021219d2355c6b2a3ff5c13d3158df5a0
Yen Lin <lynn575938@outlook.com>
2023-08-01 12:48:04 +0800

    feat: add admin restaurant page

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -4,6 +4,7 @@ const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
 
 router.get('/restaurants/create', adminController.createRestaurant) // 新增餐廳頁面
+router.get('/restaurants/:id', adminController.getRestaurant) // 瀏覽一筆餐廳資料
 router.get('/restaurants', adminController.getRestaurants) // 餐廳首頁
 router.post('/restaurants', adminController.postRestaurant) // 新增餐廳
 router.use('/', (req, res) => res.redirect('/admin/restaurants'))
