c0f745b81b6d127f0c1baccba5433550d895d296
Mike <boxingbat>
2023-08-02 00:28:11 +0800

    passport init & signin

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const routes = require('./routes')
-
+const flash = require('connect-flash')
+const session = require('express-session')
 const app = express()
 const port = process.env.PORT || 3000
+const SESSION_SECRET = 'secret'
+const passport = require('./config/passport')
 
-app.engine('hbs', exphbs.engine({ extname: '.hbs' }))
+app.engine('hbs', exphbs({ extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
+app.use(passport.initialize()) // 增加這行，初始化 Passport
+app.use(passport.session())// 增加這行，啟動 session 功能
+app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
+app.use(flash()) // 掛載套件
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages') // 設定 success_msg 訊息
+  res.locals.error_messages = req.flash('error_messages') // 設定 warning_msg 訊息
+  next()
+})
+
 app.use(routes)
 
 app.listen(port, () => {
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,19 @@
 const express = require('express')
 const router = express.Router()
-const restController = require('../controllers/restaurant-controller')
+const passport = require('../config/passport')
 const admin = require('./modules/admin')
+
+const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
 
+const { generalErrorHandler } = require('../middleware/error-handler')
+
+router.use('/admin', admin)
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
-router.use('/admin', admin)
+router.get('/signin', userController.signInPage)
+router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
+router.use('/', generalErrorHandler)
 router.get('/restaurants', restController.getRestaurants)
 router.use('/', (req, res) => res.redirect('/restaurants'))
 
