75cfca1af79a2f2c1cb8ce996e1eb87d91172c3d
lilymichael87@gmail.com <lilymichael87@gmail.com>
2023-08-01 15:45:23 +0800

    feat: user signup

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,12 @@ const router = express.Router()
 // 新增，載入 controller
 const resrController = require('../controllers/restaurant-controller')
 const admin = require('./modules/admin')
+const userController = require('../controllers/user-controller')
 
 router.use('/admin', admin)
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
 router.get('/restaurants', resrController.getRestaurants)
-
 router.get('/', (req, res) => res.redirect('/restaurants'))
 
 module.exports = router
