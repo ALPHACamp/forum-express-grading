e2d250f701d012330ca9defa03b2d60df6037fa2
Easonlu0425 <andy1234532@hotmail.com>
2023-08-01 14:44:37 +0800

    feat: admin restaurant page

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -13,7 +13,6 @@ const adminController = {
     const { name, tel, address, openingHours, description } = req.body
     if (!name) throw new Error('Restaurant name is required!')
     Restaurant.create({
-
       name,
       tel,
       address,
@@ -23,6 +22,16 @@ const adminController = {
       .then(() => {
         req.flash('success_messages', 'restaurant was successfully created')
         res.redirect('/admin/restaurants')
+      })
+      .catch(err => next(err))
+  },
+  getRestaurant: (req, res, next) => {
+    Restaurant.findByPk(req.params.id, {
+      raw: true
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        res.render('admin/restaurant', { restaurant })
       })
       .catch(err => next(err))
   }
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -4,6 +4,7 @@ const adminController = require('../../controllers/admin-controller')
 
 router.get('/restaurants', adminController.getRestaurants)
 router.get('/restaurants/create', adminController.createRestaurant)
+router.get('/restaurants/:id', adminController.getRestaurant)
 router.post('/restaurants', adminController.postRestaurant)
 router.use('/', (req, res) => res.redirect('/admin/restaurants'))
 module.exports = router
