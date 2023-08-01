94b1f976a47f46dde0e2e646374f8156fa2489e7
Keiko <keitakeiko@hotmail.com>
2023-08-01 23:49:01 +0800

    feat: user signup

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ const routes = require('./routes')
 const app = express()
 const port = process.env.PORT || 3000
 
-const db = require('./models') // 暫時新增這行，引入資料庫，檢查完可刪
-
 // 註冊 Handlebars 樣板引擎，並指定副檔名為 .hbs
 app.engine('hbs', handlebars({ extname: '.hbs' }))
 // 設定使用 Handlebars 做為樣板引擎
 app.set('view engine', 'hbs')
+
+app.use(express.urlencoded({ extended: true })) // 加入這行
 app.use(routes)
 
 app.listen(port, () => {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const router = express.Router()
-const restController = require('../controllers/restaurant-controller')
 const admin = require('./modules/admin') // 載入 admin.js
+const restController = require('../controllers/restaurant-controller')
+const userController = require('../controllers/user-controller')
 
 router.use('/admin', admin)
+// 注意順序
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp) // 注意用 post
 
 router.get('/restaurants', restController.getRestaurants)
 
