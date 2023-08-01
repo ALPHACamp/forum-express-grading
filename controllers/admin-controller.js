51dd15905a025cb66b61a31df7c9be981fbd3f59
harrison0502 <harrison0502@gmail.com>
2023-08-01 13:27:55 +0800

    feat: admin restaurant page

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -26,6 +26,16 @@ const adminController = {
         res.redirect('/admin/restaurants')
       })
       .catch(err => next(err))
+  },
+  getRestaurant: (req, res, next) => {
+    Restaurant.findByPk(req.params.id, {
+      raw: true
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        res.render('admin/restaurant', { restaurant })
+      })
+      .catch(err => next(err))
   }
 }
 
