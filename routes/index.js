e67e443a3dd24e47dd995239299bdbcab7b6344e
go2Cshop <shell800808@gmail.com>
2023-08-02 10:17:35 +0800

    feat: passport init & signin

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const router = express.Router()
-// 新增，載入 controller
-const restController = require('../controllers/restaurant-controller')
+const passport = require('../config/passport')
 const admin = require('./modules/admin')
 
+const restController = require('../controllers/restaurant-controller')
+const userController = require('../controllers/user-controller')
+const { generalErrorHandler } = require('../middleware/error-handler')
 router.use('/admin', admin)
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
+router.get('/signin', userController.signInPage)
+router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
+router.get('/logout', userController.logout)
 router.get('/restaurants', restController.getRestaurants)
-router.use('/', (req, res) => res.redirect('/restaurants'))
+router.get('/', (req, res) => res.redirect('/restaurants'))
+router.use('/', generalErrorHandler)
 
 module.exports = router
