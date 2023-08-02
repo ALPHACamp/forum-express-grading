b819f20ffc5ff95b22284682e20966a0cae4fa18
lilymichael87@gmail.com <lilymichael87@gmail.com>
2023-08-02 13:04:16 +0800

    feat: passport init & signin

--- app.js
+++ app.js
@@ -6,12 +6,15 @@ const routes = require('./routes')
 const app = express()
 const port = process.env.PORT || 3000
 const SESSION_SECRET = 'secret'
+const passport = require('./config/passport')
 
 app.engine('hbs', handlebars({ extname: '.hbs' }))
 
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
+app.use(passport.initialize())
+app.use(passport.session())
 app.use(flash()) // 掛載套件
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages') // 設定 success_msg 訊息
