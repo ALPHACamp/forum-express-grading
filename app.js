ade37cc267bddd9f4ea8af4c03d05319cbf10a2f
w3i3538 <wei5566@gmail.com>
2023-08-03 03:25:09 +0800

    feat: add signup

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
-const routes = require('./routes')
 const handlebars = require('express-handlebars')
+const routes = require('./routes')
+
 const app = express()
 const port = process.env.PORT || 3000
 
@@ -8,6 +9,8 @@ const db = require('./models')
 
 app.engine('hbs', handlebars({ extname: '.hbs' }))
 app.set('view engine', 'hbs')
+
+app.use(express.urlencoded({ extended: 'true' }))
 
 app.use(routes)
 
