1ef3e4245bdf0f5200075ca59e236f2673a178cf
yanyanyaa <yclin3411@gmail.com>
2023-08-02 23:28:07 +0800

    feat: add index page

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,21 @@
 const express = require('express')
 const router = express.Router()
+const restController = require('../controllers/restaurant-controller')
 
 router.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+// 刪除
+
+router.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+// 新增
+
+router.get('/restaurants', restController.getRestaurants)
+
+router.use('/', (req, res) => res.redirect('/restaurants'))
 
 module.exports = router
