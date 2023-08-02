b1b46b75dd4ca4b2515c19660a8d763944986aed
tingul4 <xpple413208@gmail.com>
2023-08-02 19:22:07 +0800

    feat: user model add is_admin

--- models/restaurant.js
+++ models/restaurant.js
@@ -1,28 +1,28 @@
-'use strict'
+'use strict';
 const {
   Model
-} = require('sequelize')
+} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
+  class Restaurant extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate (models) {
+    static associate(models) {
       // define association here
     }
   };
-  User.init({
+  Restaurant.init({
     name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    isAdmin: DataTypes.BOOLEAN
+    tel: DataTypes.STRING,
+    address: DataTypes.STRING,
+    openingHours: DataTypes.STRING,
+    description: DataTypes.TEXT
   }, {
     sequelize,
-    modelName: 'User',
-    tableName: 'Users',
-    underscored: true
-  })
-  return User
-}
+    modelName: 'Restaurant',
+    underscored: true,
+  });
+  return Restaurant;
+};
