d1f91ab2c1ca47ed998ba2636091f56b0ef60aa7
tingul4 <xpple413208@gmail.com>
2023-08-02 19:27:36 +0800

    feat: add restaurant model

--- models/restaurant.js
+++ models/restaurant.js
@@ -1,7 +1,7 @@
-'use strict';
+'use strict'
 const {
   Model
-} = require('sequelize');
+} = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
   class Restaurant extends Model {
     /**
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate(models) {
+    static associate (models) {
       // define association here
     }
   };
@@ -22,7 +22,8 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'Restaurant',
-    underscored: true,
-  });
-  return Restaurant;
-};
+    tableName: 'Restaurants',
+    underscored: true
+  })
+  return Restaurant
+}
