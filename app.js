a36b6347cf0aac82d3fc914922fbc459e4acb786
lilymichael87@gmail.com <lilymichael87@gmail.com>
2023-08-02 13:40:10 +0800

    feat: add header & footer

--- app.js
+++ app.js
@@ -7,8 +7,10 @@ const app = express()
 const port = process.env.PORT || 3000
 const SESSION_SECRET = 'secret'
 const passport = require('./config/passport')
+const { getUser } = require('./helpers/auth-helpers')
+const handlebarsHelpers = require('./helpers/handlebars-helpers')
 
-app.engine('hbs', handlebars({ extname: '.hbs' }))
+app.engine('hbs', handlebars({ extname: '.hbs', helpers: handlebarsHelpers }))
 
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
@@ -19,6 +21,7 @@ app.use(flash()) // 掛載套件
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages') // 設定 success_msg 訊息
   res.locals.error_messages = req.flash('error_messages') // 設定 warning_msg 訊息
+  res.locals.user = getUser(req)
   next()
 })
 app.use(routes)
