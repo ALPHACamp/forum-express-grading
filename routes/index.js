b9269527e35b55cc3286419bb5dd1b989de1f93c
freeanser <bnn01615@gmail.com>
2023-08-02 23:16:52 +0800

    feat: create main.hbs and render restaurants.hbs

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const restController = require('../controllers/restaurant-controller')
+
+router.get('/restaurants', restController.getRestaurants)
+
+// fallback 路由是指其他路由條件都不符合時，最終會通過的路由。也就是說，當程式一路由上而下執行，萬一都匹配不到和請求相符的路徑，此時不論此 request 是用哪個 HTTP method 發出的，都會匹配到這一行，將使用者重新導回 /restaurants
+router.use('/', (req, res) => res.redirect('/restaurants'))
+
+module.exports = router
 
 module.exports = router
