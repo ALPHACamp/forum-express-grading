555183db56b1eee270b8510b73dc43b41e411e5b
paul.wu <tease0623@gmail.com>
2023-08-02 20:26:33 +0800

    feat: modify config for heroku

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -7,10 +7,10 @@ const adminController = require('../../controllers/admin-controller')
 router.get('/restaurants/create', adminController.createRestaurant)
 router.get('/restaurants/:id/edit', adminController.editRestaurant)
 router.get('/restaurants/:id', adminController.getRestaurant)
-router.put('/restaurants/:id',upload.single('image'), adminController.putRestaurant)
+router.put('/restaurants/:id', upload.single('image'), adminController.putRestaurant)
 router.delete('/restaurants/:id', adminController.deleteRestaurant)
 router.get('/restaurants', adminController.getRestaurants)
-router.post('/restaurants',upload.single('image'), adminController.postRestaurant)
+router.post('/restaurants', upload.single('image'), adminController.postRestaurant)
 
 router.use('/', (req, res) => res.redirect('/admin/restaurants'))
 
