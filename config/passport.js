9fa563be2ced7edcea6b0a944294d52ab52eb942
tingul4 <xpple413208@gmail.com>
2023-08-02 16:59:12 +0800

    feat: add header & footer

--- config/passport.js
+++ config/passport.js
@@ -29,7 +29,7 @@ passport.serializeUser((user, cb) => {
 })
 passport.deserializeUser((id, cb) => {
   User.findByPk(id).then(user => {
-    return cb(null, user)
+    return cb(null, user.toJSON())
   })
 })
 module.exports = passport
