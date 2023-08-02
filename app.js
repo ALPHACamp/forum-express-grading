3247cdf349c67952176861bac25c73134b4c560e
lilymichael87@gmail.com <lilymichael87@gmail.com>
2023-08-02 16:30:14 +0800

    feat:admin update restaurant

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const handlebars = require('express-handlebars')
 const flash = require('connect-flash')
 const session = require('express-session')
+const methodOverride = require('method-override')
 const routes = require('./routes')
 const app = express()
 const port = process.env.PORT || 3000
@@ -18,6 +19,7 @@ app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: fals
 app.use(passport.initialize())
 app.use(passport.session())
 app.use(flash()) // 掛載套件
+app.use(methodOverride('_method'))
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages') // 設定 success_msg 訊息
   res.locals.error_messages = req.flash('error_messages') // 設定 warning_msg 訊息
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -4,6 +4,8 @@ const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
 
 router.get('/restaurants/create', adminController.createRestaurant)
+router.get('/restaurants/:id/edit', adminController.editRestaurant)
+router.put('/restaurants/:id', adminController.putRestaurant)
 router.get('/restaurants/:id', adminController.getRestaurant)
 router.get('/restaurants', adminController.getRestaurants)
 router.post('/restaurants', adminController.postRestaurant)
