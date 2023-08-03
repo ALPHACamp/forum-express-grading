ae115a2736d789f59372acad93a65d00a8dba5d5
muco0521 <muco0521@gmail.com>
2023-08-03 23:41:48 +0800

    feat: category create

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -8,6 +8,17 @@ const categoryController = {
     } catch (err) {
       next(err)
     }
+  },
+  postCategory: async (req, res, next) => {
+    try {
+      const { name } = req.body
+      if (!name) throw new Error('Category name is required!')
+      await Category.create({ name })
+      req.flash('success_messages', 'Category was successfully created')
+      res.redirect('/admin/categories')
+    } catch (err) {
+      next(err)
+    }
   }
 }
 
