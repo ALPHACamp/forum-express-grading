3381ecec9a86eecd43bade9167fe14392489a4d0
Sunnylin0320 <sunny8503201@gmail.com>
2023-08-03 23:08:02 +0800

    feat: user signup

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 const express = require('express')
 const router = express.Router()
+const admin = require('./modules/admin') //新增這行，載入 admin.js
 
 const restController = require("../controllers/restaurant-controller");
-const admin = require('./modules/admin') //新增這行，載入 admin.js
+
+const userController = require("../controllers/user-controller");
 router.use('/admin', admin) //新增這行
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp) //注意用 post
 router.get("/restaurants", restController.getRestaurants);
 
 router.use("/", (req, res) => res.redirect("/restaurants"));
