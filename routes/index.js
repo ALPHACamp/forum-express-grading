6195ca6914f3429eccba6671d0c6677c3642fbe6
Sunnylin0320 <sunny8503201@gmail.com>
2023-08-03 22:42:52 +0800

    feat: add admin index page

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const router = express.Router()
 
 const restController = require("../controllers/restaurant-controller");
-
+const admin = require('./modules/admin') //新增這行，載入 admin.js
+router.use('/admin', admin) //新增這行
 router.get("/restaurants", restController.getRestaurants);
+
 router.use("/", (req, res) => res.redirect("/restaurants"));
 
 module.exports = router
