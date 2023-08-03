a9923624c559c2e917c823e69b0bdea0d09955bd
w3i3538 <wei5566@gmail.com>
2023-08-04 02:10:24 +0800

    add user.toJSON()

--- config/passport.js
+++ config/passport.js
@@ -29,7 +29,7 @@ passport.serializeUser((user, cb) => {
 })
 passport.deserializeUser((id, cb) => {
   User.findByPk(id).then(user => {
-    user=user.toJSON()
+    user = user.toJSON()
     return cb(null, user)
   })
 })
