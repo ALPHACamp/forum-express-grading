1f606f2c0035c0091ebbf9c4f431b384626b0e45
paul.wu <tease0623@gmail.com>
2023-08-03 22:28:25 +0800

    feat: add column viewCounts and dashboard page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -20,6 +20,19 @@ const restaurantController = {
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
+      // raw: true, // 為向下傳遞結果並使用 increment 方法，拿掉資料格式整理
+      // nest: true,
+      include: [Category]
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist")
+        return restaurant.increment('viewCounts')
+      })
+      .then(incrementResult => res.render('restaurant', { restaurant: incrementResult.toJSON() }))
+      .catch(err => next(err))
+  },
+  getDashboard: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, {
       raw: true,
       nest: true,
       include: [Category]
@@ -27,7 +40,7 @@ const restaurantController = {
       .then(restaurant => {
         if (!restaurant) throw new Error("Restaurant didn't exist")
 
-        return res.render('restaurant', { restaurant })
+        return res.render('dashboard', { restaurant })
       })
       .catch(err => next(err))
   }
