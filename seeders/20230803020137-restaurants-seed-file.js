181624220ef5ae5b8ec4bbfa8dd2e9ce9f4eeccd
yang haotse <ha91252@gmail.com>
2023-08-03 10:07:54 +0800

    feat: update seed files

--- seeders/20230803020137-restaurants-seed-file.js
+++ seeders/20230803020137-restaurants-seed-file.js
@@ -3,6 +3,9 @@ const faker = require('faker')
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    // 查詢Categories的id有哪些
+    const categories = await queryInterface.sequelize.query('SELECT id FROM Categories;', { type: queryInterface.sequelize.QueryTypes.SELECT })
+
     await queryInterface.bulkInsert('Restaurants',
     // Array.from()內第二個參數是mapFn，下方直接簡寫
       Array.from({ length: 50 }, () => ({
@@ -14,7 +17,8 @@ module.exports = {
         image: `https://loremflickr.com/320/240/restaurant,food/?random=${Math.random() * 100}`,
         description: faker.lorem.text(),
         created_at: new Date(),
-        updated_at: new Date()
+        updated_at: new Date(),
+        category_id: categories[Math.floor(Math.random() * categories.length)].id
       }))
     )
   },
