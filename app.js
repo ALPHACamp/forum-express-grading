3d87b81ecb59ad31ed775780110de9ebf99e58fd
dingbum73 <dingbum73@gmail.com>
2023-08-03 12:49:00 +0800

    feat: passport init & signin

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const handlebars = require('express-handlebars')
 const routes = require('./routes')
 const flash = require('connect-flash')
 const session = require('express-session')
+const passport = require('./config/passport')
 const app = express()
 const port = process.env.PORT || 3000
 const SESSION_SECRET = 'secret'
@@ -18,6 +19,8 @@ app.use(session({
   resave: false,
   saveUninitialized: false
 }))
+app.use(passport.initialize())
+app.use(passport.session())
 app.use(flash())
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
