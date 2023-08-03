ef2b65f790c8525e8881ef1031a0eddc3116cdab
Sunnylin0320 <sunny8503201@gmail.com>
2023-08-03 23:43:10 +0800

    feat: flash msg & signup verification

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,23 +1,32 @@
-const bcrypt = require("bcryptjs"); //載入 bcrypt
-const db = require("../models");
-const { User } = db;
+const bcrypt = require('bcryptjs') // 載入 bcrypt
+const db = require('../models')
+const { User } = db
 const userController = {
   signUpPage: (req, res) => {
-    res.render("signup");
+    res.render('signup')
   },
-  signUp: (req, res) => {
-    bcrypt
-      .hash(req.body.password, 10)
-      .then((hash) =>
+  signUp: (req, res, next) => {
+    if (req.body.password !== req.body.passwordCheck) { throw new Error('Passwords do not match!') }
+
+    User.findOne({ where: { email: req.body.email } })
+      .then(user => {
+        if (user) throw new Error('Email already exists!')
+
+        return bcrypt.hash(req.body.password, 10)
+      })
+      .then(hash =>
         User.create({
           name: req.body.name,
           email: req.body.email,
-          password: hash,
+          password: hash
         })
       )
       .then(() => {
-        res.redirect("/signin");
-      });
-  },
-};
-module.exports = userController;
+        req.flash('success_messages', '成功註冊帳號！')
+        res.redirect('/signin')
+      })
+      .catch(err => next(err))
+  }
+}
+
+module.exports = userController
--- routes/index.js
+++ routes/index.js
@@ -5,11 +5,14 @@ const admin = require('./modules/admin') //新增這行，載入 admin.js
 const restController = require("../controllers/restaurant-controller");
 
 const userController = require("../controllers/user-controller");
+
+const { generalErrorHandler } = require("../middleware/error-handler");
 router.use('/admin', admin) //新增這行
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp) //注意用 post
 router.get("/restaurants", restController.getRestaurants);
 
 router.use("/", (req, res) => res.redirect("/restaurants"));
+router.use("/", generalErrorHandler);
 
 module.exports = router
