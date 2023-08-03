3fb9a4ccd4ed0847cdd7698a0f40b74749704166
Keiko <keitakeiko@hotmail.com>
2023-08-03 23:57:31 +0800

    feat: user model add is_admin

---
+++ migrations/20230803154654-add-is-admin-to-users.js
@@ -0,0 +1,26 @@
+'use strict'
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addColumn('Users', 'is_admin', {
+      type: Sequelize.BOOLEAN,
+      defaultValue: false
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.removeColumn('Users', 'is_admin')
+  }
+}
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const passport = require('../config/passport') // 引入 Passport，需要他幫
 const admin = require('./modules/admin') // 載入 admin.js
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
+const { authenticated } = require('../middleware/auth')
 const { generalErrorHandler } = require('../middleware/error-handler')
 
 router.use('/admin', admin)
@@ -13,7 +14,7 @@ router.post('/signup', userController.signUp) // 注意用 post
 router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn) // 注意是 post
 router.get('/logout', userController.logout)
-router.get('/restaurants', restController.getRestaurants)
+router.get('/restaurants', authenticated, restController.getRestaurants)
 
 router.use('/', (req, res) => res.redirect('/restaurants'))
 router.use('/', generalErrorHandler)
