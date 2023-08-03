753cf97aa2423957316210b059707f06e1f5e904
gibbs-shih <shih.gibbs@gmail.com>
2023-08-03 10:58:34 +0800

    feat: user signup

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const router = express.Router()
 
+const userController = require('../controllers/user-controller')
 const restController = require('../controllers/restaurant-controller')
 const admin = require('./modules/admin')
 
 router.use('/admin', admin)
+
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp) //注意用 post
+
 router.get('/restaurants', restController.getRestaurants)
 router.get('/', (req, res) => {
   res.redirect('/restaurants')
