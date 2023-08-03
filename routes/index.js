c81d680298e0f8a2753486c280397fd736fa9192
LAI <77723979+LAI-Recycle@users.noreply.github.com>
2023-08-03 23:27:25 +0800

    feat: user model add is_admin

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,7 @@ const passport = require('../config/passport')
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
 const { generalErrorHandler } = require('../middleware/error-handler')
+const { authenticated } = require('../middleware/auth')
 
 router.use('/admin', admin)
 
@@ -14,9 +15,9 @@ router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
 router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
-router.get('/restaurants', restController.getRestaurants)
+router.get('/logout', userController.logout)
+router.get('/restaurants', authenticated, restController.getRestaurants)
 router.use('/', (req, res) => res.redirect('/restaurants'))
-
 router.use('/', generalErrorHandler)
 
 module.exports = router
