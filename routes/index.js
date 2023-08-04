0da0af896c8901883872cda43e38aaa0c64479f4
claire <claire210401@gmail.com>
2023-08-05 00:35:39 +0800

    feat: add index page

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
-
-router.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const restController = require('../controllers/restaurant-controller')
+// 接收到的請求路徑/restaurants，交給 controller 的 getRestaurants 函式來處理
+router.get('/restaurants', restController.getRestaurants)
+// 設定 fallback 路由, 其他路由條件都不符合時，最終會通過的路由，將使用者重新導回 /restaurants
+router.use('/', (req, res) => res.redirect('/restaurants'))
 
 module.exports = router
