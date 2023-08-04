464bb76d43ead740bf58364418fb7480b680f577
Chiang Jou <tiffanyj.chiang@gmail.com>
2023-08-04 17:27:07 +0800

    revise routes

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const router = express.Router()
+
 const passport = require('../config/passport')
+
 const admin = require('./modules/admin')
 
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
-const commentController = require('../controllers/comment-controller')
+const commentController = require('../controllers/​​comment-controller')
 
 const { authenticated, authenticatedAdmin } = require('../middleware/auth')
 const { generalErrorHandler } = require('../middleware/error-handler')
@@ -21,42 +23,31 @@ router.post('/signin', passport.authenticate('local', { failureRedirect: '/signi
 
 router.get('/logout', userController.logout)
 
-// Top users
 router.get('/users/top', authenticated, userController.getTopUsers)
-
-// User Profile
 router.get('/users/:id/edit', authenticated, userController.editUser)
 router.get('/users/:id', authenticated, userController.getUser)
 router.put('/users/:id', authenticated, upload.single('image'), userController.putUser)
 
-// Top restaurants
-router.get('/restaurants/top', authenticated, restController.getTopRestaurants)
-
-// Feeds
 router.get('/restaurants/feeds', authenticated, restController.getFeeds)
-
-// Restaurant Dashboard
+router.get('/restaurants/top', authenticated, restController.getTopRestaurants)
 router.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard)
 router.get('/restaurants/:id', authenticated, restController.getRestaurant)
 router.get('/restaurants', authenticated, restController.getRestaurants)
 
-// Comment
 router.delete('/comments/:id', authenticatedAdmin, commentController.deleteComment)
 router.post('/comments', authenticated, commentController.postComment)
 
-// Favorite
 router.post('/favorite/:restaurantId', authenticated, userController.addFavorite)
 router.delete('/favorite/:restaurantId', authenticated, userController.removeFavorite)
 
-// Like
 router.post('/like/:restaurantId', authenticated, userController.addLike)
 router.delete('/like/:restaurantId', authenticated, userController.removeLike)
 
-// Followship
 router.post('/following/:userId', authenticated, userController.addFollowing)
 router.delete('/following/:userId', authenticated, userController.removeFollowing)
 
-router.get('/', (req, res) => res.redirect('/restaurants'))
+router.use('/', (req, res) => res.redirect('/restaurants'))
 
 router.use('/', generalErrorHandler)
+
 module.exports = router
