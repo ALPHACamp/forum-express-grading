fa825c1346fa2060ab13e1698204688c73e0c049
tingul4 <xpple413208@gmail.com>
2023-08-04 12:28:01 +0800

    feat: add imgur api

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ const passport = require('./config/passport')
 const handlebarsHelpers = require('./helpers/handlebars-helpers')
 const { getUser } = require('./helpers/auth-helpers')
 const routes = require('./routes')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -26,7 +29,6 @@ app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
   res.locals.error_messages = req.flash('error_messages')
   res.locals.user = getUser(req)
-  console.log(res.locals.user)
   next()
 })
 app.use(routes)
