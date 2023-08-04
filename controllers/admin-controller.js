93992b76cbfbcc21feb0a09470c2dd02b469aa84
Sunnylin0320 <sunny8503201@gmail.com>
2023-08-04 21:41:06 +0800

    feat: modify admin restaurants page

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,6 +1,16 @@
+const { Restaurant } = require("../models"); // 新增這裡
 const adminController = {
-  getRestaurants: (req, res) => {
-    return res.render("admin/restaurants");
+  // 修改這裡
+
+  getRestaurants: (req, res, next) => {
+    Restaurant.findAll({
+      raw: true,
+    })
+
+      .then((restaurants) => res.render("admin/restaurants", { restaurants }))
+
+      .catch((err) => next(err));
   },
 };
+
 module.exports = adminController;
