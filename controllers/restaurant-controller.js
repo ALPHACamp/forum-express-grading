514c5d8ec9936b0ade01b9ed4b608c8d9987b99b
AlanChiangg <alaln35alan@gmail.com>
2023-08-04 16:55:28 +0800

    feat: add dashboard page and getDashboard route

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -18,13 +18,24 @@ const restaurantController = {
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
-      include: Category,
-      nest: true,
-      raw: true
+      include: Category
     })
       .then(restaurant => {
         if (!restaurant) throw new Error('Restaurant didn\'t exist!')
-        res.render('restaurant', { restaurant })
+        return restaurant.increment('viewCounts', { by: 1 })
+      })
+      .then(restaurant => {
+        res.render('restaurant', { restaurant: restaurant.toJSON() })
+      })
+      .catch(err => next(err))
+  },
+  getDashboard: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, {
+      include: Category
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error('Restaurant didn\'t exist!')
+        res.render('dashboard', { restaurant: restaurant.toJSON() })
       })
       .catch(err => next(err))
   }
