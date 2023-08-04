87c7f32a999b3a8e010ff47b05ef8e7bb9f962e8
susu725 <zxcv1010017@gmail.com>
2023-08-05 01:08:34 +0800

    feat: add admin index page

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,14 @@ const express = require('express')
 const router = express.Router()
 
 const restController = require('../controllers/restaurant-controller')
+const admin = require('./modules/admin')
+
+router.use('/admin', admin)
 
 router.get('/restaurants', restController.getRestaurants)
 
 // fallback路由，當匹配不到時就會執行這一行
-router.get('/', (req, res) => res.redirect('/restaurants'))
+// 跟router.get的差別在於get只有限定'/'，use的範圍相對廣泛
+router.use('/', (req, res) => res.redirect('/restaurants'))
 
 module.exports = router
