e923275ce00996f3b8a4d22905d6a074c5a1d242
AlanChiangg <alaln35alan@gmail.com>
2023-08-04 15:05:04 +0800

    add restaurant page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -15,6 +15,18 @@ const restaurantController = {
         return res.render('restaurants', { restaurants: data })
       })
       .catch(err => next(err))
+  },
+  getRestaurant: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, {
+      include: Category,
+      nest: true,
+      raw: true
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error('Restaurant didn\'t exist!')
+        res.render('restaurant', { restaurant })
+      })
+      .catch(err => next(err))
   }
 }
 module.exports = restaurantController
