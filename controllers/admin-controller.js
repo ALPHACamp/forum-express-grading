e001bb1511b350b0213e4af3057c01eb43eaf7f5
Eric Huang <86519473+erikku54@users.noreply.github.com>
2023-08-05 00:55:03 +0800

    fix: revise admin-controller for test

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -96,7 +96,6 @@ const adminController = {
   getUsers: (req, res, next) => {
     return User.findAll({ raw: true })
       .then(users => {
-        console.log(users)
         res.render('admin/users', { users })
       })
       .catch(err => next(err))
@@ -104,10 +103,17 @@ const adminController = {
   // (功能) 修改使用者權限
   patchUser: (req, res, next) => {
     return User.findByPk(req.params.id)
-      .then(user => user.update({ isAdmin: !user.isAdmin }))
-      .then(() => {
-        req.flash('error_messages', 'user was updated successfully.')
-        res.redirect('/admin/users')
+      .then(user => {
+        if (user.email === 'root@example.com') {
+          req.flash('error_messages', '禁止變更 root 權限')
+          return res.redirect('back')
+        }
+
+        user.update({ isAdmin: !user.isAdmin })
+          .then(() => {
+            req.flash('success_messages', '使用者權限變更成功')
+            return res.redirect('/admin/users')
+          })
       })
       .catch(err => next(err))
   }
