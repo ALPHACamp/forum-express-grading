73dbe61593e4c5cf50f14e44906b43e465da3339
Sunnylin0320 <sunny8503201@gmail.com>
2023-08-04 15:26:27 +0800

    feat: passport init & signin

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const handlebars = require("express-handlebars");
 
 const flash = require("connect-flash");
 const session = require("express-session");
+const passport = require("./config/passport");
 const routes = require('./routes')
 
 const app = express()
@@ -15,6 +16,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(
   session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false })
 );
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(flash()); // 掛載套件
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash("success_messages"); // 設定 success_msg 訊息
