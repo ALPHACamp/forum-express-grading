864a3fe05e194e7aba1484897e82aa92ab96ea65
HotG8119 <k508060@gmail.com>
2023-08-04 22:32:26 +0800

    refactor: Move project to new directory structure

---
+++ controllers/admin-controller.js
@@ -0,0 +1,88 @@
+const { Restaurant } = require('../models')
+const { imgurFileHandler } = require('../helpers/file-helpers')
+
+const restaurantController = {
+  getRestaurants: (req, res) => {
+    Restaurant.findAll({ raw: true })
+      .then(restaurants => res.render('admin/restaurants', { restaurants }))
+      .catch(err => console.log(err))
+  },
+  createRestaurant: (req, res) => {
+    return res.render('admin/create-restaurant')
+  },
+  postRestaurant: (req, res, next) => {
+    const { name, tel, address, openingHours, description } = req.body // 從 req.body 拿出表單裡的資料
+    if (!name) throw new Error('Restaurant name is required!') // name 是必填，若發先是空值就會終止程式碼，並在畫面顯示錯誤提示
+
+    const { file } = req
+
+    imgurFileHandler(file).then(filePath => {
+      return Restaurant.create({ // 產生一個新的 Restaurant 物件實例，並存入資料庫
+        name,
+        tel,
+        address,
+        openingHours,
+        description,
+        image: filePath || null
+      })
+    })
+      .then(() => {
+        req.flash('success_messages', 'restaurant was successfully created') // 在畫面顯示成功提示
+        res.redirect('/admin/restaurants') // 新增完成後導回後台首頁
+      })
+      .catch(err => next(err))
+  },
+  getRestaurant: (req, res) => {
+    return Restaurant.findByPk(req.params.rest_id, { raw: true })
+      .then(restaurant => {
+        if (!restaurant) throw new Error('restaurant not found')
+        res.render('admin/restaurant', { restaurant })
+      })
+      .catch(err => console.log(err))
+  },
+  editRestaurant: (req, res) => {
+    return Restaurant.findByPk(req.params.rest_id, { raw: true })
+      .then(restaurant => res.render('admin/edit-restaurant', { restaurant }))
+      .catch(err => console.log(err))
+  },
+  putRestaurant: (req, res, next) => {
+    const { name, tel, address, openingHours, description } = req.body
+    if (!name) throw new Error('Restaurant name is required!')
+
+    const { file } = req
+
+    Promise.all([
+      Restaurant.findByPk(req.params.rest_id),
+      imgurFileHandler(file)
+    ])
+      .then(([restaurant, filePath]) => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        return restaurant.update({
+          name,
+          tel,
+          address,
+          openingHours,
+          description,
+          image: filePath || restaurant.image
+        })
+      })
+      .then(() => {
+        req.flash('success_messages', 'restaurant was successfully to update')
+        res.redirect('/admin/restaurants')
+      })
+      .catch(err => next(err))
+  },
+  deleteRestaurant: (req, res, next) => {
+    return Restaurant.findByPk(req.params.rest_id)
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        return restaurant.destroy()
+      })
+      .then(() => {
+        req.flash('success_messages', 'restaurant was successfully to delete')
+        res.redirect('/admin/restaurants')
+      })
+      .catch(err => next(err))
+  }
+}
+module.exports = restaurantController
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,26 @@
 const express = require('express')
 const router = express.Router()
+const passport = require('../config/passport')
+const admin = require('./modules/admin.js')
 
-router.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const restController = require('../controllers/restaurant-controller.js')
+const userController = require('../controllers/user-controller.js')
+
+const { authenticated, authenticatedAdmin } = require('../middleware/auth')
+const { generalErrorHandler } = require('../middleware/error-handler')
+
+router.use('/admin', authenticatedAdmin, admin)
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
+
+router.get('/signin', userController.signInPage)
+router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn) // 注意是 post
+router.get('/logout', userController.logout)
+
+router.get('/restaurants', authenticated, restController.getRestaurants)
+
+router.use('/', (req, res) => { res.redirect('/restaurants') })
+
+router.use('/', generalErrorHandler)
 
 module.exports = router
