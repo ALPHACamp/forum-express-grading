dfadc4903e8943dbe65af44dd34f4344f956a553
WinstonAdams <apollo07291@gmail.com>
2023-08-04 22:50:32 +0800

    feat: add header & footer

--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,16 @@ const passport = require('../config/passport')
 const { generalErrorHandler } = require('../middleware/error-handler')
 
 router.use('/admin', admin)
+
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
+
 router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
 router.get('/logout', userController.logout)
+
 router.get('/restaurants', restController.getRestaurant)
+
 router.use('/', (req, res) => res.redirect('/restaurants'))
 router.use('/', generalErrorHandler)
 
