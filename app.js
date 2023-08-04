e5ee1d46e6ab013cdbfd2549c1a8e96b2e172a3d
peipeipeipeigit <szupei21@gmail.com>
2023-08-05 05:46:59 +0800

    feat: add header & footer

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ const handlebars = require('express-handlebars')
 const flash = require('connect-flash')
 const session = require('express-session')
 const passport = require('./config/passport')
+const handlebarsHelpers = require('./helpers/handlebars-helpers')
+const { getUser } = require('./helpers/auth-helpers')
 const routes = require('./routes')
 
 const app = express()
 const port = process.env.PORT || 3000
 const SESSION_SECRET = 'secret'
 
-app.engine('hbs', handlebars({ extname: '.hbs' }))
+app.engine('hbs', handlebars({ extname: '.hbs', helpers: handlebarsHelpers }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
@@ -19,8 +21,9 @@ app.use(passport.session())
 // require third party package
 app.use(flash())
 app.use((req, res, next) => {
-  res.locals.success_messages = req.flash('success_messages') // 設定 success_msg 訊息
-  res.locals.error_messages = req.flash('error_messages') // 設定 warning_msg 訊息
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  res.locals.user = getUser(req)
   next()
 })
 app.use(routes)
