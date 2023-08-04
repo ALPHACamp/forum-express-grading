72de622d0d4e04876b6102c5d8297e02740946d5
peipeipeipeigit <szupei21@gmail.com>
2023-08-05 04:46:15 +0800

    feat: add admin index page

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,10 @@
 const express = require('express')
 const router = express.Router()
 
-const restController = require('../controllers/restaurant-controller') 
+const restController = require('../controllers/restaurant-controller')
+const admin = require('./modules/admin')
+
+router.use('/admin', admin)
 
 router.get('/restaurants', restController.getRestaurants)
 
