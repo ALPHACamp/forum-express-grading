94b9fb5b36712ccc757072235fdafd9d63b89b91
Sunnylin0320 <sunny8503201@gmail.com>
2023-08-05 11:13:52 +0800

    feat: admin restaurant page

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const adminController = require("../../controllers/admin-controller");
 
 router.get("/restaurants/create", adminController.createRestaurant);
+router.get('/restaurants/:id', adminController.getRestaurant) //新增這一行
 router.get("/restaurants", adminController.getRestaurants);
 router.post("/restaurants", adminController.postRestaurant);
 
