14b47405852d7cda9f479659d725104513e55835
tingul4 <40429714+tingul4@users.noreply.github.com>
2023-08-06 00:08:25 +0800

    feat: patch user's authorization

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,4 +1,4 @@
-const { Restaurant } = require('../models')
+const { Restaurant, User } = require('../models')
 const { imgurFileHandler } = require('../helpers/file-helpers')
 
 const adminController = {
@@ -86,7 +86,29 @@ const adminController = {
       .catch(err => next(err))
   },
   getUsers: (req, res, next) => {
-    return res.render('admin/users')
+    return User.findAll({
+      raw: true
+    })
+      .then(users => res.render('admin/users', { users }))
+      .catch(err => next(err))
+  },
+  patchUser: (req, res, next) => {
+    return User.findByPk(req.params.id)
+      .then(user => {
+        if (user.email === 'root@example.com') {
+          req.flash('error_messages', '禁止變更 root 權限')
+          throw res.redirect('back')
+        }
+        return user
+      })
+      .then(user => user.update({ isAdmin: !user.isAdmin }))
+      .then(() => {
+        req.flash('success_messages', '使用者權限變更成功')
+        res.redirect('/admin/users')
+      })
+      .catch(err => {
+        next(err)
+      })
   }
 }
 module.exports = adminController
