9b09a623fd390c4c45f9e596b3af4d557e4493f7
HotG8119 <k508060@gmail.com>
2023-08-06 02:49:08 +0800

    Refactor: user controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -2,38 +2,35 @@ const bcrypt = require('bcryptjs')
 const db = require('../models')
 const { User } = db
 const userController = {
-  signUpPage: async (req, res, next) => {
-    try { res.render('signup') } catch (err) { next(err) }
+  signUpPage: (req, res) => {
+    res.render('signup')
   },
-  signUp: async (req, res, next) => {
-    try {
-      if (req.body.password !== req.body.passwordCheck) throw new Error('兩次密碼輸入不同！')
+  signUp: (req, res, next) => {
+    if (req.body.password !== req.body.passwordCheck) throw new Error('兩次密碼輸入不同！')
 
-      const user = await User.findOne({ where: { email: req.body.email } })
-      if (user) throw new Error('信箱重複！')
-
-      const hash = await bcrypt.hash(req.body.password, 10)
-      await User.create({ name: req.body.name, email: req.body.email, password: hash })
-
-      req.flash('success_msg', '成功註冊帳號！')
-      res.redirect('/signin')
-    } catch (err) { next(err) }
+    User.findOne({ where: { email: req.body.email } })
+      .then(user => {
+        if (user) throw new Error('信箱重複！')
+        return bcrypt.hash(req.body.password, 10)
+      })
+      .then(hash => User.create({ name: req.body.name, email: req.body.email, password: hash }))
+      .then(() => {
+        res.flash('success_msg', '成功註冊帳號！')
+        res.redirect('/signin')
+      })
+      .catch(err => next(err))
   },
-  signInPage: async (req, res, next) => {
-    try { res.render('signin') } catch (err) { next(err) }
+  signInPage: (req, res) => {
+    res.render('signin')
   },
-  signIn: async (req, res, next) => {
-    try {
-      req.flash('success_messages', '成功登入！')
-      res.redirect('/restaurants')
-    } catch (err) { next(err) }
+  signIn: (req, res) => {
+    req.flash('success_messages', '成功登入！')
+    res.redirect('/restaurants')
   },
-  logout: async (req, res, next) => {
-    try {
-      req.flash('success_messages', '登出成功！')
-      req.logout()
-      res.redirect('/signin')
-    } catch (err) { next(err) }
+  logout: (req, res) => {
+    req.flash('success_messages', '登出成功！')
+    req.logout()
+    res.redirect('/signin')
   }
 }
 
