1446eaed78dee122301815c3ee7ce586898de6c2
mmm999xp <mmm999xp@gmail.com>
2023-08-06 04:55:05 +0800

    passport init & signin

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,20 @@
 const express = require('express')
 const router = express.Router()
+const passport = require('../config/passport')
 const restController = require('../controllers/restaurant-controller')
 const admin = require('./modules/admin')
 const userController = require('../controllers/user-controller')
 const { generalErrorHandler } = require('../middleware/error-handler')
 router.use('/admin', admin)
-
+// signup
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
+// signin
+router.get('/signin', userController.signInPage)
+router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
+// logout
+router.get('/logout', userController.logout)
+
 router.get('/restaurants', restController.getRestaurants)
 
 router.use('/', (req, res) => res.redirect('/restaurants'))
