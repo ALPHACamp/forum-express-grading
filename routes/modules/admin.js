805a19244796ab09995dc5208526794ed330d6cb
Keiko <keitakeiko@hotmail.com>
2023-08-05 13:22:40 +0800

    feat: admin restaurant page

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const router = express.Router()
+
 const adminController = require('../../controllers/admin-controller')
 // const { authenticatedAdmin } = require('../../middleware/auth')
 
 router.get('/restaurants/create', adminController.createRestaurant)
+
+router.get('/restaurants/:id', adminController.getRestaurant)
 
 router.get('/restaurants', adminController.getRestaurants)
 router.post('/restaurants', adminController.postRestaurant)
