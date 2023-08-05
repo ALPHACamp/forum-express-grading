cebb8b73632d6fed81b759557fc33a0d5f69d986
iamcoolalan <iamalan1996@gmail.com>
2023-08-05 10:05:40 +0800

    feat: adjust middleware/auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,7 +1,7 @@
-const { ensureAuthenticated, getUser } = require('../helpers/auth-helpers')
+const helpers = require('../helpers/auth-helpers')
 
 const authenticated = (req, res, next) => {
-  if (ensureAuthenticated(req)) {
+  if (helpers.ensureAuthenticated(req)) {
     return next()
   }
 
@@ -9,8 +9,8 @@ const authenticated = (req, res, next) => {
 }
 
 const authenticatedAdmin = (req, res, next) => {
-  if (ensureAuthenticated(req)) {
-    if (getUser(req).isAdmin) {
+  if (helpers.ensureAuthenticated(req)) {
+    if (helpers.getUser(req).isAdmin) {
       return next()
     }
     res.redirect('/')
