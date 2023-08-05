23f76dd8136da319e39c3054a2aa46b6b978a325
Keiko <keitakeiko@hotmail.com>
2023-08-05 16:59:01 +0800

    feat: admin update restaurant

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -6,7 +6,10 @@ const adminController = require('../../controllers/admin-controller')
 
 router.get('/restaurants/create', adminController.createRestaurant)
 
+router.get('/restaurants/:id/edit', adminController.editRestaurant)
+
 router.get('/restaurants/:id', adminController.getRestaurant)
+router.put('/restaurants/:id', adminController.putRestaurant)
 
 router.get('/restaurants', adminController.getRestaurants)
 router.post('/restaurants', adminController.postRestaurant)
