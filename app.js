05ab419cdef6384c4e2f938db0a252704344de21
claire <claire210401@gmail.com>
2023-08-06 00:20:57 +0800

    feat: flash msg & signup verification

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
+// 提示訊息
+const flash = require('connect-flash')
+const session = require('express-session')
 
 const routes = require('./routes')
 
 const app = express()
 const port = process.env.PORT || 3000
+const SESSION_SECRET = 'secret'
 const db = require('./models')
 
 // 註冊 Handlebars 樣板引擎，並指定副檔名為 .hbs
@@ -13,6 +17,16 @@ app.engine('hbs', handlebars({ extname: '.hbs' }))
 app.set('view engine', 'hbs')
 // body-parser
 app.use(express.urlencoded({ extended: true }))
+
+app.use(
+  session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false })
+)
+app.use(flash()) // 掛載套件
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages') // 設定 success_msg 訊息
+  res.locals.error_messages = req.flash('error_messages') // 設定 warning_msg 訊息
+  next()
+})
 // route
 app.use(routes)
 
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,9 @@ const express = require('express')
 const router = express.Router()
 const admin = require('./modules/admin')
 const restController = require('../controllers/restaurant-controller')
-const userController = require('../controllers/user-controller') // 新增這行
-router.use('/admin', admin)
+const userController = require('../controllers/user-controller')
+const { generalErrorHandler } = require('../middleware/error-handler')
+
 // 後台首頁
 router.use('/admin', admin)
 // 註冊頁
@@ -14,5 +15,7 @@ router.post('/signup', userController.signUp)
 router.get('/restaurants', restController.getRestaurants)
 // 設定 fallback 路由, 其他路由條件都不符合時，最終會通過的路由，將使用者重新導回 /restaurants
 router.use('/', (req, res) => res.redirect('/restaurants'))
+// err
+router.use('/', generalErrorHandler)
 
 module.exports = router
