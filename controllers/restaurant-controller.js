89ff52084282c8be87ee05299d494a3b19278a71
yiching-hsu <ychsu7149@gmail.com>
2023-08-05 11:50:50 +0800

    dashboard - R02

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -20,14 +20,28 @@ const restaurantController = {
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
       include: Category,
-      nest: true,
-      raw: true
+      nest: true
     })
       .then(restaurant => {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
+        return restaurant.increment('viewCounts')
+      })
+      .then(restaurant => {
         res.render('restaurant', {
-          restaurant
+          restaurant: restaurant.toJSON()
         })
+      })
+      .catch(err => next(err))
+  },
+  getDashboard: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, {
+      include: Category,
+      nest: true
+    })
+      .then(restaurant => {
+        // console.log(restaurant)
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        res.render('dashboard', { restaurant: restaurant.toJSON() })
       })
       .catch(err => next(err))
   }
