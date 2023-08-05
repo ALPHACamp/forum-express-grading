058003bf8367c99fd2d71a60c294cde232427c18
yanyanyaa <yclin3411@gmail.com>
2023-08-05 23:41:53 +0800

    feat: add admin restaurant page

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -8,6 +8,16 @@ const adminController = {
       .then(restaurants => res.render('admin/restaurants', { restaurants }))
       .catch(err => next(err))
   },
+  getRestaurant: (req, res, next) => {
+    Restaurant.findByPk(req.params.id, {
+      raw: true
+    })
+      .then(restaurant => {
+        if (!restaurant) return new Error("Restaurant didn't exist!")
+        res.render('admin/restaurant', { restaurant })
+      })
+      .catch(err => next(err))
+  },
   createRestaurant: (req, res) => {
     return res.render('admin/create-restaurant')
   },
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -3,7 +3,8 @@ const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
 
 router.get('/restaurants/create', adminController.createRestaurant)
-router.post('/restaurants', adminController.postRestaurant)
+router.get('/restaurants/:id', adminController.getRestaurant)
 router.get('/restaurants', adminController.getRestaurants)
+router.post('/restaurants', adminController.postRestaurant)
 router.use('/', (req, res) => res.redirect('/admin/restaurants'))
 module.exports = router
