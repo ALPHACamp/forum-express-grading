eedde9a1bcfa3ff475a1456d256ff1ec38100ca5
Keiko <keitakeiko@hotmail.com>
2023-08-05 21:23:12 +0800

    feat: add seed data

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,7 +1,7 @@
-const { Restaurant } = require('../models') // 新增這裡
+const { Restaurant } = require('../models')
+const { localFileHandler } = require('../helpers/file-helpers') // 將 file-helper 載進來
 
-const adminController = { // 修改這裡
-
+const adminController = {
   getRestaurants: (req, res, next) => {
     Restaurant.findAll({
 
@@ -19,16 +19,19 @@ const adminController = { // 修改這裡
   postRestaurant: (req, res, next) => {
     const { name, tel, address, openingHours, description } = req.body // 從 req.body 拿出表單裡的資料
     if (!name) throw new Error('Restaurant name is required!') // name 是必填，若發先是空值就會終止程式碼，並在畫面顯示錯誤提示
-    Restaurant.create({ // 產生一個新的 Restaurant 物件實例，並存入資料庫
-      name,
-      tel,
-      address,
-      openingHours,
-      description
-    })
+    const { file } = req // 把檔案取出來，也可以寫成 const file = req.file
+    localFileHandler(file) // 把上傳的檔案取出來，file 為 input 的屬性，傳給 file-helper 處理
+      .then(filePath => Restaurant.create({ // 再 create 這筆餐廳資料
+        name,
+        tel,
+        address,
+        openingHours,
+        description,
+        image: filePath || null
+      }))
       .then(() => {
-        req.flash('success_messages', 'restaurant was successfully created') // 在畫面顯示成功提示
-        res.redirect('/admin/restaurants') // 新增完成後導回後台首頁
+        req.flash('success_messages', 'restaurant was successfully created')
+        res.redirect('/admin/restaurants')
       })
       .catch(err => next(err))
   },
@@ -55,15 +58,20 @@ const adminController = { // 修改這裡
   putRestaurant: (req, res, next) => {
     const { name, tel, address, openingHours, description } = req.body
     if (!name) throw new Error('Restaurant name is required!')
-    Restaurant.findByPk(req.params.id)
-      .then(restaurant => {
+    const { file } = req // 如果有上傳照片，把檔案取出來，file 為 input 設定的屬性
+    Promise.all([ // 非同步處理
+      Restaurant.findByPk(req.params.id), // 去資料庫查有沒有這間餐廳
+      localFileHandler(file) // 把檔案傳到 file-helper 處理
+    ])
+      .then(([restaurant, filePath]) => { // 以上兩樣事都做完以後
         if (!restaurant) throw new Error("Restaurant didn't exist!")
-        return restaurant.update({
+        return restaurant.update({ // 修改這筆資料
           name,
           tel,
           address,
           openingHours,
-          description
+          description,
+          image: filePath || restaurant.image // 如果 filePath 是 Truthy (使用者有上傳新照片) 就用 filePath，是 Falsy (使用者沒有上傳新照片) 就沿用原本資料庫內的值
         })
       })
       .then(() => {
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 const adminController = require('../../controllers/admin-controller')
+const upload = require('../../middleware/multer')
 // const { authenticatedAdmin } = require('../../middleware/auth')
 
 router.get('/restaurants/create', adminController.createRestaurant)
@@ -9,12 +10,12 @@ router.get('/restaurants/create', adminController.createRestaurant)
 router.get('/restaurants/:id/edit', adminController.editRestaurant)
 
 router.get('/restaurants/:id', adminController.getRestaurant)
-router.put('/restaurants/:id', adminController.putRestaurant)
+router.put('/restaurants/:id', upload.single('image'), adminController.putRestaurant)
 
 router.delete('/restaurants/:id', adminController.deleteRestaurant)
 
 router.get('/restaurants', adminController.getRestaurants)
-router.post('/restaurants', adminController.postRestaurant)
+router.post('/restaurants', upload.single('image'), adminController.postRestaurant)
 
 router.get('/', (req, res) => res.redirect('/admin/restaurants'))
 
