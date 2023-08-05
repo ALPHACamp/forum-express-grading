1179583f0b70ada179710aef2bba393a045fd146
Sunnylin0320 <sunny8503201@gmail.com>
2023-08-05 12:29:10 +0800

    feat: delete restaurant

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -6,6 +6,7 @@ router.get("/restaurants/create", adminController.createRestaurant);
 router.get('/restaurants/:id/edit', adminController.editRestaurant) // 新增這一行
 router.get('/restaurants/:id', adminController.getRestaurant) //新增這一行
 router.put("/restaurants/:id", adminController.putRestaurant);
+router.delete("/restaurants/:id", adminController.deleteRestaurant);
 router.get("/restaurants", adminController.getRestaurants);
 router.post("/restaurants", adminController.postRestaurant);
 
