bb6d52896ceaa78094b6bed629ad3f879cbea616
Yi-Hsiu Lee <125238665+tmrmds-yhl@users.noreply.github.com>
2023-08-06 15:35:53 +0800

    feat: 編輯一筆餐廳資料

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const flash = require('connect-flash')
+const methodOverride = require('method-override')
 const session = require('express-session')
 const passport = require('./config/passport') // 一定要放在 session 後面！！！！！！！
 const routes = require('./routes')
@@ -16,7 +17,8 @@ app.use(express.urlencoded({ extended: true }))
 app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
 app.use(passport.initialize()) // 一定要放在 session 後面！！！！！！！
 app.use(passport.session())
-app.use(flash()) // 掛載套件
+app.use(flash())
+app.use(methodOverride('_method'))
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages') // 設定 success_msg 訊息
   res.locals.error_messages = req.flash('error_messages') // 設定 warning_msg 訊息
