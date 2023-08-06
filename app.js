e3186ff1ea5609e3ab6cf6377eb743c19425a64b
Elu1919 <qw29149974@gmail.com>
2023-08-06 16:06:18 +0800

    feat: flash msg & signup verification

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
+const flash = require('connect-flash')
+const session = require('express-session')
 const routes = require('./routes')
+
+const SESSION_SECRET = 'secret'
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -9,6 +13,15 @@ app.engine('hbs', handlebars({ extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
 app.use(express.urlencoded({ extended: true }))
+
+app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
+
+app.use(flash()) // 掛載套件
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 
 app.use(routes)
 
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
+const { generalErrorHandler } = require('../middleware/error-handler')
 const admin = require('./modules/admin')
 
 const router = express.Router()
@@ -10,5 +11,6 @@ router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
 router.get('/restaurants', restController.getRestaurants)
 router.use('/', (req, res) => res.redirect('/restaurants'))
+router.use('/', generalErrorHandler)
 
 module.exports = router
