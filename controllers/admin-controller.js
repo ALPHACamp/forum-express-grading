6d07c7101c17353f503077768f219277a44051ef
freeanser <bnn01615@gmail.com>
2023-08-06 14:54:06 +0800

    feat: create restaurant

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,19 +1,31 @@
 const { Restaurant } = require('../models') // 新增這裡
 
-const adminController = { // 修改這裡
-
-  getRestaurants: (req, res, next) => {
-    Restaurant.findAll({
-
-      raw: true
-
+const adminController = {
+  getRestaurants: (req, res) => {
+    return Restaurant.findAll({ raw: true }).then(restaurants => {
+      return res.render('admin/restaurants', { restaurants: restaurants })
     })
-
-      .then(restaurants => res.render('admin/restaurants', { restaurants }))
-
+  }, // 補逗點
+  // 新增這個
+  createRestaurant: (req, res) => {
+    return res.render('admin/create-restaurant')
+  },
+  // 新增以下
+  postRestaurant: (req, res, next) => {
+    const { name, tel, address, openingHours, description } = req.body // 從 req.body 拿出表單裡的資料
+    if (!name) throw new Error('Restaurant name is required!') // name 是必填，若發先是空值就會終止程式碼，並在畫面顯示錯誤提示
+    Restaurant.create({ // 產生一個新的 Restaurant 物件實例，並存入資料庫
+      name,
+      tel,
+      address,
+      openingHours,
+      description
+    })
+      .then(() => {
+        req.flash('success_messages', 'restaurant was successfully created') // 在畫面顯示成功提示
+        res.redirect('/admin/restaurants') // 新增完成後導回後台首頁
+      })
       .catch(err => next(err))
   }
-
 }
-
 module.exports = adminController
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -2,10 +2,10 @@ const express = require('express')
 const router = express.Router()
 
 const adminController = require('../../controllers/admin-controller')
-const { authenticatedAdmin } = require('../../middleware/auth')
-
-router.get('/restaurants', authenticatedAdmin, adminController.getRestaurants)
+// const { authenticatedAdmin } = require('../../middleware/auth') // 有建立在 index 了
+router.get('/restaurants/create', adminController.createRestaurant)
 router.get('/restaurants', adminController.getRestaurants)
+router.post('/restaurants', adminController.postRestaurant)
 router.use('/', (req, res) => res.redirect('/admin/restaurants'))
 
 module.exports = router
