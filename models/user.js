d5e97c543beb76621f9df232737d9aad8e9685b4
Elu1919 <qw29149974@gmail.com>
2023-08-06 20:13:02 +0800

    feat: user model add is_admin

--- models/user.js
+++ models/user.js
@@ -9,14 +9,15 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate (models) {
+    static associate(models) {
       // define association here
     }
   };
   User.init({
     name: DataTypes.STRING,
     email: DataTypes.STRING,
-    password: DataTypes.STRING
+    password: DataTypes.STRING,
+    isAdmin: DataTypes.BOOLEAN
   }, {
     sequelize,
     modelName: 'User',
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const adminController = require('../../controllers/admin-controller')
+const { authenticatedAdmin } = require('../../middleware/auth')
 
 const router = express.Router()
 
-router.get('/restaurants', adminController.getRestaurants)
+router.get('/restaurants', authenticatedAdmin, adminController.getRestaurants)
 
 router.use('/', (req, res) => res.redirect('/admin/restaurants'))
 
