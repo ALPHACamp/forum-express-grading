a8f3450b60b1d84a1c3e02226f408e0f1aadc904
Yi-Hsiu Lee <125238665+tmrmds-yhl@users.noreply.github.com>
2023-08-06 11:47:22 +0800

    feat: 建立使用者登入機制

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const flash = require('connect-flash')
 const session = require('express-session')
+const passport = require('./config/passport')
 const routes = require('./routes')
 const handlebars = require('express-handlebars')
 const app = express()
@@ -9,7 +10,9 @@ const port = process.env.PORT || 3000
 app.engine('hbs', handlebars({ extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
-app.use(session({ secret: "SESSION_SECRET", resave: false, saveUninitialized: false }))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(session({ secret: 'SESSION_SECRET', resave: false, saveUninitialized: false }))
 app.use(flash()) // 掛載套件
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')  // 設定 success_msg 訊息
