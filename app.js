e93255be942626e785405cd24280d7d8f48cef4e
Chiang Jou <tiffanyj.chiang@gmail.com>
2023-08-06 13:57:42 +0800

    feat: flash msg & signup verification

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
+const flash = require('connect-flash')
+const session = require('express-session')
 const routes = require('./routes')
 
 const app = express()
 const port = process.env.PORT || 3000
+const SESSION_SECRET = 'secret'
 
 // Handlebars
 app.engine('hbs', handlebars({ extname: '.hbs' }))
@@ -11,6 +14,17 @@ app.set('view engine', 'hbs')
 
 // Body-parser
 app.use(express.urlencoded({ extended: true }))
+
+// Express-session
+app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
+
+// Connect-flash
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 
 app.use(routes)
 
--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,9 @@ const router = express.Router()
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
 
+// Middleware
+const { generalErrorHandler } = require('../middleware/error-handler')
+
 // Admin
 const admin = require('./modules/admin')
 router.use('/admin', admin)
@@ -15,4 +18,5 @@ router.post('/signup', userController.signUp)
 
 router.get('/restaurants', restController.getRestaurants)
 router.use('/', (req, res) => res.redirect('/restaurants'))
+router.use('/', generalErrorHandler)
 module.exports = router
