eaecd47dbc703f1965ffeabc73a2769e7ed3dfcc
Yi-Hsiu Lee <125238665+tmrmds-yhl@users.noreply.github.com>
2023-08-06 15:25:20 +0800

    feat: 瀏覽一筆餐廳資料

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -2,17 +2,49 @@ const { Restaurant } = require('../models') // 新增這裡
 
 const adminController = { // 修改這裡
 
-  getRestaurants: (req, res, next) => {
-    Restaurant.findAll({
+    getRestaurants: (req, res, next) => {
+        Restaurant.findAll({
 
-      raw: true
+            raw: true
 
-    })
+        })
 
-      .then(restaurants => res.render('admin/restaurants', { restaurants }))
+            .then(restaurants => res.render('admin/restaurants', { restaurants }))
 
-      .catch(err => next(err))
-  }
+            .catch(err => next(err))
+    },
+
+    createRestaurant: (req, res) => {
+        return res.render('admin/create-restaurant')
+    },
+
+    postRestaurant: (req, res, next) => {
+        const { name, tel, address, openingHours, description } = req.body // 從 req.body 拿出表單裡的資料
+        if (!name) throw new Error('Restaurant name is required!') // name 是必填，若發先是空值就會終止程式碼，並在畫面顯示錯誤提示
+        Restaurant.create({ // 產生一個新的 Restaurant 物件實例，並存入資料庫
+            name,
+            tel,
+            address,
+            openingHours,
+            description
+        })
+            .then(() => {
+                req.flash('success_messages', 'restaurant was successfully created') // 在畫面顯示成功提示
+                res.redirect('/admin/restaurants') // 新增完成後導回後台首頁
+            })
+            .catch(err => next(err))
+    },
+
+    getRestaurant: (req, res, next) => {
+        Restaurant.findByPk(req.params.id, { //去資料庫用 id 找一筆資料
+            raw: true // 找到以後整理格式再回傳
+        })
+            .then(restaurant => {
+                if (!restaurant) throw new Error("Restaurant didn't exist!") //  如果找不到，回傳錯誤訊息，後面不執行
+                res.render('admin/restaurant', { restaurant })
+            })
+            .catch(err => next(err))
+    }
 
 }
 
--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,13 @@ const passport = require('../config/passport')
 
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
+const adminController = require('../controllers/admin-controller')
 
 const admin = require('./modules/admin')
-const { authenticated } = require('../middleware/auth')
+const { authenticated, authenticatedAdmin } = require('../middleware/auth')
 const { generalErrorHandler } = require('../middleware/error-handler')
 
-router.use('/admin', admin)
+router.use('/admin', authenticatedAdmin, admin)
 
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
