808bb2655270f2981be1fbde90bc1909d99756b4
yang haotse <ha91252@gmail.com>
2023-08-06 11:25:49 +0800

    feat: add pagination on restaurants index page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,25 +1,33 @@
 const { Restaurant, Category } = require('../models')
+const { getOffset, getPagination } = require('../helpers/pagination-helper')
 
 const restaurantController = {
   getRestaurants: (req, res, next) => {
     const categoryId = Number(req.query.categoryId) || ''
-    return Promise.all([Restaurant.findAll({
+    const DEFAULT_LIMIT = 9
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || DEFAULT_LIMIT //req.query.limit 留給未來擴充的空間
+    const offset = getOffset(limit, page) //偏移量
+
+    return Promise.all([Restaurant.findAndCountAll({
       raw: true,
       nest: true,
       where: { ...categoryId ? { categoryId } : {} },
+      limit,
+      offset,
       include: Category
     }),
     Category.findAll({ raw: true })
     ])
-      .then(([restaurant, categories]) => {
+      .then(([restaurants, categories]) => {
         // 將首頁顯示的description截至50個字
         // 展開運算子和物件搭配時，通常是用在想要拷貝物件並做出做局部修改的時候
-        const data = restaurant.map(r => ({
+        const data = restaurants.rows.map(r => ({
           ...r,
           // 修改description，若出現重複的key則會以後面的為準
           description: r.description.substring(0, 50)
         }))
-        return res.render('restaurants', { restaurants: data, categories })
+        return res.render('restaurants', { restaurants: data, categories, categoryId, pagination: getPagination(limit, page, restaurants.count) })
       })
       .catch(err => next(err))
   },
---
+++ helpers/pagination-helper.js
@@ -0,0 +1,31 @@
+const getOffset = (limit = 10, page = 1) => (page - 1) * limit
+
+const getPagination = (limit = 10, page = 1, total = 50) => {
+  const totalPage = Math.ceil(total / limit)
+  const pages = Array.from({ length: totalPage }, (_, index) => index + 1)
+  const currentPage = page < 1 ? 1 : page > totalPage ? totalPage : page //從後面往前讀
+  // if 寫法 -> currentPage
+  // let currentPage
+  // if (page < 1) {
+  //   currentPage = 1
+  // }else if (page > totalPage) {
+  //   currentPage = totalPage
+  // }else{
+  //   currentPage = page
+  // }
+  const prev = currentPage - 1 < 1 ? 1 : currentPage - 1
+  const next = currentPage + 1 > totalPage ? totalPage : currentPage + 1
+
+  return {
+    pages,
+    totalPage,
+    currentPage,
+    prev,
+    next
+  }
+}
+
+module.exports = {
+  getOffset,
+  getPagination
+}
