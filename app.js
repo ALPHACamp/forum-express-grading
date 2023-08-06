11d7aac5fdbea9c495735bfd0956b6958e966c7e
claire <claire210401@gmail.com>
2023-08-06 12:32:11 +0800

    feat: add header & footer

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ const handlebars = require('express-handlebars')
 const flash = require('connect-flash')
 const session = require('express-session')
 const passport = require('./config/passport')
-
+const handlebarsHelpers = require('./helpers/handlebars-helpers')
+const { getUser } = require('./helpers/auth-helpers')
 const routes = require('./routes')
 
 const app = express()
@@ -14,7 +15,7 @@ const SESSION_SECRET = 'secret'
 const db = require('./models')
 
 // 註冊 Handlebars 樣板引擎，並指定副檔名為 .hbs
-app.engine('hbs', handlebars({ extname: '.hbs' }))
+app.engine('hbs', handlebars({ extname: '.hbs', helpers: handlebarsHelpers }))
 // 設定使用 Handlebars 做為樣板引擎
 app.set('view engine', 'hbs')
 // body-parser
@@ -31,6 +32,8 @@ app.use(flash()) // 掛載套件
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages') // 設定 success_msg 訊息
   res.locals.error_messages = req.flash('error_messages') // 設定 warning_msg 訊息
+  // 所有的 view 都能存取
+  res.locals.user = getUser(req)
   next()
 })
 // route
---
+++ helpers/auth-helpers.js
@@ -0,0 +1,8 @@
+const getUser = req => {
+  // req.user ? req.user : null
+  // 若 req.user 存在就回傳 req.user，不存在的話函式就會回傳空值
+  return req.user || null
+}
+module.exports = {
+  getUser
+}
