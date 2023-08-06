49f7eb59da5f9631c2da0f305e1d089f360230f3
Yi-Hsiu Lee <125238665+tmrmds-yhl@users.noreply.github.com>
2023-08-06 22:35:10 +0800

    merge r01 test

--- seeders/20230806075631-users-seed-file.js
+++ seeders/20230806075631-users-seed-file.js
@@ -6,6 +6,7 @@ module.exports = {
       email: 'root@example.com',
       password: await bcrypt.hash('12345678', 10),
       is_admin: true,
+      is_superuser: true,
       name: 'root',
       created_at: new Date(),
       updated_at: new Date()
@@ -13,6 +14,7 @@ module.exports = {
       email: 'user1@example.com',
       password: await bcrypt.hash('12345678', 10),
       is_admin: false,
+      is_superuser: false,
       name: 'user1',
       created_at: new Date(),
       updated_at: new Date()
@@ -20,6 +22,7 @@ module.exports = {
       email: 'user2@example.com',
       password: await bcrypt.hash('12345678', 10),
       is_admin: false,
+      is_superuser: false,
       name: 'user2',
       created_at: new Date(),
       updated_at: new Date()
