e9d575ebaf966d54d9a5c78e7f9dbbbf31e3980e
yang haotse <ha91252@gmail.com>
2023-08-06 09:31:51 +0800

    feat: add categories navbar on restaurants index page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -2,12 +2,16 @@ const { Restaurant, Category } = require('../models')
 
 const restaurantController = {
   getRestaurants: (req, res, next) => {
-    return Restaurant.findAll({
+    const categoryId = Number(req.query.categoryId) || ''
+    return Promise.all([Restaurant.findAll({
       raw: true,
       nest: true,
-      include: [Category]
-    })
-      .then(restaurant => {
+      where: { ...categoryId ? { categoryId } : {} },
+      include: Category
+    }),
+    Category.findAll({ raw: true })
+    ])
+      .then(([restaurant, categories]) => {
         // 將首頁顯示的description截至50個字
         // 展開運算子和物件搭配時，通常是用在想要拷貝物件並做出做局部修改的時候
         const data = restaurant.map(r => ({
@@ -15,7 +19,7 @@ const restaurantController = {
           // 修改description，若出現重複的key則會以後面的為準
           description: r.description.substring(0, 50)
         }))
-        return res.render('restaurants', { restaurants: data })
+        return res.render('restaurants', { restaurants: data, categories })
       })
       .catch(err => next(err))
   },
--- seeders/20230802122301-categories-seed-file.js
+++ seeders/20230802122301-categories-seed-file.js
@@ -2,17 +2,17 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    // bulkInsert接受三個參數(資料表名稱, 資料陣列, 非必填)
-    await queryInterface.bulkInsert('Categories', ['中式料理', '日本料理', '義大利料理', '墨西哥料理', '素食料理', '美式料理', '複合式料理'].map(item => {
-      return {
-        name: item,
-        created_at: new Date(),
-        updated_at: new Date()
-      }
-    }
-    ), {})
+    await queryInterface.bulkInsert('Categories',
+      ['中式料理', '日本料理', '義大利料理', '墨西哥料理', '素食料理', '美式料理', '複合式料理']
+        .map(item => {
+          return {
+            name: item,
+            created_at: new Date(),
+            updated_at: new Date()
+          }
+        }
+        ), {})
   },
-
   down: async (queryInterface, Sequelize) => {
     await queryInterface.bulkDelete('Categories', {})
   }
