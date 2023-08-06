e99cdd18b3ee7cb9d458bcff593d39d49927229c
Elu1919 <qw29149974@gmail.com>
2023-08-06 17:35:12 +0800

    feat: passport init & signin

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-  generalErrorHandler(err, req, res, next) {
+  generalErrorHandler (err, req, res, next) {
     if (err instanceof Error) {
       req.flash('error_messages', `${err.name}: ${err.message}`)
     } else {
