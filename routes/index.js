1d05c2b1d564b3360807c49da9c96cbc2e645bc1
Chiang Jou <tiffanyj.chiang@gmail.com>
2023-08-06 23:44:03 +0800

    feat: add favorite/unfavorite button at index page

--- routes/index.js
+++ routes/index.js
@@ -45,6 +45,10 @@ router.get('/restaurants', authenticated, restController.getRestaurants)
 router.delete('/comments/:id', authenticatedAdmin, commentController.deleteComment)
 router.post('/comments', authenticated, commentController.postComment)
 
+// Favorite
+router.post('/favorite/:restaurantId', authenticated, userController.addFavorite)
+router.delete('/favorite/:restaurantId', authenticated, userController.removeFavorite)
+
 router.use('/', (req, res) => res.redirect('/restaurants'))
 
 router.use('/', generalErrorHandler)
