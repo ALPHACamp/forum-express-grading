1f252b2b67114e253eef182b85ffb8409e855622
taiwania <taiwaniajusto@gmail.com>
2023-08-06 16:02:16 +0800

    R02 homework is finished R02 作業完成

--- routes/index.js
+++ routes/index.js
@@ -8,19 +8,25 @@ const userController = require('../controllers/user-controller')
 const { authenticated, authenticatedAdmin } = require('../middleware/auth')
 const { generalErrorHandler } = require('../middleware/error-handler')
 
+// Import admin module
 const admin = require('./modules/admin')
 router.use('/admin', authenticatedAdmin, admin)
 
+// Set signup page
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
 
+// Set signin page
 router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
 router.get('/logout', userController.logout)
 
+// Set restaurants page
+router.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard)
 router.get('/restaurants/:id', authenticated, restController.getRestaurant)
 router.get('/restaurants', authenticated, restController.getRestaurants)
 router.use('/', (req, res) => res.redirect('/restaurants'))
 router.use('/', generalErrorHandler)
 
+// Export router
 module.exports = router
