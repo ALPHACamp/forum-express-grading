558346c3baadb7bebbf58a97a4b70a9c356ef1c8
taiwania <taiwaniajusto@gmail.com>
2023-08-06 14:26:41 +0800

    Add restaurants index page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,6 +1,20 @@
+const { Category, Restaurant } = require('../models')
+
 const restaurantController = {
   getRestaurants: (req, res) => {
-    return res.render('restaurants')
+    return Restaurant.findAll({
+      include: [Category],
+      raw: true,
+      nest: true
+    })
+      .then(restaurant => {
+        const data = restaurant.map(r => ({
+          ...r,
+          description: r.description.substring(0, 50)
+        }))
+        return res.render('restaurants', { restaurants: data })
+      })
   }
 }
+
 module.exports = restaurantController
