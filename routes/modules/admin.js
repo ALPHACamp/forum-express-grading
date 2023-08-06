4071b5b0fa38aaf18136383fd540a9e360c84a65
lilymichael87@gmail.com <lilymichael87@gmail.com>
2023-08-06 17:46:37 +0800

    feat:add admin categories page

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const upload = require('../../middleware/multer')
 const adminController = require('../../controllers/admin-controller')
+const categoryController = require('../../controllers/category-controller')
 
 router.get('/restaurants/create', adminController.createRestaurant)
 router.get('/restaurants/:id/edit', adminController.editRestaurant)
@@ -10,8 +11,12 @@ router.delete('/restaurants/:id', adminController.deleteRestaurant)
 router.get('/restaurants/:id', adminController.getRestaurant)
 router.get('/restaurants', adminController.getRestaurants)
 router.post('/restaurants', upload.single('image'), adminController.postRestaurant)
+
 router.get('/users', adminController.getUsers)
 router.patch('/users/:id', adminController.patchUser)
+
+router.get('/categories', categoryController.getCategories)
+
 router.use('/', (req, res) => res.redirect('/admin/restaurants'))
 
 module.exports = router
