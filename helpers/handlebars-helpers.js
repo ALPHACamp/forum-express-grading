52d0c7988591fce0be0b4a569fc668f06f5bdee1
gibbs-shih <shih.gibbs@gmail.com>
2023-08-08 00:09:45 +0800

    feat: add ifCond hbs helper & update category selector

--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -1,4 +1,7 @@
 const dayjs = require('dayjs') // 載入 dayjs 套件
 module.exports = {
-  currentYear: () => dayjs().year() // 取得當年年份作為 currentYear 的屬性值，並導出
+  currentYear: () => dayjs().year(), // 取得當年年份作為 currentYear 的屬性值，並導出
+  ifCond: function (a, b, option) {
+    return a === b ? option.fn(this) : option.inverse(this)
+  }
 }
