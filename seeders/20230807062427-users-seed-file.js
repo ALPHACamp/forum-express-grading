2a4543351873aa79b24beae5272594fead931ecd
Annie.H <anniehuang0410@gmail.com>
2023-08-07 14:39:36 +0800

    feat: add seed data

---
+++ seeders/20230807062427-users-seed-file.js
@@ -0,0 +1,35 @@
+'use strict'
+
+// for setting password
+const bcrypt = require('bcryptjs')
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('Users', [{
+      email: 'root@example.com',
+      password: await bcrypt.hash('12345678', 10),
+      is_admin: true,
+      name: 'root',
+      created_at: new Date(),
+      updated_at: new Date()
+    }, {
+      email: 'user1@example.com',
+      password: await bcrypt.hash('12345678', 10),
+      is_admin: false,
+      name: 'user1',
+      created_at: new Date(),
+      updated_at: new Date()
+    }, {
+      email: 'user2@example.com',
+      password: await bcrypt.hash('12345678', 10),
+      is_admin: false,
+      name: 'user2',
+      created_at: new Date(),
+      updated_at: new Date()
+    }], {})
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Users', {}) // 清空資料表中所有資料，第二個參數可以指定 where 條件
+  }
+}
