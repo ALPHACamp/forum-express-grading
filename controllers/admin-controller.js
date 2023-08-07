cdcc2d951b6aef0992d00bc3736e647f863e4805
claire <claire210401@gmail.com>
2023-08-08 00:08:43 +0800

    finish R01

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,4 +1,6 @@
-const { Restaurant } = require('../models')
+const { Restaurant, User } = require('../models')
+// 加入 user model
+
 const { imgurFileHandler } = require('../helpers/file-helpers')
 
 const adminController = {
@@ -9,6 +11,33 @@ const adminController = {
     })
       .then(restaurants => res.render('admin/restaurants', { restaurants }))
       .catch(err => next(err))
+  },
+  // 讀取使用者功能
+  getUsers: (req, res, next) => {
+    return User.findAll({ raw: true })
+      .then(users => {
+        res.render('admin/users', { users })
+      })
+      .catch(err => next(err))
+  },
+  // 修改使用者功能
+  patchUser: (req, res, next) => {
+    // console.log(req.params.id)
+    return User.findByPk(req.params.id)
+      .then(user => {
+        if (!user) throw new Error(" User didn't exsit!")
+
+        if (user.email === 'root@example.com') {
+          req.flash('error_messages', '禁止變更 root 權限')
+          return res.redirect('back')
+        } else {
+          return user.update({ isAdmin: !user.isAdmin })
+        }
+      }).then(() => {
+        req.flash('success_messages', '使用者權限變更成功')
+        return res.redirect('/admin/users')
+      })
+      .catch(err => { return next(err) })
   },
   // 渲染新增表單
   createRestaurant: (req, res) => {
--- middleware/auth.js
+++ middleware/auth.js
@@ -12,12 +12,12 @@ const authenticatedAdmin = (req, res, next) => {
   // 使用者是否為 ADMIN
   if (helpers.ensureAuthenticated(req)) {
     // 使用者是 ADMIN
-    if (helpers.getUser(req).isAdmin) return next();
+    if (helpers.getUser(req).isAdmin) return next()
     // 使用者不是 ADMIN
-    res.redirect("/");
+    res.redirect('/')
   } else {
     // 使用者沒登入
-    res.redirect("/signin");
+    res.redirect('/signin')
   }
 }
 module.exports = {
