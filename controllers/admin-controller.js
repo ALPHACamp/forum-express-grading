55216d9cde515d23b8f4d0b9c4992a20fca9200b
WinstonAdams <apollo07291@gmail.com>
2023-08-07 22:52:14 +0800

    feat: add admin users page and update user permission

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,13 +1,44 @@
-const { Restaurant } = require('../models')
+const { Restaurant, User } = require('../models')
 const { imgurFileHandler } = require('../helpers/file-helpers')
 
 const adminController = {
-  // 進入管理者首頁
+  // 進入管理者首頁(餐廳列表)
   getRestaurants: (req, res, next) => {
     Restaurant.findAll({
       raw: true
     })
       .then(restaurants => res.render('admin/restaurants', { restaurants }))
+      .catch(err => next(err))
+  },
+
+  // 進入使用者列表
+  getUsers: (req, res, next) => {
+    return User.findAll({
+      raw: true
+    })
+      .then(users => res.render('admin/users', { users }))
+      .catch(err => next(err))
+  },
+
+  // 修改使用者權限
+  patchUser: (req, res, next) => {
+    return User.findByPk(req.params.id)
+      .then(user => {
+        if (!user) throw new Error("User didn't exist!")
+        if (user.email === 'root@example.com') {
+          req.flash('error_messages', '禁止變更 root 權限')
+          return res.redirect('back')
+        }
+
+        const isAdmin = !user.isAdmin
+        return user.update({
+          isAdmin
+        })
+      })
+      .then(() => {
+        req.flash('success_messages', '使用者權限變更成功')
+        res.redirect('/admin/users')
+      })
       .catch(err => next(err))
   },
 
