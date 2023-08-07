903ac6ea3c9064b046388e376bfb51b0ca69041d
WinstonAdams <apollo07291@gmail.com>
2023-08-07 22:32:19 +0800

    feat: add admin users page and update user permission

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -5,12 +5,22 @@ const adminController = require('../../controllers/admin-controller')
 const upload = require('../../middleware/multer')
 
 router.get('/restaurants/create', adminController.createRestaurant)
+
 router.get('/restaurants/:id/edit', adminController.editRestaurant)
+
 router.get('/restaurants/:id', adminController.getRestaurant)
+
 router.put('/restaurants/:id', upload.single('image'), adminController.putRestaurant) //! upload.single('image')：接收表單中 name='image' 的一個檔案，並將檔案資訊存在 req.file 中
+
 router.delete('/restaurants/:id', adminController.deleteRestaurant)
-router.get('/restaurants', adminController.getRestaurants)
+
 router.post('/restaurants', upload.single('image'), adminController.postRestaurant)
+
+router.get('/users', adminController.getUsers)
+
+router.patch('/users/:id', adminController.patchUser)
+
+router.get('/restaurants', adminController.getRestaurants)
 router.use('/', (req, res) => res.redirect('/admin/restaurants'))
 
 module.exports = router
