ea9d58ac71cfb60d73e6e4413fe90a9c8611c3a8
WangRping <r17369@gmail.com>
2023-08-07 15:07:19 +0800

    feat:add restaurant page

--- controllers/restaurnat-controller.js
+++ controllers/restaurnat-controller.js
@@ -5,12 +5,24 @@ const restaurantController = {
     return Restaurant.findAll({
       raw: true,
       nest: true,
-      incloude: Category
+      include: Category
     })
       .then(restaurants => {
         const data = restaurants.map(r => ({ ...r, description: r.description.substring(0, 50) }))
         return res.render('restaurants', { restaurants: data })
       })
+  },
+  getRestaurant: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, {
+      raw: true,
+      nest: true,
+      include: [Category]
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error('restaurant is not exists')
+        return res.render('restaurant', { restaurant })
+      })
+      .catch(err => next(err))
   }
 }
 module.exports = restaurantController
