3f5c0667b0eaeaa2a8e2ae8ff5453d1e5ced8761
AlanChiangg <alaln35alan@gmail.com>
2023-08-07 23:07:53 +0800

    feat: add users-profile page and edit page

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,8 @@ const commentController = require('../controllers/comment-controller')
 const { authenticated, authenticatedAdmin } = require('../middleware/auth')
 const { generalErrorHandler } = require('../middleware/error-handler')
 
+const upload = require('../middleware/multer')
+
 router.use('/admin', authenticatedAdmin, admin)
 
 router.get('/signup', userController.signUpPage)
@@ -22,6 +24,10 @@ router.get('/restaurants', authenticated, restController.getRestaurants)
 
 router.delete('/comments/:id', authenticatedAdmin, commentController.deleteComment)
 router.post('/comments', authenticated, commentController.postComment)
+
+router.get('/users/:id/edit', authenticated, userController.editUser)
+router.get('/users/:id', authenticated, userController.getUser)
+router.put('/users/:id', upload.single('image'), userController.putUser)
 
 router.use('/', (req, res) => res.redirect('/restaurants'))
 router.use('/', generalErrorHandler)
