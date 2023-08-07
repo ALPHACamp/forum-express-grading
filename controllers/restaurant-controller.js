d6dfeb8f56b546f1f5d52b316225554d3bb9fb1e
taiwania <taiwaniajusto@gmail.com>
2023-08-07 20:13:23 +0800

    R02 homework is finished R02 作業

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -18,13 +18,28 @@ const restaurantController = {
 
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
+      include: [Category]
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        return restaurant.increment('viewCounts', { by: 1 })
+          .then(() => {
+            const updatedRestaurant = restaurant.get({ plain: true })
+            return res.render('restaurant', { restaurant: updatedRestaurant })
+          })
+      })
+      .catch(err => next(err))
+  },
+
+  getDashboard: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, {
       include: [Category],
       raw: true,
       nest: true
     })
       .then(restaurant => {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
-        return res.render('restaurant', { restaurant })
+        return res.render('dashboard', { restaurant })
       })
       .catch(err => next(err))
   }
