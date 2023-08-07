f9917f08a45ce761864817d9763a32648f2f4983
claire <claire210401@gmail.com>
2023-08-07 17:26:28 +0800

    feat:adjust middleware/auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,22 +1,23 @@
-const { ensureAuthenticated, getUser } = require('../helpers/auth-helpers')
+// const { ensureAuthenticated, getUser } = require('../helpers/auth-helpers')
+const helpers = require('../helpers/auth-helpers')
 
 const authenticated = (req, res, next) => {
   // 使用者是否登入
-  if (ensureAuthenticated(req)) {
+  if (helpers.ensureAuthenticated(req)) {
     return next()
   }
   res.redirect('/signin')
 }
 const authenticatedAdmin = (req, res, next) => {
   // 使用者是否為 ADMIN
-  if (ensureAuthenticated(req)) {
+  if (helpers.ensureAuthenticated(req)) {
     // 使用者是 ADMIN
-    if (getUser(req).isAdmin) return next()
+    if (helpers.getUser(req).isAdmin) return next();
     // 使用者不是 ADMIN
-    res.redirect('/')
+    res.redirect("/");
   } else {
     // 使用者沒登入
-    res.redirect('/signin')
+    res.redirect("/signin");
   }
 }
 module.exports = {
