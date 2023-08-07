bbe6405618c2f13fd6c554c156e201d8bf81a567
iamcoolalan <iamalan1996@gmail.com>
2023-08-07 19:48:15 +0800

    feat: category update

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -1,11 +1,17 @@
 const { Category } = require('../models')
 
-module.exports = {
+const categoryController = {
   getCategories: (req, res, next) => {
-    Category.findAll({
-      raw: true
-    })
-      .then(categories => res.render('admin/categories', { categories }))
+    Promise.all([
+      Category.findAll({ raw: true }),
+      req.params.id ? Category.findByPk(req.params.id, { raw: true }) : null
+    ])
+      .then(([categories, category]) => {
+        res.render('admin/categories', {
+          categories,
+          category
+        })
+      })
       .catch(err => next(err))
   },
   postCategory: (req, res, next) => {
@@ -15,5 +21,23 @@ module.exports = {
     return Category.create({ name })
       .then(() => res.redirect('/admin/categories'))
       .catch(err => next(err))
+  },
+  putCategory: (req, res, next) => {
+    const { name } = req.body
+    if (!name) throw new Error('Category name is required!')
+
+    return Category.findByPk(req.params.id)
+      .then(category => {
+        if (!category) throw new Error("Category doesn't exist")
+
+        return category.update({ name })
+      })
+      .then(() => {
+        req.flash('success_messages', '類別更改成功')
+        res.redirect('/admin/categories')
+      })
+      .catch(err => next(err))
   }
 }
+
+module.exports = categoryController
