c0db0ec4e5f381f27984ac8e41b9d5749835b510
yang haotse <ha91252@gmail.com>
2023-08-07 18:06:57 +0800

    feat: add getUser profile

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,7 @@ const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
 const commentController = require('../controllers/comment-controller')
 const admin = require('./modules/admin')
+
 const { generalErrorHandler } = require('../middleware/error-handler')
 const { authenticated, authenticatedAdmin } = require('../middleware/auth')
 
@@ -16,6 +17,8 @@ router.post('/signup', userController.signUp)
 router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
 router.get('/logout', userController.logout)
+
+router.get('/users/profile/:id', userController.getUser)
 
 router.delete('/comments/:id', authenticated, commentController.deleteComment)
 router.post('/comments', authenticated, commentController.postComment)
