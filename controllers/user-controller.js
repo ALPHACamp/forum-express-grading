233148702afdfd76eb61ff6e5b85e33c9f7dcae6
susu725 <zxcv1010017@gmail.com>
2023-08-08 00:57:42 +0800

    feat: flash msg & signup verification

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -5,12 +5,20 @@ const userController = {
   signUpPage: (req, res) => {
     return res.render('signup')
   },
-  signUp: (req, res) => {
-    const { name, email, password } = req.body
-    bcrypt.hash(password, 10)
+  signUp: (req, res, next) => {
+    const { name, email, password, passwordCheck } = req.body
+    if (password !== passwordCheck) throw new Error('Passwords do not match!')
+    User.findOne({ where: { email } })
+      .then(user => {
+        if (user) throw new Error('Email already exists!')
+        return bcrypt.hash(password, 10)
+      })
       .then(hash => User.create({ name, email, password: hash }))
-      .then(() => res.redirect('/signin'))
-      .catch(err => console.log(err))
+      .then(() => {
+        req.flash('success_messages', '成功註冊帳號！')
+        res.redirect('/signin')
+      })
+      .catch(err => next(err))
   },
   signInPage: (req, res) => {
     return res.render('signin')
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const router = express.Router()
 
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
+const { generalErrorHandler } = require('../middleware/error-handler')
 const admin = require('./modules/admin')
 
 router.use('/admin', admin)
@@ -13,6 +14,8 @@ router.post('/signup', userController.signUp)
 router.get('/signin', userController.signInPage)
 
 router.get('/restaurants', restController.getRestaurants)
+
+router.use('/', generalErrorHandler)
 
 // fallback路由，當匹配不到時就會執行這一行
 // 跟router.get的差別在於get只有限定'/'，use的範圍相對廣泛
