e7c09871213c75b5f8f827cce6f34ed537199a03
HotG8119 <k508060@gmail.com>
2023-08-08 21:21:30 +0800

    feat: add  dashboard & view counts

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,7 +1,7 @@
 const { Restaurant, Category } = require('../models')
 
 const restaurantController = {
-  getRestaurants: (req, res) => {
+  getRestaurants: (req, res, next) => {
     Restaurant.findAll({
       raw: true,
       nest: true,
@@ -14,17 +14,31 @@ const restaurantController = {
         }))
       return res.render('restaurants', { restaurants: data })
     })
+      .catch(error => next(error))
   },
-  getRestaurant: (req, res) => {
+  getRestaurant: (req, res, next) => {
     console.log(req.params.id)
     Restaurant.findByPk(req.params.id, {
-      raw: true,
       nest: true,
       include: [Category]
     }).then(restaurant => {
       if (!restaurant) throw new Error('restaurant not found')
-      return res.render('restaurant', { restaurant })
+
+      restaurant.update({ viewCounts: restaurant.viewCounts + 1 })
+      return res.render('restaurant', { restaurant: restaurant.toJSON() })
     })
+      .catch(error => next(error))
+  },
+  getDashboard: (req, res, next) => {
+    Restaurant.findByPk(req.params.id, {
+      nest: true,
+      include: [Category]
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error('restaurant not found')
+        return res.render('dashboard', { restaurant: restaurant.toJSON() })
+      })
+      .catch(error => next(error))
   }
 }
 module.exports = restaurantController
