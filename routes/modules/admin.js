9b817bfa2c6768a7140278b74820debe6ed7ffda
Keiko <keitakeiko@hotmail.com>
2023-08-08 23:16:24 +0800

    feat: category delete

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -22,6 +22,7 @@ router.get('/categories/:id', categoryController.getCategories)
 router.put('/categories/:id', categoryController.putCategory)
 router.get('/categories', categoryController.getCategories)
 router.post('/categories', categoryController.postCategory)
+router.delete('/categories/:id', categoryController.deleteCategory)
 
 router.get('/restaurants', adminController.getRestaurants)
 router.post('/restaurants', upload.single('image'), adminController.postRestaurant)
