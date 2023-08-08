6dd9669e7ecd6c1aab0d02f038998626258f4aa7
claire <claire210401@gmail.com>
2023-08-08 17:18:36 +0800

    R01

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -9,6 +9,10 @@ const upload = require('../../middleware/multer')
 router.get('/restaurants/create', adminController.createRestaurant)
 // 導向後臺修改頁功能
 router.get('/restaurants/:id/edit', adminController.editRestaurant)
+// 導向後臺使用者功能 - 修改單一使用者部分狀態
+router.patch('/users/:id', adminController.patchUser)
+// 導向後臺使用者功能 - 瀏覽所有使用者
+router.get('/users', adminController.getUsers)
 // 導向後臺詳細頁功能 - 順序重要!!!
 router.get('/restaurants/:id', adminController.getRestaurant)
 // 提交 修改表單
@@ -19,6 +23,7 @@ router.put(
 )
 // 刪除資料
 router.delete('/restaurants/:id', adminController.deleteRestaurant)
+
 // 導向後臺首頁功能 - 瀏覽所有餐廳
 router.get('/restaurants', adminController.getRestaurants)
 // 新增一筆餐廳
@@ -27,6 +32,7 @@ router.post(
   upload.single('image'),
   adminController.postRestaurant
 )
+
 // 其餘重新導回 /restaurants
 router.get('', (req, res) => res.redirect('/admin/restaurants'))
 
