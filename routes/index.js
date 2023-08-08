b5bfdb99ca4fe26602de3e0eb0740816db561cbf
lilymichael87@gmail.com <lilymichael87@gmail.com>
2023-08-08 16:05:23 +0800

    feat: R03 test done

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const router = express.Router()
+const upload = require('../middleware/multer')
 
 // 新增，載入 controller
 const restController = require('../controllers/restaurant-controller')
@@ -23,6 +24,10 @@ router.get('/restaurants/:id/dashboard', authenticated, restController.getDashbo
 router.get('/restaurants/:id', authenticated, restController.getRestaurant)
 router.get('/restaurants', authenticated, restController.getRestaurants)
 router.get('/', (req, res) => res.redirect('/restaurants'))
+
+router.get('/users/:id/edit', authenticated, userController.editUser)
+router.get('/users/:id', authenticated, userController.getUser)
+router.put('/users/:id', upload.single('image'), authenticated, userController.putUser)
 
 router.delete('/comments/:id', authenticatedAdmin, commentController.deleteComment)
 router.post('/comments', authenticated, commentController.postComment)
