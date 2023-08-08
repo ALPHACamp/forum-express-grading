c1bd59441b60247246bee0bc4e452db24e1948cd
Peter-Haung888 <ming31437@gmail.com>
2023-08-08 17:08:10 +0800

    feat: add header & footer

--- app.js
+++ app.js
@@ -4,13 +4,16 @@ const flash = require('connect-flash')
 const session = require('express-session')
 const passport = require('./config/passport')
 
+const { getUser } = require('./helpers/auth-helpers')
+const handlebarsHelpers = require('./helpers/handlebars-helpers')
+
 const routes = require('./routes')
 
 const app = express()
 const port = process.env.PORT || 3000
 const SESSION_SECRET = 'secret'
 
-app.engine('hbs', exphbs({ extname: '.hbs' }))
+app.engine('hbs', exphbs({ extname: '.hbs', helpers: handlebarsHelpers }))
 app.set('view engine', 'hbs')
 
 app.use(express.urlencoded({ extended: true }))
@@ -21,6 +24,7 @@ app.use(passport.session())
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')// 設定 success_msg 訊息
   res.locals.error_messages = req.flash('error_messages')// 設定 warning_msg 訊息
+  res.locals.user = getUser(req)
   next()
 })
 app.use(routes)
