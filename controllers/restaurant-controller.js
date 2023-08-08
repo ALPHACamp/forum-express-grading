87050b54e24829b73c44bafa2e69069c2907c408
iamcoolalan <iamalan1996@gmail.com>
2023-08-08 20:01:29 +0800

    feat: add categories navbar on restaurants index page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,20 +1,36 @@
 const { Restaurant, Category } = require('../models')
-const restaurant = require('../models/restaurant')
 
 const restaurantController = {
   getRestaurants: (req, res, next) => {
-    Restaurant.findAll({
-      include: [Category],
-      raw: true,
-      nest: true
-    })
-      .then(restaurants => {
+    const categoryId = Number(req.query.categoryId) || ''
+
+    /*
+    const where = {}
+    if (categoryId) where.categoryId = categoryId
+    */
+
+    Promise.all([
+      Restaurant.findAll({
+        include: [Category],
+        where: {
+          ...categoryId ? { categoryId } : {}
+        },
+        raw: true,
+        nest: true
+      }),
+      Category.findAll({ raw: true })
+    ])
+      .then(([restaurants, categories]) => {
         const data = restaurants.map(r => ({
           ...r,
           description: r.description.substring(0, 50)
         }))
 
-        res.render('restaurants', { restaurants: data })
+        res.render('restaurants', {
+          restaurants: data,
+          categories,
+          categoryId
+        })
       })
       .catch(err => next(err))
   },
@@ -30,7 +46,7 @@ const restaurantController = {
       .then(restaurant => res.render('restaurant', { restaurant: restaurant.toJSON() }))
       .catch(err => next(err))
   },
-  getDashboard: async (req, res, next) => {
+  getDashboard: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
       include: [Category],
       raw: true,
