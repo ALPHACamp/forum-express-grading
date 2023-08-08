03b1492fff91385d8144874110d5ab6a43ba6577
RobertOu0213 <starshining12345@gmail.com>
2023-08-08 10:51:24 +0800

    feat: add feeds page

--- routes/index.js
+++ routes/index.js
@@ -22,6 +22,7 @@ router.put('/users/:id', authenticated, upload.single('image'), userController.p
 
 router.use('/admin', authenticatedAdmin, admin)
 
+router.get('/restaurants/feeds', authenticated, restController.getFeeds)
 router.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard)
 router.get('/restaurants/:id', authenticated, restController.getRestaurant)
 router.get('/restaurants', authenticated, restController.getRestaurants)
