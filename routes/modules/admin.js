72d5925f0bf512745c7ba525a16f0a19312cfc21
Keiko <keitakeiko@hotmail.com>
2023-08-08 22:55:08 +0800

    feat:category create

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -17,7 +17,9 @@ router.delete('/restaurants/:id', adminController.deleteRestaurant)
 router.patch('/users/:id', adminController.patchUser)
 
 router.get('/users', adminController.getUsers)
+
 router.get('/categories', categoryController.getCategories)
+router.post('/categories', categoryController.postCategory)
 
 router.get('/restaurants', adminController.getRestaurants)
 router.post('/restaurants', upload.single('image'), adminController.postRestaurant)
