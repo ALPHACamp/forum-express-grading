b39ab89e8582768822f6e2022065447a84873f05
andybox111 <andybox11111@gmail>
2023-08-08 17:42:46 +0800

    feat: user model add is_admin

--- config/passport.js
+++ config/passport.js
@@ -29,7 +29,8 @@ passport.serializeUser((user, cb) => {
 })
 passport.deserializeUser((id, cb) => {
   User.findByPk(id).then(user => {
-    console.log(user) // 暫時添加
+    user = user.toJSON()
+    // console.log(user) // 暫時添加
     return cb(null, user)
   })
 })
