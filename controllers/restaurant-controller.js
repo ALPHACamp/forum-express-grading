04c65a97285cc084385d4418cc40eae817b69fb2
go2Cshop <shell800808@gmail.com>
2023-08-09 15:47:47 +0800

    feat:add restaurants index page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,6 +1,20 @@
+const { Restaurant, Category } = require('../models')
 const restaurantController = {
   getRestaurants: (req, res) => {
-    return res.render('restaurants')
+    return Restaurant.findAll({
+      include: Category,
+      nest: true,
+      raw: true
+    }).then(restaurants => {
+      const data = restaurants.map(r => ({
+        ...r,
+        // 當 key 重複時，會以後面出現的為準
+        description: r.description.substring(0, 50)
+      }))
+      return res.render('restaurants', {
+        restaurants: data
+      })
+    })
   }
 }
 module.exports = restaurantController
