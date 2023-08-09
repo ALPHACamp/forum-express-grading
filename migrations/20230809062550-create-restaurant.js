98a208bd31ee82dbd8b1434d73d3fd9b17cc18f5
andybox111 <andybox11111@gmail>
2023-08-09 14:28:04 +0800

    feat: modify admin restaurants page

--- migrations/20230809062550-create-restaurant.js
+++ migrations/20230809062550-create-restaurant.js
@@ -1,7 +1,7 @@
 'use strict'
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('Restautants', {
+    await queryInterface.createTable('Restaurants', {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -34,6 +34,6 @@ module.exports = {
     })
   },
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('Restautants')
+    await queryInterface.dropTable('Restaurants')
   }
 }
