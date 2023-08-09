c895a1f1b143883a63a0299285b6afea51883539
yang haotse <ha91252@gmail.com>
2023-08-09 18:15:23 +0800

    change category's name to English(test heroku)

--- seeders/20230802122301-categories-seed-file.js
+++ seeders/20230802122301-categories-seed-file.js
@@ -3,7 +3,7 @@
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert('Categories',
-      ['中式料理', '日本料理', '義大利料理', '墨西哥料理', '素食料理', '美式料理', '複合式料理']
+      ['Chinese', 'Japanese', 'Italian', 'Mexican', 'vegetarian', 'American', 'Complex']
         .map(item => {
           return {
             name: item,
