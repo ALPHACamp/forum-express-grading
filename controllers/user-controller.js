5a763719830a7d2887ee4feb66ccbc7aa0fae620
WangRping <r17369@gmail.com>
2023-08-09 22:19:51 +0800

    feat: add user profie page

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcryptjs')
 const db = require('../models')
-const { User } = db
+const { imgurFileHandler } = require('../helpers/file-helpers')
+const { User, Comment, Restaurant } = db
 
 const userController = {
   signUpPage: (req, res) => {
@@ -38,6 +39,62 @@ const userController = {
     req.flash('success_messages', '登出成功!')
     req.logout()
     res.redirect('/signin')
+  },
+  getUser: (req, res, next) => {
+    const DEFAULT_AVATAR = 'https://i.imgur.com/FUerPDO.png'
+    const id = req.params.id
+    return User.findByPk(id, {
+      include: [
+        Comment, {
+          model: Comment,
+          raw: true,
+          include: Restaurant
+        }
+      ]
+    })
+      .then(user => {
+        if (!user) throw new Error("user doesn't exist")
+        user = user.toJSON()
+        user.image = user.image || DEFAULT_AVATAR
+        res.render('users/profile', { user, comments: user.Comments })
+      })
+      .catch(err => next(err))
+  },
+  editUser: (req, res) => {
+    return User.findByPk(req.params.id, { raw: true })
+      .then(user => {
+        if (!user) throw new Error('User不存在')
+        res.render('users/edit', { user })
+      })
+  },
+  putUser: (req, res, next) => {
+    const { file } = req
+    const { name } = req.body
+    const userId = req.params.id
+
+    if (req.user.id !== Number(userId)) {
+      req.flash('error_messages', '無權請求')
+      res.redirect('/restaurants')
+    }
+
+    if (!name) throw new Error('姓名是必要欄位')
+
+    return Promise.all([
+      User.findByPk(userId),
+      imgurFileHandler(file)
+    ])
+      .then(([user, filePath]) => {
+        if (!user) throw new Error('使用者不存在')
+        return user.update({
+          name,
+          image: filePath || user.image
+        })
+      })
+      .then(() => {
+        req.flash('success_messages', '使用者資料編輯成功')
+        res.redirect(`/users/${userId}`)
+      })
+      .catch(err => next(err))
   }
 }
 
