4fd09b825c93af73ec9f2758a9bc0465aef354f6
Easonlu0425 <andy1234532@hotmail.com>
2023-08-09 21:14:09 +0800

    Revert "undo the sync"
    This reverts commit 42caa1237968bd61be2117c11982c5024ff0c50c.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const path = require('path')
 const express = require('express')
 const routes = require('./routes')
+const sequelize = require('sequelize')
 const handlebars = require('express-handlebars')
 const flash = require('connect-flash')
 const methodOverride = require('method-override')
@@ -37,8 +38,10 @@ app.use((req, res, next) => {
 
 app.use(routes)
 
-app.listen(port, () => {
-  console.info(`Example app listening on port ${port}!`)
+sequelize.sync().then(() => {
+  app.listen(port, () => {
+    console.info(`Example app listening on port ${port}!`)
+  })
 })
 
 module.exports = app
