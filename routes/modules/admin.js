bb90e4dfcfd73b87e49b810fa08d602f8dff81dd
mmm999xp <mmm999xp@gmail.com>
2023-08-10 00:19:15 +0800

    category create

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -14,5 +14,6 @@ router.post('/restaurants', upload.single('image'), adminController.postRestaura
 router.get('/users', adminController.getUsers)
 router.patch('/users/:id', adminController.patchUser)
 router.get('/categories', categoryController.getCategories)
+router.post('/categories', categoryController.postCategory)
 router.use('/', (req, res) => res.redirect('/admin/restaurants'))
 module.exports = router
