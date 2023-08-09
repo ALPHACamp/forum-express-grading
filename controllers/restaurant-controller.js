d96bef7c73e94b4e632066f9b14b34217567d14f
go2Cshop <shell800808@gmail.com>
2023-08-09 21:23:35 +0800

    feat R02 complete main

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -18,15 +18,26 @@ const restaurantController = {
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
-      include: Category,
-      nest: true,
-      raw: true
+      include: Category
     })
       .then(restaurant => {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
-        res.render('restaurant', {
-          restaurant
-        })
+        return restaurant.increment('viewCounts')
+      })
+      .then(restaurant => {
+        res.render('restaurant', { restaurant: restaurant.toJSON() })
+      })
+      .catch(err => next(err))
+  },
+  getDashboard: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, {
+      raw: true,
+      nest: true,
+      include: Category
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        res.render('dashboard', { restaurant })
       })
       .catch(err => next(err))
   }
