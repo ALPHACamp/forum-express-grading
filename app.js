a444cbf12893ad5c60c634375055147a3185bcbb
andybox111 <andybox11111@gmail>
2023-08-09 17:34:57 +0800

    feat: admin update restaurant

--- app.js
+++ app.js
@@ -9,16 +9,17 @@ const { getUser } = require('./helpers/auth-helpers')
 const app = express()
 const port = process.env.PORT || 3000
 const SESSION_SECRET = 'secret'
+const methodOverride = require('method-override')
 const db = require('./models')
-// 註冊 Handlebars 樣板引擎，並指定副檔名為 .hbs
+
 app.engine('hbs', handlebars({ extname: '.hbs', helpers: handlebarsHelpers }))
-// 設定使用 Handlebars 做為樣板引擎
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
 app.use(passport.initialize())
 app.use(passport.session())
-app.use(flash()) // 掛載套件
+app.use(methodOverride('_method'))
+app.use(flash()) 
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages') // 設定 success_msg 訊息
   res.locals.error_messages = req.flash('error_messages') // 設定 warning_msg 訊息
