85d40b95c9bd4e3da8bd587b1b6989c4e14001cd
scheng0718 <scheng0718@gmail.com>
2023-08-09 17:51:40 -0700

    feat: category create

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -9,6 +9,22 @@ const categoryController = {
         res.render('admin/categories', { categories })
       })
       .catch(err => next(err))
+  },
+  postCategory: (req, res, next) => {
+    const { name } = req.body
+    if (!name) throw new Error('Category name is required!')
+    return Category.findOne({
+      where: { name }
+    })
+      .then(category => {
+        if (category) throw new Error('The category name already exists!')
+        return Category.create({ name })
+      })
+      .then(() => {
+        req.flash('success_messages', 'New category is created successfully!')
+        res.redirect('/admin/categories')
+      })
+      .catch(err => next(err))
   }
 }
 
