5ba6b7b912cb7e0e97f25ee8f09974683c81367b
Annie <anniehuang0410@gmail.com>
2023-08-10 13:46:40 +0800

    feat: category update

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -19,6 +19,8 @@ router.patch('/users/:id', adminController.patchUser)
 router.get('/users', adminController.getUsers)
 
 // category related
+router.get('/categories/:id', categoryController.getCategories)
+router.put('/categories/:id', categoryController.putCategory)
 router.get('/categories', categoryController.getCategories)
 router.post('/categories', categoryController.postCategory)
 
