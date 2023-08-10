0333beda18d465d00245c12cea81b84fa9d32280
WinstonAdams <apollo07291@gmail.com>
2023-08-10 22:34:08 +0800

    feat: add restaurants index page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,6 +1,21 @@
+const { Restaurant, Category } = require('../models')
+
 const restaurantController = {
   getRestaurants: (req, res) => {
-    res.render('restaurants')
+    return Restaurant.findAll({
+      include: Category,
+      nest: true,
+      raw: true
+    })
+      .then(restaurants => {
+        const data = restaurants.map(r => ({
+          ...r,
+          // 當 key 重複時，後面出現的會取代前面的
+          description: r.description.substring(0, 50) // description 擷取前 50 個字元
+        }))
+
+        return res.render('restaurants', { restaurants: data })
+      })
   }
 }
 
