690eb644759880731d318f23e938da15e006a6fe
w3i3538 <wei5566@gmail.com>
2023-08-10 16:22:21 +0800

    feat: add delete comment for admin

--- routes/index.js
+++ routes/index.js
@@ -24,6 +24,7 @@ router.get('/restaurants/:id', authenticated, restController.getRestaurant)
 router.get('/restaurants', authenticated, restController.getRestaurants)
 router.get('/', (req, res) => res.redirect('/restaurants'))
 
+router.delete('/comments/:id', authenticatedAdmin, commentController.deleteComment)
 router.post('/comments', authenticated, commentController.postComment)
 
 router.use('/', generalErrorHandler)
