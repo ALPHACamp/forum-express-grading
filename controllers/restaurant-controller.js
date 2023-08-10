4eff71ebb6a1131673006ed0fc47d2fa437a7a68
Annie <anniehuang0410@gmail.com>
2023-08-10 14:17:32 +0800

    feat: add restaurants index page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,6 +1,20 @@
+const { Restaurant, Category } = require('../models') // 引入資料庫
+
 const restaurantController = {
-  getRestaurants: (req, res) => {
-    return res.render('restaurants')
+  getRestaurants: (req, res, next) => {
+    return Restaurant.findAll({
+      include: Category,
+      nest: true,
+      raw: true
+    }).then(restaurants => {
+      const data = restaurants.map(r => ({
+        ...r,
+        description: r.description.substring(0, 50)
+      }))
+      return res.render('restaurants', {
+        restaurants: data
+      })
+    })
   }
 }
 
