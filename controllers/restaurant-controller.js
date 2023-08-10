97aa469cdc3c3d93c5fe915e5e5efe0e81064f3c
muco0521 <muco0521@gmail.com>
2023-08-10 23:06:16 +0800

    feat: add comment counts

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -62,14 +62,15 @@ const restaurantController = {
   getDashboard: async (req, res, next) => {
     try {
       const restaurant = await Restaurant.findByPk(req.params.id, ({
-        include: Category,
-        nest: true,
-        raw: true
+        include: [
+          Category,
+          Comment
+        ]
       }))
 
       if (!restaurant) throw new Error("Restaurant didn't exist!")
 
-      res.render('dashboard', { restaurant })
+      res.render('dashboard', { restaurant: restaurant.toJSON() })
     } catch (err) {
       next(err)
     }
