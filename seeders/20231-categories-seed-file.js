5837b58108b2fb40597b301d8f818fe8d43c411f
claire <claire210401@gmail.com>
2023-08-10 13:34:07 +0800

    feat: update seed files

---
+++ seeders/20231-categories-seed-file.js
@@ -0,0 +1,30 @@
+'use strict'
+
+module.exports = {
+  // 將多筆資料插入資料表
+  up: async (queryInterface, Sequelize) => {
+    // bulkInsert('資料表的名稱',[])
+    await queryInterface.bulkInsert(
+      'Categories',
+      [
+        '中式料理',
+        '日本料理',
+        '義大利料理',
+        '墨西哥料理',
+        '素食料理',
+        '美式料理',
+        '複合式料理'
+      ].map(item => {
+        return {
+          name: item,
+          created_at: new Date(),
+          updated_at: new Date()
+        }
+      })
+    )
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Categories', {})
+  }
+}
