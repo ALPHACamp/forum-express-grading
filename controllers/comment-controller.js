385c4a2b15d2038b689f93407dd184063c371ace
Yen Lin <lynn575938@outlook.com>
2023-08-10 12:07:26 +0800

    feat: add post comment on restaurant page

---
+++ controllers/comment-controller.js
@@ -0,0 +1,29 @@
+const { Comment, User, Restaurant } = require('../models')
+
+const commentController = {
+  postComment: (req, res, next) => {
+    const userId = req.user.id
+    const { restaurantId, text } = req.body
+
+    if (!text) throw new Error('Comment text is required!')
+
+    return Promise.all([
+      User.findByPk(userId),
+      Restaurant.findByPk(restaurantId)
+    ])
+      .then(([user, restaurant]) => {
+        if (!user) throw new Error("User didn't exist!")
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+
+        return Comment.create({
+          text,
+          userId,
+          restaurantId
+        })
+      })
+      .then(() => res.redirect(`/restaurants/${restaurantId}`))
+      .catch(err => next(err))
+  }
+}
+
+module.exports = commentController
