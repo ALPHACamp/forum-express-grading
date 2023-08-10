26dd6532989f8f336043828b358f017de54eb9dc
pupss91090 <hsuchinzai@gmail.com>
2023-08-10 23:42:21 +0800

    add dashboard page & viewCounts model

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -18,16 +18,34 @@ const restaurantController = {
       .catch(err => next(err))
   },
   getRestaurant: (req, res, next) => {
-    Restaurant.findByPk(req.params.id, { // 去資料庫用 id 找一筆資料
+    return Restaurant.findByPk(req.params.id)
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!") //  如果找不到，回傳錯誤訊息，後面不執行
+        const vc = restaurant.viewCounts + 1
+        return restaurant.update({ viewCounts: vc })
+      })
+      .then(() => {
+        return Restaurant.findByPk(req.params.id, {
+          raw: true,
+          nest: true,
+          include: [Category]
+        })
+          .then(restaurant => res.render('restaurant', { restaurant }))
+          .catch(err => next(err))
+      })
+  },
+  getDashboard: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, { // 去資料庫用 id 找一筆資料
       raw: true, // 找到以後整理格式再回傳
       nest: true,
       include: [Category]
     })
       .then(restaurant => {
         if (!restaurant) throw new Error("Restaurant didn't exist!") //  如果找不到，回傳錯誤訊息，後面不執行
-        res.render('restaurant', { restaurant })
+        res.render('dashboard', { restaurant })
       })
       .catch(err => next(err))
   }
+
 }
 module.exports = restaurantController
