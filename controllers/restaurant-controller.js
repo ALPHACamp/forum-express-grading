8fc30fa3f4dc560198784391a20fc513a0de14b1
go2Cshop <shell800808@gmail.com>
2023-08-10 11:24:43 +0800

    Commit	Changes feat: add categories navbar on restaurants index page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,20 +1,32 @@
 const { Restaurant, Category } = require('../models')
 const restaurantController = {
-  getRestaurants: (req, res) => {
-    return Restaurant.findAll({
-      include: Category,
-      nest: true,
-      raw: true
-    }).then(restaurants => {
-      const data = restaurants.map(r => ({
-        ...r,
-        // 當 key 重複時，會以後面出現的為準
-        description: r.description.substring(0, 50)
-      }))
-      return res.render('restaurants', {
-        restaurants: data
+  getRestaurants: (req, res, next) => {
+    const categoryId = Number(req.query.categoryId) || ''// req.query回傳字串
+    return Promise.all([
+      Restaurant.findAll({
+        include: Category,
+        where: {
+          ...categoryId ? { categoryId } : {}
+          // 展開運算子比三元運算子優先值低,先檢查 categoryId 是否為空值
+        },
+        nest: true,
+        raw: true
+      }),
+      Category.findAll({ raw: true })
+    ])
+      .then(([restaurants, categories]) => {
+        const data = restaurants.map(r => ({
+          ...r,
+          // 當 key 重複時，會以後面出現的為準
+          description: r.description.substring(0, 50)
+        }))
+        return res.render('restaurants', {
+          restaurants: data,
+          categories,
+          categoryId
+        })
       })
-    })
+      .catch(err => next(err))
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
