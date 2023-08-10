3937c5e618a011724197db51eb14926756a64275
Melodystart <start99start@gmail.com>
2023-08-10 10:59:21 +0800

    feat: user signup

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 3000
 
 app.engine('hbs', handlebars.engine({ extname: '.hbs' }))
 app.set('view engine', 'hbs')
-
+app.use(express.urlencoded({ extended: true })) // body-parser 進行處理
 app.use(routes)
 
 app.listen(port, () => {
