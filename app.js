07b3fa745fdd9933d01dc87ee92751795c320a4a
Melodystart <start99start@gmail.com>
2023-08-10 11:28:38 +0800

    feat: flash msg & signup verification

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
+const flash = require('connect-flash')
+const session = require('express-session')
 const routes = require('./routes')
 
 const app = express()
 const port = process.env.PORT || 3000
+const SESSION_SECRET = 'secret'
 
 app.engine('hbs', handlebars.engine({ extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true })) // body-parser 進行處理
+app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
+app.use(flash())
+app.use((req, res, next) => {
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
+  next()
+})
 app.use(routes)
 
 app.listen(port, () => {
