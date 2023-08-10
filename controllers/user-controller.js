2a4d80f785b3c3e7493b1fd02fcc0b3341606559
iamcoolalan <iamalan1996@gmail.com>
2023-08-10 19:46:06 +0800

    feat: update profile page

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcryptjs')
 
-const { User } = require('../models')
+const { User, Comment, Restaurant } = require('../models')
 const { imgurFileHandler } = require('../helpers/file-helpers')
 
 const userController = {
@@ -42,13 +42,25 @@ const userController = {
     res.redirect('/signin')
   },
   getUser: (req, res, next) => {
-    return User.findByPk(req.params.id, {
-      raw: true
-    })
-      .then(user => {
+    return Promise.all([
+      User.findByPk(req.params.id, {
+        raw: true
+      }),
+      Comment.findAndCountAll({
+        include: Restaurant,
+        where: { userId: req.params.id },
+        raw: true,
+        nest: true
+      })
+    ])
+      .then(([user, userComments]) => {
         if (!user) throw new Error("User didn't exist!")
 
-        res.render('users/profile', { user })
+        res.render('users/profile', {
+          user,
+          comments: userComments.rows,
+          count: userComments.count
+        })
       })
       .catch(err => next(err))
   },
---
+++ seeders/20230810092025-comments-seed-file.js
@@ -0,0 +1,30 @@
+'use strict'
+const faker = require('faker')
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const restaurants = await queryInterface.sequelize.query(
+      'SELECT id FROM Restaurants;',
+      { type: queryInterface.sequelize.QueryTypes.SELECT }
+    )
+
+    const users = await queryInterface.sequelize.query(
+      'SELECT id FROM Users;',
+      { type: queryInterface.sequelize.QueryTypes.SELECT }
+    )
+
+    await queryInterface.bulkInsert('Comments',
+      Array.from({ length: 30 }, () => ({
+        text: faker.lorem.lines(1),
+        user_id: users[Math.floor(Math.random() * users.length)].id,
+        restaurant_id: restaurants[Math.floor(Math.random() * restaurants.length)].id,
+        created_at: new Date(),
+        updated_at: new Date()
+      }))
+    )
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Comments', {})
+  }
+}
