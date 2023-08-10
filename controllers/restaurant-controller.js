3422b6f03a406d7e9d10e89ccb9591b093aa4cd0
pupss91090 <hsuchinzai@gmail.com>
2023-08-10 18:39:39 +0800

    add restaurants index page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,6 +1,21 @@
+const { Restaurant, User, Category } = require('../models')
+const { imgurFileHandler } = require('../helpers/file-helpers')
+
 const restaurantController = {
-  getRestaurants: (req, res) => {
-    return res.render('restaurants')
+  getRestaurants: (req, res, next) => {
+    Restaurant.findAll({
+      raw: true,
+      nest: true,
+      include: [Category]
+    })
+      .then(restaurants => {
+        const data = restaurants.map(r => ({
+          ...r,
+          description: r.description.substring(0, 50)
+        }))
+        return res.render('restaurants', { restaurants: data })
+      })
+      .catch(err => next(err))
   }
 }
 module.exports = restaurantController
