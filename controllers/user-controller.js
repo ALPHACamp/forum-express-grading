e9f7ff14f2d6cf13ef64e9f1ffb2349b5874708a
realyutou <ssps9430@gmail.com>
2023-08-11 12:24:32 +0800

    feat: add user profile page and user can edit their own profile

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,7 +1,7 @@
 const bcrypt = require('bcryptjs')
-const db = require('../models/')
 
-const { User } = db
+const { User } = require('../models/')
+const { imgurFileHandler } = require('../helpers/file-helpers')
 
 const userController = {
   signUpPage: (req, res) => {
@@ -37,6 +37,50 @@ const userController = {
     req.flash('success_messages', '登出成功！')
     req.logout()
     res.redirect('/signin')
+  },
+  getUser: (req, res, next) => {
+    return User.findByPk(req.params.id, { raw: true })
+      .then(user => {
+        if (!user) throw new Error("User doesn't exist!")
+        return res.render('users/profile', { user })
+      })
+      .catch(err => next(err))
+  },
+  editUser: (req, res, next) => {
+    // if (req.user.id !== Number(req.params.id)) {
+    //   req.flash('error_messages', '只能編輯自己的使用者資料！')
+    //   res.redirect(`/users/${req.user.id}`)
+    // }
+    return User.findByPk(req.params.id, { raw: true })
+      .then(user => {
+        if (!user) throw new Error("User doesn't exist!")
+        return res.render('users/edit', { user })
+      })
+      .catch(err => next(err))
+  },
+  putUser: (req, res, next) => {
+    const { name } = req.body
+    const { file } = req
+    if (req.user.id !== Number(req.params.id)) {
+      req.flash('error_messages', '只能編輯自己的使用者資料！')
+      res.redirect(`/users/${req.params.id}`)
+    }
+    return Promise.all([
+      User.findByPk(req.params.id),
+      imgurFileHandler(file)
+    ])
+      .then(([user, filePath]) => {
+        if (!user) throw new Error("User doesn't exist!")
+        return user.update({
+          name: name || user.name,
+          image: filePath || user.image
+        })
+      })
+      .then(() => {
+        req.flash('success_messages', '使用者資料編輯成功')
+        res.redirect(`/users/${req.params.id}`)
+      })
+      .catch(err => next(err))
   }
 }
 
