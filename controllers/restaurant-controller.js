3898c2c6e44e05fcf1993f51b7f04c9b86b2e156
yanyanyaa <yclin3411@gmail.com>
2023-08-11 11:05:42 +0800

    feat: add restaurants index page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,6 +1,23 @@
+/* eslint-disable array-callback-return */
+const { Restaurant, Category } = require('../models')
+
 const restaurantController = {
   getRestaurants: (req, res) => {
-    return res.render('restaurants')
+    return Restaurant.findAll({
+      include: Category,
+      raw: true,
+      nest: true
+    }).then(restaurants => {
+      const data = restaurants.map(r => ({
+        ...r, description: r.description.substring(0, 50)
+      }))
+      // const data = restaurants.map(r => {
+      //   r.description = r.description.substring(0, 50)
+      //   return r
+      // })
+      return res.render('restaurants', { restaurants: data })
+    })
   }
 }
+
 module.exports = restaurantController
