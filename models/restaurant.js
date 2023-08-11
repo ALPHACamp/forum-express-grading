6c36ddcd2605c7865cc574d045f0b11fe44ee154
Annie <anniehuang0410@gmail.com>
2023-08-11 13:54:10 +0800

    feat: add comment model

--- models/restaurant.js
+++ models/restaurant.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate (models) {
       Restaurant.belongsTo(models.Category, { foriegnKey: 'categoryId' })
+      Restaurant.hasMany(models.Commet, { foriegnKey: 'restaurantId' })
     }
   };
   Restaurant.init({
