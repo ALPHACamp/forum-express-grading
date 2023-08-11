92cf4eca9269ba539e3eac68c9f79219c5b66c70
realyutou <ssps9430@gmail.com>
2023-08-11 15:39:11 +0800

    feat: show restaurants which were commented by user in user profile page

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,6 +1,6 @@
 const bcrypt = require('bcryptjs')
 
-const { User } = require('../models/')
+const { User, Comment, Restaurant } = require('../models/')
 const { imgurFileHandler } = require('../helpers/file-helpers')
 
 const userController = {
@@ -39,17 +39,24 @@ const userController = {
     res.redirect('/signin')
   },
   getUser: (req, res, next) => {
-    return User.findByPk(req.params.id, { raw: true })
-      .then(user => {
-        if (!user) throw new Error("User doesn't exist!")
-        return res.render('users/profile', { user })
+    return Promise.all([
+      User.findByPk(req.params.id, { raw: true }),
+      Comment.findAndCountAll({
+        where: { userId: req.params.id },
+        raw: true,
+        nest: true,
+        include: Restaurant
+      })
+    ])
+      .then(([user, comments]) => {
+        return res.render('users/profile', { user, count: comments.count, comments: comments.rows })
       })
       .catch(err => next(err))
   },
   editUser: (req, res, next) => {
     // if (req.user.id !== Number(req.params.id)) {
     //   req.flash('error_messages', '只能編輯自己的使用者資料！')
-    //   res.redirect(`/users/${req.user.id}`)
+    //   return res.redirect(`/users/${req.user.id}`)
     // }
     return User.findByPk(req.params.id, { raw: true })
       .then(user => {
