03b6341e12bfd3d3e2885777bc0acfe08e5cf8dd
Melodystart <start99start@gmail.com>
2023-08-11 16:05:51 +0800

    R02:Dashboard

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -10,10 +10,19 @@ const restaurantController = {
       .catch(err => next(err))
   },
   getRestaurant: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, { include: [Category] })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        return restaurant.increment('viewCounts')
+      })
+      .then(restaurant => { res.render('restaurant', { restaurant: restaurant.toJSON() }) })
+      .catch(err => next(err))
+  },
+  getDashboard: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, { raw: true, nest: true, include: [Category] })
       .then(restaurant => {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
-        res.render('restaurant', { restaurant })
+        res.render('dashboard', { restaurant })
       })
       .catch(err => next(err))
   }
