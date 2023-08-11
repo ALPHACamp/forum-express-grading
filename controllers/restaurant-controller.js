9e63b6f085ab55f83ee5eaad88cf2a5ce8ece4be
realyutou <ssps9430@gmail.com>
2023-08-11 22:15:54 +0800

    feat: add feeds page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -63,6 +63,28 @@ const restaurantController = {
         res.render('dashboard', { restaurant })
       })
       .catch(err => next(err))
+  },
+  getFeeds: (req, res, next) => {
+    return Promise.all([
+      Restaurant.findAll({
+        limit: 10,
+        order: [['created_at', 'DESC']],
+        include: [Category],
+        raw: true,
+        nest: true
+      }),
+      Comment.findAll({
+        limit: 10,
+        order: [['created_at', 'DESC']],
+        include: [User, Restaurant],
+        raw: true,
+        nest: true
+      })
+    ])
+      .then(([restaurants, comments]) => {
+        res.render('feeds', { restaurants, comments })
+      })
+      .catch(err => next(err))
   }
 }
 
