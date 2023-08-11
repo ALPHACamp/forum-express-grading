9cd03d3fe24973cafda7df3f680a869dc1e165ec
susu725 <zxcv1010017@gmail.com>
2023-08-11 14:27:46 +0800

    feat: update seed files

---
+++ seeders/20230811061527-categories-seed-file.js
@@ -0,0 +1,17 @@
+'use strict'
+
+const categories = ['中式料理', '日本料理', '義大利料理', '墨西哥料理', '素食料理', '美式料理', '複合式料理']
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkInsert('Categories', categories.map(category => ({
+      name: category,
+      created_at: new Date(),
+      updated_at: new Date()
+    })), {})
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.bulkDelete('Categories', {})
+  }
+}
