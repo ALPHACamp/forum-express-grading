787cd67c6ba6f517d85acec5c9e3beb04f4297a5
harrison0502 <harrison0502@gmail.com>
2023-08-12 01:25:38 +0800

    feat: get user's favorited restaurants & switch button

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -22,9 +22,11 @@ const restaurantController = {
       Category.findAll({ raw: true })
     ])
       .then(([restaurants, categories]) => {
+        const favoritedRestaurantsId = req.user && req.user.FavoritedRestaurants.map(fr => fr.id)
         const data = restaurants.rows.map(r => ({
           ...r,
-          description: r.description.substring(0, 50)
+          description: r.description.substring(0, 50),
+          isFavorited: favoritedRestaurantsId.includes(r.id)
         }))
         return res.render('restaurants', {
           restaurants: data,
@@ -37,17 +39,18 @@ const restaurantController = {
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
-      include: [Category, { model: Comment, include: User }], // 拿出關聯的資料
+      include: [Category, { model: Comment, include: User }, { model: User, as: 'FavoritedUsers' }], // 拿出關聯的資料
       nest: true
     })
       .then(restaurant => {
-        console.log(restaurant)
         if (!restaurant) throw new Error("Restaurant didn't exist!")
         return restaurant.increment('viewCounts', { by: 1 })
       })
       .then(updateRestaurant => {
+        const isFavorited = updateRestaurant.FavoritedUsers.some(f => f.id === req.user.id)
         res.render('restaurant', {
-          restaurant: updateRestaurant.toJSON()
+          restaurant: updateRestaurant.toJSON(),
+          isFavorited
         })
       })
       .catch(err => next(err))
