b3b8a49117b456772fa453529e19af84e978f110
jolly01008 <jollgmll292@gmail.com>
2023-08-11 14:12:29 +0800

    feat: add restaurant dashboard page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -18,14 +18,25 @@ const restaurantController = {
       })
   },
   getRestaurant: (req, res, next) => {
+    Restaurant.findByPk(req.params.id, {
+      include: [Category]
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        return restaurant.increment('viewCounts', { by: 1 })
+      })
+      .then(restaurant => res.render('restaurant', { restaurant: restaurant.toJSON() }))
+      .catch(err => next(err))
+  },
+  getDashboard: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
-      include: Category,
+      include: [Category],
       raw: true,
       nest: true
     })
       .then(restaurant => {
         if (!restaurant) throw new Error("Restaurant didn't exist!")
-        res.render('restaurant', { restaurant })
+        res.render('dashboard', { restaurant })
       })
       .catch(err => next(err))
   }
