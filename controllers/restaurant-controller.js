5a872db34d6d6aea6ff05b88f6c88d0646dcf02e
Melodystart <start99start@gmail.com>
2023-08-11 14:55:36 +0800

    feat: add restaurant page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -8,6 +8,14 @@ const restaurantController = {
         return res.render('restaurants', { restaurants: data })
       })
       .catch(err => next(err))
+  },
+  getRestaurant: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, { raw: true, nest: true, include: [Category] })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        res.render('restaurant', { restaurant })
+      })
+      .catch(err => next(err))
   }
 }
 module.exports = restaurantController
