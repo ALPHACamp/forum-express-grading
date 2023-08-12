d95046d53b8826ed734d9d5fa81c86d8caf6d591
Yi-Hsiu Lee <67046103+karmey77@users.noreply.github.com>
2023-08-12 17:44:18 +0800

    fix format

--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,7 @@ router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
 
 router.get('/signin', userController.signInPage)
-router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn) 
+router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
 
 router.get('/logout', userController.logout)
 
