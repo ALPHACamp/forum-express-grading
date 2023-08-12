98bbc0a75807b2dc88c733d66dfc99028c258ccf
Yi-Hsiu Lee <125238665+tmrmds-yhl@users.noreply.github.com>
2023-08-12 11:48:52 +0800

    瀏覽餐廳時看見分類

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,4 +1,4 @@
-const { Restaurant, User } = require('../models') // 新增這裡
+const { Restaurant, User, Category } = require('../models')
 const { imgurFileHandler } = require('../helpers/file-helpers')
 const { getUser } = require('../helpers/auth-helpers')
 const adminController = { // 修改這裡
@@ -6,7 +6,9 @@ const adminController = { // 修改這裡
   getRestaurants: (req, res, next) => {
     Restaurant.findAll({
 
-      raw: true
+      raw: true,
+      nest: true,
+      include: [Category]
 
     })
 
@@ -42,7 +44,9 @@ const adminController = { // 修改這裡
 
   getRestaurant: (req, res, next) => {
     Restaurant.findByPk(req.params.id, { // 去資料庫用 id 找一筆資料
-      raw: true // 找到以後整理格式再回傳
+      raw: true, // 找到以後整理格式再回傳
+      nest: true, // 增加這裡 
+      include: [Category]  // 增加這裡 
     })
       .then(restaurant => {
         if (!restaurant) throw new Error("Restaurant didn't exist!") //  如果找不到，回傳錯誤訊息，後面不執行
