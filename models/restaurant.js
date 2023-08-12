04ffde9d4d75293cf613bf3e4595494e375ff5b7
go2Cshop <shell800808@gmail.com>
2023-08-12 09:11:23 +0800

    feat: add comment model

--- models/restaurant.js
+++ models/restaurant.js
@@ -4,15 +4,11 @@ const {
 } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
   class Restaurant extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate (models) {
       Restaurant.belongsTo(models.Category, { foreignKey: 'categoryId' })
+      Restaurant.hasMany(models.Comment, { foreignKey: 'restaurantId' })
     }
-  };
+  }
   Restaurant.init({
     name: DataTypes.STRING,
     tel: DataTypes.STRING,
