2a684c5dd90e68524d97ee774431c18fa84c36ec
mmm999xp <mmm999xp@gmail.com>
2023-08-12 18:57:43 +0800

    add profile (R03)

--- app.js
+++ app.js
@@ -30,6 +30,7 @@ app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages') // 設定 success_msg 訊息
   res.locals.error_messages = req.flash('error_messages') // 設定 warning_msg 訊息
   res.locals.user = getUser(req)
+  res.locals.reqUser = getUser(req)
   next()
 })
 app.use(routes)
--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcryptjs') // 載入 bcrypt
-const db = require('../models')
-const { User } = db
+const { User, Comment, Restaurant } = require('../models')
+const { imgurFileHandler } = require('../helpers/file-helpers')
+const authHelper = require('../helpers/auth-helpers')
 const userController = {
   signUpPage: (req, res) => {
     res.render('signup')
@@ -37,6 +38,74 @@ const userController = {
     req.flash('success_messages', '登出成功！')
     req.logout()
     res.redirect('/signin')
+  },
+  getUser: (req, res, next) => {
+    const paramsUserId = Number(req.params.id)
+    return Promise.all([
+      User.findByPk(paramsUserId, { raw: true }),
+      Comment.findAndCountAll({
+        where: { userId: req.params.id },
+        raw: true,
+        nest: true,
+        include: Restaurant
+      })
+    ])
+      .then(([user, comments]) => {
+        res.render('users/profile', {
+          user,
+          count: comments.count,
+          comments: comments.rows
+        })
+      })
+      .catch(err => next(err))
+  },
+  editUser: (req, res, next) => {
+    // 取得passport幫我們放入的user，也就是當前登入的登入者id
+    const reqUser = authHelper.getUser(req).id
+    // 取得路由中的動態路由的字串，並轉換成數字，此為編輯者id
+    const paramsUserId = Number(req.params.id)
+    // 如果登入者與編輯者身分不同，直接丟出去
+    if (reqUser !== paramsUserId) {
+      req.flash('error_messages', '無法編輯他人的使用者資料！')
+      return res.redirect(`/users/${paramsUserId}`)
+    }
+    return User.findByPk(paramsUserId, { raw: true })
+      .then(user => {
+        if (!user) throw new Error("User doesn't exist!")
+        return res.render('users/edit', { user })
+      })
+      .catch(err => next(err))
+  },
+  putUser: (req, res, next) => {
+    const { name } = req.body
+    const { file } = req
+    // 取得passport幫我們放入的user，也就是當前登入的登入者id
+    const reqUser = authHelper.getUser(req).id
+    // 取得路由中的動態路由的字串，並轉換成數字，此為編輯者id
+    const paramsUserId = Number(req.params.id)
+    // 如果沒有name
+    if (!name) throw new Error('Name is required!')
+    // 如果登入者與編輯者身分不同，直接丟出去
+    if (reqUser !== paramsUserId) {
+      req.flash('error_messages', '無法編輯他人的使用者資料！')
+      return res.redirect(`/users/${paramsUserId}`)
+    }
+    return Promise.all([
+      User.findByPk(paramsUserId),
+      imgurFileHandler(file)
+    ])
+      .then(([user, filePath]) => {
+        if (!user) throw new Error("User doesn't exist!")
+        return user.update({
+          name: name || user.name,
+          image: filePath || user.image
+        })
+      })
+      .then(() => {
+        req.flash('success_messages', '使用者資料編輯成功')
+        res.redirect(`/users/${paramsUserId}`)
+      })
+      .catch(err => next(err))
   }
 }
 module.exports = userController
