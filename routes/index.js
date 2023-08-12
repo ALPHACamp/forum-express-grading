98297591406255be3d400024457080922a457d26
Yen Lin <lynn575938@outlook.com>
2023-08-12 15:34:39 +0800

    feat: add topUser page

--- routes/index.js
+++ routes/index.js
@@ -32,6 +32,7 @@ router.get('/restaurants', authenticated, restController.getRestaurants)
 router.delete('/comments/:id', authenticated, commentController.deleteComment)
 router.post('/comments', authenticated, commentController.postComment)
 
+router.get('/users/top', authenticated, userController.getTopUsers)
 router.get('/users/:id/edit', authenticated, userController.editUser)
 router.get('/users/:id', authenticated, userController.getUser)
 router.put('/users/:id', authenticated, upload.single('image'), userController.putUser)
