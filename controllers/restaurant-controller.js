d0ddaf0573db45b7a8eff9c7cf1db7e88d9d5634
pupss91090 <hsuchinzai@gmail.com>
2023-08-12 13:01:09 +0800

    add comments counts on dashboard

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -47,7 +47,7 @@ const restaurantController = {
       .then(restaurant => {
         // console.log(restaurant.Comments)
         // restaurant = restaurant.toJSON()
-        // restaurant.Comments.sort({ createdAt: 'asc' })
+        // restaurant.Comment.sort({ createdAt: 'asc' })
         // res.render('restaurant', { restaurant })
         res.render('restaurant', { restaurant: restaurant.toJSON() })
       })
@@ -55,13 +55,12 @@ const restaurantController = {
   },
   getDashboard: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, { // 去資料庫用 id 找一筆資料
-      raw: true, // 找到以後整理格式再回傳
-      nest: true,
-      include: [Category]
+      include: [Category, { model: Comment }]
     })
       .then(restaurant => {
         if (!restaurant) throw new Error("Restaurant didn't exist!") //  如果找不到，回傳錯誤訊息，後面不執行
-        res.render('dashboard', { restaurant })
+        console.log(restaurant.toJSON())
+        res.render('dashboard', { restaurant: restaurant.toJSON() })
       })
       .catch(err => next(err))
   }
