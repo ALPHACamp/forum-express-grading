d4119e35d27edcf9721f7ff83bb58e0f3b6faadb
muco0521 <muco0521@gmail.com>
2023-08-13 01:43:25 +0800

    feat: add feeds page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -74,7 +74,31 @@ const restaurantController = {
     } catch (err) {
       next(err)
     }
+  },
+  getFeeds: async (req, res, next) => {
+    try {
+      const [restaurants, comments] = await Promise.all([
+        Restaurant.findAll({
+          limit: 10,
+          order: [['createdAt', 'DESC']],
+          include: [Category],
+          raw: true,
+          nest: true
+        }),
+        Comment.findAll({
+          limit: 10,
+          order: [['createdAt', 'DESC']],
+          include: [User, Restaurant],
+          raw: true,
+          nest: true
+        })
+      ])
+
+      res.render('feeds', {
+        restaurants,
+        comments
+      })
+    } catch (err) { next(err) }
   }
 }
-
 module.exports = restaurantController
