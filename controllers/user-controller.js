1b86156c9aab3e55ba37b42c967c49a73b70dcc2
yang haotse <ha91252@gmail.com>
2023-08-12 14:17:27 +0800

    feat: edit getUser

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -42,6 +42,8 @@ const userController = {
     res.redirect('/signin')
   },
   getUser: (req, res, next) => {
+    const currentUserId = req.user ? req.user.id : {}
+    // console.log('req.user:', req.user.id)
     return Promise.all([User.findByPk(req.params.id), Comment.findAndCountAll({
       nest: true,
       where: { userId: req.params.id },
@@ -50,9 +52,9 @@ const userController = {
     ])
       .then(([user, comment]) => {
         if (!user) throw new Error("Profile didn't exist")
+        // if (!currentUserId) throw new Error("User didn't exist")
         const restaurants = comment.rows.map(item => item.Restaurant.toJSON())
-        const currentUserId = req.user.id
-        res.render('users/profile', { user: user.toJSON(), restaurants, commentCount: comment.count, currentUserId })
+        return res.render('users/profile', { user: user.toJSON(), restaurants, commentCount: comment.count, currentUserId })
       })
       .catch(err => next(err))
   },
