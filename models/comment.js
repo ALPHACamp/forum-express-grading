e87a772aa134dba1ef5ce7ec73b7c824734f6742
harrison0502 <harrison0502@gmail.com>
2023-08-12 22:40:29 +0800

    fix: comment table name

--- models/comment.js
+++ models/comment.js
@@ -21,7 +21,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'Comment',
-    talbeName: 'Comments',
+    tableName: 'Comments',
     underscored: true
   })
   return Comment
