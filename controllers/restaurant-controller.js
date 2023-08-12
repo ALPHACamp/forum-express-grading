5bfc5b739c1d8aa6f6f4f9ddb41edfb9348dc4d1
mmm999xp <mmm999xp@gmail.com>
2023-08-13 01:34:08 +0800

    add restaurant top (R05)

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -100,6 +100,27 @@ const restaurantController = {
         })
       })
       .catch(err => next(err))
+  },
+  getTopRestaurants: (req, res, next) => {
+    return Restaurant.findAll({
+      include: [
+        { model: User, as: 'FavoritedUsers' }
+      ]
+    })
+      .then(restaurants => {
+        if (!restaurants) throw new Error("Restaurant didn't exist!")
+        restaurants = restaurants
+          .map(r => ({
+            ...r.toJSON(),
+            description: r.description.substring(0, 50),
+            favoritedCount: r.FavoritedUsers.length,
+            isFavorited:
+              req.user &&
+              req.user.FavoritedRestaurants.some(fr => fr.id === r.id)
+          }))
+          .sort((a, b) => b.favoritedCount - a.favoritedCount).slice(0, 10)
+        res.render('top-restaurants', { restaurants })
+      })
   }
 }
 module.exports = restaurantController
--- routes/index.js
+++ routes/index.js
@@ -17,6 +17,7 @@ router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
 // logout
 router.get('/logout', userController.logout)
+router.get('/restaurants/top', authenticated, restController.getTopRestaurants)
 router.get('/restaurants/feeds', authenticated, restController.getFeeds)
 router.get('/restaurants/:id', authenticated, restController.getRestaurant)
 router.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard)
