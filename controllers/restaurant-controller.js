ca994024d9b5b6801e3bd015b1cd6f6a93a808f0
yanyanyaa <yclin3411@gmail.com>
2023-08-12 14:08:49 +0800

    feat: add categories navbar on restaurants index page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -2,21 +2,35 @@
 const { Restaurant, Category } = require('../models')
 
 const restaurantController = {
-  getRestaurants: (req, res) => {
-    return Restaurant.findAll({
-      include: Category,
-      raw: true,
-      nest: true
-    }).then(restaurants => {
-      const data = restaurants.map(r => ({
-        ...r, description: r.description.substring(0, 50)
-      }))
-      // const data = restaurants.map(r => {
-      //   r.description = r.description.substring(0, 50)
-      //   return r
-      // })
-      return res.render('restaurants', { restaurants: data })
-    })
+  getRestaurants: (req, res, next) => {
+    const categoryId = Number(req.query.categoryId)
+    return Promise.all([
+      Restaurant.findAll({
+        include: Category,
+        where: {
+          ...categoryId ? { categoryId } : {} // 檢查 categoryId 是否為空值
+        },
+        raw: true,
+        nest: true
+      }),
+      Category.findAll({ raw: true })
+    ])
+      .then(([restaurants, categories]) => {
+        const data = restaurants.map(r => ({
+          ...r,
+          description: r.description.substring(0, 50)
+        }))
+        // const data = restaurants.map(r => {
+        //   r.description = r.description.substring(0, 50)
+        //   return r
+        // })
+        return res.render('restaurants', {
+          restaurants: data,
+          categories,
+          categoryId
+        })
+      })
+      .catch(err => next(err))
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, { include: Category })
