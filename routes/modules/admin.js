392dd47b7a933480cd6c2b62fbe35705c3a81511
Elu1919 <qw29149974@gmail.com>
2023-08-12 13:10:14 +0800

    feat: category update

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -16,8 +16,10 @@ router.post('/restaurants', upload.single('image'), adminController.postRestaura
 router.patch('/users/:id', adminController.patchUser)
 router.get('/users', adminController.getUsers)
 
-router.post('/categories', categoryController.postCategory)
+router.get('/categories/:id', categoryController.getCategories)
+router.put('/categories/:id', categoryController.putCategory)
 router.get('/categories', categoryController.getCategories)
+router.post('/categories', categoryController.postCategory)
 
 router.use('/', (req, res) => res.redirect('/admin/restaurants'))
 
