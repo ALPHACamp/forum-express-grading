37e3dbeff4f18863e0f55ad43e6a76e85ae040a7
Elu1919 <qw29149974@gmail.com>
2023-08-12 14:05:13 +0800

    feat: add restaurant page

--- routes/index.js
+++ routes/index.js
@@ -9,13 +9,18 @@ const passport = require('../config/passport')
 const router = express.Router()
 
 router.use('/admin', authenticatedAdmin, admin)
+
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
+
 router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
-router.get('/restaurants', authenticated, restController.getRestaurants)
+
 router.get('/logout', userController.logout)
-router.get('/restaurants', restController.getRestaurants)
+
+router.get('/restaurants/:id', authenticated, restController.getRestaurant)
+router.get('/restaurants', authenticated, restController.getRestaurants)
+
 router.use('/', (req, res) => res.redirect('/restaurants'))
 router.use('/', generalErrorHandler)
 
