ede502624dcb956c3fe31ef780549d3d6dc69c1f
muco0521 <muco0521@gmail.com>
2023-08-13 22:55:39 +0800

    feat: create followship model

--- config/passport.js
+++ config/passport.js
@@ -28,12 +28,12 @@ passport.deserializeUser((id, cb) => {
   User.findByPk(id, {
     include: [
       { model: Restaurant, as: 'FavoritedRestaurants' },
-      { model: Restaurant, as: 'LikedRestaurants' }
+      { model: Restaurant, as: 'LikedRestaurants' },
+      { model: User, as: 'Followers' },
+      { model: User, as: 'Followings' }
     ]
   })
-    .then(user => {
-      return cb(null, user.toJSON())
-    })
+    .then(user => cb(null, user.toJSON()))
     .catch(error => cb(error))
 })
 
