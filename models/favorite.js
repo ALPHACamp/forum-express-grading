32a9bc550c7dfc2837b973e763db902a054f81fc
HotG8119 <k508060@gmail.com>
2023-08-13 15:55:47 +0800

    fix: tableName error

--- models/favorite.js
+++ models/favorite.js
@@ -19,7 +19,7 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     sequelize,
     modelName: 'Favorite',
-    tableName: 'favorites',
+    tableName: 'Favorites',
     underscored: true
   })
   return Favorite
