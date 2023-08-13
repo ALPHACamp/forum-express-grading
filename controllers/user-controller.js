381845ca6415603982f8e968facf8aab2199dca2
taiwania <taiwaniajusto@gmail.com>
2023-08-13 20:30:50 +0800

    R03 refactor - User profile and edit

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcryptjs')
 const db = require('../models')
-const { User } = db
+const { User, Restaurant, Comment } = db
+const { imgurFileHandler } = require('../helpers/file-helpers')
 
 const userController = {
   signUpPage: (req, res) => {
@@ -8,18 +9,22 @@ const userController = {
   },
 
   signUp: (req, res, next) => {
-    if (req.body.password !== req.body.passwordCheck) throw new Error('Passwords do not match!')
+    if (req.body.password !== req.body.passwordCheck) {
+      throw new Error('Passwords do not match!')
+    }
 
     User.findOne({ where: { email: req.body.email } })
       .then(user => {
         if (user) throw new Error('Email already exists!')
         return bcrypt.hash(req.body.password, 10)
       })
-      .then(hash => User.create({
-        name: req.body.name,
-        email: req.body.email,
-        password: hash
-      }))
+      .then(hash =>
+        User.create({
+          name: req.body.name,
+          email: req.body.email,
+          password: hash
+        })
+      )
       .then(() => {
         req.flash('success_messages', '成功註冊帳號！')
         res.redirect('/signin')
@@ -40,6 +45,85 @@ const userController = {
     req.flash('success_messages', '登出成功！')
     req.logout()
     res.redirect('/signin')
+  },
+
+  getUser: (req, res, next) => {
+    return User.findByPk(req.params.id, {
+      include: [
+        {
+          model: Comment,
+          include: [Restaurant]
+        }
+      ]
+    })
+      .then(user => {
+        if (!user) throw new Error('User not found!')
+
+        const userForRender = {
+          id: user.id,
+          name: user.name,
+          email: user.email,
+          image: user.image,
+          commentedRestaurants: user.Comments.map(comment => {
+            return {
+              id: comment.Restaurant.id,
+              image: comment.Restaurant.image
+            }
+          })
+        }
+
+        return res.render('users/profile', { user: userForRender })
+      })
+      .catch(err => next(err))
+  },
+
+  editUser: (req, res, next) => {
+    return User.findByPk(req.params.id)
+      .then(user => {
+        if (!user) throw new Error('User not found!')
+
+        const userForRender = {
+          id: user.id,
+          name: user.name,
+          image: user.image
+        }
+
+        return res.render('users/edit', { user: userForRender })
+      })
+      .catch(err => next(err))
+  },
+
+  putUser: async (req, res, next) => {
+    try {
+      console.log(req.body)
+      const id = Number(req.params.id)
+      console.log('User ID:', id)
+      const { name } = req.body
+      if (!name) throw new Error('User name is missing!')
+      const { file } = req
+
+      const catchUser = await User.findByPk(id)
+      if (!catchUser) throw new Error('User not found!')
+
+      const [user, filePath] = await Promise.all([
+        catchUser,
+        imgurFileHandler(file)
+      ])
+
+      await user
+        .update({
+          name,
+          image: filePath || user.image
+        })
+        .then(() => {
+          req.flash('success_messages', '使用者資料編輯成功')
+          res.redirect(`/users/${user.id}`)
+        })
+        .catch(err => next(err))
+    } catch (err) {
+      console.log('Error in putUser:', err.message)
+      next(err)
+    }
   }
 }
 
--- models/user.js
+++ models/user.js
@@ -1,7 +1,5 @@
 'use strict'
-const {
-  Model
-} = require('sequelize')
+const { Model } = require('sequelize')
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     /**
@@ -9,21 +7,25 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate (models) {
+    static associate(models) {
       // define association here
       User.hasMany(models.Comment, { foreignKey: 'userId' })
     }
-  };
-  User.init({
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    password: DataTypes.STRING,
-    isAdmin: DataTypes.BOOLEAN
-  }, {
-    sequelize,
-    modelName: 'User',
-    tableName: 'Users',
-    underscored: true
-  })
+  }
+  User.init(
+    {
+      name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING,
+      isAdmin: DataTypes.BOOLEAN,
+      image: DataTypes.STRING
+    },
+    {
+      sequelize,
+      modelName: 'User',
+      tableName: 'Users',
+      underscored: true
+    }
+  )
   return User
 }
