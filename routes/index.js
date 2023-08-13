6968dfd472ca3328595882a21e65c35faf2264b2
Annie <anniehuang0410@gmail.com>
2023-08-13 15:38:39 +0800

    feat: R03 issue fixed

--- routes/index.js
+++ routes/index.js
@@ -22,8 +22,8 @@ router.get('/logout', userController.logout)
 
 // user related
 router.get('/users/:id/edit', authenticated, userController.editUser)
-router.get('/users/:id', authenticated, upload.single('image'), userController.getUser)
-router.put('/users/:id', authenticated, userController.putUser)
+router.get('/users/:id', authenticated, userController.getUser)
+router.put('/users/:id', authenticated, upload.single('image'), userController.putUser)
 
 // front-end restaurant related
 router.get('/restaurants/feeds', authenticated, restController.getFeeds)
