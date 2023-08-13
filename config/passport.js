9c06b67bed12077539bcd101c27066cb846150b0
Yi-Hsiu Lee <67046103+karmey77@users.noreply.github.com>
2023-08-13 18:01:59 +0800

    feat: get user's favorited restaurants & switch button

--- config/passport.js
+++ config/passport.js
@@ -1,37 +1,39 @@
 const passport = require('passport')
 const LocalStrategy = require('passport-local')
 const bcrypt = require('bcryptjs')
-const db = require('../models')
-const User = db.User
+const { User, Restaurant } = require('../models')
 // set up Passport strategy
 passport.use(new LocalStrategy(
   // customize user field
-    {
-        usernameField: 'email',
-        passwordField: 'password',
-        passReqToCallback: true
-    },
-    // authenticate user
-    (req, email, password, cb) => {
-        User.findOne({ where: { email: email } })
-            .then(user => {
-                if (!user) return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤！'))
-                bcrypt.compare(password, user.password).then(res => {
-                    if (!res) return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤！'))
-                    return cb(null, user)
-                })
-            })
-    }
+  {
+    usernameField: 'email',
+    passwordField: 'password',
+    passReqToCallback: true
+  },
+  // authenticate user
+  (req, email, password, cb) => {
+    User.findOne({ where: { email: email } })
+      .then(user => {
+        if (!user) return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤！'))
+        bcrypt.compare(password, user.password).then(res => {
+          if (!res) return cb(null, false, req.flash('error_messages', '帳號或密碼輸入錯誤！'))
+          return cb(null, user)
+        })
+      })
+  }
 ))
 // serialize and deserialize user
 passport.serializeUser((user, cb) => {
-    cb(null, user.id)
+  cb(null, user.id)
 })
 passport.deserializeUser((id, cb) => {
-    User.findByPk(id).then(user => {
-        user = user.toJSON()
-        return cb(null, user)
-    })
+  User.findByPk(id, {
+    include: [
+      { model: Restaurant, as: 'FavoritedRestaurants' }
+    ]
+  })
+    .then(user => cb(null, user.toJSON()))
+    .catch(err => cb(err))
 })
 
 module.exports = passport
