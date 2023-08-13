5181cbb07c9651c56cbfadd5cae1b84b3136471a
gibbs-shih <shih.gibbs@gmail.com>
2023-08-13 12:58:39 +0800

    feat: update top restaurants

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -97,6 +97,24 @@ const restaurantController = {
         })
       })
       .catch(err => next(err))
+  },
+  getTopRestaurants: (req, res, next) => {
+    return Restaurant.findAll({
+      include: [{ model: User, as: 'FavoritedUsers' }]
+    })
+      .then(restaurants => {
+        const result = restaurants
+          .map(restaurant => ({
+            ...restaurant.toJSON(),
+            description: restaurant.description.substring(0, 50),
+            favoritedCount: restaurant.FavoritedUsers.length,
+            isFavorited: req.user && req.user.FavoritedRestaurants.some(f => f.id === restaurant.id)
+          }))
+          .sort((a, b) => b.favoritedCount - a.favoritedCount)
+        res.render('top-restaurants', { restaurants: result.slice(0, 10) })
+      })
+      .catch(err => next(err))
   }
 }
+
 module.exports = restaurantController
