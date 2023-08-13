ae64f87842bc93c9636143762020f0eb12ecb178
BoxunW <tony1572002@gmail.com>
2023-08-13 18:08:02 +0800

    for R03 Q4

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,4 +1,6 @@
 const bcrypt = require('bcryptjs')
+const { imgurFileHandler } = require('../helpers/file-helpers')
+const { getUser } = require('../helpers/auth-helpers')
 const db = require('../models')
 const { User } = db
 const userController = {
@@ -10,12 +12,17 @@ const userController = {
     User.findOne({ where: { email: req.body.email } })
       .then(user => {
         if (user) throw new Error('Email already exists!')
-        return bcrypt.hash(req.body.password, 10)
+        const { file } = req
+        return Promise.all([
+          bcrypt.hash(req.body.password, 10),
+          imgurFileHandler(file)
+        ])
       })
-      .then(hash => User.create({
+      .then(([hash, filePath]) => User.create({
         name: req.body.name,
         email: req.body.email,
-        password: hash
+        password: hash,
+        image: filePath || null
       }))
       .then(() => {
         req.flash('success_messages', '成功註冊帳號！')
@@ -34,6 +41,51 @@ const userController = {
     req.flash('success_messages', '登出成功！')
     req.logout()
     res.redirect('/signin')
+  },
+  getUser: (req, res, next) => {
+    const editPermission = (Number(req.params.id) === Number(getUser(req).id))
+    return User.findByPk(req.params.id, {
+      raw: true
+    })
+      .then(user => {
+        if (!user) throw new Error("User didn't exist!")
+        res.render('users/profile', { user, editPermission })
+      })
+      .catch(err => next(err))
+  },
+  editUser: (req, res, next) => {
+    return User.findByPk(req.params.id, {
+      raw: true
+    })
+      .then(user => {
+        if (!user) throw new Error("User didn't exist!")
+        res.render('users/edit', { user })
+      })
+      .catch(err => next(err))
+  },
+  putUser: (req, res, next) => {
+    const id = req.params.id
+    if (Number(id) !== Number(req.user.id)) {
+      return res.redirect(`/users/${id}`)
+    }
+    const name = req.body.name
+    const { file } = req
+    return Promise.all([
+      User.findByPk(req.params.id),
+      imgurFileHandler(file)
+    ])
+      .then(([user, filePath]) => {
+        if (!user) throw new Error("User didn't exist!")
+        return user.update({
+          name,
+          image: filePath || user.image
+        })
+      })
+      .then(() => {
+        req.flash('success_messages', '使用者資料編輯成功')
+        res.redirect(`/users/${id}`)
+      })
+      .catch(err => next(err))
   }
 }
 module.exports = userController
