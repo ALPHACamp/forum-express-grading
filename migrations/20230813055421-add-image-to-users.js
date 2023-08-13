a007f1680a7f1b9ffb8b3749290364f238afb89c
dingbum73 <dingbum73@gmail.com>
2023-08-13 14:41:48 +0800

    ...

--- migrations/20230813055421-add-image-to-users.js
+++ migrations/20230813055421-add-image-to-users.js
@@ -4,7 +4,12 @@ module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.addColumn('Users', 'image', {
 <<<<<<< HEAD
+<<<<<<< HEAD
       type: Sequelize.STRING
+=======
+      type: Sequelize.STRING,
+      allowNull: true
+>>>>>>> R03
 =======
       type: Sequelize.STRING,
       allowNull: true
