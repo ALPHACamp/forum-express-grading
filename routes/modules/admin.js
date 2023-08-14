712f67a3483998656935e16828348115a9f752b8
YD1234564789 <a8111202003@gmail.com>
2023-08-15 00:06:56 +0800

    feat: passport init & signin

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -3,4 +3,5 @@ const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
 router.get('/restaurants', adminController.getRestaurants)
 router.use('/', (req, res) => res.redirect('/admin/restaurants'))
+
 module.exports = router
