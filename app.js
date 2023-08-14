44d74732a0e4712b346c559b0f8a510707521d6a
Peter-Haung888 <ming31437@gmail.com>
2023-08-14 14:31:49 +0800

    feat: admin update restaurant

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const flash = require('connect-flash')
 const session = require('express-session')
+const methodOverride = require('method-override')
 const passport = require('./config/passport')
 
 const { getUser } = require('./helpers/auth-helpers')
@@ -18,9 +19,10 @@ app.set('view engine', 'hbs')
 
 app.use(express.urlencoded({ extended: true }))
 app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
-app.use(flash())
 app.use(passport.initialize())
 app.use(passport.session())
+app.use(flash())
+app.use(methodOverride('_method'))
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')// 設定 success_msg 訊息
   res.locals.error_messages = req.flash('error_messages')// 設定 warning_msg 訊息
