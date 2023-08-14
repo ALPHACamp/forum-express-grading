7cc4013272e889c7418f35d3646cfd3c9868b005
claire <claire210401@gmail.com>
2023-08-14 11:07:20 +0800

    modify head.hbs

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -67,6 +67,31 @@ const restaurantController = {
         res.render('dashboard', { restaurant })
       })
       .catch(err => next(err))
+  },
+  getFeeds: (req, res, next) => {
+    return Promise.all([
+      Restaurant.findAll({
+        limit: 10,
+        order: [['createAt', 'DESC']], // 降序
+        include: [Category], // 引入的 Model
+        raw: true,
+        nest: true
+      }),
+      Comment.findAll({
+        limit: 10,
+        order: [['createAt', 'DESC']],
+        include: [User, Restaurant],
+        raw: true,
+        nest: true
+      })
+    ])
+      .then(([restaurants, comments]) => {
+        res.render('feeds', {
+          restaurants,
+          comments
+        })
+      })
+      .catch(err => next(err))
   }
 }
 module.exports = restaurantController
