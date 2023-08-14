ad9f91fb9730b9cdf255367baa8dc3dce31ffb75
susu725 <zxcv1010017@gmail.com>
2023-08-14 12:06:16 +0800

    feat: add feeds page

--- routes/index.js
+++ routes/index.js
@@ -24,6 +24,8 @@ router.post('/signin', passport.authenticate('local', {
 router.get('/logout', userController.logout)
 
 // 餐廳
+router.get('/restaurants/feeds', authenticated, restController.getFeeds)
+
 router.get('/restaurants/:id/dashboard', authenticated, restController.getDashboard)
 
 router.get('/restaurants/:id', authenticated, restController.getRestaurant)
