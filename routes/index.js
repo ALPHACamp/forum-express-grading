79b13f96f3fe6a5853ed1c4560bb0becea650d92
Annie <anniehuang0410@gmail.com>
2023-08-15 14:22:52 +0800

    feat: add topUser page

--- routes/index.js
+++ routes/index.js
@@ -21,6 +21,7 @@ router.post('/signin', passport.authenticate('local', { failureRedirect: '/signi
 router.get('/logout', userController.logout)
 
 // user related
+router.get('/users/top', authenticated, userController.getTopUsers)
 router.get('/users/:id/edit', authenticated, userController.editUser)
 router.get('/users/:id', authenticated, userController.getUser)
 router.put('/users/:id', authenticated, upload.single('image'), userController.putUser)
