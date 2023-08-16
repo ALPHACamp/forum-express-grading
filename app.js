26c61750b4b212836b5e9e825896c25c5f483457
se040054 <peter8822967@gmail.com>
2023-08-16 14:12:10 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require('express')
+const handlebars = require('express-handlebars')
 const routes = require('./routes')
 
 const app = express()
 const port = process.env.PORT || 3000
+// 註冊 Handlebars 樣板引擎，並指定副檔名為 .hbs
+app.engine('hbs', handlebars({ extname: '.hbs' }))
+// 設定使用 Handlebars 做為樣板引擎
+app.set('view engine', 'hbs')
 
 app.use(routes)
 
@@ -11,3 +16,4 @@ app.listen(port, () => {
 })
 
 module.exports = app
+// 注意：導入自動化測試以後，由於測試環境會用到 app，所以需要在文件最下方輸出 app
