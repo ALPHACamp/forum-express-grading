6f23dfc37ad86be03bcbaf5b82c216c18f77eedb
se040054 <peter8822967@gmail.com>
2023-08-18 13:04:53 +0800

    feat: create restaurant

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -5,7 +5,20 @@ const adminController = {
     Restaurant.findAll({ raw: true })
       .then(restaurants => res.render('admin/restaurants', { restaurants }))
       .catch(err => next(err))
+  },
+  createRestaurants: (req, res) => {
+    res.render('admin/create-restaurant')
+  },
+  postRestaurant: (req, res,next) => {
+    const { name, tel, address, openingHours, description } = req.body
+    if (!name) throw new Error('名字是必填欄位')
+    Restaurant.create({
+      name, tel, address, openingHours, description
+    }).then(() => {
+      req.flash('success_messages', '新增餐廳成功')
+      res.redirect('/admin/restaurants')
+    }).catch(err => next(err))
   }
-
 }
+
 module.exports = adminController
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const router = express.Router()
-const { authenticatedAdmin } = require('../../middleware/auth')
+
 const adminController = require('../../controllers/admin-controller')
 
-router.get('/restaurants', authenticatedAdmin, adminController.getRestaurants)
-router.use('/', (req, res) => { res.redirect('/admin/restaurants') })
+router.get('/restaurants/create', adminController.createRestaurants)
 
+router.get('/restaurants', adminController.getRestaurants)
+router.post('/restaurants', adminController.postRestaurant)
+
+router.use('/', (req, res) => { res.redirect('/admin/restaurants') })
 module.exports = router
