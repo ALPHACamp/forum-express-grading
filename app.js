2a2d72027adb7cf5969776c554459039c8f0b8be
YD1234564789 <a8111202003@gmail.com>
2023-08-19 02:02:58 +0800

    feat: admin update restaurant

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const express = require('express')
 const handlebars = require('express-handlebars')
 const flash = require('connect-flash')
 const session = require('express-session')
+const methodOverride = require('method-override')
 const passport = require('./config/passport')
 const handlebarsHelpers = require('./helpers/handlebars-helpers')
 const { getUser } = require('./helpers/auth-helpers')
 const SESSION_SECRET = 'secret'
 const routes = require('./routes')
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3000 
 
 app.engine('hbs', handlebars({ extname: '.hbs', helpers: handlebarsHelpers }))
 app.set('view engine', 'hbs')
@@ -16,6 +17,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
 app.use(passport.initialize()) // 要在session後
 app.use(passport.session())
+app.use(methodOverride('_method'))
 app.use(flash())
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
