acb506b717a14f1c70b1e1b0096c8c5d031bcafc
BoxunW <tony1572002@gmail.com>
2023-08-19 19:31:49 +0800

    feat: finish R05

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -96,6 +96,24 @@ const restaurantController = {
         })
       })
       .catch(err => next(err))
+  },
+  getTopRestaurants: (req, res, next) => {
+    return Restaurant.findAll({
+      include: [{ model: User, as: 'FavoritedUsers' }]
+    })
+      .then(restaurants => {
+        const result = restaurants
+          .map(r => ({
+            ...r.toJSON(),
+            description: r.description.substring(0, 100),
+            isFavorited: req.user && req.user.FavoritedRestaurants.some(fr => r.id === fr.id),
+            favoritedCount: r.FavoritedUsers.length
+          }))
+          .sort((a, b) => b.favoritedCount - a.favoritedCount)
+          .slice(0, 10)
+        res.render('top-restaurants', { restaurants: result })
+      })
+      .catch(err => next(err))
   }
 }
 module.exports = restaurantController
