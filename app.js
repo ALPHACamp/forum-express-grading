ff8e5e05f04755417ec3e0433f1639427015018d
tsaiiii08 <lintsaiting0824@gamil.com>
2023-08-22 17:02:01 +0800

    passport init & signin

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const db = require('./models')
 const handlebars = require('express-handlebars')
 const flash = require('connect-flash')
 const session = require('express-session')
+const passport = require('./config/passport')
 const SESSION_SECRET = 'secret'
 
 // 註冊 Handlebars 樣板引擎，並指定副檔名為 .hbs
@@ -14,6 +15,8 @@ app.engine('hbs', handlebars({ extname: '.hbs' }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
+app.use(passport.initialize()) //初始化 Passport
+app.use(passport.session()) //啟動 session 功能
 app.use(flash()) // 掛載套件
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')  // 設定 success_msg 訊息
