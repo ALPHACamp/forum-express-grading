b6fdd618ad675934da0b6672ca8501fb6c34da9a
tsaiiii08 <lintsaiting0824@gamil.com>
2023-08-22 14:26:17 +0800

    user signup

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const handlebars = require('express-handlebars')
 app.engine('hbs', handlebars({ extname: '.hbs' }))
 // 設定使用 Handlebars 做為樣板引擎
 app.set('view engine', 'hbs')
+app.use(express.urlencoded({ extended: true }))
 app.use(routes)
 
 app.listen(port, () => {
