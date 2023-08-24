779cc65085297ff41ecbf290737b90978a6e99f1
ciao0603 <jocelyn94032.0@gmail.com>
2023-08-24 16:50:22 +0800

    feat: admin create restaurant

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -3,6 +3,8 @@ const router = express.Router()
 
 const adminController = require('../../controllers/admin-controller')
 
+router.get('/restaurants/create', adminController.createRestaurant)
+router.post('/restaurants', adminController.postRestaurant)
 router.get('/restaurants', adminController.getRestaurants)
 router.get('/', (req, res) => res.redirect('/admin/restaurants'))
 
