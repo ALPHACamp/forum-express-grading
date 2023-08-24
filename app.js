6b300dd72139338685a3a02d33dfa7c147d7e7fc
ciao0603 <jocelyn94032.0@gmail.com>
2023-08-24 18:19:59 +0800

    feat: admin update restaurant

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const exphbs = require('express-handlebars')
 const session = require('express-session')
 const flash = require('connect-flash')
+const methodOverride = require('method-override')
 
 const hbsHelpers = require('./helpers/hbs-helpers')
 const passport = require('./config/passport')
@@ -15,6 +16,7 @@ const SESSION_SECRET = 'secret'
 app.engine('hbs', exphbs({ extname: '.hbs', helpers: hbsHelpers }))
 app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
 app.use(session({
   secret: SESSION_SECRET,
   resave: false,
