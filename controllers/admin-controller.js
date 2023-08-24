75f368dcc25b23148f3052f153041facb2b1feae
ciao0603 <jocelyn94032.0@gmail.com>
2023-08-24 18:33:07 +0800

    feat: admin delete restaurant

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -61,6 +61,17 @@ const adminController = {
         res.redirect('/admin/restaurants')
       })
       .catch(err => next(err))
+  },
+  deleteRestaurant: (req, res, next) => {
+    const id = req.params.id
+    Restaurant.findByPk(id)
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+
+        return restaurant.destroy()
+      })
+      .then(() => res.redirect('/admin/restaurants'))
+      .catch(err => next(err))
   }
 }
 
