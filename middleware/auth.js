e04fa112cba6a11a46893e911dd040584e371d19
freeanser <bnn01615@gmail.com>
2023-08-27 20:26:41 -0700

    feat: User permission management

--- middleware/auth.js
+++ middleware/auth.js
@@ -8,12 +8,13 @@ const authenticated = (req, res, next) => {
   res.redirect('/signin')
 }
 const authenticatedAdmin = (req, res, next) => {
-  // if (req.isAuthenticated)
-  if (helpers.ensureAuthenticated(req)) {
-    if (helpers.getUser(req).isAdmin) return next()
-    res.redirect('/')
+  // if (req.isAuthenticated) // 將此行 驗證使用者功能 外包成 helpers 做管理
+
+  if (helpers.ensureAuthenticated(req)) { // 驗證使用者
+    if (helpers.getUser(req).isAdmin) return next() // 如果撈到的使用者 isAdmin=true，就 return 下一步
+    res.redirect('/') // 否則就回首頁
   } else {
-    res.redirect('/signin')
+    res.redirect('/signin') // 如果撈不到使用者（沒有登入），就回去 signin 頁面
   }
 }
 module.exports = {
