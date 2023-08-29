a25f0bd0c223f9664d9b19e64ac4245bb1db11e5
YD1234564789 <a8111202003@gmail.com>
2023-08-30 01:22:08 +0800

    feat: adjust middleware/auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,13 +1,14 @@
-const { ensureAuthenticated, getUser } = require('../helpers/auth-helpers')
+const helpers = require('../helpers/auth-helpers')
+
 const authenticated = (req, res, next) => {
-  if (ensureAuthenticated(req)) {
+  if (helpers.ensureAuthenticated(req)) {
     return next()
   }
   res.redirect('/signin')
 }
 const authenticatedAdmin = (req, res, next) => {
-  if (ensureAuthenticated(req)) {
-    if (getUser(req).isAdmin) return next()
+  if (helpers.ensureAuthenticated(req)) {
+    if (helpers.getUser(req).isAdmin) return next()
     res.redirect('/')
   } else {
     res.redirect('/signin')
