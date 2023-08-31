67db1703269ae71e26140b720077304e7c955275
candacechou <candace01250125@gmail.com>
2023-08-31 14:51:52 +0800

    feat: passport init and signin

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const handlebars = require('express-handlebars')
 const flash = require('connect-flash')
 const session = require('express-session')
 const routes = require('./routes')
-
+const passport = require('./config/passport')
 const app = express()
 const port = process.env.PORT || 3000
 const SESSION_SECRET = 'secret'
@@ -13,10 +13,12 @@ app.set('view engine', 'hbs')
 app.use(express.urlencoded({ extended: true }))
 
 app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
+app.use(passport.initialize())
+app.use(passport.session())
 app.use(flash()) // 掛載套件
 app.use((req, res, next) => {
-  res.locals.success_messages = req.flash('success_messages')  // 設定 success_msg 訊息
-  res.locals.error_messages = req.flash('error_messages')  // 設定 warning_msg 訊息
+  res.locals.success_messages = req.flash('success_messages')
+  res.locals.error_messages = req.flash('error_messages')
   next()
 })
 
