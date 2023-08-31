e8b75b5e6e3e15d97511cb21d628429ebf6599ff
candacechou <candace01250125@gmail.com>
2023-08-31 16:25:38 +0800

    feat:admin update restaurant

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
+const methodOverride = require('method-override')
 const flash = require('connect-flash')
 const session = require('express-session')
 const routes = require('./routes')
@@ -18,6 +19,7 @@ app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: fals
 app.use(passport.initialize())
 app.use(passport.session())
 app.use(flash()) // 掛載套件
+app.use(methodOverride('_method'))
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
   res.locals.error_messages = req.flash('error_messages')
