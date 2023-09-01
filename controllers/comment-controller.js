5619b84b6748ba196124ee4e15b632f844cf95b1
candacechou <candace01250125@gmail.com>
2023-09-01 15:43:25 +0800

    feat:show comments on restaurant page

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -1,9 +1,12 @@
 const { Comment, User, Restaurant } = require('../models')
+
 const commentController = {
   postComment: (req, res, next) => {
     const { restaurantId, text } = req.body
     const userId = req.user.id
+
     if (!text) throw new Error('Comment text is required!')
+
     return Promise.all([
       User.findByPk(userId),
       Restaurant.findByPk(restaurantId)
@@ -11,6 +14,7 @@ const commentController = {
       .then(([user, restaurant]) => {
         if (!user) throw new Error("User didn't exist!")
         if (!restaurant) throw new Error("Restaurant didn't exist!")
+
         return Comment.create({
           text,
           restaurantId,
@@ -23,4 +27,5 @@ const commentController = {
       .catch(err => next(err))
   }
 }
+
 module.exports = commentController
--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,46 +1,53 @@
-const { Restaurant, Category } = require('../models')
+const { Restaurant, Category, Comment, User } = require('../models')
 const { getOffset, getPagination } = require('../helpers/pagination-helper')
 const restaurantController = {
   getRestaurants: (req, res, next) => {
     const DEFAULT_LIMIT = 9
+
     const categoryId = Number(req.query.categoryId) || ''
+
     const page = Number(req.query.page) || 1
     const limit = Number(req.query.limit) || DEFAULT_LIMIT
     const offset = getOffset(limit, page)
-    const where = {}
-    if (categoryId) where.categoryId = categoryId
+
     return Promise.all([
-      Restaurant.findAll({
+      Restaurant.findAndCountAll({
         include: Category,
-        where: where,
+        where: {
+          ...categoryId ? { categoryId } : {}
+        },
         limit,
         offset,
         nest: true,
         raw: true
       }),
       Category.findAll({ raw: true })
-    ]).then(([restaurants, categories]) => {
-      const data = restaurants.map(r => ({
-        ...r,
-        description: r.description.substring(0, 50)
-      }))
-      return res.render('restaurants', {
-        restaurants: data,
-        categories,
-        categoryId,
-        pagination: getPagination(limit, page, restaurants.count)
+    ])
+      .then(([restaurants, categories]) => {
+        const data = restaurants.rows.map(r => ({
+          ...r,
+          description: r.description.substring(0, 50)
+        }))
+        return res.render('restaurants', {
+          restaurants: data,
+          categories,
+          categoryId,
+          pagination: getPagination(limit, page, restaurants.count)
+        })
       })
-    })
-      .catch(err => next(err)) // 補上
+      .catch(err => next(err))
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
-      include: Category,
-      nest: true,
-      raw: true
+      include: [
+        Category,
+        { model: Comment, include: User }
+      ]
+
     }).then(restaurant => {
       if (!restaurant) throw new Error("Restuarant didn't exist!")
-      res.render('restaurant', { restaurant })
+      console.log(restaurant.toJSON())
+      res.render('restaurant', { restaurant: restaurant.toJSON() })
     }).catch(err => next(err))
   }
 }
