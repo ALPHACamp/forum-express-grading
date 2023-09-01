ae730a5e29b32cf8b4567f23f5e7deb37f374f1c
candacechou <candace01250125@gmail.com>
2023-09-01 13:09:59 +0800

    feat:add restaurant page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -14,6 +14,16 @@ const restaurantController = {
         restaurants: data
       })
     })
+  },
+  getRestaurant: (req, res, next) => {
+    return Restaurant.findByPk(req.params.id, {
+      include: Category,
+      nest: true,
+      raw: true
+    }).then(restaurant => {
+      if (!restaurant) throw new Error("Restuarant didn't exist!")
+      res.render('restaurant', { restaurant })
+    }).catch(err => next(err))
   }
 }
 module.exports = restaurantController
--- routes/index.js
+++ routes/index.js
@@ -15,6 +15,7 @@ router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
 router.get('/logout', userController.logout)
 router.get('/restaurants', authenticated, restController.getRestaurants)
+router.get('/restaurants/:id', authenticated, restController.getRestaurant)
 router.use('/', (req, res) => res.redirect('/restaurants'))
 
 router.use('/', generalErrorHandler)
