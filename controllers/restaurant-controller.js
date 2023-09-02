d78e736ea8535a524ae50c548aa98b772daf6564
tsaiiii08 <lintsaiting0824@gamil.com>
2023-09-02 21:13:37 +0800

    show comments on restaurant page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,4 +1,4 @@
-const { Restaurant, Category } = require('../models')
+const { Restaurant, Category, Comment, User } = require('../models')
 const { getOffset, getPagination } = require('../helpers/pagination-helper')
 const restaurantController = {
   getRestaurants: (req, res) => {
@@ -36,7 +36,8 @@ const restaurantController = {
   },
   getRestaurant: (req, res) => {
     return Restaurant.findByPk(req.params.id, {
-      include: Category,
+      include: [Category,
+        { model: Comment, include: User }],
       nest: true,
       raw: false
     })
