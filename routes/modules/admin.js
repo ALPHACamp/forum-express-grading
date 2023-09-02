b2e47d854a5c1980ad5e835d944139fafa204f3c
Tommy0901 <ak710180@gmail.com>
2023-09-02 21:34:17 +0800

    feat: admin update restaurant

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -4,7 +4,9 @@ const router = express.Router()
 const adminController = require('../../controllers/admin-controller')
 
 router.get('/restaurants/create', adminController.createRestaurant) // 新增這行
+router.get('/restaurants/:id/edit', adminController.editRestaurant) // 新增這一行
 router.get('/restaurants/:id', adminController.getRestaurant) // 新增這一行
+router.put('/restaurants/:id', adminController.putRestaurant) // 新增這一行
 router.get('/restaurants', adminController.getRestaurants) // 修改這行，新增 authenticatedAdmin 參數
 router.post('/restaurants', adminController.postRestaurant) // 新增這行
 
