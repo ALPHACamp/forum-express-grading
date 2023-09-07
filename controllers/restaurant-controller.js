a0468e48f66fd39b485f78ba36de0ca0588f52c2
CYW-Allen <chen900445@gmail.com>
2023-09-07 23:31:14 +0800

    feat: add index page

---
+++ controllers/restaurant-controller.js
@@ -0,0 +1,7 @@
+const restaurantController = {
+  getRestaurants (_req, res) {
+    return res.render('restaurants')
+  }
+}
+
+module.exports = restaurantController
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const router = express.Router()
 
+const restaurantController = require('../controllers/restaurant-controller')
+
+router.get('/restaurants', restaurantController.getRestaurants)
 router.get('/', (req, res) => {
   res.send('Hello World!')
 })
