b41067b6a94c1f690bd26be76ac5bdbe82d0e65a
Peter-Haung888 <ming31437@gmail.com>
2023-09-07 18:30:17 +0800

    feat:category delete

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -1,4 +1,4 @@
-const { Category } = require('../models')
+const { Restaurant, Category } = require('../models')
 
 const categoryController = {
   getCategories: (req, res, next) => {
@@ -35,8 +35,36 @@ const categoryController = {
       .then(() => res.redirect('/admin/categories'))
       .catch(err => next(err))
   },
-  deleteCategory: (req, res, next) => {
-    console.log('function')
+  deleteCategory: async (req, res, next) => {
+    const deleteCategoryId = req.params.id
+    const uncategory = await Category.findOne({
+      raw: true,
+      where: { name: '未分類' }
+    })
+    const uncategoryId = uncategory.id
+    // 反查deleteCategory是否存在
+    try {
+      const deleteCategory = await Category.findByPk(deleteCategoryId, { raw: true })
+      if (!deleteCategory) { throw new Error("The category didn't exist!") }
+      if (deleteCategory.name === '未分類') { throw new Error("You can't delete this category!") }
+    } catch (err) {
+      next(err)
+    }
+    // 找出與deleteCategoryId相關的餐廳
+    try {
+      const restaurants = await Restaurant.findAll({ where: { categoryId: deleteCategoryId } })
+      if (restaurants) {
+        await Restaurant.update({ categoryId: uncategoryId }, { // 將categoryId變成未分類的ID
+          where: {
+            categoryId: deleteCategoryId
+          }
+        })
+      }
+      await Category.destroy({ where: { id: deleteCategoryId } })
+    } catch (err) {
+      next(err)
+    }
+    res.redirect('/admin/categories')
   }
 }
 
