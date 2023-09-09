65bda7be3799ea77413e2fb79feb0062e497b9cc
carola1408 <118614918+carola1408@users.noreply.github.com>
2023-09-09 16:36:52 +0800

    feat: add admin-services

--- controllers/pages/admin-controller.js
+++ controllers/pages/admin-controller.js
@@ -1,15 +1,10 @@
+const adminServices = require('../../services/admin-services') // 新增這裡，引入 admin-services
 const { Restaurant, User, Category } = require('../../models')
 const { imgurFileHandler } = require('../../helpers/file-helpers')
 const adminController = {
+  // 修改以下
   getRestaurants: (req, res, next) => {
-    Restaurant.findAll({
-      raw: true,
-      nest: true, // 增加這裡
-      include: [Category] // 增加這裡
-    })
-
-      .then(restaurants => res.render('admin/restaurants', { restaurants }))
-      .catch(err => next(err))
+    adminServices.getRestaurants(req, (err, data) => err ? next(err) : res.render('admin/restaurants', data))
   },
   // 修改這裡
   createRestaurant: (req, res, next) => {
--- routes/apis/index.js
+++ routes/apis/index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
-
+const admin = require('./modules/admin') // 新增這裡
 const restController = require('../../controllers/apis/restaurant-controller')
+router.use('/admin', admin) // 新增這裡
 router.get('/restaurants', restController.getRestaurants)
 module.exports = router
