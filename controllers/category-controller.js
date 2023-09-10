220123df0fb7d81fc4e35d34715d8050f5e492d8
CYW-Allen <chen900445@gmail.com>
2023-09-10 16:45:31 +0800

    feat: category update

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -1,11 +1,14 @@
 const { Category } = require('../models')
 
 module.exports = {
-  async getCategories (_req, res, next) {
+  async getCategories (req, res, next) {
     try {
-      res.render('admin/categories', {
-        categories: await Category.findAll({ raw: true })
-      })
+      const [categories, category] = await Promise.all([
+        Category.findAll({ raw: true }),
+        req.params.id ? Category.findByPk(req.params.id, { raw: true }) : null
+      ])
+
+      res.render('admin/categories', { categories, category })
     } catch (err) {
       next(err)
     }
@@ -17,6 +20,20 @@ module.exports = {
       if (!name || !name.replace(/\s/g, '').length) throw new Error('Category name is required')
       await Category.create({ name })
       req.flash('success_messages', 'A Category was successfully created')
+      res.redirect('/admin/categories')
+    } catch (err) {
+      next(err)
+    }
+  },
+  async putCategory (req, res, next) {
+    try {
+      const { name } = req.body
+      if (!name || !name.replace(/\s/g, '').length) throw new Error('Category name is required')
+
+      const category = await Category.findByPk(req.params.id)
+      if (!category) throw new Error('The category is not existed')
+      await category.update({ name })
+      req.flash('success_messages', 'The category was successfully to update')
       res.redirect('/admin/categories')
     } catch (err) {
       next(err)
