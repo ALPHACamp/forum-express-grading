47edb2d2949b12988d8098690e7978c65add87eb
CYW-Allen <chen900445@gmail.com>
2023-09-10 16:25:13 +0800

    feat: category create

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -9,5 +9,17 @@ module.exports = {
     } catch (err) {
       next(err)
     }
+  },
+  async postCategory (req, res, next) {
+    try {
+      const { name } = req.body
+
+      if (!name || !name.replace(/\s/g, '').length) throw new Error('Category name is required')
+      await Category.create({ name })
+      req.flash('success_messages', 'A Category was successfully created')
+      res.redirect('/admin/categories')
+    } catch (err) {
+      next(err)
+    }
   }
 }
--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -13,6 +13,7 @@ router.patch('/users/:id', adminController.patchUser)
 router.get('/users', adminController.getUsers)
 router.post('/restaurants', upload.single('image'), adminController.postRestaurant)
 router.get('/categories', categoryController.getCategories)
+router.post('/categories', categoryController.postCategory)
 router.get('', (_req, res) => res.redirect('/admin/restaurants'))
 
 module.exports = router
