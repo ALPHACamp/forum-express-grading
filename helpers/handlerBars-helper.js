9ef3435b0a23b5bfbc533360893afe6415904480
CYW-Allen <chen900445@gmail.com>
2023-09-10 15:36:25 +0800

    feat: add ifConf hbs helper & update category selector

--- helpers/handlerBars-helper.js
+++ helpers/handlerBars-helper.js
@@ -2,6 +2,9 @@ const dayjs = require('dayjs')
 
 module.exports = {
   currentYear: () => dayjs().year(),
+  ifCond (a, b, options) {
+    return a === b ? options.fn(this) : options.inverse(this)
+  },
   activeRestaurantsTab: currentPage => currentPage === 'restaurants' ? 'active' : '',
   activeUsersTab: currentPage => currentPage === 'users' ? 'active' : '',
   userRole: isAdmin => isAdmin ? 'admin' : 'user',
