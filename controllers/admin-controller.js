dad8f52b771da7bcc2ac5c31ed6e79182f7b0f76
CYW-Allen <chen900445@gmail.com>
2023-09-10 15:12:34 +0800

    feat: add categories selector on admin create and edit page

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -16,16 +16,16 @@ module.exports = {
     }
   },
   async createRestaurant (_req, res) {
-    res.render('admin/create-restaurant')
+    res.render('admin/create-restaurant', { categories: await Category.findAll({ raw: true }) })
   },
   async postRestaurant (req, res, next) {
     try {
-      const { name, tel, address, openingHours, description } = req.body
+      const { name, tel, address, openingHours, description, categoryId } = req.body
 
       if (!name || !name.replace(/\s/g, '').length) throw new Error('Restaurant name is required')
 
       const filePath = await localFileHandler(req.file)
-      await Restaurant.create({ name, tel, address, openingHours, description, image: filePath || null })
+      await Restaurant.create({ name, tel, address, openingHours, description, image: filePath || null, categoryId })
       req.flash('success_messages', 'A restaurant was successfully created')
       res.redirect('/admin/restaurants')
     } catch (err) {
@@ -48,17 +48,20 @@ module.exports = {
   },
   async editRestaurant (req, res, next) {
     try {
-      const restaurant = await Restaurant.findByPk(req.params.id, { raw: true })
+      const [restaurant, categories] = await Promise.all([
+        Restaurant.findByPk(req.params.id, { raw: true }),
+        Category.findAll({ raw: true })
+      ])
 
       if (!restaurant) throw new Error('The restaurant is not existed.')
-      res.render('admin/edit-restaurant', { restaurant })
+      res.render('admin/edit-restaurant', { restaurant, categories })
     } catch (err) {
       next(err)
     }
   },
   async putRestaurant (req, res, next) {
     try {
-      const { name, tel, address, openingHours, description } = req.body
+      const { name, tel, address, openingHours, description, categoryId } = req.body
 
       if (!name || !name.replace(/\s/g, '').length) throw new Error('Restaurant name is required')
 
@@ -68,7 +71,7 @@ module.exports = {
       ])
 
       if (!restaurant) throw new Error('The restaurant is not existed')
-      await restaurant.update({ name, tel, address, openingHours, description, image: filePath || restaurant.image })
+      await restaurant.update({ name, tel, address, openingHours, description, image: filePath || restaurant.image, categoryId })
       req.flash('success_messages', 'The restaurant was successfully to update')
       res.redirect('/admin/restaurants')
     } catch (err) {
