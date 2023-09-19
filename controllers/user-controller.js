bc142a7a91f5783bd717998cf2cc4f0189132522
Sunnylin0320 <sunny8503201@gmail.com>
2023-09-19 09:59:36 +0800

    feat: flash msg & signup verification

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -5,9 +5,16 @@ const userController = {
   signUpPage: (req, res) => {
     res.render("signup");
   },
-  signUp: (req, res) => {
-    bcrypt
-      .hash(req.body.password, 10)
+  signUp: (req, res, next) => {
+    if (req.body.password !== req.body.passwordCheck)
+      throw new Error("Passwords do not match!");
+
+    User.findOne({ where: { email: req.body.email } })
+      .then((user) => {
+        if (user) throw new Error("Email already exists!");
+
+        return bcrypt.hash(req.body.password, 10);
+      })
       .then((hash) =>
         User.create({
           name: req.body.name,
@@ -16,8 +23,11 @@ const userController = {
         })
       )
       .then(() => {
+        req.flash("success_messages", "成功註冊帳號！");
         res.redirect("/signin");
-      });
+      })
+      .catch((err) => next(err));
   },
 };
+
 module.exports = userController;
--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,14 @@ const admin = require("./modules/admin");
 
 const restController = require('../controllers/restaurant-controller')
 const userController = require("../controllers/user-controller");
+const { generalErrorHandler } = require("../middleware/error-handler");
+
 
 router.use('/admin', admin) // 新增這行
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp) //注意用 post
 router.get('/restaurants', restController.getRestaurants)
 router.use('/', (req, res) => res.redirect('/restaurants'))
+router.use("/", generalErrorHandler);
 
 module.exports = router
