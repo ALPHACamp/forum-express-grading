78f381bfcc36c3f8f3869fcd04b9d092baa1a34e
tw3638993 <tw3638993@gmail.com>
2023-09-20 23:33:01 +0800

    feat: delete restaurant

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -65,6 +65,19 @@ const adminController = {
         res.redirect('/admin/restaurants')
       })
       .catch(err => next(err))
+  },
+  deleteRestaurant: (req, res, next) => {
+    const id = req.params.id
+    return Restaurant.findByPk(id)
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        return restaurant.destroy()
+      })
+      .then(() => {
+        req.flash('success', 'Delete restaurant successfully!')
+        res.redirect('/admin/restaurants')
+      })
+      .catch(err => next(err))
   }
 }
 
