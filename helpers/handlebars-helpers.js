5217679731072b12bb292e1c721fecd3f419cec2
xrchitron <yuahochen1124@gmail.com>
2023-09-21 00:59:12 +0800

    feat: add ifEq hbs helper & update category selector

--- helpers/handlebars-helpers.js
+++ helpers/handlebars-helpers.js
@@ -1,4 +1,7 @@
 const dayjs = require('dayjs') // invoke dayjs
 module.exports = {
-  currentYear: () => dayjs().year() // get current year and export
+  currentYear: () => dayjs().year(), // get current year and export
+  ifEq: function (a, b, option) {
+    return a === b ? option.fn(this) : option.inverse(this)
+  }
 }
