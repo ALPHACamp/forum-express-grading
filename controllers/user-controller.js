0746fcf8cc6cc127b99c2a64bde36b5847f00950
xrchitron <yuahochen1124@gmail.com>
2023-09-22 23:09:53 +0800

    feat: add user's commited restaurant

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcryptjs')
-const db = require('../models')
-const { User } = db
+const { User, Comment, Restaurant } = require('../models')
+const { localFileHandler } = require('../helpers/file-helper')
+const DEFAULT_AVATAR = 'https://static.vecteezy.com/system/resources/previews/009/734/564/original/default-avatar-profile-icon-of-social-media-user-vector.jpg'
 const userController = {
   signUpPage: (req, res) => {
     res.render('signup')
@@ -39,10 +40,15 @@ const userController = {
     res.redirect('/signin')
   },
   getUser: (req, res, next) => {
-    return User.findByPk(req.params.id, { raw: true })
+    return User.findByPk(req.params.id, {
+      include: [
+        { model: Comment, include: Restaurant }
+      ]
+    })
       .then(user => {
+        console.log(user.toJSON())
         if (!user) throw new Error("User didn't exist!")
-        res.render('users/profile', { user })
+        res.render('users/profile', { user: user.toJSON(), DEFAULT_AVATAR })
       })
       .catch(err => next(err))
   },
@@ -54,8 +60,24 @@ const userController = {
       })
       .catch(err => next(err))
   },
-  putUser: (req, res) => {
-    res.redirect('users/profile')
+  putUser: (req, res, next) => {
+    const { name } = req.body
+    const userId = req.params.id
+    if (!name) throw new Error('User name is required!')
+    const { file } = req
+    return Promise.all([
+      User.findByPk(userId),
+      localFileHandler(file)
+    ])
+      .then(([user, filePath]) => {
+        if (!user) throw new Error("User didn't exist!")
+        return user.update({ name, image: filePath || user.image })
+      })
+      .then(() => {
+        req.flash('success_messages', '使用者資料編輯成功')
+        return res.redirect(`/users/${userId}`)
+      })
+      .catch(err => next(err))
   }
 }
 module.exports = userController
