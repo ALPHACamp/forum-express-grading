4bdd1c17e35cc71e7b62741e2e6e1a421ee427f9
tw3638993 <tw3638993@gmail.com>
2023-09-23 14:18:28 +0800

    feat: add categories selector on admin create and edit page

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -1,49 +1,57 @@
-const { Restaurant, User, Category } = require('../models')
-const { localFileHandler } = require('../helpers/file-helper')
+const { Restaurant, User, Category } = require("../models");
+const { localFileHandler } = require("../helpers/file-helper");
 
 const adminController = {
   getRestaurants: (req, res, next) => {
     Restaurant.findAll({
       raw: true,
       nest: true,
-      include: [Category]
+      include: [Category],
     })
-      .then(restaurants => {
-        res.render('admin/restaurants', { restaurants })
+      .then((restaurants) => {
+        res.render("admin/restaurants", { restaurants });
       })
-      .catch(err => next(err))
+      .catch((err) => next(err));
   },
-  createRestaurant: (req, res) => {
-    res.render('admin/create-restaurant')
+  createRestaurant: (req, res, next) => {
+    return Category.findAll({
+      raw: true,
+    })
+      .then((categories) => {
+        res.render("admin/create-restaurant", { categories });
+      })
+      .catch((err) => next(err));
   },
   postRestaurant: (req, res, next) => {
-    const { name, tel, address, openingHours, description } = req.body
-    if (!name) throw new Error('Restaurant name is required!')
+    const { name, categoryId, tel, address, openingHours, description } =
+      req.body;
+    if (!name) throw new Error("Restaurant name is required!");
 
-    const { file } = req
+    const { file } = req;
     localFileHandler(file)
-      .then(filePath =>
+      .then((filePath) =>
         Restaurant.create({
           name,
+          categoryId,
           tel,
           address,
           openingHours,
           description,
-          image: filePath || null
+          image: filePath || null,
         })
       )
       .then(() => {
-        req.flash('success', 'restaurant was successfully created')
-        res.redirect('/admin/restaurants')
+        req.flash("success", "restaurant was successfully created");
+        res.redirect("/admin/restaurants");
       })
-      .catch(err => next(err))
+      .catch((err) => next(err));
   },
   getRestaurant: (req, res, next) => {
-    const id = req.params.id
+    const id = req.params.id;
     Restaurant.findByPk(id, {
       raw: true,
       nest: true,
-      include: [Category]
+      include: [Category],
     })
       .then((restaurant) => {
         if (!restaurant) throw new Error("Restaurant didn't exist!");
@@ -52,79 +60,83 @@ const adminController = {
       .catch((err) => next(err));
   },
   editRestaurant: (req, res, next) => {
-    const id = req.params.id
-    Restaurant.findByPk(id, {
-      raw: true
-    })
-      .then(restaurant => {
-        if (!restaurant) throw new Error("Restaurant didn't exist!")
-        res.render('admin/edit-restaurant', { restaurant })
+    const id = req.params.id;
+    return Promise.all([
+      Category.findAll({ raw: true }),
+      Restaurant.findByPk(id, {
+        raw: true
       })
-      .catch(err => next(err))
+    ])
+      .then(([categories, restaurant]) => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!");
+        res.render("admin/edit-restaurant", { restaurant, categories });
+      })
+      .catch((err) => next(err));
   },
   putRestaurant: (req, res, next) => {
-    const { name, tel, address, openingHours, description } = req.body
-    const id = req.params.id
-    if (!name) throw new Error('Restaurant name is required!')
+    const { name, categoryId, tel, address, openingHours, description } = req.body;
+    const id = req.params.id;
+    if (!name) throw new Error("Restaurant name is required!");
 
-    const { file } = req
+    const { file } = req;
     Promise.all([Restaurant.findByPk(id), localFileHandler(file)])
       .then(([restaurant, filePath]) => {
-        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        if (!restaurant) throw new Error("Restaurant didn't exist!");
         return restaurant.update({
           name,
+          categoryId,
           tel,
           address,
           openingHours,
           description,
-          image: filePath || restaurant.image
-        })
+          image: filePath || restaurant.image,
+        });
       })
       .then(() => {
-        req.flash('success', 'restaurant was successfully to update')
-        res.redirect('/admin/restaurants')
+        req.flash("success", "restaurant was successfully to update");
+        res.redirect("/admin/restaurants");
       })
-      .catch(err => next(err))
+      .catch((err) => next(err));
   },
   deleteRestaurant: (req, res, next) => {
-    const id = req.params.id
+    const id = req.params.id;
     return Restaurant.findByPk(id)
-      .then(restaurant => {
-        if (!restaurant) throw new Error("Restaurant didn't exist!")
-        return restaurant.destroy()
+      .then((restaurant) => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!");
+        return restaurant.destroy();
       })
       .then(() => {
-        req.flash('success', 'Delete restaurant successfully!')
-        res.redirect('/admin/restaurants')
+        req.flash("success", "Delete restaurant successfully!");
+        res.redirect("/admin/restaurants");
       })
-      .catch(err => next(err))
+      .catch((err) => next(err));
   },
   getUsers: (req, res, next) => {
     return User.findAll({
-      raw: true
+      raw: true,
     })
-      .then(users => {
-        res.render('admin/users', { users })
+      .then((users) => {
+        res.render("admin/users", { users });
       })
-      .catch(err => next(err))
+      .catch((err) => next(err));
   },
   patchUser: (req, res, next) => {
-    const id = req.params.id
+    const id = req.params.id;
     return User.findByPk(id)
-      .then(user => {
-        if (!user) throw new Error("User didn't exist!")
-        if (user.email === 'root@example.com') {
-          req.flash('error', '禁止變更 root 權限')
-          return res.redirect('back')
+      .then((user) => {
+        if (!user) throw new Error("User didn't exist!");
+        if (user.email === "root@example.com") {
+          req.flash("error", "禁止變更 root 權限");
+          return res.redirect("back");
         }
-        return user.update({ isAdmin: user.isAdmin ? 0 : 1 })
+        return user.update({ isAdmin: user.isAdmin ? 0 : 1 });
       })
       .then(() => {
-        req.flash('success', '使用者權限變更成功')
-        return res.redirect('/admin/users')
+        req.flash("success", "使用者權限變更成功");
+        return res.redirect("/admin/users");
       })
-      .catch(err => next(err))
-  }
-}
+      .catch((err) => next(err));
+  },
+};
 
-module.exports = adminController
+module.exports = adminController;
