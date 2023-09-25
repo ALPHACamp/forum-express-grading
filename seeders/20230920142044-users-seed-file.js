d04bd47068883d2e90057a72012330a4173fb3a4
Sunnylin0320 <sunny8503201@gmail.com>
2023-09-25 22:34:58 +0800

    feat: modify config for heroku

--- seeders/20230920142044-users-seed-file.js
+++ seeders/20230920142044-users-seed-file.js
@@ -1,41 +1,41 @@
-"use strict";
-const bcrypt = require("bcryptjs");
+'use strict'
+const bcrypt = require('bcryptjs')
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.bulkInsert(
-      "Users",
+      'Users',
       [
         {
           // 一次新增三筆資料
-          email: "root@example.com",
-          password: await bcrypt.hash("12345678", 10),
+          email: 'root@example.com',
+          password: await bcrypt.hash('12345678', 10),
           is_admin: true,
-          name: "root",
+          name: 'root',
           created_at: new Date(),
-          updated_at: new Date(),
+          updated_at: new Date()
         },
         {
-          email: "user1@example.com",
-          password: await bcrypt.hash("12345678", 10),
+          email: 'user1@example.com',
+          password: await bcrypt.hash('12345678', 10),
           is_admin: false,
-          name: "user1",
+          name: 'user1',
           created_at: new Date(),
-          updated_at: new Date(),
+          updated_at: new Date()
         },
         {
-          email: "user2@example.com",
-          password: await bcrypt.hash("12345678", 10),
+          email: 'user2@example.com',
+          password: await bcrypt.hash('12345678', 10),
           is_admin: false,
-          name: "user2",
+          name: 'user2',
           created_at: new Date(),
-          updated_at: new Date(),
-        },
+          updated_at: new Date()
+        }
       ],
       {}
-    );
+    )
   },
   down: async (queryInterface, Sequelize) => {
     // 清空資料表中所有資料
-    await queryInterface.bulkDelete("Users", {});
-  },
-};
+    await queryInterface.bulkDelete('Users', {})
+  }
+}
