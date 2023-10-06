471a00ca44512b4809c1cb776ae7a4a0376bddc3
freeanser <bnn01615@gmail.com>
2023-10-06 16:54:02 -0700

    feat: add restaurant page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -14,6 +14,20 @@ const restaurantController = {
         }))
         return res.render('restaurants', { restaurants: data })
       })
+  },
+  getRestaurant: (req, res, next) => {
+    // const { name, image, openingHours, tel, address, description } = req.body
+    return Restaurant.findByPk(req.params.rid, {
+      include: Category,
+      nest: true,
+      raw: true
+    })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        res.render('restaurant', { restaurant })
+      })
+      .catch(err => next(err))
   }
 }
+
 module.exports = restaurantController
