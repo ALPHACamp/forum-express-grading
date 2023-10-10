956e97126c5dd9f3be0fe8dbd54ad271bf41249d
weitb0315 <weitb0315@gmail.com>
2023-10-10 22:24:53 +0800

    feat: build back side

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const restController = require('../controllers/restaurant-controller')
+const admin = require('./modules/admin')
 
+router.use('/admin', admin)
 router.get('/restaurants', restController.getRestaurants)
 // fallback route setting
 router.use('/', (req, res) => res.redirect('/restaurants'))
