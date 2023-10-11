d42fbfa60334c003b51060354bf4990561230f7c
weitb0315 <weitb0315@gmail.com>
2023-10-11 16:23:30 +0800

    feat: build User Model

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const handlebars = require('express-handlebars')
 const routes = require('./routes')
 const app = express()
 const port = process.env.PORT || 3000
+const db = require('./models')
 
 app.engine('hbs', handlebars({ extname: '.hbs' }))
 app.set('view engine', 'hbs')
