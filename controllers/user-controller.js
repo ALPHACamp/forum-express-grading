fe4f24e0a4f381310ac6fc59fbf365ddf55e3625
weitb0315 <weitb0315@gmail.com>
2023-10-12 11:53:16 +0800

    feat: add user login function

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -25,6 +25,18 @@ const userController = {
         res.redirect('/signin')
       })
       .catch(err => next(err)) // 接住前面拋出的錯誤，呼叫專門做錯誤處理的 middleware
+  },
+  signInPage: (req, res) => {
+    res.render('signin')
+  },
+  signIn: (req, res) => {
+    req.flash('success_messages', '成功登入！')
+    res.redirect('/restaurants')
+  },
+  logout: (req, res) => {
+    req.flash('success_messages', '登出成功！')
+    req.logout()
+    res.redirect('/signin')
   }
 }
 module.exports = userController
