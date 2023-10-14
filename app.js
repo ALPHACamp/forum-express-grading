7fb2ad0246f46f1d7d6d9b0977b8029ee6f67e7a
WenfishTseng <twy0171@outlook.com>
2023-10-14 16:51:06 +0800

    feat: user model add is_admin

--- app.js
+++ app.js
@@ -4,12 +4,14 @@ const routes = require('./routes')
 const flash = require('connect-flash')
 const session = require('express-session')
 const passport = require('./config/passport')
+const { getUser } = require('./helpers/auth-helpers')
+const handlebarsHelpers = require('./helpers/handlebars-helpers')
 
 const app = express()
 const port = process.env.PORT || 3000
 const SESSION_SECRET = 'secret'
 
-app.engine('hbs', handlebars({ extname: '.hbs' })) // 註冊 Handlebars 樣板引擎，並指定副檔名為 .hbs
+app.engine('hbs', handlebars({ extname: '.hbs', helpers: handlebarsHelpers })) // 註冊 Handlebars 樣板引擎，並指定副檔名為 .hbs
 app.set('view engine', 'hbs') // 設定使用 Handlebars 做為樣板引擎
 app.use(express.urlencoded({ extended: true }))
 app.use(session({ secret: SESSION_SECRET, resave: false, saveUninitialized: false }))
@@ -19,6 +21,7 @@ app.use(flash()) // 掛載套件
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages') // 設定 success_msg 訊息
   res.locals.error_messages = req.flash('error_messages') // 設定 warning_msg 訊息
+  res.locals.user = getUser(req)
   next()
 })
 
--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,25 @@ const express = require('express')
 const router = express.Router()
 const passport = require('../config/passport')
 
-// 新增，載入 controller
+// 載入 controller
 const restController = require('../controllers/restaurant-controller')
 const userController = require('../controllers/user-controller')
+
 const admin = require('./modules/admin')
+const { authenticated } = require('../middleware/auth')
 const { generalErrorHandler } = require('../middleware/error-handler')
 
+// 管理員頁面
+router.use('/admin', admin)
+
+// 一般使用者
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
 router.get('/signin', userController.signInPage)
 router.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
-router.use('/admin', admin)
-router.get('/restaurants', restController.getRestaurants)
+router.get('/logout', userController.logout)
+router.get('/restaurants', authenticated, restController.getRestaurants)
+
 router.use('/', (req, res) => res.redirect('/restaurants'))
 router.use('/', generalErrorHandler)
 
