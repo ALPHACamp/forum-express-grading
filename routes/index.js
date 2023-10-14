497e7dc65bbcd6bf15ad39ebfbdefec7ae72de38
WenfishTseng <twy0171@outlook.com>
2023-10-14 11:26:07 +0800

    feat: add user model

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const router = express.Router()
 // 新增，載入 controller
 const restController = require('../controllers/restaurant-controller')
+const admin = require('./modules/admin') // 新增這行，載入 admin.js
 
+router.use('/admin', admin) // 新增這行
 router.get('/restaurants', restController.getRestaurants)
 router.use('/', (req, res) => res.redirect('/restaurants'))
 
