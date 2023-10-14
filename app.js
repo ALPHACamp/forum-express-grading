e7654520a0e40ded9fee1d82cb848b203b8a4337
WenfishTseng <twy0171@outlook.com>
2023-10-14 13:00:32 +0800

    feat: user signup

--- app.js
+++ app.js
@@ -9,10 +9,11 @@ const port = process.env.PORT || 3000
 app.engine('hbs', handlebars({ extname: '.hbs' }))
 // 設定使用 Handlebars 做為樣板引擎
 app.set('view engine', 'hbs')
+app.use(express.urlencoded({ extended: true }))
 app.use(routes)
 
 app.listen(port, () => {
-  console.info(`Example app listening on port ${port}!`)
+  console.log(`App is running on http://localhost:${port}`)
 })
 
 module.exports = app
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const router = express.Router()
+
 // 新增，載入 controller
 const restController = require('../controllers/restaurant-controller')
-const admin = require('./modules/admin') // 新增這行，載入 admin.js
+const userController = require('../controllers/user-controller')
+const admin = require('./modules/admin')
 
-router.use('/admin', admin) // 新增這行
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
+router.use('/admin', admin)
 router.get('/restaurants', restController.getRestaurants)
 router.use('/', (req, res) => res.redirect('/restaurants'))
 
