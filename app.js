1e885d01d7dfd6c8bd934714e2421b28161a1c2f
WenfishTseng <twy0171@outlook.com>
2023-10-16 21:20:10 +0800

    feat: add seed data

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const handlebars = require('express-handlebars') // 引入 express-handlebars
+const path = require('path')
 const routes = require('./routes')
 const flash = require('connect-flash')
 const methodOverride = require('method-override')
@@ -20,6 +21,7 @@ app.use(passport.initialize())
 app.use(passport.session())
 app.use(flash())
 app.use(methodOverride('_method'))
+app.use('/upload', express.static(path.join(__dirname, 'upload')))
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages') // 設定 success_msg 訊息
   res.locals.error_messages = req.flash('error_messages') // 設定 warning_msg 訊息
