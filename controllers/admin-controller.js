c0272454d73c550bcdefe179bf6589924c555566
Antarctic-penguin <jamesantarcticpenguin@gmail.com>
2023-10-16 16:53:54 +0800

    瀏覽一筆餐廳資料

--- controllers/admin-controller.js
+++ controllers/admin-controller.js
@@ -3,7 +3,35 @@ const { Restaurant } = require('../models')
 const adminController = {
   getRestaurants: (req, res, next) => {
     Restaurant.findAll({ raw: true })
-      .then(restaurants => res.render('admin/restaurants',{ restaurants }))
+      .then(restaurants => res.render('admin/restaurants', { restaurants }))
+      .catch(err => next(err))
+  },
+  createRestaurant: (req, res) => {
+    return res.render('admin/create-restaurant')
+  },
+  postRestaurant: (req, res, next) => {
+    const { name, tel, address, openingHours, description } = req.body
+    if (!name) throw new Error('Restaurant name is required!')
+
+    Restaurant.create({
+      name,
+      tel,
+      address,
+      openingHours,
+      description
+    })
+      .then(() => {
+        req.flash('success_messages', 'restaurant was successfully created')
+        res.redirect('/admin/restaurants')
+      })
+      .catch(err => next(err))
+  },
+  getRestaurant: (req, res, next) => {
+    Restaurant.findByPk(req.params.id, { raw: true })
+      .then(restaurant => {
+        if (!restaurant) throw new Error("Restaurant didn't exist!")
+        res.render('admin/restaurant', { restaurant })
+      })
       .catch(err => next(err))
   }
 }
