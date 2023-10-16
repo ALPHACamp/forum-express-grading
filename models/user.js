c038f5293544a1b6c55f0f79877efdfff76bc238
LAPTOP-8420NR94\amber <amber898952@gmail.com>
2023-10-16 22:58:35 +0800

    setting form validation and error messages

---
+++ models/user.js
@@ -0,0 +1,32 @@
+'use strict'
+const {
+  Model
+} = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+  // 先定義一個 User，這個 User 繼承自 Model，可以使用 Model 提供的各種方法
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate (models) {
+      // define association here
+    }
+  }
+  // 呼叫 User.init 方法來定義欄位
+  User.init(
+    {
+      name: DataTypes.STRING,
+      email: DataTypes.STRING,
+      password: DataTypes.STRING
+    },
+    {
+      sequelize,
+      modelName: 'User',
+      tableName: 'Users',
+      underscored: true
+    }
+  )
+  return User
+}
--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,15 @@ const express = require('express')
 const router = express.Router()
 
 const restController = require('../controllers/restaurant-controller') // 載入 controller
+const userController = require('../controllers/user-controller')
+const { generalErrorHandler } = require('../middleware/error-handler')
 const admin = require('./modules/admin')
 
 router.use('/admin', admin)
+router.get('/signup', userController.signUpPage)
+router.post('/signup', userController.signUp)
 router.get('/restaurants', restController.getRestaurants)
 router.use('/', (req, res) => res.redirect('/restaurants')) // 設定fallback 路由，其他路由條件都不符合時，最終會通過的路由。
+router.use('/', generalErrorHandler)
 
 module.exports = router
