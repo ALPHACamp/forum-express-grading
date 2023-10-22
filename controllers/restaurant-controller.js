df13a6296016d4184630db450b09a9457e617fa2
WenfishTseng <twy0171@outlook.com>
2023-10-22 22:17:49 +0800

    feat: show comments on restaurant page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,4 +1,4 @@
-const { Restaurant, Category } = require('../models')
+const { Restaurant, Category, Comment, User } = require('../models')
 const { getOffset, getPagination } = require('../helpers/pagination-helper')
 
 const restaurantController = {
@@ -34,7 +34,11 @@ const restaurantController = {
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
-      include: Category // 拿出關聯的 Category model
+      include: [
+        Category,
+        { model: Comment, include: User }
+      ]
+      // 拿出關聯的 Category model
       // nest: true,  // 要確保 restaurant 是 Sequelize Model
       // raw: true
     })
