3e2ee5509e35d9d8fde9b0d24d9a77a911cc1af8
Antarctic-penguin <jamesantarcticpenguin@gmail.com>
2023-10-22 16:19:38 +0800

    定義資料模型：Comment

--- models/restaurant.js
+++ models/restaurant.js
@@ -12,6 +12,7 @@ module.exports = (sequelize, DataTypes) => {
     static associate (models) {
       // define association here
       Restaurant.belongsTo(models.Category, { foreignKey: 'categoryId' })
+      Restaurant.hasMany(models.Comment, { foreignKey: 'restaurantId' })
     }
   };
   Restaurant.init({
