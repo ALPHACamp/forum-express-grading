31916fb8663a10672ff282809afe69a51df3e8f5
freeanser <bnn01615@gmail.com>
2023-10-23 21:57:14 -0700

    feat: add user profile and comments on profile page

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -1,23 +1,58 @@
-const { Restaurant, Category } = require('../models')
+const { Restaurant, Category, Comment, User } = require('../models')
+const { getOffset, getPagination } = require('../helpers/pagination-helper')
 
 const restaurantController = {
   getRestaurants: (req, res, next) => {
-    return Restaurant.findAll({
-      include: Category,
-      nest: true,
-      raw: true
-    })
-      .then(restaurants => {
-        const data = restaurants.map(r => ({
+    const DEFAULT_LIMIT = 9
+    // req.query.categoryId是指會自動尋找 ? categoryId = 的值，也就是 ?categoryId={{this.id}}
+    // req.query.categoryId 返回字串
+    const categoryId = Number(req.query.categoryId) || ''
+    const page = Number(req.query.page) || 1
+    const limit = Number(req.query.limit) || DEFAULT_LIMIT
+    const offset = getOffset(limit, page)
+
+    return Promise.all([
+      // Sequelize 的 findAndCountAll，這個方法會在撈資料時一併計算資料量，此時回傳結果會有兩個部分：
+      // count - 資料量
+      // rows - 餐廳資料集合
+      Restaurant.findAndCountAll({
+        include: Category,
+        where: {
+          ...categoryId ? { categoryId } : {}
+        },
+        limit,
+        offset,
+        nest: true,
+        raw: true
+      }),
+      Category.findAll({
+        raw: true
+      })
+    ])
+      .then(([restaurants, categories]) => {
+        const data = restaurants.rows.map(r => ({
           ...r,
           description: r.description.substring(0, 50)
         }))
-        return res.render('restaurants', { restaurants: data })
+        return res.render('restaurants', {
+          restaurants: data,
+          categories,
+          categoryId,
+          pagination: getPagination(limit, page, restaurants.count)
+        })
       })
+      .catch(err => next(err))
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
-      include: Category,
+      include: [
+
+        Category,
+        {
+          model: Comment, include: User
+        }
+
+      ],
       nest: true
       // raw: true
     })
--- models/user.js
+++ models/user.js
@@ -11,13 +11,15 @@ module.exports = (sequelize, DataTypes) => {
      */
     static associate(models) {
       // define association here
+      User.hasMany(models.Comment, { foreignKey: 'userId' })
     }
   };
   User.init({
     name: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING,
-    isAdmin: DataTypes.BOOLEAN
+    isAdmin: DataTypes.BOOLEAN,
+    image: DataTypes.STRING
   }, {
     sequelize,
     modelName: 'User',
