86cc6a0890ee0e388a2dbd45631b947f93f5b009
WenfishTseng <twy0171@outlook.com>
2023-10-28 17:17:07 +0800

    feat: add restaurant-services

--- controllers/apis/restaurant-controller.js
+++ controllers/apis/restaurant-controller.js
@@ -1,43 +1,10 @@
-const { Restaurant, Category } = require('../../models')
-const { getOffset, getPagination } = require('../../helpers/pagination-helper')
+const restaurantServices = require('../../services/restaurant-services')
+
+// Controllers - 處理流程控制，負責接收資料來源及整理回傳結果
 const restaurantController = {
   // 餐廳首頁
-  getRestaurants: (req, res) => {
-    const DEFAULT_LIMIT = 9
-    const categoryId = Number(req.query.categoryId) || ''
-    const page = Number(req.query.page) || 1
-    const limit = Number(req.query.limit) || DEFAULT_LIMIT
-    const offset = getOffset(limit, page)
-    Promise.all([
-      Restaurant.findAndCountAll({
-        include: Category,
-        where: {
-          ...categoryId ? { categoryId } : {}
-        },
-        limit,
-        offset,
-        nest: true,
-        raw: true
-      }),
-      Category.findAll({ raw: true })
-    ])
-      .then(([restaurants, categories]) => {
-        const favoritedRestaurantsId = req.user?.FavoritedRestaurants ? req.user?.FavoritedRestaurants.map(fr => fr.id) : []
-        const likedRestaurantsId = req.user?.LikedRestaurants ? req.user?.LikedRestaurants.map(lr => lr.id) : []
-        const data = restaurants.rows.map(r => ({
-          ...r,
-          description: r.description.substring(0, 50),
-          isFavorited: favoritedRestaurantsId.includes(r.id),
-          isLiked: likedRestaurantsId.includes(r.id)
-        }))
-        // 樣板改為 JSON 格式
-        return res.json({
-          restaurants: data,
-          categories,
-          categoryId,
-          pagination: getPagination(limit, page, restaurants.count)
-        })
-      })
+  getRestaurants: (req, res, next) => {
+    restaurantServices.getRestaurants(req, (err, data) => err ? next(err) : res.json(data))
   }
 }
 module.exports = restaurantController
--- controllers/pages/restaurant-controller.js
+++ controllers/pages/restaurant-controller.js
@@ -1,40 +1,10 @@
 const { Restaurant, Category, Comment, User } = require('../../models')
-const { getOffset, getPagination } = require('../../helpers/pagination-helper')
+const restaurantServices = require('../../services/restaurant-services')
 
+// Controllers - 處理流程控制，負責接收資料來源及整理回傳結果
 const restaurantController = {
   getRestaurants: (req, res, next) => {
-    const DEFAULT_LIMIT = 9
-    const page = Number(req.query.page) || 1
-    const limit = Number(req.query.limit) || DEFAULT_LIMIT
-    const offset = getOffset(limit, page)
-    const categoryId = Number(req.query.categoryId) || ''
-    return Promise.all([Restaurant.findAndCountAll({
-      include: Category,
-      where: { // 新增查詢條件
-        ...categoryId ? { categoryId } : {} // 檢查 categoryId 是否為空值
-      },
-      limit,
-      offset,
-      nest: true,
-      raw: true
-    }), Category.findAll({ raw: true })])
-      .then(([restaurants, categories]) => {
-        const favoritedRestaurantsId = req.user && req.user.FavoritedRestaurants.map(fr => fr.id) // 找出有被收藏的餐廳
-        const likedRestaurantsId = req.user && req.user.LikedRestaurants.map(fr => fr.id) // 找出有被喜歡的餐廳
-        const data = restaurants.rows.map(restaurant => ({
-          ...restaurant,
-          description: restaurant.description.substring(0, 50),
-          isFavorited: favoritedRestaurantsId.includes(restaurant.id), // 比對收藏清單
-          isLiked: likedRestaurantsId.includes(restaurant.id) // 比對喜歡清單
-        }))
-        return res.render('restaurants', {
-          restaurants: data,
-          categories,
-          categoryId,
-          pagination: getPagination(limit, page, restaurants.count)
-        })
-      })
-      .catch(err => next(err))
+    restaurantServices.getRestaurants(req, (err, data) => err ? next(err) : res.render('restaurants', data))
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
