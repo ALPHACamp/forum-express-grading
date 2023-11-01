82990c4e9525cb87ce7e5eae3dd3bb28731594e4
LAPTOP-8420NR94\amber <amber898952@gmail.com>
2023-11-01 17:48:28 +0800

    category delete

--- routes/modules/admin.js
+++ routes/modules/admin.js
@@ -18,6 +18,7 @@ router.get('/users', adminController.getUsers)
 
 router.get('/categories/:id', categoryController.getCategories)
 router.put('/categories/:id', categoryController.putCategory)
+router.delete('/categories/:id', categoryController.deleteCategory)
 router.get('/categories', categoryController.getCategories)
 router.post('/categories', categoryController.postCategory)
 
