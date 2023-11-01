097fc40158644026f6ea03976b588e66990151a7
LAPTOP-8420NR94\amber <amber898952@gmail.com>
2023-11-01 17:31:37 +0800

    category update

--- controllers/category-controller.js
+++ controllers/category-controller.js
@@ -2,10 +2,15 @@ const { Category } = require('../models')
 
 const categoryController = {
   getCategories: (req, res, next) => {
-    return Category.findAll({
-      raw: true
-    })
-      .then(categories => res.render('admin/categories', { categories }))
+    return Promise.all([
+      Category.findAll({ raw: true }),
+      req.params.id ? Category.findByPk(req.params.id, { raw: true }) : null
+      // 也就是說，當req.params.id存在，那我控制器便從資料庫尋找該id的資料
+    ])
+      .then(([categories, category]) =>
+        // category便是該id的資料，最後回傳至前端
+        res.render('admin/categories', { categories, category })
+      )
       .catch(err => next(err))
   },
   postCategory: (req, res, next) => {
@@ -14,6 +19,17 @@ const categoryController = {
     if (!name) throw new Error('Category name is required!')
 
     return Category.create({ name })
+      .then(() => res.redirect('/admin/categories'))
+      .catch(err => next(err))
+  },
+  putCategory: (req, res, next) => {
+    const { name } = req.body
+    if (!name) throw new Error('Category name is required!')
+    return Category.findByPk(req.params.id)
+      .then(category => {
+        if (!category) throw new Error("Category doesn't exist!")
+        return category.update({ name })
+      })
       .then(() => res.redirect('/admin/categories'))
       .catch(err => next(err))
   }
