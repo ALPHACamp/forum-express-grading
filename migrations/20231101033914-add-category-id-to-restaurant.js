40858c5622a047ab02e48beff5ba145b5d787f39
LAPTOP-8420NR94\amber <amber898952@gmail.com>
2023-11-01 12:36:18 +0800

    setting up data associations and updating seed data for Restaurant & Category

---
+++ migrations/20231101033914-add-category-id-to-restaurant.js
@@ -0,0 +1,20 @@
+'use strict'
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    // 直接操作資料庫欄位，因次使用 snake case
+    // 使用queryInterface.addColumn來新增欄位
+    await queryInterface.addColumn('Restaurants', 'category_id', {
+      type: Sequelize.INTEGER,
+      allowNull: false, // 不允許空值
+      // 非常重要的關聯，當這筆migration生效時，一併將關聯設定起來
+      // 也就是告訴資料夾說，在設定這個欄位時，順便將其關聯
+      references: {
+        model: 'Categories',
+        key: 'id'
+      }
+    })
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('Restaurants', 'category_id')
+  }
+}
--- seeders/20231023102737-restaurants-seed-file.js
+++ seeders/20231023102737-restaurants-seed-file.js
@@ -2,6 +2,15 @@
 const faker = require('faker')
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    // 為了將Categories的id放置進Restaurants，首先是先了解Categories的id有多少，且其id值為多少
+    const categories = await queryInterface.sequelize.query(
+      'SELECT id FROM Categories;', // 使用SQL 查詢語句，查詢Categories' 表中的所有 'id' 欄位
+      // type: 指定查詢的類型
+      // QueryTypes.SELECT 表明這是一個選擇查詢（SELECT query），目的是獲取資料而非修改資料
+      { type: queryInterface.sequelize.QueryTypes.SELECT }
+    )
+    // 出來的值，會是一個陣列，[{id:1},{id:2},{id:3}]
+
     await queryInterface.bulkInsert(
       'Restaurants',
       Array.from({ length: 50 }, () => ({
@@ -14,7 +23,8 @@ module.exports = {
         }`,
         description: faker.lorem.text(),
         created_at: new Date(),
-        updated_at: new Date()
+        updated_at: new Date(),
+        category_id: categories[Math.floor(Math.random() * categories.length)].id
       }))
     )
   },
