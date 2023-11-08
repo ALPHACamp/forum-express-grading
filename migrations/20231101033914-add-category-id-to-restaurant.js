bb77402cf0eb695db49884a62bd82933e9ef6d6d
LAPTOP-8420NR94\amber <amber898952@gmail.com>
2023-11-08 17:38:24 +0800

    modify the migrations

--- migrations/20231101033914-add-category-id-to-restaurant.js
+++ migrations/20231101033914-add-category-id-to-restaurant.js
@@ -5,7 +5,7 @@ module.exports = {
     // 使用queryInterface.addColumn來新增欄位
     await queryInterface.addColumn('Restaurants', 'category_id', {
       type: Sequelize.INTEGER,
-      allowNull: false, // 不允許空值
+      allowNull: true, // 不允許空值
       // 非常重要的關聯，當這筆migration生效時，一併將關聯設定起來
       // 也就是告訴資料夾說，在設定這個欄位時，順便將其關聯
       references: {
