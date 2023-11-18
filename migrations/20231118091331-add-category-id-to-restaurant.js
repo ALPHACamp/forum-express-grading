eed6246d9b86244eb53177615196e1e48ea7e1df
weitb0315 <weitb0315@gmail.com>
2023-11-18 17:28:45 +0800

    feat: add category model

---
+++ migrations/20231118091331-add-category-id-to-restaurant.js
@@ -0,0 +1,18 @@
+'use strict'
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn('Restaurants', 'category_id', {
+      type: Sequelize.INTEGER,
+      allowNull: false, // 不允許空值，也就是說餐廳類別是必填，否則無法寫入資料。
+      references: {
+        model: 'Categories',
+        key: 'id'
+      }
+    })
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn('Restaurants', 'category_id')
+  }
+}
