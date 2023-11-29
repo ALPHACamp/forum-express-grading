84bdcc1e448b88a5a49c1796302962e14fae491c
LAPTOP-8420NR94\amber <amber898952@gmail.com>
2023-11-29 20:37:04 +0800

    merge the R03

--- routes/index.js
+++ routes/index.js
@@ -7,30 +7,40 @@ const commentController = require('../controllers/comment-controller')
 const { authenticated } = require('../middleware/auth') // 引入用來驗證是否登入
 const { authenticatedAdmin } = require('../middleware/auth')
 const { generalErrorHandler } = require('../middleware/error-handler')
+const upload = require('../middleware/multer')
 const admin = require('./modules/admin')
 
-router.use('/admin', authenticatedAdmin, admin)
+// 身份驗證和註冊
 router.get('/signup', userController.signUpPage)
 router.post('/signup', userController.signUp)
 router.get('/signin', userController.signInPage)
-router.post(
-  '/signin',
-  passport.authenticate('local', {
-    failureRedirect: '/signin',
-    failureFlash: true
-  }),
-  userController.signIn
-) // 注意是 post
+router.post('/signin', passport.authenticate('local', {
+  failureRedirect: '/signin',
+  failureFlash: true
+}), userController.signIn)
 router.get('/logout', userController.logout)
+
+// Admin 相關路由
+router.use('/admin', authenticatedAdmin, admin)
+
+// 具體用戶路由
+router.get('/users/:id/edit', authenticated, userController.editUser)
+router.put('/users/:id/edit', authenticated, upload.single('image'), userController.putUser)
+router.get('/users/:id', authenticated, userController.getUser)
+
+// 餐廳路由
 router.get('/restaurants/:id/dashboard', restController.getDashboard)
 router.get('/restaurants/:id', restController.getRestaurant)
 router.get('/restaurants', authenticated, restController.getRestaurants)
 
+// 評論路由
+router.post('/comments', authenticated, commentController.postComment)
 router.delete('/comments/:id', authenticatedAdmin, commentController.deleteComment)
 
-router.post('/comments', authenticated, commentController.postComment)
+// Fallback 路由
+router.use('/', (req, res) => res.redirect('/restaurants'))
 
-router.use('/', (req, res) => res.redirect('/restaurants')) // 設定fallback 路由，其他路由條件都不符合時，最終會通過的路由。
-router.use('/', generalErrorHandler)
+// 通用錯誤處理器
+router.use(generalErrorHandler)
 
 module.exports = router
