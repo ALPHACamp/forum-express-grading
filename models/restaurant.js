daa771777a80ac0de6e581bbd711251b3d9b2686
weitb0315 <weitb0315@gmail.com>
2023-12-15 10:14:55 +0800

    feat: get user's liked restaurants & switch button

--- models/restaurant.js
+++ models/restaurant.js
@@ -17,6 +17,11 @@ module.exports = (sequelize, DataTypes) => {
         foreignKey: 'restaurantId', // 對 Favorite 表設定 FK
         as: 'FavoritedUsers' // 幫這個關聯取個名稱
       })
+      Restaurant.belongsToMany(models.User, {
+        through: models.Like,
+        foreignKey: 'restaurantId',
+        as: 'LikedUsers'
+      })
     }
   };
   Restaurant.init({
